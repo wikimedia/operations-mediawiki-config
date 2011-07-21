b5d57d04700187f56e0ea543f11d6bf0b386bd5c
Aaron Schulz <aaron@users.mediawiki.org>
2011-07-21 07:40:07 +0000

    * Removed cdbmake-12.sh, not sure what this was for * Added populateWikiVersions.php script to populate wikiversions.cdb from all.dblist

---
+++ populateWikiversionCDB.php
@@ -0,0 +1,49 @@
+<?php
+if ( php_sapi_name() !== 'cli' ) {
+	die( 'This script can only be run from the command line.' );
+}
+
+error_reporting( E_ALL );
+
+/*
+ * Populate wikiversions.cdb file using all the items in all.dblist
+ * as keys, each having a value of the specified MediaWiki version.
+ * This means that all will be configured to run that version.
+ *
+ * The first argument is the version, typically of the format "php-X.XX".
+ *
+ * @return void
+ */
+function populateWikiversionsCDB() {
+	global $argv;
+
+	$argsValid = false;
+	if ( count( $argv ) >= 2 ) {
+		$version = $argv[1]; // e.g. "php-X.XX"
+		if ( preg_match( '/^php-(\d+\.\d+|trunk)$/', $version ) ) {
+			$argsValid = true;
+		}
+	}
+
+	if ( !$argsValid ) {
+		die( "Usage: populateWikiVersionsCDB.php php-X.XX" );
+	}
+
+	$path = '/home/wikipedia/common/all.dblist';
+	$dbList = explode( "\n", file_get_contents( $path ) );
+	if ( !count( $dbList ) ) {
+		die( "Unable to read all.dblist." );
+	}
+
+	$wikiVersionList = '';
+	foreach ( $dbList as $dbName ) {
+		$wikiVersionList .= "$dbName $version\n";
+	}
+
+	$path = '/home/wikipedia/common/wikiversions.cdb';
+	if ( !file_put_contents( $path, $wikiVersionList ) ) {
+		die( "Unable to write to wikiversions.cdb." );
+	}
+}
+
+populateWikiversionsCDB();
