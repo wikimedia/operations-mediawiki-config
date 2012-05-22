00166adc06cdc174291c3fd2deac11774f6881e7
Antoine Musso <hashar@free.fr>
2012-05-22 13:31:30 +0200

    database & memcached configuration for 'beta' project
    I prefer switch(), makes things easier to follow in my opinion.
    
    Since configuration are entirely different, I am using different
    filename using clustername as a suffix. To avoid causing any back
    compatibility issue, production files rename unchanged. End result is:
    
     - db.php Database configuration for production cluster
     - mc.php Memcached configuration for production cluster
     - db-wmflabs.php Database configuration for beta on labs
     - mc-wmflabs.php Memcached configuration for beta on labs
    
    Change-Id: I81479e17fa48fa056f8ed8ec03edb9a1f15f1861

---
+++ wmf-config/mc-wmflabs.php
@@ -0,0 +1,29 @@
+<?php
+# WARNING: This file is publically viewable on the web.
+#          Do not put private data here.
+if( $cluster == 'wmflabs' ) {  # safe guard
+
+/*
+ * Before altering the wgMemCachedServers array below, make sure you planned
+ * your change. Memcached compute a hash of the data and given the hash
+ * assign the value to one of the servers.
+ * If you remove / comment / change order of servers, the hash will miss
+ * and that can result in bad performance for the cluster !
+ *
+ * Hashar, based on dammit comments. Nov 28 2005.
+ *
+ */
+$wgMemCachedPersistent = true;
+$wgUseMemCached = true;
+$wgMainCacheType = CACHE_MEMCACHED;
+$wgMemCachedTimeout = 500000; # micro seconds
+$wgSessionsInMemcached = true;
+
+$wgMemCachedInstanceSize = 2000;
+
+$wgMemCachedServers = array(
+0 => 'deployment-mc:11000',
+);
+
+
+} # end safe guard
