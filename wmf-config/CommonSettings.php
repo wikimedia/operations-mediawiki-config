48fe48add72036cfb3bcb864a9fdbd9cbc10452c
Antoine Musso <hashar@free.fr>
2012-05-28 18:29:50 +0200

    move throttling related conf to throttle.php
    Not sure that the UserThrottle.php is still working, but I am just doing
    a verbatim copy and paste for now.
    
    Change-Id: Idbf6b1bdef7ebd42d873846f89581a95f8b417fe

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -1377,23 +1377,6 @@ if ( $wgDBname == 'enwikisource' ) {
 	);
 }
 
-// Added throttle for account creations on zh due to mass registration attack 2005-12-16
-// might be useful elesewhere. --brion
-// disabled temporarily due to tugela bug -- Tim
-
-if ( false /*$lang == 'zh' || $lang == 'en'*/ ) {
-	require( "$IP/extensions/UserThrottle/UserThrottle.php" );
-	$wgGlobalAccountCreationThrottle = array(
-/*
-		'min_interval' => 30,   // Hard minimum time between creations (default 5)
-		'soft_time'    => 300, // Timeout for rolling count
-		'soft_limit'   => 5,  // 5 registrations in five minutes (default 10)
-*/
-		'min_interval' => 0,   // Hard minimum time between creations (default 5)
-		'soft_time'    => 60, // Timeout for rolling count (default 5 minutes)
-		'soft_limit'   => 2,  // 2 registrations in one minutes (default 10)
-	);
-}
 
 // Customize URL handling for secure.wikimedia.org HTTPS logins
 if ( $secure ) {
@@ -1839,15 +1822,8 @@ if ( $wmgUseCollection ) {
 
 include( "$IP/extensions/OpenSearchXml/OpenSearchXml.php" );
 
-function efRaiseThrottle() {
-	global $wgAccountCreationThrottle;
-	if ( wfGetIP() == '200.156.24.98' ) {
-		$wgAccountCreationThrottle = 40;
-	}
-}
-if ( $wgDBname == 'ptwiki' && time() >= strtotime( '2012-05-22T16:00 +0:00' ) && time() <= strtotime( '2012-05-22T21:00 +0:00' ) ) {
-	$wgExtensionFunctions[] = 'efRaiseThrottle';
-}
+# Various system to allow/prevent flooding
+require( "$wmfConfigDir/throttle.php" );
 
 
 if ( $wmgUseNewUserMessage ) {
