cfdb5d84a95b841ec3d3e2c8a65ba72a736e961b
Antoine Musso <hashar@free.fr>
2012-05-29 12:19:17 +0200

    import overriding system for InitialiseSettings.php
    Hacky but works. Will probably need to be reorganized later on. Please
    note this is directly included from InitialiseSettings.php so we are
    sure we override production settings whenever InitialiseSettings.php is
    included/required.
    
    Change-Id: I3e55b7ee69dbde024d19479d557c1f9490fe58bb

---
+++ wmf-config/InitialiseSettings-wmflabs.php
@@ -0,0 +1,175 @@
+<?php
+/**
+ * This file is for overriding the default InitialiseSettings.php with our own
+ * stuff. Prefixing a setting key with '-' to override all values from
+ * InitialiseSettings.php
+ *
+ * Please wrap your code in functions to avoid tainting the global namespace.
+ */
+
+/**
+ * Main entry point to override production settings. Supports key beginning wit
+ * a dash to completely override a setting.
+ * Settings are fetched through wmfLabsSettings() defined below.
+ */
+function wmfLabsOverrideSettings() {
+	global $wmfConfigDir, $wgConf;
+
+	// Override (or add) settings that we need within the labs environment,
+	// but not in production.
+	$betaSettings = wmfLabsSettings();
+
+	foreach ( $betaSettings as $key => $value ) {
+		if ( substr( $key, 0, 1 ) == '-' ) {
+			// Settings prefixed with - are completely overriden
+			$wgConf->settings[substr( $key, 1 )] = $value;
+		} elseif ( isset( $wgConf->settings[$key] ) ) {
+			$wgConf->settings[$key] = array_merge( $wgConf->settings[$key], $value );
+		} else {
+			$wgConf->settings[$key] = $value;
+		}
+	}
+}
+
+/**
+ * Return settings for wmflabs cluster. This is used by wmfLabsOverride().
+ */
+function wmfLabsSettings() {
+return array(
+	'wgParserCacheType' => array(
+		'default' => CACHE_MEMCACHED,
+	),
+
+	'wgSitename' => array(
+		'labswiki' => 'Deployment',
+		'ee_prototypewiki' => 'Editor Engagement Prototype',
+	),
+
+	'wgServer' => array(
+		'default'     => '//$lang.wikipedia.beta.wmflabs.org',
+		'incubatorwiki'	=> '//incubator.wikimedia.beta.wmflabs.org',
+		'wiktionary'	=> '//$lang.wiktionary.beta.wmflabs.org',
+		'wikipedia'     => '//$lang.wikipedia.beta.wmflabs.org',
+		'wikiversity'	=> '//$lang.wikiversity.beta.wmflabs.org',
+		'wikispecies'	=> '//$lang.wikispecies.beta.wmflabs.org',
+		'wikisource'	=> '//$lang.wikisource.beta.wmflabs.org',
+		'wikiquote'	=> '//$lang.wikiquote.beta.wmflabs.org',
+		'wikinews'	=> '//$lang.wikinews.beta.wmflabs.org',
+		'wikibooks'     => '//$lang.wikibooks.beta.wmflabs.org',
+
+		'metawiki'      => '//meta.wikimedia.beta.wmflabs.org',
+		'commonswiki'   => '//commons.wikimedia.beta.wmflabs.org',
+		'labswiki'      => '//deployment.wikimedia.beta.wmflabs.org',
+		'ee_prototypewiki' => '//ee-prototype.wikipedia.beta.wmflabs.org',
+		'testwiki'      => '//test.wikimedia.beta.wmflabs.org',
+	),
+
+	'wgCanonicalServer' => array(
+		'default'     => 'http://$lang.wikipedia.beta.wmflabs.org',
+		'wikipedia'     => 'http://$lang.wikipedia.beta.wmflabs.org',
+		'wikibooks'     => 'http://$lang.wikibooks.beta.wmflabs.org',
+		'wikiquote'	=> 'http://$lang.wikiquote.beta.wmflabs.org',
+		'wikinews'	=> 'http://$lang.wikinews.beta.wmflabs.org',
+		'wikisource'	=> 'http://$lang.wikisource.beta.wmflabs.org',
+		'wikiversity'     => 'http://$lang.wikiversity.beta.wmflabs.org',
+		'wiktionary'     => 'http://$lang.wiktionary.beta.wmflabs.org',
+		'wikispecies'     => 'http://$lang.wikispecies.beta.wmflabs.org',
+
+		'incubatorwiki'	=> 'http://incubator.wikimedia.beta.wmflabs.org',
+		'metawiki'      => 'http://meta.wikimedia.beta.wmflabs.org',
+		'ee_prototypewiki' => 'http://ee-prototype.wikipedia.beta.wmflabs.org',
+		'commonswiki'	=> 'http://commons.wikimedia.beta.wmflabs.org',
+		'labswiki'      => 'http://deployment.wikimedia.beta.wmflabs.org',
+		'testwiki'      => 'http://test.wikimedia.beta.wmflabs.org',
+	),
+
+	'wmgUsabilityPrefSwitch' => array(
+		'default' => ''
+	),
+
+	'wmgUseLiquidThreads' => array(
+		'testwiki' => true,
+	),
+
+	'-wgUploadDirectory' => array(
+		'default'      => '/mnt/upload6/$site/$lang',
+	),
+
+	/* 'wmgUseOnlineStatusBar' => array( */
+	/* 	'default' => false, */
+	/* ), */
+
+	'-wgThumbnailScriptPath' => array(
+		'default' => '/w/thumb.php',
+	),
+
+	'-wgUploadPath' => array(
+		'default' => '//upload.beta.wmflabs.org/$site/$lang',
+	//	'private' => '/w/img_auth.php',
+	//	'wikimania2005wiki' => '//upload..org/wikipedia/wikimania', // back compat
+		'commonswiki' => '//upload.beta.wmflabs.org/wikipedia/commons',
+		'metawiki' => '//upload.beta.wmflabs.org/wikipedia/meta',
+		'testwiki' => '//upload.beta.wmflabs.org/wikipedia/test',
+	),
+
+	'-wgMathDirectory' => array(
+		'default' => '/mnt/upload6/math',
+	),
+
+	'-wgMathPath' => array(
+		'default' => '//upload.beta.wmflabs.org/math',
+	),
+
+	'wmgNoticeProject' => array(
+		'labswiki' => 'meta',
+	),
+
+	//'-wgDebugLogGroups' => array(),
+	'-wgRateLimitLog' => array(),
+	'-wgJobLogFile' => array(),
+
+	'-wgRC2UDPAddress' => array(
+		'default' => false,
+	),
+
+	'wmgUseWebFonts' => array(
+		'mywiki' => true,
+        ),
+
+        'wgLogo' => array(
+//		'commonswiki'       => '//commons.wikimedia.beta.wmflabs.org/w/thumb.php?f=Wiki.png&width=88&a',
+	),
+
+	'-wmfUseArticleCreationWorkflow' => array(
+		'default' => false,
+	),
+
+	'-wmgUsePoolCounter' => array(
+		'default' => false, # bug 36891
+	),
+	'-wmgUseAPIRequestLog' => array(
+		'default' => false,
+	),
+	'-wmgEnablePageTriage' => array(
+		# hashar, pending database update
+		'default' => false,
+	),
+	'-wmgEnableCaptcha' => array(
+		'default' => false,
+	),
+	'-wmgClickTracking' => array(
+		'default' => false,
+	),
+	# FIXME: make that settings to be applied
+	'-wgShowExceptionDetails' => array(
+		'default' => true,
+	),
+	'-wgUseContributionTracking' => array(
+		'default' => false,
+	),
+	'-wmgUseContributionReporting' => array(
+		'default' => false,
+	),
+);
+
+} # wmflLabsSettings()
