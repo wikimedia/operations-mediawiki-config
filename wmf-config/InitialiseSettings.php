a663eba98f8b0213bf4412c7e2a3ee6d8b66680b
Kaldari <rkaldari@wikimedia.org>
2012-05-30 18:51:08 -0700

    turning pagetriage off on test2 since the sql files need to be run there
    Change-Id: I3c1fcfdc683c7eb788d1ae593a6a2b74dd865348

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -10847,7 +10847,6 @@ $wgConf->settings = array(
 'wmgEnablePageTriage' => array(
 	'default' => false,
 	'testwiki' => true,
-	'test2wiki' => true,
 	'enwiki' => true,
 ),
 'wmgEnableInterwiki' => array(
