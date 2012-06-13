c0baf3e1129af796151c3a7b50cb634c1dc09538
Antoine Musso <hashar@free.fr>
2012-06-13 10:40:03 +0200

    Revert "(bug 37482) Adding Proofread Page ext. namespaces on nl.wikisource"
    This reverts commit 661c6cc862b33742d18d7c59d7e911770290ccce which does
    not use the correct global variable (Aliases instead of $wgExtraNamespace)
    
    Change-Id: If28aeb2f57258caa9ad04d99d3038f006efc544d

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -2797,10 +2797,6 @@ $wgConf->settings = array(
 	'nlwikisource' => array(
 		'Author' => 102,
 		'Author_talk' => 103,
-		'Pagina' => 104, // Bug 37482
-		'Overleg_pagina' => 105, // Bug 37482
-		'Index' => 106, // Bug 37482
-		'Overleg_index' => 107, // Bug 37482
 	),
 	'nnwiki' => array(
 		 'WP' => NS_PROJECT,
