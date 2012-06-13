9df043806e130c9771628d280d71dc9cd8113da2
Dereckson <dereckson@espace-win.org>
2012-06-13 12:52:13 +0200

    (bug 36972) activate the patroller group on nn.wiki
    Change-Id: I79aa411baecfab319e755d99c185a7b336c4120f

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -6889,7 +6889,8 @@ $wgConf->settings = array(
 	'nlwikibooks' => array( 'user' => array( 'patrol' => true ) ),
 	// 'nlwikinews' => array( 'user' => array( 'patrol' => true ) ),
 	'nnwiki' => array(
-	'autopatrolled' => array( 'autopatrol' => true ),
+		'autopatrolled' => array( 'autopatrol' => true ),
+		'patroller' => array( 'autopatrol' => true, 'patrol' => true, 'rollback' => true ),
 	),
 	'nowiki' => array(
 		'patroller' => array( 'patrol' => true, 'autopatrol' => true, 'rollback' => true, 'unwatchedpages' => true, 'suppressredirect' => true, ),
@@ -7435,8 +7436,8 @@ $wgConf->settings = array(
 		'sysop' => array( 'abusefilter' ),
 	),
 	'+nnwiki' => array(
-		'bureaucrat' => array( 'autopatrolled', ),
-		'sysop' => array( 'autopatrolled', ),
+		'bureaucrat' => array( 'autopatrolled', 'patroller' ),
+		'sysop' => array( 'autopatrolled', 'patroller' ),
 	),
 	'+noboard_chapterswikimedia' => array(
 		'bureaucrat' => array( 'import', 'transwiki' ),
@@ -7814,8 +7815,8 @@ $wgConf->settings = array(
 		'sysop' => array( 'abusefilter' ),
 	),
 	'+nnwiki' => array(
-		'bureaucrat' => array( 'autopatrolled', ),
-		'sysop' => array( 'autopatrolled', ),
+		'bureaucrat' => array( 'autopatrolled', 'patroller' ),
+		'sysop' => array( 'autopatrolled' ),
 	),
 	'+noboard_chapterswikimedia' => array(
 		'bureaucrat' => array( 'sysop', 'bureaucrat', 'import', 'transwiki' ),
