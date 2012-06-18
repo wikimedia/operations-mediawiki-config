aeab5971dc14a4cfc4245d81354a8fb6355721aa
Dereckson <dereckson@espace-win.org>
2012-06-18 11:33:09 +0200

    (bug 37675) ruwiki: enable review on Portal:
    Adding namespace 100 (Portal) to flagged revisions namespaces.
    
    Change-Id: Ie32777f361cb2081a0d89d8b06517912e4415471

--- wmf-config/flaggedrevs.php
+++ wmf-config/flaggedrevs.php
@@ -485,7 +485,10 @@ elseif ( $wgDBname == 'ptwikisource' ) {
 }
 
 elseif ( $wgDBname == 'ruwiki' ) {
-	$wgFlaggedRevsNamespaces[] = NS_CATEGORY;
+
+	// Bug 37675
+	$wgFlaggedRevsNamespaces = array( NS_CATEGORY, 100 );
+
 	$wgFlaggedRevTags['accuracy']['levels'] = 3; // Is this needed?
 	$wgFlaggedRevsOverride = false;
 
