ee70e2ea0e5ad2490dec94aac8a81aded588339d
Reedy <reedy@wikimedia.org>
2012-06-20 14:50:27 +0000

    Disabled api purge on commons (Tim did this last night)
    Change-Id: I11c9dcd40c204124379d7cb43584b0cf733dfa20

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -2451,6 +2451,11 @@ case 'wmflabs':
 // https://bugzilla.wikimedia.org/show_bug.cgi?id=37211
 $wgUseCombinedLoginLink = false;
 
+// Disable ApiPurge temporarily due to abuse
+if ( $wgDBname == 'commonswiki' ) {
+	$wgAPIModules['purge'] = 'ApiDisabled';
+}
+
 # THIS MUST BE AFTER ALL EXTENSIONS ARE INCLUDED
 #
 # REALLY ... we're not kidding here ... NO EXTENSIONS AFTER
