4bbdf6574e1d74913a611737758a6cdec39224bf
Tim Starling <tstarling@wikimedia.org>
2012-07-10 22:35:07 +1000

    Script for streaming favicon.ico
    to allow document roots to be merged
    
    Change-Id: Iebbfbb3ffb83520917576b98cb35d70de230cc64

---
+++ live-1.5/favicon.php
@@ -0,0 +1,50 @@
+<?php
+define( "MEDIAWIKI", true );
+
+require( "/apache/common/live-1.5/MWVersion.php" );
+require( getMediaWiki( "includes/WebStart.php" ) );
+
+function faviconShowError( $html ) {
+	header( 'Content-Type: text/html; charset=utf-8' );
+	echo "<html><body>$html</body></html>\n";
+}
+
+function streamFavicon() {
+	global $wgFavicon;
+	wfResetOutputBuffers();
+	if ( $wgFavicon === '/favicon.ico' ) {
+		# That's not very helpful, that's where we are already
+		header( 'HTTP/1.1 404 Not Found' );
+		faviconShowError( "\$wgFavicon is configured incorrectly, " . 
+			"it must be set to something other than /favicon.ico\n" );
+		return;
+	}
+
+	$req = RequestContext::getMain()->getRequest();
+	if ( $req->getHeader( 'X-Favicon-Loop' ) !== false ) {
+		header( 'HTTP/1.1 500 Internal Server Error' );
+		faviconShowError( "Proxy forwarding loop detected" );
+		return;
+	}
+
+	$url = wfExpandUrl( $wgFavicon, PROTO_INTERNAL );
+	$client = MWHttpRequest::factory( $url );
+	$client->setHeader( 'X-Favicon-Loop', '1' );
+	
+	$status = $client->execute();
+	if ( !$status->isOK() ) {
+		header( 'HTTP/1.1 500 Internal Server Error' );
+		faviconShowError( htmlspecialchars( 
+			"Failed to fetch URL \"$url\"" ) );
+		return;
+	}
+
+	$content = $client->getContent();
+	header( 'Content-Length: ' . strlen( $content ) );
+	header( 'Content-Type: ' . $client->getResponseHeader( 'Content-Type' ) );
+	header( 'Cache-Control: public' );
+	echo $content;
+}
+
+streamFavicon();
+
