b08b84fb2f96106c27094c8baabb4c064f017c48
Krenair <krenair@gmail.com>
2012-07-23 19:33:15 +0100

    (bug 19569) Add Portal namespace to urwiki.
    Change-Id: Id387a2b6c598adbbaee037f3978fd7d42f486463

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -3125,6 +3125,9 @@ $wgConf->settings = array(
 		'Wiktionary' => NS_PROJECT,
 		'ВС' => NS_PROJECT,
 	),
+	'urwiki' => array(
+	    'Portal' => 100,
+	),
 	'vecwikisource' => array(
 		'Author' => 100,
 		'Author_talk' => 101,
@@ -4944,6 +4947,9 @@ $wgConf->settings = array(
 	101 => 'Портал бәхәсе',
 	),
 	'ukwiki' => array( 100 => 'Портал', 101 => 'Обговорення_порталу' ),
+	'urwiki' => array(
+	    100 => 'باب',
+	),
 	'vecwiki' => array(
 		100 => 'Portałe',
 		101 => 'Discussion_portałe',
