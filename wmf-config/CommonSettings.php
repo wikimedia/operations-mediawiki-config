731422df51c32d4cd6970b88ea04f316682c1442
Kaldari <rkaldari@wikimedia.org>
2012-07-25 13:53:19 -0700

    Re-implementing wgNoticeFundraisingUrl override for wmf7 wikis
    Change-Id: Ib5d34e14bfe8ede2055ee72cdc249db2ed119004

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -1711,6 +1711,9 @@ if ( $wmgUseCentralNotice ) {
 		$wgNoticeInfrastructure = true;
 	}
 
+	// Set fundraising banners to use HTTPS on foundatoin wiki
+	$wgNoticeFundraisingUrl = 'https://donate.wikimedia.org/wiki/Special:LandingCheck';
+
 	// No caching for banners on testwiki, so we can develop them there a bit faster - NeilK 2012-01-16
 	// Never set this to zero on a highly trafficked wiki, there are server-melting consequences
 	if ( $wgDBname == 'testwiki' ) {
