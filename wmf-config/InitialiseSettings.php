125bf87ba52eccfc679d060f295a50709730e49d
Ori Livneh <ori.livneh@gmail.com>
2012-07-26 12:55:24 -0700

    Enable E3Experiments in enwiki
    Change-Id: Ib68304ed4e7806afc08deac99ea944b1cce56bea

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -11064,7 +11064,7 @@ $wgConf->settings = array(
 'wmgUseE3Experiments' => array(
 	'default' => false,
 	'testwiki' => true,
-//	'enwiki' => true,
+	'enwiki' => true,
 ),
 'wmgUseEducationProgram' => array(
 	'default' => false,
