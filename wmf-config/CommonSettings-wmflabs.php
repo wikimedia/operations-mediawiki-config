f64b08e5faee983032bbfd5b6d61bda14ecf5488
Jan Gerber <j@thing.net>
2012-08-01 14:05:30 -0700

    Removes transcoding stub
    Removes unused transcoding settings from labs setup
    
    Change-Id: I35f4040b881c958b5f0643cb44515b514b99330a

--- wmf-config/CommonSettings-wmflabs.php
+++ wmf-config/CommonSettings-wmflabs.php
@@ -58,16 +58,4 @@ if ($wgDBname == "commonswiki" || $wgDBname == 'enwiki') {
 	));
 }
 
-# Transcoding
-if ( file_exists( '/etc/wikimedia-transcoding' ) ) {
-	# configuration file for video transcoding provided by TimedMediaHandler
-
-	//transcoding environment needs more resources
-	//to create video derivatives(TimedMediaHandler)
-	$wgTranscodeBackgroundTimeLimit = 3600 * 4;
-	$wgMaxShellMemory = 3000000;
-	$wgMaxShellTime = 3600 * 4;
-	$wgMaxShellFileSize = 100*102400; //1GB
-}
-
 } # end safeguard
