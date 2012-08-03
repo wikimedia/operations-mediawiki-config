c864728675aca9065b92f9e12787d7b9bc10db55
Matthias Mullie <git@mullie.eu>
2012-08-03 14:25:16 +0200

    (bug 36722) Article Feedback - Supporting feedback on help pages
    Change-Id: Ibe31ad726364797b413e2de91febf1bfe640fe6e

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -2062,6 +2062,8 @@ if ( $wmgUseArticleFeedbackv5 ) {
 	$wgArticleFeedbackv5BlacklistCategories = $wmgArticleFeedbackv5BlacklistCategories;
 	$wgArticleFeedbackv5DashboardCategory = array( 'Article_Feedback_5', 'Article_Feedback_5_Additional_Articles' );
 	$wgArticleFeedbackv5OversightEmails = $wmgArticleFeedbackv5OversightEmails;
+	$wgArticleFeedbackv5Namespaces = $wmgArticleFeedbackv5Namespaces;
+	$wgArticleFeedbackv5LotteryOdds = $wmgArticleFeedbackv5LotteryOdds;
 
 	foreach ( array( 'afttest', 'afttest-hide', 'rollbacker', 'reviewer', 'sysop', 'oversight' ) as $group ) {
 		$wgGroupPermissions[$group]['aftv5-hide-feedback'] = true;
