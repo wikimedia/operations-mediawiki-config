567d84becbf7b16293c70cb3872afa8eeedbde07
Reedy <reedy@wikimedia.org>
2012-08-06 16:45:02 +0000

    Remove old docroot and other symlinks
    Change-Id: I288d90af1d1ac8881787894b0ab7ea7a78178f8e

--- live-1.5/MWVersion.php
+++ live-1.5/MWVersion.php
@@ -7,6 +7,9 @@ $host = $secure ? $secure : $_SERVER['HTTP_HOST'];
 if ( $host === 'test.wikipedia.org' && !$secure
 	&& !preg_match( '!thumb(_handler)?\.php!', $_SERVER['REQUEST_URI'] ) )
 {
+	if ( !is_dir( '/home/wikipedia/common' ) ) {
+		trigger_error( "/home not mounted; URI " . $_SERVER['REQUEST_URI'] . "\n" );
+	}
 	# Test wiki mostly runs off the version of MediaWiki on /home.
 	# As horrible hack for NFS-less image scalers, use regular docroot for thumbs?
 	require_once( '/home/wikipedia/common/multiversion/MWVersion.php' );
