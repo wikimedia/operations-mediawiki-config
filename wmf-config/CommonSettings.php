8d68c61a5c051fb89aa9e17b64d30024cdb2dc99
Catrope <roan.kattouw@gmail.com>
2012-08-06 14:15:26 -0700

    Parsoid is very intolerant of double slashes right now
    Change-Id: I112e4582232720158d665a83f4a625563c5d89c2

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -2165,7 +2165,7 @@ if ( $wmgUseUploadWizard ) {
 
 if ( $wmgUseVisualEditor ) {
 	require_once( "$IP/extensions/VisualEditor/VisualEditor.php" );
-	$wgVisualEditorParsoidURL = 'http://10.64.16.146:8000/'; // 10.64.16.146 is cadmium.eqiad.wmnet
+	$wgVisualEditorParsoidURL = 'http://10.64.16.146:8000'; // 10.64.16.146 is cadmium.eqiad.wmnet
 }
 
 if ( $wmgUseNarayam ) {
