3398f57a46253fc1ca78a8ec849cfdc16125dcea
Tyler Anthony Romeo <tylerromeo@gmail.com>
2012-08-24 09:11:38 -0400

    (bug 39380) Enabling secure login (HTTPS).
    Set $wgSecureLogin to true in order to enable HTTPS
    for logins on WMF wikis.
    
    Change-Id: I058fac389323b7f34134bc264c2c2cf5aa9feb6d
    Signed-off-by: Tyler Anthony Romeo <tylerromeo@gmail.com>

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -508,7 +508,7 @@ $wgSVGConverters['rsvg-secure'] = '$path/rsvg-convert --no-external-files -w $wi
 
 $wgAllowUserJs = true;
 $wgAllowUserCss = true;
-
+$wgSecureLogin = true;
 
 #######################################################################
 # Squid Configuration
