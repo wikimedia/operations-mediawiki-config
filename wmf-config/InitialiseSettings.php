c8675f8ebfa605c8e3167435fc9ce2998bcedc20
Dereckson <dereckson@espace-win.org>
2012-09-07 14:52:02 +0200

    Cleaning Berlin hackathon tutorial configuration.
    wmgReduceStartupExpiry were set at true on test. wiki
    
    Change-Id: I6a9a95baf75d04a17555d135ead61a3487d89428

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -11115,7 +11115,6 @@ $wgConf->settings = array(
 ),
 'wmgReduceStartupExpiry' => array(
 	'default' => false,
-	'testwiki' => true, # Enabled temporarily for Berlin tutorial --Roan
 ),
 'wmgMemoryLimit' => array(
 	'default' => 128*1024*1024, // 128MB
