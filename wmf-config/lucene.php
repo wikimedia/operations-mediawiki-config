93d25ad6d951d79d284bb45c23eba1b77043d4d0
pyoungmeister <py@wikimedia.org>
2012-09-07 11:20:16 -0400

    lucene.php: moving all search traffic to eqiad
    Change-Id: I4e34068c793247265305c731c2dbf5bbc41568a6

--- wmf-config/lucene.php
+++ wmf-config/lucene.php
@@ -21,7 +21,7 @@ $wgLuceneSearchTimeout = 10;
 
 # default host for mwsuggest backend
 $wgEnableLucenePrefixSearch = true;
-$wgLucenePrefixHost = '10.2.1.15'; # LVS search-prefix pool
+$wgLucenePrefixHost = '10.2.2.15'; # LVS search-prefix pool
 
 $wgLucenePort = 8123;
 if ( in_array( $wgDBname, array( 'enwiki' ) ) ) {
@@ -31,13 +31,13 @@ if ( in_array( $wgDBname, array( 'enwiki' ) ) ) {
 	// $wmgUseTitleKey = false; // Breaks go matching: https://bugzilla.wikimedia.org/show_bug.cgi?id=19882
 } elseif ( in_array( $wgDBname, array( 'dewiki', 'frwiki', 'jawiki' ) ) ) {
 	# Big RAM pool 2, via LVS
-	$wgLuceneHost = '10.2.1.12';
+	$wgLuceneHost = '10.2.2.12';
 } elseif ( in_array( $wgDBname, array( 'itwiki', 'ptwiki', 'plwiki', 'nlwiki', 'ruwiki', 'svwiki', 'zhwiki', 'eswiki'  ) ) ) {
 	# Pool 3 LVS
-	$wgLuceneHost = '10.2.1.13';
+	$wgLuceneHost = '10.2.2.13';
 } else {
 	# Pool 4 LVS
-	$wgLuceneHost = '10.2.1.14';
+	$wgLuceneHost = '10.2.2.14';
 }
 
 $wgLuceneCSSPath = '/w/extensions/LuceneSearch/lucenesearch.css';
