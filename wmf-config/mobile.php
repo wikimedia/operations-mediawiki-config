6cfc6351a0dd0e0b21192d3838edb44a28c286ce
Max Semenik <maxsem.wiki@gmail.com>
2012-09-06 17:03:06 -0700

    WLM banner
    Depends on the following commits:
    * https://gerrit.wikimedia.org/r/#/c/22853/
    * https://gerrit.wikimedia.org/r/#/c/23020/
    
    Change-Id: Idc335b35764439339c26b05c91b308b0bede4f34

--- wmf-config/mobile.php
+++ wmf-config/mobile.php
@@ -141,4 +141,34 @@ if ( $wgDBname === 'commonswiki' || $wgDBname === 'testwiki' ) {
 	};
 }
 
+// Add a WLM mobile app banner
+$wgHooks['GetMobileNotice'][] = function( SkinMobileBase $skin, $notice ) {
+	if ( $skin instanceof SkinMobile ) {
+		global $wgMemc;
+		$key = wfMemcKey( 'wmf', 'wlm', 'banner', $skin->getLanguage()->getCode() );
+		$banner = $wgMemc->get( $key );
+		if ( !$banner ) {
+			$banner = <<<HTML
+		<a style="position: absolute;top: 50%; margin-top: -16px;left: 0;" href="https://play.google.com/store/apps/details?id=org.wikipedia.wlm" >
+			<img alt="Wiki Loves Monuments Logo notext.svg" src="//upload.wikimedia.org/wikipedia/commons/thumb/c/ca/Wiki_Loves_Monuments_Logo_notext.svg/35px-Wiki_Loves_Monuments_Logo_notext.svg.png" width="35" height="32">
+		</a>
+		<a href="https://play.google.com/store/apps/details?id=org.wikipedia.wlm" style="position: absolute; right: 32px; bottom: -2px;">
+			<img alt="WLM phone half.png" src="//upload.wikimedia.org/wikipedia/commons/thumb/2/27/WLM_phone_half.png/25px-WLM_phone_half.png" width="25" height="32">
+		</a>
+		<div style="margin-left: 30px; padding: 10px; padding-right: 40px; margin-right: 40px; font-size: 0.7em;">
+			<a href="https://play.google.com/store/apps/details?id=org.wikipedia.wlm" style="color: white; text-decoration: none;">
+				{$skin->msg( 'wikimedia-wlm-app-banner2012-header' )}
+				<br>
+				{$skin->msg( 'wikimedia-wlm-app-banner2012-link' )}
+			</a>
+		</div>
+HTML;
+			$banner = $skin->createDismissableBanner( 'mf-wlm2012-app1', $banner, 'android', 'background-color:#8E141A; color: white;' );
+			$wgMemc->set( $key, $banner, 3600 );
+		}
+		$notice .= $banner;
+	}
+	return true;
+};
+
 } # safeguard
