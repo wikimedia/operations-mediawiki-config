88d3962ace35325f64f54be6b1bae2e79bbcb52f
Matthias Mullie <git@mullie.eu>
2012-10-10 13:49:22 +0200

    Deprecated: Use of wfGetIP was deprecated in MediaWiki 1.19.
    Change-Id: I49d135d6bc4d077365aed196783e5a02db12ac5f

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -1695,9 +1695,10 @@ $wgHooks['PrefsEmailAudit'][] = 'logPrefsEmail';
 $wgHooks['PrefsPasswordAudit'][] = 'logPrefsPassword';
 
 function logBadPassword( $user, $pass, $retval ) {
-	$headers = apache_request_headers();
-
 	if ( $user->isAllowed( 'delete' ) && $retval != LoginForm::SUCCESS ) {
+		global $wgRequest;
+		$headers = apache_request_headers();
+
 		switch( $retval ) {
 		case LoginForm::WRONG_PASS:
 		case LoginForm::EMPTY_PASS:
@@ -1711,7 +1712,7 @@ function logBadPassword( $user, $pass, $retval ) {
 		}
 
 		wfDebugLog( 'badpass', "$bit for sysop '" .
-			$user->getName() . "' from " . wfGetIP() .
+			$user->getName() . "' from " . $wgRequest->getIP() .
 			# " - " . serialize( apache_request_headers() )
 			" - " . @$headers['X-Forwarded-For'] .
 			' - ' . @$headers['User-Agent'] .
@@ -1732,11 +1733,13 @@ function logBadPassword( $user, $pass, $retval ) {
 
 function logPrefsEmail( $user, $old, $new ) {
 	if ( $user->isAllowed( 'delete' ) ) {
+		global $wgRequest;
 		$headers = apache_request_headers();
+
 		wfDebugLog( 'badpass', "Email changed in prefs for sysop '" .
 			$user->getName() .
 			"' from '$old' to '$new'" .
-			" - " . wfGetIP() .
+			" - " . $wgRequest->getIP() .
 			# " - " . serialize( apache_request_headers() )
 			" - " . @$headers['X-Forwarded-For'] .
 			' - ' . @$headers['User-Agent'] .
@@ -1747,11 +1750,13 @@ function logPrefsEmail( $user, $old, $new ) {
 
 function logPrefsPassword( $user, $pass, $status ) {
 	if ( $user->isAllowed( 'delete' ) ) {
+		global $wgRequest;
 		$headers = apache_request_headers();
+
 		wfDebugLog( 'badpass', "Password change in prefs for sysop '" .
 			$user->getName() .
 			"': $status" .
-			" - " . wfGetIP() .
+			" - " . $wgRequest->getIP() .
 			# " - " . serialize( apache_request_headers() )
 			" - " . @$headers['X-Forwarded-For'] .
 			' - ' . @$headers['User-Agent'] .
