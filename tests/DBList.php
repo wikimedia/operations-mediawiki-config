cffa58be84ec81c87a74a112ad7a2a2512f85b4e
Dereckson <dereckson@espace-win.org>
2012-10-19 00:51:47 +0200

    Unit testing for InitialiseSettings.php (WIP - DO NOT MERGE)
    Change-Id: Icad3cd50d6e3adba3410369b1132cf60a7e618ec

--- tests/DBList.php
+++ tests/DBList.php
@@ -21,7 +21,7 @@ class DBList {
 		'wiktionary',
 	);
 
-	public static function getall() {
+	public static function buildList() {
 		static $list = null;
 		if( $list ) return $list;
 
@@ -37,8 +37,11 @@ class DBList {
 		return $list;
 	}
 
+	public static function getAll () {
+		return file('all.dblist', FILE_IGNORE_NEW_LINES);
+	}
+
 	public static function isWikiProject( $dbname ) {
 		return in_array( $dbname, self::$wiki_projects );
 	}
 }
-
---
+++ tests/wmf-config/Defines.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Defines used by wmf-config tests
+ * They're extracted from core includes/Defines.php
+ * 
+ * @file
+ */
+
+//Namespaces
+define( 'NS_MEDIA', -2 );
+define( 'NS_SPECIAL', -1 );
+define( 'NS_MAIN', 0 );
+define( 'NS_TALK', 1 );
+define( 'NS_USER', 2 );
+define( 'NS_USER_TALK', 3 );
+define( 'NS_PROJECT', 4 );
+define( 'NS_PROJECT_TALK', 5 );
+define( 'NS_FILE', 6 );
+define( 'NS_FILE_TALK', 7 );
+define( 'NS_MEDIAWIKI', 8 );
+define( 'NS_MEDIAWIKI_TALK', 9 );
+define( 'NS_TEMPLATE', 10 );
+define( 'NS_TEMPLATE_TALK', 11 );
+define( 'NS_HELP', 12 );
+define( 'NS_HELP_TALK', 13 );
+define( 'NS_CATEGORY', 14 );
+define( 'NS_CATEGORY_TALK', 15 );
+
+//Legacy namespaces
+define( 'NS_IMAGE', NS_FILE );
+define( 'NS_IMAGE_TALK', NS_FILE_TALK );
