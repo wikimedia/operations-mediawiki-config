121e1cd7ab9ccfce577678d49f338dac041d4524
csteipp <csteipp@wikimedia.org>
2012-10-22 10:30:41 -0700

    LBFactory_Multi setup for labs
    Change-Id: Ieea46ba6d92d32a83c8795992a6a6c4012a18d8d

--- wmf-config/db-wmflabs.php
+++ wmf-config/db-wmflabs.php
@@ -3,16 +3,48 @@
 #          Do not put private data here.
 
 if( $cluster == 'wmflabs' ) { # safe guard
-# Database configuration files for the beta labs
+	# Database configuration files for the beta labs
 
-// Pretend we have a complex database setup...
-$wgDBtype           = "mysql";
-$wgDBserver         = "deployment-sql";
-$wgDBprefix         = "";
-$wgDBTableOptions   = "ENGINE=InnoDB, DEFAULT CHARSET=binary";
+	$wgDBTableOptions   = "ENGINE=InnoDB, DEFAULT CHARSET=binary";
 
-if ( $wgDBname === 'enwikivoyage' ) {
-	$wgDBserver         = "deployment-sql02";
-}
+	$wgLBFactoryConf = array(
+
+		#Requires 'sectionsByDB', 'sectionLoads', 'serverTemplate'
+
+		'class' => 'LBFactory_Multi',
+
+		'sectionsByDB' => array(
+			'enwikivoyage' => 's2',
+			'dewikivoyage' => 's2',
+			'itwikivoyage' => 's2',
+			'svwikivoyage' => 's2',
+			'frwikivoyage' => 's2',
+			'nlwikivoyage' => 's2',
+			'ruwikivoyage' => 's2',
+		),
+
+		'sectionLoads' => array(
+			'DEFAULT' => array(
+				'db1'     => 0,
+			),
+			's2' => array(
+				'db2'	  => 0,
+			)
+		),
+
+		'serverTemplate' => array(
+			'dbname'	  => $wgDBname,
+			'user'		  => $wgDBuser,
+			'password'	  => $wgDBpassword,
+			'type'		  => 'mysql',
+			'flags'		  => DBO_DEFAULT,
+			'max lag'	  => 30,
+		),
+
+		'hostsByName' => array(
+			'db1'  => 'deployment-sql',
+			'db2'  => 'deployment-sql02',
+		),
+	);
 
 } # end safe guard
