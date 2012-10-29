1276a244e14e6d1094276fe59b9aaf1e7d190dcb
Hydriz <admin@alphacorp.tk>
2012-10-29 22:07:23 +0800

    (bug 41467) Create new namespace alias WP: for Norwegian (bokmål) Wikipedia
    This change implements the namespace alias "WP" for nowiki as per
    consensus. Note however that the sysadmin implementing this change
    needs to run namespaceDupes.php on nowiki, as there are existing
    pages that start with this namespace.
    
    Change-Id: I74421385950ea1e20e568b7767a8f569f4f2d4dd

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -2853,6 +2853,9 @@ $wgConf->settings = array(
 	'nnwiki' => array(
 		 'WP' => NS_PROJECT,
 	),
+	'nowiki' => array(
+		'WP' => NS_PROJECT,
+	),
 	'nowikimedia' => array(
 		'Brukar' => NS_USER,
 		'Brukardiskusjon' => NS_USER_TALK,
