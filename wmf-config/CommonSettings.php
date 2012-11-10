47035c9c0579443a3ad484f67bc6a6d26f87b131
Reedy <reedy@wikimedia.org>
2012-11-10 12:37:07 +0000

    Revert "Enable lucene for wikidatawiki"
    This reverts commit 059eefc5d048aa7ea4aee7d10bca61e428484bd5

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -1299,6 +1299,7 @@ $wgDisableCounters     = true;
 
 wfProfileOut( "$fname-misc2" );
 
+if ( $wgDBname != 'wikidatawiki' ) {
 # This is overridden in the Lucene section below
 $wgDisableTextSearch   = true;
 $wgDisableSearchUpdate = true;
@@ -1317,6 +1318,7 @@ if ( $wgUseLuceneSearch ) {
 	wfProfileIn( "$fname-lucene" );
 	include( "$wmfConfigDir/lucene.php" );
 	wfProfileOut( "$fname-lucene" );
+}
 }
 
 // Case-insensitive title prefix search extension
