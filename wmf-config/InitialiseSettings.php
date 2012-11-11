7ccab37b161b49f0b766eecc375af0c0ef9831fd
Reedy <reedy@wikimedia.org>
2012-11-11 19:25:39 +0000

    Disable uploads on wikivoyage. Point to commons
    Change-Id: I960b9526d74240edf26e7d06b6e05563466d4da8

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -904,6 +904,7 @@ $wgConf->settings = array(
 
 	// projects
 	'wikinews' => false,
+	'wikivoyage' => false,
 
 	// wikis:
 	'arwikinews' => true,
@@ -951,6 +952,7 @@ $wgConf->settings = array(
 
 	// projects
 	'wikinews' => '//commons.wikimedia.org/wiki/Special:Upload',
+	'wikivoyage' => '//commons.wikimedia.org/wiki/Special:Upload',
 
 	// Use relative paths for same-wiki links so the SSL converter can tweak them correctly
 	'incubatorwiki' => '/wiki/Special:MyLanguage/Incubator:Upload',
