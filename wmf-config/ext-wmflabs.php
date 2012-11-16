3c07972e9ba688238053e356014c64c56aaa0bd8
Antoine Musso <hashar@free.fr>
2012-11-16 18:27:54 +0100

    beta: disable OnlineStatusBar
    It causes message cache issues with some magic word that can not be
    found. Been disabled by a live hack since last week at least.
    
    Change-Id: Iac0e5a169e73cbeb54b99075bc4cf8046581a008

--- wmf-config/ext-wmflabs.php
+++ wmf-config/ext-wmflabs.php
@@ -15,7 +15,10 @@ if( $cluster === 'wmflabs' ) {   # safeguard
 // This extension is scheduled for review and deployment on enwiki
 // Do not remove this extension, without contacting Petr Bena
 if ($wgDBname == "enwiki") {
-	include ("$IP/extensions/OnlineStatusBar/OnlineStatusBar.php");
+	# commented by hashar on 20121116, cause message cache issue on
+	# beta with the magic word it introduces.
+	# Been disabled by a live hack since last week at least.
+	//include ("$IP/extensions/OnlineStatusBar/OnlineStatusBar.php");
 }
 
 
