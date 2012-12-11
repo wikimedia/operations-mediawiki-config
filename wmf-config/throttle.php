9a624d647c431cbc78c0043dbd2bef716c357228
Reedy <reedy@wikimedia.org>
2012-12-11 22:10:30 +0000

    Remove old code
    Change-Id: I22cf4078a887604d300feb5f7b961c11ebec152e

--- wmf-config/throttle.php
+++ wmf-config/throttle.php
@@ -90,25 +90,3 @@ function efRaiseAccountCreationThrottle() {
 	}
 }
 
-
-
-// Added throttle for account creations on zh due to mass registration attack 2005-12-16
-// might be useful elesewhere. --brion
-// disabled temporarily due to tugela bug -- Tim
-
-if ( false /*$lang == 'zh' || $lang == 'en'*/ ) {
-	require( "$IP/extensions/UserThrottle/UserThrottle.php" );
-	$wgGlobalAccountCreationThrottle = array(
-/*
-		'min_interval' => 30,   // Hard minimum time between creations (default 5)
-		'soft_time'    => 300, // Timeout for rolling count
-		'soft_limit'   => 5,  // 5 registrations in five minutes (default 10)
-*/
-		'min_interval' => 0,   // Hard minimum time between creations (default 5)
-		'soft_time'    => 60, // Timeout for rolling count (default 5 minutes)
-		'soft_limit'   => 2,  // 2 registrations in one minutes (default 10)
-	);
-}
-
-
-
