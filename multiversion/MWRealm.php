8236c394864a7fd78bc479bdfe89808217a69f74
Brad Jorsch <bjorsch@wikimedia.org>
2012-12-18 12:41:41 +0100

    find filenames based on realm/datacenter
    This is an independant change meant to find out a filename based on
    $wmfRealm and $wmfDatacenter.
    
    Change-Id: I52a4b0ed0e86730a18559e3298dbd5cc0d2a828d

---
+++ multiversion/MWRealm.php
@@ -0,0 +1,71 @@
+<?php
+### Determine realm and datacenter we are on #############################
+# $wmfRealm and $wmfDatacenter are used to vary configuration based on server
+# location. Thet should be provisioned by puppet in /etc/wikimedia-site and
+# /etc/wikimedia-realm.
+#
+# The possible values of $wmfRealm and $wmfDatacenter as of October 2012 are:
+#  - labs + pmtpa
+#  - labs + eqiad
+#  - production + pmtpa
+#  - production + eqiad
+global $wmfDatacenter, $wmfRealm;
+$wmfRealm   = 'production';
+$wmfDatacenter = 'pmtpa';
+
+# Puppet provision the realm in /etc/wikimedia-realm
+if( file_exists( '/etc/wikimedia-realm' ) ) {
+	$wmfRealm = trim( file_get_contents( '/etc/wikimedia-realm' ) );
+}
+if( file_exists( '/etc/wikimedia-site' ) ) {
+	$wmfDatacenter = trim( file_get_contents( '/etc/wikimedia-site' ) );
+}
+
+# Validate settings
+switch( $wmfRealm ) {
+case 'labs':
+case 'production':
+	if ( ! in_array( $wmfDatacenter, array( 'pmtpa', 'eqiad' ) ) ) {
+		$wmfDatacenter = 'pmtpa';
+	}
+	break;
+
+default:
+	# Assume something vaguely resembling a default
+	$wmfRealm   = 'production';
+	$wmfDatacenter = 'pmtpa';
+	break;
+}
+
+# Function to get the filename for the current realm/datacenter, falling back
+# to the "base" file if not found.
+#
+# Files checked are:
+#   base-realm-datacenter.ext
+#   base-realm.ext
+#   base.ext
+#
+# @param $filename string Base filename, must contain an extension
+# @returns string Filename to be used
+function getRealmSpecificFilename( $filename ) {
+	global $wmfRealm, $wmfDatacenter;
+
+	$new_filename = preg_replace( '/(\.[^.]*)$/', "-{$wmfRealm}-{$wmfDatacenter}$1", $filename );
+	if ( file_exists( $new_filename ) ) {
+		return $new_filename;
+	}
+
+	# realm take precedence over datacenter.
+	$new_filename = preg_replace( '/(\.[^.]*)$/', "-{$wmfRealm}$1", $filename );
+	if ( file_exists( $new_filename ) ) {
+		return $new_filename;
+	}
+
+	$new_filename = preg_replace( '/(\.[^.]*)$/', "-{$wmfDatacenter}$1", $filename );
+	if ( file_exists( $new_filename ) ) {
+		return $new_filename;
+	}
+
+	return $filename;
+}
+### End /Determine realm and datacenter we are on/ ########################
---
+++ multiversion/MWRealm.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+WMF_REALM=production
+WMF_DATACENTER=pmtpa
+
+if [ -f /etc/wikimedia-realm ]
+then
+	WMF_REALM=`cat /etc/wikimedia-realm`
+fi
+if [ -f /etc/wikimedia-site ]
+then
+	WMF_DATACENTER=`cat /etc/wikimedia-site`
+fi
+
+# Validate settings
+case "$WMF_REALM" in
+	"production"|"labs")
+		case "$WMF_DATACENTER" in
+			"pmtpa"|"eqiad")
+				;;
+			*)
+				WMF_DATACENTER=pmtpa
+				;;
+		esac
+		;;
+
+	*)
+		# Assume something vaguely resembling a default
+		WMF_REALM=production
+		WMF_DATACENTER=pmtpa
+		;;
+esac
+
+# Function to get the filename for the current realm/datacenter, falling back
+# to the "base" file if not found.
+#
+# Files checked are:
+#   base-realm-datacenter.ext
+#   base-realm.ext
+#   base.ext
+#
+# @param $filename string Base filename, must contain an extension
+# @output string Filename to be used
+getRealmSpecificFilename () (
+	BASE=${1%.*}
+	EXT=${1##*.}
+	RET=$1
+
+	if [ -f "$BASE-$WMF_REALM-$WMF_DATACENTER.$EXT" ]
+	then
+		RET="$BASE-$WMF_REALM-$WMF_DATACENTER.$EXT"
+	elif [ -f "$BASE-$WMF_REALM.$EXT" ]
+	then
+		RET="$BASE-$WMF_REALM.$EXT"
+	elif [ -f "$BASE-$WMF_DATACENTER.$EXT" ]
+	then
+		RET="$BASE-$WMF_DATACENTER.$EXT"
+	else
+		RET="$1"
+	fi
+
+	printf %s "$RET"
+)
