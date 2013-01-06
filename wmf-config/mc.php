3e7ce587db73367bd19b6d0581ba7c68f85067bc
Reedy <reedy@wikimedia.org>
2013-01-06 22:59:10 +0000

    Remove old initial comments relating to old memcached config
    Change-Id: I736d95dcab88aa9f8a6f6e5bae3636cbae0155b9

--- wmf-config/mc.php
+++ wmf-config/mc.php
@@ -1,15 +1,5 @@
 <?php
 # WARNING: This file is publically viewable on the web. Do not put private data here.
-/*
- * Before altering the wgMemCachedServers array below, make sure you planned
- * your change. Memcached compute a hash of the data and given the hash
- * assign the value to one of the servers.
- * If you remove / comment / change order of servers, the hash will miss
- * and that can result in bad performance for the cluster !
- *
- * Hashar, based on dammit comments. Nov 28 2005.
- *
- */
 $wgMainCacheType = 'memcached-pecl';
 
 $wgMemCachedPersistent = false;
