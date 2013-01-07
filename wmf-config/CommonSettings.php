f029216bd6e9e2bcd6cfe0ce0bd6169f2769f964
Max Semenik <maxsem.wiki@gmail.com>
2013-01-08 01:26:53 +0400

    Reduce the load on Solr servers in pmtpa
    Change-Id: I41b842789a06ae5e2a2ca88b4781fc694c8dfffe

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -2604,9 +2604,10 @@ if ( $wmgEnableGeoData ) {
 		'solr1001.eqiad.wmnet' => 50, // master, put less read load on it
 		'solr1002.eqiad.wmnet' => 100,
 		'solr1003.eqiad.wmnet' => 100,
-		'solr1.pmtpa.wmnet' => 100,
-		'solr2.pmtpa.wmnet' => 100,
-		'solr3.pmtpa.wmnet' => 100,
+		// the Tampa servers are weaker
+		'solr1.pmtpa.wmnet' => 50,
+		'solr2.pmtpa.wmnet' => 50,
+		'solr3.pmtpa.wmnet' => 50,
 	);
 
 	# Data collection mode
