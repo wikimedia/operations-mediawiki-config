83e54269dff5ddacf1e43f428035eed04fab4018
pyoungmeister <py@wikimedia.org>
2013-01-09 13:42:26 -0800

    lucene.php: simple loadbalancing of requests across datacenters
    Change-Id: Ic94fc6517a2e8464c42281a17ca3a8eb1e74aeb0

--- wmf-config/lucene.php
+++ wmf-config/lucene.php
@@ -17,23 +17,39 @@ $wgLuceneSearchTimeout = 10;
 
 # default host for mwsuggest backend
 $wgEnableLucenePrefixSearch = true;
-$wgLucenePrefixHost = '10.2.2.15'; # LVS search-prefix pool
+
+$wgLucenePrefixVips = array(
+	'10.2.2.15', # eqiad LVS search-prefix pool
+	'10.2.1.15', # pmtpa LVS search-prefix pool
+);
+$wgLucenePrefixHost = $wgLucenePrefixVips[abs( crc32( $wgDBname ) ) % count( $wgLucenePrefixVips )];
 
 $wgLucenePort = 8123;
 if ( in_array( $wgDBname, array( 'enwiki' ) ) ) {
-	# Big RAM pool 1, via LVS
-	#$wgLuceneHost = '10.2.2.11'; # eqiad
-	$wgLuceneHost = '10.2.1.11'; # pmtpa
+	# Pool 1 LVS
+	$wgLuceneHostVips = array(
+		'10.2.1.11', # pmtpa
+		'10.2.2.11', # eqiad
+	);
 } elseif ( in_array( $wgDBname, array( 'dewiki', 'frwiki', 'jawiki' ) ) ) {
-	# Big RAM pool 2, via LVS
-	#$wgLuceneHost = '10.2.2.12'; # eqiad
-	$wgLuceneHost = '10.2.1.12'; # pmtpa
+	# Pool 2 LVS
+	$wgLuceneHostVips = array(
+		'10.2.1.12', # pmtpa
+		'10.2.2.12', # eqiad
+	);
 } elseif ( in_array( $wgDBname, array( 'itwiki', 'ptwiki', 'plwiki', 'nlwiki', 'ruwiki', 'svwiki', 'zhwiki', 'eswiki'  ) ) ) {
 	# Pool 3 LVS
-	#$wgLuceneHost = '10.2.2.13'; # eqiad
-	$wgLuceneHost = '10.2.1.13'; # pmtpa
+	$wgLuceneHostVips = array(
+		'10.2.1.13', # pmtpa
+		'10.2.2.13', # eqiad
+	);
 } else {
 	# Pool 4 LVS
-	#$wgLuceneHost = '10.2.2.14'; # eqiad
-	$wgLuceneHost = '10.2.1.14'; # pmtpa
+	$wgLuceneHostVips = array(
+		'10.2.1.14', # pmtpa
+		'10.2.2.14', # eqiad
+	);
 }
+
+$wgLuceneHost = $wgLuceneHostVips[abs( crc32( $wgDBname ) ) % count( $wgLuceneHostVips )];
+
