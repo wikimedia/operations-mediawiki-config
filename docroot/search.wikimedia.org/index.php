7ded78987f92d28630368944f1ead2aa4ed20422
Tim Starling <tstarling@wikimedia.org>
2013-01-14 14:22:05 +1100

    Add the search.wikimedia.org script to the main apache pool
    This is the Apple dictionary gateway exactly as Brion wrote it.
    Minor maintenance to follow in a subsequent commit.
    
    Change-Id: Ie8267d396b5a9d4a5fe29f813b24e4b3a930e943

---
+++ docroot/search.wikimedia.org/index.php
@@ -0,0 +1,107 @@
+<?php
+
+// Fetch a suggestion set from the OpenSearch API and translate it
+// to the form that Apple expects for their Dictionary app lookups.
+//
+// Brion Vibber <brion@wikimedia.org>
+// 2007-12-28
+
+function dieOut( $msg='' ) {
+	header( "HTTP/1.0 500 Internal Server Error" );
+	die( "Wikimedia search service internal error.\n\n$msg" );
+}
+
+error_reporting(E_ALL);
+ini_set("display_errors", false);
+ini_set('user_agent', 'Wikimedia OpenSearch to Apple Dictionary bridge');
+
+$caching = true;
+
+$site = 'wikipedia';
+$lang = 'en';
+$search = '';
+$limit = 20;
+
+$allowedSites = array(
+	'wikipedia',
+	'wiktionary',
+	'wikinews',
+	'wikisource' );
+
+if( isset( $_GET['site'] ) ) {
+	if( is_string( $_GET['site'] ) && in_array( $_GET['site'], $allowedSites ) ) {
+		$site = $_GET['site'];
+	} else {
+		dieOut( "Invalid parameter." );
+	}
+}
+
+if( isset( $_GET['lang'] ) ) {
+	if( preg_match( '/^[a-z]+(-[a-z]+)*$/', $_GET['lang'] ) ) {
+		$lang = $_GET['lang'];
+	} else {
+		dieOut( "Invalid language parameter." );
+	}
+}
+
+if( isset( $_GET['search'] ) ) {
+	if( is_string( $_GET['search'] ) ) {
+		$search = $_GET['search'];
+	} else {
+		dieOut( "Invalid search parameter." );
+	}
+}
+
+if( isset( $_GET['limit'] ) ) {
+	if( is_string( $_GET['limit'] ) && preg_match( '/^\d+/', $_GET['limit'] )
+		&& intval( $_GET['limit'] ) > 0 && intval( $_GET['limit'] ) < 100 ) {
+		$limit = intval( $_GET['limit'] );
+	} else {
+		dieOut( "Invalid limit parameter." );
+	}
+}
+
+$urlSearch = urlencode( $search );
+
+# OpenSearch JSON suggest API
+$url = "http://$lang.$site.org/w/api.php?action=opensearch&search=$urlSearch&limit=$limit";
+
+$result = file_get_contents( $url );
+if( !$result ) {
+	dieOut( "Backend failure." );
+}
+
+$suggest = json_decode( $result );
+//echo "<pre>"; var_dump( $suggest ); echo "</pre>";
+
+// Confirm return result was format we expect
+// for opensearch...
+if( is_array( $suggest ) && count( $suggest ) >= 2
+ 	&& is_string( $suggest[0] ) && is_array( $suggest[1] ) ) {
+		$returnedTerm = $suggest[0];
+		$results = $suggest[1];
+} else {
+	dieOut( "Unexpected result format." );
+}
+
+if( $caching ) {
+	header( "Cache-Control: public, max-age: 1200, s-maxage: 1200" );
+}
+print "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"DTD/xhtml1-transitional.dtd\">\n" .
+	"<html><head><meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\"></head>\n" .
+	"<body>";
+
+foreach( $results as $result ) {
+	#<div><span class="language">en</span>:<span class="key">Leban_Mohamed_Nour</span></div>
+	$htmlResult = htmlspecialchars( str_replace( ' ', '_', $result ) );
+	print "<div><span class=\"language\">$lang</span>:<span class=\"key\">$htmlResult</span></div>";
+}
+
+if( empty( $results ) ) {
+	$htmlSearch = htmlspecialchars( $search );
+	print "<p>No entries found for \"$lang:$htmlSearch\"</p>";
+}
+
+print "</body></html>\n";
+
+?>
