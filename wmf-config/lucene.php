671aef0adcbf05a23fe07b6c343ffc5ec6e5bf05
pyoungmeister <py@wikimedia.org>
2013-01-17 11:44:58 -0800

    lucene.php: moving all search traffic to eqiad
    Change-Id: I1a3274d2e3a15ba8c4751247bddc7711f90618f0

--- wmf-config/lucene.php
+++ wmf-config/lucene.php
@@ -17,7 +17,8 @@ $wgLuceneSearchTimeout = 10;
 
 # default host for mwsuggest backend
 $wgEnableLucenePrefixSearch = true;
-$wgLucenePrefixHost = '10.2.1.15'; # LVS search-prefix pool
+#$wgLucenePrefixHost = '10.2.1.15'; # pmtpa LVS search-prefix pool
+$wgLucenePrefixHost = '10.2.2.15'; # eqiad LVS search-prefix pool
 
 $wgLucenePort = 8123;
 if ( in_array( $wgDBname, array( 'enwiki' ) ) ) {
@@ -26,14 +27,14 @@ if ( in_array( $wgDBname, array( 'enwiki' ) ) ) {
 	#$wgLuceneHost = '10.2.1.11'; # pmtpa
 } elseif ( in_array( $wgDBname, array( 'dewiki', 'frwiki', 'jawiki' ) ) ) {
 	# Big RAM pool 2, via LVS
-	#$wgLuceneHost = '10.2.2.12'; # eqiad
-	$wgLuceneHost = '10.2.1.12'; # pmtpa
+	$wgLuceneHost = '10.2.2.12'; # eqiad
+	#$wgLuceneHost = '10.2.1.12'; # pmtpa
 } elseif ( in_array( $wgDBname, array( 'itwiki', 'ptwiki', 'plwiki', 'nlwiki', 'ruwiki', 'svwiki', 'zhwiki', 'eswiki'  ) ) ) {
 	# Pool 3 LVS
-	#$wgLuceneHost = '10.2.2.13'; # eqiad
-	$wgLuceneHost = '10.2.1.13'; # pmtpa
+	$wgLuceneHost = '10.2.2.13'; # eqiad
+	#$wgLuceneHost = '10.2.1.13'; # pmtpa
 } else {
 	# Pool 4 LVS
-	#$wgLuceneHost = '10.2.2.14'; # eqiad
-	$wgLuceneHost = '10.2.1.14'; # pmtpa
+	$wgLuceneHost = '10.2.2.14'; # eqiad
+	#$wgLuceneHost = '10.2.1.14'; # pmtpa
 }
