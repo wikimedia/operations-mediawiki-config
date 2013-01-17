93a7ab597c8303819945eb919a9903651bdfdc4f
vrandezo <denny.vrandecic@wikimedia.de>
2013-01-17 14:40:31 +0100

    (bug 41847) Drop the www. from the domain wikidata.org
    Goes together with the change in operations/apache-config
    Please triple check this changeset, I am a bit unsure about it.
    
    Change-Id: I1a694129c643db9984e68a71b07fbe2642aa79e2

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -836,6 +836,7 @@ if ( $wmgUseCORS ) {
 		'wikisource.org',
 		'*.wikiquote.org',
 		'*.wikidata.org',
+		'wikidata.org',
 		'www.mediawiki.org',
 		'wikimediafoundation.org',
 		'advisory.wikimedia.org',
