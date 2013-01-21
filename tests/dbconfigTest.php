c2932886e3de9e34eef8e20e2d19e923232a5bb4
Reedy <reedy@wikimedia.org>
2013-01-21 17:22:51 +0000

    Update dbconfigtest
    Test for pmtpa, eqiad and labs
    
    Don't just use db.php
    
    Change-Id: I4bba7af50f60d727d21895e09bd82fe15b6cba46

--- tests/dbconfigTest.php
+++ tests/dbconfigTest.php
@@ -7,16 +7,23 @@
  * @copyright Copyright © 2012, Antoine Musso <hashar at free dot fr>
  * @file
  */
-
-class dbconfigTests extends PHPUnit_Framework_TestCase {
+abstract class DBConfigTestCase extends PHPUnit_Framework_TestCase {
 	public $cfgPath;
 
 	/** Array: will contains the load balancer configuration */
 	protected $lb;
 
-	function __construct() {
+	/**
+	 * @param $suffix string
+	 */
+	function __construct( $suffix ) {
 		parent::__construct();
-		$this->cfgPath = dirname(__FILE__). '/../wmf-config' ;
+
+		if ( is_null( $suffix ) ) {
+			$this->markTestSkipped( 'No database suffix given' );
+		}
+
+		$this->cfgPath = dirname( __FILE__ ). '/../wmf-config' ;
 
 		# "properly" load db.php in local context:
 		$wgDBname     = 'testwiki';
@@ -25,7 +32,7 @@ class dbconfigTests extends PHPUnit_Framework_TestCase {
 		if( !defined( 'DBO_DEFAULT' ) ) {
 			define( 'DBO_DEFAULT', 16 );
 		}
-		include( $this->cfgPath . '/db.php' );
+		include( "{$this->cfgPath}/db-{$suffix}.php" );
 
 		$this->lb = $wgLBFactoryConf;
 	}
@@ -54,6 +61,9 @@ class dbconfigTests extends PHPUnit_Framework_TestCase {
 	 * Each database in 'sectionsByDB' must point to an existing cluster
 	 */
 	function testDbAssignedToAnExistingCluster() {
+if ( !isset( $this->lb['sectionsByDB'] ) ) {
+var_dump( __CLASS__ );
+}
 		foreach( $this->lb['sectionsByDB'] as $dbname => $cluster) {
 			$this->assertArrayHasKey( $cluster,
 				$this->lb['sectionLoads'],
@@ -63,3 +73,24 @@ class dbconfigTests extends PHPUnit_Framework_TestCase {
 	}
 
 }
+
+class pmtpaDBConfigTests extends DBConfigTestCase {
+	function __construct() {
+		parent::__construct( 'pmtpa' );
+	}
+}
+
+class eqiadDBConfigTests extends DBConfigTestCase {
+	function __construct() {
+		parent::__construct( 'eqiad' );
+	}
+}
+
+
+class labsDBConfigTests extends DBConfigTestCase {
+	function __construct() {
+		parent::__construct( 'labs' );
+	}
+}
+
+
