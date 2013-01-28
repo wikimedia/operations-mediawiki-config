c9ec7bf41e826141767969a5a8e0c2c42a57cf81
Antoine Musso <hashar@free.fr>
2013-01-28 15:27:00 +0100

    wikiversions.cdb for labs
    This patch adapts multiversion to detect an eventual
    wikiversions-labs.cdb file whenever it exists.  With git-deploy we will
    have to add the .cdb files to git in order to deploy them, so we want
    wikiversions-labs.dat to generate a different cdb file.
    
    Bug: 44424
    Change-Id: I812a591225bb7cd9c957cfaa2d3a27f10fd423ee

--- multiversion/MWMultiVersion.php
+++ multiversion/MWMultiVersion.php
@@ -257,23 +257,27 @@ class MWMultiVersion {
 		}
 		$this->versionLoaded = true;
 
-		$db = dba_open( MULTIVER_CDB_DIR_APACHE . '/wikiversions.cdb', 'r', 'cdb' );
+		$cdbFilename = getRealmSpecificFilename(
+			MULTIVER_CDB_DIR_APACHE . '/wikiversions.cdb'
+		);
+
+		$db = dba_open( $cdbFilename, 'r', 'cdb' );
 		if ( $db ) {
 			$version = dba_fetch( "ver:{$this->db}", $db );
 			if ( $version === false ) {
 				$extraVersion = false;
 			} else {
 				if ( strpos( $version, 'php-' ) !== 0 ) {
-					self::error( "wikiversions.cdb version entry does not start with `php-` (got `$version`).\n" );
+					self::error( "$cdbFilename version entry does not start with `php-` (got `$version`).\n" );
 				}
 				$extraVersion = dba_fetch( "ext:{$this->db}", $db );
 				if ( $extraVersion === false ) {
-					self::error( "wikiversions.cdb has no extra version entry for `$db`.\n" );
+					self::error( "$cdbFilename has no extra version entry for `$db`.\n" );
 				}
 			}
 			dba_close( $db );
 		} else {
-			self::error( "Unable to open wikiversions.cdb.\n" );
+			self::error( "Unable to open $cdbFilename.\n" );
 		}
 
 		$this->version = $version;
@@ -285,8 +289,11 @@ class MWMultiVersion {
 	 * @return bool
 	 */
 	private function assertNotMissing() {
+		$cdbFilename = getRealmSpecificFilename(
+			MULTIVER_CDB_DIR_APACHE . '/wikiversions.cdb'
+		);
 		if ( $this->isMissing() ) {
-			self::error( "wikiversions.cdb has no version entry for `{$this->db}`.\n" );
+			self::error( "$cdbFilename has no version entry for `{$this->db}`.\n" );
 		}
 	}
 
