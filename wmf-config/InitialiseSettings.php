618206297654a963b824a433f1b25081a6163bff
Krenair <krenair@gmail.com>
2013-02-18 16:47:04 +0000

    (bug 45113) Set cswiktionary favicon to the same as enwiktionary
    Change-Id: I4a80e398739e7c3b81ec5ef8519604d52dcc5b2c

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -9456,6 +9456,7 @@ $wgConf->settings = array(
 
 	# Wiktionaries
 	'enwiktionary'			=> '//bits.wikimedia.org/favicon/wiktionary/en.ico', // Bug 44843
+	'cswiktionary'			=> '//bits.wikimedia.org/favicon/wiktionary/en.ico', // Bug 45113
 	'siwiktionary'			=> '//bits.wikimedia.org/favicon/wiktionary/si.ico', // Bug 38036
 
 	# Other wikis
