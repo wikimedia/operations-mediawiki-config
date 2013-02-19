9681f685b029070dbb9e374650b2e008e338e3da
pyoungmeister <py@wikimedia.org>
2013-02-19 15:30:37 -0800

    db-pmtpa.php: commenting out db43 from s6 for dist upgrade/mariadb
    Change-Id: I41a002006633a8e2883edf7c662b207a0b2b9978

--- wmf-config/db-pmtpa.php
+++ wmf-config/db-pmtpa.php
@@ -119,7 +119,7 @@ $wgLBFactoryConf = array(
 	's6' => array(
 		'db1006'   => 0,
 		'db47'	   => 0, # pmtpa master
-		'db43'	   => 1000, # hw died 12/18/2011
+		#'db43'	   => 1000, # hw died 12/18/2011
 		'db46'	   => 400, # snapshot host
 		'db50'	   => 1000,
 	),
