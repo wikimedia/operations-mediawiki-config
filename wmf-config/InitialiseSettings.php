877d804135c53d3c1ed3115f3c42454ed4495351
Dereckson <dereckson@espace-win.org>
2013-02-21 14:59:02 +0100

    (bug 45205) Namespace configuration for hu.wiktionary
    New namespaces:
    	- 100: Függelék (Appendix)
    	- 101: Függelékvita
    	- 102: Index
    	- 103: Indexvita
    
    New aliases:
    	- Függelék vita -> 101
    	- Index vita -> 103
    
    Change-Id: I72858d16abe695e0f3212187ee68e173a04520a2

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -2691,6 +2691,10 @@ $wgConf->settings = array(
 		'Index' => 106,
 		'Index_talk' => 107,
 	),
+	'+huwiktionary' => array(
+		'Függelék_vita' => 101, // Bug 42505
+		'Index_vita' => 103,
+	),
 	'+hywikisource' => array(
 		'Author' => 100,
 		'Author_talk' => 101,
@@ -4893,6 +4897,12 @@ $wgConf->settings = array(
 	'huwiki' => array(
 		100 => 'Portál',
 		101 => 'Portálvita',
+	),
+	'huwiktionary' => array(
+		100 => 'Függelék',       // Bug 42505 - Appendix
+		101 => 'Függelékvita',
+		102 => 'Index',          // Bug 42505
+		103 => 'Indexvita',
 	),
 	'hywiki' => array(
 		100 => 'Պորտալ',
