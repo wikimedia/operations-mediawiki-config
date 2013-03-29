93c7d5ff3487c965c4190f8b414a9341afb6939f
awjrichards <arichards@wikimedia.org>
2013-03-29 14:29:41 -0700

    Enable X-Analytics logging in MobileFrontend
    Dependent on deployment of https://gerrit.wikimedia.org/r/#/c/52606/
    
    Change-Id: I64a7f1185a5c5714bccc06bd679cc5e2beba72f3

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -11092,6 +11092,9 @@ $wgConf->settings = array(
 'wmgMFLoginHandshakeUrl' => array(
 	'default' => '//commons.wikimedia.org/wiki/Special:LoginHandshake?useformat=mobile',
 ),
+'wmgMFEnableXAnalyticsLogging' => array(
+	'default' => true,
+),
 
 'wgExtraGenderNamespaces' => array(
 	'default' => array(),
