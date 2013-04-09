b3c168389a3db1c14410de2f45c29cc334e00e48
Timo Tijhof <krinklemail@gmail.com>
2013-04-09 07:05:39 +0200

    noc: Clean up conf viewer
    - Remove exception for langlist and wikiversions.dat from regular
      txt protocol. At least in Google Chrome accessing
      conf/wikiversions.dat causes a download instead of a view.
    - Re-run createTxtFileSymlinks.sh to fix symlinks paths
    - Find path to /h/w/c relatively instead of absolutely so that
      it also works when testing locally.
    - Add context to highlight.php <title> tag
    - Add favicon to conf/*.php
    - Add the remainder files from public wmf-config/*.php that
      weren't in the list yet.
    
    Change-Id: I2d8a2a0ca8da54c9a22cfe45e98a062c425d22d3

--- docroot/noc/createTxtFileSymlinks.sh
+++ docroot/noc/createTxtFileSymlinks.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-cd $(dirname $0)
+cd $(dirname $0)/conf
 
 files_array=(
+	# wmf-config/*.php
+	CommonSettings-labs.php
 	CommonSettings.php
+	InitialiseSettings-labs.php
 	InitialiseSettings.php
 	PoolCounterSettings-eqiad.php
 	PoolCounterSettings-pmtpa.php
@@ -12,28 +15,40 @@ files_array=(
 	db-eqiad.php
 	db-labs.php
 	db-pmtpa.php
+	db-secondary.php
+	ext-labs.php
+	ext-production.php
+	filebackend-labs.php
 	filebackend.php
 	flaggedrevs.php
+	jobqueue-eqiad.php
+	jobqueue-pmtpa.php
 	liquidthreads.php
+	logging-labs.php
 	lucene-common.php
 	lucene-labs.php
 	lucene-production.php
 	mc-eqiad.php
 	mc-labs.php
 	mc-pmtpa.php
+	missing.php
+	mobile-labs.php
 	mobile.php
 	proofreadpage.php
 	throttle.php
 	wgConf.php
+
+	wikiversions.dat
+	langlist
 )
 
 # if -e returns false if there is a symlink but target does not exist locally
-if ls ./conf/*.txt >/dev/null 2>&1
+if ls ./*.txt >/dev/null 2>&1
 then
-	rm ./conf/*.txt
+	rm ./*.txt
 fi
 
 for i in "${files_array[@]}"
 do
-   ln -s ../../../wmf-config/$i conf/$i.txt
+   ln -s ../../../wmf-config/$i ./$i.txt
 done
