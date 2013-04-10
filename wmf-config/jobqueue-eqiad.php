c0a07a048cd3efdcb903af3b8dc7becf875435a2
Aaron Schulz <aschulz@wikimedia.org>
2013-04-10 10:59:22 -0700

    Enabled use of redis for null test jobs.
    Change-Id: I160085e9cb7fb1762f63c896f0a0ff115e50a200

--- wmf-config/jobqueue-eqiad.php
+++ wmf-config/jobqueue-eqiad.php
@@ -1,10 +1,17 @@
 <?php
 # WARNING: This file is publically viewable on the web. Do not put private data here.
+
+// Note: on server failure, this should be switched to the slave
+$wgJobTypeConf['null'] = array(
+	'class'       => 'JobQueueRedis',
+	'redisServer' => '10.64.32.76', # rdb1001 (master)
+	#'redisServer' => '10.64.32.77', # rdb1002 (slave)
+	'redisConfig' => array( 'connectTimeout' => 1 )
+);
+// Note: on server failure, this should be changed to any other redis server
 $wgJobQueueAggregator = array(
 	'class'       => 'JobQueueAggregatorRedis',
 	'redisServer' => '10.64.0.180', # mc1001
-	'redisConfig' => array(
-		'connectTimeout' => 1
-	)
+	'redisConfig' => array( 'connectTimeout' => 1 )
 );
 # vim: set sts=4 sw=4 et :
