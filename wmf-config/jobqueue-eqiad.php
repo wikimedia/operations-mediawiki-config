0fe52f6b35f7c600aff46b26fafad0e3acdf7d29
Aaron Schulz <aschulz@wikimedia.org>
2013-04-11 10:33:19 -0700

    Moved remaining jobs to redis.
    Change-Id: I08062739d48bfd98800ffb350629d2aa79f7a864

--- wmf-config/jobqueue-eqiad.php
+++ wmf-config/jobqueue-eqiad.php
@@ -2,17 +2,12 @@
 # WARNING: This file is publically viewable on the web. Do not put private data here.
 
 // Note: on server failure, this should be switched to the slave
-$wgJobTypeConf['null'] = array(
+$wgJobTypeConf['default'] = array(
 	'class'       => 'JobQueueRedis',
 	'redisServer' => '10.64.32.76', # rdb1001 (master)
 	#'redisServer' => '10.64.32.77', # rdb1002 (slave)
 	'redisConfig' => array( 'connectTimeout' => 1 )
 );
-$wgJobTypeConf['AssembleUploadChunks'] = $wgJobTypeConf['null'];
-$wgJobTypeConf['PublishStashedFile'] = $wgJobTypeConf['null'];
-$wgJobTypeConf['htmlCacheUpdate'] = $wgJobTypeConf['null'];
-$wgJobTypeConf['refreshLinks'] = $wgJobTypeConf['null'];
-$wgJobTypeConf['refreshLinks2'] = $wgJobTypeConf['null'];
 // Note: on server failure, this should be changed to any other redis server
 $wgJobQueueAggregator = array(
 	'class'       => 'JobQueueAggregatorRedis',
