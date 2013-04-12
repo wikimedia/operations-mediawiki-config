c8eab2c0fb8f44f2b2ad791f3c258c89420512b1
Aaron Schulz <aschulz@wikimedia.org>
2013-04-12 11:15:47 -0700

    Set pmtpa queue config to match eqiad.
    * There are no queue server counterparts in tampa.
    * This should fix test wiki jobs by using the same aggregator.
    
    Change-Id: I2a22d2772e5ef1647dff86f9cfd36506523220fb

--- wmf-config/jobqueue-pmtpa.php
+++ wmf-config/jobqueue-pmtpa.php
@@ -1,10 +1,16 @@
 <?php
 # WARNING: This file is publically viewable on the web. Do not put private data here.
+$wgJobTypeConf['default'] = array(
+	'class'       => 'JobQueueRedis',
+	'redisServer' => '10.64.32.76', # rdb1001 (master)
+	#'redisServer' => '10.64.32.77', # rdb1002 (slave)
+	'redisConfig' => array( 'connectTimeout' => 1 )
+);
+// Note: on server failure, this should be changed to any other redis server
 $wgJobQueueAggregator = array(
 	'class'       => 'JobQueueAggregatorRedis',
-	'redisServer' => '10.0.12.1', # mc1
-	'redisConfig' => array(
-		'connectTimeout' => 1
-	)
+	'redisServer' => '10.64.0.180', # mc1001
+	#'redisServer' => '10.0.12.1', # mc1
+	'redisConfig' => array( 'connectTimeout' => 1 )
 );
 # vim: set sts=4 sw=4 et :
