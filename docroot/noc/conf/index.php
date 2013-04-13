566424388816a0094cf1f2720f5855ca38230562
Timo Tijhof <krinklemail@gmail.com>
2013-04-13 11:22:30 +0200

    noc: Add missing entries to createTxtFileSymlinks.sh
    The apache-config and dblist files were still manually created.
    These are now tracked in createTxtFileSymlinks.sh as well to
    avoid links from going stale or being forgotten about.
    
    Moved fc-list to not be exclusively in docroot/conf, and created
    txt links for the cdb files since they trigger a download in Chrome
    (instead of viewed raw).
    
    Change-Id: I246a6e2b71a1b3bfac710666b40e1fc25acbfa05

--- docroot/noc/conf/index.php
+++ docroot/noc/conf/index.php
@@ -1,4 +1,27 @@
-<!DOCTYPE html>
+<?php
+
+	function outputFiles( $viewFilenames, $highlight = true ) {
+		$viewFilenames = array_map( 'basename', $viewFilenames );
+		natsort( $viewFilenames );
+		foreach ( $viewFilenames as $viewFilename ) {
+			$srcFilename = substr( $viewFilename, -4 ) === '.txt'
+				? substr( $viewFilename, 0, -4 )
+				: $viewFilename;
+			echo "\n<li>";
+
+			if ( $highlight ) {
+				echo  '<a href="./highlight.php?file=' . htmlspecialchars( urlencode( $srcFilename ) ) . '">'
+					. htmlspecialchars( $srcFilename );
+				echo '</a> (<a href="./' . htmlspecialchars( urlencode( $viewFilename ) ) . '">raw text</a>)';
+			} else {
+				echo '</a> <a href="./' . htmlspecialchars( urlencode( $viewFilename ) ) . '">'
+					. htmlspecialchars( $srcFilename ) . '</a>';
+			}
+			echo '</li>';
+		}
+	}
+
+?><!DOCTYPE html>
 <html lang="en">
 <head>
 	<title>Wikimedia configuration files</title>
@@ -21,7 +44,7 @@
 <h2><img src="./images/document.png" alt=""> Apache configuration</h2>
 <ul>
 <?php
-        outputFiles( glob( __DIR__ . '/*.conf' ), false );
+    outputFiles( glob( __DIR__ . '/*.conf' ) );
 ?>
 </ul>
 
@@ -29,29 +52,10 @@
 <ul>
 <?php
 	$viewFilenames = array_merge(
-		glob( __DIR__ . '/*.txt' )
+		glob( __DIR__ . '/*.php.txt' ),
+		glob( __DIR__ . '/{fc-list,langlist,wikiversions.dat}.txt', GLOB_BRACE )
 	);
 	outputFiles( $viewFilenames );
-	function outputFiles( $viewFilenames, $highlight = true ) {
-		$viewFilenames = array_map( 'basename', $viewFilenames );
-		natsort( $viewFilenames );
-		foreach ( $viewFilenames as $viewFilename ) {
-			$srcFilename = substr( $viewFilename, -4 ) === '.txt'
-				? substr( $viewFilename, 0, -4 )
-				: $viewFilename;
-			echo "\n<li>";
-
-			if ( $highlight ) {
-				echo  '<a href="./highlight.php?file=' . htmlspecialchars( urlencode( $srcFilename ) ) . '">'
-					. htmlspecialchars( $srcFilename );
-				echo '</a> (<a href="./' . htmlspecialchars( urlencode( $viewFilename ) ) . '">raw text</a>)';
-			} else {
-				echo '</a> <a href="./' . htmlspecialchars( urlencode( $viewFilename ) ) . '">'
-					. htmlspecialchars( $srcFilename ) . '</a>';
-			}
-			echo '</li>';
-		}
-	}
 ?>
 </ul>
 
@@ -65,13 +69,17 @@
 <h3><img src="./images/document.png" alt=""> CDB files</h3>
 <ul>
 <?php
-	outputFiles( glob( __DIR__ . '/*.cdb' ), false );
+	outputFiles( glob( __DIR__ . '/*.cdb.txt' ) );
 ?>
 </ul>
 
 <h2>Lucene search configuration</h2>
 <ul>
-<li><a href="./lsearch-global-2.1.conf">lsearch-global-2.1.conf</a></li>
+<?php
+	outputFiles( array(
+		'./lsearch-global-2.1.conf',
+	) );
+?>
 </ul>
 
 <hr>
