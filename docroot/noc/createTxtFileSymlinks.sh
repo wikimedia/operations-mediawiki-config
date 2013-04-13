14f04881869fd63cf7c219bf93b4fa2e1779d257
Timo Tijhof <krinklemail@gmail.com>
2013-04-13 11:22:30 +0200

    noc: Add missing entries to createTxtFileSymlinks.sh
    The apache-config and dblist files were still manually created.
    These are now tracked in createTxtFileSymlinks.sh as well to
    avoid links from going stale or being forgotten about.
    
    Also moved fc-list to not be exclusively in docroot/conf.
    
    Change-Id: I246a6e2b71a1b3bfac710666b40e1fc25acbfa05

--- docroot/noc/createTxtFileSymlinks.sh
+++ docroot/noc/createTxtFileSymlinks.sh
@@ -1,71 +1,120 @@
 #!/bin/bash
 cd $(dirname $0)/conf
 
-# [mediawiki-config]/wmf-config/
-wmf_config_files=(
-	CommonSettings-labs.php
-	CommonSettings.php
-	InitialiseSettings-labs.php
-	InitialiseSettings.php
-	PoolCounterSettings-eqiad.php
-	PoolCounterSettings-pmtpa.php
-	StartProfiler.php
-	abusefilter.php
-	codereview.php
-	db-eqiad.php
-	db-labs.php
-	db-pmtpa.php
-	db-secondary.php
-	ext-labs.php
-	ext-production.php
-	filebackend-labs.php
-	filebackend.php
-	flaggedrevs.php
-	jobqueue-eqiad.php
-	jobqueue-pmtpa.php
-	liquidthreads.php
-	logging-labs.php
-	lucene-common.php
-	lucene-labs.php
-	lucene-production.php
-	mc-eqiad.php
-	mc-labs.php
-	mc-pmtpa.php
-	missing.php
-	mobile-labs.php
-	mobile.php
-	proofreadpage.php
-	throttle.php
-	wgConf.php
-)
-
-# [mediawiki-config]/
-misc_files=(
-	wikiversions.dat
+# Create .txt symlink from mediawiki-config
+mwconfig_files_txt=(
 	langlist
+	wikiversions.dat
+	wmf-config/CommonSettings-labs.php
+	wmf-config/CommonSettings.php
+	wmf-config/InitialiseSettings-labs.php
+	wmf-config/InitialiseSettings.php
+	wmf-config/PoolCounterSettings-eqiad.php
+	wmf-config/PoolCounterSettings-pmtpa.php
+	wmf-config/StartProfiler.php
+	wmf-config/abusefilter.php
+	wmf-config/codereview.php
+	wmf-config/db-eqiad.php
+	wmf-config/db-labs.php
+	wmf-config/db-pmtpa.php
+	wmf-config/db-secondary.php
+	wmf-config/ext-labs.php
+	wmf-config/ext-production.php
+	wmf-config/filebackend-labs.php
+	wmf-config/filebackend.php
+	wmf-config/flaggedrevs.php
+	wmf-config/jobqueue-eqiad.php
+	wmf-config/jobqueue-pmtpa.php
+	wmf-config/liquidthreads.php
+	wmf-config/logging-labs.php
+	wmf-config/lucene-common.php
+	wmf-config/lucene-labs.php
+	wmf-config/lucene-production.php
+	wmf-config/mc-eqiad.php
+	wmf-config/mc-labs.php
+	wmf-config/mc-pmtpa.php
+	wmf-config/missing.php
+	wmf-config/mobile-labs.php
+	wmf-config/mobile.php
+	wmf-config/proofreadpage.php
+	wmf-config/throttle.php
+	wmf-config/wgConf.php
 )
 
-if ls ./*.txt >/dev/null 2>&1
-then
-	rm ./*.txt
-fi
+# Create non-txt symlink from mediawiki-config
+# Mostly for backwards compatibility.
+# Raw views should use txt for consistent behaviour in browsers.
+mwconfig_files=(
+	all.dblist
+	closed.dblist
+	deleted.dblist
+	fc-list
+	fishbowl.dblist
+	flaggedrevs.dblist
+	langlist
+	large.dblist
+	medium.dblist
+	private.dblist
+	s1.dblist
+	s2.dblist
+	s3.dblist
+	s4.dblist
+	s5.dblist
+	s6.dblist
+	s7.dblist
+	small.dblist
+	special.dblist
+	trusted-xff.cdb
+	wikibooks.dblist
+	wikidataclient.dblist
+	wikimania.dblist
+	wikimedia.conf
+	wikimedia.dblist
+	wikinews.dblist
+	wikipedia.dblist
+	wikiquote.dblist
+	wikisource.dblist
+	wikiversions.dat
+	wikiversity.dblist
+	wikivoyage.dblist
+	wiktionary.dblist
+	wmf-config/interwiki.cdb
+)
 
-for i in "${wmf_config_files[@]}"
+# Create non-txt symlink from /home/wikipedia/conf
+other_config_file=(
+	httpd/en2.conf
+	httpd/foundation.conf
+	httpd/ganglia.conf
+	lucene/lsearch-global-2.1.conf
+	httpd/main.conf
+	httpd/nagios.conf
+	httpd/nonexistent.conf
+	httpd/postrewrites.conf
+	httpd/redirects.conf
+	httpd/remnant.conf
+	httpd/www.wikipedia.conf
+)
+
+for i in ./*
 do
-	ln -s ../../../wmf-config/$i ./$i.txt
+	if [[ $i != "./index.php" && $i != "./highlight.php" && $i != "./images" ]]
+	then
+		rm $i
+	fi
 done
 
-for i in "${misc_files[@]}"
+for i in "${mwconfig_files_txt[@]}"
 do
-	ln -s ../../../$i ./$i.txt
+	ln -s ../../../$i "./$(basename $i).txt"
+done
 
-	# backwards compatibity: Though non-txt sometimes triggers
-	# a download in browsers (so we use .txt for everything now)
-	# some users (RT-4927) still have links to /langlist or
-	# /wikiversions.dat
-	if [[ -e ./$i || -L ./$i ]]
-	then
-		rm ./$i
-	fi
-	ln -s ../../../$i ./$i
+for i in "${mwconfig_files[@]}"
+do
+	ln -s ../../../$i "./$(basename $i)"
+done
+
+for i in "${other_config_file[@]}"
+do
+	ln -s /home/wikipedia/conf/$i "./$(basename $i)"
 done
