2cf3e06aa99ebefc8e1cf1f8ac3affac015489f3
Dereckson <dereckson@espace-win.org>
2013-04-23 16:47:03 +0200

    (bug 47418) Babel configuration for th.wikipedia
    * Main category: User <lang>
    * By levels categories: User <lang>-<[0-5],N>
    
    Change-Id: I921d0c9d1ed7b29b683ccf77b6c7236d72897b94
    Bug: 47418

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -11584,6 +11584,15 @@ $wgConf->settings = array(
 		'5' => false,
 		'N' => 'Wiktionary:Användare %code%-N',
 	),
+	'thwiki' => array( // Bug 47418
+	        '0' => 'User %code%-0',
+	        '1' => 'User %code%-1',
+	        '2' => 'User %code%-2',
+	        '3' => 'User %code%-3',
+	        '4' => 'User %code%-4',
+	        '5' => 'User %code%-5',
+	        'N' => 'User %code%-N',
+	),
 	'tlwiki' => array(
 		'0' => false,
 		'1' => 'User %code%-1',
@@ -11729,6 +11738,7 @@ $wgConf->settings = array(
 	'specieswiki' => 'User %code%',
 	'svwiki' => 'Användare %code%',
 	'svwiktionary' => 'Wiktionary:Användare %code%', // Bug 42288
+	'thwiki' => 'User %code%', // Bug 47418
 	'tlwiki' => 'User %code%',
 	'ukwiki' => 'User %code%',
 	'wikidatawiki' => 'User %code%',
