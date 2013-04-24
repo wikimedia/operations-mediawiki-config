d9e7f780d0e4008b1d5981604689b0232668ad42
lwelling <lwelling@wikimedia.org>
2013-04-24 16:59:31 -0400

    Update event logging schema for Echo to match schema at http://meta.wikimedia.org/w/index.php?title=Schema%3AEcho&diff=5423520&oldid=5417976
    Change-Id: I55e3d57126936519d1abbcaa50d7ae8965b84f64

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -2511,7 +2511,7 @@ if ( $wmgUseEcho ) {
 	$wgJobTypeConf['MWEchoNotificationEmailBundleJob'] = array( 'checkDelay' => true ) + $wgJobTypeConf['default'];
 
 	$wgEchoConfig['eventlogging']['Echo']['enabled'] = true;
-	$wgEchoConfig['eventlogging']['Echo']['revision'] = 5364744;
+	$wgEchoConfig['eventlogging']['Echo']['revision'] = 5423520;
 	$wgEchoEnableEmailBatch = $wmgEchoEnableEmailBatch;
 	$wgEchoEmailFooterAddress = $wmgEchoEmailFooterAddress;
 	$wgEchoBundleEmailInterval = $wmgEchoBundleEmailInterval;
