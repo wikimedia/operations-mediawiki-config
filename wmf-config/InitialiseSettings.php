32e8c5e7289de05fee4ef7558563ed5459b796ed
Matthias Mullie <git@mullie.eu>
2013-04-25 11:46:48 +0200

    Enable Auto-archive for enwiki, dewiki, frwiki
    Update auto-archive delays
    Also: config var was confusing; all others are ArticleFeedbackv5 prefixed
    
    Change-Id: I7429f8774aafb0ceb547003698bde40d7975e80a

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -10709,8 +10709,12 @@ $wgConf->settings = array(
  * This should be manually set to true, to make sure the filter
  * does not show up when the cronjob is not running.
  */
-'wmgArticleFeedbackAutoArchiveEnabled' => array(
+'wmgArticleFeedbackv5AutoArchiveEnabled' => array(
 	'default' => false,
+	'testwiki' => true,
+	'enwiki' => true,
+	'dewiki' => true,
+	'frwiki' => true,
 ),
 /*
  * This is the TTL before an item is archived (if auto-archive is enabled);
@@ -10718,20 +10722,13 @@ $wgConf->settings = array(
  * faster. For a fixed date, a simple integer can be passed instead of an
  * array, to make it "amount of unreviewed feedback"-agnostic.
  */
-'wmgArticleFeedbackAutoArchiveTtl' => array(
+'wmgArticleFeedbackv5AutoArchiveTtl' => array(
 	'default' => array(
 		0 => '+2 years', // < 9 unreviewed feedback
-		10 => '+1 month', // 10-19
-		20 => '+1 week', // 20-29
-		30 => '+3 days', // 30-39
-		40 => '+2 days', // > 40
-	),
-	'dewiki' => array(
-		0 => '+1 year', // < 9 unreviewed feedback
 		10 => '+6 months', // 10-19
 		20 => '+3 months', // 20-29
-		30 => '+2 months', // 30-39
-		40 => '+1 month', // > 40
+		30 => '+1 month', // 30-39
+		40 => '+1 week', // > 40
 	),
 ),
 /*
