220704d4c73ee50d8b2cd01db8ee9a3fa36a18e4
Peter Gehres <pgehres@wikimedia.org>
2013-04-29 15:39:09 -0700

    Adding configuration variables for Extension:AccountAudit
    Change-Id: I5b38c047156b5cb050e4d2daaed8a601e6654bb0

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -12397,6 +12397,10 @@ $wgConf->settings = array(
 	'foundationwiki' => false,
 	'collabwiki' => false,
 	'donatewiki' => false,
+),
+'wmfUseAccountAudit' => array(
+	'default' => false,
+	'test2wiki' => true
 )
 );
 
