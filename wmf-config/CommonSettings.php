45d4d292328b42a7cade453185ab1f99a6dd2233
bsitu <bsitu@wikimedia.org>
2013-04-30 14:03:05 -0700

    Enable job queue to process web and email notifs on testwiki
    Will enable it for other wikis when this works on testwiki/test2wiki
    
    Change-Id: I9ccc8314daa43287fadb75219db5bba2dc47cbbd

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -2312,6 +2312,9 @@ if ( $wmgUseEcho ) {
 
 	// Allow for migration time functionality
 	$wgRecentEchoInstall = true;
+
+	// Whether to use job queue to process web and email notifications
+	$wgEchoUseJobQueue = $wmgEchoUseJobQueue;
 }
 
 if ( $wmgUseThanks ) {
