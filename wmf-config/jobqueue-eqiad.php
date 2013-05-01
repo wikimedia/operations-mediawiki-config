8200d1f437a536c2f563e5f83f9586be76dc0eb3
Tim Starling <tstarling@wikimedia.org>
2013-05-01 11:10:27 +1000

    Use a password for job queue and session redis
    This should be safe to deploy in advance of the server change.
    
    The GettingStarted change (Iab1abc8e) needs to be deployed before the
    server change.
    
    Change-Id: Ibc0f49089d39f08fa6b2d42fb6d97bccc0eb32ec

--- wmf-config/jobqueue-eqiad.php
+++ wmf-config/jobqueue-eqiad.php
@@ -6,13 +6,19 @@ $wgJobTypeConf['default'] = array(
 	'class'       => 'JobQueueRedis',
 	'redisServer' => '10.64.32.76', # rdb1001 (master)
 	#'redisServer' => '10.64.32.77', # rdb1002 (slave)
-	'redisConfig' => array( 'connectTimeout' => 1 )
+	'redisConfig' => array(
+		'connectTimeout' => 1,
+		'password' => $wmgRedisPassword,
+	)
 );
 // Note: on server failure, this should be changed to any other redis server
 $wgJobQueueAggregator = array(
 	'class'       => 'JobQueueAggregatorRedis',
 	'redisServer' => '10.64.0.180', # mc1001
-	'redisConfig' => array( 'connectTimeout' => 1 )
+	'redisConfig' => array(
+		'connectTimeout' => 1,
+		'password' => $wmgRedisPassword,
+	)
 );
 # vim: set sts=4 sw=4 et :
 
@@ -23,6 +29,9 @@ $wgJobQueueMigrationConfig = array(
 	'redis' => array(
 		'class'       => 'JobQueueRedis',
 		'redisServer' => '10.64.32.76',
-		'redisConfig' => array( 'connectTimeout' => 1 )
+		'redisConfig' => array(
+			'connectTimeout' => 1,
+			'password' => $wmgRedisPassword,
+		)
 	)
 );
