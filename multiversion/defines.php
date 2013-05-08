317b5feb8bc0cebf5637cfc1be478590d4e7a25e
Timo Tijhof <krinklemail@gmail.com>
2013-05-08 23:28:06 +0200

    Fix various path inflexibilities and inconsistencies
    - MWVersion.php#getMediaWiki
      The other accesses to $_SERVER in this function have an error supression on
      it, but this one did not. Messing with something locally from the command
      line threw an E_NOTICE on this one.
    
    - multiversion/defines
      Simplify definitions by re-using the same variables instead of repeating the
      same pattern. Also added commented out lines that would work when using
      mediawiki-config.git off-cluster (e.g. in a tool to read config information).
    
    - multiversion/populateWikiversions
      Function populateWikiversionsCDB uses MULTIVER_CDB_DIR_HOME (on line 46) but
      multiversion/defines is not loaded. Not sure how this ever worked.
    
      Warning: file_put_contents(MULTIVER_CDB_DIR_HOME/wikiversions.dat): failed to open stream: No such file or directory in /Users/krinkle/Development/wikimedia/operations/mediawiki-config/multiversion/populateWikiversions on line 45
    
    - CommonSettings
      The on-demand loading of Profiler if CommonSettings is used outside MediaWiki
      context was broken since it has moved in core and now requires two files to
      get wfProfileIn and ProfileStub.
    
    - Avoid making assumptions that php-X.X ($IP) is a direct subdirectory of
      common (e.g. "$IP/../all.dblist" etc.). Instead separating these concerns by
      having all MediaWiki inclusions use $IP, and all mediawiki-config / common
      references use $wmfCommonDir (set relatively based on where CommonSettings.php,
      which is in the same repository as all.dblist).
    
    Change-Id: Ia723694e90af8d84182620043fba0563076fde81

--- multiversion/defines.php
+++ multiversion/defines.php
@@ -2,11 +2,13 @@
 /**
  * Define installation specific MWMultiVersion values like paths
  */
+#define( 'MULTIVER_COMMON_HOME', dirname( __DIR__ ) );
 define( 'MULTIVER_COMMON_HOME', '/home/wikipedia/common' );
+#define( 'MULTIVER_COMMON_APACHE', dirname( __DIR__ ) );
 define( 'MULTIVER_COMMON_APACHE', '/usr/local/apache/common-local' );
 
-define( 'MULTIVER_CDB_DIR_HOME', '/home/wikipedia/common' );
-define( 'MULTIVER_CDB_DIR_APACHE', '/usr/local/apache/common-local' );
+define( 'MULTIVER_CDB_DIR_HOME', MULTIVER_COMMON_HOME );
+define( 'MULTIVER_CDB_DIR_APACHE', MULTIVER_COMMON_APACHE );
 
-define( 'MULTIVER_404SCRIPT_PATH_HOME', '/home/wikipedia/common/wmf-config/missing.php' );
-define( 'MULTIVER_404SCRIPT_PATH_APACHE', '/usr/local/apache/common-local/wmf-config/missing.php' );
+define( 'MULTIVER_404SCRIPT_PATH_HOME', MULTIVER_COMMON_HOME . '/wmf-config/missing.php' );
+define( 'MULTIVER_404SCRIPT_PATH_APACHE', MULTIVER_COMMON_APACHE . '/wmf-config/missing.php' );
