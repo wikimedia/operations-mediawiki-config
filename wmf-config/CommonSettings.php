637002769ac309ea631c9defcc58efc5adfa9e96
Reedy <reedy@wikimedia.org>
2013-05-15 11:37:27 +0000

    Refactor out session code
    Change-Id: Id31586a4ffcb1bf036fbbe93837d709fdaab2cee

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -366,56 +366,7 @@ $wgObjectCaches['mysql-multiwrite'] = array(
 	)
 );
 
-$sessionRedis = array(
-	'pmtpa' => array(
-		'10.0.12.1', # mc1
-		'10.0.12.2', # mc2
-		'10.0.12.3', # mc3
-		'10.0.12.4', # mc4
-		'10.0.12.5', # mc5
-		'10.0.12.6', # mc6
-		'10.0.12.7', # mc7
-		'10.0.12.8', # mc8
-		'10.0.12.9', # mc9
-		'10.0.12.10', # mc10
-		'10.0.12.11', # mc11
-		'10.0.12.12', # mc12
-		'10.0.12.13', # mc13
-		'10.0.12.14', # mc14
-		'10.0.12.15', # mc15
-		'10.0.12.16', # mc16
-	),
-	'eqiad' => array(
-		'10.64.0.180', # mc1001
-		'10.64.0.181', # mc1002
-		'10.64.0.182', # mc1003
-		'10.64.0.183', # mc1004
-		'10.64.0.184', # mc1005
-		'10.64.0.185', # mc1006
-		'10.64.0.186', # mc1007
-		'10.64.0.187', # mc1008
-		'10.64.0.188', # mc1009
-		'10.64.0.189', # mc1010
-		'10.64.0.190', # mc1011
-		'10.64.0.191', # mc1012
-		'10.64.0.192', # mc1013
-		'10.64.0.193', # mc1014
-		'10.64.0.194', # mc1015
-		'10.64.0.195', # mc1016
-	),
-);
-
-// Cache to hold user sessions in production:
-$wgObjectCaches['sessions'] = array(
-	'class'   => 'RedisBagOStuff',
-	'servers' => $sessionRedis[$wmfDatacenter],
-	'password' => $wmgRedisPassword,
-);
-
-// Override for beta:
-if( $wmfRealm == 'labs' ) {
-	$wgObjectCaches['sessions'] = $wgObjectCaches['memcached-pecl'];
-}
+require( getRealmSpecificFilename( "$wmfConfigDir/session.php" ) );
 
 // Use the cache setup above and configure sessions caching
 $wgSessionCacheType = 'sessions';
