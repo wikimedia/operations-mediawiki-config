9be704d3db3a0eee6a2ca13b2f7e56fea79028be
Ariel T. Glenn <ariel@wikimedia.org>
2013-05-16 15:33:56 +0300

    redis job queue for labs
    Change-Id: Idd23ab27fa9d886411908086e5ad0b08cba7ab1b

---
+++ wmf-config/jobqueue-labs.php
@@ -0,0 +1,24 @@
+<?php
+# WARNING: This file is publically viewable on the web. Do not put private data here.
+
+if( $wmfRealm == 'labs' ) {  # safe guard
+
+$wgJobTypeConf['default'] = array(
+	'class'       => 'JobQueueRedis',
+	'redisServer' => '10.4.0.83', # deployment-redisdb
+	'redisConfig' => array(
+		'connectTimeout' => 1,
+		'password' => $wmgRedisPassword,
+	)
+);
+
+$wgJobQueueAggregator = array(
+	'class'       => 'JobQueueAggregatorRedis',
+	'redisServer' => '10.4.0.83', # deployment-redisdb
+	'redisConfig' => array(
+		'connectTimeout' => 1,
+		'password' => $wmgRedisPassword,
+	)
+);
+
+} # end safe guard
