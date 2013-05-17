7649276ddb3f20678de265c37fe75e1d5e642956
Matt Walker <mwalker@khaosdev.com>
2013-05-17 11:04:23 -0700

    Removing France as a Special Redirect for Fundraising
    - Removal of France and its territories as a fundraising entity per their
      request due to FR regulations about 50% of earnings being remitted.
    
    - Addition of more blacklisted countries per Katie's email 2013-04-26
      "New list of countries being blocked on payments"
    
    ... we decided that we should disallow payments from a list of countries that were
    provided to the team by legal. Here is an updated list of countries that are no
    longer being allowed through the payments cluster. The list is as follows ...
    
    BY    Belarus
    CD    Democratic Republic of the Congo
    CI    Cote d'Ivoire
    CU    Cuba
    IQ    Iraq
    IR    Iran
    KP    North Korea
    LB    Lebanon
    LY    Libya
    MM    Myanmar (Burma)
    SD    Sudan
    SO    Somalia
    SY    Syria
    YE    Yemen
    ZW    Zimbabwe
    
    Change-Id: I6c541bed811023fe5adfcf4ec5eb8e4939a79416

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -1662,10 +1662,17 @@ if ( $wmgEnableLandingCheck ) {
 	require_once(  "$IP/extensions/LandingCheck/LandingCheck.php" );
 
 	$wgPriorityCountries = array(
-		'FR', 'DE', 'CH', 'SY', 'IR', 'CU',
-		// French Territories per WMFr email 2012-06-13
-		'GP', 'MQ', 'GF', 'RE', 'YT', 'PM',
-		'NC', 'PF', 'WF', 'BL', 'MF', 'TF',
+		// === Fundraising Chapers
+		'DE', 'CH',
+
+		// --- France and it's territories (per WMFr email 2012-06-13)
+		//     Not a fundraising chapter in 2013+ due to FR regulations 
+		//'FR',
+		//'GP', 'MQ', 'GF', 'RE', 'YT', 'PM',
+                //'NC', 'PF', 'WF', 'BL', 'MF', 'TF',
+
+		// === Blacklisted countries
+                'BY', 'CD', 'CI', 'CU', 'IQ', 'IR', 'KP', 'LB', 'LY', 'MM', 'SD', 'SO', 'SY', 'YE', 'ZW',
 	);
 	$wgLandingCheckPriorityURLBase = "//wikimediafoundation.org/wiki/Special:LandingCheck";
 	$wgLandingCheckNormalURLBase = "//donate.wikimedia.org/wiki/Special:LandingCheck";
