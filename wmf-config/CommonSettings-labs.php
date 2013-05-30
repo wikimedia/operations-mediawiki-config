2da3e16e397f0947b25e4a81591fa33bf7ed3b76
Catrope <roan.kattouw@gmail.com>
2013-05-30 14:31:15 +0100

    Make VisualEditor work in labs (hopefully)
    Change-Id: I5654b92503b2a82b9ee9614589ceba46b91116ef

--- wmf-config/CommonSettings-labs.php
+++ wmf-config/CommonSettings-labs.php
@@ -69,7 +69,10 @@ if ( $wmgEnableGeoData ) {
 	$wgMaxCoordinatesPerPage = 2000;
 }
 
-
+if ( $wmgUseVisualEditor ) {
+	$wgVisualEditorParsoidURL = 'http://10.4.0.61/'; // deployment-parsoidcache2
+	$wgVisualEditorParsoidReportProblemURL = 'http://10.4.0.33/_bugs/'; // parsoid-spof
+}
 
 # temporary extensions
 # ========================================================================
