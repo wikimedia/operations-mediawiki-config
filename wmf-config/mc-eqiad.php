bb1acb3e710c2ccfe3206b16b8b98aeae5f59b57
asher <afeldman@wikimedia.org>
2013-06-12 11:10:02 -0700

    test twemproxy on 10 prod api servers
    Change-Id: I0969271c15576f9bdb4ce93180e6d062f7462612

--- wmf-config/mc-eqiad.php
+++ wmf-config/mc-eqiad.php
@@ -10,27 +10,37 @@ $wgMemCachedTimeout = 250000; # default is 100000
 # rather a consistent hash based on key and server addresses,
 # so the ordering of servers is not important. Additionally, the
 # number of servers can grow/shrink without *too* much disruption.
-$wgObjectCaches['memcached-pecl'] = array(
-	'class'      => 'MemcachedPeclBagOStuff',
-	'serializer' => 'igbinary',
-	'servers'    => array(
-		'10.64.0.180',
-		'10.64.0.181',
-		'10.64.0.182',
-		'10.64.0.183',
-		'10.64.0.184',
-		'10.64.0.185',
-		'10.64.0.186',
-		'10.64.0.187',
-		'10.64.0.188',
-		'10.64.0.189',
-		'10.64.0.190',
-		'10.64.0.191',
-		'10.64.0.192',
-		'10.64.0.193',
-		'10.64.0.194',
-		'10.64.0.195',
-	)
-);
-
+if (strpos(gethostname(), "mw113") !== false ) {
+	$wgObjectCaches['memcached-pecl'] = array(
+		'class'      => 'MemcachedPeclBagOStuff',
+		'serializer' => 'igbinary',
+		'persistent' => true,
+		'servers'    => array(
+			'127.0.0.1',
+		)
+	);
+} else {
+	$wgObjectCaches['memcached-pecl'] = array(
+		'class'      => 'MemcachedPeclBagOStuff',
+		'serializer' => 'igbinary',
+		'servers'    => array(
+			'10.64.0.180',
+			'10.64.0.181',
+			'10.64.0.182',
+			'10.64.0.183',
+			'10.64.0.184',
+			'10.64.0.185',
+			'10.64.0.186',
+			'10.64.0.187',
+			'10.64.0.188',
+			'10.64.0.189',
+			'10.64.0.190',
+			'10.64.0.191',
+			'10.64.0.192',
+			'10.64.0.193',
+			'10.64.0.194',
+			'10.64.0.195',
+		)
+	);
+}
 # vim: set sts=4 sw=4 et :
