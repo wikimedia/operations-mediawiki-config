126c7628d05c7f9de42c37ed680618810f22377e
Ariel T. Glenn <ariel@wikimedia.org>
2013-06-18 13:07:51 +0300

    clean up squid NoPurge list for beta labs
    the regular ip address of the squid shouldn't be there
    but the local ip, used by ssl terminators, should be
    Change-Id: Ib812569daef45458345b0d1d74903bd83ef9f3b7

--- wmf-config/squid-labs.php
+++ wmf-config/squid-labs.php
@@ -1,4 +1,10 @@
 <?php
 
 $wgSquidServers = array( '10.4.0.17' );
-$wgSquidServersNoPurge = array( '10.4.0.17' );
+
+# our text squid in beta labs gets forwarded requests
+# from the ssl terminator, to 127.0.0.1, so adding that
+# address to the NoPurge list so that XFF headers for it
+# will be stripped; purge requests should get to it
+# on the address in the SquidServers list
+$wgSquidServersNoPurge = array( '127.0.0.1' );
