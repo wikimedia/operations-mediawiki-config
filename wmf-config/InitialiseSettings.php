efd9bf8fb974f292d297c20752eb089008ddfe2c
James D. Forrester <jforrester@wikimedia.org>
2013-06-29 20:01:14 -0700

    Enable VisualEditor for all logged-in users of the English Wikipedia
    Bug: 49997
    Change-Id: I7fa967db7abdc17e3e7d0547fa8941d2c912c1d5

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -1,4 +1,5 @@
 <?php
+<?php
 /**
  * vim: set sw=4 ts=4 noet foldmarker=@{,@} foldmethod=marker:
  * - recognize folding markers just like DefaultSettings.php
@@ -10514,7 +10515,8 @@ $wgConf->settings = array(
 
 'wmgVisualEditorDefault' => array(
 	'default' => false,
-	'mediawikiwiki' => true // Enabled for all users
+	'mediawikiwiki' => true
+	'enwiki' => true
 ),
 
 'wmgVisualEditorExperimental' => array(
@@ -10525,6 +10527,12 @@ $wgConf->settings = array(
 'wmgUseTemplateData' => array(
 	'default' => true,
 ),
+
+// Bug 50000 - to remove once roll-out complete
+'wmgVisualEditorDisableForAnons' => array(
+	'default' => false,
+	'enwiki' => true, // Bug 49997: Temporarily set during enwiki phased deployment
+)
 
 'wmgVisualEditorEnableEventLogging' => array(
 	'default' => false,
