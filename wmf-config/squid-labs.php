795e6b5bc2b150c0b6dbfaca3922879e68c393eb
Antoine Musso <hashar@free.fr>
2013-07-01 15:38:47 +0200

    beta: make use of wgHTCPMulticastRouting
    MediaWiki core received support to route HTCP requests to different
    IP/port based on the purged URL:
    	https://gerrit.wikimedia.org/r/#/c/4486/
    
    The code has never been used so far but is going to be useful for beta
    which has no multicast support and one cache per role.
    
    Change-Id: Ib1a090e7f715f216fc35a139baaf0f1bf7cca703

--- wmf-config/squid-labs.php
+++ wmf-config/squid-labs.php
@@ -13,3 +13,19 @@ $wgSquidServers = array(
 # will be stripped; purge requests should get to it
 # on the address in the SquidServers list
 $wgSquidServersNoPurge = array( '127.0.0.1' );
+
+# The beta cluster does not have multicast, hence we route the purge
+# requests directly to the varnish instances.  They have varnishhtcpd listening
+# on UDP port 4827.
+$wgHTCPMulticastRouting = array(
+	'|^https?://upload\.beta\.wmflabs\.org|' => array(
+		'host' => '10.4.0.214',  # deployment-cache-upload03
+		'port' => 4827,
+	),
+
+	# Fallback??
+	'' => array(
+		'host' => '10.4.1.133',  # deployment-cache-text1
+		'port' => 4827,
+	),
+);
