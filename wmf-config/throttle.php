07291710057639ea54876b5fdae025d8706df747
Dereckson <dereckson@espace-win.org>
2013-07-05 23:03:20 +0200

    Throttle now handles IP ranges.
    Adding a new option parameter 'range'.
    
    Option name ... range
    Type .......... string or array
    Value ......... IP range or array of such ranges
    
    Calls MediaWiki IP class to determine if an IP belongs to a range,
    or to a set of ranges.
    
    IPv4 ("a.b.c.d/n") and IPv6 ranges are supported.
    
    Change-Id: I2294bac734bca283b894df6c492a29f6fd18db99

--- wmf-config/throttle.php
+++ wmf-config/throttle.php
@@ -14,6 +14,7 @@ $wmgThrottlingExceptions = array();
 # or project dbname. Options are:
 #  'value'  => new value for $wgAccountCreationThrottle (default: 50)
 #  'IP'     => client IP as given by wfGetIP() or array (default: any IP)
+#  'range'  => alternatively, the client IP CIDR ranges or array (default: any range)
 #  'dbname' => a $wgDBname or array of dbnames to compare to
 #             (eg. enwiki, metawiki, frwikibooks, eswikiversity)
 #             (default: any project)
@@ -27,6 +28,25 @@ $wmgThrottlingExceptions[] = array( // Bug 58114
 	'value'  => 45,
 );
 ## Add throttling definitions above.
+
+## Helper methods:
+
+/**
+ * Determines if an IP address is a list of CIDR a.b.c.d/n ranges.
+ *
+ * @param string $ip the IP to check
+ * @param array $range the IP ranges, each element a range
+ *
+ * @return Boolean true if the specified adress belongs to the specified range; otherwise, false.
+ */
+function inIPRanges ( $ip, $ranges ) {
+	foreach ( $ranges as $range ) {
+		if ( IP::isInRange( $ip, $range ) ) {
+			return true;
+		}
+	}
+	return false;
+}
 
 # Will eventually raise value when MediaWiki is fully initialized:
 $wgExtensionFunctions[] = 'efRaiseAccountCreationThrottle';
@@ -65,6 +85,20 @@ function efRaiseAccountCreationThrottle() {
 			if ( is_array( $options['IP'] ) && !in_array( wfGetIP(), $options['IP'] ) ) {
 				continue;
 			} elseif ( wfGetIP() != $options['IP'] ) {
+				continue;
+			}
+		}
+		if ( isset ( $options['range'] ) ) {
+			//Loads IP class from the MediaWiki code, for IP::isInRange.
+			global $IP;
+			define( 'MEDIAWIKI', true);
+			require_once( "$IP/includes/GlobalFunctions.php" );
+			require_once( "$IP/includes/IP.php" );
+
+			//Checks if the IP is in range
+			if ( is_array( $options['range'] ) && !inIPRanges( wfGetIP(), $options['range'] ) ) {
+				continue;
+			} elseif ( !IP::isInRange( wfGetIP(), $options['range'] ) ) {
 				continue;
 			}
 		}
