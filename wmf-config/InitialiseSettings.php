85e7515c280c1e5030acba7ca1a31911bd0b4ad5
Tomasz W. Kozlowski <tomasz@twkozlowski.net>
2013-07-06 13:28:00 +0200

    (bug 50802) Create new user groups on sh.wikipedia
    Creating new user groups: 'autopatrolled', 'filemover',
    'patroller' and 'rollback' per community request, with
    default user rights for those groups. Note: the
    'autopatrolled' user group /does not/ have the 'patrol'
    user right (yet).
    
    Bureaucrats are able to add and remove users from all
    those user groups, administrators are able to add to all
    and remove from all except 'autopatrolled'.
    
    Bug: 50802
    Change-Id: Iaebb5dd8230dade14c4aea2a3e41650bc515f93a

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -7385,6 +7385,12 @@ $wgConf->settings = array(
 		'rollbacker' => array( 'rollback' => true, ),
 		'autopatrolled' => array( 'patrol' => true, 'autopatrol' => true ),
 	),
+	'shwiki' => array( // bug 50802
+		'patroller' => array( 'patrol' => true ),
+		'autopatrolled' => array( 'autopatrol' => true ),
+		'rollbacker' => array( 'rollback' => true ),
+		'filemover' => array( 'movefile' => true, 'suppressredirect' => true ),
+	),
 	'siwiki' => array(
 		'rollbacker'    => array( 'rollback' => true ),
 		'autopatrolled' => array( 'autopatrol' => true ),
@@ -7945,6 +7951,10 @@ $wgConf->settings = array(
 		'bureaucrat' => array( 'rollbacker', 'transwiki', 'patroller' ),
 		'sysop' => array( 'rollbacker', 'flood', 'patroller' ),
 	),
+	'+shwiki' => array( // bug 50802
+		'bureaucrat' => array( 'autopatrolled', 'filemover', 'patroller', 'rollbacker' ),
+		'sysop' => array( 'autopatrolled', 'filemover', 'patroller', 'rollbacker' ),
+	),
 	'+siwiki' => array(
 		'sysop' => array( 'rollbacker', 'accountcreator', 'abusefilter', 'autopatrolled', 'confirmed', 'reviewer' ),
 	),
@@ -8359,6 +8369,10 @@ $wgConf->settings = array(
 	),
 	'+sewikimedia' => array(
 		'bureaucrat' => array( 'sysop', 'bureaucrat', 'medlem' ),
+	),
+	'+shwiki' => array( // bug 50802
+		'bureaucrat' => array( 'autopatrolled', 'filemover', 'patroller', 'rollbacker' ),
+		'sysop' => array( 'filemover', 'patroller', 'rollbacker' ),
 	),
 	'+siwiki' => array(
 		'sysop' => array( 'rollbacker', 'accountcreator', 'abusefilter', 'autopatrolled', 'confirmed', 'reviewer', ),
