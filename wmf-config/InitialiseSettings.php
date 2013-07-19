9351028768dbe2cc5144ad6d5316ea53b6f823b1
Addshore <addshorewiki@gmail.com>
2013-07-19 11:35:06 +0000

    Revert "Enabling the 'property-create' right for all users on testwikidatawiki Bug: 51637"
    This reverts commit a53188b1266111b3fd5b552ffe4435708f0929f7.
    
    Change-Id: Ifc33435e05dd2f68e8a33e913bc47122e997cce1

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -7458,11 +7458,6 @@ $wgConf->settings = array(
 		'rollbacker' => array( 'rollback' => true ),
 		'centralnoticeadmin' => array( 'centralnotice-admin' => true, 'editinterface' => true ), //adding to allow CN access without local sysop JRA 2013-02-21
 	),
-	'testwikidatawiki' => array(
-		'*' => array(
-			'property-create' => true, // Bug 51637
-		),
-	),
 	'test2wiki' => array(
 		'user' => array(
 			'upload_by_url' => true, // For testing of Flickr uploading via UploadWizard
