337e49a63a3bfb7fabd3494d29697caf49b3d4d7
Aaron Schulz <aschulz@wikimedia.org>
2013-07-22 15:37:48 -0700

    Added DB performance log
    Change-Id: Iaba9457f2cac1b32248891c8b9cd4fa0c481d328

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -4193,6 +4193,7 @@ $wgConf->settings = array(
 		'GettingStarted' => "udp://$wmfUdp2logDest/gettingstarted",
 		'zero' => "udp://$wmfUdp2logDest/zero", // zero-rated debug log
 		'geodata' => "udp://$wmfUdp2logDest/geodata",
+		'DBPerformance' => => "udp://$wmfUdp2logDest/dbperformance",
 	),
 
 	'+enwiki' => array(
