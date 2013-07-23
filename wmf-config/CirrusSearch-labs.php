20200ed02436afeac8bf449dd18f5a0d965a85ca
Nik Everett <neverett@wikimedia.org>
2013-07-23 17:16:22 -0400

    Enable CirrusSearch in beta.
    Now with each server on its own line!
    
    Change-Id: Ibf3bb8f14185474de08166073ea90b372aa0ac6a

---
+++ wmf-config/CirrusSearch-labs.php
@@ -0,0 +1,13 @@
+<?php
+# WARNING: This file is publically viewable on the web. Do not put private data here.
+
+# This file holds the MediaWiki CirrusSearch configuration which is specific
+# to the 'labs' realm which in most of the cases means the beta cluster.
+# It should be loaded AFTER CirrusSearch-common.php
+
+$wgCirrusSearchServers = array(
+	'deployment-es0',
+	'deployment-es1',
+	'deployment-es2',
+	'deployment-es3',
+);
