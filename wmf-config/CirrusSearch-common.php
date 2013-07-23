8242a5b0f590d141f5d3097d3d5e41b89ce3d16c
Nik Everett <neverett@wikimedia.org>
2013-07-23 17:16:22 -0400

    Enable CirrusSearch in beta.
    Change-Id: Ibf3bb8f14185474de08166073ea90b372aa0ac6a

---
+++ wmf-config/CirrusSearch-common.php
@@ -0,0 +1,35 @@
+<?php
+# WARNING: This file is publically viewable on the web. Do not put private data here.
+
+# This file hold the CirrusSearch configuration which is common to all realms,
+# ie settings should apply to both the production cluster and the beta
+# cluster.
+# If you ever want to stick there an IP address, you should use the per realm
+# specific files CirrusSearch-labs.php and CirrusSearch-production.php
+
+# See: https://wikitech.wikimedia.org/wiki/Search
+#
+# Contact Wikimedia operations or platform engineering for more details.
+
+require_once( "$IP/extensions/CirrusSearch/CirrusSearch.php" );
+$wgSearchType = 'CirrusSearch';
+
+$wgPoolCounterConf['CirrusSearch-Update'] = array(
+    'class' => 'PoolCounter_Client',
+    'timeout' => 120, // wait timeout in seconds
+    'workers' => 20, // maximum number of active threads in each pool
+    'maxqueue' => 200, // maximum number of total threads in each pool
+);
+$wgPoolCounterConf['CirrusSearch-Search'] = array(
+    'class' => 'PoolCounter_Client',
+    'timeout' => 30, // wait timeout in seconds
+    'workers' => 50, // maximum number of active threads in each pool
+    'maxqueue' => 10, // maximum number of total threads in each pool
+);
+
+
+# Load per realm specific configuration, either:
+# - CirrusSearch-labs.php
+# - CirrusSearch-production.php
+#
+require( getRealmSpecificFilename( "$wmfConfigDir/CirrusSearch.php" ) );
