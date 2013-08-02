cd71abb6fcb35823212e29dd879da105f71fe10a
Roan Kattouw <roan.kattouw@gmail.com>
2013-08-01 20:43:59 -0700

    Enable VisualEditor beta welcome notice for all wikis
    Now that translators have had a chance to translate the message for most
    big languages.
    
    Bug: 52552
    Change-Id: Ide7a94bfa280a9acd1b1c7adec2644cfc2f596b5

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -10492,8 +10492,7 @@ $wgConf->settings = array(
 ),
 
 'wmgVisualEditorShowBetaWelcome' => array(
-	'default' => false,
-	'enwiki' => true,
+	'default' => true,
 ),
 
 'wmgVisualEditorBetaInTab' => array(
