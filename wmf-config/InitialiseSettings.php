2ffb49365818ae5cc5e6fff8f4828249bc8e54b1
DZ <dzahn@wikimedia.org>
2013-08-08 22:49:03 -0700

    add timezone for vi.wikivoyage (bug 52034)
    use Asia/Ho_Chi_Minh
    
    Change-Id: Ic8b70cdb05f03ab23d43e6858823e4528291074c

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -336,7 +336,8 @@ $wgConf->settings = array(
 	'vecwikisource' => 'Europe/Berlin',
 	'vecwiktionary' => 'Europe/Brussels',
 	'vepwiki' => 'Europe/Moscow',
-	'xmfwiki'		=> 'Asia/Tbilisi',
+	'viwikivoyage' => 'Asia/Ho_Chi_Minh',
+	'xmfwiki' => 'Asia/Tbilisi',
 ),
 # @} end of wgLocaltimezone
 
