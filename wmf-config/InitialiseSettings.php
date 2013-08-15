c50465eb3c13c3704fdf662da8447d72a4a06169
This, that and the other <at.light@live.com.au>
2013-08-16 07:12:35 +1000

    Add several additional user groups for ckbwiki
    Groups added:
    eliminator, interface_editor, trusted, editprotected
    Each able to be added/removed by bureaucrats
    
    Bug: 52866
    Change-Id: Ibac3c765006fe75e02ce0c78aeb5dca09096f0a3

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -6776,6 +6776,26 @@ $wgConf->settings = array(
 			'reupload' => true,
 			'movefile' => true,
 		), // bug 51232
+		'eliminator' => array(
+			'delete' => true,
+			'nuke' => true,
+			'suppressredirect' => true,
+			'undelete' => true,
+			'browsearchive' => true,
+			'deletedhistory' => true,
+			'deletedtext' => true,
+		), // bug 52866
+		'interface_editor' => array(
+			'editinterface' => true,
+			'editusercss' => true,
+			'edituserjs' => true,
+		), // bug 52866
+		'trusted' => array(
+			'block' => true,
+			'blockemail' => true,
+			'protect' => true,
+		), // bug 52866
+		'editprotected' => array( 'editprotected' => true ), // bug 52866
 		'flood' => array( 'bot' => true ), // bug 51803
 		'botadmin' => array(
 			'protect' => true,
@@ -7787,6 +7807,10 @@ $wgConf->settings = array(
 			'botadmin', // Bug 52578
 			'import', // bug 52633
 			'transwiki', // bug 52633
+			'eliminator', // bug 52866
+			'interface_editor', // bug 52866
+			'trusted', // bug 52866
+			'editprotected', // bug 52866
 		),
 	),
 	'+cswiki' => array(
@@ -8223,6 +8247,10 @@ $wgConf->settings = array(
 			'botadmin', // bug 52578
 			'import', // bug 52633
 			'transwiki', // bug 52633
+			'eliminator', // bug 52866
+			'interface_editor', // bug 52866
+			'trusted', // bug 52866
+			'editprotected', // bug 52866
 		),
 	),
 	'+commonswiki' => array(
