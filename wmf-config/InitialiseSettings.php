7aabc84d2d2a5619480f4e80d79ffef261088960
jamesur <jalexander@wikimedia.org>
2013-09-12 00:02:55 -0700

    Add elwikivoyage logo
    Patch 2: comma fail
    
    Change-Id: I63a3908d447db3b94b72cf747ddeea33534eec15

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -914,6 +914,7 @@ $wgConf->settings = array(
 	// Wikivoyage
 	'hewikivoyage' => '//upload.wikimedia.org/wikipedia/commons/thumb/c/c0/Wikivoyage-Logo-v3-he.png/153px-Wikivoyage-Logo-v3-he.png',
 	'plwikivoyage' => '//upload.wikimedia.org/wikipedia/commons/f/f2/Wikivoyage-Logo-v3-small-pl.png',
+	'elwikivoyage' => '//upload.wikimedia.org/wikipedia/commons/f/fa/Wikivoyage-Logo-v3-small-el.png',
 
 	// Chapter wikis
 	'arwikimedia' => '//upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Wikimedia_Argentina_logo.svg/135px-Wikimedia_Argentina_logo.svg.png',
