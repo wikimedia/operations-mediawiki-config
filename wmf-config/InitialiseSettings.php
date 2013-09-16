9ee5e2c1f8f4e096120449981e26ca14d9e4f968
James D. Forrester <jforrester@wikimedia.org>
2013-09-16 12:12:17 -0700

    Enable VisualEditor on "phase 2" Wikipedias (anons)
    Follows-up 2eb4cbbb41, which enabled VisualEditor for logged-in
    users on the wikis listed below.
    
    Per discussion with the community, we will enable VisualEditor
    by default (for opt-out), for all users, on the following
    26 Wikipedias:
    
    * bg (Bulgarian)
    * ca (Catalan)
    * ceb (Cebuano)
    * cs (Czech)
    * da (Danish)
    * el (Modern Greek)
    * et (Estonian)
    * eu (Basque)
    * fi (Finnish)
    * gl (Galician)
    * hr (Croatian)
    * hu (Hungarian)
    * id (Indonesian)
    * lv (Latvian)
    * ms (Malay)
    * nap (Neopolitan)
    * nn (Norwegian - Nynorsk)
    * no (Norwegian - Bokmål)
    * simple (Simple English)
    * sk (Slovak)
    * sl (Slovenian)
    * tr (Turkish)
    * uk (Ukrainian)
    * vec (Venetian)
    * vo (Volapük)
    * war (Waray-Waray)
    
    This change is to go live on Monday, 7 October 2013.
    
    Change-Id: I59e2547002bac53582b9201e475242c1885820be

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -10571,35 +10571,8 @@ $wgConf->settings = array(
 // This is a temporary variable for roll-out, to be removed once complete.
 'wmgVisualEditorDisableForAnons' => array(
 	'default' => false,
-
-	'bgwiki' => true,
-	'cawiki' => true,
-	'cebwiki' => true,
-	'cswiki' => true,
-	'dawiki' => true,
 	'dewiki' => true, // bug 52232
-	'elwiki' => true,
 	'enwiki' => true,
-	'etwiki' => true,
-	'euwiki' => true,
-	'fiwiki' => true,
-	'glwiki' => true,
-	'hrwiki' => true,
-	'huwiki' => true,
-	'idwiki' => true,
-	'lvwiki' => true,
-	'mswiki' => true,
-	'napwiki' => true,
-	'nnwiki' => true,
-	'nowiki' => true,
-	'simplewiki' => true,
-	'skwiki' => true,
-	'slwiki' => true,
-	'trwiki' => true,
-	'ukwiki' => true,
-	'vecwiki' => true,
-	'vowiki' => true,
-	'warwiki' => true,
 ),
 
 'wmgVisualEditorShowBetaWelcome' => array(
