e4ff4ad1c90a626fa2001237495c3eff666a8cdf
Reedy <reedy@wikimedia.org>
2013-09-20 02:38:37 +0100

    Expand MWMultiversion test cases
    Change-Id: I3c8dd59d5ea7d12b7cfa0af497a8107ffa14dc6c

--- tests/multiversion/MWMultiVersionTest.php
+++ tests/multiversion/MWMultiVersionTest.php
@@ -6,8 +6,8 @@ class MWMultiVersionTests extends PHPUnit_Framework_TestCase {
 	/**
 	 * @dataProvider provideServerNameAndDocRoot
 	 */
-	function testRealmFilenames( $expectedDB, $serverName, $docRoot='', $msg='' ) {
-		$version = MWMultiversion::initializeForWiki($serverName, $docRoot );
+	function testRealmFilenames( $expectedDB, $serverName, $docRoot = '', $msg = '' ) {
+		$version = MWMultiversion::initializeForWiki( $serverName, $docRoot );
 
 		$this->assertEquals( $expectedDB, $version->getDatabase() );
 
@@ -19,7 +19,21 @@ class MWMultiVersionTests extends PHPUnit_Framework_TestCase {
 
 		return array(
 			// (expected DB, server name, [doc root[, message]]
-			array( 'enwiki', 'en.wikipedia.org', "$root/en" ),
+			array( 'enwiki', 'en.wikipedia.org', "$root/wikipedia.org" ),
+			array( 'enwiktionary', 'en.wiktionary.org', "$root/wiktionary.org" ),
+			array( 'enwikibooks', 'en.wikibooks.org', "$root/wikibooks.org" ),
+			array( 'enwikinews', 'en.wikinews.org', "$root/wikinews.org" ),
+			array( 'enwikiquote', 'en.wikiquote.org', "$root/wikiquote.org" ),
+			array( 'enwikisource', 'en.wikisource.org', "$root/wikisource.org" ),
+			array( 'enwikiversity', 'en.wikiversity.org', "$root/wikiversity.org" ),
+			array( 'enwikivoyage', 'en.wikivoyage.org', "$root/wikivoyage.org" ),
+
+			array( 'wikidatawiki', 'wikidata.wikimedia.org', "$root/wikidata" ),
+			array( 'specieswiki', 'species.wikimedia.org', "$root/species" ),
+			array( 'sourceswiki', 'wikisource.org', "$root/sources" ),
+			array( 'mediawikiwiki', 'www.mediawiki.org', "$root/mediawiki" ),
+
+			// labs stuffs
 			array( 'enwiki', 'en.wikipedia.beta.wmflabs.org', "$root/en" ),
 			array( 'wikidatawiki', 'wikidata.beta.wmflabs.org', "$root/wikidata" ),
 		);
