ebc9557a6cefda82dfadb4f244c1839b9608fc99
Coren <marc@uberbox.org>
2013-10-07 15:03:44 -0400

    Add templateeditor right, group, and restriction
    For en.wikipedia; this change adds the 'templateeditor' restriction
    level 'between' the 'autoconfirmed' and 'sysop' levels intended for
    appying to high-traffic templates that are currently protected.
    
    This commits creates the group 'templateeditor' that has a right
    with the same name, allowing members to edit the protected templates,
    and gives that right (as well as the ability to grant and revoke
    the group) to the 'sysop' group.
    
    Change-Id: I2c2bdf0934de46ae118d22ad0260a90d02d1375b

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -3918,6 +3918,7 @@ $wgConf->settings = array(
 	'default' => array( '', 'autoconfirmed', 'sysop' ), // semi-protection level on
 	'arwiki' => array( '', 'autoconfirmed', 'autoreview', 'sysop' ), // bug 52109
 	'ckbwiki' => array( '', 'autoconfirmed', 'autopatrol', 'sysop' ), // bug 52533
+	'enwiki' => array( '', 'autoconfirmed', 'templateeditor', 'sysop' ), // bug 55432
 	'plwiki' => array( '', 'autoconfirmed', 'editor', 'sysop' ), // bug 46990
 	'ptwiki' => array( '', 'autoconfirmed', 'autoreviewer', 'sysop' ), // bug 39652
 ),
@@ -6774,6 +6775,8 @@ $wgConf->settings = array(
 		'oversight' => array( 'browsearchive' => true, 'deletedhistory' => true, 'deletedtext' => true ), // bug 28465
 		'checkuser' => array( 'browsearchive' => true, 'deletedhistory' => true, 'deletedtext' => true ), // bug 28465
 		'bureaucrat' => array( 'move-subpages' => true, 'suppressredirect' => true, 'tboverride' => true, ),
+		'templateeditor' => array( 'templateeditor' => true ), // bug 55432
+		'sysop' => array( 'templateeditor' => true ), // bug 55432
 	),
 	'enwikibooks' => array(
 		// 'rollbacker' 	=> array( 'rollback' => true ),
@@ -7737,7 +7740,7 @@ $wgConf->settings = array(
 	),
 	'+enwiki' => array(
 		'bureaucrat' => array( 'accountcreator' ),
-		'sysop' => array( 'abusefilter', 'accountcreator', 'autoreviewer', 'confirmed', 'filemover', 'reviewer', 'rollbacker' ),
+		'sysop' => array( 'abusefilter', 'accountcreator', 'autoreviewer', 'confirmed', 'filemover', 'reviewer', 'rollbacker', 'templateeditor' ),
 	),
 	'+enwikibooks' => array(
 		'sysop' => array( 'transwiki', 'uploader' ),
@@ -8186,7 +8189,7 @@ $wgConf->settings = array(
 	),
 	'+enwiki' => array(
 		'bureaucrat' => array( 'ipblock-exempt', 'accountcreator', 'sysop' ),
-		'sysop' => array( 'rollbacker', 'accountcreator', 'abusefilter', 'autoreviewer', 'confirmed', 'reviewer', 'filemover' ),
+		'sysop' => array( 'rollbacker', 'accountcreator', 'abusefilter', 'autoreviewer', 'confirmed', 'reviewer', 'filemover', 'templateeditor' ),
 	),
 	'+enwikibooks' => array(
 		'sysop' => array( 'transwiki', 'uploader', ),
