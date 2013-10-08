166bd9580993122e612e7dcb9204291cd80296a1
Tomasz W. Kozlowski <tomasz@twkozlowski.net>
2013-10-08 23:21:56 +0200

    (bug 54223) Enable EducationProgram on the Czech Wikipedia
    This patch enables the EducationProgram extension on the Czech
    Wikipedia (cswiki) per community request; see bug for URL with
    on-wiki consensus for the change. The Czech translation for
    $wmgEducationProgramDYKCat was kindly provided by bug submitter.
    
    Bug: 54223
    Change-Id: I5b6d18fd3748ab6ebd07925ad4b373ffe7cb2ef0

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -12118,6 +12118,7 @@ $wgConf->settings = array(
 ),
 'wmgUseEducationProgram' => array(
 	'default' => false,
+	'cswiki' => true, // bug 54223
 	'dewikiversity' => true,
 	'enwiki' => true,
 	'enwikinews' => true, // bug 53984
@@ -12130,6 +12131,7 @@ $wgConf->settings = array(
 
 'wmgEducationProgramDYKCat' => array(
 	'default' => 'MyCourses Did You Know',
+	'cswiki' => 'Moje kurzy - Víte že?', // bug 54223
 	'ptwiki' => '!Você Sabia sobre cursos', // bug 52870
 ),
 
