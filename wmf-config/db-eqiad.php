9f05dbf38a10e3cbe8f724cd76cfbe19dc14600b
Ariel T. Glenn <ariel@wikimedia.org>
2013-10-11 14:57:31 +0300

    db1024 back in s7 pool warming up after upgrade/conversion to mariadb
    Change-Id: I8cea9928b6755d60fa9ce042825d278111c1dd03

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -132,7 +132,7 @@ $wgLBFactoryConf = array(
 	's7' => array(
 		'db1041' => 0,
 		#'db1007' => 100, # snapshot
-		# 'db1024' => 400, # upgrade/convert to mariadb
+		'db1024' => 100,
 		'db1028' => 400,
 		'db1039' => 400,
 	),
