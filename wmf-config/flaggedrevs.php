4591b4c8579ce6117b43d56f021f24cc994ea1f6
Reedy <reedy@wikimedia.org>
2013-10-11 20:54:32 +0000

    Fix typo in ptwiki flagged revs config
    Change-Id: I98178225c9996bf681aea794a0f10dcf50868207

--- wmf-config/flaggedrevs.php
+++ wmf-config/flaggedrevs.php
@@ -497,8 +497,8 @@ elseif ( $wgDBname == 'ptwiki' ) { // bug 54828
 	unset( $wgGroupPermissions['editor'], $wgGroupPermissions['reviewer'], $wgGroupPermissions['autoreview'] );
 	$wgAddGroups['sysop'] = array_diff( $wgAddGroups['sysop'], array( 'editor', 'reviewer' ) );
 	$wgRemoveGroups['sysop'] = array_diff( $wgRemoveGroups['sysop'], array( 'editor', 'reviewer' ) );
-	$wgAddGroups['bureaucrat'] = array_diff( $wgAddGroups['sysop'], array( 'reviewer' ) );
-	$wgRemoveGroups['bureaucrat'] = array_diff( $wgRemoveGroups['sysop'], array( 'reviewer' ) );
+	$wgAddGroups['sysop'] = array_diff( $wgAddGroups['sysop'], array( 'reviewer' ) );
+	$wgRemoveGroups['sysop'] = array_diff( $wgRemoveGroups['sysop'], array( 'reviewer' ) );
 }
 
 elseif ( $wgDBname == 'ptwikibooks' ) {
