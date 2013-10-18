31318d95db41c384109aae05904bd50a99141e33
Ariel T. Glenn <ariel@wikimedia.org>
2013-10-18 18:42:09 +0300

    db1019 (s3) warming up in pool after upgrade/conversion to mariadb
    Change-Id: If82fed26b5ffc9ec091e66b464e2ae44db2c7bef

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -102,10 +102,10 @@ $wgLBFactoryConf = array(
 		'db1018'    => 100, # snaphsot
 	),
 	/* s3 */ 'DEFAULT' => array(
-		#'db1019'    => 0,
 		'db1038'    => 0,
 		'db1003'    => 400,
 		'db1010'    => 400,
+		'db1019'    => 100,
 		# 'db1035'    => 100, # snapshot
 	),
 	's4' => array(
