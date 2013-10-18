4d14aa637b74aad8e3bc79e71e156aaeb72bb607
Sean Pringle <sean.pringle@gmail.com>
2013-10-18 12:32:01 +1000

    S3 master rotation done
    Change-Id: I36566a3b084abaae65f0c9c6bbf64b396a5e74a2

--- wmf-config/db-pmtpa.php
+++ wmf-config/db-pmtpa.php
@@ -339,7 +339,7 @@ $wgLBFactoryConf = array(
 #	'DEFAULT' => 'Brief Database Maintenance in progress, please try again in 3 minutes', #s3
 #	's1'	   => 'Brief Database Maintenance in progress, please try again in 3 minutes',
 #	's5'	   => 'Brief Database Maintenance in progress, please try again in 3 minutes',
-	's3'	   => 'Brief Database Maintenance in progress, please try again in 3 minutes',
+#	's3'	   => 'Brief Database Maintenance in progress, please try again in 3 minutes',
 ),
 
 );
