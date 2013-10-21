8d42c57c4e00f1075450d92436eb73c6cafe2be5
Reedy <reedy@wikimedia.org>
2013-10-21 11:25:23 +0100

    Add "touch.php" for $wgAppleTouchIcon...
    Change-Id: I6f83e6edfade9ed8669ed9c1cc5734dee18ebf59

---
+++ w/touch.php
@@ -0,0 +1,49 @@
+<?php
+define( "MEDIAWIKI", true );
+
+require( "/apache/common/w/MWVersion.php" );
+require( getMediaWiki( "includes/WebStart.php" ) );
+
+function faviconShowError( $html ) {
+	header( 'Content-Type: text/html; charset=utf-8' );
+	echo "<html><body>$html</body></html>\n";
+}
+
+function streamAppleTouch() {
+	global $wgAppleTouchIcon;
+	wfResetOutputBuffers();
+	if ( $wgAppleTouchIcon === false ) {
+		# That's not very helpful, that's where we are already
+		header( 'HTTP/1.1 404 Not Found' );
+		faviconShowError( "\$wgAppleTouchIcon is configured incorrectly, " .
+			"it must be set to something other than false \n" );
+		return;
+	}
+
+	$req = RequestContext::getMain()->getRequest();
+	if ( $req->getHeader( 'X-Favicon-Loop' ) !== false ) {
+		header( 'HTTP/1.1 500 Internal Server Error' );
+		faviconShowError( "Proxy forwarding loop detected" );
+		return;
+	}
+
+	$url = wfExpandUrl( $wgAppleTouchIcon, PROTO_INTERNAL );
+	$client = MWHttpRequest::factory( $url );
+	$client->setHeader( 'X-Favicon-Loop', '1' );
+
+	$status = $client->execute();
+	if ( !$status->isOK() ) {
+		header( 'HTTP/1.1 500 Internal Server Error' );
+		faviconShowError( htmlspecialchars( "Failed to fetch URL \"$url\"" ) );
+		return;
+	}
+
+	$content = $client->getContent();
+	header( 'Content-Length: ' . strlen( $content ) );
+	header( 'Content-Type: ' . $client->getResponseHeader( 'Content-Type' ) );
+	header( 'Cache-Control: public' );
+	header( 'Expires: ' . gmdate( 'r', time() + 86400 ) );
+	echo $content;
+}
+
+streamAppleTouch();
