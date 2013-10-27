7ae63042cc8a7ab531904214231a09fa82bd3bf8
eikes <leon.liesener@wikipedia.de>
2013-10-27 14:49:53 +0100

    Added import source for 'wikidata'
    'testwikidata' was added as an import source for 'wikidata' in this change.
    
    Bug: 56215
    Change-Id: Ic06fd060585806b47b8e28e4d635aa86030dbd6f

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -8897,8 +8897,8 @@ $wgConf->settings = array(
 	'viwikisource' => array( 'w', 'wikt', 'b', 'q' ),
 	'viwikiquote'  => array( 'w', 'wikt', 'b', 's' ),
 
-	'wikidata' => array( 'meta', 'commons', 'en', 'de', 'fr', 'es' ),
-	'+testwikidatawiki' => array( 'd' ),
+	'wikidata' => array( 'meta', 'commons', 'en', 'de', 'fr', 'es', 'testwikidata' ),
+	'testwikidatawiki' => array( 'd', 'meta', 'commons', 'en', 'de', 'fr', 'es' ),
 	'wikimania2012wiki' => array( 'en', 'meta', 'wm2011' ),
 	'wikimania2013wiki' => array( 'en', 'meta', 'wm2011', 'wm2012' ),
 	'wikimania2014wiki' => array( 'en', 'meta', 'wm2011', 'wm2012', 'wm2013' ),
