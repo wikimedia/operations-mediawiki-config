e09c6e62131f753ea36584d59e3f24a15ce67120
Aaron Schulz <aschulz@wikimedia.org>
2013-10-28 10:12:37 -0700

    Update tampa for 7786c233a30d3c8552c862ab841d7b9dfa6d67be (also fixed gzip conf)
    Change-Id: Ia02b9cec9fc2b08f1d7aa5cc45448f07f97c0b57

--- wmf-config/jobqueue-pmtpa.php
+++ wmf-config/jobqueue-pmtpa.php
@@ -1,12 +1,33 @@
 <?php
 # WARNING: This file is publically viewable on the web. Do not put private data here.
+// Note: on server failure, partition masters should be switched to the slave
 $wgJobTypeConf['default'] = array(
-	'class'       => 'JobQueueRedis',
-	'redisServer' => '10.64.32.76', # rdb1001 (master)
-	#'redisServer' => '10.64.32.77', # rdb1002 (slave)
-	'redisConfig' => array(
-		'connectTimeout' => 1,
-		'password' => $wmgRedisPassword,
+	'class'               => 'JobQueueFederated',
+	'configByPartition'   => array(
+		'rdb1' => array(
+			'class'       => 'JobQueueRedis',
+			'redisServer' => '10.64.32.76', # rdb1001 (master)
+			#'redisServer' => '10.64.32.77', # rdb1002 (slave)
+			'redisConfig' => array(
+				'connectTimeout' => 2,
+				'password' => $wmgRedisPassword,
+				'compression' => 'gzip'
+			)
+		),
+		'rdb2' => array(
+			'class'       => 'JobQueueRedis',
+			'redisServer' => '10.64.0.201', # rdb1003 (master)
+			#'redisServer' => '10.64.16.183', # rdb1004 (slave)
+			'redisConfig' => array(
+				'connectTimeout' => 2,
+				'password' => $wmgRedisPassword,
+				'compression' => 'gzip'
+			)
+		),
+	),
+	'sectionsByWiki'      => array(), // default
+	'partitionsBySection' => array(
+		'default' => array( 'rdb1' => 50, 'rdb2' => 50 ),
 	)
 );
 // Note: on server failure, this should be changed to any other redis server
