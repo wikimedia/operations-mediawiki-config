4cdf2f44a11ab8b0cd0bea26cdbb36f83052587d
James D. Forrester <jforrester@wikimedia.org>
2013-11-05 11:09:17 -0800

    Enable VisualEditor on board, collab, office and wikimaniateam wikis
    Also add the necesary wmgVisualEditorParsoidForwardCookies setting.
    
    Change-Id: Ib00795c6a6168e0d328e13feafeed1a7db1848f3

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -1952,7 +1952,9 @@ if ( $wmgUseVisualEditor ) {
 			$wgVisualEditorTabMessages['createappendix'] =
 			$wgVisualEditorTabMessages['editsectionappendix'] = 'visualeditor-beta-appendix';
 	}
-
+	if ( $wmgVisualEditorParsoidForwardCookies ) {
+		$wgVisualEditorParsoidForwardCookies = true;
+	}
 
 	// Also include the Parsoid extension when VE is enabled
 	require_once( "$IP/extensions/Parsoid/php/Parsoid.php" );
