fea9c35b3fdb2e48b28e4253e6eceb282cc7a2e4
Max Semenik <maxsem.wiki@gmail.com>
2013-11-07 22:56:20 +0400

    Extend OpenSearchXml with images from PageImages
    It provides much better results
    
    Dependency: I9f8dad09945e69bf45ec785a6383c2449551fbac must be deployed in all
    branches.
    
    Change-Id: I22f5028a29fb8e4a0cd187dc71bae3c53076a1d9

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -2666,6 +2666,7 @@ if ( $wmgUseTemplateSandbox ) {
 
 if ( $wmgUsePageImages ) {
 	require_once( "$IP/extensions/PageImages/PageImages.php" );
+	$wgPageImagesExpandOpenSearchXml = $wmgPageImagesExpandOpenSearchXml;
 }
 
 if ( $wmgUseSearchExtraNS ) {
