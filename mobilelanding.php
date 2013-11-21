d5331fadb40c27de2093fa335c65a75bb4bc0e3e
Yuri Astrakhan <yurik@wikimedia.org>
2013-11-21 12:56:20 -0500

    Mobile m. and zero. landing page redirect handling by ZERO
    This script is similar to extract2.php - it handles redirection
    of m.wikipedia.org and zero.wikipedia.org to the specific language
    version depending on the zero configuration.
    
    For non-zero customers, it keeps current logic of redirecting to EN
    
    Soon zero will be able to redirect to sister projects as well,
    but it will be based internally on the HOST header.
    
    Change-Id: I58b03cf6751b2791c2daddf7800889d13ab421f9

---
+++ mobilelanding.php
@@ -0,0 +1,25 @@
+<?php
+putenv( "MW_LANG=en" ); // notify MWMultiVersion
+
+include '/apache/common/w/MWVersion.php';
+include getMediaWiki( 'includes/WebStart.php' );
+
+$code = '302';
+$state = Extensions\ZeroRatedMobileAccess\PageRenderingHooks::getState();
+$redirect = $state->getLandingRedirect();
+$redirect = filter_var( $redirect, FILTER_VALIDATE_URL );
+if ( $redirect === false ) {
+	$code = '500';
+}
+$message = HttpStatus::getMessage( $code );
+
+//$lastmod = gmdate( 'D, j M Y H:i:s', wfTimestamp( TS_UNIX, $wgArticle->getTouched() ) ) . ' GMT';
+//header( "Last-modified: $lastmod" );
+
+header( 'Cache-Control: s-maxage=3600, must-revalidate, max-age=0' );
+header( "HTTP/1.1 $code $message" );
+if ( $redirect !== false ) {
+	header( 'Location: ' . $redirect );
+}
+header( 'Vary: X-Subdomain,X-CS,Cookie' );
+header( 'Content-Type: text/html; charset=utf-8' );
