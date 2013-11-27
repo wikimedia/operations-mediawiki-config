7a77ea1873a09146cbf093c01b8556ffd2a55266
John F. Lewis <johnflewis93@gmail.com>
2013-11-27 20:55:17 +0000

    Enable AbuseFilter block option on Wikidata
    Default blocking duration is set to indefinite.
    
    Bug: 57681
    Change-Id: Ib34f619e7f5d8514b086868c122bdee84b547678

--- wmf-config/abusefilter.php
+++ wmf-config/abusefilter.php
@@ -312,6 +312,8 @@ switch ( $wgDBname ) {
 	case 'wikidatawiki':
 		$wgAbuseFilterNotifications = "udp"; // bug 45083
 		$wgAbuseFilterNotificationsPrivate = true; // bug 45083
+		$wgAbuseFilterAvailableActions[] = 'block'; // bug 57681
+		$wgAbuseFilterBlockDuration = '3 months'; // bug 57681
 		break;
 	case 'zh_yuewiki':
 		$wgGroupPermissions['*']['abusefilter-log-detail'] = true;
