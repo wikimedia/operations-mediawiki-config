c2f455beb764dd77ed8f62534e3ab9a6af425ba0
Aaron Schulz <aschulz@wikimedia.org>
2013-12-05 14:49:43 -0800

    Tweaked $wgJobQueueAggregator to use redis job servers and have failover
    Change-Id: I6bd3936c7ee72c63dcbca00c52440ebddcd4563c

--- wmf-config/jobqueue-pmtpa.php
+++ wmf-config/jobqueue-pmtpa.php
@@ -33,7 +33,10 @@ $wgJobTypeConf['default'] = array(
 // Note: on server failure, this should be changed to any other redis server
 $wgJobQueueAggregator = array(
 	'class'       => 'JobQueueAggregatorRedis',
-	'redisServer' => '10.64.0.180', # mc1001
+	'redisServers' => array( // all after the first are fallbacks
+		'10.64.32.76', # rdb1001
+		'10.64.0.201', # rdb1003
+	),
 	#'redisServer' => '10.0.12.1', # mc1
 	'redisConfig' => array(
 		'connectTimeout' => 1,
