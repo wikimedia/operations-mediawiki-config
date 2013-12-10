4722323e64c3f8849db38f5b2d9366b8bf8eb02a
S Page <spage@wikimedia.org>
2013-12-10 03:27:13 -0800

    Enable Flow discussions on a few wikis' test pages
    Only mediawiki.org, testwiki, and test2wiki, and only on two or three
    pages on each: 'Talk:Flow QA', 'Talk:Sandbox' (and 'Talk:Flow' on
    mw.org).
    
    Configure Flow to store in HTML and to use a cross-wiki separate DB on
    extension1 cluster and $wgDefaultExternalStore for most wikis.
    
    Adding Flow to extension-list is a separate change 100551
    
    Bug: 56506
    Change-Id: I1fba27a98059686056e82e15ab5f749a6eea14b1

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -2331,6 +2331,12 @@ if ( $wmgUseFlow ) {
 	require_once( "$IP/extensions/Flow/Flow.php" );
 	$wgFlowEditorList = $wmgFlowEditorList;
 	$wgFlowOccupyPages = $wmgFlowOccupyPages;
+	// Requires that Parsoid is available for all wikis using Flow.
+	$wgFlowContentFormat = 'html';
+
+	$wgFlowDefaultWikiDb = $wmgFlowDefaultWikiDb;
+	$wgFlowCluster = $wmgFlowCluster;
+	$wgFlowExternalStore = $wgDefaultExternalStore;
 }
 
 if ( $wmgUseDisambiguator ) {
