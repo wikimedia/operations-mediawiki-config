b704863e91a2cf7f86a8bff2acccb24a2613a721
dan-nl <d_entous@yahoo.com>
2013-12-12 21:19:13 +0100

    Production configuration for GWToolset
    Configure GWToolset for production use on commons.
    
    Bug: 56181
    Change-Id: I2d1c666e1d3f4689086cf40d564f238008671e02

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -7592,7 +7592,7 @@ $wgConf->settings = array(
 			'patrol' => true, // bug 56203
 			'autopatrol' => true, // bug 56203
 		),
-		'rollbacker' => array( 
+		'rollbacker' => array(
 			'rollback' => true, // bug 45165
 			'autopatrol' => true, // bug 55495
 			'patrol' => true, // bug 55495
@@ -7603,7 +7603,7 @@ $wgConf->settings = array(
 			'abusefilter-log-detail' => true, // bug 55495
 			'suppressredirect' => true, // bug 56203
 		),
-		'propertycreator' => array( 
+		'propertycreator' => array(
 			'property-create' => true, // bug 46953
 			'autopatrol' => true, // bug 55495
 			'patrol' => true, // bug 55495
@@ -9351,6 +9351,11 @@ $wgConf->settings = array(
 
 'wmgUseFloatedToc' => array(
 	'default' => true,
+),
+
+'wmgUseGWToolset' => array(
+	'default' => false,
+	'commonswiki' => true,
 ),
 
 'wmgUseSpecialNuke' => array(
