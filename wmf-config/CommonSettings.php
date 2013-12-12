797b3517b6e8facb743052803a17410d270a91a9
dan-nl <d_entous@yahoo.com>
2013-12-12 21:19:13 +0100

    gwtoolset-config
    preparing config for production.
    
    Change-Id: I2d1c666e1d3f4689086cf40d564f238008671e02

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -1850,6 +1850,16 @@ if ( $wmgUseCommonsMetadata ) {
 	require_once( "$IP/extensions/CommonsMetadata/CommonsMetadata.php" );
 }
 
+if ( $wmgUseGWToolset ) {
+	require_once( "$IP/extensions/GWToolset/GWToolset.php" );
+	$wgGWTFileBackend = 'local-multiwrite';
+	$wgGWTFBMaxAge = '1 week';
+	$wgJobTypesExcludedFromDefaultQueue[] = 'gwtoolsetUploadMetadataJob';
+	$wgJobTypesExcludedFromDefaultQueue[] = 'gwtoolsetUploadMediafileJob';
+	$wgJobTypesExcludedFromDefaultQueue[] = 'gwtoolsetGWTFileBackendCleanupJob';
+	$wgJobTypeConf['gwtoolsetUploadMetadataJob'] = array( 'checkDelay' => true ) + $wgJobTypeConf['default'];
+}
+
 if ( $wmgUseMultimediaViewer ) {
 	require_once( "$IP/extensions/MultimediaViewer/MultimediaViewer.php" );
 }
