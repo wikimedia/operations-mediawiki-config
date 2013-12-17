b5f12067a2fc5b64577f9390084b1076e9a81179
Matthew Flaschen <mflaschen@wikimedia.org>
2013-12-17 16:49:57 -0500

    Add VisualEditor to the draft namespace
    Change-Id: I76343a844c1db70f9c21b8c509ce15ddd0001b1e

--- wmf-config/InitialiseSettings-labs.php
+++ wmf-config/InitialiseSettings-labs.php
@@ -299,10 +299,6 @@ return array(
 		'default' => true,
 	),
 
-	'wmgVisualEditorNamespaces' => array(
-		'+enwiki' => array( 100 /* Portal */, 108 /* Book */, 110 /* Draft */ ),
-	),
-
 	'wmgUseCampaigns' => array(
 		'default' => true,
 	),
--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -10618,7 +10618,7 @@ $wgConf->settings = array(
 	'default' => array( NS_USER, NS_FILE, NS_HELP, NS_CATEGORY ),
 	'+mediawikiwiki' => array( NS_PROJECT, 100 /* Manual */, 102 /* Extension */, 104 /* API */ ), // Bug 48430
 	'+cawiki' => array( 100 /* Portal */, 102 /* Viquiprojecte */ ), // Bug 56000
-	'+enwiki' => array( 100 /* Portal */, 108 /* Book */ ), // Bug 56001
+	'+enwiki' => array( 100 /* Portal */, 108 /* Book */, 118 /* Draft */ ),
 	'svwiktionary' => array( NS_USER ), // Bug 57356
 ),
 
