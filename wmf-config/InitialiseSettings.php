4c7429c959dcbfaf448124c146f0c8903783907d
Tomasz W. Kozlowski <tomasz@twkozlowski.net>
2013-12-25 13:49:46 +0100

    Add massmessage-sender user group to enwiki
    This patch adds the 'massmessage-sender' user group
    to the English Wikipedia, with the extension's only
    user right, ie. 'massmessage'; sysops will be able
    to add and remove users from this group.
    
    See bug for URL with on-wiki consensus for the
    creation of this new user group.
    
    Bug: 58962
    Change-Id: I20acaa34171f1040052a5330caab535aa3f9aaa8

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -6786,6 +6786,9 @@ $wgConf->settings = array(
 		'bureaucrat' => array( 'move-subpages' => true, 'suppressredirect' => true, 'tboverride' => true, ),
 		'templateeditor' => array( 'templateeditor' => true, 'tboverride' => true, ), // bug 55432
 		'sysop' => array( 'templateeditor' => true ), // bug 55432
+		'massmessage-sender' => array( // bug 58962
+			'massmessage' => true,
+		),
 	),
 	'enwikibooks' => array(
 		// 'rollbacker' 	=> array( 'rollback' => true ),
@@ -7785,7 +7788,7 @@ $wgConf->settings = array(
 	),
 	'+enwiki' => array(
 		'bureaucrat' => array( 'accountcreator' ),
-		'sysop' => array( 'abusefilter', 'accountcreator', 'autoreviewer', 'confirmed', 'filemover', 'reviewer', 'rollbacker', 'templateeditor' ),
+		'sysop' => array( 'abusefilter', 'accountcreator', 'autoreviewer', 'confirmed', 'filemover', 'reviewer', 'rollbacker', 'templateeditor', 'massmessage-sender' ),
 	),
 	'+enwikibooks' => array(
 		'sysop' => array( 'transwiki', 'uploader' ),
@@ -8233,7 +8236,7 @@ $wgConf->settings = array(
 	),
 	'+enwiki' => array(
 		'bureaucrat' => array( 'ipblock-exempt', 'accountcreator', 'sysop' ),
-		'sysop' => array( 'rollbacker', 'accountcreator', 'abusefilter', 'autoreviewer', 'confirmed', 'reviewer', 'filemover', 'templateeditor' ),
+		'sysop' => array( 'rollbacker', 'accountcreator', 'abusefilter', 'autoreviewer', 'confirmed', 'reviewer', 'filemover', 'templateeditor', 'massmessage-sender' ),
 	),
 	'+enwikibooks' => array(
 		'sysop' => array( 'transwiki', 'uploader', ),
