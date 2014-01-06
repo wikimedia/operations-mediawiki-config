588f7247b261169c660620aa2d0e3c1892857a05
Antoine Musso <hashar@free.fr>
2014-01-06 17:43:13 +0100

    sanity test for refreshWikiversionsCDB
    Fetch from Gerrit the wmf branch, write directories and then run
    refreshWikiversionsCDB.  Should be done in a different path really.
    
    bug: 41861
    Change-Id: I8796f66115c53b3544664c7564cc165938eeedae

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,10 @@
 <?php
 
-// Load the shared utilities classes from here!
+// Environnement variable used to act differently for testing purposes
+putenv( 'MW_CONFIG_TESTING=' . dirname( dirname( __FILE__ ) ) );
 
+require( dirname( __FILE__ ) . '/../multiversion/defines.php' );
+
+// Load the shared utilities classes from here!
 require_once( dirname( __FILE__ ) . "/DBList.php" );
 require_once( dirname( __FILE__ ) . "/Provide.php" );
