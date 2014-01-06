63fe854323088dd69ef13f420103501349326441
Antoine Musso <hashar@free.fr>
2014-01-06 17:43:13 +0100

    sanity test for refreshWikiversionsCDB
    Fetch from Gerrit the wmf branch, write directories and then run
    refreshWikiversionsCDB.  Should be done in a different path really.
    
    bug: 41861
    Change-Id: I8796f66115c53b3544664c7564cc165938eeedae

--- multiversion/defines.php
+++ multiversion/defines.php
@@ -2,11 +2,24 @@
 /**
  * Define installation specific MWMultiVersion values like paths
  */
-define( 'MULTIVER_COMMON_HOME', '/a/common' );
-define( 'MULTIVER_COMMON_APACHE', '/usr/local/apache/common-local' );
 
-define( 'MULTIVER_CDB_DIR_HOME', '/a/common' );
-define( 'MULTIVER_CDB_DIR_APACHE', '/usr/local/apache/common-local' );
+if ( getenv( 'MULTIVER_TEST_PATH' ) !== false ) {
+	# Hack for Wikimedia Jenkins jobs
+	define( 'MULTIVER_COMMON_HOME', getenv( 'MULTIVER_TEST_PATH' ) );
+	define( 'MULTIVER_COMMON_APACHE', MULTIVER_COMMON_HOME );
 
-define( 'MULTIVER_404SCRIPT_PATH_HOME', '/a/common/wmf-config/missing.php' );
-define( 'MULTIVER_404SCRIPT_PATH_APACHE', '/usr/local/apache/common-local/wmf-config/missing.php' );
+	define( 'MULTIVER_CDB_DIR_HOME', getenv( 'MULTIVER_TEST_PATH' ) );
+	define( 'MULTIVER_CDB_DIR_APACHE', MULTIVER_CDB_DIR_HOME );
+
+	define( 'MULTIVER_404SCRIPT_PATH_HOME', getenv( 'MULTIVER_TEST_PATH' ) . '/wmf-config/missing.php' );
+	define( 'MULTIVER_404SCRIPT_PATH_APACHE', MULTIVER_404SCRIPT_PATH_HOME );
+} else {
+	define( 'MULTIVER_COMMON_HOME', '/a/common' );
+	define( 'MULTIVER_COMMON_APACHE', '/usr/local/apache/common-local' );
+
+	define( 'MULTIVER_CDB_DIR_HOME', '/a/common' );
+	define( 'MULTIVER_CDB_DIR_APACHE', '/usr/local/apache/common-local' );
+
+	define( 'MULTIVER_404SCRIPT_PATH_HOME', '/a/common/wmf-config/missing.php' );
+	define( 'MULTIVER_404SCRIPT_PATH_APACHE', '/usr/local/apache/common-local/wmf-config/missing.php' );
+}
