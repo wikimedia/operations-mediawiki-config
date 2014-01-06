740c9f39f526562fdac8e7676020ff275019a915
Antoine Musso <hashar@free.fr>
2014-01-06 17:43:13 +0100

    sanity test for refreshWikiversionsCDB
    Fetch from Gerrit the wmf branch, write directories and then run
    refreshWikiversionsCDB.  Should be done in a different path really.
    
    bug: 41861
    Change-Id: I8796f66115c53b3544664c7564cc165938eeedae

--- multiversion/defines.php
+++ multiversion/defines.php
@@ -2,11 +2,27 @@
 /**
  * Define installation specific MWMultiVersion values like paths
  */
-define( 'MULTIVER_COMMON_HOME', '/a/common' );
-define( 'MULTIVER_COMMON_APACHE', '/usr/local/apache/common-local' );
 
-define( 'MULTIVER_CDB_DIR_HOME', '/a/common' );
-define( 'MULTIVER_CDB_DIR_APACHE', '/usr/local/apache/common-local' );
+if ( getenv( 'MW_CONFIG_TESTING' ) ) {
+	# invoked via tests/bootstrap.php, act differently
 
-define( 'MULTIVER_404SCRIPT_PATH_HOME', '/a/common/wmf-config/missing.php' );
-define( 'MULTIVER_404SCRIPT_PATH_APACHE', '/usr/local/apache/common-local/wmf-config/missing.php' );
+	define( 'MW_CONFIG_TESTING', getenv( 'MW_CONFIG_TESTING' ) );
+
+	define( 'MULTIVER_COMMON_HOME', MW_CONFIG_TESTING );
+	define( 'MULTIVER_COMMON_APACHE', MW_CONFIG_TESTING );
+
+	define( 'MULTIVER_CDB_DIR_HOME', MW_CONFIG_TESTING );
+	define( 'MULTIVER_CDB_DIR_APACHE', MW_CONFIG_TESTING );
+
+	define( 'MULTIVER_404SCRIPT_PATH_HOME', MW_CONFIG_TESTING . '/wmf-config/missing.php' );
+	define( 'MULTIVER_404SCRIPT_PATH_APACHE', MW_CONFIG_TESTING . '/wmf-config/missing.php' );
+} else {
+	define( 'MULTIVER_COMMON_HOME', '/a/common' );
+	define( 'MULTIVER_COMMON_APACHE', '/usr/local/apache/common-local' );
+
+	define( 'MULTIVER_CDB_DIR_HOME', '/a/common' );
+	define( 'MULTIVER_CDB_DIR_APACHE', '/usr/local/apache/common-local' );
+
+	define( 'MULTIVER_404SCRIPT_PATH_HOME', '/a/common/wmf-config/missing.php' );
+	define( 'MULTIVER_404SCRIPT_PATH_APACHE', '/usr/local/apache/common-local/wmf-config/missing.php' );
+}
