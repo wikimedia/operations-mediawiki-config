44e6b08d49f8d770542b1fa873604cf52181a0d3
Tomasz W. Kozlowski <tomasz@twkozlowski.net>
2014-01-15 17:41:34 +0100

    Let bureaucrats add users to accountcreator on elwiki
    This patch enables users in the 'bureaucrat' user group
    on elwiki (Greek Wikipedia) to add and remove other
    users from the 'accountcreator' user group.
    
    See bug for URL with on-wiki consensus for this change.
    
    Bug: 59997
    Change-Id: Ic2222a714ceb332035695f8f2f57b64f7df24d98

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -7788,6 +7788,9 @@ $wgConf->settings = array(
 		'sysop' => array( 'inactive', 'flood' ),
 		'bureaucrat' => array( 'import', 'transwiki', 'inactive' ),
 	),
+	'+elwiki' => array(
+		'bureaucrat' => array( 'accountcreator' ), // bug 59997
+	),
 	'+elwiktionary' => array(
 		'bureaucrat' => array( 'interface_editors' ),
 		'sysop' => array( 'autopatrolled' ),
@@ -8235,6 +8238,9 @@ $wgConf->settings = array(
 	'+donatewiki' => array(
 		'sysop' => array( 'inactive', 'confirmed', 'flood' ),
 		'bureaucrat' => array( 'sysop', 'bureaucrat', 'import', 'transwiki', 'inactive', 'confirmed' ),
+	),
+	'+elwiki' => array(
+		'bureaucrat' => array( 'accountcreator' ), // bug 59997
 	),
 	'+elwiktionary' => array(
 		'bureaucrat' => array( 'interface_editors' ),
