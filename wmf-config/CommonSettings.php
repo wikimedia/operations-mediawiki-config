411d4ebcbb3e188b10f419b55c136f81873a48cf
Gergő Tisza <tgr.huwiki@gmail.com>
2014-01-17 15:20:50 -0800

    Make UploadWizard respect the Flickr blacklist on Commons
    Related UW changeset:  I4895234 (and parents)
    
    Bug: 42307
    Change-Id: Ia1d586b55d9d4792f04892c3915f30db2d8976b9

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -1797,6 +1797,7 @@ if ( $wmgUseUploadWizard ) {
 		$wgUploadWizardConfig['feedbackPage'] = 'Commons:Upload_Wizard_feedback'; # Set by neilk, 2011-11-01, per erik
 		$wgUploadWizardConfig["missingCategoriesWikiText"] = "{{subst:unc}}";
 		$wgUploadWizardConfig['blacklistIssuesPage'] = 'Commons:Upload_Wizard_blacklist_issues'; # Set by neilk, 2011-11-01, per erik
+		$wgUploadWizardConfig['flickrBlacklistPage'] = 'User:FlickreviewR/bad-authors';
 	} elseif ( $wgDBname == 'test2wiki' ) {
 		$wgUploadWizardConfig['feedbackPage'] = 'Wikipedia:Upload_Wizard_feedback'; # Set by neilk, 2011-11-01, per erik
 		$wgUploadWizardConfig["missingCategoriesWikiText"] = "{{subst:unc}}";
