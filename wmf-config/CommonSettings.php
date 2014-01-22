e686c9cf13f01c39c6b018a72b880baaa6666c3f
aude <aude.wiki@gmail.com>
2014-01-21 21:28:26 -0500

    Maintain extension-list-wikidata with Wikidata build (beta only now)
    We are moving towards using a "build" of Wikibase + dependencies.
    
    (submodules temporarily now on beta and we will directly commit
    all our libraries here in the future for deployment)
    
    See https://git.wikimedia.org/tree/mediawiki%2Fextensions%2FWikidata
    
    In maintaining our extension list here, we can keep things in sync
    and not break beta when something gets moved.
    
    Change-Id: I7d61433ab7e97efc4ad9dfa90de43c5ca9f5771e

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -2493,7 +2493,7 @@ if ( $wmgUseUniversalLanguageSelector ) {
 
 // @note getRealmSpecificFilename only works with filenames with .suffix
 if ( $wmfRealm === 'labs' ) {
-	$wgExtensionEntryPointListFiles[] = "$wmfConfigDir/extension-list-wikidata-labs";
+	$wgExtensionEntryPointListFiles[] = "$IP/extensions/Wikidata/extension-list-wikidata";
 } else {
 	$wgExtensionEntryPointListFiles[] = "$wmfConfigDir/extension-list-wikidata";
 }
