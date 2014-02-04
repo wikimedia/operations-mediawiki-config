0f5be83c610f23f184198a150e37404ac6278a98
Sean Pringle <sean.pringle@gmail.com>
2014-02-04 11:38:30 +1000

    repool db1059 as slave, warm up
    Change-Id: Ibc3e5ec7d6bbadfbf177ec5b4d49dd59a8faec17

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -115,6 +115,7 @@ $wgLBFactoryConf = array(
 		'db1004'   => 0, # watchlist, recentchangeslinked, contributions, logpager
 		'db1011'   => 400,
 		'db1020'   => 400,
+		'db1059'   => 50, # warm up
 	),
 	's5' => array(
 		'db1058'   => 0,
