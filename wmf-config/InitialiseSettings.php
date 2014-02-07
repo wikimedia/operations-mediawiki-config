06be18ae9c10698f6714508348f39d08e8d391ed
James D. Forrester <jforrester@wikimedia.org>
2014-02-07 14:24:24 -0800

    Enable wgTemplateDataUseGUI on MediaWiki.org
    Change-Id: Ic31958f53cbef470a7d31da1ad420e3e81b223ea

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -10663,6 +10663,7 @@ $wgConf->settings = array(
 
 'wmgTemplateDataUseGUI' => array(
 	'default' => false,
+	'mediawikiwiki' => true,
 ),
 
 // Bug 50000 - Disable for logged-out users if wmgVisualEditorDefault is true
