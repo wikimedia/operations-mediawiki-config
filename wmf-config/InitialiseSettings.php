06ca000fa691deb0e57923098d49b002cb6540d1
Tomasz W. Kozlowski <tomasz@twkozlowski.net>
2014-02-13 18:16:11 +0100

    Set wmgBabelCategoryNames for Esperanto Wiktionary
    Doing just that; also setting $wmgBabelMainCategory
    per community request.
    
    See bug for URL with on-wiki consensus.
    
    Bug: 61116
    Change-Id: I0eeeaed6f538a1430dee2ebae987838ea6f7bd1a

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -11418,6 +11418,15 @@ $wgConf->settings = array(
 		'5' => false,
 		'N' => 'User %code%-N',
 	),
+	'eowiktionary' => array( // bug 61116
+		'0' => 'Vikivortaristo %code%-0',
+		'1' => 'Vikivortaristo %code%-1',
+		'2' => 'Vikivortaristo %code%-2',
+		'3' => 'Vikivortaristo %code%-3',
+		'4' => 'Vikivortaristo %code%-4',
+		'5' => false,
+		'N' => 'Vikivortaristo %code%-D',
+	),
 	'eswiki' => array(
 		'0' => false,
 		'1' => 'Wikipedia:Wikipedistas con nivel básico de %wikiname%',
@@ -11892,6 +11901,7 @@ $wgConf->settings = array(
 	'enwikisource' => 'User %code%',
 	'enwikinews' => 'User %code%',
 	'enwikiversity' => 'User %code%',
+	'eowiktionary' => 'Vikivortaristo %code%', // bug 61116
 	'eswiki' => 'Wikipedia:Wikipedistas con conocimientos de %wikiname%',
 	'eswikinews' => 'Usuario %code%',
 	'fiwiki' => 'User %code%',
