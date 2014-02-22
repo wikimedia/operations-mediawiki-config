a83336c9e0a46c67452ee5fa5ba4ffaf7198fd43
Ricordisamoa <ricordisamoa@live.it>
2014-02-22 17:22:33 +0100

    enable autopatrolled group on itwikiquote per community request: https://it.wikiquote.org/w/index.php?oldid=623905#Votazione (just as it is on itwiki)
    Change-Id: I62b32888b1b5c7d4ad58a62488513ef384e2640e

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -7009,6 +7009,7 @@ $wgConf->settings = array(
 	'itwikinews' => array( 'autoconfirmed' => array( 'patrol' => true ) ),
 	'itwikiquote' => array(
 		'autoconfirmed' => array( 'patrol' => true ),
+		'autopatrolled' => array( 'autopatrol' => true ),
 		'sysop' => array( 'autopatrol' => true ),
 	),
 	'itwikivoyage' => array(
@@ -7918,6 +7919,9 @@ $wgConf->settings = array(
 	'+itwikibooks' => array(
 		'sysop' => array( 'autopatrolled', 'patroller' ),
 	),
+	'+itwikiquote' => array(
+		'sysop' => array( 'autopatrolled' ),
+	),
 	'+itwikivoyage' => array(
 		'sysop' => array( 'autopatrolled', 'patroller' ),          // Bug 43327 and 45638
 	),
@@ -8403,6 +8407,9 @@ $wgConf->settings = array(
 	),
 	'+itwikibooks' => array(
 		'sysop' => array( 'autopatrolled', 'patroller', 'flooder' ), // Flooder added per bug 39569
+	),
+	'+itwikiquote' => array(
+		'bureaucrat' => array( 'autopatrolled' ),
 	),
 	'+itwikisource' => array(
 		'bureaucrat' => array( 'flood' ), // Bug 36600
