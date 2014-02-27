ae55a71b437ece6c4107c34a5e189757f44856a6
Federico Leva <federicoleva@tiscali.it>
2014-02-27 10:28:19 +0100

    Use $wgTranslatePageTranslationULS
    Use case: see Quim's https://bugzilla.wikimedia.org/show_bug.cgi?id=61695#c5
    
    This will help in the case when all the following is true:
    * the user enters a page with a direct link without (correct) language specified;
    * the page is translatable and ULS is prominently available ("personal");
    * the automatic language detection fails (e.g. the user doesn't have a properly
      configured accept-language so MediaWiki can't redirect to the proper version
      of the main page as defined in MediaWiki:Mainpage translations;
    * the user is logged in (pending bug 56464);
    * the user manually selects the preferred interface language via ULS;
    * all the following links use Special:MyLanguage (see [[mw:MyLanguage]]) to keep
      the content language constant, i.e. equal to interface language.
    
    It's a edge case but worth addressing with this minor configuration improvemment.
    The true difference, of course, would be bug 56464; until then, ugly selectors of
    all sorts will still be needed everywhere, including mediawiki.org's main page.
    
    Change-Id: If405735881113198cd713d81d8c5dafc782df264

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -2447,6 +2447,9 @@ if ( $wmgUseUniversalLanguageSelector ) {
 	if ( $wmgUseCodeEditorForCore || $wmgUseScribunto || $wmgUseZeroNamespace ) {
 		$wgULSNoImeSelectors[] = '.ace_editor textarea';
 	}
+	if ( $wmgUseTranslate && $wmgULSPosition === 'personal' ) {
+		$wgTranslatePageTranslationULS = true;
+	}
 
 	// Remove after ULS is re-deployed
 	if ( $wmgUniversalLanguageSelectorDefault ) {
