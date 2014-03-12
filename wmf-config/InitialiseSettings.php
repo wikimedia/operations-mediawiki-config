983f72097b9ba8a1b8b4a0e7cffea3035df4c493
Denny Vrandecic <vrandecic@google.com>
2014-03-11 17:04:29 -0700

    Set up configuration for App indexing
    (BUG 60537) Set up app indexing
    
    Change-Id: Ia55be2c4c6f62168440610ffbcb7607e8164c547

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -11168,6 +11168,10 @@ $wgConf->settings = array(
 'wmgMFEnableSiteNotice' => array(
 	'default' => true,
 ),
+'wmgMFAppPackageId' => array(
+	'default' => false,
+	'wiki' => 'org.wikipedia',
+),
 
 # trademark status
 'wmgMFTrademarkSitename' => array(
