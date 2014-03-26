fe38a3aef0af6889c5cd1e0d320d5099bb842666
Antoine Musso <hashar@free.fr>
2014-03-26 15:14:31 +0100

    beta: complete redis config for eqiad
    Change-Id: I966a52dcd61a02dd403879cd31f7333126ab07b2

--- wmf-config/jobqueue-labs.php
+++ wmf-config/jobqueue-labs.php
@@ -3,20 +3,21 @@
 
 if( $wmfRealm == 'labs' ) {  # safe guard
 
+if( $wmfDatacenter === 'pmtpa' ) {
+	$jobRedisServer = '10.4.0.83';  # deployment-redisdb
+} else {  # eqiad
+	$jobRedisServer = '10.68.16.146';  # deployment-redis01
+}
+
 $wgJobTypeConf['default'] = array(
 	'class'       => 'JobQueueRedis',
-	'redisServer' => '10.4.0.83', # deployment-redisdb
+	'redisServer' => $jobRedisServer,
 	'redisConfig' => array(
 		'connectTimeout' => 1,
 		'password' => $wmgRedisPassword,
 	)
 );
 
-if( $wmfDatacenter === 'pmtpa' ) {
-	$jobRedisServer = '10.4.0.83';  # deployment-redisdb
-} else {  # eqiad
-	$jobRedisServer = '10.68.16.146';  # deployment-redis01
-}
 $wgJobQueueAggregator = array(
 	'class'       => 'JobQueueAggregatorRedis',
 	'redisServer' => $jobRedisServer,
