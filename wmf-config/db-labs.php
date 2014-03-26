6c67e12ef9acf12895e9c6be950ff12081a43280
Antoine Musso <hashar@free.fr>
2014-03-26 14:30:27 +0100

    beta: configure db1 on eqiad
    Sean has created the db1 instance in eqiad and got MariaDB installed and
    loaded with some database copies.
    
    Change-Id: I40cc4e8445500cb1db091b7eb325ac39c8d3a12f

--- wmf-config/db-labs.php
+++ wmf-config/db-labs.php
@@ -5,41 +5,76 @@
 if( $wmfRealm == 'labs' ) { # safe guard
 	# Database configuration files for the beta labs
 
-	$wgDBTableOptions   = "ENGINE=InnoDB, DEFAULT CHARSET=binary";
+	$wgDBTableOptions = "ENGINE=InnoDB, DEFAULT CHARSET=binary";
 
-	$wgLBFactoryConf = array(
+	if ( $wmfDatacenter === 'pmtpa' ) {
+		$wgLBFactoryConf = array(
 
-		#Requires 'sectionsByDB', 'sectionLoads', 'serverTemplate'
+			#Requires 'sectionsByDB', 'sectionLoads', 'serverTemplate'
 
-		'class' => 'LBFactoryMulti',
+			'class' => 'LBFactoryMulti',
 
-		'sectionsByDB' => array(
-			'wikidatawiki' => 's2',
-		),
-
-		'sectionLoads' => array(
-			'DEFAULT' => array(
-				'db1'     => 0,
+			'sectionsByDB' => array(
+				'wikidatawiki' => 's2',
 			),
-			's2' => array(
-				'db2'	  => 0,
-			)
-		),
 
-		'serverTemplate' => array(
-			'dbname'	  => $wgDBname,
-			'user'		  => $wgDBuser,
-			'password'	  => $wgDBpassword,
-			'type'		  => 'mysql',
-			'flags'		  => DBO_DEFAULT,
-			'max lag'	  => 30,
-		),
+			'sectionLoads' => array(
+				'DEFAULT' => array(
+					'db1'     => 0,
+				),
+				's2' => array(
+					'db2'	  => 0,
+				)
+			),
 
-		'hostsByName' => array(
-			'db1'  => '10.4.0.53',   # deployment-sql
-			'db2'  => '10.4.0.248',  # deployment-sql02
-		),
-	);
+			'serverTemplate' => array(
+				'dbname'	  => $wgDBname,
+				'user'		  => $wgDBuser,
+				'password'	  => $wgDBpassword,
+				'type'		  => 'mysql',
+				'flags'		  => DBO_DEFAULT,
+				'max lag'	  => 30,
+			),
+
+			'hostsByName' => array(
+				'db1'  => '10.4.0.53',   # deployment-sql
+				'db2'  => '10.4.0.248',  # deployment-sql02
+			),
+		);
+
+	} elseif ( $wmfDatacenter === 'eqiad' ) {
+
+		$wgLBFactoryConf = array(
+
+			#Requires 'sectionsByDB', 'sectionLoads', 'serverTemplate'
+
+			'class' => 'LBFactoryMulti',
+
+			# Everyone to DEFAULT
+			'sectionsByDB' => array(),
+
+			# eqiad has a single server
+			'sectionLoads' => array(
+				'DEFAULT' => array(
+					'db1'     => 0,
+				),
+			),
+
+			'serverTemplate' => array(
+				'dbname'	  => $wgDBname,
+				'user'		  => $wgDBuser,
+				'password'	  => $wgDBpassword,
+				'type'		  => 'mysql',
+				'flags'		  => DBO_DEFAULT,
+				'max lag'	  => 30,
+			),
+
+			'hostsByName' => array(
+				'db1'  => '10.68.16.193',   # deployment-db1.eqiad.wmflabs
+			),
+		);
+
+	}
 
 	# No parser cache in beta yet
 	$wmgParserCacheDBs = array();
