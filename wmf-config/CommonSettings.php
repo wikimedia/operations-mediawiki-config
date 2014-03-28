2aaddd3c0fe338a9fa8d9afc49f54a2591256f35
Max Semenik <maxsem.wiki@gmail.com>
2014-03-29 01:40:47 +0400

    Put a safeguard on GeoData's usage of CirrusSearch
    ...to prevent fatals in case of misconfiguration
    
    Change-Id: I61f2af7eaa847dbcd45669558596336466217922

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -2243,14 +2243,18 @@ if ( $wmgUseWikimediaShopLink ) {
 
 if ( $wmgEnableGeoData ) {
 	require_once( "$IP/extensions/GeoData/GeoData.php" );
-	$wgGeoDataBackend = $wmgGeoDataBackend;
+	if ( $wmgUseCirrus || $wmgUseCirrusAsAlternative ) {
+		$wgGeoDataBackend = $wmgGeoDataBackend;
+		$wgGeoDataUseCirrusSearch = $wmgGeoDataUseCirrusSearch;
+	} else {
+		$wgGeoDataBackend = 'solr';
+	}
 	$wgGeoDataSolrMaster = 'solr1001.eqiad.wmnet';
 	$wgGeoDataSolrHosts = array(
 		'solr1001.eqiad.wmnet' => 75, // master, put less read load on it
 		'solr1002.eqiad.wmnet' => 100,
 		'solr1003.eqiad.wmnet' => 100,
 	);
-	$wgGeoDataUseCirrusSearch = $wmgGeoDataUseCirrusSearch;
 
 	# Data collection mode
 	if ( !$wmgEnableGeoSearch ) {
