86ef23f06e479a60ef35007788cbf379551e9648
Steinsplitter <steinsplitter-wiki@live.com>
2014-03-28 23:27:44 +0100

    Set a special logo for Commons on Beta Labs
    From https://github.com/wikimedia/operations-mediawiki-config/pull/9
    
    Change-Id: Ic98928d541d061728509688b5b78cabff2dd210e

--- wmf-config/InitialiseSettings-labs.php
+++ wmf-config/InitialiseSettings-labs.php
@@ -142,7 +142,7 @@ return array(
 	),
 
 	'wgLogo' => array(
-	//	'commonswiki' => '//commons.wikimedia.beta.wmflabs.org/w/thumb.php?f=Wiki.png&width=88&a',
+		'commonswiki' => '$stdlogo',
 	),
 
 	// Editor Engagement stuff
