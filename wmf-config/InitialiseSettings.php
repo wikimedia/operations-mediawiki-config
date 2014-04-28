0f3e6ff17189f8f5de211ed53b83bdd17ffcf103
Tomasz W. Kozlowski <twkozlowski@gmail.com>
2014-04-28 10:50:52 +0200

    Add Library of Congress to wgCopyUploadsDomains
    So as to allow uploads with the GWToolset tool.
    
    Bug: 64487
    Change-Id: I3282e8ac5fceaf9253850085ff18f04242d4b28e

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -10417,6 +10417,7 @@ $wgConf->settings = array(
 		'*.wikimedia.ch',                // Wikimedia CH
 		'link.nypl.org',                 // New York Public Library
 		'peacepalacelibrary.nl', 	 // Peace Palace Library, The Hague  bug 64372
+		'lcweb2.loc.gov',		 // Library of Congress
 	),
 ),
 
