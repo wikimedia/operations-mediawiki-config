b797ca920bbe92115e3798b3cdf6f2c5fca99449
cpettet <rush@wikimedia.org>
2014-05-20 11:56:44 -0500

    wgRCFeedschanges/updates via udp to second destination
    refs: https://gerrit.wikimedia.org/r/#/c/132495/
    
    we are trying to migrate this functionality out
    of tampa but need to have simultaneous feeds
    for testing / migration.
    
    Change-Id: I7a02f2615d98428b6f27514e75d935d36e44fcb1

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -2681,6 +2681,13 @@ if ( $wmgUseRC2UDP ) {
 		'add_interwiki_prefix' => false,
 		'omit_bots' => false,
 	);
+
+	$wgRCFeeds['ircd'] = array(
+		'formatter' => 'IRCColourfulRCFeedFormatter',
+		'uri' => "udp://$wmgRC2UDPAddressMIGRATION:$wmgRC2UDPPort/$wmgRC2UDPPrefix",
+		'add_interwiki_prefix' => false,
+		'omit_bots' => false,
+	);
 }
 
 // Confirmed can do anything autoconfirmed can.
