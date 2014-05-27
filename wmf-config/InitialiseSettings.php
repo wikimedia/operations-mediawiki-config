c34b9a4511a41ea70bf94bcff4f0459cf0c34e48
Sam Smith <git@samsmith.io>
2014-05-27 13:50:47 +0100

    Disable the anonymous signup invite experiment
    Change-Id: I7fdafede7b3a3b581595819b7c2c5f2785193734

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -12378,12 +12378,6 @@ $wgConf->settings = array(
 ),
 'wmgGettingStartedRunTest' => array(
 	'default' => false,
-	'test' => true,
-	'test2' => true,
-	'dewiki' => true,
-	'enwiki' => true,
-	'frwiki' => true,
-	'itwiki' => true,
 ),
 'wmgUseGuidedTour' => array(
 	'default' => false,
