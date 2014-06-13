bd1840483330026a78e7c9e1c0f8fa140de010be
withoutaname <drevitchi@gmail.com>
2014-06-13 00:30:32 -0700

    Enable Echo on Wikimedia wikis by default
    Except for a few private wikis, that's been the default
    behavior for some time now, but it was never made 'official'.
    
    Change-Id: Ibde4c0c14331590c506583acec3877f79c675342

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -197,7 +197,7 @@ if ( !$globals ) {
 	$wikiTags = array();
 	foreach ( array( 'private', 'fishbowl', 'special', 'closed', 'flaggedrevs', 'small', 'medium',
 			'large', 'wikimania', 'wikidata', 'wikidataclient', 'mediaviewer', 'visualeditor',
-			'visualeditor-default', 'echowikis', 'commonsuploads', 'nonbetafeatures' ) as $tag ) {
+			'visualeditor-default', 'commonsuploads', 'nonbetafeatures' ) as $tag ) {
 		$dblist = array_map( 'trim', file( getRealmSpecificFilename( "$IP/../$tag.dblist" ) ) );
 		if ( in_array( $wgDBname, $dblist ) ) {
 			$wikiTags[] = $tag;
