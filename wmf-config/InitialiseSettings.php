ac030d01252488cb792830683b724a8b6d973989
withoutaname <drevitchi@gmail.com>
2014-06-13 10:22:13 -0700

    Restore defaults for nowikibooks bureaucrats
    The "default" settings were found in bug 13853 and
    were asked to be restored in bug 42105. Quite peculiar, it seems
    $wgAddGroups and $wgRemoveGroups must have switched their
    permissions groups somehow.
    
    Bug: 42105
    Change-Id: I1c99df65cab1064c69ee173222e073a96bf77a86

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -8004,7 +8004,7 @@ $wgConf->settings = array(
 		'sysop' => array( 'patroller', 'autopatrolled', 'abusefilter', 'transwiki' ),
 	),
 	'+nowikibooks' => array(
-		'bureaucrat' => array( 'autopatrolled', 'patroller' ),
+		'bureaucrat' => array( 'sysop', 'bureaucrat', 'bot', 'patroller', 'autopatrolled' ),
 		'sysop' => array( 'autopatrolled', 'patroller' ),
 	),
 	'+officewiki' => array(
@@ -8520,7 +8520,7 @@ $wgConf->settings = array(
 		'sysop' => array( 'autopatrolled', 'abusefilter' ),
 	),
 	'+nowikibooks' => array(
-		'bureaucrat' => array( 'bureaucrat', 'sysop', 'autopatrolled', 'patroller' ),
+		'bureaucrat' => array( 'bot', 'patroller', 'autopatrolled' ),
 		'sysop' => array( 'autopatrolled', 'patroller' ),
 	),
 	'+officewiki' => array(
