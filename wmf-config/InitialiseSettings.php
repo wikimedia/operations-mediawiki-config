abae6569818480058aed4cbe71c8cd26e321a9f1
withoutaname <drevitchi@gmail.com>
2014-06-13 00:30:32 -0700

    Enable Echo on Wikimedia wikis by default
    Except for a few private wikis, that's been the default
    behavior for some time now, but it was never made 'official'.
    
    Change-Id: Ibde4c0c14331590c506583acec3877f79c675342

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -12717,8 +12717,11 @@ $wgConf->settings = array(
 ),
 
 'wmgUseEcho' => array(
-	'default' => false,
-	'echowikis' => true,
+	'default' => true,
+	'legalteamwiki' => false,
+	'loginwiki' => false,
+	'votewiki' => false,
+	'zerowiki' => false,
 ),
 'wmgEchoEnableEmailBatch' => array(
 	'default' => true,
@@ -12800,8 +12803,11 @@ $wgConf->settings = array(
 
 // Thanks should be enabled for wikis with Echo
 'wmgUseThanks' => array(
-	'default' => false,
-	'echowikis' => true,
+	'default' => true,
+	'legalteamwiki' => false,
+	'loginwiki' => false,
+	'votewiki' => false,
+	'zerowiki' => false,
 ),
 
 'wmgUseFlow' => array(
