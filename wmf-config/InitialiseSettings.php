c16b82d260af3c91f0913a939750ea63fbd70e8d
withoutaname <drevitchi@gmail.com>
2014-06-13 17:13:42 -0700

    Disable $wgLegacyJavaScriptGlobals for test2wiki
    Bug: 65011
    Change-Id: I78f53faa086f0e8fbf28fca066a36500f26338d6

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -13587,6 +13587,11 @@ $wgConf->settings = array(
 	'votewiki' => false,
 ),
 
+'wgLegacyJavaScriptGlobals' => array(
+	'default' => true,
+	'test2wiki' => false, // bug 65011
+),
+
 'wgSecureLogin' => array(
 	'default' => true,
 ),
