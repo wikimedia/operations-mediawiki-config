33452a457bbaa2114bdacc9e7a4fd7e744aa438a
Danny B <danny.b@email.cz>
2014-06-18 23:52:35 +0200

    Set $wgCategoryCollation to 'uca-sk' on skwiki
    Doing just that; will need a shell user to rebuild category sort keys for the setting to work.
    
    Change-Id: Icf9740b70d7f3b260ccb0a114b6c16c9807ce0c0

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -11346,6 +11346,7 @@ $wgConf->settings = array(
 	'ruwikiversity' => 'uca-ru', // bug 52997
 	'ruwikivoyage' => 'uca-ru', // bug 52997
 	'ruwiktionary' => 'uca-ru', // bug 52997
+	'skwiki' => 'uca-sk', // [[w:sk:Special:PermaLink/5785371]]
 	'svwiki' => 'uca-sv', // bug 45446
 	'svwikisource' => 'uca-sv@collation=standard', // bug 46058
 	'thwikibooks' => 'uca-default', // bug 48097
