29eb4e7dbc36569dda44515f97448bc450d13eef
Chad Horohoe <chadh@wikimedia.org>
2014-06-19 11:01:08 -0700

    Move remaining pool 3 wikis to Cirrus as primary
    Change-Id: Ic886f5491b8ea34c23ec9b88e04fbe88ab68bbb8

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -13248,14 +13248,8 @@ $wgConf->settings = array(
 	'default' => true,
 	'dewiki' => false,
 	'enwiki' => false,
-	'eswiki' => false,
 	'frwiki' => false,
 	'jawiki' => false,
-	'nlwiki' => false,
-	'plwiki' => false,
-	'ruwiki' => false,
-	'svwiki' => false,
-	'zhwiki' => false,
 ),
 
 'wmgUseCirrusAsAlternative' => array(
