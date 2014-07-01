efd118a4cb603dc2bc2b227676b624d2361dc3b8
Marius Hoch <hoo@online.de>
2014-07-01 16:20:32 +0200

    Fix extension WikiHiero typo
    Change-Id: Ifd83d0d6402b2dd3ddcc9b002252b79025e145db

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -13516,7 +13516,7 @@ $wgConf->settings = array(
 	'votewiki' => false,
 ),
 
-'wmgUseWikiHeiro' => array(
+'wmgUseWikiHiero' => array(
 	'default' => true,
 	'loginwiki' => false,
 	'votewiki' => false,
