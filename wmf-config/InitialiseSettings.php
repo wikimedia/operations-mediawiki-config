c7426ce28d69818aebcd2b60e879c729b7376fc3
This, that and the other <at.light@live.com.au>
2014-07-12 21:38:55 +1000

    Add lawiki featured feed settings
    Bug: 33978
    Change-Id: I75d5545dbb9301014d2182b858cea9ce545605a4

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -12661,7 +12661,6 @@ $wgConf->settings = array(
 ),
 'wmgFeaturedFeedsOverrides' => array(
 	'default' => array(),
-
 	'fawiki' => array ( // Bug 55884
 		'dyk' => array(
 			'frequency' => 'weekly',
@@ -12670,10 +12669,14 @@ $wgConf->settings = array(
 			'frequency' => 'weekly',
 		),
 	),
-
 	'frwikisource' => array( // Bug 39919
 		'featuredtexts' => array(
 			'frequency' => 'weekly',
+		),
+	),
+	'lawiki' => array ( // bug 33978
+		'featured' => array(
+			'limit' => 180,
 		),
 	),
 	'metawiki' => array( // bug 63596
