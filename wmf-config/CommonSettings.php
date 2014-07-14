269575a50ca14aa9b45467e90fe069cb14ab42a8
Adam Roses Wight <awight@wikimedia.org>
2014-07-14 11:14:30 -0700

    Revert "Enable FundraisingTranslateWorkflow on metawiki"
    This reverts commit bda612956ab3ea0e6b0bbb102ed32e7235f646ca.
    
    Change-Id: Ieaa55e984920e66e8f980416910b6cd0ba9d855e

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -663,10 +663,6 @@ if ( $wmgUseQuiz ) {
 	include( "$IP/extensions/Quiz/Quiz.php" );
 }
 
-if ( $wmgUseFundraisingTranslateWorkflow ) {
-	include( "$IP/extensions/FundraisingTranslateWorkflow/FundraisingTranslateWorkflow.php" );
-}
-
 if ( $wmgUseGadgets ) {
 	include( "$IP/extensions/Gadgets/Gadgets.php" );
 }
--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -9482,11 +9482,6 @@ $wgConf->settings = array(
 	'default' => '//upload.wikimedia.org/math',
 ),
 
-'wmgUseFundraisingTranslateWorkflow' => array(
-	'default' => false,
-	'metawiki' => true,
-),
-
 'wmgUseScore' => array(
 	'default' => true,
 	'loginwiki' => false,
