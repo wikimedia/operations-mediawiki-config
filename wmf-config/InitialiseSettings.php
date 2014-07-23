25bb880dfe799e78f22616e603a0e726ccde9f0d
Marius Hoch <hoo@online.de>
2014-07-23 20:13:20 +0200

    Set otherProjectsLinksByDefault for kowiki
    With the current version of Wikibase this will be a
    no-op, but it will get relevant after Ib401425301 has
    been merged (this just makes sure the current behaviour
    wont change even after that patch has been deployed)...
    
    Change-Id: If777c609cb7ea0ef82f96716c22cc99afdaca390

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -13349,6 +13349,7 @@ $wgConf->settings = array(
 		'otherProjectsLinks' => array( 'itwiki', 'itwikiquote', 'commonswiki', 'itwikivoyage' ),
 	),
 	'kowiki' => array(
+		'otherProjectsLinksByDefault' => true,
 		'otherProjectsLinks' => array( 'kowikiquote', 'kowikisource', 'commonswiki' ),
 	),
 	'lbwiki' => array(
