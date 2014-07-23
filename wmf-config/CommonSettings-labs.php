07834a9d0c4af30cbd52b7d4c3f99016afe12ccd
Ori Livneh <ori@wikimedia.org>
2014-07-23 12:57:26 -0700

    beta cluster: use luastandalone
    Partially revert change I77fa1885c / commit b67eebc as a temporary workaround
    for bug 68413.
    
    Bug: 68413
    Change-Id: I8a8dc135905027d1e67368423720264d000f9fc1

--- wmf-config/CommonSettings-labs.php
+++ wmf-config/CommonSettings-labs.php
@@ -193,4 +193,10 @@ if ( $wmgUseFundraisingTranslateWorkflow ) {
 	include( "$IP/extensions/FundraisingTranslateWorkflow/FundraisingTranslateWorkflow.php" );
 }
 
+if ( $wmgUseScribunto && defined( 'HHVM_VERSION' ) ) {
+	// Temporary work around for bug 68413
+	$wgScribuntoDefaultEngine = 'luastandalone';
+	$wgScribuntoEngineConf['luastandalone']['luaPath'] = '/usr/bin/lua5.1';
+}
+
 } # end safeguard
