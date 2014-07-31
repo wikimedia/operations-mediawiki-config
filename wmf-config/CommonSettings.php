e43702cc9746f00e7b0bc59c9c50472e88805183
Reedy <reedy@wikimedia.org>
2014-07-31 18:40:01 +0000

    Revert "Remove deprecated $wgCopyrightIcon"
    This reverts commit 499aad5daa15b0eac8c6d5144f4e4216ee87e911.
    
    [19:39:06] <MaxSem> Reedy, the wikimedia project button on every page is broken
    
    Change-Id: I883c5598815fd17410022abe633f85cced94caba

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -874,9 +874,7 @@ if ( $wgDBname == 'nostalgiawiki' ) {
 
 $wgUseHashTable = true;
 
-$wgRightsUrl = '//wikimediafoundation.org/';
-$wgRightsIcon = "{$wmfHostnames['bits']}/images/wikimedia-button.png";
-$wgRightsText = 'Wikimedia Foundation';
+$wgCopyrightIcon = '<a href="//wikimediafoundation.org/"><img src="//' . $wmfHostnames['bits'] . '/images/wikimedia-button.png" width="88" height="31" alt="Wikimedia Foundation"/></a>';
 
 # For Special:Cite, we only want it on wikipedia (but can't count on $site),
 # not on these fakers.
