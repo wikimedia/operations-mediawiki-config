475dfe9af34c29a11595aaade609202932f1886a
Steinsplitter <steinsplitter@wikipedia.de>
2014-08-20 10:59:47 +0000

    Adding new domain to wgCopyUploadsDomains whitelist.
    Adding new domain www.veikkos-archiv.com to wgCopyUploadsDomains.
    
    Bug:69777
    Change-Id: Iff88dcbc9cfe72951df1a7fcd0d028990651b77d

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -10597,6 +10597,7 @@ $wgConf->settings = array(
 		'mochila_images2.s3.amazonaws.com', //temporary for GWT upload       bug 67344
 		'*.universiteitsmuseum.nl',      // Universiteits Museum Utrecht
 		'data.kb.se',                    // National Library of Sweden Open Data bug 69439
+		'www.veikkos-archiv.com',        // Veikkos-archiv                   bug 69777
 	),
 ),
 
