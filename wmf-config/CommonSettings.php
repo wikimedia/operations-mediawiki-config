9e5c0192250d00c5945a85510da4a8a7df369e85
andrewbogott <abogott@wikimedia.org>
2014-08-22 02:31:18 -0500

    Add wikitech config.
    bug: 68751
    bug: 62496
    Change-Id: I2b9322c6dbb43375c420c064e20a129df5a7cfaf

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -2613,6 +2613,10 @@ if ( $wmgUseUniversalLanguageSelector ) {
 // might be build as "aawikibooks" or something that does not have Wikibase.
 $wgExtensionEntryPointListFiles[] = "$IP/extensions/Wikidata/extension-list-wikidata";
 
+if ( $wgDBname == 'labswiki' ) {
+	$wgExtensionEntryPointListFiles[] = "$wmfConfigDir/extension-list-wikitech";
+}
+
 if ( $wmgUseWikibaseRepo || $wmgUseWikibaseClient ) {
 	if ( $wmgUseWikibaseRepo && $wmfRealm === 'labs' ) {
 		// enable on beta only
@@ -2620,6 +2624,11 @@ if ( $wmgUseWikibaseRepo || $wmgUseWikibaseClient ) {
 	}
 
 	include( "$wmfConfigDir/Wikibase.php" );
+}
+
+if ( $wgDBname == 'labswiki' ) {
+	// A zillion wikitech-specific settings
+	include( "$wmfConfigDir/wikitech.php" );
 }
 
 // put this here to ensure it is available for localisation cache rebuild
