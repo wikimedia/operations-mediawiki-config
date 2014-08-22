4d5c40796e6620fc44e1d6b0dd7f164d634e6c72
andrewbogott <abogott@wikimedia.org>
2014-08-22 02:31:18 -0500

    Add wikitech config.
    bug: 68751
    bug: 62496
    Change-Id: I2b9322c6dbb43375c420c064e20a129df5a7cfaf

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -68,6 +68,8 @@ $wgLBFactoryConf = array(
 	'fawiki' => 's7',
 	'rowiki' => 's7',
 	'kowiki' => 's7',
+
+	'labswiki' => 'virt1000',
 ),
 
 # Load lists
@@ -146,6 +148,9 @@ $wgLBFactoryConf = array(
 		'db1028' => 400, # 1.4TB  64GB, vslow, dump
 		'db1034' => 400, # 1.4TB  64GB
 		'db1039' => 400, # 1.4TB  64GB
+	),
+	'virt1000' => array(
+		'virt1000' => 100,   # I have no idea if this is right
 	),
 ),
 
@@ -411,6 +416,7 @@ $wgLBFactoryConf = array(
 	'db1068' => '10.64.48.23', #do not remove or comment out
 	'db1070' => '10.64.48.25', #do not remove or comment out
 	'db1071' => '10.64.48.26', #do not remove or comment out
+	'virt1000' => '208.80.154.18', #do not remove or comment out
 ),
 
 'externalLoads' => array(
