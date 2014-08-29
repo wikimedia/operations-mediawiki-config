9e26377635db4ca089b96e48a2eb018595a6c635
Ori Livneh <ori@wikimedia.org>
2014-08-29 11:04:13 -0700

    Scrap unused Twemproxy config files
    * Upstream refers to itself inconsistently as Twemproxy or Nutcracker. We've
      standardized on Nutcracker.
    * The Nutcracker configs are managed by Puppet, so get rid of the dupes in this
      repo.
    * Don't keep the non-Nutcracker setup in a comment. Someone will inevitably
      forget to update it and it will become gradually more dangerous. For better
      or worse, we rely on Nutcracker now. Turning it off will require more than
      uncommenting a few lines.
    
    Change-Id: I812c0bb6cc121d9df58a3eedb14e387507445ee6

--- wmf-config/mc.php
+++ wmf-config/mc.php
@@ -3,17 +3,8 @@
 $wgMainCacheType = 'memcached-pecl';
 
 $wgMemCachedPersistent = false;
-$wgMemCachedTimeout = 250000; # default is 100000
+$wgMemCachedTimeout = 0.25 * 1e6;  // 250kμs (a quarter of a second).
 
-# Newer "mc*" servers (only use the pecl client with these).
-# This does not use the "slot" system like the old setup, but
-# rather a consistent hash based on key and server addresses,
-# so the ordering of servers is not important. Additionally, the
-# number of servers can grow/shrink without *too* much disruption.
-
-# Use twemproxy for memcached access - see twemproxy-eqiad.yaml
-# NOTE: after deploying a new twemproxy.yaml config, run restart-twemproxy
-# from the deploy host to make it live.
 $wgObjectCaches['memcached-pecl'] = array(
 	'class'      => 'MemcachedPeclBagOStuff',
 	'serializer' => 'php',
@@ -22,30 +13,5 @@ $wgObjectCaches['memcached-pecl'] = array(
 	'server_failure_limit' => 1e9,
 	'retry_timeout' => -1
 );
-
-/*** No Twemproxy
-	$wgObjectCaches['memcached-pecl'] = array(
-		'class'      => 'MemcachedPeclBagOStuff',
-		'serializer' => 'php',
-		'servers'    => array(
-			'10.64.0.180', # mc1001
-			'10.64.0.181', # mc1002
-			'10.64.0.182', # mc1003
-			'10.64.0.183', # mc1004
-			'10.64.0.184', # mc1005
-			'10.64.0.185', # mc1006
-			'10.64.0.186', # mc1007
-			'10.64.0.187', # mc1008
-			'10.64.0.188', # mc1009
-			'10.64.0.189', # mc1010
-			'10.64.0.190', # mc1011
-			'10.64.0.191', # mc1012
-			'10.64.0.192', # mc1013
-			'10.64.0.193', # mc1014
-			'10.64.0.194', # mc1015
-			'10.64.0.195', # mc1016
-		)
-	);
-***/
 
 # vim: set sts=4 sw=4 et :
