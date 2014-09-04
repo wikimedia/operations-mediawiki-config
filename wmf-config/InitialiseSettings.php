4d305232ba76129dde2437ea4358c5dbd74ab59b
S Page <spage@wikimedia.org>
2014-09-04 01:58:30 -0700

    Enable Flow on several pages
    Change-Id: Ib0aaa60f095c3ad9309f816256cd0ea84b7b7015

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -13191,11 +13191,19 @@ $wgConf->settings = array(
 		'Talk:Search',
 		'Talk:Cite-from-id',
 		'Talk:Sparrow (sandbox for testing at Wikimania)',
+		'Wikipedia:Teahouse/Questions/Flow_test',
+		'Talk:MediaWiki 1.25',
 	),
 	'enwiki' => array( // Bug 60178
 		'Wikipedia talk:WikiProject Breakfast',
 		'Wikipedia talk:WikiProject Hampshire',
 		'Wikipedia talk:Flow/Developer test page',
+		'Wikipedia:Co-op/Mentorship match',
+		'Wikipedia:Teahouse/Questions/Flow test',
+	),
+	'frwiki' => array(
+		'Wikipédia:Forum des nouveaux/Flow',
+		'Discussion Wikipédia:Flow',
 	),
 ),
 // Use separate database on extension1 cluster for all non-private wikis.
