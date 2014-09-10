3506acc706ca310c8f0cc8ef3e973fbe5a26725d
Ori Livneh <ori@wikimedia.org>
2014-09-10 16:51:19 -0700

    Get rid of MULTIVER_CDB_DIR_{APACHE,HOME}
    Replace references to these vars with MULTIVER_COMMON_{APACHE,HOME}, which have
    the same value.
    
    Change-Id: I37b0a833818683b6a5955e99a39f12290f5630d4

--- multiversion/defines.php
+++ multiversion/defines.php
@@ -5,8 +5,5 @@
 define( 'MULTIVER_COMMON_HOME', '/a/common' );
 define( 'MULTIVER_COMMON_APACHE', '/srv/mediawiki' );
 
-define( 'MULTIVER_CDB_DIR_HOME', '/a/common' );
-define( 'MULTIVER_CDB_DIR_APACHE', '/srv/mediawiki' );
-
 define( 'MULTIVER_404SCRIPT_PATH_HOME', '/a/common/wmf-config/missing.php' );
 define( 'MULTIVER_404SCRIPT_PATH_APACHE', '/srv/mediawiki/wmf-config/missing.php' );
