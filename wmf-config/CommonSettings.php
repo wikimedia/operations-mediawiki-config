df7e7b54f9976c052cda337d15659dd1169b23f9
Yuri Astrakhan <yurik@wikimedia.org>
2014-09-11 15:42:07 -0400

    ZERO: fixed meta security, adjusted zerowiki rights adjustments
    fixes regression bug 70712 by restoring security settings as they
    were before the change for the metawiki
    
    Change-Id: I5dcd04a9d311cb14d540b2b55fa1005792606c7d

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -2694,17 +2694,27 @@ if ( $wmgZeroPortal ) {
 	require_once( "$IP/extensions/ZeroBanner/ZeroBanner.php" );
 	require_once( "$IP/extensions/ZeroPortal/ZeroPortal.php" );
 
+// Until meta handles ZeroPortal, need to keep it intact
+if ( $wgDBname == 'zerowiki' ) {
 	// zerowiki treats all logged-in users the same as anonymous, without giving them any extra rights
 	// Only sysops and scripts get additional rights on zerowiki
 	$zpUserRights = $wgGroupPermissions['user'];
-	$wgGroupPermissions['sysop'] = $wgGroupPermissions['sysop'] + $zpUserRights;
+
+	$wgGroupPermissions['*']['createtalk'] = fales;
+	$wgGroupPermissions['*']['createpage'] = fales;
+	$wgGroupPermissions['*']['writeapi'] = fales;
 	$wgGroupPermissions['user'] = $wgGroupPermissions['*'];
 
-	$wgGroupPermissions['zeroadmin']['zero-edit'] = true;
-	$wgGroupPermissions['zeroadmin']['zero-script'] = true;
-	$wgGroupPermissions['zeroadmin']['zero-script-ips'] = true;
-	$wgGroupPermissions['zeroadmin']['jsonconfig-flush'] = true;
-	$wgGroupPermissions['zeroadmin'] = $wgGroupPermissions['zeroadmin'] + $zpUserRights;
+	// fixme: this should go into groupOverrides or groupOverrides2, with or without a '+'
+	// 'sysop' => array( 'zero-edit', 'zero-script', 'zero-script-ips', 'jsonconfig-flush' ),
+	// 'zeroscript' => array( 'zero-script', 'jsonconfig-flush' ),
+	// 'zeroscriptips' => array( 'zero-script-ips', 'jsonconfig-flush' ),
+
+	$wgGroupPermissions['sysop']['zero-edit'] = true;
+	$wgGroupPermissions['sysop']['zero-script'] = true;
+	$wgGroupPermissions['sysop']['zero-script-ips'] = true;
+	$wgGroupPermissions['sysop']['jsonconfig-flush'] = true;
+	$wgGroupPermissions['sysop'] = $wgGroupPermissions['sysop'] + $zpUserRights;
 
 	$wgGroupPermissions['zeroscript']['zero-script'] = true;
 	$wgGroupPermissions['zeroscript']['jsonconfig-flush'] = true;
@@ -2715,6 +2725,17 @@ if ( $wmgZeroPortal ) {
 	$wgGroupPermissions['zeroscriptips'] = $wgGroupPermissions['zeroscriptips'] + $zpUserRights;
 
 	unset( $zpUserRights );
+} else {
+	// metawiki
+	$wgGroupPermissions['zeroadmin']['zero-edit'] = true;
+	$wgGroupPermissions['zeroadmin']['zero-script'] = true;
+	$wgGroupPermissions['zeroadmin']['zero-script-ips'] = true;
+	$wgGroupPermissions['zeroadmin']['jsonconfig-flush'] = true;
+	$wgGroupPermissions['zeroscript']['zero-script'] = true;
+	$wgGroupPermissions['zeroscript']['jsonconfig-flush'] = true;
+	$wgGroupPermissions['zeroscriptips']['zero-script-ips'] = true;
+	$wgGroupPermissions['zeroscriptips']['jsonconfig-flush'] = true;
+}
 }
 
 if ( $wmgUseGraph ) {
