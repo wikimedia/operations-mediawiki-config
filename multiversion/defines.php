6deee36bdf5107dac63cb3bdb2c22a6af6cb31e8
Reedy <reedy@wikimedia.org>
2014-09-16 11:21:19 +0000

    Fix MEDIAWIKI_STAGING_DIR to point to /srv/mediawiki-staging
    Change-Id: I5890abe13c815d21b6c95e55c0d674a6b9704026

--- multiversion/defines.php
+++ multiversion/defines.php
@@ -2,5 +2,5 @@
 /**
  * Define installation specific MWMultiVersion values like paths
  */
-define( 'MEDIAWIKI_STAGING_DIR', '/a/common' );
+define( 'MEDIAWIKI_STAGING_DIR', '/srv/mediawiki-staging' );
 define( 'MEDIAWIKI_DEPLOYMENT_DIR', '/srv/mediawiki' );
