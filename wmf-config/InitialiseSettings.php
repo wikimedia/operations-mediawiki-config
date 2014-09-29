19fcc3cb70edd6ad373b0fc95b60e1db525a6768
aude <aude.wiki@gmail.com>
2014-09-29 11:27:52 +0200

    Add wikidatawiki and test wikis (e.g. test2) to wikidataclient.dblist
    instead of listing these separately in InitialiseSettings
    
    Change-Id: Ia4c71aaa6268c7536d0efee929d7fdbd92e63fc3

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -13512,10 +13512,6 @@ $wgConf->settings = array(
 
 'wmgUseWikibaseClient' => array(
 	'default' => false,
-	'wikidatawiki' => true,
-	'testwikidatawiki' => true,
-	'test2wiki' => true,
-	'testwiki' => true,
 	'wikidataclient' => true,
 ),
 
