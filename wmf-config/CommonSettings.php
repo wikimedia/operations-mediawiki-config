9969a584ceed9cb97b8d7322aeccfc8061e8780b
Reedy <reedy@wikimedia.org>
2014-10-08 16:37:24 +0100

    Always use debian packaged texvc
    Follows up I30a5d35152a3a88974d6b7898213be217b372138
    
    Change-Id: I0ce52813f2bbaca9946ac8bb36b7d28b60f22633

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -2173,11 +2173,10 @@ $wgCookieExpiration = 30 * 86400;
 if ( $wmgUseMath ) {
 	require_once( "$IP/extensions/Math/Math.php" );
 
-	// Use the Debian-packaged texvc on Trusty. --Ori, 24-Sept-2014
-	$wgTexvc = defined( 'HHVM_VERSION' ) && $wmfRealm === 'production' ?
-		'/usr/bin/texvc' : '/usr/local/apache/uncommon/bin/texvc';
+	$wgTexvc = '/usr/bin/texvc';
 
-	$wgMathTexvcCheckExecutable = "/usr/local/apache/uncommon/bin/texvccheck";
+	$wgMathTexvcCheckExecutable = defined( 'HHVM_VERSION' ) && $wmfRealm === 'production' ?
+		'/usr/local/apache/uncommon/bin/texvccheck' : '/usr/bin/texvccheck';
 	if ( $wgDBname === 'hewiki' ) {
 		$wgDefaultUserOptions['math'] = 0;
 	}
