b450776223987e803ec443fe481445636d03eb6c
Gergő Tisza <tgr.huwiki@gmail.com>
2014-10-23 19:15:11 +0200

    Enable ImageMetrics extension on all wikis
    Change-Id: I9834ddf1c995747bdfac89b28d133716773d2acc

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -11585,8 +11585,7 @@ $wgConf->settings = array(
 // ----------------- ImageMetrics -----------------
 
 'wmgUseImageMetrics' => array(
-	'default' => false,
-	'group0' => true,
+	'default' => true,
 ),
 
 'wmgImageMetricsSamplingFactor' => array(
