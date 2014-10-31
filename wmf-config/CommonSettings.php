1647a37f73a011e3b98f00717a63525d454ef168
Ori Livneh <ori@wikimedia.org>
2014-10-30 17:37:30 -0700

    HHVM: back to 10% of anons
    Change-Id: I0b798b15af5a5177136618b75f7922a9383884c8

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -2656,7 +2656,7 @@ if ( $wmgUseEventLogging && $wmgUseNavigationTiming ) {
 	// lowered without careful coordination with ops.
 	$wgNavigationTimingSamplingFactor = 1000;
 
-	$wgPercentHHVM = 5;  // giuseppe, 30-Oct-2014
+	$wgPercentHHVM = 10;  // giuseppe, 30-Oct-2014
 }
 
 if ( $wmgUseUniversalLanguageSelector ) {
