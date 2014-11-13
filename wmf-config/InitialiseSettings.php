8c0e3d73868f86e5a40f0cb1441bd210ef048ed6
Jan Zerebecki <jan.wikimedia@zerebecki.de>
2014-11-13 21:06:29 +0100

    Change ru.wikinews.org to HTTPS only.
    Redirect was done in: I4e50f5305d8d04539b0b1b1ed794f3c05f54118e
    
    Change-Id: I8996b9841581d91cd709e0a610f5165a4532eecf

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -1368,6 +1368,7 @@ $wgConf->settings = array(
 	'outreachwiki' => 'http://outreach.wikimedia.org',
 	'pa_uswikimedia' => 'http://pa-us.wikimedia.org',
 	'qualitywiki' => 'http://quality.wikimedia.org',
+	'ruwikinews' => 'https://ru.wikinews.org',
 	'searchcomwiki' => 'https://searchcom.wikimedia.org',
 	'sourceswiki' => 'http://wikisource.org',
 	'spcomwiki' => 'https://spcom.wikimedia.org',
