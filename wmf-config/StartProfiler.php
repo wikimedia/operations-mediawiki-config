fa34affedc7ca7e6a37632ce74d6bf966af4582b
Aaron Schulz <aschulz@wikimedia.org>
2014-11-14 17:11:18 -0800

    Enable xhprof in labs, testwiki, and with ?forceprofile anywhere
    * This only applies if the apache reached is using HHVM, otherwise
      the old profiler is used as before.
    * Removed old profiler config format code
    
    Change-Id: I42742431d19544194c24d4650a5eb9e93ea1215a

--- wmf-config/StartProfiler.php
+++ wmf-config/StartProfiler.php
@@ -5,29 +5,27 @@
 
 # Non-logged profiling for debugging
 if ( isset( $_REQUEST['forceprofile'] ) ) {
-	if ( class_exists( 'ProfilerOutput' ) ) {
-		$wgProfiler['class'] = 'ProfilerStandard';
-		$wgProfiler['output'] = 'text';
+	if ( defined( 'HHVM_VERSION' ) ) {
+		$wgProfiler['class'] = 'ProfilerXhprof';
+		$wgProfiler['flags'] = XHPROF_FLAGS_NO_BUILTINS;
 	} else {
-		$wgProfiler['class'] = 'ProfilerSimpleText';
+		$wgProfiler['class'] = 'ProfilerStandard';
 	}
+	$wgProfiler['output'] = 'text';
 # Profiling hack for test2 wiki (not sampled, but shouldn't distort too much)
 } elseif ( isset( $_SERVER['HTTP_HOST'] ) && $_SERVER['HTTP_HOST'] === 'test2.wikipedia.org' ) {
-	if ( class_exists( 'ProfilerOutput' ) ) {
-		$wgProfiler['class'] = 'ProfilerStandard';
-		$wgProfiler['output'] = 'udp';
+	if ( defined( 'HHVM_VERSION' ) ) {
+		$wgProfiler['class'] = 'ProfilerXhprof';
+		$wgProfiler['flags'] = XHPROF_FLAGS_CPU | XHPROF_FLAGS_MEMORY | XHPROF_FLAGS_NO_BUILTINS;
 	} else {
-		$wgProfiler['class'] = 'ProfilerSimpleUDP';
+		$wgProfiler['class'] = 'ProfilerStandard';
 	}
+	$wgProfiler['output'] = 'udp';
 	$wgProfiler['profileID'] = 'test2';
 # Normal case: randomly (or not) selected for logged profiling sample
 } elseif ( PHP_SAPI !== 'cli' && $wmfDatacenter == 'eqiad' && ( mt_rand() % 50 ) == 0 ) {
-	if ( class_exists( 'ProfilerOutput' ) ) {
-		$wgProfiler['class'] = 'ProfilerStandard';
-		$wgProfiler['output'] = 'udp';
-	} else {
-		$wgProfiler['class'] = 'ProfilerSimpleUDP';
-	}
+	$wgProfiler['class'] = 'ProfilerStandard';
+	$wgProfiler['output'] = 'udp';
 	// $IP is something like '/srv/mediawiki/php-1.19'
 	$version = str_replace( 'php-', '', basename( $IP ) );
 	if ( strpos( $_SERVER['REQUEST_URI'], '/w/thumb.php' ) !== false ) {
@@ -38,12 +36,13 @@ if ( isset( $_REQUEST['forceprofile'] ) ) {
 		$wgProfiler['profileID'] = $version;
 	}
 } elseif ( $wmfRealm === 'labs' ) {
-	if ( class_exists( 'ProfilerOutput' ) ) {
-		$wgProfiler['class'] = 'ProfilerStandard';
-		$wgProfiler['output'] = 'udp';
+	if ( defined( 'HHVM_VERSION' ) ) {
+		$wgProfiler['class'] = 'ProfilerXhprof';
+		$wgProfiler['flags'] = XHPROF_FLAGS_CPU | XHPROF_FLAGS_MEMORY | XHPROF_FLAGS_NO_BUILTINS;
 	} else {
-		$wgProfiler['class'] = 'ProfilerSimpleUDP';
+		$wgProfiler['class'] = 'ProfilerStandard';
 	}
+	$wgProfiler['output'] = 'udp';
 	$coreGit = new GitInfo( $IP );
 	$wgProfiler['profileID'] = $coreGit->getHeadSHA1() ?: 'labs';
 }
