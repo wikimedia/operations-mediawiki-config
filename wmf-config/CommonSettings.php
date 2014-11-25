d48bdbd9466ace3f0ca940d1ee3cd155d4bec1ad
Reedy <reedy@wikimedia.org>
2014-11-25 17:32:08 +0000

    Add interwiki-labs.cdb
    (not physically committed, see also T75905)
    
    Bug: T69931
    Change-Id: If56abe93a627a1d4f4cf228f90a4b3118351e127

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -1159,9 +1159,11 @@ if ( extension_loaded( 'wikidiff2' ) ) {
 	$wgDiff = false;
 }
 
-if ( file_exists( "$wmfConfigDir/interwiki.cdb" ) ) {
-	$wgInterwikiCache = "$wmfConfigDir/interwiki.cdb";
+$iwCacheFile = getRealmSpecificFilename( "$wmfConfigDir/interwiki.cdb" );
+if ( file_exists( $iwCacheFile ) ) {
+	$wgInterwikiCache = $iwCacheFile;
 }
+unset( $iwCacheFile );
 
 $wgEnotifUseJobQ = true;
 
