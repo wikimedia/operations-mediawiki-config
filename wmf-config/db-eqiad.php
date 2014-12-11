9f2e1b1ca9d7000fe71d8bb17a6702c6c85622c9
Sean Pringle <sean.pringle@gmail.com>
2014-12-11 12:08:24 +1000

    repool db1015
    Change-Id: I9f42131a8dfa640b72d562bb9130e575632e7ad2

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -140,7 +140,7 @@ $wgLBFactoryConf = array(
 		'db1022' => 0,   # 1.4TB  64GB, snapshot, vslow, dump
 		'db1030' => 0,   # 1.4TB  64GB, watchlist, recentchangeslinked, contributions, logpager
 		'db1010' => 400, # 1.4TB  64GB
-		# disk issue 'db1015' => 400, # 1.4TB  64GB
+		'db1015' => 400, # 1.4TB  64GB
 		'db1006' => 400, # 1.4TB  64GB
 		'db1037' => 400, # 1.4TB  64GB
 	),
