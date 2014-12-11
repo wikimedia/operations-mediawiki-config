e213d4a3b8b212418c3edf499be07962659bb23f
Sean Pringle <sean.pringle@gmail.com>
2014-12-11 23:26:31 +1000

    pool db1004 in s7
    Change-Id: Id5aa021b5fd52ec28631ca0386c55ce13e2fc5a2

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -148,6 +148,7 @@ $wgLBFactoryConf = array(
 		'db1033' => 0,   # 1.4TB  64GB, 
 		'db1007' => 0,   # 1.4TB  64GB, vslow, dump
 		'db1041' => 0,   # 1.4TB  64GB, watchlist, recentchangeslinked, contributions, logpager
+		'db1004' => 400, # 1.4TB  64GB
 		'db1028' => 400, # 1.4TB  64GB
 		'db1034' => 400, # 1.4TB  64GB
 		'db1039' => 400, # 1.4TB  64GB
