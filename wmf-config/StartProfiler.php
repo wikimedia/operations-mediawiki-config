12a82754f07db6bf756bccd9da737caf07e2301c
Ori Livneh <ori@wikimedia.org>
2014-12-12 13:45:50 -0800

    xenon profiling: fix undefined notice; decouple from mw
    Change-Id: I0f75dcbd705572682eb2e740c826425410f95c18

--- wmf-config/StartProfiler.php
+++ wmf-config/StartProfiler.php
@@ -99,25 +99,30 @@ if ( extension_loaded( 'xenon' ) && ini_get( 'hhvm.xenon.period' ) ) {
 		// Collate stack samples and fold into single lines.
 		// This is the format expected by FlameGraph.
 		$stacks = array();
+
 		foreach ( $data as $sample ) {
 			$stack = array();
+
 			foreach( $sample['phpStack'] as $frame ) {
-				if ( !in_array( $frame['function'], $omit ) ) {
-					$stack[] = $frame['function'];
+				$func = $frame['function'];
+				if ( $func !== end( $stack ) && !in_array( $func, $omit ) ) {
+					$stack[] = $func;
 				}
 			}
-			if ( !empty( $stack ) ) {
-				$stack = implode( ';', array_reverse( $stack ) );
-				$stacks[$stack] += 1;
+
+			if ( count( $stack ) ) {
+				$strStack = implode( ';', array_reverse( $stack ) );
+				if ( !isset( $stacks[$strStack] ) ) {
+					$stacks[$strStack] = 0;
+				}
+				$stacks[$strStack] += 1;
 			}
 		}
 
-		$options = array( 'serializer' => 'none' );
-		$pool = RedisConnectionPool::singleton( $options );
-		$conn = $pool->getConnection( 'fluorine.eqiad.wmnet:6379' );
-		if ( $conn ) {
+		$redis = new Redis();
+		if ( $redis->connect( 'fluorine.eqiad.wmnet', 6379, 0.1 ) ) {
 			foreach ( $stacks as $stack => $count ) {
-				$conn->publish( 'xenon', "$stack $count" );
+				$redis->publish( 'xenon', "$stack $count" );
 			}
 		}
 	} );
