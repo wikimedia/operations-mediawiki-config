99b0cdb68f402d88fadea46a33f1f8a69b46110c
Sean Pringle <sean.pringle@gmail.com>
2014-12-17 20:36:31 +1000

    repool db1064
    Change-Id: I883f88b4bc4d0319ab62c133e0bb2779d6debb4a

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -122,7 +122,7 @@ $wgLBFactoryConf = array(
 		'db1053' => 0,   # 2.8TB  96GB, watchlist, recentchangeslinked, contributions, logpager
 		'db1056' => 100, # 2.8TB  96GB, api
 		'db1059' => 100, # 2.8TB  96GB, api
-		# upgrade 'db1064' => 500, # 2.8TB 160GB
+		'db1064' => 500, # 2.8TB 160GB
 		'db1068' => 500, # 2.8TB 160GB
 		'db1070' => 500, # 2.8TB 160GB
 	),
