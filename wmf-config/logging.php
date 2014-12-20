ffe5fb814561244d292d9bee9969694b4338b80c
Bryan Davis <bd808@wikimedia.org>
2014-12-20 15:22:51 -0700

    monolog: honor log sampling and levels for logstash
    * Use a MWLoggerMonologSamplingHandler to wrap the shared logstash
      handler when the $wgDebugLogGroups for a given group specifies
      a sample factor.
    * Create a separate redis handler for any channel that specifies
      a minimum log level.
    
    Depends-On: Icd14fc8c44ca9eef0f3f5cc4f1d1d8b68d517f07
    Bug: T85067
    Change-Id: I6d79f27e59cbd5ed0dd441707b328179102cb2f0

--- wmf-config/logging.php
+++ wmf-config/logging.php
@@ -1,5 +1,38 @@
 <?php
 if ( $wmgUseMonologLogger ) {
+	/**
+	* Create a config array for a \Monolog\Handler\RedisHandler instance.
+	* @param int $level Minimum logging level at which this handler will be
+	* triggered
+	* @return array
+	*/
+	function wmMonologRedisConfigFactory( $level ) {
+		global $wmgLogstashPassword;
+		return array(
+			'class' => '\\Monolog\\Handler\\RedisHandler',
+			'args' => array(
+				function() use ( $wmgLogstashPassword ) {
+					$servers = array(
+						'10.64.32.138', // logstash1001.eqiad.wmnet
+						'10.64.32.137', // logstash1002.eqiad.wmnet
+						'10.64.32.136', // logstash1003.eqiad.wmnet
+					);
+					// Connect to a random logstash host
+					$server = $servers[ mt_rand( 0, count($servers) - 1 ) ];
+
+					$redis = new Redis();
+					$redis->connect( $server, 6379, .25 );
+					$redis->connect( '10.68.16.134', 6379, .25 );
+					$redis->auth( $wmgLogstashPassword );
+					return $redis;
+				},
+				'logstash',
+				$level
+			),
+			'formatter' => 'logstash',
+		);
+	}
+
 	// Monolog logging configuration
 	// Note: the legacy handlers still use $wgDebugLogGroups and other legacy
 	// logging config variables to determine logging output.
@@ -34,50 +67,62 @@ if ( $wmgUseMonologLogger ) {
 				'class' => '\\Monolog\\Handler\\NullHandler',
 				'formatter' => 'line',
 			),
+			'logstash' => wmMonologRedisConfigFactory( \Monolog\Logger::DEBUG ),
+		),
+
+		'formatters' => array(
+			'legacy' => array(
+				'class' => 'MWLoggerMonologLegacyFormatter',
+			),
 			'logstash' => array(
-				'class' => '\\Monolog\\Handler\\RedisHandler',
-				'args' => array(
-					function() use ( $wmgLogstashPassword ) {
-						$servers = array(
-							'10.64.32.138', // logstash1001.eqiad.wmnet
-							'10.64.32.137', // logstash1002.eqiad.wmnet
-							'10.64.32.136', // logstash1003.eqiad.wmnet
-						);
-						// Connect to a random logstash host
-						$server = $servers[ mt_rand( 0, count($servers) - 1 ) ];
-
-						$redis = new Redis();
-						$redis->connect( $server, 6379, .25 );
-						$redis->auth( $wmgLogstashPassword );
-						return $redis;
-					},
-						'logstash'
-					),
-					'formatter' => 'logstash',
-				),
+				'class' => '\\Monolog\\Formatter\\LogstashFormatter',
+				'args'  => array( 'mediawiki', php_uname( 'n' ), null, '', 1 ),
 			),
-
-			'formatters' => array(
-				'legacy' => array(
-					'class' => 'MWLoggerMonologLegacyFormatter',
-				),
-				'logstash' => array(
-					'class' => '\\Monolog\\Formatter\\LogstashFormatter',
-					'args'  => array( 'mediawiki', php_uname( 'n' ), null, '', 1 ),
-				),
-				'line' => array(
-					'class' => '\\Monolog\\Formatter\\LineFormatter',
-				),
+			'line' => array(
+				'class' => '\\Monolog\\Formatter\\LineFormatter',
 			),
-		);
+		),
+	);
 
 	// Add logging channels defined in $wgDebugLogGroups
 	foreach ( $wgDebugLogGroups as $group => $dest ) {
+		$sample = false;
+		$level = false;
+		$logstashHandler = 'logstash';
 		if ( is_array( $dest ) ) {
+			// NOTE: sampled logs are not guaranteed to store the same events
+			// in logstash and via udp2log since the two event handlers
+			// independently check the probability of emitting.
+			$sample = isset( $dest['sample'] ) ? $dest['sample'] : $sample;
+			$level = isset( $dest['level'] ) ? $dest['level'] : $level;
 			$dest = $dest['destination'];
 		}
+
+		if ( $level !== false ) {
+			$logstashHandler = "filtered-{$group}";
+			$wmgMonologConfig['handlers'][$logstashHandler] =
+				wmMonologRedisConfigFactory( $level );
+		}
+
+		if ( $sample === false ) {
+			$handlers = array( $group, $logstashHandler );
+		} else {
+			$wmgMonologConfig['handlers']["sampled-{$group}"] = array(
+				'class' => 'MWLoggerMonologSamplingHandler',
+				'args' => array(
+					function() {
+						return MWLogger::getProvider()->getHandler(
+							$logstashHandler
+						);
+					},
+					$sample,
+				),
+			);
+			$handlers = array( $group, "sampled-{$group}" );
+		}
+
 		$wmgMonologConfig['loggers'][$group] = array(
-			'handlers' => array( $group, 'logstash' ),
+			'handlers' => $handlers,
 			'processors' => array( 'wiki', 'psr', 'pid', 'uid', 'web' ),
 		);
 		$wmgMonologConfig['handlers'][$group] = array(
