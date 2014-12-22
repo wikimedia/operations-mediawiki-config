e529a7f4aebf8f547aef0c9b7dd1930d131f9c46
Gergő Tisza <tgr.huwiki@gmail.com>
2014-12-22 11:56:02 -0800

    Deploy Sentry on the beta cluster
    Bug: T78807
    Change-Id: Iac79411d254e830e1362210e27f61f1e37d52991

--- wmf-config/CommonSettings-labs.php
+++ wmf-config/CommonSettings-labs.php
@@ -216,6 +216,11 @@ if ( $wmgUseFundraisingTranslateWorkflow ) {
 	include "$IP/extensions/FundraisingTranslateWorkflow/FundraisingTranslateWorkflow.php";
 }
 
+if ( $wmgUseSentry ) {
+	require_once( "$IP/extensions/Sentry/Sentry.php" );
+	$wgSentryDsn = $wmgSentryDsn;
+}
+
 // Experimental
 $wgGadgetsCaching = false;
 
