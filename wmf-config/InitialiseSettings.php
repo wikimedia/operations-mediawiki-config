f3866bcc1a223d21a1b15a33b3007b08a5d30cec
glaisher <glaisher.wiki@gmail.com>
2015-01-06 11:48:02 +0500

    Create 'interface_editor' user group on cawikibooks
    with editinterface, editusercss, edituserjs user rights.
    Add/remove users to this group by stewards only.
    
    Bug: T85713
    Change-Id: I31209548af679a2d3aad5df3c8ee7c5fcfb7436c

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -6871,6 +6871,13 @@ $wgConf->settings = array(
 		'sysop' => array( 'patrol' => true ),
 		'rollbacker' => array( 'rollback' => true ),
 	),
+	'+cawikibooks' => array(
+		'interface_editor' => array( // T85713
+			'editinterface' => true,
+			'editusercss' => true,
+			'edituserjs' => true,
+		),
+	),
 	'+checkuserwiki' => array( // bug 28781
 		'autoconfirmed' => array(
 			'autoconfirmed' => false,
