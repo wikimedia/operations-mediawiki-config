83608780b809f8be1dc71a1a1a175b1792fa2937
Sébastien Santoro <dereckson@espace-win.org>
2015-01-12 18:57:24 +0000

    New user message extension configuration on fa.wikivoyage
    wmgNewUserMessageOnAutoCreate set at true
    
    This is a follow-up of change I156954ef917176b79cd84dbfa7427a980c3e6d22.
    
    Bug: T76716
    Change-Id: I6a1b1eb41f58dbaccf109a3913e030ded1743deb

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -10773,6 +10773,7 @@ $wgConf->settings = array(
 	'arwikisource' => true,
 	'commonswiki' => true,
 	'enwikinews' => true,
+	'fawikivoyage' => true, // T76716
 	'guwiki' => true, // Bug 40872
 	'guwikisource' => true, // Bug 40872
 	'hiwiki' => true,
