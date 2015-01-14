79802c503d77b50ae81a1c5b93ad774e36eb3792
Ori Livneh <ori@wikimedia.org>
2015-01-14 00:53:32 -0800

    Memcached: make remaining app servers use UNIX domain socket
    Change-Id: Ic5c10915bec9640832b27173e5ecf049be2168ee

--- wmf-config/mc.php
+++ wmf-config/mc.php
@@ -1,32 +1,29 @@
 <?php
 # WARNING: This file is publically viewable on the web. Do not put private data here.
 $wgMainCacheType = 'memcached-pecl';
-
 $wgMemCachedPersistent = false;
 $wgMemCachedTimeout = 0.25 * 1e6;  // 250ms
 
 $wgObjectCaches['memcached-pecl'] = array(
-	'class'      => 'MemcachedPeclBagOStuff',
-	'serializer' => 'php',
-	'persistent' => false,
-	'servers'    => ( substr( gethostname(), 0, 4 ) === "mw12" )
-		? array( '/var/run/nutcracker/nutcracker.sock:0' )
-		: array( '127.0.0.1:11212' ),
+	'class'                => 'MemcachedPeclBagOStuff',
+	'serializer'           => 'php',
+	'persistent'           => false,
+	'servers'              => array( '/var/run/nutcracker/nutcracker.sock:0' ),
 	'server_failure_limit' => 1e9,
-	'retry_timeout' => -1
+	'retry_timeout'        => -1,
 );
 
 $wgBloomFilterStores['main'] = array(
 	'cacheId'      => 'main-v1',
 	'class'        => 'BloomCacheRedis',
 	'redisServers' => array(
-		'10.64.0.162:6379', // master; rbf1001
-		'10.64.0.163:6379' // slave; rbf1002
+		'10.64.0.162:6379', // rbf1001 - master
+		'10.64.0.163:6379', // rbf1002 - slave
 	),
 	'redisConfig'  => array(
-		'password' => $wmgRedisPassword,
-		'connectTimeout' => .25
-	)
+		'password'       => $wmgRedisPassword,
+		'connectTimeout' => .25,
+	),
 );
 
 # vim: set sts=4 sw=4 et :
