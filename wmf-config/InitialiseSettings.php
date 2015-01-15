1b7fbeca72566e5f7071b26635b0c2db444ad953
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2015-01-15 13:53:21 +0200

    Consistent hyphen in beta features whitelist
    Change-Id: Ic143ce4d645d3c8b8202a8805b028b82f69cc809

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -11337,9 +11337,9 @@ $wgConf->settings = array(
 		'uls-compact-links',                        // 2014-09-06 – Compact language links
 		'popups',                                   // 2014-09-18 – Hovercards
 		'betafeatures-vector-compact-personal-bar', // 2014-11-01 – Compact personal bar
-		'visualeditor-enable-language',             // 2014-11-01 — VE language editor
-		'wikibase-otherprojects',                   // 2015-02-27 — Other projects sidebar (wikidata)
-		'cx',                                       // 2015-07-14 _ Content translation
+		'visualeditor-enable-language',             // 2014-11-01 - VE language editor
+		'wikibase-otherprojects',                   // 2015-02-27 - Other projects sidebar (Wikidata)
+		'cx',                                       // 2015-07-14 - Content translation
 	),
 ),
 
