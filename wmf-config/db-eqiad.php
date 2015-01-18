7f57c3c622cb7fbf9a795837fab71f9e3d81af82
Sean Pringle <springle@wikimedia.org>
2015-01-19 05:33:35 +1000

    depool db1054
    Change-Id: I1afbe1a98df84d45c5c5757783e929b2a8cc5fef

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -101,7 +101,7 @@ $wgLBFactoryConf = array(
 		'db1024' => 0,   # 1.4TB  64GB
 		'db1018' => 0,   # 1.4TB  64GB, snapshot, dump, vslow
 		'db1036' => 0,   # 1.4TB  64GB, watchlist, recentchangeslinked, contributions, logpager
-		'db1054' => 200, # 2.8TB  96GB, api
+		# upgrade 'db1054' => 200, # 2.8TB  96GB, api
 		'db1060' => 200, # 2.8TB  96GB, api
 		'db1063' => 500, # 2.8TB 128GB
 		'db1067' => 500, # 2.8TB 160GB
@@ -173,7 +173,7 @@ $wgLBFactoryConf = array(
 			'db1018' => 1,
 		),
 		'api' => array(
-			'db1054' => 1,
+			# upgrade 'db1054' => 1,
 			'db1060' => 1,
 		),
 		'watchlist' => array(
