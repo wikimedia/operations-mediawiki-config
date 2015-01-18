wikimedia/operations-mediawiki-config

commit bc4e9dded2eb3354b22f6cbb33ced13a6a828c2b

Content Translation configuration for Production
Also, removes duplicate config from -labs.

Bug: T85144 T78775
Change-Id: I8165c4f0ae148546f140d9240b7fbd589d43f607

wmf-config/InitialiseSettings.php
@@ -13778,6 +13778,28 @@ $wgConf->settings = array(
 	'wikivoyage' => true,
 ),
 
+'wmgUseContentTranslationEventLogging' => array(
+	'default' => true,
+),
+
+'wmgContentTranslationCluster' => array(
+	'default' => 'extension1',
+),
+
+'wmgUseContentTranslation' => array(
+	'default' => false,
+	'cawiki' => true,
+	'dawiki' => true,
+	'eowiki' => true,
+	'eswiki' => true,
+	'idwiki' => true,
+	'mswiki' => true,
+	'nnwiki' => true,
+	'nowiki' => true,
+	'ptwiki' => true,
+	'svwiki' => true,
+),
+
 'wmgUseMapSources' => array(
 	'default' => false,
 	'wikivoyage' => true,

wmf-config/CommonSettings-labs.php
@@ -88,22 +88,8 @@ if ( $wmgUseFlow ) {
 }
 
 if ( $wmgUseContentTranslation ) {
-	require_once "$IP/extensions/ContentTranslation/ContentTranslation.php";
 	$wgContentTranslationSiteTemplates['cx'] = 'https://cxserver-beta.wmflabs.org';
-	// Used for html2wikitext when publishing
-	$wgContentTranslationParsoid = array(
-		'url' => $wmgParsoidURL,
-		'timeout' => 10000,
-		'prefix' => $wgDBname,
-	);
-
-	$wgContentTranslationEventLogging = $wmgContentTranslationEventLogging;
-
-	if ( $wmgContentTranslationCluster ) {
-		$wgContentTranslationCluster = $wmgContentTranslationCluster;
-	}
-
-	$wgContentTranslationDatabase = 'wikishared';
+	$wgContentTranslationTranslateInTarget = false;
 }
 
 if ( $wmgUseCentralNotice ) {

wmf-config/CommonSettings.php
@@ -2716,6 +2716,31 @@ if ( $wmgUseUniversalLanguageSelector ) {
 	}
 }
 
+if ( $wmgUseContentTranslation ) {
+	require_once "$IP/extensions/ContentTranslation/ContentTranslation.php";
+	// T76200: Public URL for cxserver instance
+	$wgContentTranslationSiteTemplates['cx'] = 'http://cxserver.wikimedia.org';
+	// Used for html2wikitext when publishing
+	$wgContentTranslationParsoid = array(
+		'url' => $wmgParsoidURL,
+		'timeout' => 10000,
+		'prefix' => $wgDBname,
+	);
+
+	$wgContentTranslationTranslateInTarget = true;
+
+	// Namespace to publish article
+	$wgContentTranslationTargetNamespace = 'User';
+
+	$wgContentTranslationEventLogging = $wmgContentTranslationEventLogging;
+
+	if ( $wmgContentTranslationCluster ) {
+		$wgContentTranslationCluster = $wmgContentTranslationCluster;
+	}
+
+	$wgContentTranslationDatabase = 'wikishared';
+}
+
 // @note getRealmSpecificFilename only works with filenames with .suffix
 // needs to be listed outside of use wikibase check below, as localisation cache
 // might be build as "aawikibooks" or something that does not have Wikibase.

wmf-config/InitialiseSettings-labs.php
@@ -362,10 +362,6 @@ return array(
 	    'default' => true,
 	),
 
-	'wmgContentTranslationEventLogging' => array(
-		'default' => true,
-	),
-
 	'wmgContentTranslationCluster' => array(
 		'default' => false,
 	),

commit 1befb6e4fb428a458c9be382cc97bd54d9cc4351

Re-enable event logging for cx
Of course we want logging for the feature we are deploying.

Error introduced in bc4e9dded2 and then disabled in I359e4c43222a
and nobody noticed until now (I was traveling).

Change-Id: I8a9f29c66494f3e5265a7bbac2f069d6bd87fce7

wmf-config/InitialiseSettings.php
@@ -13769,7 +13769,7 @@ $wgConf->settings = array(
 ),
 
 'wmgContentTranslationEventLogging' => array(
-	'default' => false,
+	'default' => true,
 ),
 
 'wmgContentTranslationCluster' => array(