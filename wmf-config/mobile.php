05dc3261c0db7bc4dd15a41b7d0d8e125d084153
jdlrobson <jdlrobson@gmail.com>
2015-01-30 13:25:21 -0800

    Enable JS console recruitment on mobile.
    Bug: T85815
    Change-Id: I0be36f7f91a01037d640a682889e7ba8229086a4

--- wmf-config/mobile.php
+++ wmf-config/mobile.php
@@ -136,4 +136,7 @@ if ( $wmgMobileFrontend ) {
 		$wgMFWikiGrokAbTestStartDate = 1416272400; // Tue, 18 Nov 2014 01:00:00 GMT
 		$wgMFWikiGrokAbTestEndDate = 1417482000; // Tue, 02 Dec 2014 01:00:00 GMT
 	}
+
+	// Turn on volunteer recruitment
+	$wgMFEnableJSConsoleRecruitment = true;
 }
