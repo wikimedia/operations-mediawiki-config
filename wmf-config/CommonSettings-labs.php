ca7e39076a812733845d520bdc690033d6db6df3
Tony Thomas <01tonythomas@gmail.com>
2015-02-08 15:56:59 +0530

    Un-subscribe frequently failing recipients
    Bounce threshold set to 2, to match mailman standards
    http://www.esosoft.com/support/mailinglist/mailman/bounce.html
    
    Bug: T48640
    Change-Id: I1dfef5406cfb389d4f9a36f5cc41ecfdae940dee

--- wmf-config/CommonSettings-labs.php
+++ wmf-config/CommonSettings-labs.php
@@ -150,7 +150,8 @@ if ( $wmgUseBounceHandler ) {
 	$wgBounceHandlerCluster = false;
 	$wgBounceHandlerSharedDB = false;
 	$wgBounceHandlerInternalIPs = array( '127.0.0.1', '::1', '10.68.17.78' ); //deployment-mx.wmflabs.org
-	$wgBounceHandlerUnconfirmUsers = false;
+	$wgBounceHandlerUnconfirmUsers = true;
+	$wgBounceRecordLimit = 2;
 	$wgVERPdomainPart = 'beta.wmflabs.org';
 }
 
