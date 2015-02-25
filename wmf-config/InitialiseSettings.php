7ef159006460e26fd59706a9e1a8d4d8996d908c
Kartik Mistry <kartik.mistry@gmail.com>
2015-02-25 11:11:54 +0530

    Enable Content Translation in minwiki and uzwiki
    Default publishing option for minwiki is Main.
    
    Bug: T88037 T87163
    Change-Id: I2396f292f7d9efd7a2ecdf6578fd0d2da2834a07

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -14200,11 +14200,13 @@ $wgConf->settings = array(
 	'eowiki' => true,
 	'eswiki' => true,
 	'idwiki' => true,
+	'minwiki' => true,
 	'mswiki' => true,
 	'nnwiki' => true,
 	'nowiki' => true,
 	'ptwiki' => true,
 	'svwiki' => true,
+	'uzwiki' => true,
 	'testwiki' => true,
 ),
 
@@ -14213,6 +14215,7 @@ $wgConf->settings = array(
 	'default' => 'User',
 	'cawiki' => 'Main',
 	'idwiki' => 'Main', // T89450
+	'minwiki' => 'Main',
 	'ptwiki' => 'Main', // T89450
 ),
 
