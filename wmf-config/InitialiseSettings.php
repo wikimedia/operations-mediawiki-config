5e33df524e45ab3a2d4b66e3665e36117aad64e1
glaisher <glaisher.wiki@gmail.com>
2015-02-27 22:45:42 +0500

    Enable WikiLove extension at newiki
    Please create the necessary database tables with this
    
    Bug: T89818
    Change-Id: Ieb76cae4c5acbd0e60037f30877e6432a7b33f37

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -12581,6 +12581,7 @@ $wgConf->settings = array(
 	'mediawikiwiki' => true,
 	'mkwiki' => true, // bug 31831
 	'mlwiki' => true, // bug 30500
+	'newiki' => true, // T89818
 	'nowiki' => true, // bug 30794
 	'officewiki' => true,
 	'orwiki' => true, // bug 31172
