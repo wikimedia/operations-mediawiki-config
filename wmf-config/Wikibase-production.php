3a707dd73f27f897ddf29afc09240e244bcb7790
aude <aude.wiki@gmail.com>
2015-03-02 13:00:23 +0100

    Add badge items for beta
    To do this, added realm-specific configuration for
    labs and production where badge config can go.
    
    Change-Id: I1f396c2dd32ab8b4989fedae9073706ed96bd58d

---
+++ wmf-config/Wikibase-production.php
@@ -0,0 +1,30 @@
+<?php
+
+if ( $wmgUseWikibaseRepo ) {
+	if ( $wgDBname === 'testwikidatawiki' ) {
+		$wgWBRepoSettings['badgeItems'] = array(
+			'Q608' => 'wb-badge-goodarticle',
+			'Q609' => 'wb-badge-featuredarticle'
+		);
+	} else {
+		$wgWBRepoSettings['badgeItems'] = array(
+			'Q17437798' => 'wb-badge-goodarticle',
+			'Q17437796' => 'wb-badge-featuredarticle',
+			'Q17559452' => 'wb-badge-recommendedarticle', // bug 70268
+			'Q17506997' => 'wb-badge-featuredlist', // bug 70332
+			'Q17580674' => 'wb-badge-featuredportal', // bug 73193
+		);
+	}
+}
+
+if ( $wmgUseWikibaseClient ) {
+	$wgWBClientSettings['badgeClassNames'] = array(
+		'Q17437796' => 'badge-featuredarticle',
+		'Q17437798' => 'badge-goodarticle',
+		'Q17559452' => 'badge-recommendedarticle', // bug 70268
+		'Q17506997' => 'badge-featuredlist', // bug 70332
+		'Q17580674' => 'badge-featuredportal', // bug 73193
+	);
+}
+
+require( getRealmSpecificFilename( "$wmfConfigDir/Wikibase.php" ) );
