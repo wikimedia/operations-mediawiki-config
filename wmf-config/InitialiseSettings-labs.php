9057b813189988a330f0d55274e5f45c3ad16c8e
Kartik Mistry <kartik.mistry@gmail.com>
2015-03-02 21:30:39 +0530

    CX: Publish translations to the Main namespace by default
    wmgContentTranslationTargetNamespace is set to Main namespace as default
    now.
    
    Also, remove variable from Beta as it is not needed.
    
    Bug: T76618 T91256
    Change-Id: If6dd723bc5cc9c1dc70ba07f2692f94bb4346b05

--- wmf-config/InitialiseSettings-labs.php
+++ wmf-config/InitialiseSettings-labs.php
@@ -407,10 +407,6 @@ return array(
 		'default' => false,
 	),
 
-	'wmgContentTranslationTargetNamespace' => array(
-		'default' => 'Main',
-	),
-
 	'wmgUseNavigationTiming' => array(
 		'default' => true,
 	),
