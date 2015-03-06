261c8617c4e9c9e98f8546085706dd2f52678564
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2015-03-06 09:00:37 +0100

    mediawiki: add configs to support the Dallas DC
    We do add support for the second production datacenter. Specifically we
    do the following:
    
    - Add codfw database configurations, for now with eqiad masters
    - Add redis sessions configuration
    - Add redis and poolcounter configs as mere copies of what we have in eqiad for now
    
    Bug: T91754
    
    Change-Id: I7cc7a2991a7f7cfeb8b3c3b71f38756990f5d949

---
+++ wmf-config/jobqueue-codfw.php
@@ -0,0 +1,52 @@
+<?php
+# WARNING: This file is publically viewable on the web. Do not put private data here.
+
+// Note: on server failure, partition masters should be switched to the slave
+// Note: MediaWiki will fail-over to other shards when one is down. On master
+// failure, it is best to either do nothing or just disable the whole shard
+// until the master is fixed or full fail-over is done. Proper fail over
+// requires changing the slave to stop slaving the old master before updating
+// the MediaWiki config to direct traffic there.
+$wgJobTypeConf['default'] = array(
+	'class'               => 'JobQueueFederated',
+	'configByPartition'   => array(
+		'rdb1' => array(
+			'class'       => 'JobQueueRedis',
+			'redisServer' => '10.64.32.76', # rdb1001 (master)
+			#'redisServer' => '10.64.32.77', # rdb1002 (slave)
+			'redisConfig' => array(
+				'connectTimeout' => 2,
+				'password' => $wmgRedisPassword,
+				'compression' => 'gzip'
+			),
+			'daemonized' => true
+		),
+		'rdb2' => array(
+			'class'       => 'JobQueueRedis',
+			'redisServer' => '10.64.0.201', # rdb1003 (master)
+			#'redisServer' => '10.64.16.183', # rdb1004 (slave)
+			'redisConfig' => array(
+				'connectTimeout' => 2,
+				'password' => $wmgRedisPassword,
+				'compression' => 'gzip'
+			),
+			'daemonized' => true
+		),
+	),
+	'sectionsByWiki'      => array(), // default
+	'partitionsBySection' => array(
+		'default' => array( 'rdb1' => 50, 'rdb2' => 50 ),
+	)
+);
+// Note: on server failure, this should be changed to any other redis server
+$wgJobQueueAggregator = array(
+	'class'        => 'JobQueueAggregatorRedis',
+	'redisServers' => array( // all after the first are fallbacks
+		'10.64.32.76', # rdb1001
+		'10.64.0.201', # rdb1003
+	),
+	'redisConfig'  => array(
+		'connectTimeout' => 2,
+		'password' => $wmgRedisPassword,
+	)
+);
