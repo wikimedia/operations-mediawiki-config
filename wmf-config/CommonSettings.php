be571117b6fcecfe1692a806e60641b452140edc
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2015-03-12 10:58:18 +0100

    imagescaling: remove legacy option for HHVM imagescalers
    On ubuntu trusty, a newer version of librsvg is used, which supports by
    default a security model where external entities are loaded only from
    the current working directory.
    
    Our SVG conversion code takes this into account
    
    https://phabricator.wikimedia.org/diffusion/MW/browse/master/includes/media/SVG.php;97c4e93eefc7182fb4a166e752d44e8853fe3218$202
    
    so we can make the arguments of rsvg-convert conditional on the presence
    of HHVM.
    
    Bug: T84842
    Change-Id: I5d8e8a196bcab6a394af98191332b88dd09f6526

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -435,9 +435,13 @@ if ( $wmgPrivateWikiUploads ) {
 
 # Hack for rsvg broken by security patch
 $wgSVGConverters['rsvg-broken'] = '$path/rsvg-convert -w $width -h $height -o $output < $input';
-# This converter will only work when rsvg has a suitable security patch
-$wgSVGConverters['rsvg-secure'] = '$path/rsvg-convert --no-external-files -w $width -h $height -o $output $input';
-
+if ( defined( 'HHVM_VERSION' ) ) {
+	# Newer librsvg supports a sane security model by default and doesn't need our security patch
+	$wgSVGConverters['rsvg-secure'] = '$path/rsvg-convert -w $width -h $height -o $output $input';
+} else {
+	# This converter will only work when rsvg has a suitable security patch
+	$wgSVGConverters['rsvg-secure'] = '$path/rsvg-convert --no-external-files -w $width -h $height -o $output $input';
+}
 #######################################################################
 # Squid Configuration
 #######################################################################
