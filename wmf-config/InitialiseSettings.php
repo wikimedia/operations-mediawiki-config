6f6a87fc8fe5c7419b5fb84773b9c0a56b1002a5
James D. Forrester <jforrester@wikimedia.org>
2015-03-17 19:18:07 -0700

    RESTbase production enablement step 3 – frwiki
    Change-Id: If6ac939cec40dab990204a836f0a9a1d8160a0d5

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -12255,6 +12255,7 @@ $wgConf->settings = array(
 	'testwiki' => true,
 	'test2wiki' => true,
 	'mediawikiwiki' => true,
+	'frwiki' => true,
 	'itwiki' => true,
 	'plwiki' => true,
 	'ptwiki' => true,
