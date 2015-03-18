981822ebedc2f76038748e15d4595a163cc7401b
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2015-03-18 10:48:59 +0100

    memcached: add configurations for codfw
    Bug: T91754
    Change-Id: I360c1272642ed1743d6f82d5b17ddc3f59bda3c8

---
+++ wmf-config/mc-codfw.php
@@ -0,0 +1,32 @@
+<?php
+# WARNING: This file is publically viewable on the web. Do not put private data here.
+$wgMainCacheType = 'memcached-pecl';
+$wgMemCachedPersistent = false;
+$wgMemCachedTimeout = 0.25 * 1e6;  // 250ms
+
+$wgObjectCaches['memcached-pecl'] = array(
+	'class'                => 'MemcachedPeclBagOStuff',
+	'serializer'           => 'php',
+	'persistent'           => false,
+	'servers'              => defined( 'HHVM_VERSION' )
+		? array( '/var/run/nutcracker/nutcracker.sock:0' )
+		: array( '127.0.0.1:11212' ),
+	'server_failure_limit' => 1e9,
+	'retry_timeout'        => -1,
+	'loggroup'             => 'memcached',
+);
+
+$wgBloomFilterStores['main'] = array(
+	'cacheId'      => 'main-v1',
+	'class'        => 'BloomCacheRedis',
+	'redisServers' => array(
+		'10.192.0.33:6379', // rbf2001 - master
+		'10.192.16.36:6379', // rbf2002 - slave
+	),
+	'redisConfig'  => array(
+		'password'       => $wmgRedisPassword,
+		'connectTimeout' => .25,
+	),
+);
+
+# vim: set sts=4 sw=4 et :
