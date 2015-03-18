795d909007a5f257db2eceaae9660a63f8b3e7e7
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2015-03-18 11:02:42 +0100

    filebackend: add configuration for codfw
    Bug: T91754
    Change-Id: Ibfca6285f4f2906a5a802fc3ec43bf530cee4cf4

---
+++ wmf-config/filebackend-codfw.php
@@ -0,0 +1,114 @@
+<?php
+
+# WARNING: This file is publically viewable on the web. Do not put private data here.
+
+/* Codfw Swift backend config */
+$wgFileBackends[] = array( // backend config for wiki's local repo
+	'class'              => 'SwiftFileBackend',
+	'name'               => 'local-swift-eqiad',
+	'wikiId'             => "{$site}-{$lang}",
+	'lockManager'        => 'redisLockManager',
+	'swiftAuthUrl'       => $wmfSwiftCodfwConfig['authUrl'],
+	'swiftUser'          => $wmfSwiftCodfwConfig['user'],
+	'swiftKey'           => $wmfSwiftCodfwConfig['key'],
+	'swiftTempUrlKey'    => $wmfSwiftCodfwConfig['tempUrlKey'],
+	'shardViaHashLevels' => array(
+		'local-public'     => array( 'levels' => $wmfSwiftShardLocal, 'base' => 16, 'repeat' => 1 ),
+		'local-thumb'      => array( 'levels' => $wmfSwiftShardLocal, 'base' => 16, 'repeat' => 1 ),
+		'local-temp'       => array( 'levels' => $wmfSwiftShardLocal, 'base' => 16, 'repeat' => 1 ),
+		'local-transcoded' => array( 'levels' => $wmfSwiftShardLocal, 'base' => 16, 'repeat' => 1 ),
+		'local-deleted'    => array( 'levels' => $wmfSwiftShardLocal, 'base' => 36, 'repeat' => 0 )
+	),
+	'parallelize'        => 'implicit',
+	'cacheAuthInfo'      => true
+);
+$wgFileBackends[] = array( // backend config for wiki's access to shared repo
+	'class'              => 'SwiftFileBackend',
+	'name'               => 'shared-swift-eqiad',
+	'wikiId'             => "wikipedia-commons",
+	'lockManager'        => 'redisLockManager',
+	'swiftAuthUrl'       => $wmfSwiftCodfwConfig['authUrl'],
+	'swiftUser'          => $wmfSwiftCodfwConfig['user'],
+	'swiftKey'           => $wmfSwiftCodfwConfig['key'],
+	'swiftTempUrlKey'    => $wmfSwiftCodfwConfig['tempUrlKey'],
+	'shardViaHashLevels' => array(
+		'local-public'     => array( 'levels' => $wmfSwiftShardCommon, 'base' => 16, 'repeat' => 1 ),
+		'local-thumb'      => array( 'levels' => $wmfSwiftShardCommon, 'base' => 16, 'repeat' => 1 ),
+		'local-temp'       => array( 'levels' => $wmfSwiftShardCommon, 'base' => 16, 'repeat' => 1 ),
+		'local-transcoded' => array( 'levels' => $wmfSwiftShardCommon, 'base' => 16, 'repeat' => 1 ),
+	),
+	'parallelize'        => 'implicit',
+	'cacheAuthInfo'      => true
+);
+$wgFileBackends[] = array( // backend config for wiki's access to shared files
+	'class'              => 'SwiftFileBackend',
+	'name'               => 'global-swift-eqiad',
+	'wikiId'             => "global-data",
+	'lockManager'        => 'redisLockManager',
+	'swiftAuthUrl'       => $wmfSwiftCodfwConfig['authUrl'],
+	'swiftUser'          => $wmfSwiftCodfwConfig['user'],
+	'swiftKey'           => $wmfSwiftCodfwConfig['key'],
+	'swiftTempUrlKey'    => $wmfSwiftCodfwConfig['tempUrlKey'],
+	'shardViaHashLevels' => array(
+		'math-render'  => array( 'levels' => 2, 'base' => 16, 'repeat' => 0 ),
+	),
+	'parallelize'        => 'implicit',
+	'cacheAuthInfo'      => true
+);
+/* end Codfw Swift backend config */
+
+/* Multiwrite backend config */
+$wgFileBackends[] = array(
+	'class'       => 'FileBackendMultiWrite',
+	'name'        => 'local-multiwrite',
+	'wikiId'      => "{$site}-{$lang}",
+	'lockManager' => 'redisLockManager',
+	'backends'    => array(
+		# DO NOT change the master backend unless it is fully trusted or autoRsync is off
+		array( 'template' => 'local-swift-eqiad', 'isMultiMaster' => true ),
+	),
+	'syncChecks'  => ( 1 | 4 ), // (size & sha1)
+	'autoResync'  => 'conservative' // bug 39221
+);
+$wgFileBackends[] = array(
+	'class'       => 'FileBackendMultiWrite',
+	'name'        => 'shared-multiwrite',
+	'wikiId'      => "wikipedia-commons",
+	'lockManager' => 'redisLockManager',
+	'backends'    => array(
+		# DO NOT change the master backend unless it is fully trusted or autoRsync is off
+		array( 'template' => 'shared-swift-eqiad', 'isMultiMaster' => true ),
+	),
+	'syncChecks'  => ( 1 | 4 ), // (size & sha1)
+	'autoResync'  => 'conservative' // bug 39221
+);
+$wgFileBackends[] = array(
+	'class'       => 'FileBackendMultiWrite',
+	'name'        => 'global-multiwrite',
+	'wikiId'      => "global-data",
+	'lockManager' => 'redisLockManager',
+	'backends'    => array(
+		# DO NOT change the master backend unless it is fully trusted or autoRsync is off
+		array( 'template' => 'global-swift-eqiad', 'isMultiMaster' => true ),
+	),
+	'syncChecks'  => ( 1 | 4 ), // (size & sha1)
+	'autoResync'  => 'conservative'
+);
+/* end multiwrite backend config */
+
+$wgLockManagers[] = array(
+	'name'         => 'redisLockManager',
+	'class'        => 'RedisLockManager',
+	'lockServers'  => array(
+		'rdb1' => '10.192.0.35',
+		'rdb2' => '10.192.16.38',
+		'rdb3' => '10.192.32.20'
+	),
+	'srvsByBucket' => array(
+		0 => array( 'rdb1', 'rdb2', 'rdb3' )
+	),
+	'redisConfig'  => array(
+		'connectTimeout' => 2,
+		'password'       => $wmgRedisPassword
+	)
+);
