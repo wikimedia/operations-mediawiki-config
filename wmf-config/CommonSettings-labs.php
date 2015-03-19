d6c90c23d2a41c29bd8069299ef1280ffa317ea2
Kartik Mistry <kartik.mistry@gmail.com>
2015-03-19 10:28:44 +0530

    config: Remove wgContentTranslationTranslateInTarget for existing wikis
    We need to check if wiki is in Beta or not and then set InTarget
    publishing to false, just like production.
    
    Change-Id: I75bb7a19df997bfa5fbd9423884c03cd2eaa1999

--- wmf-config/CommonSettings-labs.php
+++ wmf-config/CommonSettings-labs.php
@@ -92,8 +92,13 @@ if ( $wmgUseContentTranslation ) {
 	$wgContentTranslationSiteTemplates['cx'] = 'https://cxserver-beta.wmflabs.org/v1';
 	// $wmgParsoidURL from production is not accessible from Beta, so use public URL
 	$wgContentTranslationParsoid['url'] = 'http://parsoid-lb.eqiad.wikimedia.org';
-	$wgContentTranslationTranslateInTarget = false;
 	$wgContentTranslationCampaigns = $wmgContentTranslationCampaigns;
+
+	if ( $wgDBname == 'cawiki' || $wgDBname = 'dewiki' || $wgDBname = 'enwiki' ||
+		$wgDBname = 'eowiki' || $wgDBname = 'eswiki' || $wgDBname = 'jawiki'  ||
+		$wgDBname = 'ruwiki' || $wgDBname = 'ukwiki' || $wgDBname = 'zhwiki' ) {
+		$wgContentTranslationTranslateInTarget = false;
+	}
 }
 
 if ( $wmgUseCentralNotice ) {
