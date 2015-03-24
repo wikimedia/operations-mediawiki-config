6eb419720a5fabb699354ccbd8f1f524437b5980
Niklas Laxström <niklas.laxstrom@gmail.com>
2015-03-24 15:26:08 +0100

    Add pool counter config for Translate
    Bug: T54728
    Change-Id: I089a9a7e4924ceddf13d4130e512605c022a0aa7

--- wmf-config/PoolCounterSettings-common.php
+++ wmf-config/PoolCounterSettings-common.php
@@ -52,6 +52,13 @@ $wgPoolCounterConf = array(
 		'slots' => 8,
 		'maxqueue' => 100
 	),
+	'TranslateFetchTranslators' => array(
+		'class' => 'PoolCounter_Client',
+		'timeout' => 8,
+		'workers' => 2,
+		'slots' => 8,
+		'maxqueue' => 20,
+	),
 );
 
 require( getRealmSpecificFilename( "$wmfConfigDir/PoolCounterSettings.php" ) );
