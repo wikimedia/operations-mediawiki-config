b75bd45ed7b2df2dc3b125f11fe3f5ad5ecd68b3
James D. Forrester <jforrester@wikimedia.org>
2015-03-26 19:35:07 -0700

    Make VisualEditor access RESTbase directly in group0
    Bug: T90374
    Change-Id: I74a63bc028fdf098dad69437532adfb48bc533ed

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -12323,6 +12323,12 @@ $wgConf->settings = array(
 	'svwiktionary' => true, # Per community request
 ),
 
+// Whether VisualEditor should bypass the MediaWiki layer and contact RESTbase directly for speed
+'wmgVisualEditorAccessRESTbaseDirectly' => array(
+	'default' => false,
+	'group0' => true,
+),
+
 // Namespaces for VisualEditor to be active in, as well as wgContentNamespaces
 // NS_VISUALEDITOR is added in CommonSettings.php if wmgUseVisualEditorNamespace is true
 'wmgVisualEditorNamespaces' => array(
