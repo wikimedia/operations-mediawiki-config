ba821460c2fc7df64f2dcd3b93bfdc9c414d3878
Chad Horohoe <chadh@wikimedia.org>
2015-04-02 10:40:30 -0700

    Remove labs config of swift backups. Testing done
    Change-Id: Icc8a12103de36da36bcc2ef0f9f210665e5672df

--- wmf-config/CirrusSearch-labs.php
+++ wmf-config/CirrusSearch-labs.php
@@ -12,15 +12,6 @@ $wgCirrusSearchServers = array(
 	'deployment-elastic08',
 );
 
-$wgCirrusSearchBackup['backups'] = array(
-	'type' => 'swift',
-	'swift_url' => 'http://deployment-saio:8080/auth/v1.0',
-	'swift_container' => 'global-data-elastic-backups',
-	'swift_username' => 'test:tester',
-	'swift_password' => 'testing',
-	'max_snapshot_bytes_per_sec' => '10mb',
-);
-
 if ( $wgDBname == 'enwiki' ) {
 	$wgCirrusSearchInterwikiSources = array(
 		'wiktionary' => 'enwiktionary',
