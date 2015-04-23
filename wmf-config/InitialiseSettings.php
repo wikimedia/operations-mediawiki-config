2871b868b5ed313f39d8d2663a84654081d8139d
Gabriel Wicke <gwicke@wikimedia.org>
2015-04-23 16:01:18 -0700

    Load HTML directly from RESTBase for enwiki
    Bug: T95229
    Change-Id: I7ddf22d8c552473f8489a36e39df945155b7b243

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -12593,6 +12593,7 @@ $wgConf->settings = array(
 	'testwiki' => true,
 	'test2wiki' => true,
 	'testwikidatawiki' => true,
+	'enwiki' => true,
 ),
 
 // Namespaces for VisualEditor to be active in, as well as wgContentNamespaces
