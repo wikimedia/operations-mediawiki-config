fed96e6e4f89d2cddfb4b5c44dc8b9313b94325d
Sam Smith <git@samsmith.io>
2015-04-24 12:56:42 +0100

    Enable Browse experiment on labs
    Bug: T94739
    Change-Id: I12bf47f934c9bf144b2f8e10c883533e3ae1f0f0

--- wmf-config/mobile-labs.php
+++ wmf-config/mobile-labs.php
@@ -26,3 +26,15 @@ $wgMFSpecialCaseMainPage = $wmgMFSpecialCaseMainPage;
 $wgMFWikiDataEndpoint = $wmgMFWikiDataEndpoint;
 $wgWikiBasePropertyConfig = $wmgWikiBasePropertyConfig;
 $wgMFInfoboxConfig = $wmgMFInfoboxConfig;
+
+$wgMFIsBrowseEnabled = true;
+$wgMFBrowseTags = array(
+	"Category:National_Basketball_Association_All-Stars" => "NBA All Stars",
+	"Category:20th-century_American_politicians" => "American politicians",
+	"Category:Object-oriented_programming_languages" => "object-oriented programming languages",
+	"Category:Western_Europe" => "European states",
+	"Category:American_female_pop_singers" => "American female pop singers",
+	"Category:American_drama_television_series" => "American drama TV series",
+	"Category:Modern_painters" => "modern painters",
+	"Category:Landmarks_in_San_Francisco,_California" => "landmarks in San Francisco, California",
+);
