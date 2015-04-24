c3fc707e43fca6d423419e3a2389021c72f91850
Catrope <roan.kattouw@gmail.com>
2015-04-24 00:38:35 +0000

    Revert "Use /api/rest_v1/ entry point for VE"
    This breaks subpages, because /Foo%2FBar gets decoded to
    /Foo/Bar and RESTbase then complains that "Bar" is not a
    valid revision ID.
    
    This reverts commit 73e6f126bda7b7c360bd98f9b9588a78b54656a8.
    
    Change-Id: Ib5cc3dd137a32a098d3f119c36e989b4a120dbc9

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -12565,6 +12565,7 @@ $wgConf->settings = array(
 	'testwiki' => true,
 	'test2wiki' => true,
 	'testwikidatawiki' => true,
+	'enwiki' => true,
 ),
 
 // Namespaces for VisualEditor to be active in, as well as wgContentNamespaces
