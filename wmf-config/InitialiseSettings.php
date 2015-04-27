89a9b21f753c5401c382fa5d01355b8671fd8776
Sébastien Santoro <dereckson@espace-win.org>
2015-04-27 11:24:55 +0000

    Enable Graph extension on se.wikimedia
    Bug: T97027
    Change-Id: I83980a612a45d52ede43d4eea12da873b20a5cd8

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -12976,6 +12976,9 @@ $wgConf->settings = array(
 	'metawiki' => true,
 	'outreachwiki' => true,
 	'zerowiki' => true,
+
+	// Chapters
+	'sewikimedia' => true, // T97027
 ),
 
 'wmgUseGraphWithNamespace' => array(
