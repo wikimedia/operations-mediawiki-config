75df1533690659729b6974de5fc28f1093cb3c6e
Anomie <bjorsch@wikimedia.org>
2015-04-29 16:21:03 +0000

    Revert "Load HTML directly from RESTBase on all wikipedias"
    This reverts commit 2964c895bca89cd36dbac7625fecbd49c39166ad.
    
    Change-Id: Iad026ba98e1ec99d3b9b3c2502bc04052a04caf0

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -12597,7 +12597,7 @@ $wgConf->settings = array(
 	'testwiki' => true,
 	'test2wiki' => true,
 	'testwikidatawiki' => true,
-	'wikipedia' => true,
+	'enwiki' => true,
 ),
 
 // Namespaces for VisualEditor to be active in, as well as wgContentNamespaces
