3fc5e8ed6e2697c6d49697609b13b5ba5d0bb748
paladox <thomasmulhall410@yahoo.com>
2015-04-30 12:26:47 -0700

    Rename all main WikimediaIncubator settings to have a wg prefix
    eg. $wmincClosedWikis to $wgWmincClosedWikis
    
    Requires I22defb9431d0708d00d8294efc44cc8cf287fbcd
    
    Change-Id: I12ae377cb43d7c6100d10f0f203dd58795abf702

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -2102,7 +2102,7 @@ if ( $wmgUseDisableAccount ) {
 
 if ( $wmgUseIncubator ) {
 	require_once( "$IP/extensions/WikimediaIncubator/WikimediaIncubator.php" );
-	$wmincClosedWikis = $wgSiteMatrixClosedSites;
+	$wgWmincClosedWikis = $wgSiteMatrixClosedSites;
 }
 
 if ( $wmgUseWikiLove ) {
