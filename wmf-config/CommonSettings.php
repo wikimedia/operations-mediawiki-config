22122fd06d7be190db34401dea55c61540b45f14
paladox <thomasmulhall410@yahoo.com>
2015-04-30 12:26:47 -0700

    Rename $wmincClosedWikis to $wgwmincClosedWikis
    Requires I8ee42ebe11aa0f25eb1719069c0a13d87da54cb9
    
    Change-Id: I12ae377cb43d7c6100d10f0f203dd58795abf702

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -2108,7 +2108,7 @@ if ( $wmgUseDisableAccount ) {
 
 if ( $wmgUseIncubator ) {
 	require_once( "$IP/extensions/WikimediaIncubator/WikimediaIncubator.php" );
-	$wmincClosedWikis = $wgSiteMatrixClosedSites;
+	$wgwmincClosedWikis = $wgSiteMatrixClosedSites;
 }
 
 if ( $wmgUseWikiLove ) {
