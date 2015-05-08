730fdd6948767bd75bf1fad6a02069aa13822a11
Bryan Davis <bd808@wikimedia.org>
2015-05-08 17:26:28 -0600

    beta: switch $wmfUdp2logDest to deployment-fluorine.eqiad.wmflabs
    Bug: T98289
    Change-Id: Iae5d73c4e9ae6b6e74d0f1e04ba004e50134ae57

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -90,8 +90,8 @@ case 'production':
 	$wmfUdp2logDest = '10.64.0.21:8420';
 	break;
 case 'labs':
-	# deployment-bastion.eqiad.wmflabs
-	$wmfUdp2logDest = '10.68.16.58:8420';
+	# deployment-fluorine.eqiad.wmflabs
+	$wmfUdp2logDest = '10.68.16.198:8420';
 	break;
 default:
 	$wmfUdp2logDest = '127.0.0.1:8420';
