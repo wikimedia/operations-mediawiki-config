5f4ea053c35bf914e0f70e7bc35652621d11b6cf
Erik Bernhardson <ebernhardson@wikimedia.org>
2015-05-12 16:33:37 -0700

    Enable the CirrusSearch per-user pool counter everywhere
    Bug: T76497
    Change-Id: Ia4f6d5bcfbc014527f5b4cbd2a51208711c77d87

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -15083,13 +15083,6 @@ $wgConf->settings = array(
 	'default' => true,
 ),
 
-'wmgCirrusPerUserPoolCounter' => array(
-	'default' => false,
-	'testwiki' => true,
-	'test2wiki' => true,
-	'mediawikiwiki' => true,
-),
-
 'wmgCirrusSearchPreferRecentDefaultDecayPortion' => array(
 	'default' => 0,
 	'wikinews' => .6,
