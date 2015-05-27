55a2a128a917ffd3c7af221eb5d5bdb641503cf5
jcrespo <jcrespo@wikimedia.org>
2015-05-27 19:49:04 +0200

    Repool pc1002 after maintenance
    Buffer pool has been warmed
    
    Change-Id: If20df29086444feb68711f6fde543a0972fdd446
    References: T100301

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -9,7 +9,7 @@ if ( !defined( 'DBO_DEFAULT' ) ) {
 
 $wmgParserCacheDBs = array(
 	'10.64.16.156',  # pc1001
-#	'10.64.16.157',  # pc1002 (down for maintenance)
+	'10.64.16.157',  # pc1002
 	'10.64.16.158',  # pc1003
 );
 
