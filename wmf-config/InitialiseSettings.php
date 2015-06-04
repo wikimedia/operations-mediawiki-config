f57d4cafb0fb2c0344c5a6783bbce2be16128361
James D. Forrester <jforrester@wikimedia.org>
2015-06-04 09:37:53 -0700

    Hovercards: Disable test release on Catalan and Greek Wikipedias
    To go out on 2015-06-18, following the two month test release.
    
    Bug: T92555
    Change-Id: I56e6766f27b73e1aa3270a10473b73a7297f4ea6

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -11790,8 +11790,6 @@ $wgConf->settings = array(
 
 'wmgUsePopups' => array(
 	'default' => false,
-	'cawiki' => true,  // T88164
-	'elwiki' => true,  // T88164
 ),
 
 'wmgPopupsBetaFeature' => array(
