18b041633438ab3010cacdbe320afa4ca06e52ab
jcrespo <jcrespo@wikimedia.org>
2015-06-08 16:19:52 +0200

    Depool es1005
    Change-Id: Ib3fc3d2dcf40c228220001caa1c417d4708cb85f
    References: T101084

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -482,7 +482,7 @@ $wgLBFactoryConf = array(
 	# es2
 	'cluster24' => array(
 		'10.64.16.154' => 1, # es1006, master
-		'10.64.16.153' => 3, # es1005
+#		'10.64.16.153' => 3, # es1005 (down for maintenance)
 		'10.64.32.17'  => 3, # es1007
 	),
 	# es3
