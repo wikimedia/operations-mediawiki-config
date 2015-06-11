1b5fccd1f32e2308d94f5e846087d460a56e1eab
Gabriel Wicke <gwicke@wikimedia.org>
2015-06-11 12:53:56 -0700

    Use RESTBase as the VRS backend for all public wikis
    Change-Id: I9cec836929ba120f316e58f1ab2e68ce48dccc37

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -12581,11 +12581,9 @@ $wgConf->settings = array(
 // whether to configure RESTBase as a Virtual REST Service
 // in MW Core
 'wmgUseRestbaseVRS' => array(
-	'default' => false,
-	'wikipedia' => true,
-	'testwiki' => true,
-	'test2wiki' => true,
-	'mediawikiwiki' => true,
+	'default' => true,
+	'labswiki' => false, # T102178
+	'private' => false,
 ),
 
 // -------------- RESTBase end --------------
