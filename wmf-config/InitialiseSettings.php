ed339be81b9d0abf2cb60f7534d5aefbf7b2b23e
Brandon Black <bblack@wikimedia.org>
2015-06-18 21:59:17 +0000

    No need for wgSecureLogin on our wikis, HTTPS is forced everywhere
    Bug: T103021
    Change-Id: Ie55d8e030ae0434f91f27fe466a1a44472b519ba

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -15361,10 +15361,6 @@ $wgConf->settings = array(
 	'test2wiki' => false, // T67011
 ),
 
-'wgSecureLogin' => array(
-	'default' => true,
-),
-
 'wgRightsUrl' => array(
 	'default' => '//creativecommons.org/licenses/by-sa/3.0/',
 	'huwikinews' => '//creativecommons.org/licenses/by/3.0/',
