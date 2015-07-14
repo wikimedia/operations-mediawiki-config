f709f9cb9e4e3a6820004d837d336b2438ee87e5
Ori Livneh <ori@wikimedia.org>
2015-07-13 21:45:48 -0700

    Follow-up for Ieb62ee050e: allow LCStoreStaticArray in server mode
    Use LCStoreStaticArray if a magic file is present (/etc/lcstore) and if the
    l10n cache appears to be populated.
    
    Change-Id: I6068ff42ab7001aa55bdde243efd9a2863f5dfee

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -230,16 +230,9 @@ $wgLocalisationCacheConf['storeDirectory'] = "$IP/cache/l10n";
 $wgLocalisationCacheConf['manualRecache'] = true;
 
 // Temporary hack to facilitate migration of l10n cache implementations.
-// Use LCStoreStaticArray if a magic file is present and if one or more
-// of the following is also true:
-// - The cache of PHP files appears to be populated.
-// - The SAPI is CLI (to allow rebuildLocalisationCache to run).
-//
-// -- Ori, 2015-07-13
-if (
-	file_exists( '/etc/lcstore' )
-	&& ( PHP_SAPI === 'cli' || file_exists( "$IP/cache/l10n/zu.l10n.php" ) )
-) {
+// Use LCStoreStaticArray if a magic file is present and if the l10n cache
+// appears to be populated. -- Ori, 2015-07-13
+if ( file_exists( '/etc/lcstore' ) && file_exists( "$IP/cache/l10n/zu.l10n.php" ) ) {
 	$wgLocalisationCacheConf['storeClass'] = 'LCStoreStaticArray';
 }
 
