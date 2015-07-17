5b05a618a64271a59a38a26aa8cb5eb729026b52
Sean Pringle <springle@wikimedia.org>
2015-07-17 13:29:35 +1000

    repool db1030
    Change-Id: I04854cd34f74d969f49116fee792bcc7e96eda04

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -134,9 +134,9 @@ $wgLBFactoryConf = array(
 	),
 	's6' => array(
 		'db1023' => 0,   # 1.4TB  64GB
-		# T105879 'db1022' => 0,   # 1.4TB  64GB, vslow, dump
-		# upgrade 'db1030' => 0,   # 1.4TB  64GB
-		'db1037' => 0,   # 1.4TB  64GB, watchlist, recentchanges, contributions, logpager, vslow, dump
+		# T105879 'db1022' => 0,   # 1.4TB  64GB
+		'db1030' => 0,   # 1.4TB  64GB, vslow, dump
+		'db1037' => 0,   # 1.4TB  64GB, watchlist, recentchanges, contributions, logpager
 		'db1050' => 400, # 1.4TB  64GB
 		'db1061' => 500, # 2.8TB 128GB
 	),
@@ -298,10 +298,10 @@ $wgLBFactoryConf = array(
 	),
 	's6' => array(
 		'vslow' => array(
-			'db1037' => 1,
+			'db1030' => 1,
 		),
 		'dump' => array(
-			'db1037' => 1,
+			'db1030' => 1,
 		),
 		'watchlist' => array(
 			'db1037' => 1,
