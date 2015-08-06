84bbd2e549638800ef2d50d5bc24976ed79a0930
Ori.livneh <ori@wikimedia.org>
2015-08-06 22:57:26 +0000

    Revert "Use external diff, now that lightprocess is enabled"
    Haven't seen a positive change from this, and I prefer not shelling out
    if we can avoid it.
    
    This reverts commit 9ca969f8e39ea51f6c95845143a2f49295e67e3e.
    
    Change-Id: I02ffad60f748e22d5aad390b8487c028fd6fa071

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -1134,6 +1134,7 @@ if ( $wgDBname === 'enwiki' ) {
 
 if ( extension_loaded( 'wikidiff2' ) ) {
 	$wgExternalDiffEngine = 'wikidiff2';
+	$wgDiff = false;
 }
 
 $iwCacheFile = getRealmSpecificFilename( "$wmfConfigDir/interwiki.cdb" );
