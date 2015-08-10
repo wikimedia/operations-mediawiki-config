726cc84021e47d70e4c24678931201815068f614
jcrespo <jcrespo@wikimedia.org>
2015-08-10 10:45:30 +0200

    Depool db1042 for maintenance
    Change-Id: Iefdc325473b75ab1a7fcb49c4b461410e48f83b1

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -117,7 +117,8 @@ $wgLBFactoryConf = array(
 	),
 	's4' => array(
 		'db1040' => 0,   # 1.4TB  64GB
-		'db1042' => 0,   # 1.4TB  64GB, vslow, dump
+#		'db1042' => 0,   # 1.4TB  64GB, vslow, dump
+        #db1019 temporarily also vslow, dump
 		'db1019' => 0,   # 1.4TB  64GB, watchlist, recentchanges, contributions, logpager
 		'db1056' => 100, # 2.8TB  96GB, api
 		'db1059' => 100, # 2.8TB  96GB, api
@@ -245,10 +246,10 @@ $wgLBFactoryConf = array(
 	),
 	's4' => array(
 		'vslow' => array(
-			'db1042' => 1,
+			'db1019' => 1,
 		),
 		'dump' => array(
-			'db1042' => 1,
+			'db1019' => 1,
 		),
 		'api' => array(
 			'db1056' => 1,
