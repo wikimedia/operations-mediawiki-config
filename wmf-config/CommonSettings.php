0e07b324712e98122ca7e1b535968280bc18f50d
This, that and the other <at.light@live.com.au>
2015-08-12 23:01:49 +1000

    Allow import from any WMF project to any other
    It's been a long time coming, but here it is.
    
    The old wgImportSources configuration has been completely removed, as there
    is no need for it. In its place, we have a hook function
    "wmfImportSources" which lazy loads an importation list for the whole WMF
    cluster.
    
    Also adding a missing ) to the import user agent.
    
    IMPORTANT: DO NOT MERGE until the following REQUIRED dependencies are merged:
    * I68989203e367e7ea515e4ae2222c330b264a7cb1
    * Ice9a19cb6dfe53ae72aa71353d0553ee9338f233
    
    Bug: T17583
    Change-Id: Ib325df7c76837e1936b320d2a2441f10405090cd

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -1149,7 +1149,66 @@ $wgEnotifUseJobQ = true;
 include $IP . '/extensions/AntiSpoof/AntiSpoof.php';
 
 // For transwiki import
-ini_set( 'user_agent', 'Wikimedia internal server fetcher (noc@wikimedia.org' );
+ini_set( 'user_agent', 'Wikimedia internal server fetcher (noc@wikimedia.org)' );
+
+// Set up import sources for transwiki import
+function wmfImportSources( &$sources ) {
+	global $wgConf, $IP, $lang;
+	$wikipedias = array_flip( MWWikiversions::readDbListFile(
+		getRealmSpecificFilename( "$IP/../wikipedia.dblist" ) ) );
+	$privates = array_flip( MWWikiversions::readDbListFile(
+		getRealmSpecificFilename( "$IP/../private.dblist" ) ) );
+
+	// REMEMBER when editing this function, the values here are *interwiki prefixes*.
+	// Sometimes the interwiki map does things you don't expect.
+	// Look at dumpInterwiki.php in WikimediaMaintenance for guidance.
+
+	// Enforce a sensible order
+	$sources = array(
+		// Put really common special wikis first
+		'meta', 'commons', 'incubator',
+		'wikipedia' => array(),
+		'wiktionary' => array(),
+		'wikibooks' => array(),
+		'wikinews' => array(),
+		'wikiquote' => array(),
+		'wikisource' => array( 'oldwikisource' ),
+		'wikiversity' => array( 'betawikiversity' ),
+		'wikivoyage' => array(),
+		'chapter' => array(),
+		// Add a selection of non-private special wikis
+		'foundation', 'mediawikiwiki', 'nostalgia', 'outreach', 'strategy',
+		'tenwiki', 'testwiki', 'test2wiki', 'testwikidata', 'usability',
+		'wikidata', 'wikispecies', 'wikitech',
+		'wmania' => array(
+			'wm2005', 'wm2006', 'wm2007', 'wm2008', 'wm2009', 'wm2010', 'wm2011',
+			'wm2012', 'wm2013', 'wm2014', 'wm2015', 'wm2016',
+		),
+	);
+
+	// Add all regular language projects as import sources
+	foreach ( $wgConf->getLocalDatabases() as $dbname ) {
+		// No private wikis
+		if ( isset( $privates[$dbname] ) ) {
+			continue;
+		}
+
+		list( $project, $subdomain ) = $wgConf->siteFromDB( $dbname );
+		if ( $project === 'wikimedia' ) {
+			$sources['chapter'][] = $subdomain;
+		} elseif ( $subdomain === 'en' || $subdomain === $lang ) {
+			// Put $lang and en at the top for convenience
+			array_unshift( $sources[$project], $subdomain );
+		} elseif (
+			( $project !== 'wikipedia' || isset( $wikipedias[$dbname] ) ) && // Remove non-Wikipedias
+			$subdomain !== 'beta' // Beta Wikiversity is handled separately
+		) {
+			$sources[$project][] = $subdomain;
+		}
+	}
+}
+
+$wgHooks['ImportSources'][] = 'wmfImportSources';
 
 // CentralAuth
 if ( $wmgUseCentralAuth ) {
