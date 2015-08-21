04f5f8d5057ae7fd68d92873425e62835e45af19
Kunal Mehta <legoktm@gmail.com>
2015-08-21 10:57:38 -0700

    Load some more extensions directly through wfLoadExtension() (B-D)
    * ContributionTracking was skipped since it sounds fundraising-y
    * DoubleWiki has an extension.json, but isn't loading through it yet.
    
    Change-Id: Iac672617ecb7972c697b02739e6fc7fd9ff849a5

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -529,7 +529,7 @@ if ( $wmgUseParserFunctions ) {
 $wgExpensiveParserFunctionLimit = 500;
 
 if ( $wmgUseCite ) {
-	require( $IP . '/extensions/Cite/Cite.php' );
+	wfLoadExtension( 'Cite' );
 }
 
 if ( $wmgUseCiteThisPage ) {
@@ -1278,7 +1278,7 @@ if ( $wmgUseApiFeatureUsage ) {
 
 // taking it live 2006-12-15 brion
 if ( $wmgUseDismissableSiteNotice ) {
-	require( "$IP/extensions/DismissableSiteNotice/DismissableSiteNotice.php" );
+	wfLoadExtension( 'DismissableSiteNotice' );
 	$wgDismissableSiteNoticeForAnons = true; // T59732
 }
 $wgMajorSiteNoticeID = '2';
@@ -1851,7 +1851,7 @@ if ( $wmgUseBetaFeatures ) {
 }
 
 if ( $wmgUseCommonsMetadata ) {
-	require_once( "$IP/extensions/CommonsMetadata/CommonsMetadata.php" );
+	wfLoadExtension( 'CommonsMetadata' );
 	$wgCommonsMetadataSetTrackingCategories = $wmgCommonsMetadataSetTrackingCategories;
 	$wgCommonsMetadataForceRecalculate = $wmgCommonsMetadataForceRecalculate;
 }
@@ -2020,7 +2020,7 @@ if ( $wmgUseVisualEditor ) {
 	}
 
 	// Citoid
-	require_once "$IP/extensions/Citoid/Citoid.php";
+	wfLoadExtension( 'Citoid' );
 	$wgCitoidServiceUrl = '//citoid.wikimedia.org/api';
 }
 
@@ -2146,7 +2146,7 @@ if ( $wmgUseBabel ) {
 }
 
 if ( $wmgUseBounceHandler ) {
-	require_once "$IP/extensions/BounceHandler/BounceHandler.php";
+	wfLoadExtension( 'BounceHandler' );
 	// $wmgVERPsecret is set in PrivateSettings.php
 	$wgVERPsecret = $wmgVERPsecret;
 	$wgVERPdomainPart = 'wikimedia.org';
@@ -2494,11 +2494,12 @@ if ( $wmgUseFlow && $wmgUseParsoid ) {
 }
 
 if ( $wmgUseDisambiguator ) {
+	wfLoadExtension( 'Disambiguator' );
 	require_once( "$IP/extensions/Disambiguator/Disambiguator.php" );
 }
 
 if ( $wmgUseCodeEditorForCore || $wmgUseScribunto || $wmgZeroPortal ) {
-	include_once( "$IP/extensions/CodeEditor/CodeEditor.php" );
+	wfLoadExtension( 'CodeEditor' );
 	$wgCodeEditorEnableCore = $wmgUseCodeEditorForCore;
 	if ( $wgDBname === 'metawiki' ) {
 		$wgHooks['CodeEditorGetPageLanguage'][] = function ( Title $title, &$lang ) {
@@ -2562,7 +2563,7 @@ if ( $wmgUseSlippyMap ) {
 }
 
 if ( $wmgUseCreditsSource ) {
-	require_once( "$IP/extensions/CreditsSource/CreditsSource.php" );
+	wfLoadExtension( 'CreditsSource' );
 }
 
 if ( $wmgUseListings ) {
@@ -2674,7 +2675,7 @@ if ( $wmgUseUniversalLanguageSelector ) {
 }
 
 if ( $wmgUseContentTranslation ) {
-	require_once "$IP/extensions/ContentTranslation/ContentTranslation.php";
+	wfLoadExtension( 'ContentTranslation' );
 	// T76200: Public URL for cxserver instance
 	$wgContentTranslationSiteTemplates['cx'] = '//cxserver.wikimedia.org/v1';
 	// Used for html2wikitext when publishing
