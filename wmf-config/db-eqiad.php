606473b36ad2f1fe6f215f1b2e7c4d3020bb62e7
jcrespo <jcrespo@wikimedia.org>
2015-08-27 12:53:09 +0200

    Pool es1011, depool es1008 as storage nodes
    Pooling es1011 as new external storage node (new hardware) for
    es2. Starting with low priority to see how it behaves.
    Depooling es1008 in order to clone it to es1014.
    
    Bug: T105843
    Change-Id: I361f09a85f41c0abcf7879418ff6e02a4466f1d1

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -477,13 +477,14 @@ $wgLBFactoryConf = array(
 	# es2
 	'cluster24' => array(
 		'10.64.16.154' => 1, # es1006, master
-#		'10.64.16.153' => 3, # es1005 (downtime for cloning)
+#		'10.64.16.153' => 3, # es1005 (downtime for decomm)
 		'10.64.32.17'  => 3, # es1007
+		'10.64.0.6'    => 1, # es1011 (new server)
 	),
 	# es3
 	'cluster25' => array(
 		'10.64.32.19' => 1, # es1009, master
-		'10.64.32.18' => 3, # es1008
+#		'10.64.32.18' => 3, # es1008 (downtime for cloning)
 		'10.64.32.20' => 3, # es1010
 	),
 	# ExtensionStore shard1 - initially for AFTv5
