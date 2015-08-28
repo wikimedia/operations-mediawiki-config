0a3f891ccd7a4d81e35fd34752c7f7ef3323648a
jcrespo <jcrespo@wikimedia.org>
2015-08-28 10:15:54 +0200

    Depool es1001 for cloning, increase es1011 weight, pool es1014
    We will now clone es1 server es1001 into es1012.
    es1011 has been working correctly for 24 hours, increasing its
    load to normal.
    es1014 has been successfully cloned, and not will be a new
    server of es3, with limited load.
    
    Bug: T105843
    Change-Id: Id83a36997fde54cb627e3b13f57968806a48c6d7

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -14,7 +14,7 @@ $wmgParserCacheDBs = array(
 );
 
 $wmgOldExtTemplate = array(
-	'10.64.0.25'  => 1, # es1001
+#	'10.64.0.25'  => 1, # es1001 (down for cloning)
 	'10.64.16.40' => 1, # es1002
 	'10.64.16.41' => 1, # es1003
 	'10.64.16.42' => 1, # es1004
@@ -477,15 +477,16 @@ $wgLBFactoryConf = array(
 	# es2
 	'cluster24' => array(
 		'10.64.16.154' => 1, # es1006, master
-#		'10.64.16.153' => 3, # es1005 (downtime for decomm)
+#		'10.64.16.153' => 3, # es1005 (downtime for maint)
 		'10.64.32.17'  => 3, # es1007
-		'10.64.0.6'    => 1, # es1011 (new server)
+		'10.64.0.6'    => 3, # es1011
 	),
 	# es3
 	'cluster25' => array(
-		'10.64.32.19' => 1, # es1009, master
-#		'10.64.32.18' => 3, # es1008 (downtime for cloning)
-		'10.64.32.20' => 3, # es1010
+		'10.64.32.19'  => 1, # es1009, master
+#		'10.64.32.18'  => 3, # es1008 (downtime for maint)
+		'10.64.32.20'  => 3, # es1010
+		'10.64.16.187' => 1, # es1014 (new server)
 	),
 	# ExtensionStore shard1 - initially for AFTv5
 	'extension1' => array(
