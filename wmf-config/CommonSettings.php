9c54a0bc6088bc9f3a55b1c9b9b14f2790f8702b
Erik Bernhardson <ebernhardson@wikimedia.org>
2015-09-01 19:43:02 -0700

    Enable experiment with experimental completion suggester
    Enabled for 1 in 5000 page views. Runs from 2015-09-08 until
    2015-09-15.
    
    Bug: T111078
    Change-Id: I22a7f3d96924cc8b0ab7e2dd19bd3c5b4701cf46

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -2648,6 +2648,7 @@ if ( $wmgUseEventLogging ) {
 	include_once( "$IP/extensions/WikimediaEvents/WikimediaEvents.php" );
 	$wgWMEStatsdBaseUri = '/beacon/statsv';
 	$wgWMETrackGeoFeatures = $wmgWMETrackGeoFeatures;
+	$wgWMEEnableCompletionExperiment = $wmgWMEEnableCompletionExperiment;
 }
 
 if ( $wmgUseEventLogging && $wmgUseNavigationTiming ) {
