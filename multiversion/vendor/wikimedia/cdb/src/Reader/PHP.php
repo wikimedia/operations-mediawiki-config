41181d2fb8a2de23643561614575122471911f78
Ori Livneh <ori@wikimedia.org>
2015-09-08 13:10:48 -0700

    wikimedia/cdb 1.2.0 → 1.3.0
    Change-Id: I06a63659724a704cd9af0a353469750438e93565

--- multiversion/vendor/wikimedia/cdb/src/Reader/PHP.php
+++ multiversion/vendor/wikimedia/cdb/src/Reader/PHP.php
@@ -33,24 +33,36 @@ use Cdb\Util;
  */
 class PHP extends Reader {
 
-	/** The filename */
+	/** @var string The file name of the CDB file. **/
 	protected $fileName;
 
-	/** The CDB file's lookup table **/
+	/** @var string First 2048b of CDB file, containing the hash table. **/
 	protected $hashTable;
 
-	/* initialized if find() returns true */
+	/** @var int Offset in file where value of found key starts. **/
 	protected $dataPos;
 
-	/* initialized if find() returns true */
+	/** @var int Byte length of found key's value. **/
 	protected $dataLen;
 
-	/* reset by firstkey() */
-	protected $pos = 2048;
+	/** @var int File position indicator when iterating over keys. **/
+	protected $keyIterPos = 2048;
+
+	/** @var string Read buffer for CDB file. **/
+	protected $buf;
+
+	/** @var int File offset where read buffer starts. **/
+	protected $bufStart;
+
+	/** @var int File handle position indicator **/
+	protected $filePos = 2048;
 
 	/**
+	 * Constructor.
+	 *
 	 * @param string $fileName
-	 * @throws Exception
+	 * @throws Exception If CDB file cannot be opened or if it contains fewer
+	 *   than 2048 bytes of data.
 	 */
 	public function __construct( $fileName ) {
 		$this->fileName = $fileName;
@@ -64,6 +76,9 @@ class PHP extends Reader {
 		}
 	}
 
+	/**
+	 * Close the handle on the CDB file.
+	 */
 	public function close() {
 		if ( isset( $this->handle ) ) {
 			fclose( $this->handle );
@@ -72,8 +87,10 @@ class PHP extends Reader {
 	}
 
 	/**
+	 * Get the value of a key.
+	 *
 	 * @param mixed $key
-	 * @return bool|string
+	 * @return bool|string The key's value or false if not found.
 	 */
 	public function get( $key ) {
 		// strval is required
@@ -85,14 +102,14 @@ class PHP extends Reader {
 	}
 
 	/**
-	 * @throws Exception
-	 * @param int $start
-	 * @param int $len
-	 * @return string
+	 * Read data from the CDB file.
+	 *
+	 * @throws Exception When attempting to read past the end of the file.
+	 * @param int $start Start reading from this position.
+	 * @param int $len Number of bytes to read.
+	 * @return string Read data.
 	 */
 	protected function read( $start, $len ) {
-		static $buf, $bufStart, $pos = 2048;
-
 		$end = $start + $len;
 
 		// The first 2048 bytes are the lookup table, which is read into
@@ -103,8 +120,8 @@ class PHP extends Reader {
 
 		// Read data from the internal buffer first.
 		$bytes = '';
-		if ( $buf && $start >= $bufStart ) {
-			$bytes .= substr( $buf, $start - $bufStart, $len );
+		if ( $this->buf && $start >= $this->bufStart ) {
+			$bytes .= substr( $this->buf, $start - $this->bufStart, $len );
 			$bytesRead = strlen( $bytes );
 			$len -= $bytesRead;
 			$start += $bytesRead;
@@ -119,7 +136,7 @@ class PHP extends Reader {
 		// Many reads are sequential, so the file position indicator may
 		// already be in the right place, in which case we can avoid the
 		// call to fseek().
-		if ( $start !== $pos ) {
+		if ( $start !== $this->filePos ) {
 			if ( fseek( $this->handle, $start ) === -1 ) {
 				// This can easily happen if the internal pointers are incorrect
 				throw new Exception(
@@ -132,13 +149,15 @@ class PHP extends Reader {
 			$buf = '';
 		}
 
-		$bufStart = $start;
-		$pos = $end;
 		$bytes .= substr( $buf, 0, $len );
 		if ( strlen( $bytes ) !== $len + $bytesRead ) {
 			throw new Exception(
 				'Read from CDB file failed, file "' . $this->fileName . '" may be corrupted.' );
 		}
+
+		$this->filePos = $end;
+		$this->bufStart = $start;
+		$this->buf = $buf;
 
 		return $bytes;
 	}
@@ -146,8 +165,8 @@ class PHP extends Reader {
 	/**
 	 * Unpack an unsigned integer and throw an exception if it needs more than 31 bits.
 	 *
-	 * @param int $pos
-	 * @throws Exception
+	 * @param int $pos Position to read from.
+	 * @throws Exception When the integer cannot be represented in 31 bits.
 	 * @return int
 	 */
 	protected function readInt31( $pos = 0 ) {
@@ -161,24 +180,48 @@ class PHP extends Reader {
 	}
 
 	/**
-	 * Unpack a 32-bit integer
+	 * Unpack a 32-bit integer.
 	 *
 	 * @param int $pos
 	 * @return int
 	 */
 	protected function readInt32( $pos = 0 ) {
+		static $lookups;
+
+		if ( !$lookups ) {
+			$lookups = array();
+			for ( $i = 1; $i < 256; $i++ ) {
+				$lookups[ chr( $i ) ] = $i;
+			}
+		}
+
 		$buf = $this->read( $pos, 4 );
-		return (
-			  ord( $buf[ 0 ] )         |
-			( ord( $buf[ 1 ] ) <<  8 ) |
-			( ord( $buf[ 2 ] ) << 16 ) |
-			( ord( $buf[ 3 ] ) << 24 )
-		);
+
+		$rv = 0;
+
+		if ( $buf[0] !== "\x0" ) {
+			$rv = $lookups[ $buf[0] ];
+		}
+		if ( $buf[1] !== "\x0" ) {
+			$rv |= ( $lookups[ $buf[1] ] << 8 );
+		}
+		if ( $buf[2] !== "\x0" ) {
+			$rv |= ( $lookups[ $buf[2] ] << 16 );
+		}
+		if ( $buf[3] !== "\x0" ) {
+			$rv |= ( $lookups[ $buf[3] ] << 24 );
+		}
+
+		return $rv;
 	}
 
 	/**
+	 * Search the CDB file for a key.
+	 *
+	 * Sets `dataLen` and `dataPos` properties if successful.
+	 *
 	 * @param string $key
-	 * @return bool
+	 * @return bool Whether the key was found.
 	 */
 	protected function find( $key ) {
 		$keyLen = strlen( $key );
@@ -225,20 +268,36 @@ class PHP extends Reader {
 		return false;
 	}
 
+	/**
+	 * Check if a key exists in the CDB file.
+	 *
+	 * @param string $key
+	 * @return bool Whether the key exists.
+	 */
 	public function exists( $key ) {
 		return $this->find( strval( $key ) );
 	}
 
+	/**
+	 * Get the first key from the CDB file and reset the key iterator.
+	 *
+	 * @return string Key.
+	 */
 	public function firstkey() {
-		$this->pos = 2048;
+		$this->keyIterPos = 2048;
 		return $this->nextkey();
 	}
 
+	/**
+	 * Get the next key from the CDB file.
+	 *
+	 * @return string Key.
+	 */
 	public function nextkey() {
-		$keyLen = $this->readInt31( $this->pos );
-		$dataLen = $this->readInt31( $this->pos + 4 );
-		$key = $this->read( $this->pos + 8, $keyLen );
-		$this->pos += 8 + $keyLen + $dataLen;
+		$keyLen = $this->readInt31( $this->keyIterPos );
+		$dataLen = $this->readInt31( $this->keyIterPos + 4 );
+		$key = $this->read( $this->keyIterPos + 8, $keyLen );
+		$this->keyIterPos += 8 + $keyLen + $dataLen;
 
 		return $key;
 	}
