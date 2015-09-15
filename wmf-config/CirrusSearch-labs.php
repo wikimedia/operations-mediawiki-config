4824f818ef5422c4436ec476728ed4b6fb10eaec
Erik Bernhardson <ebernhardson@wikimedia.org>
2015-09-14 17:34:46 -0700

    alt language search configuration for labs
    Change-Id: I6c23058e246a4ea9e828cb534736c69e247301d9

--- wmf-config/CirrusSearch-labs.php
+++ wmf-config/CirrusSearch-labs.php
@@ -35,3 +35,37 @@ $wgCirrusSearchWikimediaExtraPlugin[ 'safer' ] = array(
 );
 
 $wgCirrusSearchEnableSearchLogging = true;
+
+$wgCirrusSearchLanguageToWikiMap = array(
+    'ar' => 'ar',
+    'de' => 'de',
+    'en' => 'en',
+    'es' => 'es',
+    'fa' => 'fa',
+    'he' => 'he',
+    'hi' => 'hi',
+    'ja' => 'ja',
+    'ko' => 'ko',
+    'ru' => 'ru',
+    'sq' => 'sq',
+    'uk' => 'uk',
+    'zh-cn' => 'zh',
+    'zh-tw' => 'zh',
+);
+
+$wgCirrusSearchWikiToNameMap = array(
+    'ar' => 'arwiki',
+    'de' => 'dewiki',
+    'en' => 'enwiki',
+    'es' => 'eswiki',
+    'fa' => 'fawiki',
+    'he' => 'hewiki',
+    'hi' => 'hiwiki',
+    'ja' => 'jawiki',
+    'ko' => 'kowiki',
+    'ru' => 'ruwiki',
+    'sq' => 'sqwiki',
+    'uk' => 'ukwiki',
+    'zh' => 'zhwiki',
+);
+
