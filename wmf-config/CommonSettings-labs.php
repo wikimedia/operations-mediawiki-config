4f856fcace7e6cc06369758ef46446d3a02e4af8
jdlrobson <jdlrobson@gmail.com>
2015-09-17 11:18:52 -0700

    Replicate browser test config
    Tests are currently failing on beta labs due to invalid configuration.
    This copy pastes the config changes from the test given that there is
    currently no better way to do this.
    
    Potentially we could include the LocalSettings.php in the test folder
    but that seems like it could cause us headaches down the road so this is
    deemed much safer.
    
    Bug: T112204
    Change-Id: I23e4385ff18423e0a7cc11a6d9d9f50151681296

--- wmf-config/CommonSettings-labs.php
+++ wmf-config/CommonSettings-labs.php
@@ -233,22 +233,57 @@ if ( $wmgUsePageImages ) {
 if ( $wmgUseQuickSurveys ) {
 	wfLoadExtensions( array( 'QuickSurveys' ) );
 
-	$wgQuickSurveysConfig[] = array(
-		'name' => 'internal example survey',
-		'type' => 'internal',
-		'question' => 'ext-quicksurveys-example-internal-survey-question',
-		'answers' => array(
-			'positive' => 'ext-quicksurveys-example-internal-survey-answer-positive',
-			'neutral' => 'ext-quicksurveys-example-internal-survey-answer-neutral',
-			'negative' => 'ext-quicksurveys-example-internal-survey-answer-negative',
+	$wgQuickSurveysConfig = array_merge( $wgQuickSurveysConfig, array(
+		array(
+			"name" => "drink-survey",
+			"type" => "internal",
+			"question" => "anne-survey-question",
+			"answers" => array(
+				"anne-survey-answer-one",
+				"anne-survey-answer-two",
+				"anne-survey-answer-three",
+				"anne-survey-answer-four"
+			),
+			"schema" => "QuickSurveysResponses",
+			"enabled" => true,
+			"coverage" => 0,
+			"description" => "anne-survey-description",
+			"platform" => array(
+				"desktop" => array( "stable" ),
+				"mobile" => array( "stable", "beta", "alpha" ),
+			),
 		),
-		'schema' => 'QuickSurveysResponses',
-		'enabled' => true,
-		'coverage' => .5,
-		'description' => 'ext-quicksurveys-example-internal-survey-description',
-		'platform' => array(
-			'desktop' => array( 'stable' ),
-			'mobile' => array( 'stable', 'beta', 'alpha' ),
+		array(
+			"name" => "internal example survey",
+			"type" => "internal",
+			"question" => "ext-quicksurveys-example-internal-survey-question",
+			"answers" => array(
+				"ext-quicksurveys-example-internal-survey-answer-positive",
+				"ext-quicksurveys-example-internal-survey-answer-neutral",
+				"ext-quicksurveys-example-internal-survey-answer-negative",
+			),
+			"schema" => "QuickSurveysResponses",
+			"enabled" => true,
+			"coverage" => .5,
+			"description" => "ext-quicksurveys-example-internal-survey-description",
+			"platform" => array(
+				"desktop" => array( "stable" ),
+				"mobile" => array( "stable", "beta", "alpha" ),
+			),
+		),
+		array(
+			'name' => 'external example survey',
+			'type' => 'external',
+			"question" => "ext-quicksurveys-example-external-survey-question",
+			"description" => "ext-quicksurveys-example-external-survey-description",
+			"link" => "ext-quicksurveys-example-external-survey-link",
+			"privacyPolicy" => "ext-quicksurveys-example-external-survey-privacy-policy",
+			'coverage' => .5,
+			'enabled' => true,
+			'platform' => array(
+				'desktop' => array( 'stable' ),
+				'mobile' => array( 'stable', 'beta', 'alpha' ),
+			),
 		),
 	);
 }
