56425bbccbebe2b6eb85f79857869609aea61daa
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2015-09-17 12:15:22 +0200

    poolcounter: use codfw hosts in codfw
    Change-Id: I6373ce0d5d148a9f86bfa85db14a354d7f14c738

--- wmf-config/PoolCounterSettings-codfw.php
+++ wmf-config/PoolCounterSettings-codfw.php
@@ -3,8 +3,8 @@
 # For now using eqiad PC servers
 $wgPoolCountClientConf = array(
 	'servers' => array(
-		'10.64.0.179',
-		'10.64.16.152'
+		'10.192.16.124',
+		'10.192.0.121'
 	),
 	'timeout' => 0.5,
 	'connect_timeout' => 0.01,
