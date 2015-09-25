0eae35abbdacae82ea7d338cd9df508f38e271c5
Aaron Schulz <aschulz@wikimedia.org>
2015-09-24 21:48:37 -0700

    Set "async" for SQL parser cache everywhere else
    Change-Id: I252970886b6f1242d1cb8c99fd12cf67612658fd

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -303,7 +303,7 @@ $wgObjectCaches['mysql-multiwrite'] = array(
 			'shards' => 256,
 		),
 	),
-	'replication' => ( $wgDBname === 'testwiki' ) ? 'async' : 'sync'
+	'replication' => 'async'
 );
 
 if ( $wmgUseClusterSession ) {
