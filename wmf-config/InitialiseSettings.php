dec9e1b4281b7b11c3e721cac1264d5be8dbf69e
maurelio <strigiwm@gmail.com>
2015-10-01 12:29:43 +0200

    Enable Extension:ShortUrl on mr.wikipedia
    Bug: T103646
    Change-Id: I442443fd5e81d2def98513d2fe13a6a6ac1cc8fa

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -14259,6 +14259,7 @@ $wgConf->settings = array(
 	'eswikibooks' => true, // T96668
 	'hiwiki' => true,
 	'knwiki' => true, // T97218
+	'mrwiki' => true, // T103646
 	'newiki' => true, // T92820
 	'orwiki' => true,
 	'orwiktionary' => true, // T103644
