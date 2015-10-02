134d2df2d106a847ae8d0abdb6de162f8403b1be
Aaron Schulz <aschulz@wikimedia.org>
2015-10-02 00:07:44 -0700

    Switched to pt-heartbeat lag detection on s6
    Bug: T111266
    Change-Id: I21f39be9d0fed0fad0af877cfeb5794dad732b6a

--- wmf-config/db-codfw.php
+++ wmf-config/db-codfw.php
@@ -172,6 +172,41 @@ $wgLBFactoryConf = array(
 	)
 ),
 
+'templateOverridesBySection' => array(
+	/*
+	's1' => array(
+		'lagDetectionMethod' => 'pt-heartbeat',
+		'lagDetectionOptions' => [ 'conds' => [ 'shard' => 's1' ] ],
+	),
+	's2' => array(
+		'lagDetectionMethod' => 'pt-heartbeat',
+		'lagDetectionOptions' => [ 'conds' => [ 'shard' => 's2' ] ],
+	),
+	's3' => array(
+		'lagDetectionMethod' => 'pt-heartbeat',
+		'lagDetectionOptions' => [ 'conds' => [ 'shard' => 's3' ] ],
+	),
+	's4' => array(
+		'lagDetectionMethod' => 'pt-heartbeat',
+		'lagDetectionOptions' => [ 'conds' => [ 'shard' => 's4' ] ],
+	),
+	's5' => array(
+		'lagDetectionMethod' => 'pt-heartbeat',
+		'lagDetectionOptions' => [ 'conds' => [ 'shard' => 's5' ] ],
+	),
+	*/
+	's6' => array(
+		'lagDetectionMethod' => 'pt-heartbeat',
+		'lagDetectionOptions' => [ 'conds' => [ 'shard' => 's5' ] ],
+	),
+	/*
+	's7' => array(
+		'lagDetectionMethod' => 'pt-heartbeat',
+		'lagDetectionOptions' => [ 'conds' => [ 'shard' => 's7' ] ],
+	),
+	*/
+),
+
 'groupLoadsBySection' => array(
 	's1' => array(
 		'watchlist' => array(
