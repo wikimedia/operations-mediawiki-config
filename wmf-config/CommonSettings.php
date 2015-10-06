9010f8e001ad38eb9c44edb68a863c945f0ad76a
EBernhardson <ebernhardson@wikimedia.org>
2015-10-06 23:21:56 +0000

    Revert "Revert "Update CirrusSearch config for testwiki to talk to second cluster""
    This reverts commit eca11c19e47bde446cafa6ff792a634fe1667eb9.
    
    Change-Id: I3566d40bf93f5766d0825bb63f9804550ae19d33

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -897,6 +897,8 @@ if ( in_array( $wgLanguageCode, array( 'commons', 'meta', 'sources', 'species', 
 
 # All wikis are special and get Cirrus :)
 if ( $wmgUseCirrus ) {
+	require_once( "$IP/extensions/Elastica/Elastica.php" );
+	require_once( "$IP/extensions/CirrusSearch/CirrusSearch.php" );
 	include( "$wmfConfigDir/CirrusSearch-common.php" );
 }
 
@@ -2198,7 +2200,7 @@ if ( $wmgUseTranslate ) {
 	if ( $wmgUseTranslationMemory ) {
 		$servers = array_map(
 			function ( $v ) { return array( 'host' => $v ); },
-			$wgCirrusSearchServers
+			isset( $wgCirrusSearchServers ) ? $wgCirrusSearchServers : $wgCirrusSearchCluster[$wgCirrusSearchDefaultCluster]
 		);
 		// Read only until renamed to 'TTMServer'
 		$wgTranslateTranslationServices['TTMServer'] = array(
