cb727250d43089723ea68337b6102b44b04279d0
Ori Livneh <ori@wikimedia.org>
2015-10-07 13:11:53 -0700

    Switch mw1017 to use DC-specific redis cluster names
    Follows I64bc52ca. Just mw1017 for now.
    
    Change-Id: Ie25c368a3338098d6deda279c24a9eb70be325cb

--- wmf-config/session.php
+++ wmf-config/session.php
@@ -1,6 +1,13 @@
 <?php
 
-$sessionRedis = array(
-	'eqiad' => array( '127.0.0.1:6380' ),
-	'codfw' => array( '127.0.0.1:6380' ),
-);
+if ( gethostname() === 'mw1017' && defined( 'HHVM_VERSION' ) ) {
+	$sessionRedis = array(
+		'eqiad' => array( '/var/run/nutcracker/redis_eqiad.sock' ),
+		'codfw' => array( '/var/run/nutcracker/redis_codfw.sock' ),
+	);
+} else {
+	$sessionRedis = array(
+		'eqiad' => array( '127.0.0.1:6380' ),
+		'codfw' => array( '127.0.0.1:6380' ),
+	);
+}
