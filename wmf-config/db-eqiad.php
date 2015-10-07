beca3a0a791b062489103b12934d01387e4a6d76
Jcrespo <jcrespo@wikimedia.org>
2015-10-07 15:09:29 +0000

    Pooling again db1051 with db1055's roles, to fix SPOF
    db1055 cannot be depooled because of its special table schema.
    I have mirrored its partitioning on db1051, trying to avoid it
    being a single point of failure, and with the intention of
    depooling db1055 from maintenance.
    
    Try 2 after fixing schema differences that affected a very specific API query.
    
    This reverts commit 52cfecc95994fd75530ce7e0aa50d024777bfb5d.
    
    Change-Id: I7c6e2c7ccdcbb10431b9eb357b81df56b796b9be

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -88,9 +88,9 @@ $wgLBFactoryConf = array(
 'sectionLoads' => array(
 	's1' => array(
 		'db1052' => 0,   # 2.8TB  96GB
-#		'db1051' => 0,   # 2.8TB  96GB, vslow, dump
+		'db1051' => 0,   # 2.8TB  96GB, watchlist, recentchanges, contributions, logpager
 		'db1055' => 0,   # 2.8TB  96GB, watchlist, recentchanges, contributions, logpager
-		'db1053' => 0, # 2.8TB  96GB (temp. vslow, dump)
+		'db1053' => 0,   # 2.8TB  96GB (temp. vslow, dump)
 		'db1057' => 200, # 2.8TB  96GB
 		'db1065' => 100, # 2.8TB 160GB, api
 		'db1066' => 100, # 2.8TB 160GB, api
@@ -167,19 +167,24 @@ $wgLBFactoryConf = array(
 'groupLoadsBySection' => array(
 	's1' => array(
 		'watchlist' => array(
-			'db1055' => 1,
+			'db1051' => 1,
+			'db1055' => 10,
 		),
 		'recentchanges' => array(
-			'db1055' => 1,
+			'db1051' => 1,
+			'db1055' => 10,
 		),
 		'recentchangeslinked' => array(
-			'db1055' => 1,
+			'db1051' => 1,
+			'db1055' => 10,
 		),
 		'contributions' => array(
-			'db1055' => 1,
+			'db1051' => 1,
+			'db1055' => 10,
 		),
 		'logpager' => array(
-			'db1055' => 1,
+			'db1051' => 1,
+			'db1055' => 10,
 		),
 		'dump' => array(
 			'db1053' => 1,
