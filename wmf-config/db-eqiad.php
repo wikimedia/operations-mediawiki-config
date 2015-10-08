fd1efbdcbd8113470854392ae6d41f5d1c30c1b7
Jcrespo <jcrespo@wikimedia.org>
2015-10-08 12:29:47 +0200

    Repool db1055 at 10% weight after maintenance
    Bug: T112478
    Change-Id: I3ff53045466a1c505c9f477531f6f4194f8cb881

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -90,7 +90,7 @@ $wgLBFactoryConf = array(
 		'db1052' => 0,   # 2.8TB  96GB
 		'db1053' => 0,   # 2.8TB  96GB, vslow, dump
 		'db1051' => 0,   # 2.8TB  96GB, watchlist, recentchanges, contributions, logpager
-#		'db1055' => 0,   # 2.8TB  96GB, watchlist, recentchanges, contributions, logpager
+		'db1055' => 0,   # 2.8TB  96GB, watchlist, recentchanges, contributions, logpager
 		'db1057' => 200, # 2.8TB  96GB
 		'db1065' => 100, # 2.8TB 160GB, api
 		'db1066' => 100, # 2.8TB 160GB, api
@@ -167,24 +167,24 @@ $wgLBFactoryConf = array(
 'groupLoadsBySection' => array(
 	's1' => array(
 		'watchlist' => array(
-			'db1051' => 1,
-#			'db1055' => 1,
+			'db1051' => 10,
+			'db1055' => 1,
 		),
 		'recentchanges' => array(
-			'db1051' => 1,
-#			'db1055' => 1,
+			'db1051' => 10,
+			'db1055' => 1,
 		),
 		'recentchangeslinked' => array(
-			'db1051' => 1,
-#			'db1055' => 1,
+			'db1051' => 10,
+			'db1055' => 1,
 		),
 		'contributions' => array(
-			'db1051' => 1,
-#			'db1055' => 1,
+			'db1051' => 10,
+			'db1055' => 1,
 		),
 		'logpager' => array(
-			'db1051' => 1,
-#			'db1055' => 1,
+			'db1051' => 10,
+			'db1055' => 1,
 		),
 		'dump' => array(
 			'db1053' => 1,
