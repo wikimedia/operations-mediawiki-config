2c6eea731c38cd346bb8af2b481fd90e262abf18
Ori Livneh <ori@wikimedia.org>
2015-10-11 03:23:13 -0400

    Set MEDIAWIKI_DBLIST_DIR to /srv/mediawiki
    Change-Id: I30d31c428b1ae039c104d345214a1269609bed73

--- multiversion/defines.php
+++ multiversion/defines.php
@@ -3,7 +3,7 @@
  * Define installation specific MWMultiVersion values like paths
  */
 define( 'MEDIAWIKI_STAGING_DIR', '/srv/mediawiki-staging' );
-define( 'MEDIAWIKI_DBLIST_DIR', '/srv/mediawiki-staging' );
+define( 'MEDIAWIKI_DBLIST_DIR', '/srv/mediawiki' );
 define( 'MEDIAWIKI_DEPLOYMENT_DIR', '/srv/mediawiki' );
 define( 'MEDIAWIKI_VERSION_REGEX', '(\d+\.\d+(\.\d+-)?wmf\.?\d+|master)');
 define( 'MEDIAWIKI_DIRECTORY_REGEX', '/^php-'.MEDIAWIKI_VERSION_REGEX.'$/');
