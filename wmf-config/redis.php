5eb0bda6a8b6ebebe6bbdde2c8043910ba6dfb9c
Aaron Schulz <aschulz@wikimedia.org>
2015-10-19 11:42:53 -0700

    Made the session/main stashes write to both DCs
    Bug: T111575
    Change-Id: I1932aa2b3a42c59676875adc15af5be56034fc9f

--- wmf-config/redis.php
+++ wmf-config/redis.php
@@ -1,4 +1,6 @@
 <?php
+
+$remoteCaches = array();
 foreach ( array( 'eqiad', 'codfw' ) as $dc ) {
 	$wgObjectCaches["redis_{$dc}"] = array(
 		'class'    => 'RedisBagOStuff',
@@ -6,7 +8,23 @@ foreach ( array( 'eqiad', 'codfw' ) as $dc ) {
 		'password' => $wmgRedisPassword,
 		'loggroup' => 'redis',
 	);
+	if ( $dc !== $wmfDatacenter ) {
+		$remoteCaches[] = $wgObjectCaches["redis_{$dc}"];
+	}
 }
+unset( $dc );
 
 $wgObjectCaches['redis_master'] = $wgObjectCaches['redis_eqiad'];
 $wgObjectCaches['redis_local'] = $wgObjectCaches["redis_{$wmfDatacenter}"];
+
+// Stash that writes to the master DC store first and asynchronously to the other DCs
+$wgObjectCaches['redis-multiwrite'] = array(
+	'class' => 'MultiWriteBagOStuff',
+	'caches' => array_merge(
+		array( $wgObjectCaches['redis_master'] ),
+		$remoteCaches
+	),
+	'replication' => 'async'
+);
+
+unset( $remoteCaches );
