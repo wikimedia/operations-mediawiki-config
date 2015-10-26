4e28d609519139fe0180de42ea08a2105fc6e4b9
Erik Bernhardson <ebernhardson@wikimedia.org>
2015-10-26 07:45:01 -0700

    Send CirrusSearch writes to codfw
    Change-Id: Ib3d30261b33369f8778002bd04d44e4e83c719c5

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -15315,8 +15315,8 @@ $wgConf->settings = array(
 ),
 
 'wmgCirrusSearchWriteClusters' => array(
-	'default' => array( 'eqiad' ),
-	'testwiki' => array( 'eqiad', 'labsearch' )
+	'default' => array( 'eqiad', 'codfw' ),
+	'testwiki' => array( 'eqiad', 'codfw', 'labsearch' )
 ),
 
 'wmgCirrusPerUserPoolCounter' => array(
