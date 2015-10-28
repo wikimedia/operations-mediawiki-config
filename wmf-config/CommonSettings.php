f361bc8ebdb752ed21780aac63cc343b5193c946
Aaron Schulz <aschulz@wikimedia.org>
2015-10-28 10:53:42 -0700

    Make mysql-multiwrite use getInstance() factory spec
    * This avoids "undefined index" errors for parameters like "timeout".
    * It also avoid duplicate instance in PHP memory.
    
    Change-Id: I80d38773057cb1ea03530711dd8c9412a9d80213

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -289,7 +289,10 @@ foreach ( $wmgParserCacheDBs as $host ) {
 $wgObjectCaches['mysql-multiwrite'] = array(
 	'class' => 'MultiWriteBagOStuff',
 	'caches' => array(
-		0 => $wgObjectCaches['memcached-pecl'],
+		0 => array(
+			'factory' => array( 'ObjectCache', 'getInstance' ),
+			'args' => array( 'memcached-pecl' )
+		),
 		1 => array(
 			'class' => 'SqlBagOStuff',
 			'servers' => $pcServers,
