308738e159f51e6c4b90b142bd14c088de0ccc35
Jcrespo <jcrespo@wikimedia.org>
2015-10-29 09:50:58 +0100

    Depool db1065 for maintenance
    We are going to activate performance_schema on it to debug
    a query performance issue.
    
    Change-Id: I1eb8eda5d2822335ec1a3b55b098ae9608d18373
    References: T116557 T99485

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -92,7 +92,7 @@ $wgLBFactoryConf = array(
 		'db1051' => 50,  # 2.8TB  96GB, watchlist, recentchanges, contributions, logpager
 		'db1055' => 50,  # 2.8TB  96GB, watchlist, recentchanges, contributions, logpager
 		'db1057' => 200, # 2.8TB  96GB
-		'db1065' => 100, # 2.8TB 160GB, api
+#		'db1065' => 100, # 2.8TB 160GB, api
 		'db1066' => 100, # 2.8TB 160GB, api
 		'db1072' => 500, # 2.8TB 160GB
 		'db1073' => 500, # 2.8TB 160GB
@@ -193,7 +193,7 @@ $wgLBFactoryConf = array(
 			'db1053' => 1,
 		),
 		'api' => array(
-			'db1065' => 1,
+#			'db1065' => 1,
 			'db1066' => 1,
 		),
 	),
