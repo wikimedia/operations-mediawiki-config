037272533833c5602788e79a77252d985bb14277
Jcrespo <jcrespo@wikimedia.org>
2015-10-29 11:31:14 +0100

    Repool db1065 at 100% load
    Everything looks ok.
    
    Change-Id: I17f9d4d9f2aea0dcf84f3bd43c5c9d109f05eae1
    References: T116557 T99485

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -92,7 +92,7 @@ $wgLBFactoryConf = array(
 		'db1051' => 50,  # 2.8TB  96GB, watchlist, recentchanges, contributions, logpager
 		'db1055' => 50,  # 2.8TB  96GB, watchlist, recentchanges, contributions, logpager
 		'db1057' => 200, # 2.8TB  96GB
-		'db1065' => 0,   # 2.8TB 160GB, api
+		'db1065' => 100, # 2.8TB 160GB, api
 		'db1066' => 100, # 2.8TB 160GB, api
 		'db1072' => 500, # 2.8TB 160GB
 		'db1073' => 500, # 2.8TB 160GB
@@ -194,7 +194,7 @@ $wgLBFactoryConf = array(
 		),
 		'api' => array(
 			'db1065' => 1,
-			'db1066' => 10,
+			'db1066' => 1,
 		),
 	),
 	's2' => array(
