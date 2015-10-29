6d6b247d4ff882b55932440bff99da35d0a8ddbd
Kartik Mistry <kartik.mistry@gmail.com>
2015-10-29 17:48:36 +0530

    Set ContentTranslationCXServerAuth for CX
    Change-Id: Icedf68f8c12fcea86ca5fd94da4ae9275c033f98

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -2689,6 +2689,9 @@ if ( $wmgUseContentTranslation ) {
 	$wgContentTranslationDefaultSourceLanguage = $wmgContentTranslationDefaultSourceLanguage;
 
 	$wgContentTranslationEnableSuggestions = $wmgContentTranslationEnableSuggestions;
+
+	//This is set in PrivateSettings.php
+	$wgContentTranslationCXServerAuth = $wmgContentTranslationCXServerAuth;
 }
 
 // @note getRealmSpecificFilename only works with filenames with .suffix
