b34bd24a93c7fabe9e215e89ec225f1f4042168f
James D. Forrester <jforrester@wikimedia.org>
2015-11-02 10:46:21 -0800

    Enable VisualEditor for 50% of new accounts on eswiki
    Bug: T117410
    Change-Id: If2c4104f337721a091b0cc1e0f41d3baa510fbda

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -12890,7 +12890,7 @@ $wgConf->settings = array(
 'wmgVisualEditorNewAccountEnableProportion' => array(
 	'default' => false,
 	'enwiki' => 1,
-	'eswiki' => 4,
+	'eswiki' => 2,
 ),
 
 // Whether VisualEditor should be enabled for new auto-created accounts on a
