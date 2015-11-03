5355217c14ac550c533ca9cc0dcdf231ba37c655
Alex Monk <krenair@gmail.com>
2015-11-03 18:11:21 +0000

    Fix global for wgVisualEditorFullRestbaseURL
    Change-Id: I2099a34479840556c6d7a56a24eff50e98e4138c

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -1938,7 +1938,8 @@ if ( $wmgUseVisualEditor ) {
 		// HACK: $wgServerName is not available yet at this point, it's set by Setup.php
 		// so use a hook
 		$wgExtensionFunctions[] = function () {
-			global $wgServerName, $wgVisualEditorRestbaseURL, $wmfRealm;
+			global $wgServerName, $wmfRealm,
+				$wgVisualEditorRestbaseURL, $wgVisualEditorFullRestbaseURL;
 			if ( $wmfRealm === 'production' ) {
 				$wgVisualEditorRestbaseURL = "https://$wgServerName/api/rest_v1/page/html/";
 				$wgVisualEditorFullRestbaseURL = "https://$wgServerName/api/rest_";
