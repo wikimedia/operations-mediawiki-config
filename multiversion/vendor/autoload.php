deb9624bbff3b7fbc5c9d1420aafac81d5049601
Kunal Mehta <legoktm@gmail.com>
2015-11-04 22:44:40 +0000

    Add "composer test" command to lint files and run tests
    You need to fetch the development dependencies to run tests, by running
    "composer update --dev" first.
    
    This is kind of messy, because we have part of the vendor directory
    checked-in, but development dependencies should not be. Ideally the
    multiversion code could be split out into its own respository or
    something.
    
    Bug: T85947
    Change-Id: I3fa9ac84b9882a0579cd3f81a7adc088792f9f4d

--- multiversion/vendor/autoload.php
+++ multiversion/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit77d65f2e0d4ada4815bb93d290a27837::getLoader();
+return ComposerAutoloaderInit968364272169847388126e7a099abf7b::getLoader();
