804f241d529b20a4800d7b84c54800da8678ba57
Erik Bernhardson <ebernhardson@wikimedia.org>
2015-11-09 14:42:03 -0800

    Enable CirrusSearch writes to enwiki and dewiki as well
    Change-Id: I81e092c1f9696ae909db05d04b74d03f48e5c0fc

--- tests/cirrusTest.php
+++ tests/cirrusTest.php
@@ -45,7 +45,7 @@ class cirrusTests extends PHPUnit_Framework_TestCase {
 		}
 
 		// Only eqiad and codfw for now
-		$this->assertCount( 2, $config['wgCirrusSearchWriteClusters'] );
+		$this->assertCount( 3, $config['wgCirrusSearchWriteClusters'] );
 		foreach ( $config['wgCirrusSearchWriteClusters'] as $cluster ) {
 			$this->assertArrayHasKey(
 				$cluster,
