7aa81c484f9c217570981d58218b8db95aa842ef
andrewbogott <abogott@wikimedia.org>
2015-11-13 09:59:57 -0600

    Set minimum password length of 8 for wikitech.
    Note that this also forces a password change for users with
    too-short passwords.
    
    MinimumPasswordLengthToLogin = 8
    
    Bug: T118751
    Change-Id: Id306e9badcb76a16de881fac51529b0282a47d5f

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -316,11 +316,22 @@ $wgPasswordDefault = 'pbkdf2';
 // This needs to be increased as allowable by server performance
 $wgPasswordConfig['pbkdf2']['cost'] = '64000';
 
-// Temporarily set the policy for these roles to the previous WMF setting until
-// we communicate the change to affected communities.
-$wgPasswordPolicy['policies']['bureaucrat']['MinimalPasswordLength'] = 1;
-$wgPasswordPolicy['policies']['sysop']['MinimalPasswordLength'] = 1;
-$wgPasswordPolicy['policies']['bot']['MinimalPasswordLength'] = 1;
+if ( $wgDBname === 'labswiki' ) {
+	$wgPasswordPolicy['policies']['bureaucrat']['MinimalPasswordLength'] = 8;
+	$wgPasswordPolicy['policies']['bureaucrat']['MinimumPasswordLengthToLogin'] = 8;
+	$wgPasswordPolicy['policies']['sysop']['MinimalPasswordLength'] = 8;
+	$wgPasswordPolicy['policies']['sysop']['MinimumPasswordLengthToLogin'] = 8;
+	$wgPasswordPolicy['policies']['bot']['MinimalPasswordLength'] = 8;
+	$wgPasswordPolicy['policies']['bot']['MinimumPasswordLengthToLogin'] = 8;
+	$wgPasswordPolicy['policies']['default']['MinimalPasswordLength'] = 8;
+	$wgPasswordPolicy['policies']['default']['MinimumPasswordLengthToLogin'] = 8;
+} else {
+	// Temporarily set the policy for these roles to the previous WMF setting until
+	// we communicate the change to affected communities.
+	$wgPasswordPolicy['policies']['bureaucrat']['MinimalPasswordLength'] = 1;
+	$wgPasswordPolicy['policies']['sysop']['MinimalPasswordLength'] = 1;
+	$wgPasswordPolicy['policies']['bot']['MinimalPasswordLength'] = 1;
+}
 
 if ( PHP_SAPI === 'cli' ) {
 	$wgShowExceptionDetails = true;
