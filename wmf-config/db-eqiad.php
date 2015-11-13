ad8b0973550b5c2ee6dd7a7e60d785ab9322185f
Jcrespo <jcrespo@wikimedia.org>
2015-11-13 09:16:54 +0100

    Repool db1035 at 100%, depool db1015 for maintenance
    Setting db1027 as db1015's roles. This may cause some
    slowdowns, so we need to monitor the performance and revert
    if it cases problems. Otherwise, a 5-day alter table may
    be needed previos to maintenance.
    
    Change-Id: I2c5324d5694c233e8a6119d858d1c226c4424edd
    References: T110504

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -109,9 +109,9 @@ $wgLBFactoryConf = array(
 	),
 	/* s3 */ 'DEFAULT' => array(
 		'db1038' => 0,   # 1.4TB  64GB
-		'db1027' => 0,   # 1.4TB  64GB, vslow, dump
-		'db1015' => 50,  # 1.4TB  64GB, watchlist, recentchanges, contributions, logpager
-		'db1035' => 50,  # 1.4TB  64GB
+		'db1027' => 0,  # 1.4TB  64GB, vslow, dump, (temp. watch/rc/etc.)
+#		'db1015' => 50,  # 1.4TB  64GB, watchlist, recentchanges, contributions, logpager
+		'db1035' => 400, # 1.4TB  64GB
 		'db1044' => 400, # 1.4TB  64GB
 	),
 	's4' => array(
@@ -232,19 +232,24 @@ $wgLBFactoryConf = array(
 			'db1027' => 1,
 		),
 		'watchlist' => array(
-			'db1015' => 1,
+			'db1027' => 1,
+#			'db1015' => 1,
 		),
 		'recentchanges' => array(
-			'db1015' => 1,
+			'db1027' => 1,
+#			'db1015' => 1,
 		),
 		'recentchangeslinked' => array(
-			'db1015' => 1,
+			'db1027' => 1,
+#			'db1015' => 1,
 		),
 		'contributions' => array(
-			'db1015' => 1,
+			'db1027' => 1,
+#			'db1015' => 1,
 		),
 		'logpager' => array(
-			'db1015' => 1,
+			'db1027' => 1,
+#			'db1015' => 1,
 		),
 	),
 	's4' => array(
