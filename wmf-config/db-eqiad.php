ae1c747ac0372515d83f1a4c193a268fc1b14c17
Jcrespo <jcrespo@wikimedia.org>
2015-11-19 10:32:41 +0100

    Repool db1044 after maintenance
    Change-Id: I81069bdbf6afec62cacf1ce707161b43ffb86279

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -111,8 +111,8 @@ $wgLBFactoryConf = array(
 		'db1038' => 0,   # 1.4TB  64GB
 		'db1027' => 50,   # 1.4TB  64GB, vslow, dump
 		'db1015' => 50,  # 1.4TB  64GB, watchlist, recentchanges, contributions, logpager
-		'db1035' => 200, # 1.4TB  64GB
-#		'db1044' => 400, # 1.4TB  64GB
+		'db1035' => 500, # 1.4TB  64GB
+		'db1044' => 500, # 1.4TB  64GB
 	),
 	's4' => array(
 		'db1040' => 0,   # 1.4TB  64GB
