ec81aa5864f759315764efcc9332f9fb58858748
Aaron Schulz <aschulz@wikimedia.org>
2015-11-24 21:12:10 -0800

    Update job queue config to use 2 rdb1007 instances
    Change-Id: I9bea66dfd73881a9e85a1afdbbf186abaf997a4c

--- wmf-config/jobqueue-eqiad.php
+++ wmf-config/jobqueue-eqiad.php
@@ -1,6 +1,18 @@
 <?php
 # WARNING: This file is publicly viewable on the web. Do not put private data here.
 
+/** @var string $wmgRedisPassword From PrivateSettings.php */
+
+$wmgRedisQueueBaseConfig = array(
+	'class' => 'JobQueueRedis',
+	'redisConfig' => array(
+		'connectTimeout' => 2,
+		'password' => $wmgRedisPassword,
+		'compression' => 'gzip'
+	),
+	'daemonized' => true
+);
+
 // Note: on server failure, partition masters should be switched to the slave
 // Note: MediaWiki will fail-over to other shards when one is down. On master
 // failure, it is best to either do nothing or just disable the whole shard
@@ -8,56 +20,44 @@
 // requires changing the slave to stop slaving the old master before updating
 // the MediaWiki config to direct traffic there.
 $wgJobTypeConf['default'] = array(
-	'class'               => 'JobQueueFederated',
-	'configByPartition'   => array(
-		'rdb1' => array(
-			'class'       => 'JobQueueRedis',
-			'redisServer' => 'rdb1001.eqiad.wmnet', # master
-			#'redisServer' => 'rdb1002.eqiad.wmnet', # slave
-			'redisConfig' => array(
-				'connectTimeout' => 2,
-				'password' => $wmgRedisPassword,
-				'compression' => 'gzip'
-			),
-			'daemonized' => true
-		),
-		'rdb2' => array(
-			'class'       => 'JobQueueRedis',
-			'redisServer' => 'rdb1003.eqiad.wmnet', # master
-			#'redisServer' => 'rdb1004.eqiad.wmnet', # slave
-			'redisConfig' => array(
-				'connectTimeout' => 2,
-				'password' => $wmgRedisPassword,
-				'compression' => 'gzip'
-			),
-			'daemonized' => true
-		),
-		'rdb3' => array(
-			'class'       => 'JobQueueRedis',
-			'redisServer' => 'rdb1007.eqiad.wmnet', # master
-			#'redisServer' => 'rdb1008.eqiad.wmnet', # slave
-			'redisConfig' => array(
-				'connectTimeout' => 2,
-				'password' => $wmgRedisPassword,
-				'compression' => 'gzip'
-			),
-			'daemonized' => true
-		),
+	'class' => 'JobQueueFederated',
+	'configByPartition' => array(
+		'rdb1-6379' => array(
+				'redisServer' => 'rdb1001.eqiad.wmnet:6379', # master
+				#'redisServer' => 'rdb1002.eqiad.wmnet:6379', # slave
+			) + $wmgRedisQueueBaseConfig,
+		'rdb2-6379' => array(
+				'redisServer' => 'rdb1003.eqiad.wmnet:6379', # master
+				#'redisServer' => 'rdb1004.eqiad.wmnet:6379', # slave
+			) + $wmgRedisQueueBaseConfig,
+		'rdb3-6379' => array(
+				'redisServer' => 'rdb1007.eqiad.wmnet:6379', # master
+				#'redisServer' => 'rdb1008.eqiad.wmnet:6379', # slave
+			) + $wmgRedisQueueBaseConfig,
+		'rdb3-6380' => array(
+				'redisServer' => 'rdb1007.eqiad.wmnet:6380', # master
+				#'redisServer' => 'rdb1008.eqiad.wmnet:6380', # slave
+			) + $wmgRedisQueueBaseConfig,
 	),
-	'sectionsByWiki'      => array(), // default
+	'sectionsByWiki' => array(), // default
 	'partitionsBySection' => array(
-		'default' => array( 'rdb1' => 50, 'rdb2' => 50, 'rdb3' => 50 ),
+		'default' => array(
+			'rdb1-6379' => 50,
+			'rdb2-6379' => 50,
+			'rdb3-6379' => 50,
+			'rdb3-6380' => 50,
+		),
 	)
 );
 // Note: on server failure, this should be changed to any other redis server
 $wgJobQueueAggregator = array(
-	'class'        => 'JobQueueAggregatorRedis',
-	'redisServers' => array( // all after the first are fallbacks
-		'rdb1001.eqiad.wmnet',
-		'rdb1003.eqiad.wmnet',
-		'rdb1007.eqiad.wmnet',
+	'class' => 'JobQueueAggregatorRedis',
+	'redisServers' => array(
+		'rdb1001.eqiad.wmnet:6379', // preferred
+		'rdb1003.eqiad.wmnet:6379', // fallback
+		'rdb1007.eqiad.wmnet:6379', // fallback
 	),
-	'redisConfig'  => array(
+	'redisConfig' => array(
 		'connectTimeout' => 2,
 		'password' => $wmgRedisPassword,
 	)
