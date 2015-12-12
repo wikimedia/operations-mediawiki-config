69b930f88fd7f38a1e4d517d97c6f4e117744406
Sébastien Santoro <dereckson@espace-win.org>
2015-12-12 08:33:09 +0000

    Set site name on sr.wiktionary
    New site name: Викиречник
    
    Namespace NS_PROJET has already this name.
    
    Bug: T121278
    Change-Id: I62c10d4228ad82898f15c6e460f79c7c205da732

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -1835,6 +1835,7 @@ $wgConf->settings = array(
 	'srwikinews' => 'Викивести',
 	'srwikiquote' => 'Викицитат', // T111247
 	'srwikisource' => 'Викизворник',
+	'srwiktionary' => 'Викиречник', // T121278
 	'strategywiki' => 'Strategic Planning',
 	'stewardwiki' => 'Steward Wiki',
 	'tawiki' => 'விக்கிப்பீடியா',
