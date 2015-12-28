a037be7f0618c311143787e508dd8fd5ccefb13b
Jaime Crespo <jcrespo@wikimedia.org>
2015-12-28 13:42:50 +0100

    Emergency depool of db1050 for cloning to to db1022
    Bug: T105879
    Change-Id: I228f04efef7379e35004447af5d09e363f224f80

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -134,9 +134,9 @@ $wgLBFactoryConf = array(
 	's6' => array(
 		'db1023' => 0,   # 1.4TB  64GB
 #		'db1022' => 100, # 1.4TB  64GB (data consistency issue)
-		'db1030' => 0,   # 1.4TB  64GB, vslow, dump
-		'db1037' => 0,   # 1.4TB  64GB, watchlist, recentchanges, contributions, logpager
-		'db1050' => 400, # 2.8TB  64GB
+		'db1030' => 100, # 1.4TB  64GB, vslow, dump
+		'db1037' => 100, # 1.4TB  64GB, watchlist, recentchanges, contributions, logpager
+#		'db1050' => 400, # 2.8TB  64GB (depool for cloning)
 		'db1061' => 500, # 2.8TB 128GB
 	),
 	's7' => array(
