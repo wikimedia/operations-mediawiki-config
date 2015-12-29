0dc9dec72b99bea9f278bb07a7dff768b693a2c4
Jaime Crespo <jcrespo@wikimedia.org>
2015-12-29 10:30:32 +0100

    Setting weight values for s6 to original production values
    After emergency depool.
    
    Bug: T105879
    Change-Id: Ided777ec5e26935a686c33510ffc5c68dd0c9ff7

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -133,10 +133,10 @@ $wgLBFactoryConf = array(
 	),
 	's6' => array(
 		'db1023' => 0,   # 1.4TB  64GB
-		'db1022' => 50,  # 1.4TB  64GB (low weight)
-		'db1030' => 100, # 1.4TB  64GB, vslow, dump
-		'db1037' => 100, # 1.4TB  64GB, watchlist, recentchanges, contributions, logpager
-		'db1050' => 50,  # 2.8TB  64GB (low weight)
+		'db1022' => 200, # 1.4TB  64GB
+		'db1030' => 0,   # 1.4TB  64GB, vslow, dump
+		'db1037' => 0,   # 1.4TB  64GB, watchlist, recentchanges, contributions, logpager
+		'db1050' => 400, # 2.8TB  64GB
 		'db1061' => 500, # 2.8TB 128GB
 	),
 	's7' => array(
