ea4b855b88f66b4954a7abfe2307acebf7b92a39
Sébastien Santoro <dereckson@espace-win.org>
2016-01-19 23:47:59 +0000

    Add *.archives.gov to wgCopyUploadsDomains
    Bug: T124080
    Change-Id: I0016223b9c8815cc07176189deee5ccca7699572

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -11789,6 +11789,7 @@ $wgConf->settings = array(
 		'digital.slub-dresden.de',
 		'grossstadtgeschichten-berlin.de',
 		'content.staatsbibliothek-berlin.de',
+		'*.archives.gov',                   // US National Archives - T124080
 	),
 ),
 
