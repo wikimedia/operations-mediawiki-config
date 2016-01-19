523a604fc908de9b07f83da315b5ba742b68a95d
Alex Monk <krenair@gmail.com>
2016-01-19 18:24:10 +0000

    Disable global abuse filters on nonglobal wikis
    Bug: T124002
    Change-Id: I1b5570d61a40e3df4186086b3cf9b1caefb0e4cf

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -15574,6 +15574,7 @@ $wgConf->settings = array(
 	'medium' => true,
 	'private' => false,
 	'fishbowl' => false,
+	'nonglobal' => false,
 
 	'enwikisource' => true, // T78496
 	'frwiki' => true, // T120568
