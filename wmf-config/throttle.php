bb9596e864bee1fe3b9d56c993d8bc84577272e3
Sébastien Santoro <dereckson@espace-win.org>
2016-01-28 23:23:43 +0000

    Santiago Editatón throttle rule
    New throttle rule:
        * Event name .... Santiago Editatón
        * Event start ... 2016-02-02 20:00 UTC
        * Event end ..... 2016-02-05 01:00 UTC
        * IP ............ 181.118.160.217
        * Projects ...... es., en., commons.
        * Attendees ..... 30 (margin set at 50)
    
    Cleaned former throttle rules.
    
    Bug: T125081
    Change-Id: I8de8ba303b324447abab9252acf313f9a61dd3a8

--- wmf-config/throttle.php
+++ wmf-config/throttle.php
@@ -28,11 +28,11 @@ $wmgThrottlingExceptions[] = array( // T110352 (long term)
 	'value'  => 50
 );
 
-$wmgThrottlingExceptions[] = array( // T124284 - Santiago Wikipedia editing workshop
-	'from'   => '2016-01-26T21:00 +0:00',
-	'to'     => '2016-01-27T01:00 +0:00',
-	'IP'     => array( '163.247.43.51', '200.54.105.35' ),
-	'dbname' => array( 'eswiki', 'enwiki', 'eswikivoyage' ),
+$wmgThrottlingExceptions[] = array( // T125081 - Santiago Wikipedia editing workshop
+	'from'   => '2016-02-02T20:00 +0:00',
+	'to'     => '2016-02-05T01:00 +0:00',
+	'IP'     => '181.118.160.217',
+	'dbname' => array( 'eswiki', 'enwiki', 'commonswiki' ),
 	'value'  => 50 // 30 expected
 );
 
