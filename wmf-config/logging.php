4b330242b7e1c7a593fe241589c85269c662121e
Max Semenik <maxsem.wiki@gmail.com>
2016-01-28 17:03:44 -0800

    Reduce Kafka timeouts
    Change-Id: I421c901f05673d8ee4eb65bf7c7d198308a36e3d
    Depends-On: I8f01fa61d916aeaa831a84e12b6fae08d04ca046
    Bug: T125084

--- wmf-config/logging.php
+++ wmf-config/logging.php
@@ -190,6 +190,8 @@ foreach ( $wmgMonologChannels as $channel => $opts ) {
 						'alias' => array(),
 						'swallowExceptions' => true,
 						'logExceptions' => 'wfDebugLogFile',
+						'sendTimeout' => 0.01,
+						'recvTimeout' => 0.5,
 					)
 				),
 				'formatter' => 'avro'
