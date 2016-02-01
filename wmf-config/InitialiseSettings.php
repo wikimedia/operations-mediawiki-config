1fa7f3c28cf0340ba367c1763923050e6d7b839b
Addshore <addshorewiki@gmail.com>
2016-02-01 09:06:15 +0000

    Revert "Revert "wgRCWatchCategoryMembership true on wikipedias & commons""
    This reverts commit 6d70abd534ce59f4705dfae7d10e8a9ad5e70805.
    
    To be deployed after:
      group2 to 1.27.0-wmf.12 All Wikipedias
      Evening SWAT
    
    Phab tickets relating to the revert:
      https://phabricator.wikimedia.org/T125147
      https://phabricator.wikimedia.org/T125209
    
    Change-Id: Ie834d2d8865e5b1f02c626c0e38feaccd48899c8

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -15398,7 +15398,8 @@ $wgConf->settings = array(
 	'testwiki' => true,
 	'testwikidatawiki' => true,
 	'zerowiki' => true,
-	'dewiki' => true,
+	'wikipedia' => true,
+	'commonswiki' => true,
 ),
 
 'wmgRelatedSitesPrefixes' => array(
