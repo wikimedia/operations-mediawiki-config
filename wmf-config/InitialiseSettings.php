8fe52c901ff17d84b38a19599e3f8101e545c4c4
maurelio <strigiwm@gmail.com>
2016-02-01 22:24:49 +0100

    Enabling Extension:ShortUrl for bhwiki
    Fixes T113348
    
    Bug: T113348
    Change-Id: If8e93dfe67a5f700a4bbf5274f70fe5172d9fdc1

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -14655,6 +14655,7 @@ $wgConf->settings = array(
 
 'wmgUseShortUrl' => array(
 	'default' => false,
+	'bhwiki' => true, // T113348
 	'bnwiki' => true, // T62956
 	'eswikibooks' => true, // T96668
 	'hiwiki' => true,
