1a0f9ed9bd8f9ff32ddecbb9a06a250e3806e537
Jaime Crespo <jcrespo@wikimedia.org>
2016-02-10 00:25:50 +0100

    Set mediawiki back in read-only mode
    Change-Id: I390f0d6b32e8431e3e008658d0a3df1f44d9be66

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -543,7 +543,7 @@ $wgDefaultExternalStore = array(
 	'DB://cluster25',
 );
 
-$wgLBFactoryConf['readOnlyBySection']['s2'] =
-'Scheduled maintenance, s2 wikis in read-only mode for a few minutes';
+#$wgLBFactoryConf['readOnlyBySection']['s2'] =
+#'Scheduled maintenance, s2 wikis in read-only mode for a few minutes';
 # $wgLBFactoryConf['readOnlyBySection']['s2a'] =
 # 'Emergency maintenance, need more servers up, new estimate ~18:30 UTC';
