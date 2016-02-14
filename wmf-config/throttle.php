1e7e5fd5b4412e216f245cbf4d5a50e843853855
maurelio <strigiwm@gmail.com>
2016-02-14 23:34:23 +0100

    Termporary lift of IP cap for an Edit-a-thon
    Needs deployement before event starts, on Saturday 20 February 2016.
    
    Lifting IP cap restrictions for Edit-a-thon in the State of Nebraska, USA.
    
    Bug: T126939
    Change-Id: I2eeca627305ec566793c393c9c4159108d140f07

--- wmf-config/throttle.php
+++ wmf-config/throttle.php
@@ -43,6 +43,14 @@ $wmgThrottlingExceptions[] = array( // Wikipedia:Wikimedia Deutschland/WPFF Berl
 	'value'  => 50
 );
 
+$wmgThrottlingExceptions[] = array( // T126939 - Wikipedia Edit-a-thon at Do Space in Omaha, NE, USA
+	'from'   => '2016-02-20T04:00 +0:00',
+	'to'     => '2016-02-20T09:00 +0:00',
+	'IP'     => '97.107.199.77',
+	'dbname' => array( 'enwiki', 'commonswiki' ),
+	'value'  => 60 // 50 expected
+);
+
 ## Add throttling definitions above.
 
 /**
