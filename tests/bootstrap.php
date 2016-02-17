9b9577f2ef315d94cce0c9d2b9fdab0113c543a4
Kunal Mehta <legoktm@member.fsf.org>
2016-02-16 23:33:27 -0800

    Output PHP version before running phpunit tests
    Change-Id: Ia038ab9d1d5d39c1534d92a44aaaca157d6ac509

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,3 +4,6 @@
 require_once __DIR__ . "/DBList.php";
 require_once __DIR__ . "/Provide.php";
 require_once __DIR__ . "/Defines.php";
+echo defined( 'HHVM_VERSION' ) ?
+	'Using HHVM ' . HHVM_VERSION . ' (' . PHP_VERSION . ")\n" :
+	'Using PHP ' . PHP_VERSION . "\n";
