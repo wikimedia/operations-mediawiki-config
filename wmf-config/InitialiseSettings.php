ad5052bc8d81cfb20eba4c77c534c49be5e790a3
James D. Forrester <jforrester@wikimedia.org>
2016-02-23 13:48:00 -0800

    VisualEditor: Enable in two extra namespaces on French Wiktionary
    Bug: T127819
    Change-Id: I0a907eaac0b5e584d8fda6a92f1daed8a60b58cc

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -13248,6 +13248,10 @@ $wgConf->settings = array(
 	),
 
 	// Wiktionaries
+	'+frwiktionary' => array(
+		100 /* Annexe */ => true, // T127819
+		106 /* Thésaurus */ => true // T127819
+	),
 	'svwiktionary' => array(
 		NS_USER => true // T59356
 	),
