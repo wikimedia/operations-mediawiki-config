cb53dd6f636820ac6e538dd9e37e71adeaff0f46
elukey <ltoscano@wikimedia.org>
2016-03-01 10:37:24 +0100

    Remove mc1002 from the Lock Manager pool for maintenance.
    Bug: T123711
    Change-Id: I3cec9e02f6bb60b131112360aa0a9a43662baea4

--- wmf-config/filebackend-production.php
+++ wmf-config/filebackend-production.php
@@ -131,16 +131,17 @@ $wgFileBackends[] = array(
 
 // Lock manager config must use the master datacenter
 // @TODO: configure as a switch
+// Hosts removed from the pool for maintenance:
+// 'rdb2' => '10.64.0.181',
 $wgLockManagers[] = array(
 	'name'         => 'redisLockManager',
 	'class'        => 'RedisLockManager',
 	'lockServers'  => array(
 		'rdb1' => '10.64.0.180',
-		'rdb2' => '10.64.0.181',
 		'rdb3' => '10.64.0.182'
 	),
 	'srvsByBucket' => array(
-		0 => array( 'rdb1', 'rdb2', 'rdb3' )
+		0 => array( 'rdb1', 'rdb3' )
 	),
 	'redisConfig'  => array(
 		'connectTimeout' => 2,
