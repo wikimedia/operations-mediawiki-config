e57c9ddeb6ff8782c9a8b542d59af37b8ff54395
Riccardo Coccioli <rcoccioli@wikimedia.org>
2016-03-09 16:53:34 +0100

    Change codfw extenal storage topology
    Promotes one of the new servers as local master in codfw for es2 and es3
    
    Bug: T127330
    Change-Id: Iae05521063eb319760bfddedb93932957bc1d0c8

--- wmf-config/db-codfw.php
+++ wmf-config/db-codfw.php
@@ -349,21 +349,21 @@ $wgLBFactoryConf = array(
 	# es2
 	'cluster24' => array(
 		'10.64.32.184'  => 0, # es1015, master
-		'10.192.0.27'   => 1, # es2005, codfw local master - compressed data
+		'10.192.32.130' => 1, # es2015, codfw local master
+		'10.192.0.27'   => 1, # es2005 - compressed data
 		'10.192.0.28'   => 3, # es2006
 		'10.192.0.29'   => 3, # es2007
 		'10.192.0.141'  => 1, # es2014 - compressed data
-		'10.192.32.130' => 1, # es2015
 		'10.192.48.41'  => 1, # es2016
 	),
 	# es3
 	'cluster25' => array(
 		'10.64.16.187'  => 0, # es1019, master
-		'10.192.16.30'  => 1, # es2009, codfw local master
+		'10.192.16.172' => 1, # es2018, codfw local master
+		'10.192.16.30'  => 1, # es2009
 		'10.192.16.29'  => 3, # es2008
 #		'10.192.16.31'  => 3, # es2010 - depooled because of T127769 - volans
 		'10.192.0.142'  => 1, # es2017
-		'10.192.16.172' => 1, # es2018
 		'10.192.48.42'  => 1, # es2019
 	),
 	# ExtensionStore shard1 - initially for AFTv5
