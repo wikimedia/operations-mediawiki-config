fed4262e9b2394df032f1462bb34323ed8a4fd6c
BryanDavis <bdavis@wikimedia.org>
2016-03-17 22:24:10 +0000

    Revert "Temporarily disable ApiRequest logging"
    This reverts commit f46e75d141f763607c575155b34c0e4692ec1630.
    
    Change-Id: I18e1be86f02e61ee0f96dad41f1e0ad0ccc0b3f4

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -4436,6 +4436,10 @@ $wgConf->settings = array(
 			'schema' => file_get_contents( __DIR__ . '/event-schemas/avro/mediawiki/CirrusSearchRequestSet/111448028943.avsc' ),
 			'revision' => 111448028943,
 		),
+		'ApiRequest' => array(
+			'schema' => file_get_contents( __DIR__ . '/event-schemas/avro/mediawiki/ApiRequest/101453221640.avsc' ),
+			'revision' => 101453221640,
+		),
 	),
 ),
 # @} end of wmgAvroSchemas
