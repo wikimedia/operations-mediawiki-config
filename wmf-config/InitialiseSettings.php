e440b46f0fe2344ff13e8ef0b262cd845b1ba12e
Sébastien Santoro <dereckson@espace-win.org>
2016-03-23 16:27:55 +0000

    Remove T44894 FIXME note
    The Swahili community has decided to restrict page creation for IPs.
    
    Plan was to reevaluate the situation around 2015-09-15. It has then
    be decided to keep the restriction, as there aren't any volunteers
    on sw.wikipedia comfortable to use AbuseFilter and implement filters.
    
    We can so discard the comment to revisit the issue.
    
    Change-Id: I49a0169b73809702196c7d4eeec90e13286b1136

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -8516,7 +8516,7 @@ $wgConf->settings = array(
 		'rollbacker' => array( 'rollback' => true ), // T93339
 	),
 	'+swwiki' => array(
-		'*' => array( 'createpage' => false ), // T44894 FIXME recheck for need around September 15 2015
+		'*' => array( 'createpage' => false ), // T44894
 	),
 	'tawiki' => array(
 		'nocreate' => array( 'createpage' => false, ),
