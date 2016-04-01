81873b578e82248288991891291fb6e2b762793c
James D. Forrester <jforrester@wikimedia.org>
2016-04-01 14:31:42 +0300

    [Cleanup] Remove VisualEditor AutoAccountEnable config now unused
    Change-Id: I7c7395d7301ceab165b840246e83ccee99d5d998

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -13536,9 +13536,6 @@ $wgConf->settings = array(
 // wiki where it's not already on by default.
 'wmgVisualEditorAutoAccountEnable' => array(
 	'default' => false,
-	'dewiki' => true,
-	'enwiki' => true,
-	'eswiki' => true,
 ),
 
 // Whether VisualEditor should pay attention to the -autodisable user preference
