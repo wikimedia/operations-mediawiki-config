e97ce92a2b7428658d459878f04d216c3f2cb1b7
Sébastien Santoro <dereckson@espace-win.org>
2016-04-03 01:14:46 +0000

    Use extension registration for Gadgets
    Get rid of $wg = $wmg hack.
    
    Bug: T119117
    Change-Id: Idc3ebadae1dd748cdad8e82506c57a7f4b7fdf0b

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -11239,8 +11239,8 @@ $wgConf->settings = [
 	'default' => true, // set 2007-12-17 by brion
 	'loginwiki' => false,
 	'votewiki' => false,
-],
-'wmgSpecialGadgetUsageActiveUsers' => [
+),
+'wgSpecialGadgetUsageActiveUsers' => array(
 	'default' => true,
 	'enwiki' => false, // T121949
 ],
