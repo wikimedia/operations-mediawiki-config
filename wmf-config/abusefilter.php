b3adeec3595db2694d4d585aa69180eb031c1964
Bartosz Dziewoński <matma.rex@gmail.com>
2016-04-11 22:52:04 +0200

    Enable $wgAbuseFilterProfile for commonswiki
    The filters on Wikimedia Commons have been running into the condition
    limit recently. To avoid raising the limit (T132048), let's enable
    profiling for a few days to make it possible to optimize them.
    
    Bug: T132200
    Change-Id: I0e26e6a58d44dcbcc6389e4641e037bcda3a8f43

--- wmf-config/abusefilter.php
+++ wmf-config/abusefilter.php
@@ -70,6 +70,7 @@ switch ( $wgDBname ) {
 		$wgGroupPermissions['sysop']['abusefilter-log-private'] = true;
 		break;
 	case 'commonswiki':
+		$wgAbuseFilterProfile = true; // T132200
 		$wgGroupPermissions['sysop']['abusefilter-modify-restricted'] = true;
 		break;
 	case 'cswiktionary':
