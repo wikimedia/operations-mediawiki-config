f301918bd1bd90373db3db7791f1e425af1d2b14
Sébastien Santoro <dereckson@espace-win.org>
2016-04-13 22:51:06 +0000

    Consider all pages as valid content articles on pl.wikisource
    Bug: T131771
    Change-Id: Ifa049c1689c8997355440f10c7e69c81310466de

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -12259,6 +12259,7 @@ $wgConf->settings = array(
 	'enwikibooks' => 'comma', // T29256
 	'ptwikibooks' => 'comma', // T29256
 	'guwikisource' => 'any', // T42173
+	'plwikisource' => 'any', // T131771
 	'zhwikinews' => 'any', // T53604
 ),
 
