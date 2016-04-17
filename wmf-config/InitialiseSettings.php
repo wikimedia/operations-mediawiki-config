5c153e934a88f7b5e16a2d4b244ed76a9dfc1cdc
Sébastien Santoro <dereckson@espace-win.org>
2016-04-17 19:46:18 +0000

    Enable RC patrol on ta.wikiquote
    Bug: T132868
    Change-Id: I0dcb4a3460da085868e842558b24c1e874223009

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -6951,6 +6951,7 @@ $wgConf->settings = array(
 	'specieswiki' => true, // T89147
 	'srwiki' => true, // req by dungodung in #wikimedia-tech, 2008-02-25
 	'srwikisource' => true, // #25963
+	'tawikiquote' => true, // T132868
 	'viwiki' => true, // T7060
 	'testwiki' => true,
 	'test2wiki' => true, // request by Krinkle for testing
