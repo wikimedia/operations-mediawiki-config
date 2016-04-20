9ae6fa5b168f6201b765a5bdceae140b692602f0
Jaime Crespo <jcrespo@wikimedia.org>
2016-04-20 20:07:51 +0200

    Promote db1031 as the new x1 eqiad local master
    Change-Id: I199dcb8048838b610d575f426de689647dcf0f77

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -539,8 +539,8 @@ $wgLBFactoryConf = array(
 	),
 	# ExtensionStore shard1 - initially for AFTv5
 	'extension1' => array(
-		'10.64.16.18' => 10, # db1029, master
-		'10.64.16.20' => 20, # db1031 snapshot host
+		'10.64.16.20' => 20, # db1031, master
+		'10.64.16.18' => 10, # db1029
 	),
 ),
 
