7125651240e3a2e84ca9307bc0fc3e54e0a9a82a
Martin Urbanec <martin@urbanec.cz>
2016-04-23 14:49:19 +0200

    Add Subject namespace to hiwikibooks
    Bug: T133440
    Change-Id: Ida2d1bc20007a5ad669c4c09075f01594c98b6b3

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -6186,6 +6186,10 @@ $wgConf->settings = array(
 		104 => 'מדף', # bookshelf
 		105 => 'שיחת_מדף', # bookshelf talk
 	),
+	'hiwikibooks' => array(
+		106 => 'विषय', // Subject, T133440
+		107 => 'विषय_चर्चा', // Subject talk, T133440
+	),
 	'hywikibooks' => array( // T55162
 		102 => 'Եփութուխ', // cookbook
 		103 => 'Եփութուխի_քննարկում', // cookbook talk
