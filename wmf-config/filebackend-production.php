bc463a7a01f6002429202da51e4c9ed4629a18ce
Bartosz Dziewoński <matma.rex@gmail.com>
2016-04-27 21:33:24 +0200

    Configure 'testwiki' as foreign file repo for 'test2wiki', allow cross-wiki uploads
    * 'testwiki' now acts as a file repository for 'test2wiki' (the way
      Commons acts as a file repository for everyone). I just copy-pasted
      the configuration, I hope it works.
    * Cross-wiki uploads from 'test2wiki' will now be uploaded to 'testwiki'
      (rather than Commons).
    * Cross-wiki uploads from 'testwiki' will now be uploaded to itself
      (rather than Commons).
    
    Bug: T133305
    Change-Id: Ic21f674aef0897ce82a85cdb7df8f5ea1dbfd05f

--- wmf-config/filebackend-production.php
+++ wmf-config/filebackend-production.php
@@ -82,6 +82,20 @@ foreach ( array( 'eqiad', 'codfw' ) as $specificDC ) {
 		// When used by FileBackendMultiWrite, read from this cluster if it's the local one
 		'readAffinity'       => ( $specificDC === $wmfDatacenter )
 	);
+	$wgFileBackends[] = array( // backend config for wiki's access to shared test repo
+		'class'              => 'SwiftFileBackend',
+		'name'               => "shared-testwiki-swift-{$specificDC}",
+		'wikiId'             => "wikipedia-test",
+		'lockManager'        => 'redisLockManager',
+		'swiftAuthUrl'       => $wmfSwiftConfig[$specificDC]['authUrl'],
+		'swiftUser'          => $wmfSwiftConfig[$specificDC]['user'],
+		'swiftKey'           => $wmfSwiftConfig[$specificDC]['key'],
+		'swiftTempUrlKey'    => $wmfSwiftConfig[$specificDC]['tempUrlKey'],
+		'parallelize'        => 'implicit',
+		'cacheAuthInfo'      => true,
+		// When used by FileBackendMultiWrite, read from this cluster if it's the local one
+		'readAffinity'       => ( $specificDC === $wmfDatacenter )
+	);
 }
 /* end DC-specific Swift backend config */
 
@@ -126,6 +140,19 @@ $wgFileBackends[] = array(
 	'replication' => 'async',
 	'syncChecks'  => ( 1 | 4 ) // (size & sha1)
 );
+$wgFileBackends[] = array(
+	'class'       => 'FileBackendMultiWrite',
+	'name'        => 'shared-testwiki-multiwrite',
+	'wikiId'      => "wikipedia-test",
+	'lockManager' => 'redisLockManager',
+	# DO NOT change the master backend unless it is fully trusted or autoRsync is off
+	'backends'    => array(
+		array( 'template' => 'shared-testwiki-swift-eqiad', 'isMultiMaster' => true ),
+		array( 'template' => 'shared-testwiki-swift-codfw' ),
+	),
+	'replication' => 'async',
+	'syncChecks'  => ( 1 | 4 ), // (size & sha1)
+);
 /* end multiwrite backend config */
 
 // Lock manager config must use the master datacenter
@@ -143,23 +170,50 @@ $wgLockManagers[] = array(
 );
 
 $wgLocalFileRepo = array(
-		'class'             => 'LocalRepo',
-		'name'              => 'local',
-		'backend'           => 'local-multiwrite',
-		'url'               => $wgUploadBaseUrl ? $wgUploadBaseUrl . $wgUploadPath : $wgUploadPath,
-		'scriptDirUrl'      => $wgScriptPath,
-		'hashLevels'        => 2,
-		'thumbScriptUrl'    => $wgThumbnailScriptPath,
-		'transformVia404'   => true,
-		'initialCapital'    => $wgCapitalLinks,
-		'deletedHashLevels' => 3,
-		'abbrvThreshold'    => 160,
-		'isPrivate'         => $wmgPrivateWiki,
-		'zones'             => $wmgPrivateWiki
-			? array(
-				'thumb' => array( 'url' => "$wgScriptPath/thumb_handler.php" ) )
-			: array(),
+	'class'             => 'LocalRepo',
+	'name'              => 'local',
+	'backend'           => 'local-multiwrite',
+	'url'               => $wgUploadBaseUrl ? $wgUploadBaseUrl . $wgUploadPath : $wgUploadPath,
+	'scriptDirUrl'      => $wgScriptPath,
+	'hashLevels'        => 2,
+	'thumbScriptUrl'    => $wgThumbnailScriptPath,
+	'transformVia404'   => true,
+	'initialCapital'    => $wgCapitalLinks,
+	'deletedHashLevels' => 3,
+	'abbrvThreshold'    => 160,
+	'isPrivate'         => $wmgPrivateWiki,
+	'zones'             => $wmgPrivateWiki
+		? array(
+			'thumb' => array( 'url' => "$wgScriptPath/thumb_handler.php" ) )
+		: array(),
 );
+// test2wiki uses testwiki as foreign file repo (e.g. local => testwiki => commons)
+if ( $wgDBname === 'test2wiki' ) {
+	$wgForeignFileRepos[] = array(
+		'class'            => 'ForeignDBViaLBRepo',
+		'name'             => 'testwikirepo',
+		'backend'          => 'shared-testwiki-multiwrite',
+		'url'              => "//upload.wikimedia.org/wikipedia/test",
+		'hashLevels'       => 2,
+		'thumbScriptUrl'   => false,
+		'transformVia404'  => true,
+		'hasSharedCache'   => true,
+		'descBaseUrl'      => "https://test.wikipedia.org/wiki/File:",
+		'scriptDirUrl'     => "https://test.wikipedia.org/w",
+		'favicon'          => "/static/favicon/black-globe.ico",
+		'fetchDescription' => true,
+		'descriptionCacheExpiry' => 86400,
+		'wiki'             => 'testwiki',
+		'initialCapital'   => true,
+		'zones'            => array( // actual swift containers have 'local-*'
+			'public'  => array( 'container' => 'local-public' ),
+			'thumb'   => array( 'container' => 'local-thumb' ),
+			'temp'    => array( 'container' => 'local-temp' ),
+			'deleted' => array( 'container' => 'local-deleted' )
+		),
+		'abbrvThreshold'   => 160 /* Keep in sync with with local repo on testwiki or things break. */
+	);
+}
 if ( $wgDBname != 'commonswiki' ) {
 	$wgForeignFileRepos[] = array(
 		'class'            => 'ForeignDBViaLBRepo',
