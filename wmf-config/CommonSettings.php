c340b3c237014c3f05cb2e29026933e0b0160637
Glaisher <glaisher.wiki@gmail.com>
2016-05-04 23:02:16 +0500

    Configure $wgCheckUserCAMultiLock for CentralAuth wikis
    Bug: T128605
    Change-Id: Ibbfe5e4d283100ddc64eb55da1c27f5b9ba3c3b7

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -3156,6 +3156,14 @@ if ( $wmfRealm === 'labs' ) {
 if ( $wmgUseCheckUser ) {
 	include( $IP . '/extensions/CheckUser/CheckUser.php' );
 	$wgCheckUserForceSummary = $wmgCheckUserForceSummary;
+	if ( $wmgUseCentralAuth ) {
+		// T128605
+		// Only for CA wikis - will break stuff otherwise
+		$wgCheckUserCAMultiLock = [
+			'centralDB' => 'metawiki',
+			'groups' => [ 'steward' ]
+		];
+	}
 }
 
 // T39211
