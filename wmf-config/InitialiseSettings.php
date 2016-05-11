796843e3b685436f51484f8474b30ba00c261f1c
addshore <addshorewiki@gmail.com>
2016-05-11 09:56:36 +0100

    Dont log dewiki_diffstats to logstash
    Change-Id: I4b7f30eebf254a288baaa9d3f755b139a255ffff

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -4498,6 +4498,7 @@ $wgConf->settings = [
 		'cite' => 'debug',
 		'collection' => 'debug', // -cscott for T73675
 		'DBPerformance' => [ 'logstash' => 'debug', 'udp2log' => 'warning' ],
+		'dewiki_diffstats' => [ 'logstash' => false ],
 		'diff' => 'debug',
 		'Echo' => 'debug',
 		'es-hit' => 'debug',
