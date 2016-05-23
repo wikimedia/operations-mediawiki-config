99a587857710ad3d1929d0abdb42ed2b6ca3cfbe
Kartik Mistry <kartik.mistry@gmail.com>
2016-05-23 10:48:39 +0530

    Enable Compact Language Links as default in Beta
    Introduce new variables first.
    
    Bug: T134966
    Change-Id: Idec9ce655f8fc755ae38de4bc6c40a68e3b1877e

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -12590,8 +12590,16 @@ $wgConf->settings = [
 	'nonbetafeatures' => false,
 ],
 
+// To be removed
 'wmgULSCompactLinks' => [
 	'default' => true,
+	'nonbetafeatures' => false,
+],
+
+'wmgULSCompactLanguageLinksBetaFeature' => [
+	'default' => true,
+	'testwiki' => false,
+	'test2wiki' => false,
 	'nonbetafeatures' => false,
 ],
 
@@ -15930,6 +15938,16 @@ $wgConf->settings = [
 
 'wmgULSEventLogging' => [
 	'default' => true,
+],
+
+// Whether Compact Links is enabled for new accounts *by default*
+'wmgULSCompactLinksForNewAccounts' => [
+	'default' => false,
+],
+
+// Whether Compact Links is enabled for anonymous users *by default*
+'wmgULSCompactLinksEnableAnon' => [
+	'default' => false,
 ],
 
 'wmgWikibaseCachePrefix' => [
