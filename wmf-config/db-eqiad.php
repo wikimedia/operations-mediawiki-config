ee387394a7fdbbffb4e3c89be672edf3d86f4a55
Jcrespo <jcrespo@wikimedia.org>
2016-05-25 15:41:52 +0000

    Revert "Depool db1056 for hardware maintenance"
    This reverts commit f14ec8b930ee0bdffb69300af7fe13bcbac236af.
    
    Change-Id: Ic5091d8ec6039f02ee30c56cc757387631af4dc3

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -123,7 +123,7 @@ $wgLBFactoryConf = array(
 		'db1042' => 0,   # 1.4TB  64GB, master
 		'db1040' => 0,   # 1.4TB  64GB, vslow, dump, old master
 		'db1019' => 0,   # 1.4TB  64GB, watchlist, recentchanges, contributions, logpager
-#		'db1056' => 100, # 2.8TB  96GB, api - down for maintenance T136136
+		'db1056' => 100, # 2.8TB  96GB, api
 		'db1059' => 100, # 2.8TB  96GB, api
 		'db1064' => 500, # 2.8TB 160GB
 		'db1068' => 500, # 2.8TB 160GB
@@ -262,7 +262,7 @@ $wgLBFactoryConf = array(
 			'db1040' => 1,
 		),
 		'api' => array(
-#			'db1056' => 1,
+			'db1056' => 1,
 			'db1059' => 1,
 		),
 		'watchlist' => array(
