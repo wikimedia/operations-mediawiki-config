ba49cdda77957b9bdcbb5d4b7447f5f1609be2d6
Roan Kattouw <roan.kattouw@gmail.com>
2016-06-01 11:43:42 -0700

    Enable Flow beta feature on frwiki
    Bug: T136684
    Change-Id: Ifcb3ed86900fd65c8dc374351538b0281da740dc

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -15659,6 +15659,7 @@ $wgConf->settings = [
 	'mediawikiwiki' => true,
 	'bswiki' => true,
 	'cawiki' => true, // T120829
+	'frwiki' => true, // T136684
 	'frwikisource' => true, // T132914
 	'frwikivoyage' => true, // T135702
 	'nowiki' => true, // T132693
