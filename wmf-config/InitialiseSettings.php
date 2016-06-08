38d638c32dbf495eb6ae26b069fa95080afd6350
Bartosz Dziewoński <matma.rex@gmail.com>
2016-06-08 21:14:49 +0200

    Update cross-wiki upload configuration for I2489004271078a
    This is a no-op change. It should be deployed after I2489004271078a is
    merged, but before it is deployed. Old code is compatible with both
    old and new config, but new code is only compatible with new config.
    
    Change-Id: Icefaa6a226b62270bd90b1ea15c6aac7e27eaa11

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -12487,9 +12487,9 @@ $wgConf->settings = [
 
 'wgForeignUploadTargets' => [
 	'default' => [ 'shared' ],
-	'commonswiki' => [],
+	'commonswiki' => [ '' ],
 	'test2wiki' => [ 'testwikirepo' ],
-	'testwiki' => [],
+	'testwiki' => [ '' ],
 ],
 
 'wgForeignUploadTestEnabled' => [
