9fd22d665e9a072295c99b5edeee9cfdf8deb92c
Roan Kattouw <roan.kattouw@gmail.com>
2016-06-20 16:48:41 +0100

    Enable Echo transition flags in beta labs for testing
    Bug: T132954
    Change-Id: Ibddc138cc2e862ecd9106d98320ccb94f1806732

--- wmf-config/CommonSettings-labs.php
+++ wmf-config/CommonSettings-labs.php
@@ -315,6 +315,10 @@ if ( $wmgUseEcho && $wmgUseCentralAuth ) {
 	$wgEchoSharedTrackingDB = 'wikishared';
 	// Set cluster back to false, to override CommonSettings.php setting it to 'extension1'
 	$wgEchoSharedTrackingCluster = false;
+
+	// Testing these transition flags in beta
+	$wgEchoSectionTransition = true;
+	$wgEchoBundleTransition = true;
 }
 
 if ( $wmgUseGraph ) {
