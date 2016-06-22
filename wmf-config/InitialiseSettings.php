83721dc27d3ecadb550a759e9004c794ad0cb792
Martin Urbanec <martin@urbanec.cz>
2016-06-22 14:34:23 +0200

    Add www.wpc.ncep.noaa.gov to wgCopyUploadsDomains
    Bug: T138383
    Change-Id: Iaba6b455ae78e16245c1b6021dd8be01a3a5e5a3

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -12175,7 +12175,8 @@ $wgConf->settings = [
 		'museudaimigracao.org.br',          // Immigration museum in Brazil - T134566
 		'*.tasnimnews.com',                 // CC-BY 4.0 content - T134472
 		'*.khamenei.ir',                    // CC-BY 4.0 content - T134472
-		'images.nypl.org'                   // New York Public Library - T137687
+		'images.nypl.org',                  // New York Public Library - T137687
+		'www.wpc.ncep.noaa.gov'             // National Oceanic and Atmospheric Administration - T138383
 	],
 ],
 
