71c05dda6b0fde06c56c1d356ff74311d6ec156d
jdlrobson <jdlrobson@gmail.com>
2016-06-22 16:42:44 -0700

    Complete list of legacy main pages, switch default to false
    After auditing which wiki projects use the legacy wgMFSpecialCaseMainPage
    option, add them all to the mobilemainpagelegacy dblist.
    
    This will force all new wikis and existing wikis not currently using
    the special casing to be forced to not use it and educate existing
    projects to move away from using it.
    
    Additional changes:
    * Add wikispecies to the all.dblist
    
    Bug: T138425
    Change-Id: I0d5e658663fef08bd1cae1d059789af6b8145bee

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -14246,10 +14246,7 @@ $wgConf->settings = [
 ],
 
 'wgMFSpecialCaseMainPage' => [
-	'default' => true,
-	'wikidata' => false,
-	'wikiquote' => false, // T138129
-	'wiktionary' => false, // T138129
+	'default' => false,
 	'mobilemainpagelegacy' => true,
 ],
 
