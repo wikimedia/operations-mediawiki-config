e76a80ed95388004f58ec6cdfa0e0853af10825c
jhobs <jhobson@wikimedia.org>
2016-06-27 17:58:47 -0400

    Introduce variable for wikidata taglines
    Bug: T138738
    Change-Id: Id2e38fd5b60efeb3d4459d9a00ac125364e784b5
    Depends-On: Idb747699ebbba3e40100f697848cc10a980f1f0a

--- wmf-config/mobile.php
+++ wmf-config/mobile.php
@@ -14,7 +14,6 @@ if ( $wmgMobileFrontend ) {
 	$wgMFContentNamespace = $wmgMFContentNamespace;
 	$wgMFPhotoUploadAppendToDesc = $wmgMFPhotoUploadAppendToDesc;
 	$wgMFUseWikibaseDescription = $wmgMFUseWikibaseDescription;
-	$wgMFDisplayWikibaseDescription = $wmgMFDisplayWikibaseDescription;
 	$wgMFMobileFormatterHeadings = $wmgMFMobileFormatterHeadings;
 
 	if ( $wmgMobileFrontendLogo ) {
