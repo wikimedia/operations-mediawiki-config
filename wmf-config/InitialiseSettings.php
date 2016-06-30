43e7bc1148851b3b4b546a8c1f92a18f0e19e290
MaxSem <maxsem.wiki@gmail.com>
2016-06-30 00:13:12 +0000

    Revert "Cleanup: Move never-altered GlobalBlockingBlockXFF into CommonSettings"
    This reverts commit b4d232a8362283b58b09d08e3d767242aa5002d7.
    
    Change-Id: I772ca6b7d2cae523e95c1b40148e20e4a628fb87

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -16834,6 +16834,11 @@ $wgConf->settings = [
 ],
 ### End (roughly) of general extensions ########################
 
+// Apply blocks to IPs in XFF (T25343)
+'wmgUseXFFBlocks' => [
+	'default' => true,
+],
+
 'wgUseTidy' => [
 	'default' => true,
 	'foundationwiki' => false,
