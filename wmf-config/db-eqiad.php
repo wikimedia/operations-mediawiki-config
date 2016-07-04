2ed0791d9ce85cfaea6ec08dffd1c463b6ccd50a
Jaime Crespo <jcrespo@wikimedia.org>
2016-07-04 09:46:27 +0200

    Failover db1034 to db1062
    Change-Id: Iaa1a7e41e70725b12587422e74e7e63e15c31fc3

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -162,7 +162,7 @@ $wgLBFactoryConf = [
 		'db1028' => 0,   # 1.4TB  64GB, vslow, dump
 		'db1034' => 0,   # 1.4TB  64GB, watchlist, recentchanges, contributions, logpager
 #		'db1039' => 0,   # 1.4TB  64GB
-#		'db1062' => 0,   # 2.8TB 128GB
+		'db1062' => 0,   # 2.8TB 128GB, watchlist, recentchanges, contributions, logpager
 		'db1079' => 500, # 3.6TB 512GB
 		'db1086' => 500, # 3.6TB 512GB
 		'db1094' => 500, # 3.6TB 512GB
@@ -358,19 +358,19 @@ $wgLBFactoryConf = [
 			'db1028' => 1,
 		],
 		'watchlist' => [
-			'db1034' => 1,
+			'db1062' => 1,
 		],
 		'recentchanges' => [
-			'db1034' => 1,
+			'db1062' => 1,
 		],
 		'recentchangeslinked' => [
-			'db1034' => 1,
+			'db1062' => 1,
 		],
 		'contributions' => [
-			'db1034' => 1,
+			'db1062' => 1,
 		],
 		'logpager' => [
-			'db1034' => 1,
+			'db1062' => 1,
 		],
 	],
 ],
