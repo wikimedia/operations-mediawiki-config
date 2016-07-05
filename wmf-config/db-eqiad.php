7b1d25957cbf54835778c4fce4e3e21f223f05f3
Jcrespo <jcrespo@wikimedia.org>
2016-07-05 12:57:21 +0200

    Fix commons recentchanges by failovering it to db1081
    Change-Id: If2a9b777ccf085feeb573393d26673a2625a4747

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -131,7 +131,7 @@ $wgLBFactoryConf = [
 		'db1059' => 0,   # 2.8TB  96GB, api
 		'db1064' => 0,   # 2.8TB 160GB, vslow, dump
 #		'db1068' => 0,   # 2.8TB 160GB
-		'db1081' => 500, # 3.6TB 512GB
+		'db1081' => 250, # 3.6TB 512GB
 		'db1084' => 500, # 3.6TB 512GB
 		'db1091' => 500, # 3.6TB 512GB
 	],
@@ -282,19 +282,19 @@ $wgLBFactoryConf = [
 			'db1059' => 1, # pooled with low weight
 		],
 		'watchlist' => [
-			'db1019' => 1,
+			'db1081' => 1,
 		],
 		'recentchanges' => [
-			'db1019' => 1,
+			'db1081' => 1,
 		],
 		'recentchangeslinked' => [
-			'db1019' => 1,
+			'db1081' => 1,
 		],
 		'contributions' => [
-			'db1019' => 1,
+			'db1081' => 1,
 		],
 		'logpager' => [
-			'db1019' => 1,
+			'db1081' => 1,
 		],
 	],
 	's5' => [
