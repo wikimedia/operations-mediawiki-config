108ff1d99191077a15a1fdf62af3b16b9a0be3ac
Matthew Flaschen <mflaschen@wikimedia.org>
2016-07-05 14:06:58 -0400

    Enable Echo transition flags everywhere
    Change-Id: Ibdf12116c1794265eb994b00ee3a90443df7a19e

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -15590,8 +15590,7 @@ $wgConf->settings = [
 
 // Whether to enable the transition flags for the bundling and recategorization changes in Echo
 'wmgEchoTransition' => [
-	'default' => false,
-	'group0'  => true,
+	'default' => true,
 ],
 
 // Thanks should be enabled for wikis with Echo
