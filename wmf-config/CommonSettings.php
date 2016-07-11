11e0e85d70382bfb27461ddd5aefab8f65e6ae40
Alex Monk <krenair@gmail.com>
2016-07-12 00:52:38 +0100

    Remove old pre-Swift directory variables referencing upload7
    wgFileStore doesn't even appear to be referenced anywhere beyond
    MediaWiki's HISTORY file.
    
    Bug: T64835
    Bug: T129586
    Change-Id: Id42124e39e24f19ba542aecc8ec69339439e37d5

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -283,8 +283,6 @@ $wgHooks['LocalisationCacheRecache'][] = function( $cache, $code, &$allData, &$p
 	return true;
 };
 
-$wgFileStore['deleted']['directory'] = "/mnt/upload7/private/archive/$site/$lang";
-
 # used for mysql/search settings
 $tmarray = getdate( time() );
 $hour = $tmarray['hours'];
@@ -1198,7 +1196,6 @@ if ( $wmgEnableCaptcha ) {
 	$wgGroupPermissions['autoconfirmed']['skipcaptcha'] = true;
 	$wgCaptchaFileBackend = 'global-multiwrite';
 	$wgCaptchaSecret = $wmgCaptchaSecret;
-	$wgCaptchaDirectory = '/mnt/upload7/private/captcha';
 	$wgCaptchaDirectoryLevels = 3;
 	$wgCaptchaStorageClass = 'CaptchaCacheStore';
 	$wgCaptchaClass = 'FancyCaptcha';
@@ -2367,7 +2364,6 @@ if ( $wmgUseMath ) {
 	} elseif ( $wmgUseMathML && $wmgUseRestbaseVRS ) {
 		$wgDefaultUserOptions['math'] = 'mathml';
 	}
-	$wgMathDirectory   = '/mnt/upload7/math'; // just for sanity
 	$wgUseMathJax      = true;
 	// This variable points to non-WMF servers by default.
 	// Prevent accidental use.
