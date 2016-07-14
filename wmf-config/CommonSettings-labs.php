c9219d7ba4dd3a2d11d995fc3e9de0fd1c5b3ba4
Yuri Astrakhan <yurik@wikimedia.org>
2016-07-14 21:53:44 +0300

    Enable "geoshapes" graphs protocol
    Change-Id: Id85c7331bba29dca076966b828032df8e6678f8b

--- wmf-config/CommonSettings-labs.php
+++ wmf-config/CommonSettings-labs.php
@@ -303,7 +303,6 @@ if ( $wmgUseGraph ) {
 	$wgGraphAllowedDomains['http'] = [ 'wmflabs.org' ];
 	$wgGraphAllowedDomains['wikirawupload'][] = 'upload.beta.wmflabs.org';
 	$wgGraphAllowedDomains['wikidatasparql'][] = 'wdqs-test.wmflabs.org';
-	$wgGraphAllowedDomains['geoshape'] = [ 'maps.wikimedia.org' ];
 }
 
 if ( $wmgUseKartographer ) {
