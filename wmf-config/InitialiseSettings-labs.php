b8b7183e29cc18b3746e80e2359ed34786bfb23a
Greg Grossmeier <greg@wikimedia.org>
2016-07-13 23:10:42 -0700

    [Beta Cluster] Remove PoolCounter override
    PoolCounter is enabled in production, and was disabled in Beta Cluster.
    Remove the Beta Cluster override.
    
    Apparently we've had a poolcounter instance in Beta Cluster for a while
    (T112501).
    
    And not having a poolcounter in Beta Cluster was part of what made us
    miss a bug that caused an outage in production.
    
    Bug: T38891
    Change-Id: I1703c76c41df6c46f17938b7a4ae34af46547f82

--- wmf-config/InitialiseSettings-labs.php
+++ wmf-config/InitialiseSettings-labs.php
@@ -184,9 +184,6 @@ function wmfLabsSettings() {
 		'wgVectorResponsive' => [
 			'default' => true,
 		],
-		'-wmgUsePoolCounter' => [
-			'default' => false, // T38891
-		],
 
 		'-wmgEnableCaptcha' => [
 			'default' => true,
