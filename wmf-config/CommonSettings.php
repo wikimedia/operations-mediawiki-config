2c371154b497440b8d74f748396cfba54b21afa0
Reedy <reedy@wikimedia.org>
2016-07-20 20:04:30 +0100

    Apply WMF specific SiteMatrix config in CommonSettings.php
    Bug: T132125
    Change-Id: I61337e69ef852fb16d9065cb6879afa4cd6ae3d8

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -698,6 +698,50 @@ include( $IP . '/extensions/SiteMatrix/SiteMatrix.php' );
 
 // Config for sitematrix
 $wgSiteMatrixFile = ( $wmfRealm === 'labs' ) ? "$IP/../langlist-labs" : "$IP/../langlist";
+
+$wgSiteMatrixSites = [
+	'wiki' => [
+		'name' => 'Wikipedia',
+		'host' => 'www.wikipedia.org',
+		'prefix' => 'w',
+	],
+	'wiktionary' => [
+			'name' => 'Wiktionary',
+			'host' => 'www.wiktionary.org',
+			'prefix' => 'wikt',
+	],
+	'wikibooks' => [
+			'name' => 'Wikibooks',
+			'host' => 'www.wikibooks.org',
+			'prefix' => 'b',
+	],
+	'wikinews' => [
+			'name' => 'Wikinews',
+			'host' => 'www.wikinews.org',
+			'prefix' => 'n',
+	],
+	'wikiquote' => [
+			'name' => 'Wikiquote',
+			'host' => 'www.wikiquote.org',
+			'prefix' => 'q',
+	],
+	'wikisource' => [
+			'name' => 'Wikisource',
+			'host' => 'www.wikisource.org',
+			'prefix' => 's',
+	],
+	'wikiversity' => [
+			'name' => 'Wikiversity',
+			'host' => 'www.wikiversity.org',
+			'prefix' => 'v',
+	],
+	'wikivoyage' => [
+			'name' => 'Wikivoyage',
+			'host' => 'www.wikivoyage.org',
+			'prefix' => 'voy',
+	],
+];
+
 $wgSiteMatrixClosedSites = MWWikiversions::readDbListFile( 'closed' );
 $wgSiteMatrixPrivateSites = MWWikiversions::readDbListFile( 'private' );
 $wgSiteMatrixFishbowlSites = MWWikiversions::readDbListFile( 'fishbowl' );
