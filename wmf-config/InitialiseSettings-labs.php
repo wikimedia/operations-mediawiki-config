ec81d2973ca34bc7ad142388bfa5c37e79cb9a37
Max Semenik <maxsem.wiki@gmail.com>
2016-07-21 20:24:05 -0700

    Labs: remove wmgUseGuidedTour - matches prod
    Change-Id: I25ad29abdbc1d20b5f67e3e9393c2c20a129ff80

--- wmf-config/InitialiseSettings-labs.php
+++ wmf-config/InitialiseSettings-labs.php
@@ -409,10 +409,6 @@ function wmfLabsSettings() {
 			'default' => 'P751',
 		],
 
-		'wmgUseGuidedTour' => [
-			'wikidatawiki' => true,
-		],
-
 		'wmgUseRelatedArticles' => [
 			'default' => true,
 		],
