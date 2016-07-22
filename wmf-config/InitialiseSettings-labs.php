c9492d672e9113359e0090de7e73c036ca83a9e2
Max Semenik <maxsem.wiki@gmail.com>
2016-07-21 20:51:30 -0700

    Labs: remove wmgUseNavigationTiming - matches prod
    Change-Id: I4032f646f4368e5489d7d48fc32f50320a58f530

--- wmf-config/InitialiseSettings-labs.php
+++ wmf-config/InitialiseSettings-labs.php
@@ -361,10 +361,6 @@ function wmfLabsSettings() {
 			'default' => true,
 		],
 
-		'wmgUseNavigationTiming' => [
-			'default' => true,
-		],
-
 		'wgSecureLogin' => [
 			// Setting false throughout Labs for now due to untrusted SSL certificate
 			// T50501
