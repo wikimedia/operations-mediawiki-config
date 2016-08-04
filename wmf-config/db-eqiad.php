4d5b1fb58fd1831a132611c065e16bc28215de96
Jcrespo <jcrespo@wikimedia.org>
2016-08-04 19:58:32 +0200

    Pool db1074 as backup rc node in case db1036 lags
    Bug: T116425
    Change-Id: I36f7ac484b6c029d6136acedfa8b3c30f8651229

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -110,7 +110,7 @@ $wgLBFactoryConf = [
 		'db1060' => 0,   # 2.8TB  96GB, api
 #		'db1063' => 0,   # 2.8TB 128GB
 #		'db1067' => 0,   # 2.8TB 160GB
-		'db1074' => 500, # 3.6TB 512GB
+		'db1074' => 400, # 3.6TB 512GB, + backup rc until T116425 is resolved
 		'db1076' => 500, # 3.6TB 512GB
 		'db1090' => 500, # 3.6TB 512GB
 	],
@@ -232,19 +232,24 @@ $wgLBFactoryConf = [
 			'db1060' => 1,
 		],
 		'watchlist' => [
-			'db1036' => 1,
+			'db1036' => 100,
+			'db1074' => 1,
 		],
 		'recentchanges' => [
-			'db1036' => 1,
+			'db1036' => 100,
+			'db1074' => 1,
 		],
 		'recentchangeslinked' => [
-			'db1036' => 1,
+			'db1036' => 100,
+			'db1074' => 1,
 		],
 		'contributions' => [
-			'db1036' => 1,
+			'db1036' => 100,
+			'db1074' => 1,
 		],
 		'logpager' => [
-			'db1036' => 1,
+			'db1036' => 100,
+			'db1074' => 1,
 		],
 	],
 	/* s3 */ 'DEFAULT' => [
