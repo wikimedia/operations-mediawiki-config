60ba890b286b08a96a65cb514858b9f38e8bb2e1
Reedy <reedy@wikimedia.org>
2016-08-04 22:16:03 +0100

    Load WikimediaMessages via wfLoadExtension
    Bug: T140852
    Depends-On: I8df715883e71cda5c3fe45ff47ea7d0270a03656
    Change-Id: Ia8b9081894613e00b3cc4dcc5d190097ed3e4dee

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -1758,11 +1758,8 @@ if ( $wmgUseCentralNotice ) {
 	];
 }
 
-// Load our site-specific l10n extensions
-include "$IP/extensions/WikimediaMessages/WikimediaMessages.php";
-if ( $wmgUseWikimediaLicenseTexts ) {
-	include "$IP/extensions/WikimediaMessages/WikimediaLicenseTexts.php";
-}
+// Load our site-specific l10n extension
+wfLoadExtension( 'WikimediaMessages.php' );
 
 if ( $wgDBname == 'enwiki' ) {
 	// Please don't interfere with our hundreds of wikis ability to manage themselves.
