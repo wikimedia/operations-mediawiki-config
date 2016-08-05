aaf08a4cdc6db29a1ea7f8b0c13ea406052a9b61
Jcrespo <jcrespo@wikimedia.org>
2016-08-05 13:47:17 +0200

    Pool db1079 as an api server; reduce main load
    There was saturation on one of the smallest servers on s7,
    probably not related to T142135 changes, but it may be another
    cause for T141090.
    
    Bug: T141090
    Change-Id: I50b67ecf23171c8993a159ccd0357e584bfefc62

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -168,7 +168,7 @@ $wgLBFactoryConf = [
 		'db1034' => 1,   # 1.4TB  64GB, watchlist, recentchanges, contributions, logpager
 #		'db1039' => 0,   # 1.4TB  64GB
 		'db1062' => 1,   # 2.8TB 128GB, watchlist, recentchanges, contributions, logpager
-		'db1079' => 500, # 3.6TB 512GB
+		'db1079' => 300, # 3.6TB 512GB, api
 		'db1086' => 500, # 3.6TB 512GB
 		'db1094' => 500, # 3.6TB 512GB
 	],
@@ -361,6 +361,9 @@ $wgLBFactoryConf = [
 		],
 		'dump' => [
 			'db1028' => 1,
+		],
+		'api' => [
+			'db1079' => 1,
 		],
 		'watchlist' => [
 			'db1062' => 1,
