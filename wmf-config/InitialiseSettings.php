31646d383d4a49de486d44c7e7c764788dca838d
Glaisher <glaisher.wiki@gmail.com>
2016-08-20 22:29:51 +0500

    Enable T143073 debug log channel
    Bug: T143073
    Change-Id: I532ee1b38958700660b747efab453392f377c452

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -4572,6 +4572,7 @@ $wgConf->settings = [
 		'T138987' => 'debug',
 		'T76305' => 'debug', // - Krinkle and Krenair, for https://gerrit.wikimedia.org/r/188987
 		'T87645' => 'debug', // - Ori on behalf of Tim, for https://gerrit.wikimedia.org/r/#/c/188304/
+		'T143073' => 'debug',
 		'temp-debug' => 'debug', // generic admin debug log
 		'texvc' => 'debug',
 		'thumbnailaccess' => 'debug', // T106323
