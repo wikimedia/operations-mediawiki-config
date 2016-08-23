5597de60a4d3f92b17e7945968098094d23f6854
Mukunda Modell <mmodell@wikimedia.org>
2016-08-23 13:29:43 -0500

    Scap swat command
    Implemented so far:
    
    *  Can accept Change-Id, change number or a full gerrit url.
    * `scap scrape --start` Enqueue patches from the deployment calendar and begin SWAT deployment.
    * `scap scrape --list`  List patches in the queue.
    * `scap swat search query:terms` to find patches by providing a gerrit search query
    * `scap swat --changeid 123456` get change details
    * `scap swat pick 123456` cherry-pick a patch to one or more branches
    * `scap swat https://gerrit.wikimedia.org/r/#/c/123456/` - same as --changeid 12345
    * `scap swat pick --rebase 123456` rebase the change on branch tip.
    * `scap swat revert 123457` revert an aleready-merged change.
    
    Username and password are read from ~/.netrc (for lack of a better solution)
    
    I have included tox, nose and flake8 configuration/support files for python lint,
    everything passes flake8.
    
    Depends-on: https://phabricator.wikimedia.org/D349
    
    Bug: T142880
    
    Change-Id: If1a73894ae3d63187811aa99caac6e0e7ce3a8bd

---
+++ scap/plugins/swat.py
@@ -0,0 +1,163 @@
+from __future__ import division, absolute_import
+from __future__ import print_function, unicode_literals
+
+from inspect import cleandoc
+import inspect
+import sys
+import scap.arg
+import scap.cli as cli
+import scap.utils as utils
+from scap.plugins.gerrit import Changes, Change, ChangeDetail
+from scap.plugins.gerrit import dump_json, parse_gerrit_uri
+import scap.plugins.dep as dep
+from scap.utils import confirm
+
+@cli.command('swat', help='Mediawiki SWAT deployment helper.', subcommands=True)
+@cli.argument('-b', '--branch', nargs="?",
+              help='One or more branches to merge into.'
+              + ' Default: active wmf/branches.')
+@cli.argument('-m', '--message', nargs=1,
+              help='Include a message or comment with your action.')
+class Swat(cli.Application):
+    '''
+    scap swat: cherry-pick and deploy patches for Mediawiki SWAT.
+
+    Usage Examples:
+    ----------------
+    Query changes from gerrit and display the results:
+        scap swat search 'status:open'
+
+    Cherry-pick a patch to active deployment branches:
+        scap swat pick 123456
+
+    Specify a specific branch target:
+        scap swat pick 123456 --branch 1.28.0-wmf.1
+
+    Diplay detailed information about a change in gerrit:
+        scap swat --changeid 123456
+
+    More info: https://wikitech.wikimedia.org/wiki/SWAT_deploys
+    '''
+    @cli.argument('changeids', nargs='+', help='Change-id or gerrit patch url')
+    def main(self, *extra_args):
+        """ scap swat """
+        for changeid in changeids:
+            changeid = parse_gerrit_uri(changeid)
+            change = ChangeDetail(changeid)
+            change.get()
+            code_review = change.data.labels['Code-Review']
+            del change.data.messages
+            del change.data.removable_reviewers
+            del change.data.labels
+            del change.data.permitted_labels
+            dump_json(change.data)
+
+            print("Votes:")
+            for vote in code_review.all:
+                value = vote.value
+                if value > 0:
+                    value = "+%s" % value
+                print("  {:<22}{:<2}".format(vote.name, value))
+            actions = change.revision('actions').get()
+            if actions.rebase.enabled:
+                # if the change is not submittable, attempt to rebase
+                prompt = 'This change must be rebased on top of {}, continue?'
+                prompt = prompt.format(change.data.branch)
+                bail = 'Cannot merge without rebase.'
+                self.confirm_if_flag_missing('rebase', prompt, False, bail)
+                # Rebase on top of the target branch
+                res = change.revision('rebase').post(data={"base": ''})
+                dump_json(res)
+            # refresh change details
+            change.get()
+
+            def submit():
+                change.revision('submit').post()
+
+            def approve():
+                change.revision('review').post(data={
+                    "tag": "swat",
+                    "message": "Approved for SWAT.",
+                    "labels": {
+                        "Code-Review": 2
+                    }
+                })
+
+            if change.data.submittable:
+                if confirm('Submit the change?', True):
+                    submit()
+            else:
+                if confirm('Vote +2 and submit?', True):
+                    approve()
+
+
+    def _process_arguments(self, args, extra_args):
+        if not args.branch:
+            self.branches = self.active_wikiversions().keys()
+        elif type(args.branch) is not list:
+            self.branches = [args.branch]
+        else:
+            self.branches = args.branch
+        ''' extra command line arguments get passed to workflows'''
+        return args, extra_args
+
+
+    def confirm_if_flag_missing(self, flag, prompt, default=False, bail=None):
+        """Get confirmation from the user if a specified cli flag was omitted."""
+        if (flag in self.arguments):
+            return True
+        if bail:
+            bail = Exception(bail)
+        confirm(prompt, default, on_rejected=bail)
+
+
+    @cli.argument('changeid', nargs="+",
+                  help='The ChangeId of a patch to merge.')
+    @cli.argument('--rebase', action='store_true',
+                  help='Rebase the change onto the tip of the target branch.')
+    def pick(self, command):
+        '''Cherry-pick one or more changes to all active wiki branches.
+
+        Once the changes have been cherry-picked, merge them and deploy.
+        '''
+        changeids = self.arguments.changeids
+        for changeid in changeids:
+            print('Cherry-picking %s' % changeid)
+            changeid = parse_gerrit_uri(changeid)
+            change = ChangeDetail(changeid)
+            change.get()
+
+            for branch in self.branches:
+                branch = 'wmf/%s' % branch
+                print(' to %s' % branch)
+                data = {
+                    'message': 'Cherry-pick for SWAT deployment.',
+                    'destination': branch
+                }
+                res = change.revision('cherrypick').post(data=data)
+                print(res.text)
+
+    @cli.argument('term', nargs='+',
+                  help='Gerrit search term(s). E.g: status:open owner:self')
+    @cli.argument('-n', type=int, nargs=1, default='10',
+                  help='Number of results to show. Default: 10')
+    def query(self, args):
+        ''' execute a gerrit query '''
+        query = ' '.join(self.arguments.term)
+        changes = Changes()
+        res = changes.query(query, n=self.arguments.n)
+        dump_json(res)
+
+    @cli.argument('changeid', nargs=1,
+                  help='The ChangeId of a patch to revert.')
+    def revert(self, changeids):
+        ''' revert a change in gerrit '''
+        message = self.arguments.message
+        changeids = self.arguments.changeid
+        for changeid in changeids:
+            change = Change(changeid)
+            try:
+                res = change('revert').post(data={"message": message})
+                dump_json(res)
+            except Exception as e:
+                print(e.message)
