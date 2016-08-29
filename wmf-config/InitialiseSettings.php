8c4b6742222e649313eec400d107d483b86e7dff
jdlrobson <jdlrobson@gmail.com>
2016-08-29 14:55:54 -0700

    End lazy loading reference experiments
    This is being put on hold for a while. We have collected
    enough data to assess this feature.
    
    Bug: T144240
    Change-Id: I2055319fc76a947f86c4da66fb90c7916eb23f7b

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -14435,21 +14435,6 @@ $wgConf->settings = [
 		'base' => false,
 		'beta' => true,
 	],
-	// T136731
-	'thwiki' => [
-		'base' => true,
-		'beta' => true,
-	],
-	// T137822
-	'tlwiki' => [
-		'base' => true,
-		'beta' => true,
-	],
-	// T140197
-	'ruwiki' => [
-		'base' => true,
-		'beta' => true,
-	],
 ],
 
 'wgMinervaUsePageActionBarV2' => [
