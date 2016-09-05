414e91396e4637587f7b628a8804f25eb394ab8b
Manuel Arostegui <marostegui@wikimedia.org>
2016-09-05 12:12:29 +0200

    mariadb: Depool db1064 for maintenance; pool db1019 instead
    db1064 is going to be reimaged and upgraded.
    
    Bug: T144723
    Change-Id: I5c94357f512f8563f121331616cdfd179d1f4eb9

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -129,12 +129,11 @@ $wgLBFactoryConf = [
 		'db1078' => 500, # 3.6TB 512GB
 	],
 	's4' => [
-#		'db1042' => 0,   # 1.4TB  64GB, temporary master
 		'db1040' => 0,   # 1.4TB  64GB, master
-#		'db1019' => 0,   # 1.4TB  64GB, watchlist, recentchanges, contributions, logpager
+		'db1019' => 0,   # 1.4TB  64GB, vslow, dump
 		'db1056' => 1,   # 2.8TB  96GB, watchlist, recentchanges, contributions, logpager
 		'db1059' => 1,   # 2.8TB  96GB, api
-		'db1064' => 0,   # 2.8TB 160GB, vslow, dump
+#		'db1064' => 0,   # 2.8TB 160GB, vslow, dump # under maintenance - T144723
 		'db1068' => 1,   # 2.8TB 160GB, api
 		'db1081' => 500, # 3.6TB 512GB
 		'db1084' => 500, # 3.6TB 512GB
@@ -322,10 +321,12 @@ $wgLBFactoryConf = [
 	],
 	's4' => [
 		'vslow' => [
-			'db1064' => 1,
+#			'db1064' => 1,
+			'db1019' => 1,
 		],
 		'dump' => [
-			'db1064' => 1,
+#			'db1064' => 1,
+			'db1019' => 1,
 		],
 		'api' => [
 			'db1059' => 1,
