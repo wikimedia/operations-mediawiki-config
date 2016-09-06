cdadfad241b8334d36a8077bf4ceaf826074029c
Sébastien Santoro <dereckson@espace-win.org>
2016-09-06 23:04:41 +0000

    Restore Redaktor namespace on sk.wikipedia
    Previously, the Redaktor namespace, a choice specific for sk.wikipedia,
    was hardcoded in the general Slovak language localisation.
    
    It was renamed to a more universal Užívateľ in 4f12dc25e773.
    
    Meanwhile, for the sk.wikipedia specific configuration, we improved
    the localisation of Redaktor / Redaktora by genre in 9e7831fc935a.
    
    But the configuration never included Redaktor as the canonical User
    namespace, so this namespace has been Užívateľ since 1.28.0-wmf.17.
    
    This configuration change applies what should have been done from
    the start instead of edit the core Slovak localisation: define
    Redaktor as the canonical User namespace for sk.wikipedia.
    
    Bug: T143472
    Change-Id: Ic1f043159ed76fc4e45f96d45e5f935cf64786e7

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -5289,6 +5289,8 @@ $wgConf->settings = [
 	'shwiki' => [ 100 => 'Portal', 101 => 'Razgovor_o_portalu' ], // T32928
 	'siwiki' => [ 100 => 'ද්වාරය', 101 => 'ද්වාරය_සාකච්ඡාව' ], // T8435, T26936
 	'skwiki' => [
+		NS_USER => 'Redaktor', // T143472
+		NS_USER_TALK => 'Diskusia_s_redaktorom',
 		100 => 'Portál',
 		101 => 'Diskusia_k_portálu',
 	],
