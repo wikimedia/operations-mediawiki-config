017015ff5f51ed9b72509c5f423b371b3abaa20c
Sébastien Santoro <dereckson@espace-win.org>
2016-09-10 15:31:30 +0000

    Women in Science (Vancouver, BCIT) throttle rule
    New throttle rule:
        * Event name .... Women in Science (Vancouver, BCIT)
        * Event start ... 2016-09-21 09:30 -7:00
        * Event end ..... 2016-09-21 16:00 -7:00
        * IP ............ 142.232.0.0/16
        * Projects ...... enwiki, frwiki, commonswiki
        * Attendees ..... 70 (margin set at 70)
    
    Grouped all Women in Science throttle rules in their section.
    
    Bug: T145253
    Change-Id: I7152a94977f29875d22a2b38253238d74ed7b3ae

--- wmf-config/throttle.php
+++ wmf-config/throttle.php
@@ -28,26 +28,44 @@ $wmgThrottlingExceptions = [];
 # ];
 ## Add throttling definitions below.
 
-$wmgThrottlingExceptions[] = [ // T143951 - University of British Columbia
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Woman in Science
+#
+
+$wmgThrottlingExceptions[] = [ // T145253 - Vancouver, British Columbia Institute of Technology
+	'from'   => '2016-09-21T09:30 -7:00',
+	'to'     => '2016-09-21T16:00 -7:00',
+	'range'  => '142.232.0.0/16',
+	'dbname' => [ 'enwiki', 'frwiki', 'commonswiki' ],
+	'value'  => 70 // 50 expected
+];
+
+$wmgThrottlingExceptions[] = [ // T143951 - Vancouver, University of British Columbia
 	'from' => '2016-09-21T10:00 -8:00',
 	'to' => '2016-09-21T16:00 -8:00',
 	'range' => [ '128.189.64.0/19', '128.189.192.0/18', '206.12.64.0/21', '206.87.112.0/21', '206.87.120.0/21', '206.87.128.0/19', '206.87.192.0/21', '206.87.208.0/21', '206.87.216.0/21', '137.82.104.0/24', '137.82.79.0/24', '137.82.82.128/25' ],
 	'dbname' => [ 'enwiki' ],
 	'value' => 60 //50 expected
 ];
+
+$wmgThrottlingExceptions[] = [ // T145115 - Montréal, Concordia Library and McGill University
+	'from' => '2016-09-21T13:00 -4:00',
+	'to' => '2016-09-21T17:00 -4:00',
+	'range' => '132.205.228.0/24',
+	'dbname' => [ 'enwiki', 'frwiki' ],
+	'value' => 50 //40 expected
+];
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Other rules
+#
+
 $wmgThrottlingExceptions[] = [ // https://www.mediawiki.org/wiki/Wikimedia_Hackathon_Amrita_University
 	'from' => '2016-09-07T00:00 +5:30',
 	'to' => '2016-10-03T00:00 +5:30',
 	'IP' => '182.19.48.18',
 	'dbname' => 'labswiki',
 	'value' => 60 //50 expected
-];
-$wmgThrottlingExceptions[] = [ // T145115
-	'from' => '2016-09-21T13:00 -4:00',
-	'to' => '2016-09-21T17:00 -4:00',
-	'range' => '132.205.228.0/24',
-	'dbname' => [ 'enwiki', 'frwiki' ],
-	'value' => 50 //40 expected
 ];
 
 ## Add throttling definitions above.
