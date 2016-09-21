6d4280622a5a4aae100db68bf806b855bbae36d1
Manuel Arostegui <marostegui@wikimedia.org>
2016-09-21 11:16:11 +0200

    db-eqiad.php: Repool db1086 after the ALTER table
    db1086 needed to be depooled to get an ALTER table. db1079 took the
    traffic for a while.
    db1086 comes back with its original weight before the operation (500)
    and gets added to the API service to make sure we have HA.
    db1079 gets a bit less weight for general traffic as it is serving
    API with weight 100 too.
    
    Bug:T141951
    Change-Id: I51f99157872fc31520a68b66d39c21eadf0cdc59

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -167,8 +167,8 @@ $wgLBFactoryConf = [
 		'db1034' => 1,   # 1.4TB  64GB, watchlist, recentchanges, contributions, logpager
 #		'db1039' => 0,   # 1.4TB  64GB
 		'db1062' => 1,   # 2.8TB 128GB, watchlist, recentchanges, contributions, logpager
-		'db1079' => 350, # 3.6TB 512GB, api
-#		'db1086' => 350, # 3.6TB 512GB,
+		'db1079' => 300, # 3.6TB 512GB, api
+		'db1086' => 500, # 3.6TB 512GB, api
 		'db1094' => 500, # 3.6TB 512GB
 	],
 	'silver' => [
@@ -407,7 +407,8 @@ $wgLBFactoryConf = [
 			'db1028' => 1,
 		],
 		'api' => [
-			'db1079' => 1,
+			'db1079' => 100,
+			'db1086' => 1,
 		],
 		'watchlist' => [
 			'db1034' => 1,
