bc1d4253bd22d052d8a97804748a03bebbf6f2ba
Gergő Tisza <tgr.huwiki@gmail.com>
2016-09-22 13:37:44 -0700

    Add 'message-format' log channel
    Logging is added in Id51cf6a5a937bc41a914f317e980ef42e4d385fb.
    
    Bug: T146416
    Change-Id: I4914814c9f9716289ecb5a7d205bf5ff95be64fe

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -4562,6 +4562,7 @@ $wgConf->settings = [
 		'MassMessage' => 'debug', // for 59464 -legoktm 2013/12/15
 		'Math' => 'info',  // mobrovac for T121445
 		'memcached' => 'error', // -aaron 2012/10/24
+		'message-format' => [ 'logstash' => 'warning' ],
 		'mobile' => 'debug',
 		'NewUserMessage' => 'debug',
 		'oai' => 'debug',
