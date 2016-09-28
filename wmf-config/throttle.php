5bfbbce91a1a06427ef786771db3a838dc657c67
Max Semenik <maxsem.wiki@gmail.com>
2016-09-27 18:36:34 -0700

    throttle: remove expired exceptions
    Change-Id: If0e352269c6a8183882d9d74202ee87b4b6a4c89

--- wmf-config/throttle.php
+++ wmf-config/throttle.php
@@ -28,66 +28,11 @@ $wmgThrottlingExceptions = [];
 # ];
 ## Add throttling definitions below.
 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# Woman in Science
-#
-
-$wmgThrottlingExceptions[] = [ // T145253 - Vancouver, British Columbia Institute of Technology
-	'from'   => '2016-09-21T09:30 -7:00',
-	'to'     => '2016-09-21T16:00 -7:00',
-	'range'  => '142.232.0.0/16',
-	'dbname' => [ 'enwiki', 'frwiki', 'commonswiki' ],
-	'value'  => 70 // 50 expected
-];
-
-$wmgThrottlingExceptions[] = [ // T143951 - Vancouver, University of British Columbia
-	'from' => '2016-09-21T10:00 -8:00',
-	'to' => '2016-09-21T16:00 -8:00',
-	'range' => [ '128.189.64.0/19', '128.189.192.0/18', '206.12.64.0/21', '206.87.112.0/21', '206.87.120.0/21', '206.87.128.0/19', '206.87.192.0/21', '206.87.208.0/21', '206.87.216.0/21', '137.82.104.0/24', '137.82.79.0/24', '137.82.82.128/25' ],
-	'dbname' => [ 'enwiki' ],
-	'value' => 70 //50 expected
-];
-
-$wmgThrottlingExceptions[] = [ // T145115 - Montréal, Concordia Library and McGill University
-	'from' => '2016-09-21T13:00 -4:00',
-	'to' => '2016-09-21T17:00 -4:00',
-	'range' => '132.205.228.0/24',
-	'dbname' => [ 'enwiki', 'frwiki' ],
-	'value' => 50 //40 expected
-];
-$wmgThrottlingExceptions[] = [ // T145838
-	'from' => '2016-09-19T8:00 -8:00',
-	'to' => '2016-09-19T17:00 -8:00',
-	'range' => [ '142.58.113.128/26', '142.58.128.0/21', '207.23.160.0/19', '207.23.192.0/19', '142.58.80.0/21', '142.58.160.0/21', '142.58.168.0/21' ],
-	'dbname' => [ 'enwiki', 'frwiki', 'commonswiki' ],
-	'value' => 70 // 50 expected
-];
-
-$wmgThrottlingExceptions[] = [ // T145838
-	'from' => '2016-09-21T8:00 -8:00', 
-	'to' => '2016-09-21T17:00 -8:00', 
-	'range' => [ '142.58.113.128/26', '142.58.128.0/21', '207.23.160.0/19', '207.23.192.0/19', '142.58.80.0/21', '142.58.160.0/21', '142.58.168.0/21' ],
-	'dbname' => [ 'enwiki', 'frwiki', 'commonswiki' ],
-	'value' => 70 // 50 expected
-];
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# Other rules
-#
-
 $wmgThrottlingExceptions[] = [ // https://www.mediawiki.org/wiki/Wikimedia_Hackathon_Amrita_University
 	'from' => '2016-09-07T00:00 +5:30',
 	'to' => '2016-10-03T00:00 +5:30',
 	'IP' => '182.19.48.18',
 	'dbname' => 'labswiki',
-	'value' => 60 //50 expected
-];
-
-$wmgThrottlingExceptions[] = [
-	'from' => '2016-09-17T9:00 +12:00',
-	'to' => '2016-09-18T16:00 +12:00',
-	'IP' => '49.224.252.88',
-	'dbname' => [ 'enwiki' ],
 	'value' => 60 //50 expected
 ];
 
