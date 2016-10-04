72843f95f2929bb4470056fc8b3c61fb6dcbf616
Martin Urbanec <martin.urbanec@wikimedia.cz>
2016-10-04 12:23:15 +0200

    [throttle] Increase account creation limits for an event in Perpignan on 2016-10-15
    Bug: T147293
    Change-Id: I327b81d833dbba5069c24781a0ba43daed77220e

--- wmf-config/throttle.php
+++ wmf-config/throttle.php
@@ -99,6 +99,14 @@ $wmgThrottlingExceptions[] = [ // T146788
 	'value' => 45 //35 expected
 ];
 
+$wmgThrottlingExceptions[] = [ // T147293
+	'from' => '2016-10-14T00:00 -0:00',
+	'to' => '2016-10-16T0:00 -0:00',
+	'IP' => '194.167.137.246',
+	'dbname' => ['frwiki', 'cawiki', 'wikidatawiki'],
+	'value' => 40
+];
+
 ## Add throttling definitions above.
 
 /**
