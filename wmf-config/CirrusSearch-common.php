b08a457ca9d3da4355e8633b7ea03dd739bae11c
dcausse <dcausse@wikimedia.org>
2016-10-05 14:58:11 +0200

    Initialize subphrases autocomplete on wikisources, mw.org and wikitech
    First step is to build the new FST
    
    Bug: T146208
    Change-Id: I59ce20d75061866466f608d9bd1d00748b494be5

--- wmf-config/CirrusSearch-common.php
+++ wmf-config/CirrusSearch-common.php
@@ -473,6 +473,9 @@ if ( $wgCirrusSearchDefaultCluster !== $wmfDatacenter ) {
 // Enable completion suggester
 $wgCirrusSearchUseCompletionSuggester = $wmgCirrusSearchUseCompletionSuggester;
 
+// Configure sub-phrases completion
+$wgCirrusSearchCompletionSuggesterSubphrases = $wmgCirrusSearchCompletionSuggesterSubphrases;
+
 // Enable phrase suggester (did you mean)
 $wgCirrusSearchEnablePhraseSuggest = $wmgCirrusSearchEnablePhraseSuggest;
 
