cd6f68419c9a966747a870ab7ad03f80e51811e4
Sébastien Santoro <dereckson@espace-win.org>
2016-10-06 01:40:16 +0000

    Initial configuration for ec.wikimedia
    Wikimedistas de Ecuador has been recognized as a Wikimedia user group
    in September 2015. To work efficiently, they wish a private wiki.
    
    This configuration is mostly based on be.wikimedia.
    
    Lang code ........ es
    Shard ............ s3
    Site name ........ Wikimedistas de Ecuador
    Logo ............. https://commons.wikimedia.org/wiki/File:Wikimedians_of_Ecuador_User_Group_Logo_PNG_version.png
    Timezone ......... America/Guayaquil (UTC-5)
    Uploads .......... allowed
    Import sources ... meta
    Features ......... Visual Editor, private wiki
    
    Bug: T135521
    Change-Id: I75af02c458aeb3dd0ae93fd30aeace81cbd11fed

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -122,6 +122,7 @@ $wgConf->settings = [
 	'cawikimedia' => 'en', // T88843#1098439
 	'cowikimedia' => 'es',
 	'dkwikimedia' => 'da',
+	'ecwikimedia' => 'es', // T135521
 	'ilwikimedia' => 'he',
 	'mkwikimedia' => 'mk',
 	'mxwikimedia' => 'es',
@@ -276,6 +277,7 @@ $wgConf->settings = [
 	'dsbwiki' => 'Europe/Berlin',
 	'dvwiki' => 'Indian/Maldives', // T48351
 	'dvwiktionary' => 'Indian/Maldives', // T48351
+	'ecwikimedia' => 'America/Guayaquil', // T135521
 	'elwikinews' => 'Europe/Athens',
 	'elwikivoyage' => 'Europe/Athens',
 	'etwiki' => 'Europe/Tallinn',
@@ -1048,6 +1050,7 @@ $wgConf->settings = [
 	'cawikimedia' => '/static/images/project-logos/cawikimedia.png',
 	'cowikimedia' => '/static/images/project-logos/cowikimedia.png',
 	'dkwikimedia' => '/static/images/project-logos/dkwikimedia.png',
+	'ecwikimedia' => '/static/images/project-logos/ecwikimedia.png', // T135521
 	'etwikimedia' => '/static/images/project-logos/etwikimedia.png',
 	'fiwikimedia' => '/static/images/project-logos/fiwikimedia.png',
 	'mkwikimedia' => '/static/images/project-logos/mkwikimedia.png',
@@ -1173,6 +1176,7 @@ $wgConf->settings = [
 	'viwikivoyage' => [ '1.5x' => '/static/images/project-logos/viwikivoyage-1.5x.png', '2x' => '/static/images/project-logos/viwikivoyage-2x.png' ],
 
     # Chapter wikis
+	'ecwikimedia' => [ '1.5x' => '/static/images/project-logos/ecwikimedia-1.5x.png', '2x' => '/static/images/project-logos/ecwikimedia-2x.png'],
 	'trwikimedia' => [ '1.5x' => '/static/images/project-logos/trwikimedia-1.5x.png', '2x' => '/static/images/project-logos/trwikimedia-2x.png'],
     # Special wikis
 	'foundationwiki' => [ '1.5x' => '/static/images/project-logos/foundationwiki-1.5x.png', '2x' => '/static/images/project-logos/foundationwiki-2x.png' ],
@@ -1641,6 +1645,7 @@ $wgConf->settings = [
 	'dsbwiki' => 'Wikipedija',
 	'dvwiki' => 'ވިކިޕީޑިއާ', // T50075
 	'dvwiktionary' => 'ވިކިރަދީފު',
+	'ecwikimedia' => 'Wikimedistas de Ecuador', // T135521
 	'elwiki' => 'Βικιπαίδεια',
 	'elwikibooks' => 'Βικιβιβλία',
 	'elwikinews' => 'Βικινέα',
@@ -3991,6 +3996,7 @@ $wgConf->settings = [
 	'+checkuserwiki' => [ 0 => 1 ],
 	'+collabwiki' => [ 0 => 1 ],
 	'+commonswiki' => [ 6 => 0, 8 => 0 ],
+	'+ecwikimedia' => [ 0 => 1 ],
 	'+execwiki' => [ 0 => 1, 6 => 0, 8 => 0, 10 => 0 ],
 	'+fdcwiki' => [ 0 => 1 ],
 	'+foundationwiki' => [ 0 => 1, 6 => 0, 8 => 0 ],
@@ -10506,6 +10512,7 @@ $wgConf->settings = [
 	'bewikimedia' => [ 'meta' ],
 	'brwikimedia' => [ 'wikipedia:pt', 'meta', 'wiktionary:pt', 'wikisource:pt', 'wikinews:pt', 'wikiquote:pt', 'wikibooks:pt', 'wikiversity:pt', 'commons' ], // T60936
 	'cowikimedia' => [ 'meta', 'wikipedia:es', 'wikibooks:es', 'wikiversity:es' ],
+	'ecwikimedia' => [ 'meta' ], // T135521
 	'etwikimedia' => [ 'wikipedia:et', 'meta' ],
 	'fiwikimedia' => [ 'wikipedia:fi', 'meta' ],
 	'mkwikimedia' => [ 'meta', 'wikipedia:mk' ],
