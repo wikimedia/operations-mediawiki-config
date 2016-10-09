dd22fb9b268cbc730740993d3188706fd856238a
MarcoAurelio <strigiwm@gmail.com>
2016-10-09 11:38:48 +0200

    Send abusefilter hit notifications from es.wikibooks to UDP
    Bug: T147744
    Change-Id: Ic8f6068cd02384bb76c78708cf8c8f934d9f0942

--- wmf-config/abusefilter.php
+++ wmf-config/abusefilter.php
@@ -140,7 +140,8 @@ switch ( $wgDBname ) {
 		$wgAbuseFilterActions['block'] = true;
 		$wgAbuseFilterBlockDuration = 'indefinite'; // T96669
 		$wgAbuseFilterAnonBlockDuration = '31 hours'; // T96669
-		$wgAbuseFilterNotifications = false;
+		$wgAbuseFilterNotifications = true; // T147744
+		$wgAbuseFilterNotificationsPrivate = true; // T147744
 		break;
 	case 'eswiktionary':
 		$wgGroupPermissions['*']['abusefilter-log-detail'] = true;
