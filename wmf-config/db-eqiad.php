e3b1bb364739dcf623b2c176abfd474cb65a9b3b
Manuel Arostegui <marostegui@wikimedia.org>
2016-10-25 11:55:09 +0200

    db-eqiad.php: Depool db1059
    I am going to use db1059 as a clone source for dbstore2001 for that
    I need to depool it
    As db1068 will handle the API traffic itself, I have removed it
    from the main traffic service until db1059 is back.
    
    Bug: T146261
    Change-Id: Ibc8865d7e04424bf9a9c90ee36981a490907d016

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -131,7 +131,7 @@ $wgLBFactoryConf = [
 		'db1040' => 0,   # 1.4TB  64GB, master
 		'db1053' => 1,   # 2.8TB  96GB, watchlist, recentchanges, contributions, logpager
 		'db1056' => 1,   # 2.8TB  96GB, watchlist, recentchanges, contributions, logpager
-		'db1059' => 1,   # 2.8TB  96GB, api
+#		'db1059' => 1,   # 2.8TB  96GB, api
 		'db1064' => 0,   # 2.8TB 160GB, vslow, dump
 		'db1068' => 1,   # 2.8TB 160GB, api
 		'db1081' => 500, # 3.6TB 512GB
@@ -326,7 +326,7 @@ $wgLBFactoryConf = [
 			'db1064' => 1,
 		],
 		'api' => [
-			'db1059' => 1,
+#			'db1059' => 1,
 			'db1068' => 3,
 		],
 		'watchlist' => [
