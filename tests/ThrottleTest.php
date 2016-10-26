wikimedia/operations-mediawiki-config

commit d0b37a87a88fee14daf680dd7124b31cafd22483

[throttle] Rule for Winona State University
The University is holding several events. Add throttling rules for all
of them.

Bug: T146600
Change-Id: I9335dfcba729fb76ccd926433bf9bb8d36ccae14

wmf-config/throttle.php
@@ -99,6 +99,44 @@ $wmgThrottlingExceptions[] = [ // T146788
 	'value' => 45 //35 expected
 ];
 
+// Winona State University - T146600
+
+// October 4th
+$wmgThrottlingExceptions[] = [ // T146600
+	'from' => '2016-10-04T15:30 -6:00',
+	'to' => '2016-10-04T18:30 -6:00',
+	'range' => '199.17.0.0/16',
+	'dbname' => 'enwiki',
+	'value' => 40 //35 expected
+];
+
+// October 10th
+$wmgThrottlingExceptions[] = [ // T146600
+	'from' => '2016-10-10T13:30 -6:00',
+	'to' => '2016-10-10T18:30 -6:00',
+	'range' => '199.17.0.0/16',
+	'dbname' => 'enwiki',
+	'value' => 40 //35 expected
+];
+
+// October 11th
+$wmgThrottlingExceptions[] = [ // T146600
+	'from' => '2016-10-11T15:30 -6:00',
+	'to' => '2016-10-11T18:30 -6:00',
+	'range' => '199.17.0.0/16',
+	'dbname' => 'enwiki',
+	'value' => 40 //35 expected
+];
+
+// December 2nd
+$wmgThrottlingExceptions[] = [ // T146600
+	'from' => '2016-12-02T12:30 -6:00',
+	'to' => '2016-12-02T13:00 -6:00',
+	'ramge' => '199.17.0.0/16',
+	'dbname' => 'enwiki',
+	'value' => 40 // 35 expected
+];
+
 ## Add throttling definitions above.
 
 /**

commit f50ad3928ba4b6bc45f0f4787e084f9db894c476

Tests for throttle rules
Change d0b37a87 introduced a parameter typo to a rule. In the past,
we got several times issues with ip/IP (now both cases are accepted).

This test checks the parameters of the throttle rules' arrays to avoid
future occurrences of the issue.

Change-Id: I1f7a07441f78c45e1469e7de0c89e96be5995f08

tests/ThrottleTest.php
@@ -1,0 +1,29 @@
+<?php
+
+require_once __DIR__ . '/../wmf-config/throttle.php';
+
+class ThrottleTest extends PHPUnit_Framework_TestCase {
+
+	public function testThrottlingExceptionsKeys () {
+		global $wmgThrottlingExceptions;
+
+		$validParameters = self::getThrottlingExceptionsValidParameters();
+
+		foreach ( $wmgThrottlingExceptions as $rule ) {
+			foreach ( $rule as $key => $value ) {
+					$this->assertContains(
+						$key, $validParameters,
+						"Invalid parameter in a throttle rule detected: $key"
+					);
+			}
+		}
+	}
+
+	protected static function getThrottlingExceptionsValidParameters () {
+		return [
+			'from', 'to',
+			'IP', 'ip', 'range',
+			'dbname', 'value',
+		];
+	}
+}
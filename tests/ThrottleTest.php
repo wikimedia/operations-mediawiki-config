f50ad3928ba4b6bc45f0f4787e084f9db894c476
Sébastien Santoro <dereckson@espace-win.org>
2016-10-26 19:08:39 +0000

    Tests for throttle rules
    Change d0b37a87 introduced a parameter typo to a rule. In the past,
    we got several times issues with ip/IP (now both cases are accepted).
    
    This test checks the parameters of the throttle rules' arrays to avoid
    future occurrences of the issue.
    
    Change-Id: I1f7a07441f78c45e1469e7de0c89e96be5995f08

---
+++ tests/ThrottleTest.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once __DIR__ . '/../wmf-config/throttle.php';
+
+class ThrottleTest extends PHPUnit_Framework_TestCase {
+
+	public function testThrottlingExceptionsKeys () {
+		global $wmgThrottlingExceptions;
+
+		$validParameters = self::getThrottlingExceptionsValidParameters();
+
+		foreach ( $wmgThrottlingExceptions as $rule ) {
+			foreach ( $rule as $key => $value ) {
+					$this->assertContains(
+						$key, $validParameters,
+						"Invalid parameter in a throttle rule detected: $key"
+					);
+			}
+		}
+	}
+
+	protected static function getThrottlingExceptionsValidParameters () {
+		return [
+			'from', 'to',
+			'IP', 'ip', 'range',
+			'dbname', 'value',
+		];
+	}
+}
