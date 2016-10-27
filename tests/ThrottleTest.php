63599bd1fff2537eef3b672bb101a81d4ddab851
Sébastien Santoro <dereckson@espace-win.org>
2016-10-27 14:26:10 +0000

    Test for throttle rules: parameters logic
    Throttle rules parameters and parameters' values obey a DSL-like
    logic. This change detects some logic errors and invalid values.
    
    The goal is to avoid to commit an incomplete rule, where expected
    parameters will be superseded by other values.
    
    Change-Id: I6730b021343ab6ce1ddb1201000915a5afa53f48

--- tests/ThrottleTest.php
+++ tests/ThrottleTest.php
@@ -4,17 +4,80 @@ require_once __DIR__ . '/../wmf-config/throttle.php';
 
 class ThrottleTest extends PHPUnit_Framework_TestCase {
 
-	public function testThrottlingExceptionsKeys () {
-		global $wmgThrottlingExceptions;
-
+	/**
+	 * @dataProvider provideRules
+	 */
+	public function testThrottlingExceptionsKeys ( $rule ) {
 		$validParameters = self::getThrottlingExceptionsValidParameters();
 
-		foreach ( $wmgThrottlingExceptions as $rule ) {
-			foreach ( $rule as $key => $value ) {
-					$this->assertContains(
-						$key, $validParameters,
-						"Invalid parameter in a throttle rule detected: $key"
-					);
+		foreach ( $rule as $key => $value ) {
+				$this->assertContains(
+					$key, $validParameters,
+					"Invalid parameter in a throttle rule detected: $key"
+				);
+		}
+	}
+
+	/**
+	 * @dataProvider provideRules
+	 */
+	public function testIfThrottlingExceptionsContainTheRequiredParameters ( $rule ) {
+		$keys = array_keys( $rule );
+		$this->assertContains( 'from', $keys, "Throttle rule required parameter missing: from" );
+		$this->assertContains( 'to', $keys, "Throttle rule required parameter missing: to" );
+	}
+
+	/**
+	 * @dataProvider provideRules
+	 */
+	public function testIfThrottlingExceptionsDontContainBothRangeAndIP ( $rule ) {
+		$keys = array_keys( $rule );
+		$this->assertFalse (
+			in_array( 'IP', $keys ) &&
+			in_array( 'range', $keys ),
+			"Throttle rules can apply to range(s) or IP(s) but not both"
+		);
+	}
+
+	/**
+	 * @dataProvider provideRules
+	 * @depends testIfThrottlingExceptionsContainTheRequiredParameters
+	 */
+	public function testThrottlingExceptionsValues ( $rule ) {
+		foreach ( $rule as $key => $value ) {
+			// Parses date
+			// strtotime returns false when the string can't be parsed.
+			$this->assertNotSame(
+				false, strtotime( $rule['from'] ),
+				"Invalid value in a throttle rule detected: from should be a valid date"
+			 );
+			$this->assertNotSame(
+				false, strtotime( $rule['to'] ),
+				"Invalid value in a throttle rule detected: to should be a valid date"
+			);
+
+			// Parses integer
+			// We accept numeric integer and string representation of integers as digits.
+			if ( array_key_exists( 'value', $rule ) ) {
+				$this->assertTrue(
+					is_int( $rule['value'] ) || ctype_digit( $rule['value'] ),
+					"Invalid value in a throttle rule detected: range should be integer"
+				);
+			}
+
+			// Parses IP and range
+			// Should be a string or an array
+			if ( array_key_exists( 'IP', $rule ) ) {
+				$this->assertTrue(
+					is_array( $rule['IP'] ) || is_string( $rule['IP'] ),
+					"Invalid valid in a throttle rule detected: IP should be a string or an array"
+				);
+			}
+			if ( array_key_exists( 'range', $rule ) ) {
+				$this->assertTrue(
+					is_array( $rule['range'] ) || is_string( $rule['range'] ),
+					"Invalid valid in a throttle rule detected: range should be a string or an array"
+				);
 			}
 		}
 	}
@@ -29,4 +92,17 @@ class ThrottleTest extends PHPUnit_Framework_TestCase {
 			'value',
 		];
 	}
+
+	public function provideRules () {
+		global $wmgThrottlingExceptions;
+
+		$rules = [];
+
+		foreach ( $wmgThrottlingExceptions as $rule ) {
+			$rules[] = [ $rule ];
+		}
+
+		return $rules;
+	}
+
 }
