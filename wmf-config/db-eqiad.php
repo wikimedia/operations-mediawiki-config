94d4c1997eaf7291fafa961b7739ed2db346c6c4
Jaime Crespo <jcrespo@wikimedia.org>
2016-11-08 16:47:10 +0100

    mariadb: Pool db1052 to help with the extra api load
    Change-Id: Ia66bcac58455517be7b297debd58b9d615c7ac95

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -94,13 +94,13 @@ $wgLBFactoryConf = [
 'sectionLoads' => [
 	's1' => [
 		'db1057' => 0,   # 2.8TB  96GB, master
-#		'db1052' => 1  , # 2.8TB  96GB, old master
+		'db1052' => 1  , # 2.8TB  96GB, api, old master
 		'db1051' => 50,  # 2.8TB  96GB, watchlist, recentchanges, contributions, logpager
 		'db1055' => 50,  # 2.8TB  96GB, watchlist, recentchanges, contributions, logpager
 		'db1065' => 50,  # 2.8TB 160GB, api
 		'db1066' => 50,  # 2.8TB 160GB, api
 		'db1072' => 0,   # 2.8TB 160GB, vslow, dump
-#		'db1073' => 1,   # 2.8TB 160GB
+#		'db1073' => 1,   # 2.8TB 160GB multiple hardware issues T149728
 #		'db1080' => 500, # 3.6TB 512GB schema change T139090
 		'db1083' => 500, # 3.6TB 512GB
 		'db1089' => 500, # 3.6TB 512GB
@@ -266,6 +266,7 @@ $wgLBFactoryConf = [
 		'api' => [
 			'db1065' => 1,
 			'db1066' => 1,
+			'db1052' => 1,
 		],
 	],
 	's2' => [
