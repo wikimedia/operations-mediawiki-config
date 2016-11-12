5bfccced642217822cc1976d4a7ca2707ea8db01
XXN <dan15i@yahoo.com>
2016-11-12 21:05:23 +0000

    Fixes for namespace definitions for some Romanian (ro) projects
    Change-Id: I122e53148882862d2983b955b88692a576e317ed

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -3543,11 +3543,11 @@ $wgConf->settings = [
 		'Discussão_em_tradução' => 109,
 	],
 	'+rowiki' => [
-		'Discuţie_MediWiki' => NS_MEDIAWIKI_TALK,
+		'Discuţie_MediaWiki' => NS_MEDIAWIKI_TALK,
 		'Discuţie_Portal' => 101,
 		'Discuţie_Proiect' => 103,
 		'Book' => 110, // T68530
-		'Discuţie_Book' => 111,
+		'Discuție_Book' => 111,
 	],
 	'+rowikibooks' => [
 		'Discuţie_Wikibooks' => NS_PROJECT_TALK,
@@ -3555,7 +3555,7 @@ $wgConf->settings = [
 	'+rowikinews' => [
 		'Wikiştiri' => NS_PROJECT,
 		'Discuţie_Wikiştiri' => NS_PROJECT_TALK,
-		'Discuţie_MediWiki' => NS_MEDIAWIKI_TALK,
+		'Discuție_MediaWiki' => NS_MEDIAWIKI_TALK,
 	],
 	'+rowikisource' => [
 		'Author' => 102,
