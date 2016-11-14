15547eca052f2b708493389217a4d1cbfbab4c6e
Antoine Musso <hashar@free.fr>
2016-11-15 00:08:27 +0100

    Drop '.ttf' from $wgTimelineFontFile + bump epoch
    Bump /fonts/ submodule so it includes the suffix less symbolic links.
    Raise $wgTimelineEpochTimestamp for those wikis, since the rendering is
    changed/fixed.
    
    Covered by tests in a subsequent commit.
    
    Bug: T22825
    Change-Id: I6962803e6e92dd02cb3be0ef3a0b77ccec81538a
    Depends-On: I40ee0bc843c4c2ac8363c99c3421be8bf09b9e18

--- wmf-config/timeline.php
+++ wmf-config/timeline.php
@@ -4,6 +4,7 @@
 # Do not put private data here.
 
 wfLoadExtension( 'timeline' );
+$wgTimelineEpochTimestamp = '20130601000000';
 
 if ( $wgDBname === 'testwiki' || $wgDBname === 'mlwiki' ) {
 	// FreeSansWMF has been generated from FreeSans and FreeSerif by using this script with fontforge:
@@ -11,9 +12,10 @@ if ( $wgDBname === 'testwiki' || $wgDBname === 'mlwiki' ) {
 	// MergeFonts("FreeSerif.ttf");
 	// SetFontNames("FreeSans-WMF", "FreeSans WMF", "FreeSans WMF Regular", "Regular", "");
 	// Generate("FreeSansWMF.ttf", "", 4 );
-	$wgTimelineFontFile = 'FreeSansWMF.ttf';
+	$wgTimelineEpochTimestamp = '20161201000000';
+	$wgTimelineFontFile = 'FreeSansWMF';
 } elseif ( $lang == 'zh' ) {
-	$wgTimelineFontFile = 'unifont-5.1.20080907.ttf';
+	$wgTimelineEpochTimestamp = '20161201000000';
+	$wgTimelineFontFile = 'unifont-5.1.20080907';
 }
 $wgTimelineFileBackend = 'local-multiwrite';
-$wgTimelineEpochTimestamp = '20130601000000';
