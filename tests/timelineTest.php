d544ee4030ba5f2cb4393f85cc0b80e9a941e607
Antoine Musso <hashar@free.fr>
2016-11-14 23:58:07 +0100

    Test for $wgTimelineFontFile values
    Parse wmf-config/timeline.php for potential values given to
    $wgTimelineFontFile.
    
    The file must not end up with '.ttf' which is stripped by ploticus and
    would cause it to fail finding the file.
    We have GDFONTPATH pointing to /fonts/ ensure the given filename exists
    there.
    
    Bug: T22825
    Change-Id: I2a79931822e687e9463f7e1a76a39d7134b5157c

---
+++ tests/timelineTest.php
@@ -0,0 +1,64 @@
+<?php
+
+class timelineTest extends \PHPUnit_Framework_TestCase {
+
+	/**
+	 * Ploticus strip the '.ttf' suffix from the font name and then fails
+	 * to find the .ttf font in /fonts/. Make sure we strip the suffix both
+	 * in the config and in the fonts files. T22825.
+	 *
+	 * @dataProvider wgTimelineFontFileValues
+	 */
+	function testTimelinefontfile_does_not_have_ttf_suffix( $filename ) {
+		$this->assertStringEndsNotWith( '.ttf', $filename );
+	}
+
+
+	/**
+	 * @dataProvider wgTimelineFontFileValues
+	 */
+	function testTimelinefontfile_exists( $filename ) {
+		$this->assertFileExists( __DIR__ . "/../fonts/" . $filename );
+	}
+
+	/**
+	 * Parse wmf-config/timeline.php and find values for $wgTimelineFontFile
+	 */
+	public static function wgTimelineFontFileValues() {
+
+		$testCases = [];
+		$conf = file_get_contents( __DIR__ . '/../wmf-config/timeline.php' );
+		$tokens = ( token_get_all( $conf ) );
+
+		while( $token = each( $tokens )[1] ) {
+
+			# Skip until we find $wgTimelineFontFile
+			if ( !(
+					is_array( $token )
+					&& $token[0] == T_VARIABLE
+					&& $token[1] == '$wgTimelineFontFile'
+			) ) {
+				continue;
+			}
+
+			while ( $next_token = next($tokens) ) {
+				# Skip ' = ' to reach the actual value being set
+				if (
+					$next_token == '='
+					|| is_array( $next_token ) && $next_token[0] == T_WHITESPACE ) {
+					continue;
+				}
+				break;
+			}
+			self::assertInternalType( 'array', $next_token );
+			self::assertEquals(
+				T_CONSTANT_ENCAPSED_STRING,
+				$next_token[0],
+				'Test suite expects $wgTimelineFontFile to be set to a string' );
+
+			$testCases[] = [ trim( $next_token[1], '\'' ) ];
+		}
+		return $testCases;
+	}
+
+}
