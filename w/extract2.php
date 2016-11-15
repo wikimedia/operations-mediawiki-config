295a7ca25f43dd828902db8958b96f082cd024fb
Chad Horohoe <chadh@wikimedia.org>
2016-11-14 19:37:04 -0800

    Move 2 web entry points to w/ where they belong
    There's no reason for these to exist in the top level, all apache
    and other URLs point to the w/ symlinks anyway
    
    Change-Id: I1a1364401b2a8803eae176b6c2323c9020077da1

--- w/extract2.php
+++ w/extract2.php
@@ -1,1 +1,35 @@
-../extract2.php
+<?php
+$lang = 'meta';
+putenv( "MW_LANG={$lang}" ); // notify MWMultiVersion
+
+require_once __DIR__ . '/../multiversion/MWVersion.php';
+require getMediaWiki( 'includes/WebStart.php' );
+
+$allowed_templates = array(
+	'Www.wikimedia.org_template',
+	'Www.wikipedia.org_template',
+	'Www.wikinews.org_template',
+	'Www.wiktionary.org_template',
+	'Www.wikiquote.org_template',
+	'Www.wikiversity.org_template',
+	'Www.wikibooks.org_template',
+	'Www.wikivoyage.org_template',
+	'API_listing_template',
+);
+
+$template = $wgRequest->getText( 'template', 'Www.wikipedia.org_template' );
+if ( !in_array( $template, $allowed_templates ) ) {
+	header( 'Content-Type: text/plain; charset=utf-8' );
+	echo 'Invalid parameters...';
+	exit;
+}
+
+$wgTitle = Title::newFromText( $template );
+$wgArticle = new Article( $wgTitle );
+$rawHtml = $wgArticle->getContent( false );
+
+$lastmod = gmdate( 'D, j M Y H:i:s', wfTimestamp( TS_UNIX, $wgArticle->getTouched() ) ) . ' GMT';
+header( 'Content-Type: text/html; charset=utf-8' );
+header( 'Cache-Control: s-maxage=3600, must-revalidate, max-age=0' );
+header( "Last-modified: $lastmod" );
+echo $rawHtml;
