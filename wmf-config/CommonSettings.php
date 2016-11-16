07be193667844be770cbeeb6e285400f42559af5
Reedy <reedy@wikimedia.org>
2016-11-16 16:59:41 +0000

    Revert "Try again with GeoIP in AuthManagerLoginAuthenticateAudit"
    This reverts commit ff0da32ffbcc561636105db192ca77b58ccaf95d.
    
    Change-Id: Ia6e0bcbf1dd9f94dbf68253dbfdf2b344b22dc8f

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -1514,14 +1514,13 @@ $wgHooks['AuthManagerLoginAuthenticateAudit'][] = function( $response, $user, $u
 		$headers = apache_request_headers();
 
 		$logger = LoggerFactory::getInstance( 'badpass' );
-		$logger->info( 'Login failed for sysop {name} from {ip} - {xff} - {ua} - {geoip}: {messagestr}', [
+		$logger->info( 'Login failed for sysop {name} from {ip} - {xff} - {ua}: {messagestr}', [
 			'name' => $user->getName(),
 			'ip' => $wgRequest->getIP(),
 			'xff' => @$headers['X-Forwarded-For'],
 			'ua' => @$headers['User-Agent'],
 			'guessed' => $guessed,
 			'messagestr' => $response->message->toString(),
-			'geoip' => $wgRequest->getIP( 'GeoIP', '' ),
 		] );
 	}
 };
