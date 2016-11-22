b9d7f65d8c73d8e7b4826528e5a5dd53e92d73f0
Manuel Arostegui <marostegui@wikimedia.org>
2016-11-22 09:05:30 +0100

    db-eqiad.php: Depool db1081 for maintenance
    db1081 needs an ALTER table so it needs to be depooled
    
    Bug: T147305
    Change-Id: I3608065c34068e024b41d6b20653c2a4f7e7a6be

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -134,7 +134,7 @@ $wgLBFactoryConf = [
 		'db1059' => 1,   # 2.8TB  96GB, api
 		'db1064' => 0,   # 2.8TB 160GB, vslow, dump
 		'db1068' => 1,   # 2.8TB 160GB, api
-		'db1081' => 500, # 3.6TB 512GB
+#		'db1081' => 500, # 3.6TB 512GB
 		'db1084' => 500, # 3.6TB 512GB
 		'db1091' => 500, # 3.6TB 512GB
 	],
