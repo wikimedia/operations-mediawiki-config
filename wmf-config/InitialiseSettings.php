f81ec1d9ae82be19e8b384352bca8e145bb27ded
addshore <addshorewiki@gmail.com>
2016-11-30 16:42:52 +0000

    Enable ElectronPdfService extension on metawiki
    Bug: T150943
    Change-Id: Ib5edceedacf53141d9362e84861d763a8b5cf231

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -12034,6 +12034,7 @@ $wgConf->settings = [
 	'testwiki' => true, // T150944
 	'test2wiki' => true, // T150944
 	'mediawikiwiki' => true, // T150944
+	'metawiki' => true, // T150943
 ],
 
 'wmgUseSpamBlacklist' => [
