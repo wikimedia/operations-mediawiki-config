02fe9353ef44411f73ed75d89d621325fcf83596
Sébastien Santoro <dereckson@espace-win.org>
2016-11-30 13:57:39 +0000

    Add dashboard.wikiedu.org IPv6 to en.wikipedia rate limit exempt
    Bug: T151823
    Change-Id: Id13ac0e357151a3ed82ee5d9b6043573fea532bc

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -7331,6 +7331,7 @@ $wgConf->settings = [
 	'default' => [],
 	'enwiki' => [
 		'45.56.98.206',  // Wiki Education Foundation dashboard T126541
+		'2600:3c03::f03c:91ff:fe08:7973', // idem, T151823
 	],
 ],
 
