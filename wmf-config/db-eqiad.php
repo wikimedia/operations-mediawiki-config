6c5eb11a9e6fe22f93071d2ba1355dd9666fc4fc
Jaime Crespo <jcrespo@wikimedia.org>
2016-12-01 19:53:28 +0100

    mariadb: Depool pc1004
    As technically we cannot depool these server without breaking the
    partitioning key, I just maintaned the key and assigned its role
    to pc1005.
    
    Bug: T152029
    Change-Id: I99ed2beb88970152eba6110218374c657ca6b8ea

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -8,7 +8,7 @@ if ( !defined( 'DBO_DEFAULT' ) ) {
 # $wgReadOnly = "Wikimedia Sites are currently read-only during maintenance, please try again soon.";
 
 $wmgParserCacheDBs = [
-	'10.64.0.12'   => '10.64.0.12',   # pc1004
+	'10.64.0.12'   => '10.64.32.72'   # '10.64.0.12',   # pc1004 temp. depooled, caching to pc1005
 	'10.64.32.72'  => '10.64.32.72',  # pc1005
 	'10.64.48.128' => '10.64.48.128', # pc1006
 ];
