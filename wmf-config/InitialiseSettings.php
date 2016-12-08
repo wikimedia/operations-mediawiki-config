845831c24986a3f332919cb6da80be3ea23f552b
Thiemo Mättig (WMDE) <thiemo.maettig@wikimedia.de>
2016-12-08 17:41:48 +0000

    Revert "Add Abenaki language (abe) to Wikidata"
    This reverts commit 61b6711b35b3333ba74a1b007eec2864cc74e673.
    
    To add language codes to the configuration only to use them for
    Monolingual properties works, but isn't the correct thing to do.
    
    Instead, Wikibase extension code handles directly such languages.
    
    We so move this 'abe' language code to the
    Wikibase\Repo\WikibaseRepo::getMonolingualTextLanguages method.
    
    Bug: T150633
    Depends-On: Id11c27431aa1a2e770699b7cd62579c288823504?
    Change-Id: I60331c505a69e0908b67641c0dcbdc986baaf516

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -16442,7 +16442,6 @@ $wgConf->settings = [
 		'rwr' => 'मारवाड़ी',           // T61905
 		'sje' => 'bidumsámegiella', // T146707
 		'smj' => 'julevsámegiella', // T146707
-		'abe' => 'wôbanakiôdwawôgan', // T150633
 	],
 ],
 
