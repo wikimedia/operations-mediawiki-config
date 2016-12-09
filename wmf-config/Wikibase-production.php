696d5c8895d99e3136e7256130bb6c802cfb7648
Marius Hoch <hoo@online.de>
2016-12-09 16:17:28 +0100

    Load the property order from Wikidata per default
    The URL is similar to the ones we use for the spam and the title
    blacklist.
    
    The results are cached and local wikis can still override the
    order, if needed.
    
    Bug: T149540
    Change-Id: I82b209a499ba0133577a65cf7c73997862d787a4

--- wmf-config/Wikibase-production.php
+++ wmf-config/Wikibase-production.php
@@ -111,4 +111,6 @@ if ( $wmgUseWikibaseClient ) {
 
 	// Overwrite or add commons links in the "other projects sidebar" with the "commons category" (P373), per T126960
 	$wgWikimediaBadgesCommonsCategoryProperty = $wgDBname === 'commonswiki' ? null : 'P373';
+
+	$wgWBClientSettings['propertyOrderUrl'] = 'https://www.wikidata.org/w/index.php?title=MediaWiki:Wikibase-SortedProperties&action=raw&sp_ver=1';
 }
