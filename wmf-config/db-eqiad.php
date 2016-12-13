1d9d2f64b9a3a3106229a81bb8f436f0b435215f
Jaime Crespo <jcrespo@wikimedia.org>
2016-12-13 13:39:38 +0100

    Repool db1083 with low load after maintenance
    Change-Id: I825a8f6ea375521d9a3916de2d1671700fbf08a5

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -102,7 +102,7 @@ $wgLBFactoryConf = [
 		'db1072' => 0,   # 2.8TB 160GB, vslow, dump
 #		'db1073' => 1,   # 2.8TB 160GB multiple hardware issues T149728
 		'db1080' => 500, # 3.6TB 512GB
-#		'db1083' => 500, # 3.6TB 512GB # depooled for maintenance T69223
+		'db1083' => 50,  # 3.6TB 512GB, pooled with low load
 		'db1089' => 500, # 3.6TB 512GB
 	],
 	's2' => [
