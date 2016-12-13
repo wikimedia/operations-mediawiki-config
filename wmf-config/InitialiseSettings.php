bb6edb0667d849bee806f4a1279fdf8a6c8f30ef
Martin Urbanec <martin.urbanec@wikimedia.cz>
2016-12-13 12:53:01 +0100

    Allow sysops to manage translationadmin group on no.wikimedia
    This is a follow-up for commit 767bc4a6a67b21.
    
    Bug: T152490
    Change-Id: I3024a9cb122bf3086689030bee87df4dfe79fd67

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -9648,6 +9648,9 @@ $wgConf->settings = [
 		'bureaucrat' => [ 'sysop', 'bureaucrat', 'bot', 'patroller', 'autopatrolled' ],
 		'sysop' => [ 'autopatrolled', 'patroller' ],
 	],
+	'+nowikimedia' => [
+		'sysop' => [ 'translationadmin' ], // T152490
+	],
 	'+officewiki' => [
 		'bureaucrat' => [ 'import', 'transwiki', 'communityapps', 'flood', 'securepoll' ],
 	],
@@ -10353,6 +10356,9 @@ $wgConf->settings = [
 	'+nowikibooks' => [
 		'bureaucrat' => [ 'bot', 'patroller', 'autopatrolled' ],
 		'sysop' => [ 'autopatrolled', 'patroller' ],
+	],
+	'+nowikimedia' => [
+		'sysop' => [ 'translationadmin' ], // T152490
 	],
 	'+officewiki' => [
 		'bureaucrat' => [ 'sysop', 'bureaucrat', 'import', 'transwiki', 'communityapps', 'flood', 'securepoll' ],
