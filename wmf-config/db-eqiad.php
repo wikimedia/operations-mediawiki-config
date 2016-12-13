de1884bdc98ff2e0e0620109e85f57769df61a57
Jcrespo <jcrespo@wikimedia.org>
2016-12-13 08:33:52 +0000

    Revert "mariadb: Depool db1051 for schema change"
    This reverts commit 6b2a057c74d05cdf9a945b6b8d2d450fac2fd5c3.
    
    Change-Id: Ic3a68e7d0c566037c03b3d208edd7109a7b8ef0d

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -95,7 +95,7 @@ $wgLBFactoryConf = [
 	's1' => [
 		'db1057' => 0,   # 2.8TB  96GB, master
 #		'db1052' => 1  , # 2.8TB  96GB, api, old master #Temporary master for db1095 - new sanitarium #T150960
-#		'db1051' => 50,  # 2.8TB  96GB, watchlist, recentchanges, contributions, logpager - down for maintenance T69223
+		'db1051' => 50,  # 2.8TB  96GB, watchlist, recentchanges, contributions, logpager
 		'db1055' => 50,  # 2.8TB  96GB, watchlist, recentchanges, contributions, logpager
 		'db1065' => 50,  # 2.8TB 160GB, api
 		'db1066' => 50,  # 2.8TB 160GB, api
@@ -238,23 +238,23 @@ $wgLBFactoryConf = [
 'groupLoadsBySection' => [
 	's1' => [
 		'watchlist' => [
-#			'db1051' => 1,
+			'db1051' => 1,
 			'db1055' => 1,
 		],
 		'recentchanges' => [
-#			'db1051' => 1,
+			'db1051' => 1,
 			'db1055' => 1,
 		],
 		'recentchangeslinked' => [
-#			'db1051' => 1,
+			'db1051' => 1,
 			'db1055' => 1,
 		],
 		'contributions' => [
-#			'db1051' => 1,
+			'db1051' => 1,
 			'db1055' => 1,
 		],
 		'logpager' => [
-#			'db1051' => 1,
+			'db1051' => 1,
 			'db1055' => 1,
 		],
 		'dump' => [
