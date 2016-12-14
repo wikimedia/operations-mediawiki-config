7b755ab69cd672f8c80a5232d23de467e772d7cd
Jcrespo <jcrespo@wikimedia.org>
2016-12-14 10:59:31 +0000

    Revert "mariadb: Depool db1089 for schema change"
    This reverts commit d8556957b17e02aa962ace7969b74e67b64f3665.
    
    Change-Id: Ia3ad16a072efdccce6914fdab0455338dc4f22bc

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -103,7 +103,7 @@ $wgLBFactoryConf = [
 #		'db1073' => 1,   # 2.8TB 160GB multiple hardware issues T149728
 		'db1080' => 500, # 3.6TB 512GB
 		'db1083' => 500, # 3.6TB 512GB
-#		'db1089' => 500, # 3.6TB 512GB, depooled for maintenance T69223
+		'db1089' => 500, # 3.6TB 512GB
 	],
 	's2' => [
 		'db1018' => 0,   # 1.4TB  64GB, master
