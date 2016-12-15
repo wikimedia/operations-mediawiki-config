23cfa624a7fe397699ed71533c0eb70f5dd93620
Marostegui <marostegui@wikimedia.org>
2016-12-15 10:21:03 +0000

    Revert "db-eqiad.php: Depool db1070 for maintenance"
    db1070 has finished its maintenance
    
    This reverts commit af91d2fe10caa71369a8b281b78b8d84a57782f4.
    
    Change-Id: Ife54c6979019af4eba4834344d58790c1f694033

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -142,9 +142,9 @@ $wgLBFactoryConf = [
 		'db1049' => 0,   # 2.8TB  64GB, master
 		'db1026' => 1,   # 1.4TB  64GB, watchlist, recentchanges, contributions, logpager
 		'db1045' => 0,   # 1.4TB  64GB, vslow, dump
-#		'db1070' => 50,  # 2.8TB 160GB, api, old master #T150644
+		'db1070' => 50,  # 2.8TB 160GB, api, old master
 		'db1071' => 50,  # 2.8TB 160GB, api
-		'db1082' => 300, # 3.6TB 512GB  #temporarily serving API #T150644
+		'db1082' => 500, # 3.6TB 512GB
 		'db1087' => 500, # 3.6TB 512GB
 		'db1092' => 500, # 3.6TB 512GB
 	],
@@ -358,9 +358,8 @@ $wgLBFactoryConf = [
 			'db1045' => 1,
 		],
 		'api' => [
-#			'db1070' => 1,
-			'db1071' => 3,
-			'db1082' => 1,
+			'db1070' => 1,
+			'db1071' => 1,
 		],
 		'watchlist' => [
 			'db1026' => 1,
