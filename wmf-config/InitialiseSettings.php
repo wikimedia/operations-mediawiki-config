8d5e2fe591413786a34819909a0a7d6dbdaf6902
MaxSem <maxsem.wiki@gmail.com>
2016-12-20 20:20:56 +0000

    Revert "Remind people not to enable wmgUseKartographer elsewhere"
    Not true anymore.
    
    This reverts commit d4f61be142e64d76da3361703c94f333770a82db.
    
    Change-Id: I152a107406f3d0ed065884d528733a65541b68a8

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -17734,7 +17734,6 @@ $wgConf->settings = [
 	'testwikidatawiki' => true,
 ],
 
-// Feature does not work for non-JS read clients. This MUST NOT be enabled on any additional wikis.
 'wmgUseKartographer' => [
 	'default' => true,
 	'private' => false,
