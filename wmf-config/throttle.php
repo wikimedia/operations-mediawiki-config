a4303a334f26a1e7011d79d4e685ee2e034b1271
Martin Urbanec <martin.urbanec@wikimedia.cz>
2016-12-28 18:50:43 +0100

    [throttle] New rules + remove obsolete rules
    Location: Queen Mary University, London, UK
    Duration: 00:00 to 23:59 2017-01-13 and 00:00 to 23:59 2017-01-20
    Ranges: IPv4: 161.23.0.0/16, 138.37.0.0/16, IPv6: 2a01:56c0::/32
    Site: en.wikipedia.org
    Estimated accounts: 40 (50 set)
    
    Bug: T154245
    Change-Id: Iae17aa43fb4d882288b3405c6e0ee5f67e814626

--- wmf-config/throttle.php
+++ wmf-config/throttle.php
@@ -28,11 +28,19 @@ $wmgThrottlingExceptions = [];
 # ];
 ## Add throttling definitions below.
 
-$wmgThrottlingExceptions[] = [ // T153741
-	'from' => '2016-12-23T14:00 +5:30',
-	'to' => '2016-12-23T19:00 +5:30',
-	'IP' => '14.139.121.212',
-	'dbname' => [ 'enwiki', 'mrwiki', 'commonswiki' ],
+$wmgThrottlingExceptions[] = [ // T154245
+	'from' => '2017-01-13T00:00 +0:00',
+	'to' => '2017-01-13T23:59 +0:00',
+	'range' => [ '161.23.0.0/16', '138.37.0.0/16', '2a01:56c0::/32' ],
+	'dbname' => 'enwiki',
+	'value' => 50 // 40 expected
+];
+
+$wmgThrottlingExceptions[] = [ // T154245
+	'from' => '2017-01-20T0:00 +0:00',
+	'to' => '2017-01-20T23:59 +0:00',
+	'range' => [ '161.23.0.0/16', '138.37.0.0/16', '2a01:56c0::/32' ],
+	'dbname' => 'enwiki',
 	'value' => 50 // 40 expected
 ];
 
