0a2a0966011932bb503ee8a292fd75161fe029f7
Chad Horohoe <chadh@wikimedia.org>
2017-01-06 11:55:25 -0800

    Remove MWVersion, fold its two functions into MWMultiVersion
    Change-Id: I81698f037d1c2c98c2aa772234e551fb78394dee

--- w/index.php
+++ w/index.php
@@ -1,3 +1,3 @@
 <?php
-require_once __DIR__ . '/../multiversion/MWVersion.php';
-require getMediaWiki( 'index.php' );
+require_once __DIR__ . '/../multiversion/MWMultiVersion.php';
+require MWMultiVersion::getMediaWiki( 'index.php' );
