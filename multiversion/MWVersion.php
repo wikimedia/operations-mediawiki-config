8fdc87387da62fc131bb48c0260fe311fc6091af
Reedy <reedy@wikimedia.org>
2017-01-10 22:22:16 +0000

    Reinstate "Remove MWVersion, fold its two functions into MWMultiVersion"
    This reverts commit 2a6f5a13f8f198c0e9a75f34a3c1439216fb11e0.
    
    Change-Id: I248894fd4a1ef3b47550b98580c7f110714cceeb

--- multiversion/MWVersion.php
+++ multiversion/MWVersion.php
@@ -17,40 +17,8 @@
  * @return string Absolute file path with proper MW location
  */
 function getMediaWiki( $file, $wiki = null ) {
-	global $IP;
 	require_once( __DIR__ . '/MWMultiVersion.php' );
-
-	if ( $wiki === null ) {
-		$scriptName = @$_SERVER['SCRIPT_NAME'];
-		$serverName = @$_SERVER['SERVER_NAME'];
-		# Upload URL hit (to upload.wikimedia.org rather than wiki of origin)...
-		if ( $scriptName === '/w/thumb.php' && $serverName === 'upload.wikimedia.org' ) {
-			$multiVersion = MWMultiVersion::initializeForUploadWiki( $_SERVER['PATH_INFO'] );
-		# Regular URL hit (wiki of origin)...
-		} else {
-			$multiVersion = MWMultiVersion::initializeForWiki( $serverName );
-		}
-	} else {
-		$multiVersion = MWMultiVersion::initializeFromDBName( $wiki );
-	}
-
-	# Wiki doesn't exist yet?
-	if ( $multiVersion->isMissing() ) {
-		header( "Cache-control: no-cache" ); // same hack as CommonSettings.php
-		include( MEDIAWIKI_DEPLOYMENT_DIR . '/wmf-config/missing.php' );
-		exit;
-	}
-
-	# Get the MediaWiki version running on this wiki...
-	$version = $multiVersion->getVersion();
-
-	# Get the correct MediaWiki path based on this version...
-	$IP = MEDIAWIKI_DEPLOYMENT_DIR . "/$version";
-
-	chdir( $IP );
-	putenv( "MW_INSTALL_PATH=$IP" );
-
-	return "$IP/$file";
+	require MWMultiVersion::getMediaWiki( $file, $wiki );
 }
 
 /**
@@ -66,24 +34,6 @@ function getMediaWiki( $file, $wiki = null ) {
  * @return string Absolute file path with proper MW location
  */
 function getMediaWikiCli( $file ) {
-	global $IP;
-
 	require_once( __DIR__ . '/MWMultiVersion.php' );
-	$multiVersion = MWMultiVersion::getInstance();
-	if( !$multiVersion ) {
-		$multiVersion = MWMultiVersion::initializeForMaintenance();
-	}
-	if ( $multiVersion->getDatabase() === 'testwiki' ) {
-		define( 'TESTWIKI', 1 );
-	}
-
-	# Get the MediaWiki version running on this wiki...
-	$version = $multiVersion->getVersion();
-
-	# Get the correct MediaWiki path based on this version...
-	$IP = dirname( __DIR__ ) . "/$version";
-
-	putenv( "MW_INSTALL_PATH=$IP" );
-
-	return "$IP/$file";
+	require MWMultiVersion::getMediaWikiCli( $file );
 }
--- w/opensearch_desc.php
+++ w/opensearch_desc.php
@@ -1,3 +1,3 @@
 <?php
-require_once __DIR__ . '/../multiversion/MWVersion.php';
-require getMediaWiki( 'opensearch_desc.php' );
+require_once __DIR__ . '/../multiversion/MWMultiVersion.php';
+require MWMultiVersion::getMediaWiki( 'opensearch_desc.php' );
