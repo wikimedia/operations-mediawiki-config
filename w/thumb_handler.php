629c98ecc3659b84cd66f2aef2c31a5def8a83cf
Chad Horohoe <chadh@wikimedia.org>
2017-01-17 14:49:48 -0800

    Swap most remaining entry points to using MWMultiVersion
    Omitting mobilelanding for right now, it had weird errors last time
    and I want to do it by itself
    
    Also requires additional cleanups in puppet & possibly elsewhere
    
    Change-Id: I91374c814cfce4e03d32607aa73097ceddd901f2

--- w/thumb_handler.php
+++ w/thumb_handler.php
@@ -1,3 +1,3 @@
 <?php
-require_once __DIR__ . '/../multiversion/MWVersion.php';
-require getMediaWiki( 'thumb_handler.php' );
+require_once __DIR__ . '/../multiversion/MWMultiVersion.php';
+require MWMultiVersion::getMediaWiki( 'thumb_handler.php' );
