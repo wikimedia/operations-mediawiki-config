3acff82a347fe3f103304289c5e9f1571cc09226
Martin Urbanec <martin.urbanec@wikimedia.cz>
2017-01-25 18:37:28 +0100

    [throttle] Her Girl Friday + Lenny Unconference / Editathon in NYC, Sat Jan 28
    Change-Id: I5ce43e9dde5c6eeab1032fbfb25a512c21272ece

--- wmf-config/throttle.php
+++ wmf-config/throttle.php
@@ -78,6 +78,14 @@ $wmgThrottlingExceptions[] = [ // T155877
 	'value'  => 70 // 50 expected
 ];
 
+$wmgThrottlingExceptions[] = [ // T156278
+	'from' => '2017-01-28T09:00 -5:00',
+	'to' => '2017-01-28T18:00 -5:00',
+	'IP' => '104.130.254.117',
+	'dbname' => [ 'enwiki' ],
+	'value' => 250 // max 200 expected
+];
+
 ## Add throttling definitions above.
 
 /**
