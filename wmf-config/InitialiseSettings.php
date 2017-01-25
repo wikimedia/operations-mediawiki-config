5271ca8eca8002a1fddd10938e8ac86b458b843c
Erik Bernhardson <ebernhardson@wikimedia.org>
2017-01-25 14:01:48 -0800

    Enable deprecation logging in prod
    Our deprecation policy requires code that is pending removal to
    call wfDeprecated(), which will log to the 'deprecated' channel. We are
    not logging this to logstash, which makes it possible for code to be
    removed that is still being used somewhere in production. Enable this
    logging channel so we get information about deprecated usage.
    
    I think this is all that is needed, but there is a bit of a dance going
    on in MWDebug::deprecated() so I'm not 100% sure.
    
    Bug: T156310
    Change-Id: Ia8a7ddab914d537c591619ec7926e1630262f86f

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -4575,6 +4575,7 @@ $wgConf->settings = [
 		'csp' => 'info',
 		'csp-report-only' => 'info',
 		'DBPerformance' => [ 'logstash' => 'debug', 'udp2log' => 'warning' ],
+		'deprecated' => 'debug',
 		'diff' => 'debug',
 		'Echo' => 'debug',
 		'es-hit' => 'debug',
