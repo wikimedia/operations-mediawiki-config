eefa4d46f8c3c6c77c54cddc271d938d35b3167b
David Causse <dcausse@wikimedia.org>
2017-01-27 16:59:39 +0100

    Configure A/B test for CrossProject search results sidebar
    Sets up the triggers that will be used in the cross project search
    AB test. These should work on beta immediately, but will need to wait
    for wmf.11 to be deployed to work in production. This can be deployed
    before wmf.11 as the code is only triggered when passing the specified
    query parameters.
    
    Bug: T149806
    Change-Id: I9bfda0eb73582b05c03897863199c88fa2fc11e1

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -17219,6 +17219,41 @@ $wgConf->settings = [
 
 'wmgCirrusSearchUserTesting' => [
 	'default' => [],
+	// Specific wikis are enabled from the client
+	// Enabling this on itwiki might lead to unexpected behaviors
+	'wikipedia' => [
+		'sidebar' => [
+			'sampleRate' => 0,
+			'globals' => [
+				// Should enable crossproject with sitematrix
+				'wgCirrusSearchEnableCrossProjectSearch' => true,
+				// Enable for all buckets
+				'wgCirrusSearchNewCrossProjectPage' => true,
+				'wgCirrusSearchNumCrossProjectSearchResults' => 1,
+			],
+			'buckets' => [
+				// control, we explicitly hide the side bar
+				'no_sidebar' => [
+					'trigger' => 'no_sidebar',
+					'globals' => [
+						'wgCirrusSearchHideCrossProjectResults' => true,
+					]
+				],
+				'recall_sidebar_results' => [
+					'trigger' => 'recall_sidebar_results',
+					'globals' => [
+						'wgCirrusSearchCrossProjectOrder' => 'recall',
+					],
+				],
+				'random_sidebar_results' => [
+					'trigger' => 'random_sidebar_results',
+					'globals' => [
+						'wgCirrusSearchCrossProjectOrder' => 'random',
+					],
+				]
+			]
+		]
+	],
 ],
 
 'wmgCirrusSearchLanguageDetectors' => [
