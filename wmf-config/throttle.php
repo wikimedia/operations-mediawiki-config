daa44d9bd91b0ee5f4576fdb4c043f2567256d84
Sébastien Santoro <dereckson@espace-win.org>
2017-01-27 00:25:44 +0000

    Fix throttle rule for Her Girl Friday + Lenny Unconference
    Bug: T156278
    Change-Id: I23dcd3b6477144ed556d575a9c55ccb12198ae18

--- wmf-config/throttle.php
+++ wmf-config/throttle.php
@@ -89,7 +89,11 @@ $wmgThrottlingExceptions[] = [ // T156258
 $wmgThrottlingExceptions[] = [ // T156278
 	'from' => '2017-01-28T09:00 -5:00',
 	'to' => '2017-01-28T18:00 -5:00',
-	'IP' => '104.130.254.117',
+	'IP' => [
+		'104.130.254.117',
+		'207.87.175.130',
+		'4.15.214.210',
+	],
 	'dbname' => [ 'enwiki' ],
 	'value' => 250 // max 200 expected
 ];
