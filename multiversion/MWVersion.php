da0ed075b06fa033859619d700fde26b27b84278
Chad Horohoe <chadh@wikimedia.org>
2017-02-07 11:23:22 -0800

    multiversion: Drop remaining MWVersion.php shim
    Change-Id: I7a0f2d30c185409f2081b92767e4726ae79b6a24

--- multiversion/MWVersion.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * @see MWMultiVersion::getMediaWiki()
- */
-function getMediaWiki( $file, $wiki = null ) {
-	require_once __DIR__ . '/MWMultiVersion.php';
-	return MWMultiVersion::getMediaWiki( $file, $wiki );
-}
