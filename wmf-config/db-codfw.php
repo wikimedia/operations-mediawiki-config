f88b64ff18e6062dc63cbf671ba2b6d4f6b2844c
Jaime Crespo <jcrespo@wikimedia.org>
2017-02-09 11:27:07 +0100

    mariadb: Depool db2040 for maintenance
    Bug: T111654
    Change-Id: Id0f5420b83083de699cb21a6273de5e7e66ffb8c

--- wmf-config/db-codfw.php
+++ wmf-config/db-codfw.php
@@ -144,7 +144,7 @@ $wgLBFactoryConf = [
 	],
 	's7' => [
 		'db2029' => 0,   # B6 2.9TB  96GB, master
-		'db2040' => 200, # C6 2.9TB 160GB, rc, log
+#		'db2040' => 200, # C6 2.9TB 160GB, rc, log
 		'db2047' => 400, # C6 2.9TB 160GB
 		'db2054' => 200, # D6 2.9TB 160GB, dump (inactive), vslow
 		'db2061' => 200, # D6 3.3TB 160GB, api
@@ -386,19 +386,19 @@ $wgLBFactoryConf = [
 	],
 	's7' => [
 		'watchlist' => [
-			'db2040' => 1,
+			'db2047' => 1,
 		],
 		'recentchanges' => [
-			'db2040' => 1,
+			'db2047' => 1,
 		],
 		'recentchangeslinked' => [
-			'db2040' => 1,
+			'db2047' => 1,
 		],
 		'contributions' => [
-			'db2040' => 1,
+			'db2047' => 1,
 		],
 		'logpager' => [
-			'db2040' => 1,
+			'db2047' => 1,
 		],
 		'dump' => [
 			'db2054' => 1,
