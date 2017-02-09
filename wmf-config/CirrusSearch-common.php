dd4c0c691fd8e32811cc8efe63efb177fdd3253f
Erik Bernhardson <ebernhardson@wikimedia.org>
2017-02-09 14:39:42 -0800

    Configure cirrus per-index settings
    In elasticsearch 5 it is no longer possible to have global configuration
    that is applied to all indices. This sets that configuration so that
    CirrusSearch can provide it to elasticsearch when creating new indices.
    
    Bug: T155578
    Change-Id: If2a66ac32d009e148b7dcfdc8864cfdcfed975f2

--- wmf-config/CirrusSearch-common.php
+++ wmf-config/CirrusSearch-common.php
@@ -262,6 +262,23 @@ $wgCirrusSearchFiletypeAliases = [
 	"svg" => "drawing"
 ];
 
+$wgCirrusSearchExtraIndexSettings = [
+	// indexing slow log
+	'indexing.slowlog.threshold.index.warn' => '10s',
+	'indexing.slowlog.threshold.index.info' => '5s',
+	'indexing.slowlog.threshold.index.debug' => '2s',
+	'indexing.slowlog.threshold.index.trace' => -1,
+	// search slow log
+	'search.slowlog.threshold.fetch.warn' => '1s',
+	'search.slowlog.threshold.fetch.info' => '800ms',
+	'search.slowlog.threshold.fetch.debug' => '500ms',
+	'search.slowlog.threshold.fetch.trace' => '200ms',
+	// Number of merge threads to use. Use only 1 thread
+	// (instead of 3) to avoid updates interfering with
+	// actual searches
+	'merge.scheduler.max_thread_count' => 1,
+];
+
 # Load per realm specific configuration, either:
 # - CirrusSearch-labs.php
 # - CirrusSearch-production.php
