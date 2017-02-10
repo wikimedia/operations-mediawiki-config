938d28a2fc35f868036960b76cdc0b4db9ae002d
Kaldari <rkaldari@wikimedia.org>
2017-02-09 18:26:46 -0600

    Set $wgPageAssessmentsSubprojects to true on English Wikipedia
    Already tested on Beta Labs and Test Wikipedia. Also remove
    Beta Cluster config for this since it's no longer needed.
    
    Bug: T157654
    Change-Id: I65fcab7db93251d0dc78272a43144588691aaa43

--- wmf-config/InitialiseSettings-labs.php
+++ wmf-config/InitialiseSettings-labs.php
@@ -506,10 +506,6 @@ function wmfLabsSettings() {
 		'wmgUsePageAssessments' => [
 			'default' => true,
 		],
-		'wgPageAssessmentsSubprojects' => [
-			'default' => false,
-			'enwiki' => true,
-		],
 
 		// Test PerformanceInspector
 		'wmgUsePerformanceInspector' => [
