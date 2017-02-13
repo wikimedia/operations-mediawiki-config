afb27900655fe4312e737aa4c4b82b97304acc8f
Marostegui <marostegui@wikimedia.org>
2017-02-13 09:10:47 +0000

    Revert "db-eqiad.php: Depool db1077"
    This reverts commit 79a8d0a00ef5c4359000df917fae48e62c187ae3.
    
    Change-Id: Ia41c171c4103e3ea0866d8e4c038b7de93fa9892

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -124,7 +124,7 @@ $wgLBFactoryConf = [
 		'db1015' => 1,   # A2 1.4TB  64GB, watchlist, recentchanges, contributions, logpager
 #		'db1035' => 0,   # B2 1.4TB  64GB
 #		'db1044' => 0,   # B2 1.4TB  64GB, #Temporary master for db1095 - new sanitarium #T150802
-#		'db1077' => 500, # B1 3.6TB 512GB #T136428
+		'db1077' => 500, # B1 3.6TB 512GB
 		'db1078' => 500, # C3 3.6TB 512GB
 	],
 	's4' => [
