2a60d44e8282be9df21e122511880566eb8c2f68
Chad Horohoe <chadh@wikimedia.org>
2017-02-21 16:29:53 -0800

    clean.py: Minor abstraction of param handling
    I want to eventually make this give you a list of branches
    to operate on (clean these y/n) when you give it a plain
    `scap clean`
    
    Change-Id: I033f0819c5b97e6da9bad75b47000f08e2b807a5

--- scap/plugins/clean.py
+++ scap/plugins/clean.py
@@ -32,11 +32,11 @@ class Clean(main.AbstractSync):
                   help='Only keep static assets (CSS/JS and the like).')
     def main(self, *extra_args):
         """ Clean old branches from the cluster for space savings! """
+        self.cleanup_branch(self.arguments.branch, self.arguments.keep_static)
 
-        stage_dir = os.path.join(self.config['stage_dir'],
-                                 'php-%s' % self.arguments.branch)
-        deploy_dir = os.path.join(self.config['deploy_dir'],
-                                  'php-%s' % self.arguments.branch)
+    def cleanup_branch(self, branch, keep_static):
+        stage_dir = os.path.join(self.config['stage_dir'], 'php-%s' % branch)
+        deploy_dir = os.path.join(self.config['deploy_dir'], 'php-%s' % branch)
 
         # Prune junk from masters owned by l10nupdate
         self.execute_remote(
@@ -50,14 +50,14 @@ class Clean(main.AbstractSync):
         self.execute_remote(
             'clean-masters',
             self._get_master_list(),
-            self.clean_command(stage_dir)
+            self.clean_command(stage_dir, keep_static)
         )
 
         # Update apaches
         self.execute_remote(
             'clean-apaches',
             self._get_target_list(),
-            self.clean_command(deploy_dir)
+            self.clean_command(deploy_dir, keep_static)
         )
 
     def execute_remote(self, description, targets, command):
@@ -71,9 +71,9 @@ class Clean(main.AbstractSync):
             if failed:
                 self.get_logger().warning('%d had clean errors', failed)
 
-    def clean_command(self, path):
+    def clean_command(self, path, keep_static):
         regex = '".*\.(%s)"' % ('|'.join(STATIC_TYPES))
-        if self.arguments.keep_static:
+        if keep_static:
             return ['find', path, '-type', 'f', '-regextype', 'posix-extended',
                     '-not', '-regex', regex, '-delete']
         else:
