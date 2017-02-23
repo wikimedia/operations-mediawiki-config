4db5742427e4fca434db5c8410c3595e582d7c77
David Causse <dcausse@wikimedia.org>
2017-02-23 15:16:10 +0100

    [cirrus] Add $wgCirrusSearchElasticQuirks
    Needed to workaround a bug that affects elastic versions 5.0, 5.1 and 5.2.
    Can be abandonned if the version deployed to production is 5.3.
    
    Change-Id: I1119c1afe6719f9cc2b41b2cd6c3c8da7a307cfb

--- wmf-config/CirrusSearch-common.php
+++ wmf-config/CirrusSearch-common.php
@@ -21,6 +21,13 @@ if ( $wmgUseClusterJobqueue ) {
 		$wgJobTypeConf['default'];
 }
 
+// Needed for elastic version 5.0 to 5.3 is deployed
+// in 5.3 it'll produce, we should really silent this
+// log message when upgrading to 5.3
+$wgCirrusSearchElasticQuirks = [
+	'query_string_max_determinized_states' => true,
+];
+
 # Set up the the default cluster to send queries to,
 # and the list of clusters to write to.
 if ( $wmgCirrusSearchDefaultCluster === 'local' ) {
