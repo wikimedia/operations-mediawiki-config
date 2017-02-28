33e65005de14b6c3850ce5a18084f5e32380a14c
Max Semenik <maxsem.wiki@gmail.com>
2017-02-28 12:44:30 -0800

    Disable SemanticForms API, slowwww
    Change-Id: I82e92b78f080a2c71cb84a6b7984cf7def6824f6

--- wmf-config/wikitech.php
+++ wmf-config/wikitech.php
@@ -23,6 +23,9 @@ unset( $wgSpecialPages['SMWAdmin'] );
 // Disable creation of new Forms on wikitech, we're moving away from this
 $wgNamespaceProtection[106] = 'nomorenewforms';
 
+$wgAPIModules['sfautocomplete'] = 'ApiDisabled';
+$wgAPIModules['sfautoedit'] = 'ApiDisabled';
+
 require_once( "$IP/extensions/LdapAuthentication/LdapAuthentication.php" );
 $wgAuthManagerAutoConfig['primaryauth'] += [
 	LdapPrimaryAuthenticationProvider::class => [
