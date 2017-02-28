077c935bb5ca6019cea676d27cdd316e1133c614
Jaime Crespo <jcrespo@wikimedia.org>
2017-02-28 12:06:55 +0100

    mariadb: Depool db1053 for maintenance
    Bug: T147747
    Change-Id: Ie508c2fed5f3956739eb028260ebb7eb113d6326

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -129,7 +129,7 @@ $wgLBFactoryConf = [
 	],
 	's4' => [
 		'db1040' => 0,   # B2 1.4TB  64GB, master
-		'db1053' => 1,   # A2 2.8TB  96GB, watchlist, recentchanges, contributions, logpager
+#		'db1053' => 1,   # A2 2.8TB  96GB, watchlist, recentchanges, contributions, logpager
 		'db1056' => 1,   # C2 2.8TB  96GB, watchlist, recentchanges, contributions, logpager
 		'db1059' => 1,   # C2 2.8TB  96GB, api
 		'db1064' => 0,   # D1 2.8TB 160GB, vslow, dump #Master for db1095 - new sanitarium #T153743
@@ -331,23 +331,23 @@ $wgLBFactoryConf = [
 			'db1068' => 3,
 		],
 		'watchlist' => [
-			'db1053' => 1,
+#			'db1053' => 1,
 			'db1056' => 1,
 		],
 		'recentchanges' => [
-			'db1053' => 1,
+#			'db1053' => 1,
 			'db1056' => 1,
 		],
 		'recentchangeslinked' => [
-			'db1053' => 1,
+#			'db1053' => 1,
 			'db1056' => 1,
 		],
 		'contributions' => [
-			'db1053' => 1,
+#			'db1053' => 1,
 			'db1056' => 1,
 		],
 		'logpager' => [
-			'db1053' => 1,
+#			'db1053' => 1,
 			'db1056' => 1,
 		],
 	],
