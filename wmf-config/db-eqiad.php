0971a6049149dc27c01b1315c7d0a5ef8f9d0f60
Manuel Arostegui <marostegui@wikimedia.org>
2017-03-06 20:19:27 +0100

    db-eqiad.php: depool db1060
    server is having issues
    
    Change-Id: Id487a703a1dead6623493339e7546d1d5b8fb920

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -111,8 +111,8 @@ $wgLBFactoryConf = [
 		'db1021' => 0,   # B1 1.4TB  64GB, vslow, dump
 		'db1036' => 1,   # B2 1.4TB  64GB, watchlist, recentchanges, contributions, logpager
 		'db1054' => 1,   # A3 2.8TB  96GB, api
-		'db1060' => 1,   # C2 2.8TB  96GB, api
-#		'db1063' => 0,   # D1 2.8TB 128GB
+#		'db1060' => 1,   # C2 2.8TB  96GB, api #disk issues
+		'db1063' => 1,   # D1 2.8TB 128GB #temporary API
 #		'db1067' => 0,   # D1 2.8TB 160GB
 		'db1074' => 500, # A2 3.6TB 512GB
 		'db1076' => 500, # B1 3.6TB 512GB
@@ -278,7 +278,7 @@ $wgLBFactoryConf = [
 		],
 		'api' => [
 			'db1054' => 1,
-			'db1060' => 1,
+			'db1063' => 1,
 		],
 		'watchlist' => [
 			'db1036' => 1,
