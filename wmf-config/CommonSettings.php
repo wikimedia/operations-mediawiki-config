17ded338cf53ed8d690ed2e412823ee03fb57146
Reedy <reedy@wikimedia.org>
2017-03-07 16:54:16 +0000

    Remove EducationProgram config back compat hack
    Change-Id: I319ef26f3427c9f87dfff8eda43e0568ba331a87

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -2596,7 +2596,7 @@ $wgJobTypesExcludedFromDefaultQueue[] = 'gwtoolsetGWTFileBackendCleanupJob';
 
 if ( $wmgUseEducationProgram ) {
 	require_once( "$IP/extensions/EducationProgram/EducationProgram.php" );
-	$egEPSettings['dykCategory'] = $wgEPSettings['dykCategory'] = $wmgEducationProgramDYKCat;
+	$wgEPSettings['dykCategory'] = $wmgEducationProgramDYKCat;
 	$wgNamespaceProtection[EP_NS] = [ 'ep-course' ]; // T112806 (security)
 }
 
