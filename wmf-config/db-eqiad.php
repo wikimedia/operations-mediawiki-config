330befea1b9a64d23c1089d4fbf2a15cee7edb41
Jaime Crespo <jcrespo@wikimedia.org>
2017-03-09 08:52:09 +0100

    mariadb: Repool db1051 with low weight
    Bug: T159319
    Change-Id: Ib919399382e98647e318d20160778c94643f11cb

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -95,7 +95,7 @@ $wgLBFactoryConf = [
 	's1' => [
 		'db1052' => 0,   # B3 2.8TB  96GB, master
 #		'db1057' => 0,   # C2 2.8TB  96GB, old master
-#		'db1051' => 50,  # B3 2.8TB  96GB, watchlist, recentchanges, contributions, logpager
+		'db1051' => 1,   # B3 2.8TB  96GB, watchlist, recentchanges, contributions, logpager
 		'db1055' => 50,  # C2 2.8TB  96GB, watchlist, recentchanges, contributions, logpager
 		'db1065' => 0,   # D1 2.8TB 160GB, vslow, dump, master for sanitarium
 		'db1066' => 50,  # D1 2.8TB 160GB, api
@@ -238,24 +238,24 @@ $wgLBFactoryConf = [
 'groupLoadsBySection' => [
 	's1' => [
 		'watchlist' => [
-#			'db1051' => 1,
-			'db1055' => 1,
+			'db1051' => 1,
+			'db1055' => 10,
 		],
 		'recentchanges' => [
-#			'db1051' => 1,
-			'db1055' => 1,
+			'db1051' => 1,
+			'db1055' => 10,
 		],
 		'recentchangeslinked' => [
-#			'db1051' => 1,
-			'db1055' => 1,
+			'db1051' => 1,
+			'db1055' => 10,
 		],
 		'contributions' => [
-#			'db1051' => 1,
-			'db1055' => 1,
+			'db1051' => 1,
+			'db1055' => 10,
 		],
 		'logpager' => [
-#			'db1051' => 1,
-			'db1055' => 1,
+			'db1051' => 1,
+			'db1055' => 10,
 		],
 		'dump' => [
 			'db1065' => 1,
