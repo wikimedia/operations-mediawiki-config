e06f734de5a4ea7726b840a11647a42d6320f8c0
Zppix <Megadev44s.mail@gmail.com>
2017-03-13 13:39:03 +0000

    Removal of "editusercssjs"
    Deprecated in MediaWiki 1.16
    
    Change-Id: Ie3b4ddfba904ae064f74881370e01f412c419e7c

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -7835,7 +7835,8 @@ $wgConf->settings = [
 		],
 		'interface-editor' => [ // T109755
 			'editinterface' => true, // T109755
-			'editusercssjs' => true, // T109755
+			'editusercss' => true, // T14110
+			'edituserjs' => true,  // T14110
 			'tboverride' => true, // T109755
 		],
 		'patroller' => [ // T109755
@@ -8429,7 +8430,8 @@ $wgConf->settings = [
 			'abusefilter-view' => true,
 			'abusefilter-view-private' => true,
 			'editinterface' => true,
-			'editusercssjs' => true,
+			'editusercss' => true,// T14110
+			'edituserjs' => true, // T14110
 			'import' => true,
 			'tboverride' => true,
 			'templateeditor' => true, // T102466
