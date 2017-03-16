3c2bd85c1d94ec6b4d777566ab3f2fd47fbb568f
Addshore <addshorewiki@gmail.com>
2017-03-16 11:29:47 +0000

    wmgUseInterwikiSorting true for wikidataclients
    This reverts commit cd8554ddfc57cb6a865b7cd03673659a2fa193f3.
    
    Bug: T160465
    Bug: T150183
    Depends-On: Ib47938781b1b6ee8c73ac57aacf1daea8a112f7f
    Change-Id: I54b8b392b02bf8b57e65acb82b4e67e9f2cfa7ef

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -17005,7 +17005,7 @@ $wgConf->settings = [
 
 'wmgUseInterwikiSorting' => [
 	'default' => false,
-	// 'wikidataclient' => true, // T150183
+	'wikidataclient' => true, // T150183
 ],
 
 'wgInterwikiSortingSort' => [
