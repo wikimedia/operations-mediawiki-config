b19bf1b50229adb958bb125e3e0d71fff379079e
Manuel Arostegui <marostegui@wikimedia.org>
2017-03-16 10:46:34 +0100

    db-eqiad.php: Depool db1051
    Needs an alter table
    
    Bug: T160415
    Change-Id: I0340441f14b8b42ac9b9ccb085476be8dac21954

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -96,7 +96,7 @@ $wgLBFactoryConf = [
 		'db1052' => 0,   # B3 2.8TB  96GB, master
 #		'db1067' => 0,   # D1 2.8TB 160GB, old master
 #		'db1057' => 0,   # C2 2.8TB  96GB, down, T160435
-		'db1051' => 50,  # B3 2.8TB  96GB, watchlist, recentchanges, contributions, logpager
+#		'db1051' => 50,  # B3 2.8TB  96GB, watchlist, recentchanges, contributions, logpager #T160415
 		'db1055' => 50,  # C2 2.8TB  96GB, watchlist, recentchanges, contributions, logpager
 		'db1065' => 0,   # D1 2.8TB 160GB, vslow, dump, master for sanitarium
 		'db1066' => 50,  # D1 2.8TB 160GB, api
@@ -238,23 +238,23 @@ $wgLBFactoryConf = [
 'groupLoadsBySection' => [
 	's1' => [
 		'watchlist' => [
-			'db1051' => 1,
+#			'db1051' => 1,
 			'db1055' => 1,
 		],
 		'recentchanges' => [
-			'db1051' => 1,
+#			'db1051' => 1,
 			'db1055' => 1,
 		],
 		'recentchangeslinked' => [
-			'db1051' => 1,
+#			'db1051' => 1,
 			'db1055' => 1,
 		],
 		'contributions' => [
-			'db1051' => 1,
+#			'db1051' => 1,
 			'db1055' => 1,
 		],
 		'logpager' => [
-			'db1051' => 1,
+#			'db1051' => 1,
 			'db1055' => 1,
 		],
 		'dump' => [
