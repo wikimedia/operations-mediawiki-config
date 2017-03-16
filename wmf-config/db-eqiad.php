8b8ae2573b5c1b31c38cbc884c7f2b86ae42de06
Manuel Arostegui <marostegui@wikimedia.org>
2017-03-16 14:42:16 +0100

    db-eqiad.php: Depool db1045
    Needs an alter table
    
    Bug: T160415
    Change-Id: I182c742f9c5d40d19c088526929d124555b9a904

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -141,7 +141,7 @@ $wgLBFactoryConf = [
 	's5' => [
 		'db1049' => 0,   # B1 2.8TB  64GB, master
 		'db1026' => 1,   # B1 1.4TB  64GB, watchlist, recentchanges, contributions, logpager
-		'db1045' => 1,   # B2 1.4TB  64GB, watchlist, recentchanges, contributions, logpager
+#		'db1045' => 1,   # B2 1.4TB  64GB, watchlist, recentchanges, contributions, logpager #T160415
 #		'db1070' => 50,  # D1 2.8TB 160GB, vslow, dump #ROW based replication - master for db1095
 		'db1071' => 50,  # D1 2.8TB 160GB, #temporary vslow #T153743
 		'db1082' => 300, # A2 3.6TB 512GB, api
@@ -364,23 +364,23 @@ $wgLBFactoryConf = [
 		],
 		'watchlist' => [
 			'db1026' => 1,
-			'db1045' => 1,
+#			'db1045' => 1,
 		],
 		'recentchanges' => [
 			'db1026' => 1,
-			'db1045' => 1,
+#			'db1045' => 1,
 		],
 		'recentchangeslinked' => [
 			'db1026' => 1,
-			'db1045' => 1,
+#			'db1045' => 1,
 		],
 		'contributions' => [
 			'db1026' => 1,
-			'db1045' => 1,
+#			'db1045' => 1,
 		],
 		'logpager' => [
 			'db1026' => 1,
-			'db1045' => 1,
+#			'db1045' => 1,
 		],
 	],
 	's6' => [
