621fe2bc8277dfe6260f15ab24f46f3a55a6320f
David Causse <dcausse@wikimedia.org>
2017-03-20 17:16:05 +0100

    [es5 upgrade] step 3: depool eqiad for writes (take 2)
    - Depool eqiad
    - Remove hack and officially switch traffic to codfw
    - Re-enable the completion suggester
    
    Bug: T157479
    Change-Id: I597af39c049ddbbcf6951e0371bdf48d41f1e940

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -17292,7 +17292,7 @@ $wgConf->settings = [
 // $wgTranslateTranslationDefaultService in CommonSettings.php if you plan to
 // bring down a specific cluster.
 'wmgCirrusSearchDefaultCluster' => [
-	'default' => 'local',
+	'default' => 'codfw',
 ],
 
 'wmgCirrusSearchClusterOverrides' => [
@@ -17300,7 +17300,7 @@ $wgConf->settings = [
 ],
 
 'wmgCirrusSearchWriteClusters' => [
-	'default' => [ 'eqiad', 'codfw' ],
+	'default' => [ /* 'eqiad', */ 'codfw' ],
 ],
 
 'wmgCirrusSearchPreferRecentDefaultDecayPortion' => [
