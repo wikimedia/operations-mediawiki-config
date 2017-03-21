707fc74ac2ecfb7bb414f7f68fbe889a2713fec0
Erik Bernhardson <ebernhardson@wikimedia.org>
2017-03-21 13:19:31 -0700

    Turn completion suggester off until length error is fixed
    The patch for T161001 needs to be deployed, and a rebuild of completion
    indices needs to be done to before completion suggester can be turned
    back on. As is the suggester is erroring out for a variety of use cases
    
    Bug: T161001
    Change-Id: I54589ec3074b9e5947d466a66ee2b3748a83ae19

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -17528,7 +17528,7 @@ $wgConf->settings = [
 
 // Enable completion suggester on all wikis (except wikidata)
 'wmgCirrusSearchUseCompletionSuggester' => [
-	'default' => 'yes',
+	'default' => 'no',
 	'wikidatawiki' => 'no',
 ],
 
