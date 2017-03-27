9c438d0721bcb409a130c4c0a631013c89beec96
Deskana <dgarry@wikimedia.org>
2017-03-27 12:44:15 +0100

    Updates and typo fixes to CirrusSearch-common.php
    This patch fixes a few typos, fixes some weird grammar, and updates the
    contact points listed in CirrusSearch-common.php.
    
    Change-Id: Id4a6f430dd4d63d7e3b510fb765d4b92d9f4aa4b

--- wmf-config/CirrusSearch-common.php
+++ wmf-config/CirrusSearch-common.php
@@ -1,15 +1,15 @@
 <?php
 # WARNING: This file is publically viewable on the web. Do not put private data here.
 
-# This file hold the CirrusSearch configuration which is common to all realms,
-# ie settings should apply to both the production cluster and the beta
+# This file holds the CirrusSearch configuration which is common to all realms,
+# i.e. settings should apply to both the production cluster and the beta
 # cluster.
-# If you ever want to stick there an IP address, you should use the per realm
-# specific files CirrusSearch-labs.php and CirrusSearch-production.php
+# If you ever want to put in an IP address, you should use the realm-specific
+# files CirrusSearch-labs.php and CirrusSearch-production.php
 
 # See: https://wikitech.wikimedia.org/wiki/Search
 #
-# Contact Wikimedia operations or platform engineering for more details.
+# Contact Wikimedia Operations or Wikimedia Discovery for more details.
 
 $wgSearchType = 'CirrusSearch';
 
