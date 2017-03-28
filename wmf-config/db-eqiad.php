884c5a60f236dc6248cabf1346d3c1af135196c1
Manuel Arostegui <marostegui@wikimedia.org>
2017-03-28 16:30:54 +0200

    db-eqiad.php: Depool db1091
    Convert UNIQUE keys into PK for all the used tables
    
    Bug: T17441
    Change-Id: If05415bee427468d9694b8211107a4bcb65ba957

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -136,7 +136,7 @@ $wgLBFactoryConf = [
 		'db1068' => 1,   # D1 2.8TB 160GB, api
 		'db1081' => 500, # A2 3.6TB 512GB
 		'db1084' => 500, # B1 3.6TB 512GB
-		'db1091' => 500, # D2 3.6TB 512GB
+#		'db1091' => 500, # D2 3.6TB 512GB #T17441
 	],
 	's5' => [
 		'db1049' => 0,   # B1 2.8TB  64GB, master
