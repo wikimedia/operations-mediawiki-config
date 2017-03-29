500ceb5428a670c31dd4c27b8deae8aa2c99d621
Timo Tijhof <krinklemail@gmail.com>
2017-03-28 18:42:45 -0700

    errorpages: Restyle 503/php-fatal error to match Varnish error
    The Varnish error page was originally based on the 503/php-fatal page,
    including all the localisations, "try again", Freenode, donate,
    and "please include the details below." components.
    
    This was updated in Puppet 2015 to match the redesigned "Unknown domain"
    and "404.html" page (commit a806dd0a61f0afb9c5).
    
    It kept only the "try again" and "please include the details below." parts.
    
    I don't know how often the 503.html is used (as far as I can tell, Varnish
    will capture that in most cases and display its own page). But let's update
    it and the php-fatal pages to match the ones from Varnish.
    
    This commit also updates 404.html/404.php/default.html to feature an
    element for `<div class="content" role="main">`, matching the updates that
    were made to the Varnish error page. (Visually this is a no-op.)
    
    Before: https://phabricator.wikimedia.org/F2618019
    After: https://phabricator.wikimedia.org/F7065754
    
    Bug: T113114
    Change-Id: I15295835a1a1dc6b703e2c7c90e398eaf5840c53

--- errorpages/404.php
+++ errorpages/404.php
@@ -34,7 +34,8 @@ $outputHtml=<<<END
 <link rel="shortcut icon" href="/favicon.ico">
 <style>
 * { margin: 0; padding: 0; }
-body { background: #fff; margin: 7% auto 0; padding: 2em 1em 1em; font: 15px/1.6 sans-serif; color: #333; max-width: 640px; }
+body { background: #fff; font: 15px/1.6 sans-serif; color: #333; }
+.content { margin: 7% auto 0; padding: 2em 1em 1em; max-width: 640px; }
 img { float: left; margin: 0 2em 2em 0; }
 a img { border: 0; }
 h1 { margin-top: 1em; font-size: 1.2em; }
@@ -43,6 +44,7 @@ a { color: #0645AD; text-decoration: none; }
 a:hover { text-decoration: underline; }
 em { font-style: normal; color: #777; }
 </style>
+<div class="content" role="main">
 <a href="https://www.wikimedia.org"><img src="https://www.wikimedia.org/static/images/wmf.png" srcset="https://www.wikimedia.org/static/images/wmf-2x.png 2x" alt=Wikimedia width=135 height=135></a>
 <h1>Page not found</h1>
 <p><em>$encUrl</em></p>
@@ -50,6 +52,7 @@ em { font-style: normal; color: #777; }
 <p><b>Did you mean: <a href="$encTarget">$encTarget</a></b></p>
 <div style="clear:both;"></div>
 <p>Alternatively, you can visit the <a href="/">Main Page</a> or read <a href="https://en.wikipedia.org/wiki/HTTP_404" title="Wikipedia: HTTP 404">more information</a> about this type of error.</p>
+</div>
 </html>
 END;
 
