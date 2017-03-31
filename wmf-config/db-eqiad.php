dfc62aea3281114b30e1632cc457b9ffc70e4950
Jaime Crespo <jcrespo@wikimedia.org>
2017-03-31 10:44:37 +0200

    Pool db1034 (with the right partit. & indexes) as the db-special-s7
    Bug: T159319
    Change-Id: I864244981c7096113a3089cc15897c27358cb76d

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -164,7 +164,7 @@ $wgLBFactoryConf = [
 		'db1028' => 0,   # B1 1.4TB  64GB, vslow, dump
 		'db1034' => 1,   # B2 1.4TB  64GB, watchlist, recentchanges, contributions, logpager
 #		'db1039' => 0,   # B2 1.4TB  64GB
-		'db1062' => 1,   # D1 2.8TB 128GB, watchlist, recentchanges, contributions, logpager
+#		'db1062' => 1,   # D1 2.8TB 128GB, watchlist, recentchanges, contributions, logpager
 		'db1079' => 300, # A2 3.6TB 512GB, api
 		'db1086' => 500, # B3 3.6TB 512GB, api
 		'db1094' => 500, # D2 3.6TB 512GB
@@ -421,23 +421,18 @@ $wgLBFactoryConf = [
 		],
 		'watchlist' => [
 			'db1034' => 1,
-			'db1062' => 100,
 		],
 		'recentchanges' => [
 			'db1034' => 1,
-			'db1062' => 100,
 		],
 		'recentchangeslinked' => [
 			'db1034' => 1,
-			'db1062' => 100,
 		],
 		'contributions' => [
 			'db1034' => 1,
-			'db1062' => 100,
 		],
 		'logpager' => [
 			'db1034' => 1,
-			'db1062' => 100,
 		],
 	],
 ],
