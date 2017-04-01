219e20e904551c8d674dd9ff9ad9b02b143a47aa
Sébastien Santoro <dereckson@espace-win.org>
2017-04-01 19:09:09 +0000

    Enable NewUserMessage on tr.wikiquote
    Bug: T161962
    Change-Id: Icdf7ddf96e1fa9e01103df6deb94ff40f1567457

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -12548,6 +12548,7 @@ $wgConf->settings = [
 	'thwikisource' => true, // T33600
 	'thwikibooks' => true, // T33600
 	'thwikiquote' => true, // T33600
+	'trwikiquote' => true, // T161962
 	'ukwiki' => true, // T67125
 	'urwiki' => true, // T64398
 	'zhwiki' => true, // T32362
