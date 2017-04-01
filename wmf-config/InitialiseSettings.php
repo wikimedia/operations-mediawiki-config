f4f86e57df95f96e9fac90d53d9fa3b50cea566b
Martin Urbanec <martin.urbanec@wikimedia.cz>
2017-04-01 12:46:32 +0200

    Add autopatrolled group to svwiktionary
    Bug: T161919
    Change-Id: I19e2b82a5424f63b8b8140f72149638e9ce84b04

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -9159,6 +9159,9 @@ $wgConf->settings = [
 		'patroller' => [ 'patrol' => true ], // T93339
 		'rollbacker' => [ 'rollback' => true ], // T93339
 	],
+	'+svwiktionary' => [
+		'autopatrolled' => [ 'autopatrol' => true ], // T161919
+	],
 	'+swwiki' => [
 		'*' => [ 'createpage' => false ], // T44894
 	],
@@ -10119,6 +10122,11 @@ $wgConf->settings = [
 			'rollbacker', // T93339
 		],
 	],
+	'+svwiktionary' => [
+		'sysop' => [
+			'autopatrolled', // T161919
+		],
+	],
 	'+tawiki' => [
 		'bureaucrat' => [ 'nocreate' ],
 		'sysop' => [ 'patroller', 'rollbacker', 'autopatrolled' ], // T95180
@@ -10862,6 +10870,11 @@ $wgConf->settings = [
 			'autopatrolled', // T93339
 			'patroller', // T93339
 			'rollbacker', // T93339
+		],
+	],
+	'+svwiktionary' => [
+		'sysop' => [
+			'autopatrolled', // T161919
 		],
 	],
 	'+tawiki' => [
