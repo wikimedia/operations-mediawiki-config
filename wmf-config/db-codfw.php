e2c91cae20740e494e4e9f170f3c5300a3dd912f
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2017-04-04 10:01:34 +0200

    Switch master DC from eqiad to codfw
    Please note that files included in this patch need to be synced at
    separate times:
    
    * First db-eqiad.php to set eqiad to read-only
    * Once all preparations are done, CommonSettings.php should be synced to
      actually switch over.
    * Once all the next steps are done, db-codfw.php should be synced to end
      the read-only phase
    
    Change-Id: If9acc25a83dcd4f212cc72f35450131b5b12da2c

--- wmf-config/db-codfw.php
+++ wmf-config/db-codfw.php
@@ -635,15 +635,16 @@ $wgLBFactoryConf = [
 # This key must exist for the master switch script to work.
 #
 # These read only messages should currently be kept,
-# to prevent accidental write to eqiad from codfw.
+# to prevent accidental write to eqiad from codfw,
+# when the master dc is eqiad.
 'readOnlyBySection' => [
-	's1'      => 'MediaWiki is in read-only mode for maintenance. Please try again in a few minutes',
-	's2'      => 'MediaWiki is in read-only mode for maintenance. Please try again in a few minutes',
-	'DEFAULT' => 'MediaWiki is in read-only mode for maintenance. Please try again in a few minutes', # s3
-	's4'      => 'MediaWiki is in read-only mode for maintenance. Please try again in a few minutes',
-	's5'      => 'MediaWiki is in read-only mode for maintenance. Please try again in a few minutes',
-	's6'      => 'MediaWiki is in read-only mode for maintenance. Please try again in a few minutes',
-	's7'      => 'MediaWiki is in read-only mode for maintenance. Please try again in a few minutes',
+#	's1'      => 'MediaWiki is in read-only mode for maintenance. Please try again in a few minutes',
+#	's2'      => 'MediaWiki is in read-only mode for maintenance. Please try again in a few minutes',
+#	'DEFAULT' => 'MediaWiki is in read-only mode for maintenance. Please try again in a few minutes', # s3
+#	's4'      => 'MediaWiki is in read-only mode for maintenance. Please try again in a few minutes',
+#	's5'      => 'MediaWiki is in read-only mode for maintenance. Please try again in a few minutes',
+#	's6'      => 'MediaWiki is in read-only mode for maintenance. Please try again in a few minutes',
+#	's7'      => 'MediaWiki is in read-only mode for maintenance. Please try again in a few minutes',
 ],
 
 ];
