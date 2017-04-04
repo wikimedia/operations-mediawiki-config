8b8cdd6927c3da766ef43cc2d3e6a3444fefa0cf
Marostegui <marostegui@wikimedia.org>
2017-04-04 08:13:25 +0000

    Revert "db-eqiad.php: Depool db1015"
    This reverts commit 08e83abb1ae07c298c5b59f3f637437c00e38fba.
    
    Change-Id: Ia124eff6ea544f2ce4648a140034c59f7b4dae2b

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -120,8 +120,8 @@ $wgLBFactoryConf = [
 	/* s3 */ 'DEFAULT' => [
 		'db1075' => 0,   # A2 3.6TB 512GB, master
 		'db1038' => 0,   # B2 1.4TB  64GB, vslow, dump, old master
-#		'db1015' => 1,   # A2 1.4TB  64GB, watchlist, recentchanges, contributions, logpager #T159319
-		'db1035' => 1,   # B2 1.4TB  64GB #temporary watchlist, recent changes #T159319
+		'db1015' => 1,   # A2 1.4TB  64GB, watchlist, recentchanges, contributions, logpager
+#		'db1035' => 0,   # B2 1.4TB  64GB
 #		'db1044' => 0,   # B2 1.4TB  64GB, #Temporary master for db1095 - new sanitarium #T150802
 		'db1077' => 500, # B1 3.6TB 512GB
 		'db1078' => 500, # C3 3.6TB 512GB
@@ -303,19 +303,19 @@ $wgLBFactoryConf = [
 			'db1038' => 1,
 		],
 		'watchlist' => [
-			'db1035' => 1,
+			'db1015' => 1,
 		],
 		'recentchanges' => [
-			'db1035' => 1,
+			'db1015' => 1,
 		],
 		'recentchangeslinked' => [
-			'db1035' => 1,
+			'db1015' => 1,
 		],
 		'contributions' => [
-			'db1035' => 1,
+			'db1015' => 1,
 		],
 		'logpager' => [
-			'db1035' => 1,
+			'db1015' => 1,
 		],
 	],
 	's4' => [
