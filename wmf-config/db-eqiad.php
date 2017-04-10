a6ea31600fe7da1c40ecf5d1bf0e92259f7743ad
Manuel Arostegui <marostegui@wikimedia.org>
2017-04-10 16:25:09 +0200

    db-eqiad.php: Depool db1073
    db1073 needs an ALTER table.
    Reduce the weight for the other API hosts for the main traffic
    
    Bug: T132416
    Change-Id: Ib89caaf8e4dfabe48397e46f546c462bdaaac3bc

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -98,9 +98,9 @@ $wgLBFactoryConf = [
 		'db1051' => 50,  # B3 2.8TB  96GB, watchlist, recentchanges, contributions, logpager
 		'db1055' => 1,   # C2 2.8TB  96GB, watchlist, recentchanges, contributions, logpager
 		'db1065' => 0,   # D1 2.8TB 160GB, vslow, dump, master for sanitarium
-		'db1066' => 50,  # D1 2.8TB 160GB, api
-		'db1072' => 50,  # B2 2.8TB 160GB, api
-		'db1073' => 50,  # B3 2.8TB 160GB, api
+		'db1066' => 20,  # D1 2.8TB 160GB, api #less main traffic weight T142725
+		'db1072' => 20,  # B2 2.8TB 160GB, api #less main traffic weight T142725
+#		'db1073' => 50,  # B3 2.8TB 160GB, api #T142725
 		'db1080' => 500, # A2 3.6TB 512GB
 		'db1083' => 500, # B1 3.6TB 512GB
 		'db1089' => 500, # C3 3.6TB 512GB
@@ -265,7 +265,7 @@ $wgLBFactoryConf = [
 		'api' => [
 			'db1066' => 1,
 			'db1072' => 1,
-			'db1073' => 1,
+#			'db1073' => 1,
 		],
 	],
 	's2' => [
