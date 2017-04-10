527b3e4fa08fae2b98f317cbb80bee99f8eeec3d
Kunal Mehta <legoktm@member.fsf.org>
2017-04-10 13:03:55 -0700

    Deploy Linter to all wikis
    Minus loginwiki and votewiki because it's not needed there.
    
    Bug: T148609
    Change-Id: I39592ad5628855e3da8ce716075f7cd4f1de2f12

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -14185,11 +14185,9 @@ $wgConf->settings = [
 ],
 
 'wmgUseLinter' => [
-	'default' => false,
-	'small' => true,
-	'medium' => true,
-	'testwiki' => true,
-	'group0' => true,
+	'default' => true,
+	'votewiki' => false,
+	'loginwiki' => false,
 ],
 
 'wgLinterStatsdSampleFactor' => [
