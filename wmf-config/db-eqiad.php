64260acc568d0d5e319c6862312b6c9ff31cd261
Manuel Arostegui <marostegui@wikimedia.org>
2017-04-11 08:22:34 +0200

    db-eqiad.php: Depool db1072
    db1072 needs an alter table
    
    Bug: T132416
    Change-Id: I21352d5f77020efce502022ea1f4f58e6e8f19e1

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -98,9 +98,9 @@ $wgLBFactoryConf = [
 		'db1051' => 50,  # B3 2.8TB  96GB, watchlist, recentchanges, contributions, logpager
 		'db1055' => 50,  # C2 2.8TB  96GB, watchlist, recentchanges, contributions, logpager
 		'db1065' => 0,   # D1 2.8TB 160GB, vslow, dump, master for sanitarium
-		'db1066' => 50,  # D1 2.8TB 160GB, api
-		'db1072' => 50,  # B2 2.8TB 160GB, api
-		'db1073' => 50,  # B3 2.8TB 160GB, api
+		'db1066' => 20,  # D1 2.8TB 160GB, api #less main traffic weight T132416
+#		'db1072' => 50,  # B2 2.8TB 160GB, api #T132416
+		'db1073' => 20,  # B3 2.8TB 160GB, api #less main traffic weight T132416
 		'db1080' => 500, # A2 3.6TB 512GB
 		'db1083' => 500, # B1 3.6TB 512GB
 		'db1089' => 500, # C3 3.6TB 512GB
@@ -264,7 +264,7 @@ $wgLBFactoryConf = [
 		],
 		'api' => [
 			'db1066' => 1,
-			'db1072' => 1,
+#			'db1072' => 1,
 			'db1073' => 1,
 		],
 	],
