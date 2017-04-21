7229bd9147c64ccc856c48dcb40f611285a596b8
Antoine Musso <hashar@free.fr>
2017-04-21 13:30:58 +0200

    phpunit: factor out logic to handle globals vars
    loggingTest data providers loads $wgConf in global scope. With PHPUnit
    backing up globals on each test, that slow down the test run.  The class
    has logic to restore the globals in the data provider so tests get run
    with a clean global scope.
    
    cirrusTest has the same need. Hence factor out global handling code to a
    new PHPUnit_FrameWork_TestCase: WgConfTestCase.
    
    Switch loggingTest to it.
    Port cirrusTest.
    
    When setting globals with setGlobals(), we want to make sure they are
    restored with restoreGlobals().  Thus throw an exception whenever the
    test case object is descruted but globals have not been restored, that
    will prevent unintentional leaks.
    
    Test runs for cirrusTest goes from 7s to 3s.
    
    Later on WgConfTestCase can be extended to load a $wgConf object. That
    seems to be a common behavior of loggingTest and cirrusTest.
    
    Change-Id: I7a86e856881da69431c29b81cf1005070e029a0b

---
+++ tests/WgConfTestCase.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * PHPUnit testcase for $wgConf testing.
+ *
+ * @license GPLv2 or later
+ * @author Erik Bernhardson <ebernhardson@wikimedia.org>
+ * @copyright Copyright © 2015, Erik Bernhardson <ebernhardson@wikimedia.org>
+ * @file
+ */
+
+class WgConfTestCase extends PHPUnit_Framework_TestCase {
+
+	protected $globals = array();
+
+	protected function restoreGlobals() {
+		foreach ( $this->globals as $key => $value ) {
+			$GLOBALS[$key] = $value;
+		}
+		$this->globals = array();
+	}
+
+	protected function setGlobals( $pairs, $value = null ) {
+		if ( is_string( $pairs ) ) {
+			$pairs = array( $pairs => $value );
+		}
+		foreach ( $pairs as $key => $value ) {
+			// only set value in $this->globals on first call
+			if ( !array_key_exists( $key, $this->globals ) ) {
+				if ( isset( $GLOBALS[$key] ) ) {
+					// break any object references
+					try {
+						$this->globals[$key] = unserialize( serialize( $GLOBALS[$key] ) );
+					} catch ( \Exception $e ) {
+						$this->globals[$key] = $GLOBALS[$key];
+					}
+				} else {
+					$this->globals[$key] = null;
+				}
+			}
+			$GLOBALS[$key] = $value;
+		}
+	}
+
+	function __destruct() {
+		if ( ! empty( $this->globals ) ) {
+			throw new Exception( __CLASS__ . ': setGlobals() used without restoreGlobals()' );
+		}
+	}
+
+}
