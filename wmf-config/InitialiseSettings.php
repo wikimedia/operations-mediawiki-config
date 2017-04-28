0d034763681f1fed9d0fe10b001ed1556a6fdaed
Elliott Eggleston <ejegg@ejegg.com>
2017-04-28 11:20:41 -0700

    Update instances of Wikimedia Foundation logo #1
    Original Author: Martin Urbanec <martin.urbanec@wikimedia.cz>
    
    * Site logo for wikimediafoundation.org at 1x (135px), 1.5x and 2x sizes
    * Favicon
    * Notification icons for foundationwiki
    * Site logo for donatewiki which has "Wikimedia Foundation" on it;
      leaving off Loginwiki as it doesn't, so whether
      it is Wikimedia or Wikimedia Foundation is less immediately clear
      (though no-one sees it, so it matters little).
    
    All images exported from the SVG and brute-force crushed using zopflipng.
    
    Bug: T144254
    Change-Id: I9b4a1107a4cead0adf9a16c16ddbaea6488a2c61

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -1429,6 +1429,7 @@ $wgConf->settings = [
 
     # Special wikis
 	'arbcom_cswiki' => ['1.5x' => '/static/images/project-logos/arbcom_cswiki-1.5x.png', '2x' => '/static/images/project-logos/arbcom_cswiki-2x.png'], // T151731
+	'donatewiki' => [ '1.5x' => '/static/images/project-logos/donatewiki-1.5x.png', '2x' => '/static/images/project-logos/donatewiki-2x.png' ],
 	'foundationwiki' => [ '1.5x' => '/static/images/project-logos/foundationwiki-1.5x.png', '2x' => '/static/images/project-logos/foundationwiki-2x.png' ],
 	'metawiki' => [ '1.5x' => '/static/images/project-logos/metawiki-1.5x.png', '2x' => '/static/images/project-logos/metawiki-2x.png' ],
 	'test2wiki' => [ '1.5x' => '/static/images/project-logos/test2wiki-1.5x.png', '2x' => '/static/images/project-logos/test2wiki-2x.png' ],
