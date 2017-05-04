cbc11bdd948c262367acfabe690430504b5cc25b
Manuel Arostegui <marostegui@wikimedia.org>
2017-05-04 08:21:52 +0200

    db-codfw.php: Depool tempdb2001
    This host is no longer needed
    
    Bug: T161712
    Change-Id: I33bcc818ed2bba4d455e444746a05390b4452629

--- wmf-config/db-codfw.php
+++ wmf-config/db-codfw.php
@@ -605,8 +605,8 @@ $wgLBFactoryConf = [
 	],
 	# ExtensionStore shard1 - initially for AFTv5
 	'extension1' => [
-		'10.192.32.4' => 0, # db2033, master
-		'10.192.48.14' => 5, # tempdb2001, slave #temporary #T162290
+		'10.192.32.4' => 1, # db2033, master
+#		'10.192.48.14' => 5, # tempdb2001, slave #temporary #T162290
 	],
 ],
 
