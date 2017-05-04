fc58fae6f4216821d890797ccf59707534ae8253
Manuel Arostegui <marostegui@wikimedia.org>
2017-05-04 17:05:20 +0200

    db-codfw.php: Repool db2066, depool db2059
    db2066 finished its alter table
    db2059 needs the alter table
    
    Bug: T162539
    Bug: T163548
    Change-Id: If84b0e6e1df6b9760f594476f25544dee659d0e4

--- wmf-config/db-codfw.php
+++ wmf-config/db-codfw.php
@@ -132,8 +132,8 @@ $wgLBFactoryConf = [
 		'db2038' => 50,  # C6 2.9TB 160GB, rc, log
 		'db2045' => 400, # C6 2.9TB 160GB
 		'db2052' => 50,  # D6 2.9TB 160GB, dump (inactive), vslow
-		'db2059' => 100, # D6 3.3TB 160GB, api
-#		'db2066' => 400, # D6 3.3TB 160GB #T162539 T163548
+#		'db2059' => 100, # D6 3.3TB 160GB, api #T162539 T163548
+		'db2066' => 100, # D6 3.3TB 160GB #temporary api
 	],
 	's6' => [
 		'db2028' => 0,   # B6  2.9TB  96GB, master
@@ -358,7 +358,7 @@ $wgLBFactoryConf = [
 			'db2052' => 1,
 		],
 		'api' => [
-			'db2059' => 1,
+			'db2066' => 1,
 		],
 	],
 	's6' => [
