138e379c404fb7b1ad6c3192b0fa5bf86e49b865
Reedy <reedy@wikimedia.org>
2017-05-08 11:41:00 +0000

    Re-instate "Run Pdf Processors in firejails"
    This reverts commit bcac46a86e35386d45f3a81e951109d4053a4d2d.
    
    Bug: T164145
    Bug: T164000
    Change-Id: Ic01e5ad1ac365cbc9ff3c6d109b0d08c0fbab6aa

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -1859,6 +1859,8 @@ $wgAbuseFilterEmergencyDisableAge = $wmgAbuseFilterEmergencyDisableAge;
 
 if ( $wmgUsePdfHandler ) {
 	wfLoadExtension( 'PdfHandler' );
+	$wgPdfProcessor = '/usr/local/bin/mediawiki-firejail-ghostscript';
+	$wgPdfPostProcessor = '/usr/local/bin/mediawiki-firejail-convert';
 }
 
 wfLoadExtension( 'WikiEditor' );
