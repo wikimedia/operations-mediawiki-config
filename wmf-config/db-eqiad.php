d07cc0d504ed44fa22bf3096fd9e3238dd534359
Jaime Crespo <jcrespo@wikimedia.org>
2017-05-10 12:24:45 +0200

    mariadb: Depool db1056 for reimage
    db1056 is one of the few production hosts left with trusty that
    are not going to be decommissioned ASAP.
    
    Change-Id: I4d2cdf078b0af0225a5bd3cfe5facfeff151a46f

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -127,7 +127,7 @@ $wgLBFactoryConf = [
 	's4' => [
 		'db1068' => 0,   # D1 2.8TB 160GB, master
 		'db1053' => 1,   # A2 2.8TB  96GB, watchlist, recentchanges, contributions, logpager
-		'db1056' => 1,   # C2 2.8TB  96GB, watchlist, recentchanges, contributions, logpager
+#		'db1056' => 1,   # C2 2.8TB  96GB, watchlist, recentchanges, contributions, logpager
 		'db1059' => 1,   # C2 2.8TB  96GB, api
 		'db1064' => 0,   # D1 2.8TB 160GB, vslow, dump #Master for db1095 - new sanitarium
 		'db1081' => 300, # A2 3.6TB 512GB, api
@@ -329,23 +329,23 @@ $wgLBFactoryConf = [
 		],
 		'watchlist' => [
 			'db1053' => 1,
-			'db1056' => 1,
+#			'db1056' => 1,
 		],
 		'recentchanges' => [
 			'db1053' => 1,
-			'db1056' => 1,
+#			'db1056' => 1,
 		],
 		'recentchangeslinked' => [
 			'db1053' => 1,
-			'db1056' => 1,
+#			'db1056' => 1,
 		],
 		'contributions' => [
 			'db1053' => 1,
-			'db1056' => 1,
+#			'db1056' => 1,
 		],
 		'logpager' => [
 			'db1053' => 1,
-			'db1056' => 1,
+#			'db1056' => 1,
 		],
 	],
 	's5' => [
