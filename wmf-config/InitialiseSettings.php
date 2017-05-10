858d4ed12521bc099e7f1f99a6b4bae7b204797c
Stanislav Malyshev <smalyshev@gmail.com>
2017-05-10 09:35:52 -0700

    Enable archive search on select wikis.
    This is still behind feature flag (fuzzy=1) so nothing changes in the
    default case. Indexing is not enabled yet because it needs manual index
    to add mappings.
    
    Change-Id: I0905971e29c29507cc614886c3cd5d6ed90aa28f

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -18566,11 +18566,22 @@ $wgConf->settings = [
 'wmgCirrusSearchIndexDeletes' => [
 	'default' => false,
 	'testwiki' => true,
+	'mediawikiwiki' => true,
 ],
 
 'wmgCirrusSearchEnableArchive' => [
 	'default' => false,
 	'testwiki' => true,
+        'mediawikiwiki' => true,
+        'cawiki' => true,
+        'itwiki' => true,
+        'ruwiki' => true,
+        'fawiki' => true,
+        'dewiki' => true,
+        'frwiki' => true,
+        'plwiki' => true,
+        'arwiki' => true,
+        'enwiki' => true,
 ],
 
 ];
