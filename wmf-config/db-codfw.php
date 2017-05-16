8f3dad7120109223dea638214e9eaa74deabcc5f
Manuel Arostegui <marostegui@wikimedia.org>
2017-05-16 16:47:06 +0200

    db-codfw.php: Depool db2041, repool db2056
    db2041 needs alter table
    db2056 finished alter table
    
    Bug: T162611
    Change-Id: Ia5e706fe7132f7ff5d72f99551da5847102eb477

--- wmf-config/db-codfw.php
+++ wmf-config/db-codfw.php
@@ -106,10 +106,10 @@ $wgLBFactoryConf = [
 	's2' => [
 		'db2017' => 0,   # B6 2.9TB  96GB, master
 		'db2035' => 50,  # C6 2.9TB 160GB, rc, log
-		'db2041' => 100, # C6 2.9TB 160GB, api
+#		'db2041' => 100, # C6 2.9TB 160GB, api #T162611
 #		'db2049' => 400, # C6 2.9TB 160GB, #T162611
-#		'db2056' => 50,  # D6 3.3TB 160GB, dump (inactive), vslow #T162611
-		'db2063' => 100, # D6 3.3TB 160GB, api #temporary vslow #T162611
+		'db2056' => 50,  # D6 3.3TB 160GB, dump (inactive), vslow #T162611
+		'db2063' => 100, # D6 3.3TB 160GB, api
 		'db2064' => 400, # D6 3.3TB 160GB
 	],
 	/* s3 */ 'DEFAULT' => [
@@ -268,13 +268,13 @@ $wgLBFactoryConf = [
 			'db2035' => 1,
 		],
 		'dump' => [
-			'db2063' => 1,
+			'db2056' => 1,
 		],
 		'vslow' => [
-			'db2063' => 1,
+			'db2056' => 1,
 		],
 		'api' => [
-			'db2041' => 1,
+#			'db2041' => 1,
 			'db2063' => 1,
 		],
 	],
