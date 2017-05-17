fb8cdbf2b70ec0c8c9b848a253cccb6f540490d7
Manuel Arostegui <marostegui@wikimedia.org>
2017-05-17 08:14:10 +0200

    db-codfw.php: Repool db2041 and db2049
    They finished the alter table
    
    Bug: T162611
    Change-Id: If7659e6b48eae3090059986fa33028ecc830ece4

--- wmf-config/db-codfw.php
+++ wmf-config/db-codfw.php
@@ -106,8 +106,8 @@ $wgLBFactoryConf = [
 	's2' => [
 		'db2017' => 0,   # B6 2.9TB  96GB, master
 		'db2035' => 50,  # C6 2.9TB 160GB, rc, log
-#		'db2041' => 100, # C6 2.9TB 160GB, api #T162611
-#		'db2049' => 400, # C6 2.9TB 160GB, #T162611
+		'db2041' => 100, # C6 2.9TB 160GB, api
+		'db2049' => 400, # C6 2.9TB 160GB,
 		'db2056' => 50,  # D6 3.3TB 160GB, dump (inactive), vslow
 		'db2063' => 100, # D6 3.3TB 160GB, api
 		'db2064' => 400, # D6 3.3TB 160GB
@@ -274,7 +274,7 @@ $wgLBFactoryConf = [
 			'db2056' => 1,
 		],
 		'api' => [
-#			'db2041' => 1,
+			'db2041' => 1,
 			'db2063' => 1,
 		],
 	],
