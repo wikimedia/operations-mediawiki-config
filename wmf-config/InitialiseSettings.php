6713547d7fefd6a0b8c568334b8da29ef318cac9
David Causse <dcausse@wikimedia.org>
2017-05-19 15:16:05 +0200

    [wikitech] Increase weight on Tool and Nova Resource ns
    Bug: T165725
    Change-Id: I97b405591ab7e4cc1ee2486214be3bfd2fbe0ca3

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -17670,6 +17670,10 @@ $wgConf->settings = [
 	'wikisource' => [
 		'author' => 1,
 	],
+	'wikitech' => [ // T165725
+		116 => 1.0,
+		498 => 1.0,
+	],
 ],
 
 // Enable the "Give us feedback" link after search results on enwiki
