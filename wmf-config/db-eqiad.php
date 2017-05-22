0ae5e4d9bffdd3fa26fc568a3710080cedf431e7
Manuel Arostegui <marostegui@wikimedia.org>
2017-05-22 08:32:35 +0200

    db-eqiad.php: Depool db1036
    db1036 needs an alter table
    pool db1076 for rc changes temporarily
    
    Bug: T162611
    Change-Id: I97108ce70a1ca91aa7e30c88084b0a93d9f815dc

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -109,10 +109,10 @@ $wgLBFactoryConf = [
 		'db1054' => 0,   # A3 2.8TB  96GB, master
 #		'db1018' => 0,   # B1 1.4TB  64GB, old master 2
 		'db1021' => 0,   # B1 1.4TB  64GB, vslow, dump
-		'db1036' => 1,   # B2 1.4TB  64GB, watchlist, recentchanges, contributions, logpager
+#		'db1036' => 1,   # B2 1.4TB  64GB, watchlist, recentchanges, contributions, logpager #T162611
 		'db1060' => 1,   # C2 2.8TB  96GB, api
 		'db1074' => 300, # A2 3.6TB 512GB, api
-		'db1076' => 500, # B1 3.6TB 512GB
+		'db1076' => 200, # B1 3.6TB 512GB #temporary rc #T162611
 		'db1090' => 500, # C3 3.6TB 512GB
 	],
 	/* s3 */ 'DEFAULT' => [
@@ -278,19 +278,19 @@ $wgLBFactoryConf = [
 			'db1074' => 1,
 		],
 		'watchlist' => [
-			'db1036' => 1,
+			'db1076' => 1,
 		],
 		'recentchanges' => [
-			'db1036' => 1,
+			'db1076' => 1,
 		],
 		'recentchangeslinked' => [
-			'db1036' => 1,
+			'db1076' => 1,
 		],
 		'contributions' => [
-			'db1036' => 1,
+			'db1076' => 1,
 		],
 		'logpager' => [
-			'db1036' => 1,
+			'db1076' => 1,
 		],
 	],
 	/* s3 */ 'DEFAULT' => [
