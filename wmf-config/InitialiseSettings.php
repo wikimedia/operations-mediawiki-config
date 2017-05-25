baeb444cdeec6a002300a16fda97a57d1046a36c
Sébastien Santoro <dereckson@espace-win.org>
2017-05-25 15:28:24 +0000

    Add Wikipedia wordmark in Serbian/Macedonian
    Source: https://commons.wikimedia.org/wiki/File:Wikipedia_wordmark_sr.svg
    File prepared by Srdjan m.
    
    The logo is also updated for Macedonian Wikipedia for which the logo
    is the same.
    
    Bug: T165896
    Change-Id: Id0421fd1bcbd8de73ed6da8cff05522217f4150d

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -15181,6 +15181,16 @@ $wgConf->settings = [
 		'copyright-width' => 75,
 		'copyright-height' => 18,
 	],
+	'mkwiki' => [ // T165896
+		'copyright' => '/static/images/mobile/copyright/wikipedia-wordmark-sr.svg',
+		'copyright-width' => 100,
+		'copyright-height' => 18,
+	],
+	'srwiki' => [ // T165896
+		'copyright' => '/static/images/mobile/copyright/wikipedia-wordmark-sr.svg',
+		'copyright-width' => 100,
+		'copyright-height' => 18,
+	],
 	'wikivoyage' => [
 		'copyright' => '/static/images/mobile/copyright/wikivoyage-wordmark-en.svg',
 		'copyright-width' => 96,
