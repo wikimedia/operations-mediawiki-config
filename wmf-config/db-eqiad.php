301e8c733ddfb7f8dd80d88075288ad4f0d5982b
Manuel Arostegui <marostegui@wikimedia.org>
2017-05-30 08:29:02 +0200

    db-eqiad.php: Repool db1084, depool db1081
    db1084 finished alter table
    db1081 needs alter table
    
    Bug: T166206
    Change-Id: Id9f7e6c795095ce6fec40ce35087b3e81f5fb86b

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -130,8 +130,8 @@ $wgLBFactoryConf = [
 		'db1056' => 1,   # C2 2.8TB  96GB, watchlist, recentchanges, contributions, logpager
 		'db1059' => 1,   # C2 2.8TB  96GB, api
 		'db1064' => 0,   # D1 2.8TB 160GB, vslow, dump #Master for db1095 - new sanitarium
-		'db1081' => 300, # A2 3.6TB 512GB, api
-#		'db1084' => 500, # B1 3.6TB 512GB #T166206
+#		'db1081' => 300, # A2 3.6TB 512GB, api #T166206
+		'db1084' => 300, # B1 3.6TB 512GB #temporary api #T166206
 		'db1091' => 500, # D2 3.6TB 512GB
 		'db1097' => 0,   # D1 3.6TB 512GB, old master
 	],
@@ -324,7 +324,7 @@ $wgLBFactoryConf = [
 		],
 		'api' => [
 			'db1059' => 1,
-			'db1081' => 2,
+			'db1084' => 2,
 		],
 		'watchlist' => [
 			'db1053' => 1,
