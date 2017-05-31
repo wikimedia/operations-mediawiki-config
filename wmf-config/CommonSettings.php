ec3c808af9b99d7ad7b8c5888a8c827110305397
Kunal Mehta <legoktm@member.fsf.org>
2017-05-31 14:55:55 -0700

    ExtensionDistributor: Add REL1_29, drop REL1_23
    Change-Id: I2209d478e289da0200358793feccdc7d32da20bf

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -961,14 +961,14 @@ if ( $wgDBname === 'mediawikiwiki' ) {
 	$wgExtDistDefaultSnapshot = 'REL1_28';
 
 	// Current development snapshot
-	// $wgExtDistCandidateSnapshot = 'REL1_29';
+	$wgExtDistCandidateSnapshot = 'REL1_29';
 
 	// Available snapshots
 	$wgExtDistSnapshotRefs = [
 		'master',
+		'REL1_29',
 		'REL1_28',
 		'REL1_27',
-		'REL1_23',
 	];
 
 	// Use Graphite for popular list
