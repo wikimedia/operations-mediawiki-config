00f64bb9428847b89d1a32e445a8f8cd28f05fe7
Manuel Arostegui <marostegui@wikimedia.org>
2017-05-31 11:22:56 +0200

    db-eqiad.php: Repool db1078, depool db1077
    db1078 finished alter table
    db1077 needs alter table
    
    Bug: T166278
    Change-Id: I50f52ffd6d4542f72098c6c4bac73c0848d59188

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -121,8 +121,8 @@ $wgLBFactoryConf = [
 #		'db1015' => 1,   # A2 1.4TB  64GB, do not pool, it is missing cebwiki
 		'db1035' => 50,  # B2 1.4TB  64GB, watchlist, recentchanges, contributions, logpager
 #		'db1044' => 0,   # B2 1.4TB  64GB, #Temporary master for db1095 - new sanitarium #T150802
-		'db1077' => 500, # B1 3.6TB 512GB
-#		'db1078' => 500, # C3 3.6TB 512GB #T166278
+#		'db1077' => 500, # B1 3.6TB 512GB #T166278
+		'db1078' => 500, # C3 3.6TB 512GB
 	],
 	's4' => [
 		'db1068' => 0,   # D1 2.8TB 160GB, master
