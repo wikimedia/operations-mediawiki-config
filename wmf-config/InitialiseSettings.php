73ff5308c243a80f779583c29774296c8b56f32f
Ori Livneh <ori.livneh@gmail.com>
2017-06-02 10:27:40 -0400

    Capture messages on 'autoloader' debug log channel
    To help determine whether $wgAutoloadAttemptLowercase is still needed.
    
    Bug: T166759
    Change-Id: Ia7ef9e90aea077754386f9263b37240a7ab3a634

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -4887,6 +4887,7 @@ $wgConf->settings = [
 			'buffer' => true,
 		],
 		'authentication' => 'info',
+		'autoloader' => 'debug',  // ori, for T166759 (2017-06-02)
 		'badpass' => 'debug',
 		'BounceHandler' => 'debug',
 		'Bug58676' => 'debug', # Invalid message parameter
