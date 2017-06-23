a8ec1f8216f55a7b4c69df19f96e65055813bb2e
Framawiki <framawiki@tools.wmflabs.org>
2017-06-23 20:02:18 +0200

    Enable Quiz extension on huwikibooks
    Bug: T168471
    Change-Id: Icd13fed94eb6984d0cc5915d4cc2352c6f40fced

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -11830,6 +11830,7 @@ $wgConf->settings = [
 	'nlwikibooks' => true,
 	'plwikibooks' => true,
 	'ptwikibooks' => true,
+	'huwikibooks' => true, // T168471
 	'svwiki' => true,
 	'viwikibooks' => true, // T40361
 	'zhwiki' => true,
