7ed0aad7c10d79af0f9e858d044f3bcb20621999
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2017-07-04 17:04:47 +0200

    Enable WikibaseQualityConstraints statements
    This setting of the WikibaseQualityConstraints extension enables a hook
    that runs on each Wikibase change, and which (if the change is on an
    entity of type “property”) schedules a job (“constraintsTableUpdate” job
    command) which imports constraint statements on the entity into the
    wbqc_constraints database table.
    
    This allows Wikidata editors to edit constraints immediately on the
    property, instead of editing the constraint template on the property
    talk page and then having to wait until the next time that the templates
    are imported using the UpdateConstraintsTable.php maintenance script of
    the WikibaseQualityConstraints extension (in the past, this was
    generally done once every few weeks or months).
    
    Bug: T169647
    Change-Id: I8201bef3fd87793944a95af7686e53e4bd1a41a9

--- wmf-config/Wikibase-production.php
+++ wmf-config/Wikibase-production.php
@@ -111,6 +111,7 @@ if ( $wmgUseWikibaseRepo ) {
 		}
 	}
 
+	$wgWBQualityConstraintsEnableConstraintsImportFromStatements = true;
 	$wgWBRepoSettings['dispatchingLockManager'] = 'redisLockManager';
 }
 
