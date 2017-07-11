e5327733918a3d4f058f47d5b2ec46af8ec3213a
Manuel Arostegui <marostegui@wikimedia.org>
2017-07-11 12:04:35 +0200

    db-eqiad.php: Increase db1079 weight
    Change-Id: I338946f6f2f54420796cb84ca5ee92706d3f1266

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -162,7 +162,7 @@ $wgLBFactoryConf = [
 		'db1028' => 0,   # B1 1.4TB  64GB, vslow, dump
 		'db1034' => 1,   # B2 1.4TB  64GB, watchlist, recentchanges, contributions, logpager
 #		'db1039' => 0,   # B2 1.4TB  64GB
-		'db1079' => 50, # A2 3.6TB 512GB, api #master for db1102 (sanitarium 3)
+		'db1079' => 100, # A2 3.6TB 512GB, api #master for db1102 (sanitarium 3)
 		'db1086' => 300, # B3 3.6TB 512GB, api
 		'db1094' => 500, # D2 3.6TB 512GB
 	],
@@ -414,7 +414,7 @@ $wgLBFactoryConf = [
 		],
 		'api' => [
 			'db1079' => 1,
-			'db1086' => 5,
+			'db1086' => 4,
 		],
 		'watchlist' => [
 			'db1034' => 1,
