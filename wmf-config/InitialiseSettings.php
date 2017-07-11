33df58eb01a0f39bd9dcaa6c9c97a648d7f6097f
Amire80 <amir.aharoni@mail.huji.ac.il>
2017-07-11 16:09:41 +0300

    Remove compact language links dblist for simplicity (no-op)
    This retains the current situation, whilst making it simpler and
    more convenient to get it enabled by default on new Wikipedias.
    
    This change removes the compact-language-links dblist and all
    references to it. All of the current list consists of Wikipedias,
    and there are only two Wikipedias in which the feature is still
    in beta, so it's easier to list them in InitialiseSettings.php.
    
    Change-Id: Ifb241eb13a0af040c1ba8ed323b1c0709edcd509

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -13743,19 +13743,10 @@ $wgConf->settings = [
 ],
 
 'wmgULSCompactLanguageLinksBetaFeature' => [
-	'default' => true,
-	'testwiki' => false,
-	'test2wiki' => false,
-	'nonbetafeatures' => false,
-	'wikiversity' => false,
-	'wikivoyage' => false,
+	'default' => false,
 	'ruwikivoyage' => true,
-	'wikinews' => false,
-	'wikibooks' => false,
-	'wikiquote' => false,
-	'wiktionary' => false,
-	'incubatorwiki' => false,
-	'compact-language-links' => false,
+	'dewiki' => true,
+	'enwiki' => true,
 ],
 
 // Whether Compact Links is enabled for new accounts *by default*
