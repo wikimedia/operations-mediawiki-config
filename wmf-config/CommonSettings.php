142dedf44308a6d683174f99470ff82ffa025461
Reedy <reedy@wikimedia.org>
2017-07-19 01:28:14 +0100

    Remove rcs100[12] from $wgRCFeeds
    Stops a load of logspam
    
    Bug: T170157
    Change-Id: I4cb27b635290e06ec0526891c380bd34c46ee7ea

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -3481,19 +3481,6 @@ if ( $wmgUseRC2UDP ) {
 		'add_interwiki_prefix' => false,
 		'omit_bots' => false,
 	];
-
-	// RCStream / stream.wikimedia.org
-	if ( $wmfRealm === 'production' ) {
-		$wgRCFeeds['rcs1001'] = [
-			'uri'       => "redis://rcs1001.eqiad.wmnet:6379/rc.$wgDBname",
-			'formatter' => 'JSONRCFeedFormatter',
-		];
-
-		$wgRCFeeds['rcs1002'] = [
-			'uri'       => "redis://rcs1002.eqiad.wmnet:6379/rc.$wgDBname",
-			'formatter' => 'JSONRCFeedFormatter',
-		];
-	}
 }
 
 // Confirmed can do anything autoconfirmed can.
