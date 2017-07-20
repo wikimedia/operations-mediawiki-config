6308f4e8aa5012f128f32f3c69e14642f4b2527d
Phuedx <samsmith@wikimedia.org>
2017-07-20 11:38:48 +0000

    Revert "Revert "Stop RelatedArticles A/B test and clean up config""
    This reverts commit d4518868774791aafd5fcd4eddfa10cf8f764c6f.
    
    Bug: T169948
    Change-Id: I75142d594f7e4fe5c5c8ba4f05bee8a2f45b11f1

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -17474,16 +17474,8 @@ $wgConf->settings = [
 	'related-articles-footer-blacklisted-skins' => [],
 ],
 
-'wgRelatedArticlesEnabledSamplingRate' => [
-	'default' => 1,
-	// T167310
-	'enwiki' => 0.98,
-],
-
 'wgRelatedArticlesEnabledBucketSize' => [
 	'default' => 1,
-	// T167310
-	'enwiki' => 0.98,
 ],
 
 'wmgUseRelatedSites' => [
