0ef03170a9ca936eac68c2a4fb24d40cd483d6ae
Jaime Crespo <jcrespo@wikimedia.org>
2017-07-26 17:38:20 +0200

    mariadb: Depool db2069 for maintenance
    It will be rebooted and upgraded.
    
    Change-Id: Iab4b0454f0a54f8bdbcb93b9976733ce31018f24

--- wmf-config/db-codfw.php
+++ wmf-config/db-codfw.php
@@ -99,7 +99,7 @@ $wgLBFactoryConf = [
 		'db2048' => 400, # C6 2.9TB 160GB,
 		'db2055' => 50,  # D6 3.3TB 160GB, dump (inactive), vslow, api
 		'db2062' => 50,  # B5 3.3TB 160GB, api # mariadb 10.1
-		'db2069' => 50,  # D6 3.3TB 160GB, api
+		# 'db2069' => 50,  # D6 3.3TB 160GB, api
 		'db2070' => 400, # C5 3.3TB 160GB
 		'db2071' => 50,  # A6 3.6TB 512GB, api
 		'db2072' => 500, # B6 3.6TB 512GB
@@ -255,7 +255,7 @@ $wgLBFactoryConf = [
 		'api' => [
 			'db2055' => 1,
 			'db2062' => 1,
-			'db2069' => 1,
+			# 'db2069' => 1,
 			'db2071' => 5,
 		],
 	],
