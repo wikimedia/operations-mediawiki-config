d979e7fc605d71073b20df21f7868be26fc4c368
jdlrobson <jdlrobson@gmail.com>
2017-07-26 11:35:17 -0700

    Update several Wikipedia projects to existing wordmarks
    Bug: T171556
    Change-Id: I20711163d03c8bbb7bacca9ffafed79c13ddffad

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -174,7 +174,9 @@ if ( !$globals ) {
 	foreach ( [ 'private', 'fishbowl', 'special', 'closed', 'flow', 'flaggedrevs', 'small', 'medium',
 			'large', 'wikimania', 'wikidata', 'wikidataclient', 'visualeditor-nondefault',
 			'commonsuploads', 'nonbetafeatures', 'group0', 'group1', 'group2', 'wikipedia', 'nonglobal',
-			'wikitech', 'nonecho', 'mobilemainpagelegacy', 'compact-language-links', 'nowikidatadescriptiontaglines',
+			'wikitech', 'nonecho', 'mobilemainpagelegacy', 'compact-language-links',
+			'wikipedia-cyrillic', 'wikipedia-e-acute', 'wikipedia-devanagari',
+			'nowikidatadescriptiontaglines',
 			'related-articles-footer-blacklisted-skins',
 			'top6-wikipedia'
 		] as $tag ) {
