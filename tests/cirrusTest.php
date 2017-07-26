a5cf5528627f6bb52d7b9ab07995ca1e44842380
jdlrobson <jdlrobson@gmail.com>
2017-07-26 11:35:17 -0700

    Update several Wikipedia projects to existing wordmarks
    Bug: T171556
    Change-Id: I20711163d03c8bbb7bacca9ffafed79c13ddffad

--- tests/cirrusTest.php
+++ tests/cirrusTest.php
@@ -85,7 +85,9 @@ class cirrusTests extends WgConfTestCase {
 		foreach ( [ 'private', 'fishbowl', 'special', 'closed', 'flow', 'flaggedrevs', 'small', 'medium',
 				'large', 'wikimania', 'wikidata', 'wikidataclient', 'visualeditor-nondefault',
 				'commonsuploads', 'nonbetafeatures', 'group0', 'group1', 'group2', 'wikipedia', 'nonglobal',
-				'wikitech', 'nonecho', 'mobilemainpagelegacy', 'compact-language-links', 'nowikidatadescriptiontaglines',
+				'wikitech', 'nonecho', 'mobilemainpagelegacy', 'compact-language-links',
+				'wikipedia-cyrillic', 'wikipedia-e-acute', 'wikipedia-devanagari',
+				 'nowikidatadescriptiontaglines',
 				'top6-wikipedia'
 			] as $tag ) {
 			$dblist = MWWikiversions::readDbListFile( $tag );
