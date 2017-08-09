cdfe1c0f09fb22ddd2891a2560c0ac1f7111174e
Zach <terracodes@tools.wmflabs.org>
2017-08-09 01:38:38 -0800

    Update InitialiseSettings.php
    Remove confirmed from bureaucrat in wgAddGroups and wgRemoveGroups for
    per wiki, since its now default.
    
    Bug: T101983
    Change-Id: I83f391faf306439dc9cc8f3239953218a46bb05e

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -9846,11 +9846,11 @@ $wgConf->settings = [
 		'sysop' => [ 'curator' ], // T113109
 	],
 	'+eswiki' => [
-		'bureaucrat' => [ 'rollbacker', 'confirmed' ],
+		'bureaucrat' => [ 'rollbacker' ],
 		'sysop' => [ 'rollbacker', 'autopatrolled', 'patroller' ],
 	],
 	'+eswikibooks' => [
-		'bureaucrat' => [ 'flood', 'confirmed' ], // T93371
+		'bureaucrat' => [ 'flood' ], // T93371
 		'sysop' => [ 'rollbacker', 'patroller', 'autopatrolled' ], // T93371, T111455
 	],
 	'+eswikinews' => [
@@ -9863,7 +9863,6 @@ $wgConf->settings = [
 		'sysop' => [ 'autopatrolled' ], // T69557
 	],
 	'+eswikivoyage' => [
-		'bureaucrat' => [ 'confirmed', ], // T49325
 		'sysop' => [ 'rollbacker', 'patroller', 'autopatrolled', ], // T46285, T57665
 	],
 	'+eswiktionary' => [
@@ -10166,7 +10165,7 @@ $wgConf->settings = [
 		'bureaucrat' => [ 'inactive' ],
 	],
 	'+ptwiki' => [
-		'bureaucrat' => [ 'eliminator', 'confirmed', 'autoreviewer', 'interface-editor', 'accountcreator' ], // T41905, T65750
+		'bureaucrat' => [ 'eliminator', 'autoreviewer', 'interface-editor', 'accountcreator' ], // T41905, T65750
 		'sysop' => [ 'rollbacker', 'autoreviewer', 'confirmed', 'accountcreator' ], // T65750
 	],
 	'+quwiki' => [
@@ -10297,7 +10296,7 @@ $wgConf->settings = [
 	],
 	'+urwiki' => [
 		// T44737, T47643, T120348, T133564, T137888, T139302, T144701
-		'bureaucrat' => [ 'import', 'confirmed', 'abusefilter', 'rollbacker', 'massmessage-sender', 'interface-editor' ],
+		'bureaucrat' => [ 'import', 'abusefilter', 'rollbacker', 'massmessage-sender', 'interface-editor' ],
 		'sysop' => [ 'confirmed', 'abusefilter', 'rollbacker', 'accountcreator', 'filemover', 'autopatrolled', 'massmessage-sender' ],
 	],
 	'+viwiki' => [
@@ -10357,7 +10356,6 @@ $wgConf->settings = [
 		'bureaucrat' => [
 			'bot',
 			'bureaucrat',
-			'confirmed',
 			'contentadmin',
 			'oathauth',
 			'oauthadmin',
@@ -10589,11 +10587,11 @@ $wgConf->settings = [
 		'sysop' => [ 'abusefilter', 'autopatrolled', ],
 	],
 	'+eswiki' => [
-		'bureaucrat' => [ 'rollbacker', 'confirmed' ],
+		'bureaucrat' => [ 'rollbacker' ],
 		'sysop' => [ 'rollbacker', 'autopatrolled', 'patroller' ],
 	],
 	'+eswikibooks' => [
-		'bureaucrat' => [ 'flood', 'confirmed' ],
+		'bureaucrat' => [ 'flood' ],
 		'sysop' => [ 'rollbacker', 'patroller', 'autopatrolled' ], // T111455
 	],
 	'+eswikinews' => [
@@ -10606,7 +10604,6 @@ $wgConf->settings = [
 		'sysop' => [ 'autopatrolled' ], // T69557
 	],
 	'+eswikivoyage' => [
-		'bureaucrat' => [ 'confirmed', ], // T49325
 		'sysop' => [ 'rollbacker', 'patroller', 'autopatrolled', ], // T46285, T57665
 	],
 	'+eswiktionary' => [
@@ -10911,7 +10908,7 @@ $wgConf->settings = [
 		'bureaucrat' => [ 'interface-editor' ], // T133472
 	],
 	'+ptwiki' => [
-		'bureaucrat' => [ 'eliminator', 'confirmed', 'autoreviewer', 'interface-editor', 'accountcreator', 'bureaucrat', 'sysop' ], // T41905, T65750, T107661
+		'bureaucrat' => [ 'eliminator', 'autoreviewer', 'interface-editor', 'accountcreator', 'bureaucrat', 'sysop' ], // T41905, T65750, T107661
 		'sysop' => [ 'rollbacker', 'autoreviewer', 'confirmed', 'accountcreator' ], // T65750
 	],
 	'+ptwikinews' => [
@@ -11054,7 +11051,7 @@ $wgConf->settings = [
 	],
 	'+urwiki' => [
 		// T44737, T47643, T120348, T133564, T137888, T139302, T144701
-		'bureaucrat' => [ 'import', 'confirmed', 'abusefilter', 'rollbacker', 'massmessage-sender', 'interface-editor' ],
+		'bureaucrat' => [ 'import', 'abusefilter', 'rollbacker', 'massmessage-sender', 'interface-editor' ],
 		'sysop' => [ 'confirmed', 'accountcreator', 'filemover', 'autopatrolled', 'massmessage-sender' ],
 	],
 	'+viwiki' => [
@@ -11118,7 +11115,6 @@ $wgConf->settings = [
 		],
 		'bureaucrat' => [
 			'bot',
-			'confirmed',
 			'contentadmin',
 			'flood',
 			'oathauth',
