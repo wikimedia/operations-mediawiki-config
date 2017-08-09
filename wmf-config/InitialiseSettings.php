3be03972d9b10d382234887382f7c988d3a2ac4e
MarcoAurelio <maurelio@tools.wmflabs.org>
2017-08-09 08:04:30 +0000

    Follow-up Ia62ad0f4
    On Ia62ad0f4 we forgot to grant the ability to remove the permissions
    to the 'bureaucrat' group as well. This patch fixes that.
    
    Bug: T101983
    Change-Id: Iae4435fd9d60b44bd6c20365c359c274613c12f8

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -10400,7 +10400,7 @@ $wgConf->settings = [
 	// The '+' in front of the DB name means 'add to the default'. It saves us duplicating
 	// changes to the default across all overrides --Andrew 2009-04-28
 	'default' => [
-		'bureaucrat' => [ 'bot' ],
+		'bureaucrat' => [ 'bot', 'confirmed' ],
 		'sysop' => [ 'ipblock-exempt' ],
 	],
 	'+testwiki' => [
