04f26fcbebf2e8feebcc96f8112612b40b5a1bf4
Catrope <roan@wikimedia.org>
2017-08-17 20:39:28 +0000

    Reapply "Remove temporary wgStructuredChangeFiltersEnableExperimentalViews setting"
    This reverts commit 21ed25e584024e8541047719d8a5bf5d61bb2914
    and reapplies commit 590fac6d6a734c76e9a7da16eb1a0d0cd5237c96.
    
    Change-Id: I7f43a8c473f8d6c52ec078cebae715f3f331578e

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -19406,11 +19406,6 @@ $wgConf->settings = [
 	'commonswiki' => [ NS_FILE ],
 ],
 
-// Temporarily enable until 1.30.0-wmf.12 rolls out -RK
-'wgStructuredChangeFiltersEnableExperimentalViews' => [
-	'default' => true,
-],
-
 'wmgUseTimeless' => [
 	'default' => false,
 	'testwiki' => true,
