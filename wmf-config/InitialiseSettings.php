3653a8d0475906d09b3da48e7957224c7f4a5e17
Niharika29 <nkohli@wikimedia.org>
2017-08-22 20:38:49 +0000

    Redo "Enable CodeMirror everywhere but RTL wikis"
    This reverts commit ed93eb4587c992aae8fe91d5a9e091ae46a06fcf.
    
    Change-Id: Ib9820dca2887f00d9f110074f9b84204884dfdc9

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -1724,7 +1724,7 @@ $wgConf->settings = [
 	'aswiki' => '/wiki/ৱিকিপিডিয়া:ফাইল_আপল’ড_বিশেষজ্ঞ',
 	'azbwiki' => '//commons.wikimedia.org/wiki/Special:UploadWizard?uselang=azb', // T106305
 	'bgwiki' => '/wiki/MediaWiki:Uploadtext', // T16836
-	'bnwiki' => '/wiki/উইকিপিডিয়া:আপলোড',
+	'bnwiki' => '/wiki/উইকিপিডিয়া:আপলোড',
 	'bswiki' => '/wiki/Wikipedia:Upload', // T14044
 	'dawiki' => '//commons.wikimedia.org/wiki/Special:UploadWizard?uselang=da&campaign=dk', // T39662
 	'dewikisource' => '//commons.wikimedia.org/wiki/Special:UploadWizard?uselang=de',
@@ -19373,9 +19373,8 @@ $wgConf->settings = [
 ],
 
 'wmgUseCodeMirror' => [
-	'default' => false,
-	'simplewiki' => true,
-	'testwiki' => true, // T169284
+	'default' => true,
+	'rtl' => false,
 ],
 
 'wmgUseParserMigration' => [
