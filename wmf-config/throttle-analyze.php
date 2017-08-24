89ea7455bca1e9b2a69a4c83c226d79ddd6b2465
Martin Urbanec <martin.urbanec@wikimedia.cz>
2017-08-24 23:27:36 +0200

    throttle.php: Separate the throttling definitions from the exception values itself
    Bug: T167040
    Change-Id: If97fef2b72401e50960fdecd44b95ce933635e4d

---
+++ wmf-config/throttle-analyze.php
@@ -0,0 +1,47 @@
+/**
+ * Helper to easily add a throttling request.
+ */
+$wgExtensionFunctions[] = function () {
+	global $wmgThrottlingExceptions, $wgDBname, $wgRequest;
+
+	foreach ( $wmgThrottlingExceptions as $options ) {
+		# Validate entry, skip when it does not apply to our case
+
+		# 1) skip when it does not apply to our database name
+
+		if ( isset( $options['dbname'] ) && !in_array( $wgDBname, (array)$options['dbname'] ) ) {
+			continue;
+		}
+
+		# 2) skip expired entries
+		$inTimeWindow = time() >= strtotime( $options['from'] )
+			&& time() <= strtotime( $options['to'] );
+
+		if ( !$inTimeWindow ) {
+			continue;
+		}
+
+		# 3) skip when throttle does not apply to the client IP
+		$ip = $wgRequest->getIP();
+		if ( isset ( $options['IP'] ) ) {
+			$throttleIP = $options['IP'];
+		}
+		if ( isset( $throttleIP ) && !in_array( $ip, (array)$throttleIP ) ) {
+			continue;
+		}
+		if ( isset ( $options['range'] ) && !IP::isInRanges( $ip, (array)$options['range'] ) ) {
+			continue;
+		}
+
+		# Finally) set up the throttle value
+		global $wgAccountCreationThrottle, $wgRateLimits;
+		if ( isset( $options['value'] ) && is_numeric( $options['value'] ) ) {
+			$wgAccountCreationThrottle = $options['value'];
+		} else {
+			$wgAccountCreationThrottle = 50; // Provide some sane default
+		}
+		$wgRateLimits['badcaptcha']['ip'] = [ 1000, 86400 ];
+		$wgRateLimits['badcaptcha']['newbie'] = [ 1000, 86400 ];
+		return; # No point in proceeding to another entry
+	}
+};
