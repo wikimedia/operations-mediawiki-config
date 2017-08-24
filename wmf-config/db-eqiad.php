eab8bcc6bca61268bdf1ad192b1d7bd38c438144
Manuel Arostegui <marostegui@wikimedia.org>
2017-08-24 15:59:46 +0200

    db-eqiad.php: Add db1096 to s5
    db1096 is going to be added to s5 as a rc slave to replace db1026, so
    let's add it commented out for now until the transfer is done
    
    Bug: T172679
    Change-Id: I7c1bda2d751a71e3b5dd288708eded89de7f2890

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -144,6 +144,7 @@ $wgLBFactoryConf = [
 		'db1082' => 300, # A2 3.6TB 512GB, api
 		'db1087' => 500, # C2 3.6TB 512GB
 		'db1092' => 500, # D2 3.6TB 512GB
+		# 'db1096' => 1,   # A6 3.6TB  512GB, watchlist, recentchanges, contributions, logpager # T172679
 	],
 	's6' => [
 		'db1061' => 0,   # C3 2.8TB 128GB, master
@@ -367,22 +368,27 @@ $wgLBFactoryConf = [
 		'watchlist' => [
 			# 'db1026' => 1,
 			'db1045' => 1,
+			# 'db1096' => 1,
 		],
 		'recentchanges' => [
 			# 'db1026' => 1,
 			'db1045' => 1,
+			# 'db1096' => 1,
 		],
 		'recentchangeslinked' => [
 			# 'db1026' => 1,
 			'db1045' => 1,
+			# 'db1096' => 1,
 		],
 		'contributions' => [
 			# 'db1026' => 1,
 			'db1045' => 1,
+			# 'db1096' => 1,
 		],
 		'logpager' => [
 			# 'db1026' => 1,
 			'db1045' => 1,
+			# 'db1096' => 1,
 		],
 	],
 	's6' => [
