56cd756e95aa37b4f28d242ab06a1422a010de81
Tim Starling <tstarling@wikimedia.org>
2017-09-01 13:12:33 +1000

    Re-enable EtcdConfig in beta cluster
    Use a single EtcdConfig object, with slashes in key names
    
    This reverts commit b729a21a710dfb82befc364cfaa2ee3b79a398af.
    
    Depends-On: Ica0914e61baba9c0462481925be15d79b66dc342
    Change-Id: I36d43b534c6c872566fa2a6aa06508b7d6a0b077

---
+++ wmf-config/etcd.php
@@ -0,0 +1,36 @@
+<?php
+# WARNING: This file is publically viewable on the web. Do not put private data here.
+#
+# Included very early by CommonSettings.php
+# Only two sets of globals available here:
+# - $wmfRealm, $wmfDatacenter (from multiversion/MWRealm)
+# - $wmfLocalServices (from wmf-config/*Services.php)
+
+function wmfSetupEtcd() {
+	global $wmfRealm, $wmfDatacenter, $wmfLocalServices, $wgReadOnly, $wmfMasterDatacenter;
+
+	# Create a local cache
+	if ( PHP_SAPI === 'cli' ) {
+		$localCache = new HashBagOStuff;
+	} else {
+		$localCache = new APCBagOStuff;
+	}
+
+	# Set up local EtcdConfig with fallback to common EtcdConfig, this allows
+	# us to set $wgReadOnly differently in each datacenter
+	$etcdConfig = new EtcdConfig( [
+		'host' => $wmfLocalServices['etcd'],
+		'protocol' => 'https',
+		'directory' => "conftool/v1/mediawiki-config",
+		'cache' => $localCache,
+	] );
+
+	# Read only mode
+	$wgReadOnly = $etcdConfig->get( "$wmfDatacenter/ReadOnly" );
+
+	# Master datacenter
+	# The datacenter from which we serve traffic.
+	$wmfMasterDatacenter = $etcdConfig->get( 'common/WMFMasterDatacenter' );
+}
+
+wmfSetupEtcd();
