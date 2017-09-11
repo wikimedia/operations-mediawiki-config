e78433d6dfc0ba5e75b82bac155ddc8d75fddc5e
Sébastien Santoro <dereckson@espace-win.org>
2017-09-11 22:06:49 +0000

    Enable Special:PageLanguage on mul.wikisource
    Bug: T175622
    Change-Id: Ief7b02a5b2237f01ddd6fb5f0b849fad8dadd85a

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -11979,6 +11979,7 @@ $wgConf->settings = [
 
 'wgPageLanguageUseDB' => [
 	'default' => false,
+	'wikisource' => true, // T175622
 	// overridden to true in CommonSettings for wikis using the Translate extension
 ],
 
