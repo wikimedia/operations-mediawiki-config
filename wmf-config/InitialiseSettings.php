e61f623e2732fe3b0a88a4e9aa19a08bd41d6592
Roan Kattouw <roan.kattouw@gmail.com>
2017-09-15 10:08:21 -0700

    Enable structured change filters by default on cawiki, frwiki and hewiki
    Bug: T157642
    Change-Id: I317b3efe3846d9df7376323b93f896848da7926a

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -18988,6 +18988,12 @@ $wgConf->settings = [
 'wgStructuredChangeFiltersOnWatchlist' => [
 	'default' => true,
 ],
+'wgStructuredChangeFiltersShowPreference' => [
+	'default' => false,
+	'cawiki' => true,
+	'frwiki' => true,
+	'hewiki' => true,
+],
 
 ### End (roughly) of general extensions ########################
 
