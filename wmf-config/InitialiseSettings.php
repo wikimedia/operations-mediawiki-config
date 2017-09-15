821f846eed88b673c3e25763a313374b9f1c64ed
Roan Kattouw <roan.kattouw@gmail.com>
2017-09-15 10:06:55 -0700

    Enable $wgStructuredChangeFiltersOnWatchlist on all wikis
    Bug: T164234
    Change-Id: I5224476faa4563ba295d7f981fcab525e01a927b

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -18985,7 +18985,7 @@ $wgConf->settings = [
 	'default' => true,
 ],
 'wgStructuredChangeFiltersOnWatchlist' => [
-	'default' => false,
+	'default' => true,
 ],
 
 ### End (roughly) of general extensions ########################
