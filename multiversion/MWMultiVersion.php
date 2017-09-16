d1f31b52fb1e6ff8cf593bad1c9fc77a34b76cd7
Amir Sarabadani <ladsgroup@gmail.com>
2017-09-16 19:58:13 +0400

    Add config for amwikimedia
    The logos are from:
    https://commons.wikimedia.org/wiki/File:Wikimedia_Armenia_logo.svg
    
    Bug: T176042
    Change-Id: Iaec8e247248ebff8bcbe6d9e6ee71c37550cce45

--- multiversion/MWMultiVersion.php
+++ multiversion/MWMultiVersion.php
@@ -185,8 +185,9 @@ class MWMultiVersion {
 				|| ( $matches[2] === 'wikimedia' && in_array(
 					$lang,
 					[
-						'ar', 'bd', 'be', 'br', 'ca', 'cn', 'co', 'dk', 'ec', 'et', 'fi', 'il', 'mai', 'mk', 'mx', 'nl',
-						'noboard-chapters', 'no', 'nyc', 'nz', 'pa-us', 'pl', 'pt', 'rs', 'ru', 'se', 'tr', 'ua', 'uk', 've', 'wb'
+						'am', 'ar', 'bd', 'be', 'br', 'ca', 'cn', 'co', 'dk', 'ec', 'et', 'fi', 'il', 'mai', 'mk', 'mx',
+						'nl', 'noboard-chapters', 'no', 'nyc', 'nz', 'pa-us', 'pl', 'pt', 'rs', 'ru', 'se', 'tr', 'ua',
+						'uk', 've', 'wb'
 					]
 			) ) ) {
 				// wikimedia (non chapters) sites stay as wiki
