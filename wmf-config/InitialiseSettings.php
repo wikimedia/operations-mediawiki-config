84a1e7dbe65d91c3ce4ee9cac080da2df9e001f4
Petr Pchelko <ppchelko@wikimedia.org>
2017-09-18 15:34:38 +0200

    [Logging config] Enable logging for updateBetaFeaturesUserCounts
    See also: I9d0345292a63e5516aae727de7c047117faa5ae0
    
    Change-Id: Ieb8bbc94fca9d1f8a33af45b0e11a1d79b7ec78c

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -5211,6 +5211,7 @@ $wgConf->settings = [
 		'torblock' => 'debug',
 		'ts_badpass' => 'debug',
 		'UpdateRepo' => 'debug',
+		'updateBetaFeaturesUserCounts' => 'info', // T175637
 		'updateTranstagOnNullRevisions' => 'debug',
 		'upload' => 'debug',
 		'wfLogDBError' => 'debug', // Former $wgDBerrorLog
