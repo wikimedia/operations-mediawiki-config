0973749279245755b3994c24bda473ee74e94fe8
MarcoAurelio <maurelio@tools.wmflabs.org>
2017-09-18 14:59:25 +0200

    Create a 'patroller' user group at Meta-Wiki
    - Added rights: autopatrol, patrol, patrolmarks and rollback.
    - Granted and removed by administrators
    
    Bug: T176079
    Change-Id: Iac8ec9e0f535b95d4be3f14b2e871dc48b3a425a

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -8907,6 +8907,12 @@ $wgConf->settings = [
 		'oauthadmin' => [ // T142123
 			'autopatrol' => true,
 		],
+		'patroller' => [ // T176079
+			'autopatrol' => true,
+			'patrol' => true,
+			'patrolmarks' => true,
+			'rollback' => true,
+		],
 		'steward' => [
 			'centralauth-rename' => true,
 			'userrights-interwiki' => true,
@@ -10116,7 +10122,7 @@ $wgConf->settings = [
 	],
 	'+metawiki' => [
 		'bureaucrat' => [ 'centralnoticeadmin', 'flood', 'uploader', 'translationadmin' ], // T48639, T110674
-		'sysop' => [ 'autopatrolled', 'massmessage-sender' ], // T59611
+		'sysop' => [ 'autopatrolled', 'massmessage-sender', 'patroller' ], // T59611, T176079
 	],
 	'+mkwiki' => [
 		'bureaucrat' => [ 'patroller', 'autopatrolled', 'autoreviewed' ],
@@ -10871,7 +10877,7 @@ $wgConf->settings = [
 	],
 	'+metawiki' => [
 		'bureaucrat' => [ 'flood', 'translationadmin', 'centralnoticeadmin', 'uploader' ], // T39198, T52287, T110674
-		'sysop' => [ 'autopatrolled', 'massmessage-sender' ], // T59611
+		'sysop' => [ 'autopatrolled', 'massmessage-sender', 'patroller' ], // T59611, T176079
 	],
 	'+mkwiki' => [
 		'bureaucrat' => [ 'patroller', 'autopatrolled', 'autoreviewed' ],
