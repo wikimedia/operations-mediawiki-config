3dfd2fe65419be59b191f90473c55b024065a63c
David Causse <dcausse@wikimedia.org>
2017-09-19 10:45:26 +0200

    Switch elasticsearch active cluster to codfw
    Needed to run a hard restart on eqiad once codfw is done.
    
    Change-Id: I13a5e247d531f3c8e5a7df80c4ca7bf05d905ff0

--- tests/cirrusTest.php
+++ tests/cirrusTest.php
@@ -10,7 +10,7 @@ class cirrusTests extends WgConfTestCase {
 		$this->assertArrayNotHasKey( 'wgCirrusSearchServers', $config );
 		$this->assertArrayHasKey( 'wgCirrusSearchClusters', $config );
 		$this->assertArrayHasKey( 'wgCirrusSearchDefaultCluster', $config );
-		$this->assertEquals( 'unittest', $config['wgCirrusSearchDefaultCluster'] );
+		$this->assertEquals( 'codfw', $config['wgCirrusSearchDefaultCluster'] );
 		$this->assertCount( 2, $config['wgCirrusSearchClusters'] );
 
 		// testwiki writes to eqiad and codfw
