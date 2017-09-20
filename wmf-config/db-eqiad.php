826c268edf14f3718a4875974a9dab8a60d3cb47
Jaime Crespo <jcrespo@wikimedia.org>
2017-09-20 13:27:24 +0200

    mariadb: Depool db1101 for maintenance
    We are going to repartition it to replace db1036.
    
    Bug: T176311
    Change-Id: I394ebfc65591d1a57ab4769649e10a81b65cf44a

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -121,7 +121,7 @@ $wgLBFactoryConf = [
 		'db1074' => 300, # A2 3.6TB 512GB, api
 		'db1076' => 500, # B1 3.6TB 512GB
 		'db1090' => 500, # C3 3.6TB 512GB
-		'db1101' => 0,   # C2 3.6TB 512GB, old master 2
+		# 'db1101' => 0,   # C2 3.6TB 512GB, old master 2, depool for maintenance
 	],
 	/* s3 */ 'DEFAULT' => [
 		'db1075' => 0,   # A2 3.6TB 512GB, master
