1961d6d3dd867052ecd6b885dd40f9f303cd48ae
Zoranzoki21 <dori.gord@orion.rs>
2017-09-30 20:23:02 +0000

    Enable Extension:Newsletter on hewiki
    Bug: T177151
    Change-Id: Ib634fd2f3a550233eea95b892c437552444e2a3c

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -20589,6 +20589,7 @@ $wgConf->settings = [
 	'testwiki' => true, // T110170
 	'mediawikiwiki' => true, // T110170
 	'officewiki' => true, // T176199
+	'hewiki' => true, // T177151
 ],
 
 'wmgUseLoginNotify' => [
