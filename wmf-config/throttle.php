83983782596f9b34550b48d6a76671e75e74203b
Zoranzoki21 <dori.gord@orion.rs>
2017-10-01 15:35:43 +0000

    Removing expired rules
    Change-Id: I0b0b03efc1a4f01c38392ffd3228820b2a427d32

--- wmf-config/throttle.php
+++ wmf-config/throttle.php
@@ -28,29 +28,4 @@ $wmgThrottlingExceptions = [];
 # ];
 ## Add throttling definitions below.
 
-$wmgThrottlingExceptions[] = [ // T175113
-	'from' => '2017-09-05T0:00 +2:00',
-	'to' => '2017-09-30T0:00 +2:00',
-	'IP' => '93.91.145.154',
-	'dbname' => [ 'cswiki', 'cswikiversity' ],
-	'value' => 70, // 50 expected
-];
-
-$wmgThrottlingExceptions[] = [
-	'from' => '2017-09-29T12:00 UTC',
-	'to' => '2017-09-30T02:00 UTC',
-	'range' => [ '186.67.125.0/24', '163.247.67.20/30', '163.247.67.24/29', '163.247.67.32/28', '163.247.67.48/29', '163.247.67.56/30', '163.247.67.60/31', '163.247.67.62/32' ],
-	'dbname' => [ 'eswiki', 'enwiki', 'commonswiki', 'wikidatawiki' ],
-	'value' => 120 // 100 expected
-];
-
-$wmgThrottlingExceptions[] = [ // T176287 - John Michael Kohler Art Center
-	'from'   => '2017-09-21T13:00-05:00', // Central
-	'to'     => '2017-09-26T22:00-05:00',
-	'IP'  => [
-		'192.168.50.52',
-	],
-	'dbname' => [ 'enwiki', 'commonswiki' ],
-	'value'  => 100, // 100 expected participants (not specified)
-];
 ## Add throttling definitions above.
