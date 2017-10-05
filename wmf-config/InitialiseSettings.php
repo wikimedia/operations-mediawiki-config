5e2138c36e54db272d0cf020e34375a2dc833f1c
Roan Kattouw <roan.kattouw@gmail.com>
2017-10-04 17:41:34 -0700

    Enable structured change filters by default on all remaining wikis
    It was still disabled on wikis with FlaggedRevs protection.
    
    Bug: T177444
    Change-Id: I8d675e06cfdac1289c9b52e0396d820e2bb8db95

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -19153,7 +19153,6 @@ $wgConf->settings = [
 ],
 'wgStructuredChangeFiltersShowPreference' => [
 	'default' => true,
-	// Disabled on FlaggedRevs wikis with $wgFlaggedRevsProtection set to false in flaggedrevs.php (T177445)
 ],
 
 ### End (roughly) of general extensions ########################
