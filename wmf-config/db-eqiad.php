b91218756ac32df3ca8a13a5906a9a18b28ee4aa
Manuel Arostegui <marostegui@wikimedia.org>
2017-10-11 11:20:45 +0200

    db-eqiad.php: Depool db1087
    Going to optimize pagelinks and templatelinks tables
    
    Bug: T174509
    Change-Id: I07ee3cee7b509fca19c1c0ec8d48c90a6e09736d

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -145,14 +145,14 @@ $wgLBFactoryConf = [
 		'db1070' => 1,   # D1 2.8TB 160GB, vslow, dump #ROW based replication - master for db1095
 		'db1071' => 50,  # D1 2.8TB 160GB, api
 		'db1082' => 300, # A2 3.6TB 512GB, api
-		'db1087' => 500, # C2 3.6TB 512GB
+		# 'db1087' => 500, # C2 3.6TB 512GB #T174509
 		'db1092' => 500, # D2 3.6TB 512GB
 		'db1096' => 1,   # A6 3.6TB 512GB, watchlist, recentchanges, contributions, logpager
 		'db1099' => 1,   # B2 3.6TB 512GB, watchlist, recentchanges, contributions, logpager
 		# 'db1100' => 1,   # C2 3.6TB 512GB, old master, crashed
 		# 'db1104' => 100,  # B3 3.6TB 512GB # Cloning db1106
-		'db1105' => 100,  # C3 3.6TB 512GB #low weight after being provisioned
-		'db1106' => 100,  # D3 3.6TB 512GB #low weight after being provisioned
+		'db1105' => 300,  # C3 3.6TB 512GB #low weight after being provisioned
+		'db1106' => 300,  # D3 3.6TB 512GB #low weight after being provisioned
 	],
 	's6' => [
 		'db1061' => 0,   # C3 2.8TB 128GB, master
