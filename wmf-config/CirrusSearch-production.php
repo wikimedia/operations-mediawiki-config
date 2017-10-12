50d60cb4eb8621ec4fa12ce50e39af59179befac
zoranzoki21 <zorandori4444@gmail.com>
2017-10-12 23:58:38 +0200

    Rename $wmf* to $wmg* in wmf-config
    Bug: T45956
    Change-Id: Ia3a1cbdeae33a3c433be36537751145cafa4153f

--- wmf-config/CirrusSearch-production.php
+++ wmf-config/CirrusSearch-production.php
@@ -11,8 +11,8 @@
 $wgCirrusSearchMasterTimeout = '2m';
 
 $wgCirrusSearchClusters = [
-	'eqiad' => $wmfAllServices['eqiad']['search'],
-	'codfw' => $wmfAllServices['codfw']['search'],
+	'eqiad' => $wmgAllServices['eqiad']['search'],
+	'codfw' => $wmgAllServices['codfw']['search'],
 ];
 if ( defined( 'HHVM_VERSION' ) ) {
 	$wgCirrusSearchClusters['eqiad'] = array_map( function ( $host ) {
@@ -39,8 +39,8 @@ if ( defined( 'HHVM_VERSION' ) ) {
 
 if ( $wgDBname === 'labswiki' || $wgDBname === 'labtestwiki' ) {
 	$wgCirrusSearchClusters = [
-		'eqiad' => $wmfAllServices['eqiad']['search'],
-		'codfw' => $wmfAllServices['codfw']['search'],
+		'eqiad' => $wmgAllServices['eqiad']['search'],
+		'codfw' => $wmgAllServices['codfw']['search'],
 	];
 }
 
@@ -48,10 +48,10 @@ $wgCirrusSearchConnectionAttempts = 3;
 
 $wgCirrusSearchBackup['backups'] = [
 	'type' => 'swift',
-	'swift_url' => $wmfSwiftEqiadConfig['cirrusAuthUrl'],
+	'swift_url' => $wmgSwiftEqiadConfig['cirrusAuthUrl'],
 	'swift_container' => 'global-data-elastic-backups',
-	'swift_username' => $wmfSwiftEqiadConfig['cirrusUser'],
-	'swift_password' => $wmfSwiftEqiadConfig['cirrusKey'],
+	'swift_username' => $wmgSwiftEqiadConfig['cirrusUser'],
+	'swift_password' => $wmgSwiftEqiadConfig['cirrusKey'],
 	'max_snapshot_bytes_per_sec' => '10mb',
 	'compress' => false,
 	'chunk_size' => '1g',
@@ -81,8 +81,8 @@ if ( ! isset( $wmgCirrusSearchReplicas['eqiad'] ) ) {
 
 // 5 second timeout for local cluster, 10 seconds for remote.
 $wgCirrusSearchClientSideConnectTimeout = [
-	'eqiad' => $wmfDatacenter === 'eqiad' ? 5 : 10,
-	'codfw' => $wmfDatacenter === 'codfw' ? 5 : 10,
+	'eqiad' => $wmgDatacenter === 'eqiad' ? 5 : 10,
+	'codfw' => $wmgDatacenter === 'codfw' ? 5 : 10,
 ];
 
 $wgCirrusSearchDropDelayedJobsAfter = [
