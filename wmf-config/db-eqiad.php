3cb562e5e931530d5e64e75dc90e58caff6dc701
Manuel Arostegui <marostegui@wikimedia.org>
2017-10-30 10:52:14 +0100

    db-eqiad.php: Restore db1097 original weight
    Change-Id: Idabdd8ebb6c8418d704fa237c8d1a9aa5cae1ae1

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -134,10 +134,10 @@ $wgLBFactoryConf = [
 		'db1053' => 1,   # A2 2.8TB  96GB, watchlist, recentchanges, contributions, logpager
 		'db1056' => 1,   # C2 2.8TB  96GB, watchlist, recentchanges, contributions, logpager
 		'db1064' => 0,   # D1 2.8TB 160GB, vslow, dump #Master for db1095 - new sanitarium
-		'db1081' => 200, # A2 3.6TB 512GB, api #low weight T161088
+		'db1081' => 300, # A2 3.6TB 512GB, api
 		'db1084' => 500, # B1 3.6TB 512GB
 		'db1091' => 500, # D2 3.6TB 512GB
-		'db1097' => 1,   # D1 3.6TB 512GB, api, old master #T161088 #low weight after maintenance
+		'db1097' => 1,   # D1 3.6TB 512GB, api, old master
 		# 'db1103' => 0,  # A3 3.6TB 512GB, #temporary on s4 to help rebuilding db1084 and db1091
 	],
 	's5' => [
@@ -336,7 +336,7 @@ $wgLBFactoryConf = [
 		],
 		'api' => [
 			'db1081' => 1,
-			'db1097' => 2,
+			'db1097' => 3,
 		],
 		'watchlist' => [
 			'db1053' => 1,
