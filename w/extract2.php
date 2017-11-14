6b1bb539c9ea19979637472e655a1e23eda90a94
Chad Horohoe <chadh@wikimedia.org>
2017-11-14 15:00:55 -0800

    extract2.php: Stop allowing the www portal templates
    None of them actually load from this file anymore. Its sole remaining
    use is for the API listing endpoint.
    
    Also kill $wgArticle while I'm here
    
    Change-Id: Ic4194b7625d2b375ee9286c94e473a3b9467985d

--- w/extract2.php
+++ w/extract2.php
@@ -5,28 +5,8 @@ putenv( "MW_LANG={$lang}" ); // notify MWMultiVersion
 require_once __DIR__ . '/../multiversion/MWMultiVersion.php';
 require MWMultiVersion::getMediaWiki( 'includes/WebStart.php' );
 
-$allowed_templates = [
-	'Www.wikimedia.org_template',
-	'Www.wikipedia.org_template',
-	'Www.wikinews.org_template',
-	'Www.wiktionary.org_template',
-	'Www.wikiquote.org_template',
-	'Www.wikiversity.org_template',
-	'Www.wikibooks.org_template',
-	'Www.wikivoyage.org_template',
-	'API_listing_template',
-];
-
-$template = $wgRequest->getText( 'template', 'Www.wikipedia.org_template' );
-if ( !in_array( $template, $allowed_templates ) ) {
-	header( 'Content-Type: text/plain; charset=utf-8' );
-	echo 'Invalid parameters...';
-	exit;
-}
-
-$wgTitle = Title::newFromText( $template );
-$wgArticle = new Article( $wgTitle );
-$rawHtml = $wgArticle->getPage()->getContent()->getNativeData();
+$wgTitle = Title::newFromText( 'API_listing_template' );
+$rawHtml = (new Article( $wgTitle ))->getPage()->getContent()->getNativeData();
 
 $lastmod = gmdate( 'D, j M Y H:i:s', wfTimestamp( TS_UNIX, $wgArticle->getTouched() ) ) . ' GMT';
 header( 'Content-Type: text/html; charset=utf-8' );
