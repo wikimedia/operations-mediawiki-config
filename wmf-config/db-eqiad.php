bfc9bf54469b32a7cbd34cfcb5bcb57affc13319
Jaime Crespo <jcrespo@wikimedia.org>
2017-11-14 13:34:07 +0100

    WIP: setup s8 and db-common.php refactoring
    Setup the new replica set, s8, to contain wikidata only. s5 will
    stay with dewiki only for now.
    
    Add a db-common.php to avoid defining the same things, that are
    unlikely to change between datacenters, twice. I have no idea of
    mediawiki code style (global variables, etc.) so please critizise
    it. What makes no sense is to duplicate the same definitions many
    times.
    
    Bug: T177208
    Change-Id: Icd52d164dade38f248c3526fd44a6c243151bef1

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -1,17 +1,7 @@
 <?php
 # WARNING: This file is publically viewable on the web. Do not put private data here.
 
-if ( !defined( 'DBO_DEFAULT' ) ) {
-	define( 'DBO_DEFAULT', 16 );
-}
-
-# $wgReadOnly = "Wikimedia Sites are currently read-only during maintenance, please try again soon.";
-
-$wmgParserCacheDBs = [
-	'10.64.0.12'   => '10.64.0.12',   # pc1004, A3 2.4TB 256GB
-	'10.64.32.72'  => '10.64.32.72',  # pc1005, C7 2.4TB 256GB
-	'10.64.48.128' => '10.64.48.128', # pc1006, D3 2.4TB 256GB
-];
+require('db-common.php');
 
 $wmgOldExtTemplate = [
 	'10.64.0.7'    => 1, # es1012, A2 11TB 128GB
@@ -23,62 +13,7 @@ $wgLBFactoryConf = [
 
 'class' => 'LBFactoryMulti',
 
-'sectionsByDB' => [
-	# s1: enwiki
-	'enwiki'       => 's1',
-
-	# s2: large wikis
-	'bgwiki'       => 's2',
-	'bgwiktionary' => 's2',
-	'cswiki'       => 's2',
-	'enwikiquote'  => 's2',
-	'enwiktionary' => 's2',
-	'eowiki'       => 's2',
-	'fiwiki'       => 's2',
-	'idwiki'       => 's2',
-	'itwiki'       => 's2',
-	'nlwiki'       => 's2',
-	'nowiki'       => 's2',
-	'plwiki'       => 's2',
-	'ptwiki'       => 's2',
-	'svwiki'       => 's2',
-	'thwiki'       => 's2',
-	'trwiki'       => 's2',
-	'zhwiki'       => 's2',
-
-	# s3 (default)
-
-	# s4: commons
-	'commonswiki'  => 's4',
-
-	# s5: dewiki and wikidata
-	'dewiki'       => 's5',
-	'wikidatawiki' => 's5',
-
-	# s6: large wikis
-	'frwiki'       => 's6',
-	'jawiki'       => 's6',
-	'ruwiki'       => 's6',
-
-	# s7: large wikis, centralauth
-	'eswiki'       => 's7',
-	'huwiki'       => 's7',
-	'hewiki'       => 's7',
-	'ukwiki'       => 's7',
-	'frwiktionary' => 's7',
-	'metawiki'     => 's7',
-	'arwiki'       => 's7',
-	'centralauth'  => 's7',
-	'cawiki'       => 's7',
-	'viwiki'       => 's7',
-	'fawiki'       => 's7',
-	'rowiki'       => 's7',
-	'kowiki'       => 's7',
-
-	# labs-related wikis
-	'labswiki'     => 'silver',
-	'labtestwiki'  => 'labtestweb2001',
-],
+'sectionsByDB' => $sectionsByDB,
 
 # Load lists
 #
@@ -143,17 +78,12 @@ $wgLBFactoryConf = [
 		'db1103:3314' => 1,  # A3 3.6TB 512GB # rc, log: s2 and s4
 	],
 	's5' => [
-		'db1063' => 0,   # C5 2.8TB 128GB, master
-		'db1070' => 1,   # D1 2.8TB 160GB, vslow, dump #ROW based replication - master for db1095
-		'db1071' => 1,   # D1 2.8TB 160GB, api
+		'db1070' => 1,   # D1 2.8TB 160GB, master
 		'db1082' => 300, # A2 3.6TB 512GB, api
 		'db1087' => 500, # C2 3.6TB 512GB
-		'db1092' => 500, # D2 3.6TB 512GB
 		'db1096' => 1,   # A6 3.6TB 512GB, watchlist, recentchanges, contributions, logpager
 		'db1099' => 1,   # B2 3.6TB 512GB, watchlist, recentchanges, contributions, logpager
 		# 'db1100' => 1,   # C2 3.6TB 512GB, old master #T174569
-		# 'db1104' => 100,  # B3 3.6TB 512GB #T174569
-		# 'db1106' => 300,  # D3 3.6TB 512GB #T174569
 	],
 	's6' => [
 		'db1061' => 0,   # C3 2.8TB 128GB, master
@@ -172,75 +102,18 @@ $wgLBFactoryConf = [
 		'db1086' => 500, # B3 3.6TB 512GB, api
 		'db1094' => 500, # D2 3.6TB 512GB
 	],
+	's8' => [
+		'db1071' => 1,   # D1 2.8TB 160GB, master
+		'db1092' => 500, # D2 3.6TB 512GB
+		# 'db1104' => 100,  # B3 3.6TB 512GB #T174569
+		# 'db1106' => 300,  # D3 3.6TB 512GB #T174569
+
+	],
 	'silver' => [
 		'silver' => 1,
 	],
 	'labtestweb2001' => [
 		'labtestweb2001' => 1,
-	],
-],
-
-'serverTemplate' => [
-	'dbname'	  => $wgDBname,
-	'user'		  => $wgDBuser,
-	'password'	  => $wgDBpassword,
-	'type'		  => 'mysql',
-	'flags'		  => DBO_DEFAULT,
-	'max lag'	  => 6, // should be safely less than $wgCdnReboundPurgeDelay
-	'variables'   => [
-		'innodb_lock_wait_timeout' => 15
-	]
-],
-
-'templateOverridesBySection' => [
-	's1' => [
-		'lagDetectionMethod' => 'pt-heartbeat',
-		'lagDetectionOptions' => [
-			'conds' => [ 'shard' => 's1', 'datacenter' => $wmfMasterDatacenter ]
-		],
-		'useGTIDs' => true
-	],
-	's2' => [
-		'lagDetectionMethod' => 'pt-heartbeat',
-		'lagDetectionOptions' => [
-			'conds' => [ 'shard' => 's2', 'datacenter' => $wmfMasterDatacenter ]
-		],
-		'useGTIDs' => true
-	],
-	'DEFAULT' /* s3 */  => [
-		'lagDetectionMethod' => 'pt-heartbeat',
-		'lagDetectionOptions' => [
-			'conds' => [ 'shard' => 's3', 'datacenter' => $wmfMasterDatacenter ]
-		],
-		'useGTIDs' => true
-	],
-	's4' => [
-		'lagDetectionMethod' => 'pt-heartbeat',
-		'lagDetectionOptions' => [
-			'conds' => [ 'shard' => 's4', 'datacenter' => $wmfMasterDatacenter ]
-		],
-		'useGTIDs' => true
-	],
-	's5' => [
-		'lagDetectionMethod' => 'pt-heartbeat',
-		'lagDetectionOptions' => [
-			'conds' => [ 'shard' => 's5', 'datacenter' => $wmfMasterDatacenter ]
-		],
-		'useGTIDs' => true
-	],
-	's6' => [
-		'lagDetectionMethod' => 'pt-heartbeat',
-		'lagDetectionOptions' => [
-			'conds' => [ 'shard' => 's6', 'datacenter' => $wmfMasterDatacenter ]
-		],
-		'useGTIDs' => true
-	],
-	's7' => [
-		'lagDetectionMethod' => 'pt-heartbeat',
-		'lagDetectionOptions' => [
-			'conds' => [ 'shard' => 's7', 'datacenter' => $wmfMasterDatacenter ]
-		],
-		'useGTIDs' => true
 	],
 ],
 
@@ -465,178 +338,37 @@ $wgLBFactoryConf = [
 			'db1034' => 1,
 		],
 	],
+	's8' => [
+		'vslow' => [
+		],
+		'dump' => [
+		],
+		'api' => [
+		],
+		'watchlist' => [
+		],
+		'recentchanges' => [
+		],
+		'recentchangeslinked' => [
+		],
+		'contributions' => [
+		],
+		'logpager' => [
+		],
+	],
 ],
 
 'groupLoadsByDB' => [],
+
+'serverTemplate' => $serverTemplate,
+
+'templateOverridesBySection' => $templateOverridesBySection,
 
 # Hosts settings
 # Do not remove servers from this list ever
 # Removing a server from this list does not remove the server from rotation,
 # it just breaks the site horribly.
-'hostsByName' => [
-	'db1001' => '10.64.0.5', # do not remove or comment out
-	'db1009' => '10.64.0.13', # do not remove or comment out
-	'db1011' => '10.64.0.15', # do not remove or comment out
-	'db1020' => '10.64.16.9', # do not remove or comment out
-	'db1021' => '10.64.16.10', # do not remove or comment out
-	'db1030' => '10.64.16.19', # do not remove or comment out
-	'db1034' => '10.64.16.23', # do not remove or comment out
-	'db1039' => '10.64.16.28', # do not remove or comment out
-	'db1043' => '10.64.16.32', # do not remove or comment out
-	'db1044' => '10.64.16.33', # do not remove or comment out
-	'db1047' => '10.64.16.36', # do not remove or comment out
-	'db1051' => '10.64.16.76', # do not remove or comment out
-	'db1052' => '10.64.16.77', # do not remove or comment out
-	'db1053' => '10.64.0.87', # do not remove or comment out
-	'db1054' => '10.64.0.206', # do not remove or comment out
-	'db1055' => '10.64.32.25', # do not remove or comment out
-	'db1056' => '10.64.32.26', # do not remove or comment out
-	'db1060' => '10.64.32.30', # do not remove or comment out
-	'db1061' => '10.64.32.227', # do not remove or comment out
-	'db1062' => '10.64.48.15', # do not remove or comment out
-	'db1063' => '10.64.32.228', # do not remove or comment out
-	'db1064' => '10.64.48.19', # do not remove or comment out
-	'db1065' => '10.64.48.20', # do not remove or comment out
-	'db1066' => '10.64.48.21', # do not remove or comment out
-	'db1067' => '10.64.48.22', # do not remove or comment out
-	'db1068' => '10.64.48.23', # do not remove or comment out
-	'db1069' => '10.64.48.24', # do not remove or comment out
-	'db1070' => '10.64.48.25', # do not remove or comment out
-	'db1071' => '10.64.48.26', # do not remove or comment out
-	'db1072' => '10.64.16.39', # do not remove or comment out
-	'db1073' => '10.64.16.79', # do not remove or comment out
-	'db1074' => '10.64.0.204', # do not remove or comment out
-	'db1075' => '10.64.0.205', # do not remove or comment out
-	'db1076' => '10.64.16.190', # do not remove or comment out
-	'db1077' => '10.64.16.191', # do not remove or comment out
-	'db1078' => '10.64.32.136', # do not remove or comment out
-	'db1079' => '10.64.0.91', # do not remove or comment out
-	'db1080' => '10.64.0.92', # do not remove or comment out
-	'db1081' => '10.64.0.93', # do not remove or comment out
-	'db1082' => '10.64.0.94', # do not remove or comment out
-	'db1083' => '10.64.16.101', # do not remove or comment out
-	'db1084' => '10.64.16.102', # do not remove or comment out
-	'db1085' => '10.64.16.103', # do not remove or comment out
-	'db1086' => '10.64.16.104', # do not remove or comment out
-	'db1087' => '10.64.32.113', # do not remove or comment out
-	'db1088' => '10.64.32.114', # do not remove or comment out
-	'db1089' => '10.64.32.115', # do not remove or comment out
-	'db1090' => '10.64.32.116', # do not remove or comment out
-	'db1091' => '10.64.48.150', # do not remove or comment out
-	'db1092' => '10.64.48.151', # do not remove or comment out
-	'db1093' => '10.64.48.152', # do not remove or comment out
-	'db1094' => '10.64.48.153', # do not remove or comment out
-	'db1096' => '10.64.0.163', # do not remove or comment out
-	'db1097' => '10.64.48.11', # do not remove or comment out
-	'db1098' => '10.64.16.83', # do not remove or comment out
-	'db1099' => '10.64.16.84', # do not remove or comment out
-	'db1100' => '10.64.32.197', # do not remove or comment out
-	'db1101' => '10.64.32.198', # do not remove or comment out
-	'db1103:3312' => '10.64.0.164:3312', # do not remove or comment out
-	'db1103:3314' => '10.64.0.164:3314', # do not remove or comment out
-	'db1104' => '10.64.16.85', # do not remove or comment out
-	'db1105:3311' => '10.64.32.222:3311', # do not remove or comment out
-	'db1105:3312' => '10.64.32.222:3312', # do not remove or comment out
-	'db1106' => '10.64.48.13', # do not remove or comment out
-	'db2001' => '10.192.0.4', # do not remove or comment out
-	'db2002' => '10.192.0.5', # do not remove or comment out
-	'db2003' => '10.192.0.6', # do not remove or comment out
-	'db2004' => '10.192.0.7', # do not remove or comment out
-	'db2005' => '10.192.0.8', # do not remove or comment out
-	'db2006' => '10.192.0.9', # do not remove or comment out
-	'db2007' => '10.192.0.10', # do not remove or comment out
-	'db2011' => '10.192.0.14', # do not remove or comment out
-	'db2012' => '10.192.0.15', # do not remove or comment out
-	'db2013' => '10.192.0.16', # do not remove or comment out
-	'db2014' => '10.192.0.17', # do not remove or comment out
-	'db2015' => '10.192.0.18', # do not remove or comment out
-	'db2016' => '10.192.16.4', # do not remove or comment out
-	'db2017' => '10.192.16.5', # do not remove or comment out
-	'db2018' => '10.192.16.6', # do not remove or comment out
-	'db2019' => '10.192.16.7', # do not remove or comment out
-	'db2020' => '10.192.16.8', # do not remove or comment out
-	'db2021' => '10.192.16.9', # do not remove or comment out
-	'db2022' => '10.192.16.10', # do not remove or comment out
-	'db2023' => '10.192.16.11', # do not remove or comment out
-	'db2024' => '10.192.16.12', # do not remove or comment out
-	'db2025' => '10.192.16.13', # do not remove or comment out
-	'db2026' => '10.192.16.14', # do not remove or comment out
-	'db2027' => '10.192.16.15', # do not remove or comment out
-	'db2028' => '10.192.16.16', # do not remove or comment out
-	'db2029' => '10.192.16.17', # do not remove or comment out
-	'db2030' => '10.192.16.18', # do not remove or comment out
-	'db2031' => '10.192.16.19', # do not remove or comment out
-	'db2032' => '10.192.16.20', # do not remove or comment out
-	'db2033' => '10.192.32.4', # do not remove or comment out
-	'db2034' => '10.192.0.87', # do not remove or comment out
-	'db2035' => '10.192.32.6', # do not remove or comment out
-	'db2036' => '10.192.32.7', # do not remove or comment out
-	'db2037' => '10.192.32.8', # do not remove or comment out
-	'db2038' => '10.192.32.9', # do not remove or comment out
-	'db2039' => '10.192.32.10', # do not remove or comment out
-	'db2040' => '10.192.32.11', # do not remove or comment out
-	'db2041' => '10.192.32.12', # do not remove or comment out
-	'db2042' => '10.192.32.13', # do not remove or comment out
-	'db2043' => '10.192.32.103', # do not remove or comment out
-	'db2044' => '10.192.32.104', # do not remove or comment out
-	'db2045' => '10.192.32.105', # do not remove or comment out
-	'db2046' => '10.192.32.106', # do not remove or comment out
-	'db2047' => '10.192.32.107', # do not remove or comment out
-	'db2048' => '10.192.32.108', # do not remove or comment out
-	'db2049' => '10.192.32.109', # do not remove or comment out
-	'db2050' => '10.192.32.110', # do not remove or comment out
-	'db2051' => '10.192.16.22', # do not remove or comment out
-	'db2052' => '10.192.48.4', # do not remove or comment out
-	'db2053' => '10.192.48.5', # do not remove or comment out
-	'db2054' => '10.192.48.6', # do not remove or comment out
-	'db2055' => '10.192.48.7', # do not remove or comment out
-	'db2056' => '10.192.48.8', # do not remove or comment out
-	'db2057' => '10.192.48.9', # do not remove or comment out
-	'db2058' => '10.192.48.10', # do not remove or comment out
-	'db2059' => '10.192.48.11', # do not remove or comment out
-	'db2060' => '10.192.48.12', # do not remove or comment out
-	'db2061' => '10.192.48.13', # do not remove or comment out
-	'db2062' => '10.192.16.195', # do not remove or comment out
-	'db2063' => '10.192.48.15', # do not remove or comment out
-	'db2064' => '10.192.48.16', # do not remove or comment out
-	'db2065' => '10.192.48.17', # do not remove or comment out
-	'db2066' => '10.192.48.18', # do not remove or comment out
-	'db2067' => '10.192.48.19', # do not remove or comment out
-	'db2068' => '10.192.48.20', # do not remove or comment out
-	'db2069' => '10.192.48.21', # do not remove or comment out
-	'db2070' => '10.192.32.5', # do not remove or comment out
-	'db2071' => '10.192.0.4', # do not remove or comment out
-	'db2072' => '10.192.16.37', # do not remove or comment out
-	'db2073' => '10.192.32.167', # do not remove or comment out
-	'db2074' => '10.192.48.84', # do not remove or comment out
-	'db2075' => '10.192.0.5', # do not remove or comment out
-	'db2076' => '10.192.16.38', # do not remove or comment out
-	'db2077' => '10.192.32.168', # do not remove or comment out
-	'db2079' => '10.192.0.6', # do not remove or comment out
-	'db2080' => '10.192.32.169', # do not remove or comment out
-	'db2081' => '10.192.0.7', # do not remove or comment out
-	'db2082' => '10.192.16.39', # do not remove or comment out
-	'db2083' => '10.192.32.170', # do not remove or comment out
-	'db2084:3314' => '10.192.48.86:3314', # do not remove or comment out
-	'db2084:3315' => '10.192.48.86:3315', # do not remove or comment out
-	'db2085:3313' => '10.192.0.8:3313', # do not remove or comment out
-	'db2085:3315' => '10.192.0.8:3315', # do not remove or comment out
-	'db2086:3315' => '10.192.16.40:3315', # do not remove or comment out
-	'db2086:3317' => '10.192.16.40:3317', # do not remove or comment out
-	'db2087:3316' => '10.192.32.171:3316', # do not remove or comment out
-	'db2087:3317' => '10.192.32.171:3317', # do not remove or comment out
-	'db2088:3311' => '10.192.48.87:3311', # do not remove or comment out
-	'db2088:3312' => '10.192.48.87:3312', # do not remove or comment out
-	'db2089:3315' => '10.192.0.9:3315', # do not remove or comment out
-	'db2089:3316' => '10.192.0.9:3316', # do not remove or comment out
-	'db2091:3312' => '10.192.0.10:3312', # do not remove or comment out
-	'db2091:3314' => '10.192.0.10:3314', # do not remove or comment out
-	'db2092:3311' => '10.192.16.41:3311', # do not remove or comment out
-	'db2092:3313' => '10.192.16.41:3313', # do not remove or comment out
-	'virt1000' => '208.80.154.18', # do not remove or comment out
-	'silver' => '208.80.154.136', # do not remove or comment out
-	'labtestweb2001' => '208.80.153.14', # do not remove or comment out
-],
+'hostsByName' => $hostsByName,
 
 'externalLoads' => [
 	# Recompressed stores
@@ -681,31 +413,11 @@ $wgLBFactoryConf = [
 	],
 ],
 
-'masterTemplateOverrides' => [],
+'masterTemplateOverrides' => $masterTemplateOverrides,
 
-'externalTemplateOverrides' => [
-	'flags' => 0, // No transactions
-],
+'externalTemplateOverrides' => $externalTemplateOverrides,
 
-'templateOverridesByCluster' => [
-	'rc1'		=> [ 'is static' => true ],
-	'cluster1'	=> [ 'blobs table' => 'blobs_cluster1', 'is static' => true ],
-	'cluster2'	=> [ 'blobs table' => 'blobs_cluster2', 'is static' => true ],
-	'cluster3'	=> [ 'blobs table' => 'blobs_cluster3', 'is static' => true ],
-	'cluster4'	=> [ 'blobs table' => 'blobs_cluster4', 'is static' => true ],
-	'cluster5'	=> [ 'blobs table' => 'blobs_cluster5', 'is static' => true ],
-	'cluster6'	=> [ 'blobs table' => 'blobs_cluster6', 'is static' => true ],
-	'cluster7'	=> [ 'blobs table' => 'blobs_cluster7', 'is static' => true ],
-	'cluster8'	=> [ 'blobs table' => 'blobs_cluster8', 'is static' => true ],
-	'cluster9'	=> [ 'blobs table' => 'blobs_cluster9', 'is static' => true ],
-	'cluster10'	=> [ 'blobs table' => 'blobs_cluster10', 'is static' => true ],
-	'cluster20'	=> [ 'blobs table' => 'blobs_cluster20', 'is static' => true ],
-	'cluster21'	=> [ 'blobs table' => 'blobs_cluster21', 'is static' => true ],
-	'cluster22'	=> [ 'blobs table' => 'blobs_cluster22', 'is static' => true ],
-	'cluster23'	=> [ 'blobs table' => 'blobs_cluster23', 'is static' => true ],
-	'cluster24'	=> [ 'blobs table' => 'blobs_cluster24' ],
-	'cluster25'	=> [ 'blobs table' => 'blobs_cluster25' ],
-],
+'templateOverridesByCluster' => $templateOverridesByCluster,
 
 # This key must exist for the master switch script to work, which means comment and uncomment
 # the individual shards, but leave the 'readOnlyBySection' => [ ], alone.
@@ -725,16 +437,14 @@ $wgLBFactoryConf = [
 	# 's5'      => 'This request is served by a passive datacenter. If you see this something is really wrong.',
 	# 's6'      => 'This request is served by a passive datacenter. If you see this something is really wrong.',
 	# 's7'      => 'This request is served by a passive datacenter. If you see this something is really wrong.',
+	# 's8'      => 'This request is served by a passive datacenter. If you see this something is really wrong.',
 ],
 
 ];
 
-$wgDefaultExternalStore = [
-	'DB://cluster24',
-	'DB://cluster25',
+$wmgParserCacheDBs = [
+	'10.64.0.12'   => '10.64.0.12',   # pc1004, A3 2.4TB 256GB
+	'10.64.32.72'  => '10.64.32.72',  # pc1005, C7 2.4TB 256GB
+	'10.64.48.128' => '10.64.48.128', # pc1006, D3 2.4TB 256GB
 ];
 
-# $wgLBFactoryConf['readOnlyBySection']['s2'] =
-# 'Scheduled maintenance, s2 wikis in read-only mode for a few minutes';
-# $wgLBFactoryConf['readOnlyBySection']['s2a'] =
-# 'Emergency maintenance, need more servers up, new estimate ~18:30 UTC';
