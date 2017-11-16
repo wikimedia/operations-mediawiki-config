86c3fff8704562fa48849a2c8fa65d8565c0bbd6
Jaime Crespo <jcrespo@wikimedia.org>
2017-11-16 17:20:00 +0100

    mariadb: Depool db1071, pool db1104 as api
    We are going to pool db1071 as the future s8 master. Depool it so
    we can restart it, change configuration, and set it as the master
    of codfw s8.
    
    Bug: T177208
    Change-Id: I46d73bcfe527f90c5b037c5cf07300b957a974c9

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -145,14 +145,14 @@ $wgLBFactoryConf = [
 	's5' => [
 		'db1063' => 0,   # C5 2.8TB 128GB, master
 		'db1070' => 1,   # D1 2.8TB 160GB, vslow, dump #ROW based replication - master for db1095
-		'db1071' => 1,   # D1 2.8TB 160GB, api
+		# 'db1071' => 1,   # D1 2.8TB 160GB, future s8 master
 		'db1082' => 300, # A2 3.6TB 512GB, api
 		'db1087' => 500, # C2 3.6TB 512GB
 		# 'db1092' => 500, # D2 3.6TB 512GB #T174569
 		# 'db1096' => 1,   # A6 3.6TB 512GB, watchlist, recentchanges, contributions, logpager #T174569
 		'db1099' => 1,   # B2 3.6TB 512GB, watchlist, recentchanges, contributions, logpager
 		'db1100' => 1,   # C2 3.6TB 512GB, old master
-		# 'db1104' => 100,  # B3 3.6TB 512GB
+		'db1104' => 100,  # B3 3.6TB 512GB, api
 		'db1106' => 500,  # D3 3.6TB 512GB
 	],
 	's6' => [
@@ -388,8 +388,8 @@ $wgLBFactoryConf = [
 			'db1070' => 1,
 		],
 		'api' => [
-			'db1071' => 1,
-			'db1082' => 1,
+			'db1082' => 5,
+			'db1104' => 1,
 		],
 		'watchlist' => [
 			# 'db1096' => 1,
