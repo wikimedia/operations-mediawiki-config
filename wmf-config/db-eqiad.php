ced5640403420d8693d6c657ad67b9ce903e41e1
Jaime Crespo <jcrespo@wikimedia.org>
2017-11-16 21:00:45 +0100

    mariadb: Depool full all delayed servers
    Bug: T180714
    Change-Id: I1253c77d9e4086766712a4b3541e1e212061da79

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -150,10 +150,10 @@ $wgLBFactoryConf = [
 		'db1087' => 500, # C2 3.6TB 512GB
 		# 'db1092' => 500, # D2 3.6TB 512GB #T174569
 		# 'db1096' => 1,   # A6 3.6TB 512GB, watchlist, recentchanges, contributions, logpager #T174569
-		'db1099' => 1,   # B2 3.6TB 512GB, watchlist, recentchanges, contributions, logpager
-		'db1100' => 1,   # C2 3.6TB 512GB, old master
-		'db1104' => 100,  # B3 3.6TB 512GB, api
-		'db1106' => 500,  # D3 3.6TB 512GB
+		# 'db1099' => 1,   # B2 3.6TB 512GB, watchlist, recentchanges, contributions, logpager
+		# 'db1100' => 1,   # C2 3.6TB 512GB, old master
+		# 'db1104' => 100,  # B3 3.6TB 512GB, api
+		# 'db1106' => 500,  # D3 3.6TB 512GB
 	],
 	's6' => [
 		'db1061' => 0,   # C3 2.8TB 128GB, master
