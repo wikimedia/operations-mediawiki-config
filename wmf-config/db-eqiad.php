2eacea5ffe84512afaa6c8839006a39da571670f
Manuel Arostegui <marostegui@wikimedia.org>
2017-11-17 06:06:36 +0100

    db-eqiad.php: Start repooling all the hosts
    All the broken hosts are now up to date.
    Start repooling them back with a slighltly lower weight than usual as
    they have been out for hours.
    
    Bug: T180714
    Change-Id: Ibe61f6723d9e77aa1fb2d1acc04b46f806d933a5

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -143,17 +143,17 @@ $wgLBFactoryConf = [
 		'db1103:3314' => 1,  # A3 3.6TB 512GB # rc, log: s2 and s4
 	],
 	's5' => [
-		# 'db1063' => 0,   # C5 2.8TB 128GB, master, down
 		'db1070' => 0,   # D1 2.8TB 160GB, master
+		# 'db1063' => 0,   # C5 2.8TB 128GB, master, crashed #T180714
 		# 'db1071' => 1,   # D1 2.8TB 160GB, future s8 master
-		'db1082' => 300, # A2 3.6TB 512GB, api, recentchanges, slow
+		'db1082' => 300, # A2 3.6TB 512GB, api
 		'db1087' => 500, # C2 3.6TB 512GB
-		# 'db1092' => 500, # D2 3.6TB 512GB #T174569
-		# 'db1096' => 1,   # A6 3.6TB 512GB, watchlist, recentchanges, contributions, logpager #T174569
-		# 'db1099' => 1,   # B2 3.6TB 512GB, watchlist, recentchanges, contributions, logpager
-		# 'db1100' => 1,   # C2 3.6TB 512GB, old master
-		# 'db1104' => 100,  # B3 3.6TB 512GB, api
-		# 'db1106' => 500,  # D3 3.6TB 512GB
+		'db1092' => 100, # D2 3.6TB 512GB
+		'db1096' => 1,   # A6 3.6TB 512GB, watchlist, recentchanges, contributions, logpager #T174569
+		'db1099' => 1,   # B2 3.6TB 512GB, watchlist, recentchanges, contributions, logpager
+		'db1100' => 1,   # C2 3.6TB 512GB, old master, vslow
+		'db1104' => 200,  # B3 3.6TB 512GB, api
+		'db1106' => 200,  # D3 3.6TB 512GB
 	],
 	's6' => [
 		'db1061' => 0,   # C3 2.8TB 128GB, master
@@ -382,28 +382,34 @@ $wgLBFactoryConf = [
 	],
 	's5' => [
 		'vslow' => [
-			'db1082' => 1,
+			'db1100' => 1,
 		],
 		'dump' => [
-			'db1082' => 1,
+			'db1100' => 1,
 		],
 		'api' => [
 			'db1082' => 1,
+			'db1104' => 1,
 		],
 		'watchlist' => [
-			'db1082' => 1,
+			'db1096' => 1,
+			'db1099' => 1,
 		],
 		'recentchanges' => [
-			'db1082' => 1,
+			'db1096' => 1,
+			'db1099' => 1,
 		],
 		'recentchangeslinked' => [
-			'db1082' => 1,
+			'db1096' => 1,
+			'db1099' => 1,
 		],
 		'contributions' => [
-			'db1082' => 1,
+			'db1096' => 1,
+			'db1099' => 1,
 		],
 		'logpager' => [
-			'db1082' => 1,
+			'db1096' => 1,
+			'db1099' => 1,
 		],
 	],
 	's6' => [
