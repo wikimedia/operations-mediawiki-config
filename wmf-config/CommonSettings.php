c9564cffe653da56678de3105251f15d00fcf730
Reedy <reedy@wikimedia.org>
2017-11-18 19:22:52 +0000

    Convert GWToolset to extension.json etc
    Bug: T87928
    Change-Id: I50fb7e7ee2475c229eff0bf5c5fee24d4e9a17c0

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -2186,18 +2186,15 @@ if ( $wmgUseCommonsMetadata ) {
 }
 
 if ( $wmgUseGWToolset ) {
-	require_once "$IP/extensions/GWToolset/GWToolset.php";
+	wfLoadExtension( 'GWToolset' );
 	$wgGWTFileBackend = 'local-multiwrite';
 	$wgGWTFBMaxAge = '1 week';
 	if ( $wmgUseClusterJobqueue ) {
 		$wgJobTypeConf['gwtoolsetUploadMetadataJob'] = [ 'checkDelay' => true ] + $wgJobTypeConf['default'];
 	}
+
 	// extra throttling until the image scalers are more robust
-	if ( class_exists( 'GWToolset\Config' ) ) {
-		GWToolset\Config::$mediafile_job_throttle_default = 5; // 5 files per batch
-	} else {
-		$wgGWToolsetConfigOverrides['mediafile_job_throttle_default'] = 5;
-	}
+	$wgGWToolsetConfigOverrides['mediafile_job_throttle_default'] = 5; // 5 files per batch
 	$wgJobBackoffThrottling['gwtoolsetUploadMetadataJob'] = 5 / 3600; // 5 batches per hour
 }
 
