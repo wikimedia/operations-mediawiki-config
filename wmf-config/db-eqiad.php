e1c6d7ff66d4eec007c0fab93acf37e135eb5059
Manuel Arostegui <marostegui@wikimedia.org>
2017-11-19 20:37:29 +0100

    db-eqiad.php: Load 0 for db1100
    db1100 is serving vslow service, it should have weight 0
    
    Change-Id: I5a1dc75dc22f7a2378a832780301654953f1c1dd

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -151,7 +151,7 @@ $wgLBFactoryConf = [
 		'db1092' => 500, # D2 3.6TB 512GB
 		'db1096' => 1,   # A6 3.6TB 512GB, watchlist, recentchanges, contributions, logpager #T174569
 		'db1099' => 1,   # B2 3.6TB 512GB, watchlist, recentchanges, contributions, logpager
-		'db1100' => 1,   # C2 3.6TB 512GB, old master, vslow
+		'db1100' => 0,   # C2 3.6TB 512GB, old master, vslow
 		'db1104' => 300,  # B3 3.6TB 512GB, api
 		'db1106' => 500,  # D3 3.6TB 512GB
 	],
