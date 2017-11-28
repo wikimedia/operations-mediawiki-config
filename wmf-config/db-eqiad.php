b5927738f8d243c3395f7d34398fa52ee038811c
Jaime Crespo <jcrespo@wikimedia.org>
2017-11-28 18:35:09 +0100

    mariadb: Setup s8 empty on eqiad
    Before actual deployment, setup s8 in read only mode, with no
    wikis pointing to it. This will allow to clean up configuration,
    syntax testing and and one-line change on deployment.
    
    Bug: T177208
    Change-Id: Ib31b3aaef25b53ee5a2b346dc55e085de56bbb24

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -75,6 +75,9 @@ $wgLBFactoryConf = [
 	'rowiki'       => 's7',
 	'kowiki'       => 's7',
 
+	# s8: future dedicated wikidata
+	# 'wikidatawiki' => 's8',
+
 	# labs-related wikis
 	'labswiki'     => 'silver',
 	'labtestwiki'  => 'labtestweb2001',
@@ -140,20 +143,13 @@ $wgLBFactoryConf = [
 	],
 	's5' => [
 		'db1070' => 0,   # D1 2.8TB 160GB, master
-		'db1051' => 0,  # B3 2.8TB  96GB, vslow, dump in s5
-		# 'db1063' => 0,   # C5 2.8TB 128GB, vslow, dump in s8
-		# 'db1071' => 1,   # D1 2.8TB 160GB, future s8 master
-		'db1082' => 300, # A2 3.6TB 512GB, api
-		# 'db1087' => 500, # C2 3.6TB 512GB #s8
-		# 'db1092' => 300, # D2 3.6TB 512GB #s8
-		# 'db1096' => 1,   # A6 3.6TB 512GB, watchlist, recentchanges, contributions, logpager #T178359
+		'db1051' => 0,   # B3 2.8TB  96GB, vslow, dump in s5
+		'db1082' => 300, # A2 3.6TB 512GB, api # db1095 master
+		# 'db1096:3315' => 1,   # A6 3.6TB 512GB, watchlist, recentchanges, contributions, logpager
 		'db1097:3315' => 1,   # D1 3.6TB 512GB, # rc, log: s4 and s5
 		'db1099:3318' => 1,   # B2 3.6TB 512GB, watchlist, recentchanges, contributions, logpager #s8
 		'db1100' => 50,   # C2 3.6TB 512GB, old master #api
-		# 'db1101:3318' => 1,   # C2 3.6TB 512GB # rc, log: s7 and s8
-		# 'db1104' => 300,  # B3 3.6TB 512GB, api #s8
 		'db1106' => 500,  # D3 3.6TB 512GB
-		# 'db1109' => 500,  # D8 3.6TB 512GB #s8
 		# 'db1110' => 500,  # C3 3.6TB 512GB # dns-originated ip-conflict T165519
 	],
 	's6' => [
@@ -174,6 +170,17 @@ $wgLBFactoryConf = [
 		'db1094' => 500, # D2 3.6TB 512GB
 		'db1101:3317' => 1,   # C2 3.6TB 512GB # rc, log: s7 and s8
 	],
+	's8' => [
+		'db1071' => 0,   # D1 2.8TB 160GB, master
+		'db1063' => 0,   # C5 2.8TB 128GB, vslow, dump
+		'db1087' => 500, # C2 3.6TB 512GB, # db1095 master
+		'db1092' => 300, # D2 3.6TB 512GB, api
+		'db1099:3318' => 1,   # B2 3.6TB 512GB # rc, log: s1 and s8
+		'db1101:3318' => 1,   # C2 3.6TB 512GB # rc, log: s7 and s8
+		'db1104' => 300,  # B3 3.6TB 512GB, api
+		'db1109' => 500,  # D8 3.6TB 512GB
+	],
+
 	'silver' => [
 		'silver' => 1,
 	],
@@ -241,6 +248,13 @@ $wgLBFactoryConf = [
 		'lagDetectionMethod' => 'pt-heartbeat',
 		'lagDetectionOptions' => [
 			'conds' => [ 'shard' => 's7', 'datacenter' => $wmfMasterDatacenter ]
+		],
+		'useGTIDs' => true
+	],
+	's8' => [
+		'lagDetectionMethod' => 'pt-heartbeat',
+		'lagDetectionOptions' => [
+			'conds' => [ 'shard' => 's8', 'datacenter' => $wmfMasterDatacenter ]
 		],
 		'useGTIDs' => true
 	],
@@ -374,47 +388,39 @@ $wgLBFactoryConf = [
 	],
 	's5' => [
 		'vslow' => [
-			# 'db1063' => 1,
 			'db1051' => 1,
 		],
 		'dump' => [
-			# 'db1063' => 1,
 			'db1051' => 1,
 		],
 		'api' => [
 			'db1082' => 1,
 			'db1100' => 3,
-			# 'db1104' => 1,
 		],
 		'watchlist' => [
-			# 'db1096' => 1,
+			# 'db1096:3315' => 1,
 			'db1097:3315' => 1,
 			'db1099:3318' => 1,
-			# 'db1101:3318' => 1,
 		],
 		'recentchanges' => [
-			# 'db1096' => 1,
+			# 'db1096:3315' => 1,
 			'db1097:3315' => 1,
 			'db1099:3318' => 1,
-			# 'db1101:3318' => 1,
 		],
 		'recentchangeslinked' => [
-			# 'db1096' => 1,
+			# 'db1096:3315' => 1,
 			'db1097:3315' => 1,
 			'db1099:3318' => 1,
-			# 'db1101:3318' => 1,
 		],
 		'contributions' => [
-			# 'db1096' => 1,
+			# 'db1096:3315' => 1,
 			'db1097:3315' => 1,
 			'db1099:3318' => 1,
-			# 'db1101:3318' => 1,
 		],
 		'logpager' => [
-			# 'db1096' => 1,
+			# 'db1096:3315' => 1,
 			'db1097:3315' => 1,
 			'db1099:3318' => 1,
-			# 'db1101:3318' => 1,
 		],
 	],
 	's6' => [
@@ -473,6 +479,38 @@ $wgLBFactoryConf = [
 		'logpager' => [
 			'db1034' => 1,
 			'db1101:3317' => 1,
+		],
+	],
+	's8' => [
+		'vslow' => [
+			'db1063' => 1,
+		],
+		'dump' => [
+			'db1063' => 1,
+		],
+		'api' => [
+			'db1092' => 3,
+			'db1104' => 1,
+		],
+		'watchlist' => [
+			'db1099:3318' => 1,
+			'db1101:3318' => 1,
+		],
+		'recentchanges' => [
+			'db1099:3318' => 1,
+			'db1101:3318' => 1,
+		],
+		'recentchangeslinked' => [
+			'db1099:3318' => 1,
+			'db1101:3318' => 1,
+		],
+		'contributions' => [
+			'db1099:3318' => 1,
+			'db1101:3318' => 1,
+		],
+		'logpager' => [
+			'db1099:3318' => 1,
+			'db1101:3318' => 1,
 		],
 	],
 ],
@@ -535,7 +573,7 @@ $wgLBFactoryConf = [
 	'db1092' => '10.64.48.151', # do not remove or comment out
 	'db1093' => '10.64.48.152', # do not remove or comment out
 	'db1094' => '10.64.48.153', # do not remove or comment out
-	'db1096' => '10.64.0.163', # do not remove or comment out
+	'db1096:3315' => '10.64.0.163:3315', # do not remove or comment out
 	'db1097:3314' => '10.64.48.11:3314', # do not remove or comment out
 	'db1097:3315' => '10.64.48.11:3315', # do not remove or comment out
 	'db1098' => '10.64.16.83', # do not remove or comment out
@@ -736,9 +774,10 @@ $wgLBFactoryConf = [
 	# 's2'      => 'This request is served by a passive datacenter. If you see this something is really wrong.',
 	# 'DEFAULT' => 'This request is served by a passive datacenter. If you see this something is really wrong.', # s3
 	# 's4'      => 'This request is served by a passive datacenter. If you see this something is really wrong.',
-	# 's5'      => 'This request is served by a passive datacenter. If you see this something is really wrong.',
+	# 's5'      => 'Temporary read-only time for maintenance, please try again in some minutes.',
 	# 's6'      => 'This request is served by a passive datacenter. If you see this something is really wrong.',
 	# 's7'      => 'This request is served by a passive datacenter. If you see this something is really wrong.',
+	's8'      => 'Temporary read-only time for maintenance, please try again in some minutes.',
 ],
 
 ];
