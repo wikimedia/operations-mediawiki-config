7189589b013de8b239bdaf0a554d5ead82fc9e6d
Max Semenik <maxsem.wiki@gmail.com>
2017-11-29 10:50:20 -0800

    Switch all wikis to HTML5 section IDs
    Bug: T152540
    Bug: T175725
    Change-Id: Ie6d0b2cfb9126c17a135842a1fa06d67905c4e39

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -19724,19 +19724,7 @@ $wgConf->settings = [
 
 // T152540
 'wgFragmentMode' => [
-	'default' => [ 'legacy', 'html5' ],
-	'testwiki' => [ 'html5', 'legacy' ],
-	'test2wiki' => [ 'html5', 'legacy' ],
-	'mediawikiwiki' => [ 'html5', 'legacy' ],
-	'ruwiki' => [ 'html5', 'legacy' ],
-	'ruwikibooks' => [ 'html5', 'legacy' ],
-	'ruwikimedia' => [ 'html5', 'legacy' ],
-	'ruwikinews' => [ 'html5', 'legacy' ],
-	'ruwikiquote' => [ 'html5', 'legacy' ],
-	'ruwikisource' => [ 'html5', 'legacy' ],
-	'ruwikiversity' => [ 'html5', 'legacy' ],
-	'ruwikivoyage' => [ 'html5', 'legacy' ],
-	'ruwiktionary' => [ 'html5', 'legacy' ],
+	'default' => [ 'html5', 'legacy' ],
 ],
 
 'wmgUse3d' => [
