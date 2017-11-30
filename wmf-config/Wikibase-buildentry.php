47b36344bed3f3729914961eb7a7bd0220235eb5
Addshore <addshorewiki@gmail.com>
2017-11-30 20:42:33 +0000

    Revert "Revert "wdbuild: add switch to ease killing""
    This reverts commit 29cdd8ad4d7bb7f79c7bc1ef57cb1c40a8e92ffb.
    
    Change-Id: I1f21767339d1ff36375a93ab7aa383414ec32117

--- wmf-config/Wikibase-buildentry.php
+++ wmf-config/Wikibase-buildentry.php
@@ -7,26 +7,47 @@
 
 $wgWikidataBuildBaseDir = $IP . '/extensions/Wikidata';
 
-if ( file_exists( $wgWikidataBuildBaseDir . '/vendor/autoload.php' ) ) {
-	include_once $wgWikidataBuildBaseDir . '/vendor/autoload.php';
-}
-
-if ( !empty( $wmgUseWikibaseRepo ) ) {
-	include_once "$wgWikidataBuildBaseDir/extensions/Wikibase/repo/Wikibase.php";
-	include_once "$wgWikidataBuildBaseDir/extensions/Wikidata.org/WikidataOrg.php";
-	include_once "$wgWikidataBuildBaseDir/extensions/PropertySuggester/PropertySuggester.php";
-	include_once "$wgWikidataBuildBaseDir/extensions/Quality/WikibaseQuality.php";
-	include_once "$wgWikidataBuildBaseDir/extensions/Constraints/WikibaseQualityConstraints.php";
-}
-
-if ( !empty( $wmgUseWikibaseClient ) ) {
-	include_once "$wgWikidataBuildBaseDir/extensions/Wikibase/client/WikibaseClient.php";
-	wfLoadExtension( 'WikimediaBadges', "$wgWikidataBuildBaseDir/extensions/WikimediaBadges/extension.json" );
-	if ( !empty( $wmgUseArticlePlaceholder ) ) {
-		wfLoadExtension( 'ArticlePlaceholder', "$wgWikidataBuildBaseDir/extensions/ArticlePlaceholder/extension.json" );
+if ( $wmgUseWikidataBuild ) {
+	if ( file_exists( $wgWikidataBuildBaseDir . '/vendor/autoload.php' ) ) {
+		include_once $wgWikidataBuildBaseDir . '/vendor/autoload.php';
 	}
-}
 
-// This should be kept until the Wikidata build extension is actually turned off
-// so that we can keep track of the version deployed on Special:Version
-require_once "$wgWikidataBuildBaseDir/Wikidata.credits.php";
+	if ( !empty( $wmgUseWikibaseRepo ) ) {
+		include_once "$wgWikidataBuildBaseDir/extensions/Wikibase/repo/Wikibase.php";
+		include_once "$wgWikidataBuildBaseDir/extensions/Wikidata.org/WikidataOrg.php";
+		include_once "$wgWikidataBuildBaseDir/extensions/PropertySuggester/PropertySuggester.php";
+		include_once "$wgWikidataBuildBaseDir/extensions/Quality/WikibaseQuality.php";
+		include_once "$wgWikidataBuildBaseDir/extensions/Constraints/WikibaseQualityConstraints.php";
+	}
+
+	if ( !empty( $wmgUseWikibaseClient ) ) {
+		include_once "$wgWikidataBuildBaseDir/extensions/Wikibase/client/WikibaseClient.php";
+		wfLoadExtension( 'WikimediaBadges', "$wgWikidataBuildBaseDir/extensions/WikimediaBadges/extension.json" );
+		if ( !empty( $wmgUseArticlePlaceholder ) ) {
+			wfLoadExtension( 'ArticlePlaceholder', "$wgWikidataBuildBaseDir/extensions/ArticlePlaceholder/extension.json" );
+		}
+	}
+
+	// This should be kept until the Wikidata build extension is actually turned off
+	// so that we can keep track of the version deployed on Special:Version
+	require_once "$wgWikidataBuildBaseDir/Wikidata.credits.php";
+
+} else {
+
+	if ( !empty( $wmgUseWikibaseRepo ) ) {
+		include_once "$IP/extensions/Wikibase/repo/Wikibase.php";
+		include_once "$IP/extensions/Wikidata.org/WikidataOrg.php";
+		include_once "$IP/extensions/PropertySuggester/PropertySuggester.php";
+		include_once "$IP/extensions/WikibaseQuality/WikibaseQuality.php";
+		include_once "$IP/extensions/WikibaseQualityConstraints/WikibaseQualityConstraints.php";
+	}
+
+	if ( !empty( $wmgUseWikibaseClient ) ) {
+		include_once "$IP/extensions/Wikibase/client/WikibaseClient.php";
+		wfLoadExtension( 'WikimediaBadges' );
+		if ( !empty( $wmgUseArticlePlaceholder ) ) {
+			wfLoadExtension( 'ArticlePlaceholder' );
+		}
+	}
+
+}
