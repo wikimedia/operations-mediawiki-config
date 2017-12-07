1f3a39aaa9b81fbe14fdfa17738e09ff9d5671b4
addshore <addshorewiki@gmail.com>
2017-12-07 11:27:17 +0100

    Wikidata dispatch, Use a LockManager with short TTL for testwikidata
    This lockManager also has a domain set, where the previous
    lock manager was global.
    
    The global manager may have resulted in locks clashing with
    testwikidatawiki leading to longer client selection
    however this was probably unlikely due to the low
    traffic of the test wiki.
    
    And use this new lock manager on testwikidatawiki
    
    Bug: T178652
    Change-Id: Ieafcfac10617126b4f9558e815205f99102e892c

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -18530,6 +18530,7 @@ $wgConf->settings = [
 
 'wmgWikibaseDispatchingLockManager' => [
 	'default' => 'redisLockManager',
+	'testwikidatawiki' => 'wikibaseDispatchRedisLockManager',
 ],
 
 'wmgWikibaseCachePrefix' => [
