f55d5db6c72037fb16fc8581c1576190a89b7194
Reedy <reedy@wikimedia.org>
2017-12-08 22:11:23 +0000

    Fix LandingCheck indenting
    Remove oooold comments
    
    Change-Id: I0f70fb3cbe8847a949094eb861829a0960b2b193

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -1967,14 +1967,8 @@ if ( $wmgEnableLandingCheck ) {
 		// === Fundraising Chapers
 		'DE', 'CH',
 
-		// --- France and it's territories (per WMFr email 2012-06-13)
-		// Not a fundraising chapter in 2013+ due to FR regulations
-		// 'FR',
-		// 'GP', 'MQ', 'GF', 'RE', 'YT', 'PM',
-				// 'NC', 'PF', 'WF', 'BL', 'MF', 'TF',
-
 		// === Blacklisted countries
-				'BY', 'CD', 'CI', 'CU', 'IQ', 'IR', 'KP', 'LB', 'LY', 'MM', 'SD', 'SO', 'SY', 'YE', 'ZW',
+		'BY', 'CD', 'CI', 'CU', 'IQ', 'IR', 'KP', 'LB', 'LY', 'MM', 'SD', 'SO', 'SY', 'YE', 'ZW',
 	];
 	$wgLandingCheckPriorityURLBase = "//wikimediafoundation.org/wiki/Special:LandingCheck";
 	$wgLandingCheckNormalURLBase = "//donate.wikimedia.org/wiki/Special:LandingCheck";
