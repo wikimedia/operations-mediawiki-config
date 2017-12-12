55cf39fcbab6afe8244a7a2bf8320338f8d4a240
Jaime Crespo <jcrespo@wikimedia.org>
2017-12-12 13:17:48 +0100

    mariadb: Depool db2071
    Bug: T181777
    Change-Id: Ia68b260f2eb19e54f87979faa9348cbcb1e5a401

--- wmf-config/db-codfw.php
+++ wmf-config/db-codfw.php
@@ -111,7 +111,7 @@ $wgLBFactoryConf = [
 		'db2062'      => 50,  # B5 3.3TB 160GB, api # mariadb 10.1
 		'db2069'      => 50,  # D6 3.3TB 160GB, api
 		'db2070'      => 400, # C5 3.3TB 160GB
-		'db2071'      => 50,  # A6 3.6TB 512GB, api
+		# 'db2071'      => 50,  # A6 3.6TB 512GB, api
 		'db2072'      => 500, # B6 3.6TB 512GB, # mariadb 10.1
 		'db2088:3311' => 1, # D1 3.3TB 512GB # rc, log: s1 and s2
 		'db2085:3311' => 1, # A5 3.3TB 512GB # rc, log: s1 and s8
@@ -310,7 +310,7 @@ $wgLBFactoryConf = [
 			'db2055' => 1,
 			'db2062' => 1,
 			'db2069' => 1,
-			'db2071' => 5,
+		#	'db2071' => 5,
 		],
 	],
 	's2' => [
