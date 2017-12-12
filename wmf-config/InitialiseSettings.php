97e2660a41a4c067ef63d26794d95f26cd7db1d9
Eddie Greiner-Petter <git@eddie-sh.de>
2017-12-12 11:38:15 +0100

    Restrict sending mails to new users
    WMF Support & Safety implemented a new feature to forbid sending
    messages to users who have neither edited nor any logged actions on a
    given wiki. This permission change will change the userrights so that
    nobody but members of the following groups will be able to still email
    such users:
    - Bureaucrats
    - Stewards
    - WMF Support & Safety
    - Global renamers
    
    Bug: T182541
    Change-Id: I798f4be25ebdefd5dbb7f4593db6f06c7bda52cf

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -8985,6 +8985,7 @@ $wgConf->settings = [
 		'global-renamer' => [ // T142123, T71651
 			'autopatrol' => true,
 			'centralauth-rename' => true,
+			'sendemail-new-users' => true,
 		],
 		'massmessage-sender' => [ // T59611, T142123
 			'autopatrol' => true,
@@ -9028,6 +9029,7 @@ $wgConf->settings = [
 			'massmessage' => true,
 			'userrights' => true,
 			'userrights-interwiki' => true,
+			'sendemail-new-users' => true,
 		],
 	],
 	'mkwiki' => [
@@ -9745,9 +9747,11 @@ $wgConf->settings = [
 		],
 		'bureaucrat' => [
 			'noratelimit' => true,
+			'sendemail-new-users' => true,
 		],
 		'steward' => [
 			'noratelimit' => true,
+			'sendemail-new-users' => true,
 		],
 		// 'rollback' => array( 'rollback' => true, ),
 		'import' => [ 'importupload' => true, 'import' => true ],
@@ -9761,6 +9765,7 @@ $wgConf->settings = [
 			'move-subpages' => false, // for now...
 			'movefile' => false, // r93871 CR
 			'editcontentmodel' => false, // temp, pending T85847
+			'sendemail-new-users' => false,
 		],
 		'autoconfirmed' => [
 			'reupload' => true,
