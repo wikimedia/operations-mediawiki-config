21407db0cf8ae492c5e1b22279948274cf23169b
Marostegui <marostegui@wikimedia.org>
2017-12-18 09:38:24 +0000

    Revert "db-eqiad.php: Depool db1073"
    This reverts commit 95648cd3ef98c69475772208f2bee4f2c5e8f8bf.
    
    Change-Id: I3472d6169ba68cbc47ba04c8f0c4c3beb043b4e5

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -109,7 +109,7 @@ $wgLBFactoryConf = [
 		# 'db1055' => 1,   # C2 2.8TB  96GB, watchlist, recentchanges, contributions, logpager # T178359
 		'db1065' => 0,   # D1 2.8TB 160GB, vslow, dump, master for sanitarium
 		'db1066' => 50,  # D1 2.8TB 160GB, api
-		# 'db1073' => 50,  # B3 2.8TB 160GB, api # T174569
+		'db1073' => 50,  # B3 2.8TB 160GB, api
 		'db1080' => 300, # A2 3.6TB 512GB, api
 		'db1083' => 500, # B1 3.6TB 512GB
 		'db1089' => 500, # C3 3.6TB 512GB
@@ -296,7 +296,7 @@ $wgLBFactoryConf = [
 		],
 		'api' => [
 			'db1080' => 1,
-			# 'db1073' => 1,
+			'db1073' => 1,
 			'db1066' => 1,
 		],
 	],
