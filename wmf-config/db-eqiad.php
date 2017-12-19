936d14553af9bf63e20bbcaa3c45abd54db23bf0
Manuel Arostegui <marostegui@wikimedia.org>
2017-12-19 16:03:39 +0100

    db-eqiad.php: Depool db1109, db1099:3318
    Bug: T161294
    Change-Id: Ib966fbf81f13cc703ae290c265cc7874424d27a8

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -176,10 +176,10 @@ $wgLBFactoryConf = [
 		'db1063' => 0,   # C5 2.8TB 128GB, vslow, dump
 		'db1087' => 500, # C2 3.6TB 512GB, # db1095 master
 		'db1092' => 300, # D2 3.6TB 512GB, api
-		'db1099:3318' => 1,   # B2 3.6TB 512GB # rc, log: s1 and s8
+		# 'db1099:3318' => 1,   # B2 3.6TB 512GB # rc, log: s1 and s8 # T161294
 		'db1101:3318' => 1,   # C2 3.6TB 512GB # rc, log: s7 and s8
 		'db1104' => 300,  # B3 3.6TB 512GB, api
-		'db1109' => 500,  # D8 3.6TB 512GB
+		# 'db1109' => 500,  # D8 3.6TB 512GB # T161294
 	],
 
 	'silver' => [
@@ -499,23 +499,23 @@ $wgLBFactoryConf = [
 			'db1104' => 1,
 		],
 		'watchlist' => [
-			'db1099:3318' => 1,
+			# 'db1099:3318' => 1,
 			'db1101:3318' => 1,
 		],
 		'recentchanges' => [
-			'db1099:3318' => 1,
+			# 'db1099:3318' => 1,
 			'db1101:3318' => 1,
 		],
 		'recentchangeslinked' => [
-			'db1099:3318' => 1,
+			# 'db1099:3318' => 1,
 			'db1101:3318' => 1,
 		],
 		'contributions' => [
-			'db1099:3318' => 1,
+			# 'db1099:3318' => 1,
 			'db1101:3318' => 1,
 		],
 		'logpager' => [
-			'db1099:3318' => 1,
+			# 'db1099:3318' => 1,
 			'db1101:3318' => 1,
 		],
 	],
