6b0accdb801d06eb6e76d3ed54a26151988d0f3a
Martin Urbanec <martin.urbanec@wikimedia.cz>
2017-12-26 16:42:23 +0000

    Initial configuration for lfnwiki
    Bug: T183561
    Change-Id: Ibd314fc231cc18f5b6f35945fa19b1a5115a6d36

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -2319,6 +2319,7 @@ $wgConf->settings = [
 	'lbewiki' => 'Википедия',
 	'legalteamwiki' => 'Legal Team Wiki',
 	'lezwiki' => 'Википедия',
+	'lfnwiki' => 'Vicipedia',
 	'liwikibooks' => 'Wikibeuk',
 	'liwikisource' => 'Wikibrónne',
 	'loginwiki' => 'Wikimedia Login Wiki',
