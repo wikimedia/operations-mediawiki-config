aca897ce340dc9a98ce3567d1df4cb4328243914
Manuel Arostegui <marostegui@wikimedia.org>
2018-01-10 16:58:13 +0100

    db-eqiad.php: Depool db1096:3315
    Needs alter table
    
    Bug: T174569
    Change-Id: I61a7cfe897477d012b93ea0c57496e8dc87d53bb

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -143,7 +143,7 @@ $wgLBFactoryConf = [
 		'db1070' => 0,   # D1 2.8TB 160GB, master
 		'db1051' => 0,   # B3 2.8TB  96GB, vslow, dump in s5
 		'db1082' => 300, # A2 3.6TB 512GB, api # db1095 master
-		'db1096:3315' => 1,   # A6 3.6TB 512GB, # rc, log: s5 and s6
+		# 'db1096:3315' => 1,   # A6 3.6TB 512GB, # rc, log: s5 and s6 # T174569
 		'db1097:3315' => 1,   # D1 3.6TB 512GB, # rc, log: s4 and s5
 		'db1100' => 50,  # C2 3.6TB 512GB, old master #api
 		'db1106' => 500, # D3 3.6TB 512GB
@@ -390,23 +390,23 @@ $wgLBFactoryConf = [
 			'db1100' => 3,
 		],
 		'watchlist' => [
-			'db1096:3315' => 1,
+			# 'db1096:3315' => 1,
 			'db1097:3315' => 1,
 		],
 		'recentchanges' => [
-			'db1096:3315' => 1,
+			# 'db1096:3315' => 1,
 			'db1097:3315' => 1,
 		],
 		'recentchangeslinked' => [
-			'db1096:3315' => 1,
+			# 'db1096:3315' => 1,
 			'db1097:3315' => 1,
 		],
 		'contributions' => [
-			'db1096:3315' => 1,
+			# 'db1096:3315' => 1,
 			'db1097:3315' => 1,
 		],
 		'logpager' => [
-			'db1096:3315' => 1,
+			# 'db1096:3315' => 1,
 			'db1097:3315' => 1,
 		],
 	],
