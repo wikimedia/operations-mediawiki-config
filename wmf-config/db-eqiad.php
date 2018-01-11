71aaf6411c0ce125919a4a69ef6fb318a1d36b2f
Manuel Arostegui <marostegui@wikimedia.org>
2018-01-11 13:05:02 +0100

    db-eqiad.php: Repool db1099:3318 with low weight
    After rebooting it for kernel upgrade
    
    Change-Id: I911c8fe3d09c86ff76235896df03aacc79a1eb3c

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -172,7 +172,7 @@ $wgLBFactoryConf = [
 		'db1063' => 0,   # C5 2.8TB 128GB, vslow, dump
 		'db1087' => 500, # C2 3.6TB 512GB, # db1095 master
 		'db1092' => 300, # D2 3.6TB 512GB, api
-		# 'db1099:3318' => 1,   # B2 3.6TB 512GB # rc, log: s1 and s8
+		'db1099:3318' => 1,   # B2 3.6TB 512GB # rc, log: s1 and s8
 		'db1101:3318' => 1,   # C2 3.6TB 512GB # rc, log: s7 and s8
 		'db1104' => 300,  # B3 3.6TB 512GB, api
 		'db1109' => 500,  # D8 3.6TB 512GB
@@ -485,7 +485,7 @@ $wgLBFactoryConf = [
 			'db1104' => 1,
 		],
 		'watchlist' => [
-			# 'db1099:3318' => 1,
+			'db1099:3318' => 1,
 			'db1101:3318' => 1,
 		],
 		'recentchanges' => [
