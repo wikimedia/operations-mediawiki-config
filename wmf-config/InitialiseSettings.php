5db3799e8b171650a028558bcd1cb0406cb81957
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-01-13 18:33:45 +0100

    Whitelist audiovis.nac.gov.pl
    Bug: T184853
    Change-Id: I878ec868f8fd0898735027ab612d6845bd100101

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -13298,7 +13298,8 @@ $wgConf->settings = [
 		'aleph500.biblacad.ro',             // Domain of the Romanian academy - T178753
 		'images.collection.cooperhewitt.org', // Cooper Hewitt - T180241
 		'*.nli.org.il',                     // National Library of Israel - T1881179
-		'studiezaal.nijmegen.nl'            // Regionaal Archief Nijmegen, NL - T181713
+		'studiezaal.nijmegen.nl',           // Regionaal Archief Nijmegen, NL - T181713
+		'audiovis.nac.gov.pl',              // Zbiori, Poland goverment - T184853
 	],
 ],
 
