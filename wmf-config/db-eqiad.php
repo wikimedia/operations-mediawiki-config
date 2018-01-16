2dbe5f648139b9081ca1a47a53321aae3358d061
Manuel Arostegui <marostegui@wikimedia.org>
2018-01-16 17:46:58 +0100

    db-eqiad.php: Increase weight for db1092
    Change-Id: Iee276ad90bd570154b9351deca7671ffd0373d56

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -174,7 +174,7 @@ $wgLBFactoryConf = [
 		'db1092' => 200, # D2 3.6TB 512GB, api # T174569
 		'db1099:3318' => 1,   # B2 3.6TB 512GB # rc, log: s1 and s8
 		# 'db1101:3318' => 1,   # C2 3.6TB 512GB # rc, log: s7 and s8 # T174569
-		'db1104' => 200,  # B3 3.6TB 512GB, api
+		'db1104' => 300,  # B3 3.6TB 512GB, api
 		'db1109' => 500,  # D8 3.6TB 512GB
 	],
 
@@ -481,7 +481,7 @@ $wgLBFactoryConf = [
 			'db1087' => 1,
 		],
 		'api' => [
-			 'db1092' => 1,
+			 'db1092' => 2,
 			'db1104' => 1,
 		],
 		'watchlist' => [
