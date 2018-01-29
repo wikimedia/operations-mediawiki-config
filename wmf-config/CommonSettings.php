b92117f2b1108900c635dd6219c5850530ef356a
MarcoAurelio <maurelio@tools.wmflabs.org>
2018-01-29 19:40:25 +0000

    Revert "Remove upload rights on wikis where local uploads are disabled"
    For some reason the permission changes ain't working.
    
    This reverts commit 8c98f79462fb1bdc3aacfef8b5eec267498eaf79.
    
    Change-Id: Iba06eecccd034355dd317c7d8fa0cccda3462e5e

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -197,7 +197,7 @@ if ( !$globals ) {
 			'nowikidatadescriptiontaglines',
 			'related-articles-footer-blacklisted-skins',
 			'top6-wikipedia', 'rtl',
-			'pp_stage0', 'pp_stage1', 'uploadsdisabled'
+			'pp_stage0', 'pp_stage1'
 		] as $tag ) {
 		$dblist = MWWikiversions::readDbListFile( $tag );
 		if ( in_array( $wgDBname, $dblist ) ) {
