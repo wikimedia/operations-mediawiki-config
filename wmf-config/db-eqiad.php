702d55e15622dc47b300d23f75ce414c02ce3972
Manuel Arostegui <marostegui@wikimedia.org>
2018-02-02 14:48:55 +0100

    db-eqiad.php: Clarifying that db1100 status
    db1100 is the candidate master for s5 in case of master failure
    
    Bug: T186321
    Change-Id: Ife23783fadcc4a5be5d7be8304eef0d25313255a

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -145,7 +145,7 @@ $wgLBFactoryConf = [
 		'db1082' => 300, # A2 3.6TB 512GB, api # master for db1095, temporary vslow
 		'db1096:3315' => 1,   # A6 3.6TB 512GB, # rc, log: s5 and s6
 		'db1097:3315' => 1,   # D1 3.6TB 512GB, # rc, log: s4 and s5
-		'db1100' => 50,  # C2 3.6TB 512GB, old master #api
+		'db1100' => 50,  # C2 3.6TB 512GB, old master #api # candidate master
 		'db1106' => 500, # D3 3.6TB 512GB
 		'db1110' => 500, # C3 3.6TB 512GB
 	],
