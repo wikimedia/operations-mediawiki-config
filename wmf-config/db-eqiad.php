6f9081ba243b8ed19599a086f209d412cb716ead
Manuel Arostegui <marostegui@wikimedia.org>
2018-02-06 07:53:59 +0100

    db-eqiad.php: Depool db1077
    Going to upgrade MariaDB, kernel and socket location.
    
    Change-Id: I2f1acc7085fe39ea6c9239d89b3055dd11d770fe

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -127,7 +127,7 @@ $wgLBFactoryConf = [
 	/* s3 */ 'DEFAULT' => [
 		'db1075' => 0,   # A2 3.6TB 512GB, master
 		'db1072' => 50,  # B2 2.8TB 160GB, vslow, dump, old master # master for db1095
-		'db1077' => 400, # B1 3.6TB 512GB, watchlist, recentchanges, contributions, logpager
+		# 'db1077' => 400, # B1 3.6TB 512GB, watchlist, recentchanges, contributions, logpager
 		'db1078' => 500, # C3 3.6TB 512GB # candidate master
 	],
 	's4' => [
@@ -330,19 +330,19 @@ $wgLBFactoryConf = [
 			'db1072' => 1,
 		],
 		'watchlist' => [
-			'db1077' => 1,
+			'db1078' => 1,
 		],
 		'recentchanges' => [
-			'db1077' => 1,
+			'db1078' => 1,
 		],
 		'recentchangeslinked' => [
-			'db1077' => 1,
+			'db1078' => 1,
 		],
 		'contributions' => [
-			'db1077' => 1,
+			'db1078' => 1,
 		],
 		'logpager' => [
-			'db1077' => 1,
+			'db1078' => 1,
 		],
 	],
 	's4' => [
