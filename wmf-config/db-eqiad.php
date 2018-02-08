660281562ce588466d78d46ce1afe694de1aebe0
Manuel Arostegui <marostegui@wikimedia.org>
2018-02-08 14:02:45 +0100

    db-eqiad.php: Depool db1051
    Bug: T184599
    Change-Id: I47e284dea00763b9ef2af4fae6e9b5c4707f36d3

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -141,8 +141,8 @@ $wgLBFactoryConf = [
 	],
 	's5' => [
 		'db1070' => 0,   # D1 2.8TB 160GB, master
-		'db1051' => 0,   # B3 2.8TB  96GB, vslow, dump in s5
-		'db1082' => 300, # A2 3.6TB 512GB, api # master for db1095
+		# 'db1051' => 0,   # B3 2.8TB  96GB, vslow, dump in s5  # T184599
+		'db1082' => 300, # A2 3.6TB 512GB, api # master for db1095 # temporary vslow T184599
 		'db1096:3315' => 1,   # A6 3.6TB 512GB, # rc, log: s5 and s6
 		'db1097:3315' => 1,   # D1 3.6TB 512GB, # rc, log: s4 and s5
 		'db1100' => 50,  # C2 3.6TB 512GB, old master #api # candidate master
@@ -379,10 +379,10 @@ $wgLBFactoryConf = [
 	],
 	's5' => [
 		'vslow' => [
-			'db1051' => 1,
+			'db1082' => 1,
 		],
 		'dump' => [
-			'db1051' => 1,
+			'db1082' => 1,
 		],
 		'api' => [
 			'db1082' => 1,
