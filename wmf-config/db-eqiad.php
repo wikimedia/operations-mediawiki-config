d4787835eda71afcaca424e6cb8483ff7252a86a
Manuel Arostegui <marostegui@wikimedia.org>
2018-02-08 12:20:39 +0100

    db-eqiad.php: Depool db1096:3315
    Dropping wikidatawiki takes around 10 minutes and makes the slaves lag,
    depool it
    
    Bug: T184599
    Change-Id: Ia51ce6cdf49e3e6c2aabea44bec9820e459ecc25

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -143,7 +143,7 @@ $wgLBFactoryConf = [
 		'db1070' => 0,   # D1 2.8TB 160GB, master
 		'db1051' => 0,   # B3 2.8TB  96GB, vslow, dump in s5
 		'db1082' => 300, # A2 3.6TB 512GB, api # master for db1095
-		'db1096:3315' => 1,   # A6 3.6TB 512GB, # rc, log: s5 and s6
+		# 'db1096:3315' => 1,   # A6 3.6TB 512GB, # rc, log: s5 and s6 # T184599
 		'db1097:3315' => 1,   # D1 3.6TB 512GB, # rc, log: s4 and s5
 		'db1100' => 50,  # C2 3.6TB 512GB, old master #api # candidate master
 		'db1106' => 500, # D3 3.6TB 512GB
@@ -389,23 +389,23 @@ $wgLBFactoryConf = [
 			 'db1100' => 3,
 		],
 		'watchlist' => [
-			'db1096:3315' => 1,
+			# 'db1096:3315' => 1,
 			'db1097:3315' => 1,
 		],
 		'recentchanges' => [
-			'db1096:3315' => 1,
+			# 'db1096:3315' => 1,
 			'db1097:3315' => 1,
 		],
 		'recentchangeslinked' => [
-			'db1096:3315' => 1,
+			# 'db1096:3315' => 1,
 			'db1097:3315' => 1,
 		],
 		'contributions' => [
-			'db1096:3315' => 1,
+			# 'db1096:3315' => 1,
 			'db1097:3315' => 1,
 		],
 		'logpager' => [
-			'db1096:3315' => 1,
+			# 'db1096:3315' => 1,
 			'db1097:3315' => 1,
 		],
 	],
