9452896073b23badf90cc5273ba75671b6c3a368
Manuel Arostegui <marostegui@wikimedia.org>
2018-02-12 07:59:02 +0100

    db-eqiad.php: Slowly repool db1109
    Kernel has been upgraded
    
    Change-Id: Ie4d7a43d56eae58a8a4d12eba7de360ac08eedab

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -174,7 +174,7 @@ $wgLBFactoryConf = [
 		'db1099:3318' => 1,   # B2 3.6TB 512GB # rc, log: s1 and s8
 		'db1101:3318' => 1,   # C2 3.6TB 512GB # rc, log: s7 and s8
 		'db1104' => 300,  # B3 3.6TB 512GB, api
-		# 'db1109' => 500,  # D8 3.6TB 512GB # T184599
+		'db1109' => 100,  # D8 3.6TB 512GB
 	],
 
 	'silver' => [
