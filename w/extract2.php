cbb2bd83afd15203f6f3be44514533121153034a
Timo Tijhof <krinklemail@gmail.com>
2018-02-12 22:06:15 -0800

    extract2: Set wiki context directly instead of MW_LANG indirection
    The current flow depended on:
    * Client request (through Varnish) coming in with hostname www.wikipedia.org or
      some other wikipedia.org subdomain.
    * URL rewritten by Apache config from "/" to "/w/extract2.php".
    * This file setting MW_LANG=meta
    * MWMultiVersion::getMediaWiki( $file, $wiki = false )
      -> initializeForWiki( $_SERVER['SERVER_NAME'] )
      -> setSiteInfoForWiki()
      -> $site = "wikipedia";
      -> $lang = getenv( 'MW_LANG' ) ?: $subdomain [..]
    * MWMultiVersion::loadDBFromSite( $site, $lang )
      -> $dbSuffix = $site === "wikipedia" ? "wiki" : [..]
      -> $this->db = str_replace( "-", "_", $lang . $dbSuffix );
    
    Instead, just set $wiki = 'metawiki' directly.
    
    Bonus: If this works, we can remove support for MW_LANG, given this
    file is the last place that uses it.
    
    Change-Id: Ib6d77e863bede071036bc768ed9cbb553ba69ee4

--- w/extract2.php
+++ w/extract2.php
@@ -1,9 +1,6 @@
 <?php
-$lang = 'meta';
-putenv( "MW_LANG={$lang}" ); // notify MWMultiVersion
-
 require_once __DIR__ . '/../multiversion/MWMultiVersion.php';
-require MWMultiVersion::getMediaWiki( 'includes/WebStart.php' );
+require MWMultiVersion::getMediaWiki( 'includes/WebStart.php', 'metawiki' );
 
 $wgTitle = Title::newFromText( 'API_listing_template' );
 $article = new Article( $wgTitle );
