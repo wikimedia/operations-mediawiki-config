be5242e979459dfda2d36039b5feebd2f4ef7a7b
Marostegui <marostegui@wikimedia.org>
2018-02-13 09:23:38 +0000

    Revert "db-eqiad.php: Depool db1065"
    This reverts commit a6f0a4236f748296e0a87750b3e9b8fd1fbbac5f.
    
    Change-Id: Ib3b2babc09e7ca62dab1ea69f8ce5f954c8da7c6

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -104,8 +104,8 @@ $wgLBFactoryConf = [
 'sectionLoads' => [
 	's1' => [
 		'db1052' => 0,   # B3 2.8TB  96GB, master
-		 'db1067' => 0,  # D1 2.8TB 160GB, old master # temporary vslow
-		# 'db1065' => 0,   # D1 2.8TB 160GB, vslow, dump # master for db1095 # T162807
+		 'db1067' => 50,  # D1 2.8TB 160GB, old master
+		'db1065' => 0,   # D1 2.8TB 160GB, vslow, dump # master for db1095
 		'db1066' => 50,  # D1 2.8TB 160GB, api
 		'db1073' => 50,  # B3 2.8TB 160GB, api
 		'db1080' => 300, # A2 3.6TB 512GB, api
@@ -279,10 +279,10 @@ $wgLBFactoryConf = [
 			'db1105:3311' => 1,
 		],
 		'dump' => [
-			'db1067' => 1,
+			'db1065' => 1,
 		],
 		'vslow' => [
-			'db1067' => 1,
+			'db1065' => 1,
 		],
 		'api' => [
 			'db1080' => 1,
