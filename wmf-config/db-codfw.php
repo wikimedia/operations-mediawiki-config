6e1de43320b7399d068edd49bd72a67f6430dc82
Marostegui <marostegui@wikimedia.org>
2018-02-13 07:59:09 +0000

    Revert "db-eqiad.php: Depool db2089:3315"
    This reverts commit 5585cd4d9b24bb2d55b5a759086bcbe5f9fc50eb.
    
    Change-Id: Ia4718e104cf084ca872edfb2028e7fdec3434e78

--- wmf-config/db-codfw.php
+++ wmf-config/db-codfw.php
@@ -148,7 +148,7 @@ $wgLBFactoryConf = [
 		'db2066'      => 400, # D6 3.3TB 160GB
 		'db2075'      => 400, # A1 3.3TB 512GB # Compressed InnoDB
 		'db2084:3315' => 1, # D6 3.3TB 512GB # rc, log: s4 and s5
-		# 'db2089:3315' => 1, # A3 3.3TB 512GB # rc, log: s5 and s6 # alter table
+		'db2089:3315' => 1, # A3 3.3TB 512GB # rc, log: s5 and s6
 	],
 	's6' => [
 		'db2039'      => 0,   # C6 2.9TB 160GB, master
@@ -384,23 +384,23 @@ $wgLBFactoryConf = [
 	's5' => [
 		'watchlist' => [
 			'db2084:3315' => 1,
-			# 'db2089:3315' => 1,
+			'db2089:3315' => 1,
 		],
 		'recentchanges' => [
 			'db2084:3315' => 1,
-			# 'db2089:3315' => 1,
+			'db2089:3315' => 1,
 		],
 		'recentchangeslinked' => [
 			'db2084:3315' => 1,
-			# 'db2089:3315' => 1,
+			'db2089:3315' => 1,
 		],
 		'contributions' => [
 			'db2084:3315' => 1,
-			# 'db2089:3315' => 1,
+			'db2089:3315' => 1,
 		],
 		'logpager' => [
 			'db2084:3315' => 1,
-			# 'db2089:3315' => 1,
+			'db2089:3315' => 1,
 		],
 		'dump' => [
 			'db2038' => 1,
