daf9a059e480f04c9c896bc2df55aa04d599c0c0
Manuel Arostegui <marostegui@wikimedia.org>
2018-02-15 18:01:42 +0100

    db-eqiad.php: Repool db1051
    After alter table
    
    Change-Id: I54230eb6cef233337d7b4be86d31bf9f3f97597e

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -141,8 +141,8 @@ $wgLBFactoryConf = [
 	],
 	's5' => [
 		'db1070' => 0,      # D1 2.8TB 160GB, master
-		# 'db1051' => 0,      # B3 2.8TB  96GB, vslow, dump in s5 # alter table
-		'db1082' => 0,    # A2 3.6TB 512GB, api # master for db1095 # temporary vslow
+		'db1051' => 0,      # B3 2.8TB  96GB, vslow, dump in s5
+		'db1082' => 300,    # A2 3.6TB 512GB, api # master for db1095
 		'db1096:3315' => 1, # A6 3.6TB 512GB, # rc, log: s5 and s6
 		'db1097:3315' => 1, # D1 3.6TB 512GB, # rc, log: s4 and s5
 		'db1100' => 50,     # C2 3.6TB 512GB, old master #api # candidate master
@@ -379,13 +379,13 @@ $wgLBFactoryConf = [
 	],
 	's5' => [
 		'vslow' => [
-			'db1082' => 1,
+			'db1051' => 1,
 		],
 		'dump' => [
-			'db1082' => 1,
+			'db1051' => 1,
 		],
 		'api' => [
-			# 'db1082' => 1,
+			'db1082' => 1,
 			'db1100' => 3,
 		],
 		'watchlist' => [
