b92d029c211b87c98618f1b143130c6d35024434
Chad Horohoe <chadh@wikimedia.org>
2018-02-16 08:16:40 -0800

    Remove indirection from search-redirect.php
    The only place this exists and is referenced from is the www portals.
    Move it to that docroot directly instead of using a pointless symlink.
    
    Change-Id: If9afac5636f9583454322fcb8f5092f296bc24bd

--- docroot/wwwportal/w/search-redirect.php
+++ docroot/wwwportal/w/search-redirect.php
@@ -1,1 +1,49 @@
-../../../search-redirect.php
+<?php
+// get params
+$language = isset( $_GET['language'] ) ? $_GET['language'] : '';
+$search   = isset( $_GET['search'] ) ? $_GET['search'] : '';
+$fulltext = isset( $_GET['fulltext'] ) ? $_GET['fulltext'] : false;
+$go       = isset( $_GET['go'] ) ? $_GET['go'] : false;
+$family   = isset( $_GET['family'] ) ? $_GET['family'] : 'wikipedia';
+
+if ( ( isset( $_SERVER['HTTP_X_FORWARDED_PROTO'] ) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https' )
+	|| ( isset( $_SERVER['HTTPS'] ) && $_SERVER['HTTPS'] === 'on' )
+) {
+	$proto = 'https';
+} else {
+	$proto = 'http';
+}
+
+// validate $language
+if ( !preg_match( '/^[a-zA-Z\-]*$/', $language ) ) {
+	$language = 'en';
+}
+
+// validate $family
+$sites = [
+	'wikipedia',
+	'wiktionary',
+	'wikisource',
+	'wikinews',
+	'wikiversity',
+	'wikimedia',
+	'wikiquote',
+	'wikibooks',
+	'wikivoyage',
+];
+
+if ( !in_array( $family, $sites ) ) {
+	$family = 'wikipedia';
+}
+
+// make url
+$url = "$proto://" . $language . '.' . $family . '.org/wiki/Special:Search?search=' . urlencode( $search );
+if ( $fulltext ) {
+	$url .= '&fulltext=Search';
+}
+if ( $go ) {
+	$url .= '&go=Go';
+}
+
+// Redirect
+header( "Location: {$url}" );
