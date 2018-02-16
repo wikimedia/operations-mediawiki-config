fdd91920809bae44cf81b4abb6797ed8caea6175
Manuel Arostegui <marostegui@wikimedia.org>
2018-02-16 12:24:45 +0100

    db-eqiad.php: Increase traffic for db1093
    Change-Id: Ib07fdf1740182647e89732de2b86315a37876f43

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -153,7 +153,7 @@ $wgLBFactoryConf = [
 		'db1063' => 0,      # C5 2.8TB 128GB, vslow, dump # MariaDB 10.1
 		'db1085' => 200,    # B3 3.6TB 512GB, api #master for db1102 (sanitarium 3)
 		'db1088' => 500,    # C2 3.6TB 512GB
-		'db1093' => 100,    # D2 3.6TB 512GB, api
+		'db1093' => 200,    # D2 3.6TB 512GB, api
 		'db1096:3316' => 1, # A6 3.6TB 512GB, # rc, log: s5 and s6
 		'db1098:3316' => 1, # B5 3.6TB 512GB, # rc, log: s6 and s7
 	],
@@ -417,7 +417,7 @@ $wgLBFactoryConf = [
 		],
 		'api' => [
 			'db1085' => 10,
-			# 'db1093' => 1,
+			'db1093' => 1,
 		],
 		'watchlist' => [
 			'db1096:3316' => 1,
