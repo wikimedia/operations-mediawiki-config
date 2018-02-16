409570002a439105c9ca5972cd979569ac23d288
Manuel Arostegui <marostegui@wikimedia.org>
2018-02-16 12:37:35 +0100

    db-eqiad.php: Increase weight for db1093
    Also restore original weight for db1085
    
    Change-Id: Ia963758c6d071c4cfefbdae5e46c432c5be17174

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -151,9 +151,9 @@ $wgLBFactoryConf = [
 	's6' => [
 		'db1061' => 0,      # C3 2.8TB 128GB, master
 		'db1063' => 0,      # C5 2.8TB 128GB, vslow, dump # MariaDB 10.1
-		'db1085' => 200,    # B3 3.6TB 512GB, api #master for db1102 (sanitarium 3)
+		'db1085' => 300,    # B3 3.6TB 512GB, api #master for db1102 (sanitarium 3)
 		'db1088' => 500,    # C2 3.6TB 512GB
-		'db1093' => 200,    # D2 3.6TB 512GB, api
+		'db1093' => 300,    # D2 3.6TB 512GB, api
 		'db1096:3316' => 1, # A6 3.6TB 512GB, # rc, log: s5 and s6
 		'db1098:3316' => 1, # B5 3.6TB 512GB, # rc, log: s6 and s7
 	],
