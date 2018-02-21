1359d838d5bd1a39a3d80dd1d20a4c70d2018d8e
Gilles <gdubuc@wikimedia.org>
2018-02-21 14:27:37 +0000

    Revert "Add Thumbor/Mediawiki shared secret"
    This reverts commit ddbaf082ad5a4762314fb19b2d564825c560cb0e.
    
    Change-Id: I9bf5c2c5915503029143648c63c2ce317da19d2a

--- wmf-config/filebackend.php
+++ wmf-config/filebackend.php
@@ -225,7 +225,6 @@ $wgLocalFileRepo = [
 	'thumbProxyUrl'     => $wmgPrivateWiki
 		? false
 		: ( $wmfSwiftConfig[$specificDC]['thumborUrl'] . '/' . $site . '/' . $lang . '/thumb/' ),
-	'thumbProxySecret'  => $wmfSwiftConfig[$specificDC]['thumborSecret'],
 	'zones'             => $wmgPrivateWiki
 		? [
 			'thumb' => [ 'url' => "$wgScriptPath/thumb_handler.php" ] ]
