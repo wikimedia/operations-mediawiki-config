a0d276e1336597c6d3998736372b40d764499b8a
Manuel Arostegui <marostegui@wikimedia.org>
2018-02-22 11:06:03 +0100

    db-eqiad.php: Increase traffic for db1104
    Change-Id: I9a1afbd49273a1af18a8f4db530297092e147adf

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -172,7 +172,7 @@ $wgLBFactoryConf = [
 		'db1092' => 100,    # D2 3.6TB 512GB, api
 		'db1099:3318' => 1, # B2 3.6TB 512GB # rc, log: s1 and s8
 		'db1101:3318' => 1, # C2 3.6TB 512GB # rc, log: s7 and s8
-		'db1104' => 100,    # B3 3.6TB 512GB, api # candidate master
+		'db1104' => 200,    # B3 3.6TB 512GB, api # candidate master
 		'db1109' => 500,    # D8 3.6TB 512GB
 	],
 
