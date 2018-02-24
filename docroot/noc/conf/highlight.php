1f131b79040316929268a821df28f015fbe205ea
Chad Horohoe <chadh@wikimedia.org>
2018-02-23 17:44:25 -0800

    highlight.php: Don't use the escaped URL for the raw URL either
    Change-Id: Ib8b3ebc131e67bb6b20336762ea86715c527d4a7

--- docroot/noc/conf/highlight.php
+++ docroot/noc/conf/highlight.php
@@ -81,7 +81,6 @@ if ( !$selectedFilePath ) {
 }
 
 $selectedFileNameEsc = htmlspecialchars( $selectedFileName );
-$selectedFileViewRawUrlEsc = htmlspecialchars( $selectedFileViewRawUrl );
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -98,7 +97,7 @@ if ( $selectedFilePath !== false ) {
 <p>(
 <a href="https://gerrit.wikimedia.org/g/operations/mediawiki-config/+log/master/<?php echo $selectedFileRepoPath; ?>">version control</a> &bull;
 <a href="https://gerrit.wikimedia.org/g/operations/mediawiki-config/+blame/master/<?php echo $selectedFileRepoPath; ?>?blame=1">blame</a> &bull;
-<a href="<?php echo $selectedFileViewRawUrlEsc; ?>">raw text</a>
+<a href="<?php echo $selectedFileViewRawUrl; ?>">raw text</a>
 )</p>
 <?php
 }
