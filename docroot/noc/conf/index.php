5ba9b7560116ee4d05ef1eefca84d1c36b5b46b9
Chad Horohoe <chadh@wikimedia.org>
2018-02-26 12:09:51 -0800

    noc.wm.o: Stop urlencoding filenames
    Change-Id: I7832cabb3db8a9471abf7a62e3a5d736b56703fc

--- docroot/noc/conf/index.php
+++ docroot/noc/conf/index.php
@@ -13,11 +13,11 @@
 			echo "\n<li>";
 
 			if ( $highlight ) {
-				echo '<a href="./highlight.php?file=' . htmlspecialchars( urlencode( $srcFilename ) ) . '">'
+				echo '<a href="./highlight.php?file=' . htmlspecialchars( $srcFilename ) . '">'
 					. htmlspecialchars( $srcFilename );
-				echo '</a> (<a href="./' . htmlspecialchars( urlencode( $viewFilename ) ) . '">raw text</a>)';
+				echo '</a> (<a href="./' . htmlspecialchars( $viewFilename ) . '">raw text</a>)';
 			} else {
-				echo '<a href="./' . htmlspecialchars( urlencode( $viewFilename ) ) . '">'
+				echo '<a href="./' . htmlspecialchars( $viewFilename ) . '">'
 					. htmlspecialchars( $srcFilename ) . '</a>';
 			}
 			echo '</li>';
