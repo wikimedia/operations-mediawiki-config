2bd66cea40aa89b91a94b3fbd8d015cf5537e6b8
Petr Pchelko <ppchelko@wikimedia.org>
2018-02-26 17:04:37 -0300

    [JoqbQueue] Switch refreshLinks for all but wikipedia and wiktionary.
    Bug: T185052
    Change-Id: If34d94f499e800fb55909b89f729508b3098da6f

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -20137,12 +20137,9 @@ $wgConf->settings = [
 ],
 
 'wmgDisableRefreshLinksInRedis' => [
-	'testwiki' => true,
-	'test2wiki' => true,
-	'testwikidatawiki' => true,
-	'mediawikiwiki' => true,
-	'ruwikivoyage' => true,
-	'default' => false,
+	'wikipedia' => false,
+	'wiktionary' => false,
+	'default' => true,
 ],
 
 'wmgUseCapiunto' => [
