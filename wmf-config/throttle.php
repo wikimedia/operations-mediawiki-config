9d805f0bc13757596c1fc838e441c441bbe4d294
Gergő Tisza <tgr.huwiki@gmail.com>
2018-03-01 10:51:43 -0800

    Temporary account creation limit raise for outreach dashboard
    ...for a day, until they fix OAuth permissions to include no
    rate limiting for editathon organizers.
    
    Bug: T188630
    Change-Id: I2c3c0e8642ad38352c6ee118611880ead9efbe60

--- wmf-config/throttle.php
+++ wmf-config/throttle.php
@@ -90,4 +90,11 @@ $wmgThrottlingExceptions[] = [ // T188529
 	'value' => 60 // 50 expected
 ];
 
+$wmgThrottlingExceptions[] = [ // T188630
+	'from' => '2018-02-01T11:00 -8:00',
+	'to' => '2018-02-02T00:00 -8:00',
+	'IP' => '10.68.16.172', # outreachdashboard.wmflabs.org
+	'value' => 100,
+];
+
 ## Add throttling definitions above.
