23d7c9da6abbf12df77ea0d514e3aa2ebd315e5f
Gergő Tisza <tgr.huwiki@gmail.com>
2018-03-07 17:49:44 -0800

    Add throttle exception for ptwiki editathon
    Also remove some old ones.
    
    Bug: T189161
    Change-Id: I1bc5c3105b3cc1cf72846822c44b78e13665cc94

--- wmf-config/throttle.php
+++ wmf-config/throttle.php
@@ -26,6 +26,14 @@ $wmgThrottlingExceptions = [];
 # ];
 ## Add throttling definitions below.
 
+$wmgThrottlingExceptions[] = [ // T189161
+	'from' => '2018-03-08T12:00:00 UTC',
+	'to' => '2018-03-08T21:00:00 UTC',
+	'IP' => '177.15.131.174',
+	'dbname' => [ 'ptwiki' ],
+	'value' => 60 // 40 expected
+];
+
 $wmgThrottlingExceptions[] = [ // T185930
 	'from' => '2018-03-10T11:00:00 -5:00',
 	'to' => '2018-03-10T16:00:00 -5:00',
@@ -39,14 +47,6 @@ $wmgThrottlingExceptions[] = [ // T185794
 	'to' => '2018-03-04T18:00 -5:00',
 	'IP' => '198.179.69.250',
 	'dbname' => [ 'enwiki', 'eswiki' ],
-	'value' => 70 // 50 expected
-];
-
-$wmgThrottlingExceptions[] = [ // T185794
-	'from' => '2018-02-08T16:00 -7:00',
-	'to' => '2018-03-04T20:00 -7:00',
-	'IP' => '66.99.86.39',
-	'dbname' => [ 'enwiki' ],
 	'value' => 70 // 50 expected
 ];
 
@@ -66,14 +66,6 @@ $wmgThrottlingExceptions[] = [ // T187171
 	'value' => 50, // 20 expected
 ];
 
-$wmgThrottlingExceptions[] = [ // T188292
-	'from' => '2018-03-03T0:00 -5:00',
-	'to' => '2018-03-03T08:00 -5:00',
-	'IP' => [ '38.125.33.54', '38.125.15.118' ],
-	'dbname' => [ 'enwiki' ],
-	'value' => 130 // up to 100 expected
-];
-
 $wmgThrottlingExceptions[] = [ // T188034
 	'from' => '2018-03-06T0:00 UTC',
 	'to' => '2018-03-11T0:00 UTC',
@@ -88,13 +80,6 @@ $wmgThrottlingExceptions[] = [ // T188529
 	'IP' => '72.43.130.34',
 	'dbname' => [ 'enwiki' ],
 	'value' => 60 // 50 expected
-];
-
-$wmgThrottlingExceptions[] = [ // T188630
-	'from' => '2018-03-01T11:00 -8:00',
-	'to' => '2018-03-02T00:00 -8:00',
-	'IP' => '10.68.16.172', # outreachdashboard.wmflabs.org
-	'value' => 100,
 ];
 
 $wmgThrottlingExceptions[] = [ // T188626
