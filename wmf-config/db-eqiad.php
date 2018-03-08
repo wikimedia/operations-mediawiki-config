092573f5e0e838cc9aa79a3746038fa3869921a1
Marostegui <marostegui@wikimedia.org>
2018-03-08 07:03:35 +0000

    Revert "db-eqiad.php: Depool db1064"
    This reverts commit 922af582b1944b29e3f8394c7c85bd69ce9282eb.
    
    Change-Id: I533bc28cae7095a7bec572cfa9e81ac56f2367c7

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -131,9 +131,9 @@ $wgLBFactoryConf = [
 	],
 	's4' => [
 		'db1068' => 0,      # D1 2.8TB 160GB, master
-		# 'db1064' => 0,      # D1 2.8TB 160GB, vslow, dump # master for db1095 # T188685
-		'db1081' => 100,    # A2 3.6TB 512GB, api # candidate master # temporary vslow # T188685
-		'db1084' => 300,    # B1 3.6TB 512GB, api
+		'db1064' => 0,      # D1 2.8TB 160GB, vslow, dump # master for db1095
+		'db1081' => 300,    # A2 3.6TB 512GB, api # candidate master
+		'db1084' => 400,    # B1 3.6TB 512GB, api
 		'db1091' => 500,    # D2 3.6TB 512GB
 		'db1097:3314' => 1, # D1 3.6TB 512GB, # rc, log: s4 and s5
 		'db1103:3314' => 1, # A3 3.6TB 512GB # rc, log: s2 and s4
@@ -348,14 +348,14 @@ $wgLBFactoryConf = [
 	],
 	's4' => [
 		'vslow' => [
-			'db1081' => 1,
+			'db1064' => 1,
 		],
 		'dump' => [
-			'db1081' => 1,
+			'db1064' => 1,
 		],
 		'api' => [
-			'db1081' => 1,
-			'db1084' => 2,
+			'db1081' => 2,
+			'db1084' => 1,
 		],
 		'watchlist' => [
 			'db1097:3314' => 1,
