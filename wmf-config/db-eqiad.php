1298566f5385196158ef1757efdefc4460dc9374
Manuel Arostegui <marostegui@wikimedia.org>
2018-03-09 13:10:46 +0100

    db-eqiad.php: Depool db1063
    db1063 is going to be used to clone db1113:3316 which will serve as
    multiinstance vslow slave
    
    Bug: T184161
    Change-Id: I01bca9bef6e69e21a99299822389e71c131412dd

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -150,10 +150,10 @@ $wgLBFactoryConf = [
 	],
 	's6' => [
 		'db1061' => 0,      # C3 2.8TB 128GB, master
-		'db1063' => 0,      # C5 2.8TB 128GB, vslow, dump # MariaDB 10.1
-		'db1085' => 300,    # B3 3.6TB 512GB, api #master for db1102 (sanitarium 3)
+		# 'db1063' => 0,      # C5 2.8TB 128GB, vslow, dump # MariaDB 10.1 # cloning db1113
+		'db1085' => 100,    # B3 3.6TB 512GB, api #master for db1102 (sanitarium 3) # temporary vslow
 		'db1088' => 500,    # C2 3.6TB 512GB
-		'db1093' => 500,    # D2 3.6TB 512GB, api # candidate master
+		'db1093' => 300,    # D2 3.6TB 512GB, api # candidate master
 		'db1096:3316' => 1, # A6 3.6TB 512GB, # rc, log: s5 and s6
 		'db1098:3316' => 1, # B5 3.6TB 512GB, # rc, log: s6 and s7
 	],
@@ -412,13 +412,13 @@ $wgLBFactoryConf = [
 	],
 	's6' => [
 		'vslow' => [
-			'db1063' => 1,
+			'db1085' => 1,
 		],
 		'dump' => [
-			'db1063' => 1,
+			'db1085' => 1,
 		],
 		'api' => [
-			'db1085' => 10,
+			# 'db1085' => 10,
 			'db1093' => 1,
 		],
 		'watchlist' => [
