eeac988665b26822c9722b56eb06475e5e5a6fbe
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-03-12 09:32:39 +0000

    New throttle rule, clean expired rules
    Bug: T189442
    Change-Id: I15fdca9939b424b1b45a20159567302969d35f9f

--- wmf-config/throttle.php
+++ wmf-config/throttle.php
@@ -26,30 +26,6 @@ $wmgThrottlingExceptions = [];
 # ];
 ## Add throttling definitions below.
 
-$wmgThrottlingExceptions[] = [ // T185930
-	'from' => '2018-03-10T11:00:00 -5:00',
-	'to' => '2018-03-10T16:00:00 -5:00',
-	'range' => '38.125.10.42',
-	'dbname' => [ 'enwiki' ],
-	'value' => 30 // 20 expected
-];
-
-$wmgThrottlingExceptions[] = [ // T188034
-	'from' => '2018-03-06T0:00 UTC',
-	'to' => '2018-03-11T0:00 UTC',
-	'IP' => '93.242.192.25',
-	'dbname' => [ 'enwiki', 'frwiki', 'eswiki', 'arwiki', 'ruwiki', 'zhwiki' ],
-	'value' => 250, // 200 expected
-];
-
-$wmgThrottlingExceptions[] = [ // T188529
-	'from' => '2018-03-11T0:00 -5:00',
-	'to' => '2018-03-11T8:00 -5:00',
-	'IP' => '72.43.130.34',
-	'dbname' => [ 'enwiki' ],
-	'value' => 60 // 50 expected
-];
-
 $wmgThrottlingExceptions[] = [ // T188626
 	'from' => '2018-03-06T11:30 -6:00',
 	'to' => '2018-03-23T13:00 -6:00',
@@ -62,6 +38,14 @@ $wmgThrottlingExceptions[] = [ //
 	'to' => '2018-03-28T14:00 -7:00',
 	'IP' => '129.107.76.40',
 	'value' => 70 // 60 expected
+];
+
+$wmgThrottlingExceptions[] = [ // T189442
+	'from' => '2018-03-16T8:00 +5:30',
+	'to' => '2018-03-17T19:00 +5:30',
+	'IP' => [ '103.25.155.186', '103.216.147.34' ],
+	'dbname' => [ 'mrwiki', 'enwiki', 'commonswiki' ],
+	'value' => 50 // 35 expected
 ];
 
 ## Add throttling definitions above.
