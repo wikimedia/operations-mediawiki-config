d2d654f990da2e72880bd8b8aba8fc09d84e0993
Marostegui <marostegui@wikimedia.org>
2018-03-14 09:27:52 +0000

    Revert "db-codfw.php: Depool pc2004"
    This reverts commit 8416e1a584be2e4007a48d5c8602d1a67fdc23b5.
    
    Change-Id: I2038a612db6beccfd90c39ed41572d90982b85d1

--- wmf-config/db-codfw.php
+++ wmf-config/db-codfw.php
@@ -8,8 +8,7 @@ if ( !defined( 'DBO_DEFAULT' ) ) {
 # $wgReadOnly = "Wikimedia Sites are currently read-only during maintenance, please try again soon.";
 
 $wmgParserCacheDBs = [
-	# '10.64.0.12'   => '10.192.16.170', # pc2004, B5 2.4TB 256GB # kernel upgrade
-	'10.64.0.12'   => '10.192.16.128', # pc2005, C5 2.4TB 256GB # temporary pc2005 while pc2004 is upgraded
+	'10.64.0.12'   => '10.192.16.170', # pc2004, B5 2.4TB 256GB
 	'10.64.32.72'  => '10.192.32.128', # pc2005, C5 2.4TB 256GB
 	'10.64.48.128' => '10.192.48.39',  # pc2006, D5 2.4TB 256GB
 ];
