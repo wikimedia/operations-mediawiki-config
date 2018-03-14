d1d3b2de33249cc6cb26105897b5870763c80b09
Marostegui <marostegui@wikimedia.org>
2018-03-14 10:12:34 +0000

    Revert "db-codfw.php: Depool pc2006"
    This reverts commit 02fea7f5e24545d49cd6e147917937c35b2d377a.
    
    Change-Id: I329c4bcea0009aabbd7e325e020b5d64240c5a49

--- wmf-config/db-codfw.php
+++ wmf-config/db-codfw.php
@@ -10,8 +10,7 @@ if ( !defined( 'DBO_DEFAULT' ) ) {
 $wmgParserCacheDBs = [
 	'10.64.0.12'   => '10.192.16.170', # pc2004, B5 2.4TB 256GB
 	'10.64.32.72'  => '10.192.32.128', # pc2005, C5 2.4TB 256GB
-	# '10.64.48.128' => '10.192.48.39',  # pc2006, D5 2.4TB 256GB # kernel upgrade
-	'10.64.48.128' => '10.192.32.128',  # pc2005 while pc2006 gets ugprade
+	'10.64.48.128' => '10.192.48.39',  # pc2006, D5 2.4TB 256GB
 ];
 
 $wmgOldExtTemplate = [
