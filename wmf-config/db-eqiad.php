49e3aa23905f4628267529c928be01f7cb1375b1
Manuel Arostegui <marostegui@wikimedia.org>
2018-03-19 16:28:59 +0100

    db-eqiad.php: Restore db1091 original weight
    Change-Id: I7003bc2203c25018da26974bd5a1f53a6262f2bf

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -135,7 +135,7 @@ $wgLBFactoryConf = [
 		'db1064' => 0,      # D1 2.8TB 160GB, vslow, dump # master for db1095
 		'db1081' => 300,    # A2 3.6TB 512GB, api # candidate master
 		'db1084' => 400,    # B1 3.6TB 512GB, api
-		'db1091' => 300,    # D2 3.6TB 512GB
+		'db1091' => 500,    # D2 3.6TB 512GB
 		'db1097:3314' => 1, # D1 3.6TB 512GB, # rc, log: s4 and s5
 		'db1103:3314' => 1, # A3 3.6TB 512GB # rc, log: s2 and s4
 	],
