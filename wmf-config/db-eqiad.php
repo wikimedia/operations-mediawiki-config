4e02a9fdb09334c4545411993b250e10129c21cf
Manuel Arostegui <marostegui@wikimedia.org>
2018-03-19 10:46:39 +0100

    db-eqiad,db-codfw.php: Remove db1009 from config
    db1009 is ready to be decommissioned
    
    Bug: T189216
    Change-Id: I6c4596e4c4decf7d0f84052a924e7c6e0371e8e1

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -518,7 +518,6 @@ $wgLBFactoryConf = [
 # it just breaks the site horribly.
 'hostsByName' => [
 	'db1001' => '10.64.0.5', # do not remove or comment out
-	'db1009' => '10.64.0.13', # do not remove or comment out
 	'db1020' => '10.64.16.9', # do not remove or comment out
 	'db1043' => '10.64.16.32', # do not remove or comment out
 	'db1047' => '10.64.16.36', # do not remove or comment out
