d72eb1556b95053bf99e7aaec7379309f5a5815e
Gergő Tisza <tgr.huwiki@gmail.com>
2018-03-20 16:14:14 -0700

    Temporarily preserve sysops' JS editing ability
    This prevents Ifefd8726 from having any effect. It's meant as
    a temporary measure while transition to the new group happens.
    
    Bug: T190015
    Change-Id: Ia9b2ea1450aff6121dc0f3777bf029292c8aaad9

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -10041,6 +10041,8 @@ $wgConf->settings = [
 			'deleterevision' => true,
 			'deletelogentry' => true,
 			'editcontentmodel' => true,
+			'edituserjs' => true, // temporary for T190015
+			'editusercss' => true, // temporary for T190015
 		],
 		'bot' => [
 			'noratelimit' => true,
