4b097e4db70f1ff347b97a2e8cb3577f5a2e1669
Chad <chadh@wikimedia.org>
2018-03-21 19:03:56 +0000

    Revert "beta: disable abusefilter from collecting user IP addresses"
    This reverts commit 37d8c2185ce213e84173ead72adf4118296555fd.
    
    Change-Id: I2c9524694001567268759b77014464f1ce42a42d

--- wmf-config/CommonSettings-labs.php
+++ wmf-config/CommonSettings-labs.php
@@ -60,11 +60,6 @@ $wgDnsBlacklistUrls   = [
 	'proxies.dnsbl.sorbs.net.',
 ];
 
-if ( $wmgUseAbuseFilter ) {
-	$wgAbuseFilterLogIP = false; // Prevent the collection of IP addresses
-	$wgAbuseFilterLogIPMaxAge = 2629746; // Purge data each 30 days, not 90
-}
-
 if ( $wmgUseFlow ) {
 	// Override CommonSettings.php, which has:
 	// $wgFlowExternalStore = $wgDefaultExternalStore;
