65ac41054ced1e9f89281f431c8151c89dcbaa14
MarcoAurelio <maurelio@tools.wmflabs.org>
2018-03-21 15:46:39 +0000

    guwiki: fix $wg{Add,Remove}Groups configuration
    Change-Id: Icfa4a67ea069d6508db0629c97794bdc3cdd221d

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -10287,7 +10287,7 @@ $wgConf->settings = [
 		'sysop' => [ 'confirmed' ],
 	],
 	'+guwiki' => [
-		'sysop' => [ 'rollback' /* T119787 */, 'autopatrolled' /* T119787 */, 'transwiki' /* T120346 */ ],
+		'sysop' => [ 'autopatrolled', 'rollback', 'transwiki' ], // T119787, T119787, T120346
 	],
 	'+hewiki' => [
 		'sysop' => [ 'patroller', 'autopatrolled', 'accountcreator' ],
@@ -11040,8 +11040,8 @@ $wgConf->settings = [
 	'+glwiki' => [ // T128948
 		'sysop' => [ 'confirmed' ],
 	],
-	'+guwiki' => [ // T119787
-		'sysop' => [ 'rollback' /* T119787 */, 'autopatrolled' /* T119787 */, 'transwiki' /* T120346 */ ],
+	'+guwiki' => [
+		'sysop' => [ 'autopatrolled', 'rollback', 'transwiki' ], // T119787, T119787, T120346
 	],
 	'+hewiki' => [
 		'sysop' => [ 'patroller', 'autopatrolled', 'accountcreator' ],
