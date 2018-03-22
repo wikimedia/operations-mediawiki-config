8de6b4e152d3a04f33d91816e794e9ab50ea4cc7
Jcrespo <jcrespo@wikimedia.org>
2018-03-22 08:25:01 +0000

    Revert "mariadb: Depool db1060 for maintenance"
    This reverts commit 2f3c9018c8e52edb57b7ac8ba044a8cc78cba89a.
    
    Change-Id: I8bd11208b7f0ce811a038a70f9d7c9bb26e53c0f

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -116,9 +116,9 @@ $wgLBFactoryConf = [
 	],
 	's2' => [
 		'db1054' => 0,      # A3 2.8TB  96GB, master
-		# 'db1060' => 1,      # C2 2.8TB  96GB, vslow, dump, api #master for db1102 (sanitarium 3)
+		'db1060' => 1,      # C2 2.8TB  96GB, vslow, dump, api #master for db1102 (sanitarium 3)
 		'db1074' => 300,    # A2 3.6TB 512GB, api
-		'db1076' => 300,    # B1 3.6TB 512GB # candidate master, temp. vslow, dump
+		'db1076' => 500,    # B1 3.6TB 512GB # candidate master
 		'db1090' => 500,    # C3 3.6TB 512GB
 		'db1103:3312' => 1, # A3 3.6TB 512GB # rc, log: s2 and s4
 		'db1105:3312' => 1, # C3 3.6TB 512GB # rc, log: s1 and s2
@@ -292,12 +292,13 @@ $wgLBFactoryConf = [
 	],
 	's2' => [
 		'vslow' => [
-			'db1076' => 1,
+			'db1060' => 1,
 		],
 		'dump' => [
-			'db1076' => 1,
+			'db1060' => 1,
 		],
 		'api' => [
+			'db1060' => 1,
 			'db1074' => 5,
 		],
 		'watchlist' => [
