d33c3dc93185665bdcddfa5083eb4fd7c457be63
Jaime Crespo <jcrespo@wikimedia.org>
2018-03-23 11:25:15 +0100

    mariadb: Increase db1072 weight to compensate for higher s3 load
    Change-Id: I2183382ae750c2d6a38e7ac6e2b19365213f2c9e

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -125,7 +125,7 @@ $wgLBFactoryConf = [
 	],
 	/* s3 */ 'DEFAULT' => [
 		'db1075' => 0,      # A2 3.6TB 512GB, master
-		'db1072' => 50,     # B2 2.8TB 160GB, vslow, dump, old master # master for db1095
+		'db1072' => 100,    # B2 2.8TB 160GB, vslow, dump, old master # master for db1095
 		'db1077' => 400,    # B1 3.6TB 512GB, watchlist, recentchanges, contributions, logpager
 		'db1078' => 500,    # C3 3.6TB 512GB # candidate master
 	],
