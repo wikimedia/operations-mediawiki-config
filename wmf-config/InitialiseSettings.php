378a394f9e04d0bd61ff905cff86293740ea9f63
jdlrobson <jdlrobson@gmail.com>
2018-03-27 11:21:40 -0700

    Rollout VirtualPageViews (stage 3)
    As the third part of the roll out plan we'll enable this on all
    the smaller/medium sized wikis. We don't enable this on the larger
    wikis, this will be done in later deployment(s)
    
    Bug: T189906
    Change-Id: I1f5f67a45dc9a3e77b983b94d1e0812753f9596d

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -13978,11 +13978,14 @@ $wgConf->settings = [
 
 'wgPopupsVirtualPageViews' => [
 	'default' => false,
-	'huwiki' => true,
-	// enable VirtualPageViews for S6 wikis T189906
-	'ruwiki' => true,
-	'jawiki' => true,
-	'frwiki' => true,
+	'wikipedia' => true,
+	// Temporarily disabled for the following larger wikis (T189906) as part of roll out
+	'enwiki' => false,
+	'dewiki' => false,
+	'eswiki' => false,
+	'itwiki' => false,
+	'ptwiki' => false,
+	'zhwiki' => false,
 ],
 
 'wmgPopupsBetaFeature' => [
