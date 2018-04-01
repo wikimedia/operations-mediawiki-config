2e6944373358967996f536bc1ec5957b959cb84f
Gergő Tisza <tgr.huwiki@gmail.com>
2018-04-01 11:42:18 +0200

    Set $wgPropagateErrors to false in production
    Disable sending of PHP errors to 'type:hhvm' in Logstash that are
    already sent (+stacktraces) to 'type:mediawiki channel:error'.
    
    Bug: T45086
    Change-Id: I4857e8ac2624e20957565c5e6aac0a1ba48299d8

--- wmf-config/InitialiseSettings-labs.php
+++ wmf-config/InitialiseSettings-labs.php
@@ -745,9 +745,6 @@ function wmfLabsSettings() {
 		'wgWMEAICaptchaEnabled' => [
 			'default' => true,
 		],
-		'wgPropagateErrors' => [
-			'default' => false,
-		],
 		// T184668
 		'wmgUseGlobalPreferences' => [
 			// Explicitly disabled on non-CentralAuth wikis in CommonSettings.php
--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -20751,6 +20751,10 @@ $wgConf->settings = [
 	'default' => false,
 ],
 
+'wgPropagateErrors' => [
+	'default' => false,
+],
+
 'wmgUseCongressLookup' => [
 	'default' => false,
 	'testwiki' => true,
