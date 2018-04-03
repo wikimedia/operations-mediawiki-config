b3ad035b1d5e1e54e9f43819020628a695d7ed2e
Manuel Arostegui <marostegui@wikimedia.org>
2018-04-03 14:53:16 +0200

    db-eqiad.php: Increase traffic for db1077
    Change-Id: I86d53d11f7f30dba5ff2596479166f9d94142a30

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -126,7 +126,7 @@ $wgLBFactoryConf = [
 	/* s3 */ 'DEFAULT' => [
 		'db1075' => 0,      # A2 3.6TB 512GB, master
 		'db1072' => 100,    # B2 2.8TB 160GB, vslow, dump, old master # master for db1095 # temporary rc
-		'db1077' => 100,    # B1 3.6TB 512GB, watchlist, recentchanges, contributions, logpager
+		'db1077' => 200,    # B1 3.6TB 512GB, watchlist, recentchanges, contributions, logpager
 		'db1078' => 500,    # C3 3.6TB 512GB # candidate master
 	],
 	's4' => [
@@ -330,7 +330,7 @@ $wgLBFactoryConf = [
 			'db1072' => 1,
 		],
 		'watchlist' => [
-			'db1072' => 1,
+			'db1077' => 1,
 		],
 		'recentchanges' => [
 			'db1072' => 1,
