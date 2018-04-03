4c152305d411306cd69ced77a866a796af7650e5
Manuel Arostegui <marostegui@wikimedia.org>
2018-04-03 10:30:26 +0200

    db-eqiad.php: Depool db1077
    db1077 needs alter table
    db1072 will be the temporary rc slave for s3
    
    Bug: T187089
    Bug: T185128
    Bug: T153182
    Change-Id: I3b1b48f2ad563264cb8891a9ed32a42a31743cab

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -125,8 +125,8 @@ $wgLBFactoryConf = [
 	],
 	/* s3 */ 'DEFAULT' => [
 		'db1075' => 0,      # A2 3.6TB 512GB, master
-		'db1072' => 100,    # B2 2.8TB 160GB, vslow, dump, old master # master for db1095
-		'db1077' => 400,    # B1 3.6TB 512GB, watchlist, recentchanges, contributions, logpager
+		'db1072' => 100,    # B2 2.8TB 160GB, vslow, dump, old master # master for db1095 # temporary rc
+		# 'db1077' => 400,    # B1 3.6TB 512GB, watchlist, recentchanges, contributions, logpager # alter table
 		'db1078' => 500,    # C3 3.6TB 512GB # candidate master
 	],
 	's4' => [
@@ -330,19 +330,19 @@ $wgLBFactoryConf = [
 			'db1072' => 1,
 		],
 		'watchlist' => [
-			'db1077' => 1,
+			'db1072' => 1,
 		],
 		'recentchanges' => [
-			'db1077' => 1,
+			'db1072' => 1,
 		],
 		'recentchangeslinked' => [
-			'db1077' => 1,
+			'db1072' => 1,
 		],
 		'contributions' => [
-			'db1077' => 1,
+			'db1072' => 1,
 		],
 		'logpager' => [
-			'db1077' => 1,
+			'db1072' => 1,
 		],
 	],
 	's4' => [
