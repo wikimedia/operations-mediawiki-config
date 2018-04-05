d2d8808ebaf83a473262ddf9641c2321d69d1c3c
David Causse <dcausse@wikimedia.org>
2018-04-05 17:10:27 +0200

    Add cirrussearch settings for wikibase
    Will allow to fine tune search settings by config instead
    of updating the codebase.
    
    Moved all search settings to a single file.
    Switched to term_boost instead of statement_boost as I plan
    to remove the statementBoost config entry.
    The impact is:
     - fulltext will use statement boosting
     - labs settings will also start using statement boost
    
    The deploy order should be:
    - wmf-config/InitialiseSettings.php
    - wmf-config/InitialiseSettings-labs.php
    - wmf-config/WikibaseSearchSettings.php
    - wmf-config/Wikibase.php
    - wmf-config/Wikibase-production.php
    
    Bug: T182717
    Change-Id: I892e1ede24144d352a54b07bb565244f2ae270c2

--- wmf-config/InitialiseSettings-labs.php
+++ wmf-config/InitialiseSettings-labs.php
@@ -460,6 +460,10 @@ function wmfLabsSettings() {
 			'wikidatawiki' => true,
 		],
 
+		'-wmgWikibaseSearchStatementBoosts' => [
+			'default' => []
+		],
+
 		// Test the extension Collection in other languages for book creator,
 		// which avoids the bugs related to the PDF generator.
 		'wmgUseCollection' => [
