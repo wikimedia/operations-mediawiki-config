78f29417101e364e1b16def38e7f38c7ea749b18
Jcrespo <jcrespo@wikimedia.org>
2018-04-17 14:32:52 +0000

    Revert "mariadb: Depool es1017 for reimage" but pool it slowly
    Pool es1017 with low load to check it is working again as intended,
    also as a secondary warmup.
    
    This partially reverts commit 71d2e7010be98d24f725665899062262e5883269.
    
    Change-Id: Iea3101958ceb66efd9acfeed2c9ca7fc762b9397

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -671,8 +671,8 @@ $wgLBFactoryConf = [
 	],
 	# es3
 	'cluster25' => [
-		'10.64.16.187' => 1, # es1014, B1 11TB 128GB, master
-		# '10.64.48.114' => 1, # es1017, D1 11TB 128GB, depooled for reimage
+		'10.64.16.187' => 0, # es1014, B1 11TB 128GB, master
+		'10.64.48.114' => 1, # es1017, D1 11TB 128GB
 		'10.64.48.116' => 10, # es1019, D8 11TB 128GB
 	],
 	# ExtensionStore shard1
