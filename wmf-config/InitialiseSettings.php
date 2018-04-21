7c3eec7de35c7ef7c42f3ec28f3e200446ebacb2
MarcoAurelio <maurelio@tools.wmflabs.org>
2018-04-21 11:38:51 +0000

    euwikisource: add missing $wgMetaNamespace
    Bug: T189465
    Change-Id: I111d28628c58d11ad9ca7ae894cea8652ab2ce92

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -2727,6 +2727,7 @@ $wgConf->settings = [
 	'etwikisource' => 'Vikitekstid',
 	'etwikiquote' => 'Vikitsitaadid',
 	'etwiktionary' => 'Vikisõnastik',
+	'euwikisource' => 'Wikiteka', // T189465
 	'execwiki' => 'Project',
 	'extwiki' => 'Güiquipeya',
 	'fawiki' => 'ویکی‌پدیا',
