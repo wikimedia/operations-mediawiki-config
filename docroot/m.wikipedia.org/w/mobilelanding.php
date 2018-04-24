wikimedia/operations-mediawiki-config

commit 2f0afc4ab13dcd61eeaf810d2dcefd16ef564f86

Don't bother symlinking mobilelanding.php
It's only used by a single docroot

Change-Id: I0c2d0e2d3af261c6606bdf3ce64a6c1103b9bace

docroot/m.wikipedia.org/w/mobilelanding.php
@@ -1,1 +1,27 @@
-../../../w/mobilelanding.php
+<?php
+putenv( 'MW_LANG=en' ); // notify MWMultiVersion
+
+require_once __DIR__ . '/../multiversion/MWMultiVersion.php';
+require MWMultiVersion::getMediaWiki( 'includes/WebStart.php' );
+
+$code = '302';
+$state = ZeroBanner\PageRenderingHooks::getState();
+$redirect = $state->getLandingRedirect();
+$redirect = filter_var( $redirect, FILTER_VALIDATE_URL );
+if ( $redirect === false ) {
+	$code = '500';
+}
+$message = HttpStatus::getMessage( $code );
+
+// $lastmod = gmdate( 'D, j M Y H:i:s', wfTimestamp( TS_UNIX, $wgArticle->getTouched() ) ) . ' GMT';
+// header( "Last-modified: $lastmod" );
+
+// note that the following will override any Cache-Control set earlier
+// in extension code called above
+header( 'Cache-Control: public, s-maxage=900, max-age=900' );
+header( "HTTP/1.1 $code $message" );
+if ( $redirect !== false ) {
+	header( 'Location: ' . $redirect );
+}
+header( 'Vary: X-Forwarded-Proto,X-CS,X-Subdomain,Accept-Language' );
+header( 'Content-Type: text/html; charset=utf-8' );

w/mobilelanding.php
@@ -1,27 +1,0 @@
-<?php
-putenv( 'MW_LANG=en' ); // notify MWMultiVersion
-
-require_once __DIR__ . '/../multiversion/MWMultiVersion.php';
-require MWMultiVersion::getMediaWiki( 'includes/WebStart.php' );
-
-$code = '302';
-$state = ZeroBanner\PageRenderingHooks::getState();
-$redirect = $state->getLandingRedirect();
-$redirect = filter_var( $redirect, FILTER_VALIDATE_URL );
-if ( $redirect === false ) {
-	$code = '500';
-}
-$message = HttpStatus::getMessage( $code );
-
-// $lastmod = gmdate( 'D, j M Y H:i:s', wfTimestamp( TS_UNIX, $wgArticle->getTouched() ) ) . ' GMT';
-// header( "Last-modified: $lastmod" );
-
-// note that the following will override any Cache-Control set earlier
-// in extension code called above
-header( 'Cache-Control: public, s-maxage=900, max-age=900' );
-header( "HTTP/1.1 $code $message" );
-if ( $redirect !== false ) {
-	header( 'Location: ' . $redirect );
-}
-header( 'Vary: X-Forwarded-Proto,X-CS,X-Subdomain,Accept-Language' );
-header( 'Content-Type: text/html; charset=utf-8' );

commit 29203a60b8909197cf9c52cd2f75f6f6e636000c

mobilelanding: Use actual location of MWMultiVersion
I broke it in 2f0afc4ab13dcd61eeaf810d2dcefd16ef564f86 when
I removed the symlink indirection. That path worked, the new
one doesn't.

Considering how infrequently this error arises, I continue
to question how useful this script is

Change-Id: Iba70e93adb41cf3f33503a2a5eeea0866f49d612

docroot/m.wikipedia.org/w/mobilelanding.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../multiversion/MWMultiVersion.php';
+require_once __DIR__ . '/../../../multiversion/MWMultiVersion.php';
 require MWMultiVersion::getMediaWiki( 'includes/WebStart.php', 'enwiki' );
 
 $code = '302';