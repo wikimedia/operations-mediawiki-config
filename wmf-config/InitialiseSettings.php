7ee4c63fc8443bda66aa7df0c032799e20d9365b
Petr Pchelko <ppchelko@wikimedia.org>
2018-04-30 17:41:35 -0300

    Switch all jobs for everything except wikipedia, commons and wikidata.
    Bug: T190327
    Change-Id: Ibae3a25c8586674d54e3f05840c91adb720f1060

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -20266,11 +20266,10 @@ $wgConf->settings = [
 ],
 
 'wmgBulk2JobsInRedis' => [
-	'default' => false,
-	'mediawikiwiki' => true,
-	'testwiki' => true,
-	'test2wiki' => true,
-	'testwikidatawiki' => true,
+	'default' => true,
+	'wikipedia' => false,
+	'commonswiki' => false,
+	'wikidatawiki' => false,
 ],
 
 'wmgUseCapiunto' => [
