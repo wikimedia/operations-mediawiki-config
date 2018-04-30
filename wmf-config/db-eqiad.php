d5bee7fe74ef510203d315dfb7446d18e39d926d
Jaime Crespo <jcrespo@wikimedia.org>
2018-04-30 17:29:44 +0200

    mariadb: Fully pool back db1056 and db1069 as x1 replicas
    Bug: T186320
    Change-Id: Ie909dc070d18b272f22f20b18d31d5638ab4679e

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -677,7 +677,7 @@ $wgLBFactoryConf = [
 	],
 	# ExtensionStore shard1
 	'extension1' => [
-		'10.64.32.25' => 10, # db1055, C2 2.8TB 96GB, master
+		'10.64.32.25' => 0, # db1055, C2 2.8TB 96GB, master
 		'10.64.32.26' => 1, # db1056, C3 2.8TB 96GB
 		'10.64.0.108' => 1, # db1069, A1 2.8TB 160GB
 	],
