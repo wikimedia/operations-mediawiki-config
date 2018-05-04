9fd949334b59ca9ddd183cee70cc0fe55054336a
Subramanya Sastry <ssastry@wikimedia.org>
2018-05-04 08:39:02 -0500

    Enable RemexHtml on more wikibooks wikis
    * These have < 100 linter issues in main namespace for all categories
    
    Bug: T192821
    Change-Id: Ib3536e0a2f2e00e02b61e5781d4cd8341f9049eb

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -19917,18 +19917,11 @@ $wgConf->settings = [
 	// Wikibookses
 	'dewikibooks' => null,
 	'enwikibooks' => null,
-	'eowikibooks' => null,
 	'eswikibooks' => null,
 	'fiwikibooks' => null,
 	'frwikibooks' => null,
 	'hewikibooks' => null,
-	'huwikibooks' => null,
-	'itwikibooks' => null,
-	'kawikibooks' => null,
-	'nlwikibooks' => null,
-	'ptwikibooks' => null,
 	'sqwikibooks' => null,
-	'urwikibooks' => null,
 
 	// Wikisources
 	'sourceswiki' => null,
