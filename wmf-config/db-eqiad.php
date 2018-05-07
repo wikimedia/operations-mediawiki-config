7d183ccdad6d877982f3abb682c70a2d191bddb3
Manuel Arostegui <marostegui@wikimedia.org>
2018-05-07 15:36:16 +0200

    db-eqiad.php: Promote db1069 to be x1 master
    db1069 is the new x1 master
    
    Bug: T186320
    Change-Id: I2ff61148aab1548be9e60247a67be338b26290bc

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -680,8 +680,8 @@ $wgLBFactoryConf = [
 	],
 	# ExtensionStore shard1
 	'extension1' => [
-		'10.64.32.25' => 0, # db1055, C2 2.8TB 96GB, master
-		'10.64.0.108' => 1, # db1069, A1 2.8TB 160GB
+		'10.64.0.108' => 0, # db1069, A1 2.8TB 160GB # master
+		'10.64.32.25' => 1, # db1055, C2 2.8TB 96GB
 		# '10.64.48.19' => 1, # db1064, D1 2.8TB 160GB, not yet reimaged/provisioned
 	],
 ],
