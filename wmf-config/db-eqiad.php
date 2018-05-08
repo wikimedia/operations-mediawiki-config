fbe3ea89ffa8620d3226fab1bbb61aeb88896529
Jaime Crespo <jcrespo@wikimedia.org>
2018-05-08 13:44:21 +0200

    mariadb: Pool db1064 into x1 with low load
    Bug: T194118
    Change-Id: I7982741a22fa2ca8aace9757bca56997025b0169

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -678,9 +678,8 @@ $wgLBFactoryConf = [
 	],
 	# ExtensionStore shard1
 	'extension1' => [
-		'10.64.0.108' => 0, # db1069, A1 2.8TB 160GB # master
-		# '10.64.32.25' => 1, # db1055, C2 2.8TB 96GB
-		# '10.64.48.19' => 1, # db1064, D1 2.8TB 160GB, not yet reimaged/provisioned
+		'10.64.0.108' => 10, # db1069, A1 2.8TB 160GB # master
+		'10.64.48.19' => 1,  # db1064, D1 2.8TB 160GB
 	],
 ],
 
