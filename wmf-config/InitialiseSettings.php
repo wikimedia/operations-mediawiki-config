8306123a2032ebc54ceb207c8eb0c204239863a6
MarcoAurelio <maurelio@tools.wmflabs.org>
2018-05-15 12:34:37 +0200

    security: remove dangerous unused groups at mlwik{tionary|isource}
    Bug: T152296
    Change-Id: I78d32f3225a5f70e11f55b85065af15bab2689fc

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -9265,20 +9265,6 @@ $wgConf->settings = [
 	'+mlwikisource' => [
 		'patroller' => [ 'patrol' => true ],
 		'autopatrolled' => [ 'autopatrol' => true ],
-		'botadmin' => [
-			'blockemail' => true, 'block' => true, 'ipblock-exempt' => true, 'protect' => true, 'editprotected' => true, 'createaccount' => true, 'deleterevision' => true, 'delete' => true, 'globalblock-whitelist' => true, 'editusercss' => true, 'edituserjs' => true,
-			'autoconfirmed' => true, 'editsemiprotected' => true, 'editinterface' => true, 'autopatrol' => true, 'import' => true, 'patrol' => true, 'markbotedits' => true, 'nuke' => true, 'abusefilter-modify' => true, 'movefile' => true, 'move' => true, 'move-subpages' => true,
-			'move-rootuserpages' => true, 'noratelimit' => true, 'suppressredirect' => true, 'reupload-shared' => true, 'override-antispoof' => true, 'tboverride' => true, 'reupload' => true, 'skipcaptcha' => true, 'rollback' => true, 'browsearchive' => true, 'unblockself' => true,
-			'undelete' => true, 'upload' => true, 'upload_by_url' => true, 'apihighlimits' => true, 'unwatchedpages' => true, 'deletedhistory' => true, 'deletedtext' => true, 'abusefilter-log-detail' => true,
-		],
-	],
-	'+mlwiktionary' => [
-		'botadmin' => [
-			'blockemail' => true, 'block' => true, 'ipblock-exempt' => true, 'protect' => true, 'editprotected' => true, 'createaccount' => true, 'deleterevision' => true, 'delete' => true, 'globalblock-whitelist' => true, 'editusercss' => true, 'edituserjs' => true,
-			'autoconfirmed' => true, 'editsemiprotected' => true, 'editinterface' => true, 'autopatrol' => true, 'import' => true, 'patrol' => true, 'markbotedits' => true, 'nuke' => true, 'abusefilter-modify' => true, 'movefile' => true, 'move' => true, 'move-subpages' => true,
-			'move-rootuserpages' => true, 'noratelimit' => true, 'suppressredirect' => true, 'reupload-shared' => true, 'override-antispoof' => true, 'tboverride' => true, 'reupload' => true, 'skipcaptcha' => true, 'rollback' => true, 'browsearchive' => true, 'unblockself' => true,
-			'undelete' => true, 'upload' => true, 'upload_by_url' => true, 'apihighlimits' => true, 'unwatchedpages' => true, 'deletedhistory' => true, 'deletedtext' => true, 'abusefilter-log-detail' => true,
-		],
 	],
 	'+mznwiki' => [
 		// Uploads are restricted to a uploader group - T187187
@@ -10467,10 +10453,6 @@ $wgConf->settings = [
 	],
 	'+mlwikisource' => [
 		'sysop' => [ 'patroller', 'autopatrolled' ],
-		'bureaucrat' => [ 'botadmin' ],
-	],
-	'+mlwiktionary' => [
-		'bureaucrat' => [ 'botadmin' ],
 	],
 	'+mznwiki' => [
 		'sysop' => [ 'uploader' ],
@@ -11230,10 +11212,6 @@ $wgConf->settings = [
 	],
 	'+mlwikisource' => [
 		'sysop' => [ 'patroller', 'autopatrolled' ],
-		'bureaucrat' => [ 'botadmin' ],
-	],
-	'+mlwiktionary' => [
-		'bureaucrat' => [ 'botadmin' ],
 	],
 	'+mznwiki' => [
 		'sysop' => [ 'uploader' ]
@@ -13260,8 +13238,6 @@ $wgConf->settings = [
 	'ladwiki' => [ 'flood' => [ 'flood' ] ], // T131527
 	'lvwiki' => [ 'flood' => [ 'flood' ] ], // T121238
 	'mlwiki' => [ 'botadmin' => [ 'botadmin' ] ],
-	'mlwikisource' => [ 'botadmin' => [ 'botadmin' ] ], // T46335
-	'mlwiktionary' => [ 'botadmin' => [ 'botadmin' ] ],
 	'simplewiki' => [ 'sysop' => [ 'flood' ] ],
 	'plwiki' => [ 'flood' => [ 'flood' ] ], // T22155 , T23238
 	'sourceswiki' => [ 'flood' => [ 'flood' ] ], // T193350
