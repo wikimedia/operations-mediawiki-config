d600e33c2b9cd3f573f43d71cd100bd8e0dac362
Marostegui <marostegui@wikimedia.org>
2018-05-17 07:00:06 +0000

    Revert "db-eqiad.php: Depool db1079"
    This reverts commit 657063ddb8ead1af297e69e564bc4942c861604c.
    
    Change-Id: Iae43722c8d6062b9ba58fa3558347246a8fef366

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -159,7 +159,7 @@ $wgLBFactoryConf = [
 	],
 	's7' => [
 		'db1062' => 0,      # D4 2.8TB 128GB, master
-		# 'db1079' => 300,    # A2 3.6TB 512GB, api #master for db1102 (sanitarium 3) # alter table
+		'db1079' => 300,    # A2 3.6TB 512GB, api #master for db1102 (sanitarium 3)
 		'db1086' => 100,    # B3 3.6TB 512GB, api # candidate master
 		'db1090:3317' => 1, # C3 3.6TB 512GB, vslow, dump: s2 and s7, old master
 		'db1094' => 500,    # D2 3.6TB 512GB
@@ -447,7 +447,7 @@ $wgLBFactoryConf = [
 			'db1090:3317' => 1,
 		],
 		'api' => [
-			# 'db1079' => 10,
+			'db1079' => 10,
 			'db1086' => 1,
 		],
 		'watchlist' => [
