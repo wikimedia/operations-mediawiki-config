1c578ee572b6145708728a631b671a392d2e6e1e
Manuel Arostegui <marostegui@wikimedia.org>
2018-05-21 09:56:53 +0200

    db-eqiad.php: Depool db1077
    alter table
    
    Bug: T190148
    Bug: T191519
    Bug: T188299
    Change-Id: I1005a679e25b51f66437f5ca5e47ed6446dc3b61

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -126,9 +126,9 @@ $wgLBFactoryConf = [
 	],
 	/* s3 */ 'DEFAULT' => [
 		'db1075' => 0,      # A2 3.6TB 512GB, master
-		'db1077' => 400,    # B1 3.6TB 512GB, rc, log # master for db1095
+		# 'db1077' => 400,    # B1 3.6TB 512GB, rc, log # master for db1095 # alter table
 		'db1078' => 500,    # C3 3.6TB 512GB # candidate master
-		'db1123' => 100,    # D8 3.6TB 512GB, vslow, dump
+		'db1123' => 100,    # D8 3.6TB 512GB, vslow, dump # temporary rc slave
 	],
 	's4' => [
 		'db1068' => 0,      # D1 2.8TB 160GB, master
@@ -328,19 +328,19 @@ $wgLBFactoryConf = [
 			'db1123' => 1,
 		],
 		'watchlist' => [
-			'db1077' => 1,
+			'db1123' => 1,
 		],
 		'recentchanges' => [
-			'db1077' => 1,
+			'db1123' => 1,
 		],
 		'recentchangeslinked' => [
-			'db1077' => 1,
+			'db1123' => 1,
 		],
 		'contributions' => [
-			'db1077' => 1,
+			'db1123' => 1,
 		],
 		'logpager' => [
-			'db1077' => 1,
+			'db1123' => 1,
 		],
 	],
 	's4' => [
