9a760687f896b1416ac1310e607460980cb3b087
Aaron Schulz <aschulz@wikimedia.org>
2018-05-30 02:57:29 -0700

    Add "memcached-mcrouter" to $wgObjectCaches as default for testwiki
    Change-Id: I9347266f7e5d96febcc602be8e5b262ad54c6c89

--- wmf-config/mc.php
+++ wmf-config/mc.php
@@ -1,7 +1,11 @@
 <?php
 # WARNING: This file is publicly viewable on the web. Do not put private data here.
 
-$wgMainCacheType = 'memcached-pecl';
+if ( in_array( $wgDBname, [ 'testwiki', 'test2wiki' ], true ) ) {
+	$wgMainCacheType = 'memcached-mcrouter';
+} else {
+	$wgMainCacheType = 'memcached-pecl';
+}
 $wgMemCachedPersistent = false;
 $wgMemCachedTimeout = 0.25 * 1e6;  // 250ms
 
@@ -16,6 +20,30 @@ $wgObjectCaches['memcached-pecl'] = [
 	'retry_timeout'        => -1,
 	'loggroup'             => 'memcached',
 	'timeout'              => $wgMemCachedTimeout
+];
+
+$wgObjectCaches['memcached-mcrouter'] = [
+	'class' => 'MultiWriteBagOStuff',
+	'caches' => [
+		// new mcrouter consistent hash scheme (uses host:port)
+		0 => [
+			'class'                => 'MemcachedPeclBagOStuff',
+			'serializer'           => 'php',
+			'persistent'           => false,
+			'servers'              => [ '127.0.0.1:11213' ],
+			'server_failure_limit' => 1e9,
+			'retry_timeout'        => -1,
+			'loggroup'             => 'memcached',
+			'timeout'              => $wgMemCachedTimeout
+		],
+		// old nutcracker consistent hash scheme (uses shard tag);
+		// make sure this cache scheme gets purges and stays warm
+		1 => [
+			'factory' => [ 'ObjectCache', 'getInstance' ],
+			'args' => [ 'memcached-pecl' ]
+		],
+	],
+	'reportDupes' => false
 ];
 
 # vim: set sts=4 sw=4 et :
