74439d320ed01fdfe958d8487d4b24c8bcf59d2f
Manuel Arostegui <marostegui@wikimedia.org>
2018-05-31 11:39:18 +0200

    db-codfw.php: Depool db2062
    Going to reclone db2092
    
    Change-Id: I47119dee93d932459ae3490e7b70ef4a5fa9670e

--- wmf-config/db-codfw.php
+++ wmf-config/db-codfw.php
@@ -105,7 +105,7 @@ $wgLBFactoryConf = [
 	's1' => [
 		'db2048'      => 0,   # A1 2.9TB 160GB, master
 		'db2055'      => 50,  # D6 3.3TB 160GB, dump (inactive), vslow, api # candidate master
-		'db2062'      => 50,  # B5 3.3TB 160GB, api # mariadb 10.1
+		# 'db2062'      => 50,  # B5 3.3TB 160GB, api # mariadb 10.1 # cloning db2092
 		'db2070'      => 400, # C5 3.3TB 160GB
 		'db2071'      => 50,  # A6 3.6TB 512GB, api
 		'db2072'      => 500, # B6 3.6TB 512GB, # mariadb 10.1 # sanitarium master
@@ -284,7 +284,7 @@ $wgLBFactoryConf = [
 		],
 		'api' => [
 			'db2055' => 1,
-			'db2062' => 1,
+			# 'db2062' => 1,
 			'db2071' => 5,
 			# 'db2092' => 3,
 		],
