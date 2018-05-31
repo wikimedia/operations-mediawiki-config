f2dd15e6487eda28c251f5eb41e465643ba8f668
Manuel Arostegui <marostegui@wikimedia.org>
2018-05-31 07:15:50 +0200

    db-codfw.php: Depool db2084:3315
    alter table
    
    Bug: T191316
    Bug: T192926
    Bug: T89737
    Bug: T195193
    Change-Id: I9fe48aa03bacaf5b3bbdd0e45417ffa20bc43ecd

--- wmf-config/db-codfw.php
+++ wmf-config/db-codfw.php
@@ -144,7 +144,7 @@ $wgLBFactoryConf = [
 		'db2059'      => 100, # D6 3.3TB 160GB, api
 		'db2066'      => 400, # D6 3.3TB 160GB # sanitarium master
 		# 'db2075'      => 400, # A1 3.3TB 512GB # Compressed InnoDB # temporary sanitarium in codfw T190704
-		'db2084:3315' => 1, # D6 3.3TB 512GB # rc, log: s4 and s5
+		# 'db2084:3315' => 1, # D6 3.3TB 512GB # rc, log: s4 and s5 # alter
 		'db2089:3315' => 1, # A3 3.3TB 512GB # rc, log: s5 and s6
 	],
 	's6' => [
@@ -362,23 +362,23 @@ $wgLBFactoryConf = [
 	],
 	's5' => [
 		'watchlist' => [
-			'db2084:3315' => 1,
+			# 'db2084:3315' => 1,
 			'db2089:3315' => 1,
 		],
 		'recentchanges' => [
-			'db2084:3315' => 1,
+			# 'db2084:3315' => 1,
 			'db2089:3315' => 1,
 		],
 		'recentchangeslinked' => [
-			'db2084:3315' => 1,
+			# 'db2084:3315' => 1,
 			'db2089:3315' => 1,
 		],
 		'contributions' => [
-			'db2084:3315' => 1,
+			# 'db2084:3315' => 1,
 			'db2089:3315' => 1,
 		],
 		'logpager' => [
-			'db2084:3315' => 1,
+			# 'db2084:3315' => 1,
 			'db2089:3315' => 1,
 		],
 		'dump' => [
