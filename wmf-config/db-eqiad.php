93a1b2fb9970bd887bedab1d2905a8eb4b714343
Jaime Crespo <jcrespo@wikimedia.org>
2018-06-05 10:57:09 +0200

    mariadb: Depool db1080 for maintenance
    Change-Id: I36330be2e6355cafb31511cf678959b64ec0b302

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -105,14 +105,14 @@ $wgLBFactoryConf = [
 	's1' => [
 		'db1052' => 0,      # B3 2.8TB  96GB, master
 		'db1067' => 50,     # C6 2.8TB 160GB, old master # candidate master
-		'db1080' => 200,    # A2 3.6TB 512GB, api
+		# 'db1080' => 200,    # A2 3.6TB 512GB, api
 		'db1083' => 500,    # B1 3.6TB 512GB
 		'db1089' => 500,    # C3 3.6TB 512GB
 		'db1099:3311' => 1, # B2 3.6TB 512GB # rc, log: s1 and s8
 		'db1105:3311' => 1, # C3 3.6TB 512GB # rc, log: s1 and s2
 		'db1106' => 50,     # D3 3.6TB 512GB, vslow, dump # master for db1095 (sanitarium)
-		'db1114' => 200,    # D4 3.6TB 512GB, api # MariaDB 10.1
-		'db1119' => 200,    # B8 3.6TB 512GB, api # MariaDB 10.1
+		'db1114' => 200,    # D4 3.6TB 512GB, api
+		'db1119' => 200,    # B8 3.6TB 512GB, api
 	],
 	's2' => [
 		'db1054' => 0,      # A3 2.8TB  96GB, master
@@ -283,7 +283,7 @@ $wgLBFactoryConf = [
 			'db1106' => 1,
 		],
 		'api' => [
-			'db1080' => 1,
+			# 'db1080' => 1,
 			'db1114' => 1,
 			'db1119' => 1,
 		],
