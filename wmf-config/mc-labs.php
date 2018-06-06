4e4e56e8d30f5368964a830e5dec29300a059b16
Timo Tijhof <krinklemail@gmail.com>
2018-06-07 00:11:36 +0100

    mc-labs: Sync with prod or document differences
    * wgMemCachedPersistent: Outdated.
      When mc-labs.php was created, it was copied from a brief moment
      in time where wgMemCachedPersistent=true was being experimented
      with in prod. It was changed back in prod but beta was never
      updated.
    
    * timeout: Keep as-is for now, document the difference.
    
    * servers: Intentional, document the difference.
    
    * retry_timeout: Outdated (was copied from prod). Update beta to match.
    
    * Bring back useful comments in prod settings that were
      accidentally removed in 9e14869f15d77.
    
    Change-Id: I619a2ff5db611dc3332fa3f408de3d090a255720

--- wmf-config/mc-labs.php
+++ wmf-config/mc-labs.php
@@ -2,28 +2,20 @@
 # WARNING: This file is publicly viewable on the web. Do not put private data here.
 
 if ( $wmfRealm == 'labs' ) {  # safe guard
-/*
- * Before altering the wgMemCachedServers array below, make sure you planned
- * your change. Memcached compute a hash of the data and given the hash
- * assign the value to one of the servers.
- * If you remove / comment / change order of servers, the hash will miss
- * and that can result in bad performance for the cluster !
- *
- * Hashar, based on dammit comments. Nov 28 2005.
- *
- */$wgMemCachedPersistent = true;
 
 $wgMainCacheType = "memcached-pecl";
-
-$wgMemCachedTimeout = 500000; # micro seconds
+$wgMemCachedPersistent = false;
+// Beta Cluster: Increase timeout to 500ms (in microseconds)
+$wgMemCachedTimeout = 0.5 * 1e6;
 
 $wgObjectCaches['memcached-pecl'] = [
-	'class'      => 'MemcachedPeclBagOStuff',
-	'serializer' => 'php',
-	'persistent' => false,
-	'servers'    => [ '127.0.0.1:11213' ], // mcrouter
-	'retry_timeout' => 1,
-	'loggroup' => 'memcached',
+	'class'                => 'MemcachedPeclBagOStuff',
+	'serializer'           => 'php',
+	'persistent'           => false,
+	// Beta Cluster: Use mcrouter port
+	'servers'              => [ '127.0.0.1:11213' ],
+	'retry_timeout'        => -1,
+	'loggroup'             => 'memcached',
 ];
 
 $wgMainWANCache = 'wancache-main-mcrouter';
