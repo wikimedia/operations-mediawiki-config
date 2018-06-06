067d8b8bd0d866b52b76c84f20d6cd2e63a8605c
Timo Tijhof <krinklemail@gmail.com>
2018-06-07 00:11:36 +0100

    mc-labs: Update wgMemCachedPersistent override
    When mc-labs.php was created, it was copied from a brief moment
    in time where wgMemCachedPersistent=true was being experimented
    with in prod. It was changed back in prod but beta was never
    updated.
    
    Update it now given there is no reason for it to differ from prod.
    
    Change-Id: I95392203a8c174ee05cb8e9aa69aa419e4015d3a

--- wmf-config/mc-labs.php
+++ wmf-config/mc-labs.php
@@ -2,18 +2,9 @@
 # WARNING: This file is publicly viewable on the web. Do not put private data here.
 
 if ( $wmfRealm == 'labs' ) {  # safe guard
-/*
- * Before altering the wgMemCachedServers array below, make sure you planned
- * your change. Memcached compute a hash of the data and given the hash
- * assign the value to one of the servers.
- * If you remove / comment / change order of servers, the hash will miss
- * and that can result in bad performance for the cluster !
- *
- * Hashar, based on dammit comments. Nov 28 2005.
- *
- */$wgMemCachedPersistent = true;
 
 $wgMainCacheType = "memcached-pecl";
+$wgMemCachedPersistent = false;
 
 $wgMemCachedTimeout = 500000; # micro seconds
 
--- wmf-config/mc.php
+++ wmf-config/mc.php
@@ -2,6 +2,8 @@
 # WARNING: This file is publicly viewable on the web. Do not put private data here.
 
 $wgMainCacheType = 'memcached-pecl';
+// Disabled here for sanity (although matches MediaWiki default,
+// and isn't used given we set 'persistent' explicitly).
 $wgMemCachedPersistent = false;
 $wgMemCachedTimeout = 0.25 * 1e6;  // 250ms
 
