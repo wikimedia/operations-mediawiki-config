6be94ba6306eab2815240098a588ccaa8a833b0f
Timo Tijhof <krinklemail@gmail.com>
2018-06-07 00:24:06 +0100

    mc-labs: Sync with prod or document differences
    * timeout: Keep as-is for now, document the difference.
    
    * servers: Intentional, document the difference.
    
    * retry_timeout: Outdated (was copied from prod). Update beta to match.
    
    * Bring back useful comments in prod settings that were
      accidentally removed in 9e14869f15d77.
    
    Change-Id: I619a2ff5db611dc3332fa3f408de3d090a255720

--- wmf-config/mc-labs.php
+++ wmf-config/mc-labs.php
@@ -5,16 +5,17 @@ if ( $wmfRealm == 'labs' ) {  # safe guard
 
 $wgMainCacheType = "memcached-pecl";
 $wgMemCachedPersistent = false;
-
-$wgMemCachedTimeout = 500000; # micro seconds
+// Beta Cluster: Increase timeout to 500ms (in microseconds)
+$wgMemCachedTimeout = 0.5 * 1e6;
 
 $wgObjectCaches['memcached-pecl'] = [
-	'class'      => 'MemcachedPeclBagOStuff',
-	'serializer' => 'php',
-	'persistent' => false,
-	'servers'    => [ '127.0.0.1:11213' ], // mcrouter
-	'retry_timeout' => 1,
-	'loggroup' => 'memcached',
+	'class'                => 'MemcachedPeclBagOStuff',
+	'serializer'           => 'php',
+	'persistent'           => false,
+	// Beta Cluster: Use mcrouter port
+	'servers'              => [ '127.0.0.1:11213' ],
+	'retry_timeout'        => 1,
+	'loggroup'             => 'memcached',
 ];
 
 $wgMainWANCache = 'wancache-main-mcrouter';
