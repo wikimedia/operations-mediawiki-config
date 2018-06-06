8acd537c6b77c38488f848b66e385c76d4204927
Timo Tijhof <krinklemail@gmail.com>
2018-06-07 00:11:36 +0100

    mc-labs: Sync with prod or document differences
    * wgMemCachedPersistent: Outdated.
      When mc-labs.php was created, it was copied from a brief moment
      in time where wgMemCachedPersistent=true was being experimented
      with in prod. It was changed back in prod but beta was never
      updated.
    
    * timeout: Keep as-is for now, document the difference, and
      match the way that prod sets it. That is, set it explicitly in the
      object cache conf, instead of letting it fallback to the global
      within the class at run-time.
    
    * servers: Intentional, document the difference.
    
    * retry_timeout: Outdated (was copied from prod). Update beta to match.
    
    * Bring back useful comments in prod settings that were
      accidentally removed in 9e14869f15d77.
    
    Change-Id: I619a2ff5db611dc3332fa3f408de3d090a255720

--- wmf-config/mc.php
+++ wmf-config/mc.php
@@ -1,9 +1,16 @@
 <?php
 # WARNING: This file is publicly viewable on the web. Do not put private data here.
 
-$wgMainCacheType = 'memcached-pecl';
+if ( in_array( $wgDBname, [ 'testwiki', 'test2wiki' ], true ) ) {
+	$wgMainCacheType = 'memcached-mcrouter';
+} else {
+	$wgMainCacheType = 'memcached-pecl';
+}
+// Disabled here for sanity (although matches MediaWiki default,
+// and isn't used given we set 'persistent' explicitly).
 $wgMemCachedPersistent = false;
-$wgMemCachedTimeout = 0.25 * 1e6;  // 250ms
+// Set timeout to 250ms (in microseconds)
+$wgMemCachedTimeout = 0.25 * 1e6;
 
 $wgObjectCaches['memcached-pecl'] = [
 	'class'                => 'MemcachedPeclBagOStuff',
@@ -12,7 +19,9 @@ $wgObjectCaches['memcached-pecl'] = [
 	'servers'              => defined( 'HHVM_VERSION' )
 		? [ '/var/run/nutcracker/nutcracker.sock:0' ]
 		: [ '127.0.0.1:11212' ],
+	// Basically disable failure limit (0 is invalid)
 	'server_failure_limit' => 1e9,
+	// Basically disable retry timeout
 	'retry_timeout'        => -1,
 	'loggroup'             => 'memcached',
 	'timeout'              => $wgMemCachedTimeout
