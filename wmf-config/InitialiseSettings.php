29385090c63e88f346bc249a7545a3481a9440c2
Addshore <addshorewiki@gmail.com>
2018-06-07 13:43:48 +0000

    Load WikibaseLexeme on all of group0 (again)
    This reverts commit a915d84e60f8d90d2b73fcea412241a899aab117.
    
    Bug: T197454
    Change-Id: Id2092c677082394636d3a19d3ee9f60e9f685ec4

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -18670,7 +18670,7 @@ $wgConf->settings = [
 	'default' => false,
 	'testwikidatawiki' => true,
 	'wikidatawiki' => true,
-	'testwiki' => true,
+	'group0' => true,
 ],
 
 'wgLexemeLanguageCodePropertyId' => [
