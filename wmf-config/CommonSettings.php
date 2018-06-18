c9fcb76995eaa6ea254b00e55731fc60a9a0090d
MarcoAurelio <maurelio@tools.wmflabs.org>
2018-06-18 11:27:06 +0200

    Increase password policies for 'steward' to max
    Bug: T197577
    Change-Id: I0344e50f0e7e9ede6eb4815f23f22cc19d8cc0a2

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -450,7 +450,7 @@ if ( $wmgUseCentralAuth ) {
 		];
 
 		if ( array_intersect(
-			[ 'bureaucrat', 'sysop', 'checkuser', 'oversight', 'interface-editor' ],
+			[ 'bureaucrat', 'sysop', 'checkuser', 'oversight', 'interface-editor', 'steward' ],
 			$central->getLocalGroups()
 		) ) {
 			$effectivePolicy = UserPasswordPolicy::maxOfPolicies(
@@ -1541,11 +1541,12 @@ if ( $wmgUseCentralAuth ) {
 		'PasswordCannotBePopular' => PHP_INT_MAX,
 	];
 
-	// See T104371
+	// See T104371 and T197577
 	$wgCentralAuthGlobalPasswordPolicies['steward'] = [
 		'MinimalPasswordLength' => 8,
 		'MinimumPasswordLengthToLogin' => 1,
 		'PasswordCannotMatchUsername' => true,
+		'PasswordCannotBePopular' => PHP_INT_MAX,
 	];
 
 	// See [[m:Requests_for_comment/Password_policy_for_users_with_certain_advanced_permissions]]
