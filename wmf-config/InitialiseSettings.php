42ab47bc7a63561469f331feaa5ab442a8359798
Baha <bmansurov@wikimedia.org>
2018-06-18 09:59:11 -0400

    Increase Schema:CitationUsage sampling rate to 15%
    Bug: T191086
    Change-Id: Ib55b5c3447ef901d6a2790a64f606b3c511c182d

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -20402,7 +20402,7 @@ $wgConf->settings = [
 ],
 
 'wgWMECitationUsagePopulationSize' => [
-	'enwiki' => 100  // T191086
+	'enwiki' => 6.666666666666667  // 15% — T191086
 ],
 
 'wgWMEReadingDepthEnabled' => [
