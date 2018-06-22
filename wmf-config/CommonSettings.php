c3c119956a58e1495f48ca54cf1275c691c3a89e
James D. Forrester <jforrester@wikimedia.org>
2018-06-21 17:53:53 -0700

    Stop loading the MwEmbedSupport extension, part II
    Change-Id: Ic5b8648ab93e40f26bca74045c164da953b4fec5

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -900,10 +900,6 @@ if ( $wmgUseGadgets ) {
 	$wgGadgetsCacheType = CACHE_ACCEL;
 }
 
-if ( $wmgUseMwEmbedSupport && !isset( $wmfIgnoreMwEmbed ) /* T191056, T197918 */ ) {
-	wfLoadExtension( 'MwEmbedSupport' );
-}
-
 if ( $wmgUseTimedMediaHandler ) {
 	require_once "$IP/extensions/TimedMediaHandler/TimedMediaHandler.php";
 	$wgTimedTextForeignNamespaces = [ 'commonswiki' => 102 ];
