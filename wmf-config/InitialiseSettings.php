3798d8bf3aeb2c20518645845ad5122c9d63f7cd
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-06-25 14:03:31 +0200

    Create TemplateEditor group on enwikivoyage
    Bug: T198056
    Change-Id: I6a1a644a5ad27a874a63e996609b7e278d2aaadf

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -5185,6 +5185,7 @@ $wgConf->settings = [
 	'dewiki' => [ '', 'autoconfirmed', 'editeditorprotected', 'sysop' ], // T94368
 	'enwiki' => [ '', 'autoconfirmed', 'extendedconfirmed', 'templateeditor', 'sysop' ], // T126607, T57432
 	'enwiktionary' => [ '', 'autoconfirmed', 'templateeditor', 'sysop' ], // T148007
+	'enwikivoyage' => [ '', 'autoconfirmed', 'templateeditor', 'sysop' ], // T198056
 	'etwiki' => [ '', 'autoconfirmed', 'autopatrol', 'sysop' ], // T153465
 	'fawiki' => [ '', 'autoconfirmed', 'extendedconfirmed', 'sysop' ], // T140839
 	'frwiki' => [ '', 'autoconfirmed', 'editextendedsemiprotected', 'sysop' ], // T131109
@@ -8735,6 +8736,8 @@ $wgConf->settings = [
 	'enwikivoyage' => [
 		'autopatrolled' => [ 'autopatrol' => true ],
 		'patroller' => [ 'patrol' => true, 'rollback' => true ],
+		'templateeditor' => [ 'templateeditor' => true, ], // T198056
+		'sysop' => [ 'templateeditor' => true ],
 	],
 	// T7033
 	'+enwiktionary' => [
@@ -10320,8 +10323,8 @@ $wgConf->settings = [
 		'sysop' => [ 'flood' ],
 	],
 	'+enwikivoyage' => [
-		'sysop' => [ 'autopatrolled' ],
-		'bureaucrat' => [ 'patroller' ],
+		'sysop' => [ 'autopatrolled', 'templateeditor' ],
+		'bureaucrat' => [ 'patroller', 'templateeditor' ],
 	],
 	'+enwikiversity' => [
 		'sysop' => [ 'curator' ], // T113109
@@ -11079,8 +11082,8 @@ $wgConf->settings = [
 		'sysop' => [ 'flood' ],
 	],
 	'+enwikivoyage' => [
-		'sysop' => [ 'autopatrolled' ],
-		'bureaucrat' => [ 'sysop', 'patroller' ],
+		'sysop' => [ 'autopatrolled', 'templateeditor' ],
+		'bureaucrat' => [ 'sysop', 'patroller', 'templateeditor' ],
 	],
 	'+enwikiversity' => [
 		'sysop' => [ 'curator' ], // T113109
