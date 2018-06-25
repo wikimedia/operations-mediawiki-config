2e9a1d174a8fb651795eba5835a580b827867ed9
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-06-25 14:03:31 +0200

    Create TemplateEditor group on enwikivoyage
    Bug: T198056
    Change-Id: I6a1a644a5ad27a874a63e996609b7e278d2aaadf

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -5182,6 +5182,7 @@ $wgConf->settings = [
 	'dewiki' => [ '', 'autoconfirmed', 'editeditorprotected', 'sysop' ], // T94368
 	'enwiki' => [ '', 'autoconfirmed', 'extendedconfirmed', 'templateeditor', 'sysop' ], // T126607, T57432
 	'enwiktionary' => [ '', 'autoconfirmed', 'templateeditor', 'sysop' ], // T148007
+	'enwikivoyage' => [ '', 'autoconfirmed', 'templateeditor', 'sysop' ], // T198056
 	'etwiki' => [ '', 'autoconfirmed', 'autopatrol', 'sysop' ], // T153465
 	'fawiki' => [ '', 'autoconfirmed', 'extendedconfirmed', 'sysop' ], // T140839
 	'frwiki' => [ '', 'autoconfirmed', 'editextendedsemiprotected', 'sysop' ], // T131109
@@ -8729,6 +8730,8 @@ $wgConf->settings = [
 	'enwikivoyage' => [
 		'autopatrolled' => [ 'autopatrol' => true ],
 		'patroller' => [ 'patrol' => true, 'rollback' => true ],
+		'templateeditor' => [ 'templateeditor' => true, ], // T198056
+		'sysop' => [ 'templateeditor' => true ],
 	],
 	// T7033
 	'+enwiktionary' => [
