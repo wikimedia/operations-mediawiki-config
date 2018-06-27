2e4a038e607da1069601ee9bb2c4e755a61281f7
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-06-27 18:49:45 +0200

    Enable SandboxLink on eswikiversity
    Bug: T198335
    Change-Id: I9566ba42016655c447df6932bf7b8215de364fbb

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -13898,6 +13898,7 @@ $wgConf->settings = [
 	'eowiki' => true,
 	'eswiki' => true,
 	'eswiktionary' => true, // T97135
+	'eswikiversity' => true, // T198335
 	'euwiki' => true, // T166553
 	'fawiki' => true,
 	'frwiki' => true, // T95604
