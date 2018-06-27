70fcb75818640e38ecc5b30c9a5dfd7634bcbe80
David Causse <dcausse@wikimedia.org>
2018-06-27 16:50:36 +0200

    Add cirrussearch settings for wikibase (1.5/3)
    statement_keywords was expected not statement_keyword
    
    Bug: T182717
    Change-Id: Ifba879620ea7a648481e7759ca30b8e6fec05739

--- wmf-config/WikibaseSearchSettings.php
+++ wmf-config/WikibaseSearchSettings.php
@@ -126,13 +126,13 @@ $wgWBRepoSettings['entitySearch']['rescoreFunctionChains'] = [
 		'score_mode' => 'sum',
 		'functions' => [
 			[
-				// Incoming links: k = 50
+				// Incoming links: k = 100, since it is normal to have a bunch of incoming links
 				'type' => 'satu',
 				'weight' => '0.6',
-				'params' => [ 'field' => 'incoming_links', 'missing' => 0, 'a' => 2 , 'k' => 50 ]
+				'params' => [ 'field' => 'incoming_links', 'missing' => 0, 'a' => 1 , 'k' => 100 ]
 			],
 			[
-				// Site links: k = 20
+				// Site links: k = 20, tens of sites is a lot
 				'type' => 'satu',
 				'weight' => '0.4',
 				'params' => [ 'field' => 'sitelink_count', 'missing' => 0, 'a' => 2, 'k' => 20 ]
@@ -142,7 +142,7 @@ $wgWBRepoSettings['entitySearch']['rescoreFunctionChains'] = [
 				'weight' => 0.1,
 				'params' => [
 					// Will be replaced by $wmgWikibaseSearchStatementBoosts
-					'statement_keyword' => '_statementBoost_',
+					'statement_keywords' => '_statementBoost_',
 				]
 			]
 		]
