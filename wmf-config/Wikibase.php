3031d03181b953d27d24e1cd43bb05516afaea4f
Timo Tijhof <krinklemail@gmail.com>
2018-07-02 19:09:21 -0700

    Move wgCacheEpoch override from Wikibase.php to InitialiseSettings (2/2)
    Previous commit adds them to InitialiseSettings,
    this commit removes the now-redundant assignments.
    
    Change-Id: I1e78ba436540c54bbd5e83e9a77d571cb30ea6be

--- wmf-config/Wikibase.php
+++ wmf-config/Wikibase.php
@@ -166,8 +166,6 @@ if ( $wmgUseWikibaseRepo ) {
 	// T53637 and T48953
 	$wgGroupPermissions['*']['property-create'] = ( $wgDBname === 'testwikidatawiki' );
 
-	$wgCacheEpoch = '20170724130500';
-
 	$wgWBRepoSettings['dataSquidMaxage'] = 1 * 60 * 60;
 	$wgWBRepoSettings['sharedCacheDuration'] = 60 * 60 * 24;
 	$wgWBRepoSettings['sharedCacheKeyPrefix'] = $wgWBSharedCacheKey;
