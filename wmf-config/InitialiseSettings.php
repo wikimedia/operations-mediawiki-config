8a839be9508539db29410c47980ed4e5904f2e9f
Prateek Saxena <prtksxna@gmail.com>
2018-07-09 18:08:24 +0530

    Remove obsolete $wgPopupsBetaFeature, Part III: InitialiseSettings
    This was added for in
    I66739b01e405bd7be29e912388005e0bb1f25b3f for T146889,
    and removed in
    Iea832cd1f37b0e7df6ff95efd66e4a1ff2a9004e for T193053.
    
    $wgPopupsBetaFeature is not referenced anywhere and no
    longer exists in Popups.
    
    Bug: T203589
    Change-Id: I4c180706931ce02c7fb4dd0609f63c33d68477fa

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -14247,10 +14247,6 @@ $wgConf->settings = [
 	'wikipedia' => true,
 ],
 
-'wmgPopupsBetaFeature' => [
-	'default' => false,
-],
-
 'wgPopupsStatsvSamplingRate' => [
 	// Careful! This value should not be increased without coordination with the
 	// Operations and/or Analytics teams.
