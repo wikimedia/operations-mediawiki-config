78fd0b38d4daaa020ccdbc3e4e29f65df79c69d8
James D. Forrester <jforrester@wikimedia.org>
2018-07-09 12:01:26 -0700

    Cleanup: Stop setting wgVisualEditorNonAccountEnableProportion, dropped in master
    Change-Id: If2ee24ae5bff450e8b4a376eb56a71880786da5a

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -2374,10 +2374,6 @@ if ( $wmgUseVisualEditor ) {
 	if ( $wmgVisualEditorNewAccountEnableProportion ) {
 		$wgVisualEditorNewAccountEnableProportion = $wmgVisualEditorNewAccountEnableProportion;
 	}
-	// Enable for a proportion of non accounts ("IPs")
-	if ( $wmgVisualEditorNonAccountEnableProportion ) {
-		$wgVisualEditorNonAccountEnableProportion = $wmgVisualEditorNonAccountEnableProportion;
-	}
 
 	// Citoid
 	wfLoadExtension( 'Citoid' );
