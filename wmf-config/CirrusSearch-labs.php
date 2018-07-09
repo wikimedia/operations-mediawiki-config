4e0a23267a58e736e0f0adf6b8c7326a3422edc5
David Causse <dcausse@wikimedia.org>
2018-07-09 14:55:19 +0200

    [cirrus] cleanup unused config vars 1/2
    Change-Id: I90ce0cdd0642cccbb26b0ba6f36cf6c2c84f46b3

--- wmf-config/CirrusSearch-labs.php
+++ wmf-config/CirrusSearch-labs.php
@@ -37,7 +37,7 @@ $wgCirrusSearchMaxShardsPerNode = [];
 # Override prod configuration, there is only one cluster in beta
 $wgCirrusSearchDefaultCluster = 'eqiad';
 # Don't specially configure cluster for more like queries in beta
-$wgCirrusSearchFullTextClusterOverrides = [];
+$wgCirrusSearchClusterOverrides = [];
 # write to all configured clusters, there should only be one in labs
 $wgCirrusSearchWriteClusters = null;
 
