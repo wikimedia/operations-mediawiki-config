be8dcccd288585bd94ce35b3e0f81959870c8cde
Marostegui <marostegui@wikimedia.org>
2018-07-09 13:26:48 +0000

    Revert "db-eqiad.php: Depool db1080"
    This reverts commit 174fadee53c03b8ffc4c990934bd946ec86a3b92.
    
    Change-Id: Ia63d8ba0b47a1616935df6e486dd2053378559ab

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -105,7 +105,7 @@ $wgLBFactoryConf = [
 	's1' => [
 		'db1052' => 0,      # B3 2.8TB  96GB, master
 		'db1067' => 50,     # C6 2.8TB 160GB, old master # candidate master
-		# 'db1080' => 200,    # A2 3.6TB 512GB, api # alter
+		'db1080' => 200,    # A2 3.6TB 512GB, api
 		'db1083' => 500,    # B1 3.6TB 512GB
 		'db1089' => 500,    # C3 3.6TB 512GB
 		'db1099:3311' => 1, # B2 3.6TB 512GB # rc, log: s1 and s8
@@ -282,7 +282,7 @@ $wgLBFactoryConf = [
 			'db1106' => 1,
 		],
 		'api' => [
-			# 'db1080' => 1,
+			'db1080' => 1,
 			'db1114' => 1,
 			'db1119' => 1,
 		],
