d499f983276dfd376cad57b714b2ec7bdf07a5ed
Manuel Arostegui <marostegui@wikimedia.org>
2018-07-11 07:24:47 +0200

    db-eqiad.php: Depool db1079
    alter table
    
    Bug: T146591
    Bug: T197891
    Bug: T196379
    Change-Id: Ie60cdd7a9902999ceb9e6e1dd96820093e069442

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -158,7 +158,7 @@ $wgLBFactoryConf = [
 	],
 	's7' => [
 		'db1062' => 0,      # D4 2.8TB 128GB, master
-		'db1079' => 300,    # A2 3.6TB 512GB, api # master for sanitarium db1125
+		# 'db1079' => 300,    # A2 3.6TB 512GB, api # master for sanitarium db1125 # alter
 		'db1086' => 100,    # B3 3.6TB 512GB, api # candidate master
 		'db1090:3317' => 1, # C3 3.6TB 512GB, vslow, dump: s2 and s7, old master
 		'db1094' => 500,    # D2 3.6TB 512GB
@@ -446,7 +446,7 @@ $wgLBFactoryConf = [
 			'db1090:3317' => 1,
 		],
 		'api' => [
-			'db1079' => 10,
+			# 'db1079' => 10,
 			'db1086' => 1,
 		],
 		'watchlist' => [
