2f54b414a079ecf933c1fd306e23e7716eeddfa9
James D. Forrester <jforrester@wikimedia.org>
2018-07-19 16:49:49 +0200

    Enable the WikibaseMediaInfo extension in Beta Cluster Commons
    Bug: T180981
    Change-Id: Ia37c0dc7038f8993806b97bb67c69b2d955276a4

--- wmf-config/InitialiseSettings-labs.php
+++ wmf-config/InitialiseSettings-labs.php
@@ -475,6 +475,11 @@ function wmfLabsSettings() {
 			'default' => []
 		],
 
+		'wgMediaInfoEnable' => [
+			'default' => false,
+			'commonswiki' => true,
+		]
+
 		// Test the extension Collection in other languages for book creator,
 		// which avoids the bugs related to the PDF generator.
 		'wmgUseCollection' => [
