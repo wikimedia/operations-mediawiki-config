c330eef4c7c3eb67321708c856d4b3e81b31129c
Manuel Arostegui <marostegui@wikimedia.org>
2018-07-20 12:00:20 +0200

    db-eqiad.php: Depool db1090:3312 and db1103:3312
    Going to fix inconsistencies on bgwiki.change_tag
    
    Bug: T200061
    Change-Id: I36f3e35c9218c763e3e0824825db56e1ed890929

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -116,11 +116,11 @@ $wgLBFactoryConf = [
 	],
 	's2' => [
 		'db1066' => 0,     # A6 2.8TB 160GB # master
-		'db1074' => 300,    # A2 3.6TB 512GB, api # master for sanitarium db1125
+		'db1074' => 200,    # A2 3.6TB 512GB, api # master for sanitarium db1125 # temporary vslow
 		'db1076' => 500,    # B1 3.6TB 512GB # candidate master
-		'db1090:3312' => 1, # C3 3.6TB 512GB, vslow, dump: s2 and s7
+		# 'db1090:3312' => 1, # C3 3.6TB 512GB, vslow, dump: s2 and s7 # T200061
 		'db1122' => 500,    # D6 3.6TB 512GB, api
-		'db1103:3312' => 1, # A3 3.6TB 512GB # rc, log: s2 and s4
+		# 'db1103:3312' => 1, # A3 3.6TB 512GB # rc, log: s2 and s4 # T200061
 		'db1105:3312' => 1, # C3 3.6TB 512GB # rc, log: s1 and s2
 	],
 	/* s3 */ 'DEFAULT' => [
@@ -289,33 +289,33 @@ $wgLBFactoryConf = [
 	],
 	's2' => [
 		'vslow' => [
-			'db1090:3312' => 1,
+			'db1074' => 1,
 		],
 		'dump' => [
-			'db1090:3312' => 1,
+			'db1074' => 1,
 		],
 		'api' => [
 			'db1074' => 10,
 			'db1122' => 1,
 		],
 		'watchlist' => [
-			'db1103:3312' => 1,
+			# 'db1103:3312' => 1,
 			'db1105:3312' => 1,
 		],
 		'recentchanges' => [
-			'db1103:3312' => 1,
+			# 'db1103:3312' => 1,
 			'db1105:3312' => 1,
 		],
 		'recentchangeslinked' => [
-			'db1103:3312' => 1,
+			# 'db1103:3312' => 1,
 			'db1105:3312' => 1,
 		],
 		'contributions' => [
-			'db1103:3312' => 1,
+			# 'db1103:3312' => 1,
 			'db1105:3312' => 1,
 		],
 		'logpager' => [
-			'db1103:3312' => 1,
+			# 'db1103:3312' => 1,
 			'db1105:3312' => 1,
 		],
 	],
