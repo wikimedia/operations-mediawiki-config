25b82bf034b8a6f728d7f4b359c2d00f4b3633b8
Manuel Arostegui <marostegui@wikimedia.org>
2018-07-24 16:48:33 +0200

    db-eqiad.php: Repool db1084
    Change-Id: I5eff43d118bcf7ff2d839c943753d3429d9614ee

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -132,9 +132,9 @@ $wgLBFactoryConf = [
 	's4' => [
 		'db1068' => 0,      # D1 2.8TB 160GB, master
 		'db1081' => 100,    # A2 3.6TB 512GB, api # candidate master
-		# 'db1084' => 300,    # B1 3.6TB 512GB, api # alter
+		'db1084' => 300,    # B1 3.6TB 512GB, api
 		'db1091' => 500,     # D2 3.6TB 512GB
-		'db1097:3314' => 150, # D1 3.6TB 512GB # rc, log: s4 and s5
+		'db1097:3314' => 50, # D1 3.6TB 512GB # rc, log: s4 and s5
 		# 'db1103:3314' => 150, # A3 3.6TB 512GB # rc, log: s2 and s4 # alter
 		'db1121' => 1,      # C6 3.6TB 512GB, vslow, dump # master for sanitarium db1125
 	],
@@ -351,7 +351,7 @@ $wgLBFactoryConf = [
 		],
 		'api' => [
 			'db1081' => 3,
-			# 'db1084' => 1,
+			'db1084' => 1,
 		],
 		'watchlist' => [
 			'db1097:3314' => 1,
