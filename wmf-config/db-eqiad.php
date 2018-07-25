710c0cf74aa041d28420db5c68092aa0ea1c62e5
Marostegui <marostegui@wikimedia.org>
2018-07-25 15:28:48 +0000

    Revert "db-eqiad.php: Depool db1098:3317"
    This reverts commit ef7b8175bc7c340d96cc8e1e02450edefd332b27.
    
    Change-Id: I6c95f42304d2440333c8ed23eef05a2d8a10aeac

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -162,7 +162,7 @@ $wgLBFactoryConf = [
 		'db1086' => 400,    # B3 3.6TB 512GB, api # candidate master
 		'db1090:3317' => 1, # C3 3.6TB 512GB, vslow, dump: s2 and s7, old master
 		'db1094' => 500,    # D2 3.6TB 512GB
-		# 'db1098:3317' => 1, # B5 3.6TB 512GB # rc, log: s6 and s7 # alter
+		'db1098:3317' => 1, # B5 3.6TB 512GB # rc, log: s6 and s7
 		'db1101:3317' => 1, # C2 3.6TB 512GB # rc, log: s7 and s8
 	],
 	's8' => [
@@ -450,23 +450,23 @@ $wgLBFactoryConf = [
 			'db1086' => 1,
 		],
 		'watchlist' => [
-			# 'db1098:3317' => 1,
+			'db1098:3317' => 1,
 			'db1101:3317' => 1,
 		],
 		'recentchanges' => [
-			# 'db1098:3317' => 1,
+			'db1098:3317' => 1,
 			'db1101:3317' => 1,
 		],
 		'recentchangeslinked' => [
-			# 'db1098:3317' => 1,
+			'db1098:3317' => 1,
 			'db1101:3317' => 1,
 		],
 		'contributions' => [
-			# 'db1098:3317' => 1,
+			'db1098:3317' => 1,
 			'db1101:3317' => 1,
 		],
 		'logpager' => [
-			# 'db1098:3317' => 1,
+			'db1098:3317' => 1,
 			'db1101:3317' => 1,
 		],
 	],
