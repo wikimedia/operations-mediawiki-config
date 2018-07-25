03520001a4d6f73b1da2eeec1f2748d908a11b18
Jcrespo <jcrespo@wikimedia.org>
2018-07-25 09:59:20 +0000

    Revert "mariadb: Depool es1014 for reimage"
    This reverts commit 361a8bd572f3360996e5e78ec1f219af7f76ca8b.
    
    Change-Id: I897fef13dfbebc520100544838d7755f6149acd7

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -668,8 +668,8 @@ $wgLBFactoryConf = [
 	# es3
 	'cluster25' => [
 		'10.64.32.65'  => 0, # es1017, C3 11TB 128GB, master
-		'10.64.16.187' => 1, # es1014, B1 11TB 128GB, upgrade to stretch
-		'10.64.48.116' => 10, # es1019, D8 11TB 128GB
+		'10.64.16.187' => 1, # es1014, B1 11TB 128GB
+		'10.64.48.116' => 1, # es1019, D8 11TB 128GB
 	],
 	# ExtensionStore shard1
 	'extension1' => [
