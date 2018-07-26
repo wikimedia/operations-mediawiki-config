df5b35873914de57adfcf15332117a2a0d042241
Marostegui <marostegui@wikimedia.org>
2018-07-26 09:01:34 +0000

    Revert "db-eqiad.php: Depool db1090:3317"
    This reverts commit cdaffb226a79226473925e33dcaccf65a83d1507.
    
    Change-Id: I375de8048ea7db084e7e03a3a8843d81b36beddf

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -158,9 +158,9 @@ $wgLBFactoryConf = [
 	],
 	's7' => [
 		'db1062' => 0,      # D4 2.8TB 128GB, master
-		'db1079' => 300,    # A2 3.6TB 512GB, api # master for sanitarium db1125 # temporary vslow
+		'db1079' => 300,    # A2 3.6TB 512GB, api # master for sanitarium db1125
 		'db1086' => 400,    # B3 3.6TB 512GB, api # candidate master
-		# 'db1090:3317' => 1, # C3 3.6TB 512GB, vslow, dump: s2 and s7, old master
+		'db1090:3317' => 1, # C3 3.6TB 512GB, vslow, dump: s2 and s7, old master
 		'db1094' => 500,    # D2 3.6TB 512GB
 		'db1098:3317' => 1, # B5 3.6TB 512GB # rc, log: s6 and s7
 		'db1101:3317' => 1, # C2 3.6TB 512GB # rc, log: s7 and s8
@@ -440,10 +440,10 @@ $wgLBFactoryConf = [
 	],
 	's7' => [
 		'vslow' => [
-			'db1079' => 1,
+			'db1090:3317' => 1,
 		],
 		'dump' => [
-			'db1079' => 1,
+			'db1090:3317' => 1,
 		],
 		'api' => [
 			'db1079' => 3,
