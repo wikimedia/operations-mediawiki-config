ad2ae587d5a5876441ba85730ff3ab7276351a52
Reedy <reedy@wikimedia.org>
2018-07-27 01:20:58 +0100

    wfLoadExtension for Sentry and LiquidThreads
    Change-Id: Iaaa37f79834285746363b3861c3a73f52e2e98b2

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -2023,7 +2023,7 @@ if ( $wmgUseLivePreview ) {
 }
 
 if ( $wmgUseSentry ) {
-	require_once "$IP/extensions/Sentry/Sentry.php";
+	wfLoadExtension( 'Sentry' );
 	$wgSentryDsn = $wmgSentryDsn;
 	$wgSentryLogPhpErrors = false;
 }
