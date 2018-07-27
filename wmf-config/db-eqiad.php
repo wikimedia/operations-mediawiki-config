3b28e7f15743f431ef4587bf63358cda3986efd0
Jaime Crespo <jcrespo@wikimedia.org>
2018-07-27 16:25:10 +0200

    mariadb: Repool db1094 with low load after reimage
    Change-Id: I9e3a6a5ca4ca56b95d2da1cd9c86ff55a341ddc7

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -161,7 +161,7 @@ $wgLBFactoryConf = [
 		'db1079' => 300,    # A2 3.6TB 512GB, api # master for sanitarium db1125
 		'db1086' => 400,    # B3 3.6TB 512GB, api # candidate master
 		'db1090:3317' => 50, # C3 3.6TB 512GB, vslow, dump: s2 and s7, old master
-		# 'db1094' => 500,    # D2 3.6TB 512GB
+		'db1094' => 50,     # D2 3.6TB 512GB
 		'db1098:3317' => 50, # B5 3.6TB 512GB # rc, log: s6 and s7
 		'db1101:3317' => 50, # C2 3.6TB 512GB # rc, log: s7 and s8
 	],
