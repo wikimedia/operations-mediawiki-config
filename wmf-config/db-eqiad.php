f90fa1acbf53a229a6eb2df2fbe685497b6c8552
Manuel Arostegui <marostegui@wikimedia.org>
2018-07-30 09:36:04 +0200

    db-eqiad.php: Pool db1120 with some weight
    Let's warm it up and give it some traffic
    
    Change-Id: Ifc13c82ed2d7bcf8dc2d207f01ef5a1f9df06d04

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -675,8 +675,8 @@ $wgLBFactoryConf = [
 	# ExtensionStore shard1
 	'extension1' => [
 		'10.64.0.108' => 0, # db1069, A1 2.8TB 160GB # master
-		'10.64.48.19' => 1, # db1064, D1 2.8TB 160GB
-		# '10.64.32.11' => 1, # db1120, C5 3.6TB 512GB
+		'10.64.48.19' => 2, # db1064, D1 2.8TB 160GB
+		'10.64.32.11' => 1, # db1120, C5 3.6TB 512GB
 	],
 ],
 
