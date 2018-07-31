023ded8daf40e3f7c001166fcad8a18447677b8d
Zoranzoki21 <zorandori4444@gmail.com>
2018-07-31 17:54:03 +0000

    Enable TemplateStyles on Meta-Wiki
    Bug: T200613
    Change-Id: I865e47e60634b9e6841eacbf489a1116a7fc6673

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -20603,6 +20603,7 @@ $wgConf->settings = [
 
 'wmgUseTemplateStyles' => [
 	'default' => false,
+	'metawiki' => true, // T200613
 	'testwiki' => true,
 	'mediawikiwiki' => true,
 	'labswiki' => true,
