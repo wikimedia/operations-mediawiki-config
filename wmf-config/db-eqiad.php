8d953dae9c602739021db8a004b7e0fcea0cce20
Manuel Arostegui <marostegui@wikimedia.org>
2018-08-02 07:39:29 +0200

    db-eqiad.php: Depool db1119
    alter
    
    Change-Id: Ibc638916b7a0d788144a7b8e4fe92ca52b530a4c

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -111,7 +111,7 @@ $wgLBFactoryConf = [
 		'db1105:3311' => 1, # C3 3.6TB 512GB # rc, log: s1 and s2
 		'db1106' => 50,     # D3 3.6TB 512GB, vslow, dump # master for sanitarium db1124
 		'db1114' => 200,    # D4 3.6TB 512GB, api
-		'db1119' => 200,    # B8 3.6TB 512GB, api
+		# 'db1119' => 200,    # B8 3.6TB 512GB, api # alter
 	],
 	's2' => [
 		'db1066' => 0,     # A6 2.8TB 160GB # master
@@ -283,7 +283,7 @@ $wgLBFactoryConf = [
 		'api' => [
 			'db1080' => 1,
 			'db1114' => 1,
-			'db1119' => 1,
+			# 'db1119' => 1,
 		],
 	],
 	's2' => [
