07ac269aabf0e007bead794aecba271ec97b7916
Jforrester <jforrester@wikimedia.org>
2018-08-06 23:43:23 +0000

    Re-apply "PageImages: Make it possible to add extra namespaces, part II"
    This reverts commit e31b3d308ffb2b0d9b3b5a0b27a9178e9b19c80d.
    
    Change-Id: I3471219e5515c5756c5ff4f816ff87ffa2a252a5

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -3256,6 +3256,12 @@ if ( $wmgUsePageAssessments ) {
 
 if ( $wmgUsePageImages ) {
 	wfLoadExtension( 'PageImages' );
+	if ( $wmgPageImagesExtraNamespaces ) {
+		$wgPageImagesNamespaces = array_merge(
+			[ 0 ],
+			$wmgPageImagesExtraNamespaces
+		);
+	}
 }
 
 if ( $wmgUseSearchExtraNS ) {
