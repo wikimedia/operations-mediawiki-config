90bcde52958b55d447bad157ca1401da8b9a0c4a
Jaime Crespo <jcrespo@wikimedia.org>
2018-08-13 10:33:22 +0200

    mariadb: Depool db1101 for maintenance
    Depool both s7 and s8 instances at db1101 for upgrade and restart.
    
    Change-Id: I55d8a86ec24c91a13f6d6bdd4dcd13f68084c6f8

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -162,14 +162,14 @@ $wgLBFactoryConf = [
 		'db1090:3317' => 1, # C3 3.6TB 512GB, vslow, dump: s2 and s7, old master
 		'db1094' => 500,    # D2 3.6TB 512GB
 		'db1098:3317' => 1, # B5 3.6TB 512GB # rc, log: s6 and s7
-		'db1101:3317' => 1, # C2 3.6TB 512GB # rc, log: s7 and s8
+		# 'db1101:3317' => 1, # C2 3.6TB 512GB # rc, log: s7 and s8
 	],
 	's8' => [
 		'db1071' => 0,      # D1 2.8TB 160GB, master
 		'db1087' => 1,      # C2 3.6TB 512GB, vslow, dump # master for sanitarium db1124
 		'db1092' => 200,    # D2 3.6TB 512GB, api
 		'db1099:3318' => 1, # B2 3.6TB 512GB # rc, log: s1 and s8
-		'db1101:3318' => 1, # C2 3.6TB 512GB # rc, log: s7 and s8
+		# 'db1101:3318' => 1, # C2 3.6TB 512GB # rc, log: s7 and s8
 		'db1104' => 400,    # B3 3.6TB 512GB, api # candidate master
 		'db1109' => 500,    # D8 3.6TB 512GB
 	],
@@ -450,23 +450,23 @@ $wgLBFactoryConf = [
 		],
 		'watchlist' => [
 			'db1098:3317' => 1,
-			'db1101:3317' => 1,
+			# 'db1101:3317' => 1,
 		],
 		'recentchanges' => [
 			'db1098:3317' => 1,
-			'db1101:3317' => 1,
+			# 'db1101:3317' => 1,
 		],
 		'recentchangeslinked' => [
 			'db1098:3317' => 1,
-			'db1101:3317' => 1,
+			# 'db1101:3317' => 1,
 		],
 		'contributions' => [
 			'db1098:3317' => 1,
-			'db1101:3317' => 1,
+			# 'db1101:3317' => 1,
 		],
 		'logpager' => [
 			'db1098:3317' => 1,
-			'db1101:3317' => 1,
+			# 'db1101:3317' => 1,
 		],
 	],
 	's8' => [
@@ -482,23 +482,23 @@ $wgLBFactoryConf = [
 		],
 		'watchlist' => [
 			'db1099:3318' => 1,
-			'db1101:3318' => 1,
+			# 'db1101:3318' => 1,
 		],
 		'recentchanges' => [
 			'db1099:3318' => 1,
-			'db1101:3318' => 1,
+			# 'db1101:3318' => 1,
 		],
 		'recentchangeslinked' => [
 			'db1099:3318' => 1,
-			'db1101:3318' => 1,
+			# 'db1101:3318' => 1,
 		],
 		'contributions' => [
 			'db1099:3318' => 1,
-			'db1101:3318' => 1,
+			# 'db1101:3318' => 1,
 		],
 		'logpager' => [
 			'db1099:3318' => 1,
-			'db1101:3318' => 1,
+			# 'db1101:3318' => 1,
 		],
 	],
 ],
