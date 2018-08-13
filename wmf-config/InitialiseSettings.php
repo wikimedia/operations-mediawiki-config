cfe5b31b1d3c9c2b3c46a93c97edb2aaa4be2cdf
Guillaume Lederrey <guillaume.lederrey@gmail.com>
2018-08-13 20:05:14 +0200

    [cirrus] switch search traffic to codfw
    eqiad elasticsearch cluster is having latency issues during reimaging
    of servers, let's move traffic to cofdw for now.
    
    Change-Id: I6be1b2a7f1aefdf5c1d8efeef846bede4ee66035

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -18915,7 +18915,7 @@ $wgConf->settings = [
 // $wgTranslateTranslationDefaultService in CommonSettings.php if you plan to
 // bring down a specific cluster.
 'wmgCirrusSearchDefaultCluster' => [
-	'default' => 'eqiad',
+	'default' => 'codfw',
 ],
 
 'wmgCirrusSearchClusterOverrides' => [
