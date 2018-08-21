4ecccdb62f928b3784e2d346f89aada05b565c4a
MarcoAurelio <maurelio@tools.wmflabs.org>
2018-08-21 11:07:26 +0200

    Configure gendered namespaces for pl.wiktionary
    Bug: T202347
    Change-Id: Ib37a73be6acb9a876350ec759349210da17ecc3b

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -4269,6 +4269,13 @@ $wgConf->settings = [
 	],
 	'+plwiktionary' => [
 		'WS' => NS_PROJECT,
+		'Wikipedysta' => NS_USER, // T202347
+		'Wikipedystka' => NS_USER, // T202347
+		'Wikisłownikarz' => NS_USER, // T202347
+		'Dyskusja_wikipedysty' => NS_USER_TALK, // T202347
+		'Dyskusja_wikipedystki' => NS_USER_TALK, // T202347
+		'Dyskusja_wikisłownikarza' => NS_USER_TALK, // T202347
+		'Wikidyskusja' => NS_PROJECT_TALK, // ported from $wgExtraNamespaces, wrongly added there
 	],
 	'+pswikivoyage' => [
 		'وس' => NS_PROJECT, // T197507
@@ -6858,9 +6865,6 @@ $wgConf->settings = [
 		115 => 'ଆଧାର_ଆଲୋଚନା',
 	],
 	'plwiktionary' => [
-		NS_PROJECT_TALK => 'Wikidyskusja',
-		NS_USER => 'Wikipedysta',
-		NS_USER_TALK => 'Dyskusja_wikipedysty',
 		100 => 'Aneks',
 		101 => 'Dyskusja_aneksu',
 		102 => 'Indeks',
@@ -16371,9 +16375,9 @@ $wgConf->settings = [
 		NS_USER => [ 'male' => 'Wikireporter', 'female' => 'Wikireporterka' ],
 		NS_USER_TALK => [ 'male' => 'Dyskusja_wikireportera', 'female' => 'Dyskusja_wikireporterki' ],
 	],
-	'plwiktionary' => [
-		NS_USER => [ 'male' => 'Wikipedysta', 'female' => 'Wikipedystka' ],
-		NS_USER_TALK => [ 'male' => 'Dyskusja_wikipedysty', 'female' => 'Dyskusja_wikipedystki' ],
+	'plwiktionary' => [ // T202347
+		NS_USER => [ 'male' => 'Wikisłownikarz', 'female' => 'Wikisłownikarka' ],
+		NS_USER_TALK => [ 'male' => 'Dyskusja_wikisłownikarza', 'female' => 'Dyskusja_wikisłownikarki' ],
 	],
 	'ptwiki' => [
 		NS_USER => [ 'male' => 'Usuário', 'female' => 'Usuária' ],
