ddd741259ba2118edb96286b711ea9b34225fc0a
Jcrespo <jcrespo@wikimedia.org>
2018-08-23 14:50:44 +0000

    Revert "mariadb: depool db1113 for maintenance"
    This reverts commit 561b917ea5ee6a036930e488964f45085df7e146.
    
    Change-Id: Ie960dc1486986d249f0066ec3494730c61fabd93

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -143,17 +143,17 @@ $wgLBFactoryConf = [
 		'db1096:3315' => 1, # A6 3.6TB 512GB # rc, log: s5 and s6
 		'db1097:3315' => 1, # D1 3.6TB 512GB # rc, log: s4 and s5
 		'db1100' => 100,    # C2 3.6TB 512GB, old master #api # candidate master
-		'db1110' => 500,    # C3 3.6TB 512GB, temp. vslow
-		# 'db1113:3315' => 1, # B8 3.6TB 512GB # vslow, dump: s5 and s6
+		'db1110' => 500,    # C3 3.6TB 512GB
+		'db1113:3315' => 1, # B8 3.6TB 512GB # vslow, dump: s5 and s6
 	],
 	's6' => [
 		'db1061' => 0,      # C3 2.8TB 128GB, master
 		'db1085' => 300,    # B3 3.6TB 512GB, api # master for sanitarium db1125
-		'db1088' => 500,    # C2 3.6TB 512GB, temp. vslow
+		'db1088' => 500,    # C2 3.6TB 512GB
 		'db1093' => 300,    # D2 3.6TB 512GB, api # candidate master
 		'db1096:3316' => 1, # A6 3.6TB 512GB # rc, log: s5 and s6
 		'db1098:3316' => 1, # B5 3.6TB 512GB # rc, log: s6 and s7
-		# 'db1113:3316' => 1, # B8 3.6TB 512GB # vslow, dump: s5 and s6
+		'db1113:3316' => 1, # B8 3.6TB 512GB # vslow, dump: s5 and s6
 	],
 	's7' => [
 		'db1062' => 0,      # D4 2.8TB 128GB, master
@@ -375,10 +375,10 @@ $wgLBFactoryConf = [
 	],
 	's5' => [
 		'vslow' => [
-			'db1110' => 1,
+			'db1113:3315' => 1,
 		],
 		'dump' => [
-			'db1110' => 1,
+			'db1113:3315' => 1,
 		],
 		'api' => [
 			'db1082' => 1,
@@ -407,10 +407,10 @@ $wgLBFactoryConf = [
 	],
 	's6' => [
 		'vslow' => [
-			'db1088' => 1,
+			'db1113:3316' => 1,
 		],
 		'dump' => [
-			'db1088' => 1,
+			'db1113:3316' => 1,
 		],
 		'api' => [
 			'db1085' => 10,
