3abb62ce917a0aa3dbcbde28e77f4838a598d0a9
Marostegui <marostegui@wikimedia.org>
2018-08-24 09:02:47 +0000

    Revert "db-eqiad.php: Depool db1122"
    This reverts commit 528c76060244c08a4ece102bffa822d172d5bd67.
    
    Change-Id: Icc8c7dab72ef427501b53fee68874314d248853f

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -115,12 +115,12 @@ $wgLBFactoryConf = [
 	],
 	's2' => [
 		'db1066' => 0,      # A6 2.8TB 160GB # master
-		'db1074' => 200,    # A2 3.6TB 512GB, api # master for sanitarium db1125
+		'db1074' => 300,    # A2 3.6TB 512GB, api # master for sanitarium db1125
 		'db1076' => 500,    # B1 3.6TB 512GB # candidate master
 		'db1090:3312' => 1, # C3 3.6TB 512GB, vslow, dump: s2 and s7
-		# 'db1122' => 500,    # D6 3.6TB 512GB, api # alter
-		'db1103:3312' => 150, # A3 3.6TB 512GB # rc, log: s2 and s4
-		'db1105:3312' => 150, # C3 3.6TB 512GB # rc, log: s1 and s2
+		'db1122' => 500,    # D6 3.6TB 512GB, api
+		'db1103:3312' => 1, # A3 3.6TB 512GB # rc, log: s2 and s4
+		'db1105:3312' => 1, # C3 3.6TB 512GB # rc, log: s1 and s2
 	],
 	/* s3 */ 'DEFAULT' => [
 		'db1075' => 0,      # A2 3.6TB 512GB, master
@@ -295,7 +295,7 @@ $wgLBFactoryConf = [
 		],
 		'api' => [
 			'db1074' => 10,
-			# 'db1122' => 1,
+			'db1122' => 1,
 		],
 		'watchlist' => [
 			'db1103:3312' => 1,
