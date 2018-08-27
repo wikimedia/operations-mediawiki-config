7f513f1fd89891353a5e6ceb4fa457e2dc1c9a42
Marostegui <marostegui@wikimedia.org>
2018-08-27 09:21:19 +0000

    Revert "db-eqiad.php: Depool db1109"
    This reverts commit 188c7e968d28be8f3c17fe0d29ae3c6c2c9a3613.
    
    Change-Id: I2dad7e77de4ce81ac0f1dadcca2b0dc32a827295

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -166,12 +166,12 @@ $wgLBFactoryConf = [
 	],
 	's8' => [
 		'db1071' => 0,      # D1 2.8TB 160GB, master
-		'db1087' => 100,      # C2 3.6TB 512GB, vslow, dump # master for sanitarium db1124
+		'db1087' => 1,      # C2 3.6TB 512GB, vslow, dump # master for sanitarium db1124
 		'db1092' => 200,    # D2 3.6TB 512GB, api
-		'db1099:3318' => 150, # B2 3.6TB 512GB # rc, log: s1 and s8
-		'db1101:3318' => 150, # C2 3.6TB 512GB # rc, log: s7 and s8
+		'db1099:3318' => 1, # B2 3.6TB 512GB # rc, log: s1 and s8
+		'db1101:3318' => 1, # C2 3.6TB 512GB # rc, log: s7 and s8
 		'db1104' => 400,    # B3 3.6TB 512GB, api # candidate master
-		# 'db1109' => 500,    # D8 3.6TB 512GB # alter
+		'db1109' => 500,    # D8 3.6TB 512GB
 	],
 
 	'wikitech' => [
