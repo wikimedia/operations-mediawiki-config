2ade9c1093a7c07b5345b4e16a6ed907ff0278db
Stephane Bisson <sbisson@wikimedia.org>
2018-09-06 16:15:50 -0400

    Enable PageTriage AfC on enwiki
    Bug: T203184
    Change-Id: I656d9811b58eb7f1355025abbc19e2f52cdbc521

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -17813,6 +17813,7 @@ $wgConf->settings = [
 'wgPageTriageDraftNamespaceId' => [
 	'default' => false,
 	'testwiki' => 118,
+	'enwiki' => 118, // T203184
 ],
 'wgPageTriageEnableOresFilters' => [
 	'default' => false,
