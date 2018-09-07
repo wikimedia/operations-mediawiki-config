1685a9becfdaeac90dbb13cec5748c3dcc4cf4d9
Tim Starling <tstarling@wikimedia.org>
2018-09-07 11:28:40 +1000

    Set PHP time limit
    It was previously infinity. For job runners and POST requests, use the
    existing value in the the Apache configuration. For GET requests, reduce
    the timeout to 60 seconds.
    
    Bug: T97192
    Change-Id: Ice702b46ab2a15eb2bc5df95efdf9951d45c0e13

---
+++ wmf-config/set-time-limit.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Configure timeouts. These should be slightly less than the Apache timeouts,
+ * so that the slightly more informative PHP error message is delivered to the
+ * user, and so that we can verify that PHP timeouts actually exist (T97192).
+ */
+function wmfSetTimeLimit() {
+	if ( PHP_SAPI === 'cli' ) {
+		// It should already be zero, and Maintenance.php should set it to zero
+	} else {
+		if ( defined( 'MEDIAWIKI_JOB_RUNNER' ) ) {
+			$host = isset( $_SERVER['HTTP_HOST'] ) ? $_SERVER['HTTP_HOST'] : '';
+			switch ( $host ) {
+				case 'videoscaler.svc.eqiad.wmnet':
+				case 'videoscaler.discovery.wmnet':
+					set_time_limit( 86400 );
+					break;
+
+				default:
+					set_time_limit( 1200 );
+			}
+		} elseif ( $_SERVER['REQUEST_METHOD'] === 'POST' ) {
+			set_time_limit( 200 );
+		} else {
+			set_time_limit( 60 );
+		}
+	}
+}
+
+wmfSetTimeLimit();
