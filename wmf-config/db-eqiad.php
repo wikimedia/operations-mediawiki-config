621f8788de0bcd7d46287edd3b0e409bbee28924
Jaime Crespo <jcrespo@wikimedia.org>
2018-09-12 10:33:53 +0200

    mariadb: Depool db1121 for maintenance
    It will be restarted and upgraded to 10.1.36.
    
    Change-Id: Ie14b6f9b3bbd7530e0e78040c7ea4255f335914b

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -135,7 +135,7 @@ $wgLBFactoryConf = [
 		'db1091' => 500,     # D2 3.6TB 512GB
 		'db1097:3314' => 50, # D1 3.6TB 512GB # rc, log: s4 and s5
 		'db1103:3314' => 50, # A3 3.6TB 512GB # rc, log: s2 and s4
-		'db1121' => 1,      # C6 3.6TB 512GB, vslow, dump # master for sanitarium db1125
+		# 'db1121' => 1,      # C6 3.6TB 512GB, vslow, dump # master for sanitarium db1125
 	],
 	's5' => [
 		'db1070' => 0,      # D1 2.8TB 160GB, master
@@ -343,10 +343,10 @@ $wgLBFactoryConf = [
 	],
 	's4' => [
 		'vslow' => [
-			'db1121' => 1,
+			'db1103:3314' => 1,
 		],
 		'dump' => [
-			'db1121' => 1,
+			'db1103:3314' => 1,
 		],
 		'api' => [
 			'db1081' => 3,
