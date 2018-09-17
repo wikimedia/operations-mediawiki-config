07a6db7605741baa8ca81d793a39a1a69a7edd7d
Brad Jorsch <bjorsch@wikimedia.org>
2018-09-17 12:08:15 -0400

    Set ActorTableSchemaMigrationStage => WRITE_BOTH on test wikis, mw.org
    Bug: T188327
    Change-Id: I6db2762e67abfc96061456ba5f78fad0109f7e4e

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -20794,6 +20794,14 @@ $wgConf->settings = [
 	'default' => true, // T199800
 ],
 
+'wgActorTableSchemaMigrationStage' => [
+	'default' => MIGRATION_OLD,
+	'testwiki' => MIGRATION_WRITE_BOTH,
+	'test2wiki' => MIGRATION_WRITE_BOTH,
+	'testwikidatawiki' => MIGRATION_WRITE_BOTH,
+	'mediawikiwiki' => MIGRATION_WRITE_BOTH,
+],
+
 'wgCommentTableSchemaMigrationStage' => [
 	'default' => MIGRATION_WRITE_BOTH,
 ],
