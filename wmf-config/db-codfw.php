aa19806c69151a0742203bc902561ea97a04a1fe
Marostegui <marostegui@wikimedia.org>
2018-09-19 05:31:28 +0000

    Revert "db-codfw.php: Depool db2074"
    This reverts commit 21a4d14d5ff6a7ab5933a852a1fcb5e06d0206c2.
    
    Change-Id: I5dc916f9a7b8b1dc63c81822191658153193d4a2

--- wmf-config/db-codfw.php
+++ wmf-config/db-codfw.php
@@ -127,7 +127,7 @@ $wgLBFactoryConf = [
 		'db2036'      => 50,  # C6 2.9TB 160GB, old master
 		'db2050'      => 50,  # C6 2.9TB 160GB dump (inactive), vslow
 		'db2057'      => 150, # D6 3.3TB 160GB # rc, log # candidate master
-		# 'db2074'      => 400, # D6 3.3TB 512GB # InnoDB compressed # master for sanitarium db2094
+		'db2074'      => 400, # D6 3.3TB 512GB # InnoDB compressed # master for sanitarium db2094
 	],
 	's4' => [
 		'db2051'      => 0,   # B8 2.9TB 160GB, master
