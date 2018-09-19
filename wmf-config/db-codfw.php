588a0344d609b2a93d6edf6bc35cd4ef99865166
Manuel Arostegui <marostegui@wikimedia.org>
2018-09-19 07:07:46 +0200

    db-codfw.php: Depool db2057
    Going to drop the renamed echo_XXX tables
    
    Bug: T153638
    Change-Id: Ib28a1d6f9cc836d23a4f5c4e00d35cea0082fa91

--- wmf-config/db-codfw.php
+++ wmf-config/db-codfw.php
@@ -126,8 +126,8 @@ $wgLBFactoryConf = [
 		'db2043'      => 0,   # C6 2.9TB 160GB, master
 		'db2036'      => 50,  # C6 2.9TB 160GB, old master
 		'db2050'      => 50,  # C6 2.9TB 160GB dump (inactive), vslow
-		'db2057'      => 150, # D6 3.3TB 160GB # rc, log # candidate master
-		'db2074'      => 400, # D6 3.3TB 512GB # InnoDB compressed # master for sanitarium db2094
+		# 'db2057'      => 150, # D6 3.3TB 160GB # rc, log # candidate master
+		'db2074'      => 400, # D6 3.3TB 512GB # InnoDB compressed # master for sanitarium db2094 # temporary rc
 	],
 	's4' => [
 		'db2051'      => 0,   # B8 2.9TB 160GB, master
@@ -330,19 +330,19 @@ $wgLBFactoryConf = [
 			'db2050' => 1,
 		],
 		'watchlist' => [
-			'db2057' => 1,
+			'db2074' => 1,
 		],
 		'recentchanges' => [
-			'db2057' => 1,
+			'db2074' => 1,
 		],
 		'recentchangeslinked' => [
-			'db2057' => 1,
+			'db2074' => 1,
 		],
 		'contributions' => [
-			'db2057' => 1,
+			'db2074' => 1,
 		],
 		'logpager' => [
-			'db2057' => 1,
+			'db2074' => 1,
 		],
 	],
 	's4' => [
