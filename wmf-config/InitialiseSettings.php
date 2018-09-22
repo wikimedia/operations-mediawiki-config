dfe5dcab842d98e0a536ae0082fba5b27c99aa9f
Max Semenik <maxsem.wiki@gmail.com>
2018-09-21 19:17:29 -0700

    Remove old ArticleCreationWorkflows config
    Now that Ibe04a3ad01cfadd903eff58faa33913e4c5167e0 is live,
    this can deployed.
    
    * Remove old-style config.
    * Remove all labs-specific config and let it use the prod one.
    
    Bug: T204016
    Change-Id: Icaa57ee37124bc051aa8761efc63ded239b5aab8

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -21707,22 +21707,6 @@ $wgConf->settings = [
 	'enwiki' => true, // T192455
 ],
 
-'wgArticleCreationWorkflows' => [
-	'default' => [],
-	'test2wiki' => [
-		[
-			'namespaces' => [ 0 ],
-			'excludeRight' => 'autoconfirmed'
-		],
-	],
-	'enwiki' => [
-		[
-			'namespaces' => [ 0 ],
-			'excludeRight' => 'autoconfirmed'
-		],
-	],
-],
-
 'wgArticleCreationLandingPage' => [
 	'default' => 'Project:New user landing page',
 ],
