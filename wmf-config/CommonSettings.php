e93689b6bf33e718d2f8929afb1e315a638c1521
Thomas Arrow <thomas.arrow_ext@wikimedia.de>
2018-09-27 08:05:28 +0100

    RejectParserCacheValue Hook to purge wikidatawiki to 2018-09-15
    Bug: T205330
    Change-Id: Ia587969b982cc697da2c987048969ad28cfa2fc5

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -410,6 +410,16 @@ $wgObjectCaches['mysql-multiwrite'] = [
 	'reportDupes' => false
 ];
 
+// T205330: Purge wikidatawiki parser output from before 2018-09-15
+if ( $wgDBname === 'wikidatawiki' ) {
+	$wgHooks['RejectParserCacheValue'][] = function ( $value, $wikiPage, $popts ) {
+		if ( $value->expired( '20180915200000' ) ) {
+			return false;
+		}
+		return true;
+	};
+}
+
 $wgSessionsInObjectCache = true;
 session_name( $lang . 'wikiSession' );
 
