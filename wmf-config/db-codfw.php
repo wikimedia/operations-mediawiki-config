4dabdd3c83eba3f3692b7d00deebbfeddc1181c7
Jcrespo <jcrespo@wikimedia.org>
2018-10-01 12:55:16 +0200

    mariadb: Emergency depool db2058, issues?
    Change-Id: Iccf158409276f095373ae04989efecdb28ea3a73

--- wmf-config/db-codfw.php
+++ wmf-config/db-codfw.php
@@ -131,7 +131,7 @@ $wgLBFactoryConf = [
 	],
 	's4' => [
 		'db2051'      => 0,   # B8 2.9TB 160GB, master
-		'db2058'      => 50,  # D6 3.3TB 160GB, dump (inactive), vslow # candidate master
+		# 'db2058'      => 50,  # D6 3.3TB 160GB, dump (inactive), vslow # candidate master
 		'db2065'      => 50,  # D6 3.3TB 160GB, api
 		'db2073'      => 500, # C6 3.3TB 512GB, api # Compressed InnoDB # master for sanitarium db2095
 		'db2084:3314' => 300, # D6 3.3TB 512GB # rc, log: s4 and s5
@@ -372,10 +372,10 @@ $wgLBFactoryConf = [
 			'db2091:3314' => 1,
 		],
 		'dump' => [
-			'db2058' => 1,
+			'db2065' => 1,
 		],
 		'vslow' => [
-			'db2058' => 1,
+			'db2065' => 1,
 		],
 		'api' => [
 			'db2065' => 1,
