3e528a2cea9d0b45db9e674e5d85dcd083595ba6
Manuel Arostegui <marostegui@wikimedia.org>
2018-10-03 08:30:42 +0200

    db-codfw.php: Depool db2055
    alter table
    
    Bug: T205913
    Change-Id: I1c4075193174df07d3ef7e02c6e51886192c8965

--- wmf-config/db-codfw.php
+++ wmf-config/db-codfw.php
@@ -104,7 +104,7 @@ $wgLBFactoryConf = [
 'sectionLoads' => [
 	's1' => [
 		'db2048'      => 0,   # A1 2.9TB 160GB, master
-		'db2055'      => 50,  # D6 3.3TB 160GB, dump (inactive), vslow, api # candidate master
+		# 'db2055'      => 50,  # D6 3.3TB 160GB, dump (inactive), vslow, api # candidate master # alter
 		'db2062'      => 50,  # B5 3.3TB 160GB, api
 		'db2070'      => 100, # C5 3.3TB 160GB
 		'db2071'      => 50,  # A6 3.6TB 512GB, api
@@ -277,13 +277,13 @@ $wgLBFactoryConf = [
 			'db2085:3311' => 1,
 		],
 		'dump' => [
-			'db2055' => 1,
+			'db2062' => 1,
 		],
 		'vslow' => [
-			'db2055' => 1,
+			'db2062' => 1,
 		],
 		'api' => [
-			'db2055' => 1,
+			# 'db2055' => 1,
 			'db2062' => 1,
 			'db2071' => 10,
 			'db2092' => 3,
