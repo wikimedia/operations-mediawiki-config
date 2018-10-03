a568164d872e2d057bfd912987f04c5e5f64ce3e
Timo Tijhof <krinklemail@gmail.com>
2018-10-03 19:27:07 +0100

    profiler: Move flush to a function and prep beta/prod consolidate (beta-only)
    This will share the code between beta and prod instead of duplicating
    in two places. It is also a step toward having different flush in
    PHP7 vs HHVM.
    
    For now, this uses the new file (which is otherwise logically identical)
    only Beta Cluster. Once it works fine there for a couple hours, the next
    commit applies it to production.
    
    Bug: T176916
    Change-Id: Id79baae9071ccef64af9ce362ef14d327de9e05b

---
+++ wmf-config/arclamp.php
@@ -0,0 +1,87 @@
+<?php
+# WARNING: This file is publicly viewable on the web. Do not put private data here.
+#
+# Included by PhpAutoPrepend.php BEFORE any other wmf-config or MediaWiki file.
+# MUST NOT use any predefined state, only plain PHP.
+#
+# This both for PRODUCTION and for Beta Cluster.
+#
+
+/**
+ * Flush data from Xenon, the sampling profiler for production traffic.
+ *
+ * If Xenon is enabled, ask HHVM for recently collected data.
+ * Will not actually yielding something every request, only at particular
+ * intervals controlled by hhvm.xenon.period.
+ *
+ * Based on https://github.com/wikimedia/arc-lamp
+ *
+ * @param string $redisHost Redis hostname
+ * @param int $redisPort Redis port
+ * @param float $redisTimeout Redis timeout (for socket reads)
+ */
+function wmfArcLampFlush( $redisHost, $redisPort, $redisTimeout ) {
+	if ( !extension_loaded( 'xenon' ) || !ini_get( 'hhvm.xenon.period' ) ) {
+		return;
+	}
+
+	$data = HH\xenon_get_data();
+	if ( !$data ) {
+		return;
+	}
+
+	$entryPoint = basename( $_SERVER['SCRIPT_NAME'] );
+	$reqMethod = '{' . $_SERVER['REQUEST_METHOD'] . '}';
+
+	// Collate stack samples and fold into single lines,
+	// in the format expected by FlameGraph.
+	$stacks = [];
+	foreach ( $data as $sample ) {
+		if ( empty( $sample['phpStack'] ) ) {
+			continue;
+		}
+		$stack = [];
+		foreach ( $sample['phpStack'] as $frame ) {
+			if ( $frame['function'] === 'include' ) {
+				// For file scope, just use the path as the name.
+				$func = $frame['file'];
+			} elseif ( $frame['function'] === '{closure}' && isset( $frame['line'] ) ) {
+				// Annotate anonymous functions with their location in the
+				// source code. Example: {closure:/path/to/file.php(123)}
+				$func = "{closure:{$frame['file']}({$frame['line']})}";
+			} else {
+				$func = $frame['function'];
+			}
+
+			if ( $func !== end( $stack ) ) {
+				$stack[] = $func;
+			}
+		}
+		if ( $stack ) {
+			// The last element is usually (but not always) the full file
+			// path of the script name. We want things nice and consistent,
+			// so we pop off the path if it is there, and push the basename
+			// instead.
+			if ( strpos( end( $stack ), $entryPoint ) !== false ) {
+				array_pop( $stack );
+			}
+			$stack[] = $reqMethod;
+			$stack[] = $entryPoint;
+
+			$strStack = implode( ';', array_reverse( $stack ) );
+			if ( !isset( $stacks[$strStack] ) ) {
+				$stacks[$strStack] = 0;
+			}
+			$stacks[$strStack] += 1;
+		}
+	}
+
+	$redis = new Redis();
+	$ok = $redis->connect( $redisHost, $redisPort, $redisTimeout );
+	if ( !$ok ) {
+		return;
+	}
+	foreach ( $stacks as $stack => $count ) {
+		$redis->publish( 'xenon', "$stack $count" );
+	}
+}
--- wmf-config/profiler-labs.php
+++ wmf-config/profiler-labs.php
@@ -7,7 +7,10 @@
 # Exposes:
 # - $wmgProfiler (used by CommonSettings.php)
 
+require_once __DIR__ . '/arclamp.php';
+
 global $wmgProfiler;
+
 $wmgProfiler = [];
 
 /**
@@ -93,70 +96,13 @@ if ( ini_get( 'hhvm.stats.enable_hot_profiler' ) ) {
 	unset( $xwd, $xhprofFlags );
 }
 
-/**
- * Sampling profiler for production traffic.
- *
- * If Xenon is enabled, register a shutdown callback to ask HHVM for
- * recently collected data. Will not end up yielding something every
- * request.
- *
- * Based on https://github.com/wikimedia/arc-lamp
- */
-if ( extension_loaded( 'xenon' ) && ini_get( 'hhvm.xenon.period' ) ) {
-	register_shutdown_function( function () {
-		$data = HH\xenon_get_data();
-		if ( empty( $data ) ) {
-			return;
-		}
-
-		$entryPoint = basename( $_SERVER['SCRIPT_NAME'] );
-		$reqMethod = '{' . $_SERVER['REQUEST_METHOD'] . '}';
-
-		// Collate stack samples and fold into single lines.
-		// This is the format expected by FlameGraph.
-		$stacks = [];
-		foreach ( $data as $sample ) {
-			if ( empty( $sample['phpStack'] ) ) {
-				continue;
-			}
-			$stack = [];
-			foreach ( $sample['phpStack'] as $frame ) {
-				if ( $frame['function'] === 'include' ) {
-					// For file scope, just use the path as the name.
-					$func = $frame['file'];
-				} elseif ( $frame['function'] === '{closure}' && isset( $frame['line'] ) ) {
-					// Annotate anonymous functions with their location in the
-					// source code. Example: {closure:/path/to/file.php(123)}
-					$func = "{closure:{$frame['file']}({$frame['line']})}";
-				} else {
-					$func = $frame['function'];
-				}
-				if ( $func !== end( $stack ) ) {
-					$stack[] = $func;
-				}
-			}
-			if ( count( $stack ) ) {
-				// The last element is usually (but not always) the full file
-				// path of the script name. We want things nice and consistent,
-				// so we pop off the path if it is there, and push the basename
-				// instead.
-				if ( strpos( end( $stack ), $entryPoint ) !== false ) {
-					array_pop( $stack );
-				}
-				$stack[] = $reqMethod;
-				$stack[] = $entryPoint;
-				$strStack = implode( ';', array_reverse( $stack ) );
-				if ( !isset( $stacks[$strStack] ) ) {
-					$stacks[$strStack] = 0;
-				}
-				$stacks[$strStack] += 1;
-			}
-		}
-		$redis = new Redis();
-		if ( $redis->connect( 'deployment-fluorine02.deployment-prep.eqiad.wmflabs', 6379, 1.0 ) ) {
-			foreach ( $stacks as $stack => $count ) {
-				$redis->publish( 'xenon', "$stack $count" );
-			}
-		}
-	} );
-}
+register_shutdown_function( function () {
+	wmfArcLampFlush(
+		// Redis host
+		'deployment-fluorine02.deployment-prep.eqiad.wmflabs',
+		// Redis port
+		6379,
+		// Redis timeout (for socket reads)
+		1.0
+	);
+} );
