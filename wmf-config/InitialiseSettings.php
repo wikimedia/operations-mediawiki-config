5ae2141dbc2eab69686bd125d3f28d16d447843f
Zoranzoki21 <zorandori4444@gmail.com>
2018-10-04 08:27:17 +0200

    Add permission "move-rootuserpages" to usergroup "eliminator" at ptwiki
    Bug: T205595
    Change-Id: I5d5a9c5ff0e30d5142e29030cb7a19ef1ac3059e

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -9602,6 +9602,7 @@ $wgConf->settings = [
 			'suppressredirect' => true,
 			'autoreviewer' => true,
 			'movefile' => true, // T161532
+			'move-rootuserpages' => true, // T205595
 		],
 		'rollbacker' => [
 			'rollback' => true,
