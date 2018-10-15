68457c699af794439ebb11f1aea8f5148d6af0d1
Manuel Arostegui <marostegui@wikimedia.org>
2018-10-15 10:16:28 +0200

    db-eqiad.php: Slowly repool db1104
    This host has been rebuilt
    
    Bug: T206743
    Change-Id: I9728c7468af870bb7c54f6f0e2d3199c9993cbab

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -175,7 +175,7 @@ $wgLBFactoryConf = [
 		'db1092' => 200,    # D2 3.6TB 512GB, api # no BBU T205514
 		'db1099:3318' => 100, # B2 3.6TB 512GB # rc, log: s1 and s8
 		'db1101:3318' => 100, # C2 3.6TB 512GB # rc, log: s7 and s8
-		# 'db1104' => 200,    # B3 3.6TB 512GB, api # candidate master # rebuilding T206743
+		'db1104' => 50,    # B3 3.6TB 512GB, api # candidate master # low weight after rebuilding T206743
 		'db1109' => 500,    # D8 3.6TB 512GB
 	],
 
@@ -482,8 +482,8 @@ $wgLBFactoryConf = [
 			'db1087' => 1,
 		],
 		'api' => [
-			'db1092' => 1, # reduced weight, normal weight is # 3
-			# 'db1104' => 1,
+			'db1092' => 2, # reduced weight, normal weight is # 3
+			'db1104' => 1,
 		],
 		'watchlist' => [
 			'db1099:3318' => 1,
