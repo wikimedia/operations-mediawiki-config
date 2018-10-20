0ed3a9684859f4fbaa5c2eb1217f19cd66b1af9d
Matěj Suchánek <matejsuchanek97@gmail.com>
2018-10-20 10:54:00 +0200

    Update several Wikidata-related configs
    Remove deleted properties, add wikibase-sense example and sandbox property.
    
    Change-Id: Ieac5bdfb56ff4c4662c9c657fd450df52039b949

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -19254,9 +19254,7 @@ $wgConf->settings = [
 		 */
 		134, // has dialect (DEPRECATED)
 		558, // unit symbol (DEPRECATED)
-		1962, // patron (DEPRECATED)
 		2315, // comment (DEPRECATED)
-		4237, // Heritage Building in Finland ID (DEPRECATED)
 		5130, // island of location (DEPRECATED)
 		/**
 		 * @see https://www.wikidata.org/w/index.php?oldid=335040857
@@ -19281,6 +19279,7 @@ $wgConf->settings = [
 		4047, // geo-shape
 		5188, // wikibase-lexeme
 		5189, // wikibase-form
+		5979, // wikibase-sense
 	],
 ],
 
@@ -19333,7 +19332,7 @@ $wgConf->settings = [
 
 'wgWBQualityConstraintsPropertiesWithViolatingQualifiers' => [
 	'default' => [],
-	'wikidatawiki' => [ 'P1855', 'P2271', 'P5192', 'P5193' ], // T183267
+	'wikidatawiki' => [ 'P1855', 'P2271', 'P5192', 'P5193', 'P5977' ], // T183267
 ],
 
 'wmgUseWikibaseLexeme' => [
