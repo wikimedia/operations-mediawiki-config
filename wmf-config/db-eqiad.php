1f9739fa5a6ebb8c3077b618b47f06b0764d8c22
Manuel Arostegui <marostegui@wikimedia.org>
2018-10-24 12:52:03 +0200

    db-eqiad.php: Restore db1092 and db1104 weight
    Finally getting them with their original weight
    
    Change-Id: I8be64d0b51d02525791fe1a40173bbd44b2ad419

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -175,7 +175,7 @@ $wgLBFactoryConf = [
 		'db1092' => 200,    # D2 3.6TB 512GB, api
 		'db1099:3318' => 100, # B2 3.6TB 512GB # rc, log: s1 and s8
 		'db1101:3318' => 100, # C2 3.6TB 512GB # rc, log: s7 and s8
-		'db1104' => 300,    # B3 3.6TB 512GB, api # candidate master
+		'db1104' => 400,    # B3 3.6TB 512GB, api # candidate master
 		'db1109' => 500,    # D8 3.6TB 512GB
 	],
 
@@ -482,7 +482,7 @@ $wgLBFactoryConf = [
 			'db1087' => 1,
 		],
 		'api' => [
-			'db1092' => 2,
+			'db1092' => 3,
 			'db1104' => 1,
 		],
 		'watchlist' => [
