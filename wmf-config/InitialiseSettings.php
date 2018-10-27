cdd8a0dac0464b5c91af79ee275ab6047ff707e8
addshore <addshorewiki@gmail.com>
2018-10-27 11:23:57 +0200

    Wikibase, create and use wmgWikibaseMaxSerializedEntitySize
    Change-Id: I6f645f6c7fc8f61d53f5a1f38bd8512f774ccbee

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -18908,6 +18908,10 @@ $wgConf->settings = [
 	'default' => [ 'lexeme', 'form', 'sense' ],
 ],
 
+'wmgWikibaseMaxSerializedEntitySize' => [
+	'default' => 2500,
+],
+
 // List of properties to be indexed by elasticsearch
 // see WikibaseSearchSettings.php
 'wmgWikibaseSearchIndexProperties' => [
