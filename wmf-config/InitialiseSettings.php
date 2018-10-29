4fabffbf4524d44ddfae7a0a4af467cbbecc707c
James D. Forrester <jforrester@wikimedia.org>
2018-10-29 12:24:32 -0700

    Enable WikibaseMediaInfo on Commons
    Bug: T159708
    Change-Id: I5aad0012196446226ed89cbc92ecb1d26265541a

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -19911,6 +19911,7 @@ $wgConf->settings = [
 // Feature flag for WikibaseMediaInfo during deployment
 'wgMediaInfoEnable' => [
 	'default' => false,
+	'commonswiki' => true,
 	'testcommonswiki' => true,
 ],
 
