46a3c9d0e1d1273759882b2f9768372d7afa51a5
Zoranzoki21 <zorandori4444@gmail.com>
2018-11-04 00:27:37 +0100

    Enable moving files for users with patrol and rollbacker rights on srwiki
    Bug: T208663
    Change-Id: I3ba2ab23b0efa06e3614d61b365f024122cde4aa

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -9863,9 +9863,9 @@ $wgConf->settings = [
 	'srwiki' => [
 		'user' => [ 'upload' => false ],
 		'autoconfirmed' => [ 'upload' => true ],
-		'patroller' => [ 'patrol' => true ],
+		'patroller' => [ 'patrol' => true, 'movefile' => true ], // T208663
 		'autopatrolled' => [ 'autopatrol' => true ],
-		'rollbacker' => [ 'rollback' => true ],
+		'rollbacker' => [ 'rollback' => true, 'movefile' => true ], // T208663
 		'flood' => [ 'bot' => true ],
 	],
 	'+srwikisource' => [
