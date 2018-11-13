83ad9951f9cac1492a8ef726a726704fa19cb65a
Marostegui <marostegui@wikimedia.org>
2018-11-13 10:31:19 +0000

    Revert "db-eqiad.php: Depool db1092"
    This reverts commit e715485496bf44c22d10b81f1ce8e4e764a70d38.
    
    Change-Id: I86688496058b9abb3c3fadf59be5c154db0bf1e7

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -172,10 +172,10 @@ $wgLBFactoryConf = [
 	's8' => [
 		'db1071' => 0,      # D1 2.8TB 160GB, master
 		'db1087' => 1,      # C2 3.6TB 512GB, vslow, dump # master for sanitarium db1124
-		# 'db1092' => 200,    # D2 3.6TB 512GB, api # T203709
-		'db1099:3318' => 200, # B2 3.6TB 512GB # rc, log: s1 and s8
-		'db1101:3318' => 200, # C2 3.6TB 512GB # rc, log: s7 and s8
-		'db1104' => 200,    # B3 3.6TB 512GB, api # candidate master
+		'db1092' => 200,    # D2 3.6TB 512GB, api
+		'db1099:3318' => 100, # B2 3.6TB 512GB # rc, log: s1 and s8
+		'db1101:3318' => 100, # C2 3.6TB 512GB # rc, log: s7 and s8
+		'db1104' => 400,    # B3 3.6TB 512GB, api # candidate master
 		'db1109' => 500,    # D8 3.6TB 512GB
 	],
 
@@ -483,7 +483,7 @@ $wgLBFactoryConf = [
 			'db1087' => 1,
 		],
 		'api' => [
-			# 'db1092' => 3,
+			'db1092' => 3,
 			'db1104' => 1,
 		],
 		'watchlist' => [
