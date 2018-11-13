94b31d8be8ac2152a1e5c3040c07dcdc5f1e2736
Stephen Niedzielski <stephen@niedzielski.com>
2018-11-13 07:50:11 -0700

    Prod: increase Schema.org page split test to 25% sampling
    Increase the sampling rate to 25%. Given the 50/50% split, 12.5% of main
    namespaces will now include a JSON-LD Article schema model of their
    content.
    
    Bug: T208755
    Change-Id: Id9b3cfea31b6eb203cdf73765512966eef87982c

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -18943,9 +18943,9 @@ $wgConf->settings = [
 	]
 ],
 'wmgWikibaseClientPageSchemaSplitTestSamplingRatio' => [
-	// 5% of pages on the Beta Cluster will be sampled. Half of those (.025) will receive the new
+	// 25% of pages on the Beta Cluster will be sampled. Half of those (.125) will receive the new
 	// treatment.
-	'default' => 0.05,
+	'default' => 0.25,
 ],
 'wmgWikibaseClientPageSchemaSplitTestBuckets' => [
 	// Pages are bucketed in [0, .5) for control and [.5, 1) for treatment. If a page is sampled
