e3ddee42af8bfca7a1d17cf4f0aabfd3bcd5d714
Manuel Arostegui <marostegui@wikimedia.org>
2018-11-19 11:02:04 +0100

    db-eqiad.php: Increase weight for db1078
    Change-Id: I2a34fa37e01954d9a4d2b2bb69080a98ef3bb128

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -131,7 +131,7 @@ $wgLBFactoryConf = [
 		'db1075' => 0,      # A2 3.6TB 512GB, master
 		'db1077' => 400,    # B1 3.6TB 512GB, rc, log # master for sanitarium db1124
 		'db1123' => 100,    # D8 3.6TB 512GB, vslow, dump # low weight
-		'db1078' => 200,    # C3 3.6TB 512GB, candidate master # low weight
+		'db1078' => 300,    # C3 3.6TB 512GB, candidate master # low weight
 	],
 	's4' => [
 		'db1068' => 0,      # D1 2.8TB 160GB, master
