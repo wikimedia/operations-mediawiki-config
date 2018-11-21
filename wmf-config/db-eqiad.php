e928e72e85332fbe9940997df36b2a081954fa36
Balazs Pocze <bpocze@wikimedia.org>
2018-11-21 14:27:12 +0100

    mariadb: depool db1085
    Due to a schema change
    
    Bug: T85757
    Change-Id: I50a69013eb8dccc0c69e15609952bb45114bfe80

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -153,11 +153,11 @@ $wgLBFactoryConf = [
 	],
 	's6' => [
 		'db1061' => 0,      # C3 2.8TB 128GB, master
-		'db1085' => 300,    # B3 3.6TB 512GB, api # master for sanitarium db1125
+		# 'db1085' => 300,  # T85757  # B3 3.6TB 512GB, api # master for sanitarium db1125
 		'db1088' => 500,    # C2 3.6TB 512GB
 		'db1093' => 300,    # D2 3.6TB 512GB, api # candidate master
-		'db1096:3316' => 1, # A6 3.6TB 512GB # rc, log: s5 and s6
-		'db1098:3316' => 1, # B5 3.6TB 512GB # rc, log: s6 and s7
+		'db1096:3316' => 150, # A6 3.6TB 512GB # rc, log: s5 and s6
+		'db1098:3316' => 150, # B5 3.6TB 512GB # rc, log: s6 and s7
 		'db1113:3316' => 1, # B8 3.6TB 512GB # vslow, dump: s5 and s6
 	],
 	's7' => [
@@ -419,7 +419,7 @@ $wgLBFactoryConf = [
 			'db1113:3316' => 1,
 		],
 		'api' => [
-			'db1085' => 10,
+			# 'db1085' => 10,
 			'db1093' => 1,
 		],
 		'watchlist' => [
