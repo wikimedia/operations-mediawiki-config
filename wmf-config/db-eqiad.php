ae70a4af27de6aad34be839c06e7870e0c48bd91
Banyek <bpocze@wikimedia.org>
2018-11-21 10:39:30 +0000

    Revert "mariadb: depool db1096"
    This reverts commit c3630a8cbbc029fdbeec09a9accaf5c869861662.
    
    Change-Id: I43c09a12eb3ff7f2ffce94eb888d4ac68e4a4b2a

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -156,7 +156,7 @@ $wgLBFactoryConf = [
 		'db1085' => 300,    # B3 3.6TB 512GB, api # master for sanitarium db1125
 		'db1088' => 500,    # C2 3.6TB 512GB
 		'db1093' => 300,    # D2 3.6TB 512GB, api # candidate master
-		# 'db1096:3316' => 1,# T85757 # A6 3.6TB 512GB # rc, log: s5 and s6
+		'db1096:3316' => 1, # A6 3.6TB 512GB # rc, log: s5 and s6
 		'db1098:3316' => 1, # B5 3.6TB 512GB # rc, log: s6 and s7
 		'db1113:3316' => 1, # B8 3.6TB 512GB # vslow, dump: s5 and s6
 	],
@@ -423,23 +423,23 @@ $wgLBFactoryConf = [
 			'db1093' => 1,
 		],
 		'watchlist' => [
-			# 'db1096:3316' => 1,
+			'db1096:3316' => 1,
 			'db1098:3316' => 1,
 		],
 		'recentchanges' => [
-			# 'db1096:3316' => 1,
+			'db1096:3316' => 1,
 			'db1098:3316' => 1,
 		],
 		'recentchangeslinked' => [
-			# 'db1096:3316' => 1,
+			'db1096:3316' => 1,
 			'db1098:3316' => 1,
 		],
 		'contributions' => [
-			# 'db1096:3316' => 1,
+			'db1096:3316' => 1,
 			'db1098:3316' => 1,
 		],
 		'logpager' => [
-			# 'db1096:3316' => 1,
+			'db1096:3316' => 1,
 			'db1098:3316' => 1,
 		],
 	],
