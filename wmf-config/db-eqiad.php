5e6710e2096f5ddff64b92f9c6fac0799b65db76
Manuel Arostegui <marostegui@wikimedia.org>
2018-11-23 07:19:54 +0100

    db-eqiad.php: Depool db1123
    alter table
    
    Bug: T86339
    Change-Id: I88bd7b8051fb271d5f015a249cfb459589a714a6

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -129,8 +129,8 @@ $wgLBFactoryConf = [
 	],
 	/* s3 */ 'DEFAULT' => [
 		'db1075' => 0,      # A2 3.6TB 512GB, master
-		'db1077' => 400,    # B1 3.6TB 512GB, rc, log # master for sanitarium db1124
-		'db1123' => 100,    # D8 3.6TB 512GB, vslow, dump # low weight
+		'db1077' => 400,    # B1 3.6TB 512GB, rc, log # master for sanitarium db1124 # temporary vslow
+		# 'db1123' => 100,    # D8 3.6TB 512GB, vslow, dump # low weight # T86339
 		'db1078' => 500,    # C3 3.6TB 512GB, candidate master
 	],
 	's4' => [
@@ -326,10 +326,10 @@ $wgLBFactoryConf = [
 	],
 	/* s3 */ 'DEFAULT' => [
 		'vslow' => [
-			'db1123' => 1,
+			'db1077' => 1,
 		],
 		'dump' => [
-			'db1123' => 1,
+			'db1077' => 1,
 		],
 		'watchlist' => [
 			'db1077' => 1,
