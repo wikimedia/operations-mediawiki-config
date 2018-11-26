d87471c8a44c8277b46a6234aa3af1ed9cbca5fc
WMDE-Fisch <christoph.jauera@wikimedia.de>
2018-11-26 15:54:01 +0100

    Make AdvancedSearch default on all wikis
    Bug: T207639
    Change-Id: If3552cc14eec305bc9ed7188fb67f42ae9b3232c

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -14418,12 +14418,7 @@ $wgConf->settings = [
 ],
 
 'wgAdvancedSearchBetaFeature' => [
-	'default' => true,
-	'group0' => false, // T207641
-	'arwiki' => false, // T207640
-	'dewiki' => false, // T207640
-	'fawiki' => false, // T207640
-	'huwiki' => false, // T207640
+	'default' => false, // T207639
 ],
 
 'wmgUseFileImporter' => [
