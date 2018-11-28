18063503b43429a4cf4583c546c32faf6d35f48e
Baha <bmansurov@wikimedia.org>
2018-11-28 14:31:31 -0500

    Enable reader trust survey
    Bug: T209882
    Change-Id: I0459a1c7c259b46bd99fd559881a6aaaeb52cf1b

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -21051,6 +21051,7 @@ $wgConf->settings = [
 // https://phabricator.wikimedia.org/T110661
 'wmgUseQuickSurveys' => [
 	'default' => false,
+	'enwiki' => true,
 	'frwiki' => true,
 	'cawiki' => true,
 	'enwikivoyage' => true,
@@ -21078,6 +21079,24 @@ $wgConf->settings = [
 			'shuffleAnswersDisplay' => true,
 		],
 	],
+	'enwiki' => [
+		// T209882
+		[
+			"enabled" => true,
+			"type" => "external",
+			"name" => "Reader-trust-survey-en-v1",
+			"description" => "Reader-trust-1-description",
+			"link" => "Reader-trust-1-link",
+			"instanceTokenParameterName" => "token",
+			"question" => "Reader-trust-1-message",
+			"privacyPolicy" => "Reader-trust-1-privacy",
+			"coverage" => 0.002,  // 1 out of 500
+			"platforms" => [
+				"desktop" => [ "stable" ],
+				"mobile" => [ "stable" ],
+			],
+		],
+	]
 ],
 
 'wmgUseSentry' => [
