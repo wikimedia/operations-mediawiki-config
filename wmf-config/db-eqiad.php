be61a49b532d3e437554c3553acdd6b00c2e795d
Manuel Arostegui <marostegui@wikimedia.org>
2018-11-29 14:21:29 +0100

    db-eqiad, db-codfw.php: Clarify sharding keys
    Add a comment to clarify how the parsercache keys are defined
    
    Change-Id: Iacb8f97de6ec7033212c0200e81d46831e60d3ab

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -8,9 +8,10 @@ if ( !defined( 'DBO_DEFAULT' ) ) {
 # $wgReadOnly = "Wikimedia Sites are currently read-only during maintenance, please try again soon.";
 
 $wmgParserCacheDBs = [
-	'10.64.0.12'   => '10.64.48.174', # pc1010, D3 4.4TB 256GB
-	'10.64.32.72'  => '10.64.16.20',  # pc1008, B8 4.4TB 256GB
-	'10.64.48.128' => '10.64.32.29',  # pc1009, C3 4.4TB 256GB
+	# 'sharding key' => 'server ip address' # DO NOT CHANGE THE SHARDING KEY - T210725
+	'10.64.0.12'   => '10.64.48.174', # pc1010, D3 4.4TB 256GB # pc1
+	'10.64.32.72'  => '10.64.16.20',  # pc1008, B8 4.4TB 256GB # pc2
+	'10.64.48.128' => '10.64.32.29',  # pc1009, C3 4.4TB 256GB # pc3
 ];
 
 $wmgOldExtTemplate = [
