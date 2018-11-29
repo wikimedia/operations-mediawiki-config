a213a0f4fed72786a459aba875b4b58f0323b6f6
Erik Bernhardson <ebernhardson@wikimedia.org>
2018-11-29 14:55:41 -0800

    Update wbsearchentities ab test configuration
    * Remove SQL version, it's sampling has been disabled
    * Split traffic evenly between the two remaining buckets
    
    Bug: T209402
    Change-Id: I18d94ed1e128696f37e54189015737dd3f88fd2a

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -21286,23 +21286,15 @@ $wgConf->settings = [
 		'enabled' => true,
 		'buckets' => [
 			'control' => [
-				'samplingRate' => 0.9,
+				'samplingRate' => 0.5,
 			],
 			'item_en_tuned' => [
-				'samplingRate' => 0.1,
+				'samplingRate' => 0.5,
 				'context' => 'item',
 				'language' => 'en',
 				'searchApiParameters' => [
 					'cirrusWBProfile' => 'wikibase_config_prefix_query_20181126',
 					'cirrusRescoreProfile' => 'wikibase_config_entity_weight_20181126',
-				],
-			],
-			'classic_entity' => [
-				'samplingRate' => 0.0,
-				'context' => 'item',
-				'language' => 'en',
-				'searchApiParameters' => [
-					'useCirrus' => '0',
 				],
 			],
 		],
