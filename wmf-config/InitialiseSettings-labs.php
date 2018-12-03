b82022e1ddf546642095f8c655e87e0739a2ba08
Stephane Bisson <sbisson@wikimedia.org>
2018-12-03 16:20:29 -0500

    Configure a sample welcome survey experiment for kowiki in labs
    40% get nothing
    30% get the special page
    30% get the popup (or fallback to the special page if no js)
    
    Bug: T207717
    Change-Id: I96769e8fa958204e4a65936e09ec4d1d3f0d8cdd

--- wmf-config/InitialiseSettings-labs.php
+++ wmf-config/InitialiseSettings-labs.php
@@ -819,6 +819,43 @@ function wmfLabsSettings() {
 		'wgWelcomeSurveyPrivacyPolicyUrl' => [
 			'kowiki' => 'https://foundation.wikimedia.org/wiki/%EC%83%88_%EC%82%AC%EC%9A%A9%EC%9E%90_%ED%99%98%EC%98%81_%EC%84%A4%EB%AC%B8_%EA%B0%9C%EC%9D%B8_%EC%A0%95%EB%B3%B4_%EB%B3%B4%ED%98%B8_%EC%A0%95%EC%B1%85',
 		],
+		'wgWelcomeSurveyExperimentalGroups' => [
+			'kowiki' => [
+				'beta_no_survey' => [
+					'range' => '0-3',
+					'questions' => [],
+				],
+				'beta_specialpage' => [
+					'format' => 'specialpage',
+					'range' => '4-6',
+					'questions' => [
+						'reason',
+						'reason-other',
+						'edited',
+						'topics',
+						'topics-other-nojs',
+						'topics-other-js',
+						'email',
+						'mentor-info',
+						'mentor',
+					],
+				],
+				'beta_popup' => [
+					'format' => 'popup',
+					'range' => '7-9',
+					'nojs-fallback' => 'beta_specialpage',
+					'questions' => [
+						'reason',
+						'edited',
+						'topics',
+						'topics-other-js',
+						'email',
+						'mentor-info',
+						'mentor',
+					]
+				]
+			]
+		],
 		'wgEnablePartialBlocks' => [
 			'default' => true,
 		],
