0a2de4e4cf342b2a59ac3b19a60f8c7b38e28758
Marostegui <marostegui@wikimedia.org>
2018-12-03 14:55:15 +0000

    Revert "db-eqiad.php: Depool db1100"
    This reverts commit a2f395ea922bfaf62149da3f1776ae654262b877.
    
    Change-Id: I7ecb85b06a69e9e062ebb2a8c7ae8b27e83ca415

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -145,10 +145,10 @@ $wgLBFactoryConf = [
 	],
 	's5' => [
 		'db1070' => 0,      # D1 2.8TB 160GB, master
-		'db1082' => 100,    # A2 3.6TB 512GB, api # master for sanitarium db1124
-		'db1096:3315' => 100, # A6 3.6TB 512GB # rc, log: s5 and s6
-		'db1097:3315' => 100, # D1 3.6TB 512GB # rc, log: s4 and s5
-		# 'db1100' => 100,    # C2 3.6TB 512GB, old master #api # candidate master T86338 T202167
+		'db1082' => 300,    # A2 3.6TB 512GB, api # master for sanitarium db1124
+		'db1096:3315' => 1, # A6 3.6TB 512GB # rc, log: s5 and s6
+		'db1097:3315' => 1, # D1 3.6TB 512GB # rc, log: s4 and s5
+		'db1100' => 100,    # C2 3.6TB 512GB, old master #api # candidate master
 		'db1110' => 500,    # C3 3.6TB 512GB
 		'db1113:3315' => 1, # B8 3.6TB 512GB # vslow, dump: s5 and s6
 	],
@@ -389,7 +389,7 @@ $wgLBFactoryConf = [
 		],
 		'api' => [
 			'db1082' => 1,
-			# 'db1100' => 3,
+			'db1100' => 3,
 		],
 		'watchlist' => [
 			'db1096:3315' => 1,
