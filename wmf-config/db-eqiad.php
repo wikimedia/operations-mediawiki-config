c7738ce0a53ffc65faf721b9e15778ee4bcb4df5
Balazs Pocze <bpocze@wikimedia.org>
2018-12-04 17:46:20 +0100

    mariadb: depool db1100
    depool for schema change
    ALTER TABLE user DROP COLUMN user_options
    
    Bug: T85757
    Change-Id: Idc34772ad53ae32a03cc285d35ba7394bfa3fc99

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -146,9 +146,9 @@ $wgLBFactoryConf = [
 	's5' => [
 		'db1070' => 0,      # D1 2.8TB 160GB, master
 		'db1082' => 300,    # A2 3.6TB 512GB, api # master for sanitarium db1124
-		'db1096:3315' => 1, # A6 3.6TB 512GB # rc, log: s5 and s6
-		'db1097:3315' => 1, # D1 3.6TB 512GB # rc, log: s4 and s5
-		'db1100' => 100,    # C2 3.6TB 512GB, old master #api # candidate master
+		'db1096:3315' => 50, # A6 3.6TB 512GB # rc, log: s5 and s6
+		'db1097:3315' => 50, # D1 3.6TB 512GB # rc, log: s4 and s5
+		#'db1100' => 100,   # T85757 # C2 3.6TB 512GB, old master #api # candidate master
 		'db1110' => 500,    # C3 3.6TB 512GB
 		'db1113:3315' => 1, # B8 3.6TB 512GB # vslow, dump: s5 and s6
 	],
@@ -389,7 +389,7 @@ $wgLBFactoryConf = [
 		],
 		'api' => [
 			'db1082' => 1,
-			'db1100' => 3,
+			#'db1100' => 3,
 		],
 		'watchlist' => [
 			'db1096:3315' => 1,
