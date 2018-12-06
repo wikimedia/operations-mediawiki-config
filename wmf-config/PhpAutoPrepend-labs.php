42fae62b974d41816c80e47fad71c2dff0ad8db6
Tim Starling <tstarling@wikimedia.org>
2018-12-06 14:38:31 +1100

    Put profiler hostnames in ProductionServices.php
    And thus unify PhpAutoPrepend.php with PhpAutoPrepend-labs.php. After
    the change is deployed, php.in can be updated to use PhpAutoPrepend.php
    directly in labs.
    
    Change-Id: Ie4190277fb34f8d1751f80209cac2cbca4a8e616

--- wmf-config/PhpAutoPrepend-labs.php
+++ wmf-config/PhpAutoPrepend-labs.php
@@ -1,26 +1,4 @@
 <?php
 # WARNING: This file is publicly viewable on the web. Do not put private data here.
 
-/**
- * Initialisation code for all PHP processes.
- *
- * PHP is configured to execute this file before the main script, through
- * the `auto_prepend_file` setting. This can apply both to web request
- * and CLI processes.
- *
- * This is executed in the same run-time as the main script, which means
- * it CAN expose state, such as variables and constants.
- *
- * @see https://secure.php.net/manual/en/ini.core.php#ini.auto-prepend-file
- */
-
-require_once __DIR__ . '/profiler.php';
-
-wmfSetupProfiler( [
-	'redis-host' => 'deployment-fluorine02.deployment-prep.eqiad.wmflabs',
-	'redis-port' => 6379,
-	'redis-timeout' => 1.0,
-	'use-xhgui' => false, // Not yet available in Beta Cluster, T180761
-] );
-
-require __DIR__ . '/set-time-limit.php';
+require_once __DIR__ . '/PhpAutoPrepend.php';
