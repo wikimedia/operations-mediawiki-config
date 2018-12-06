d9bf777d4ed810eeb93fe0fd4ea7ff5a4964a698
Baha <bmansurov@wikimedia.org>
2018-12-06 11:55:33 -0500

    Labs: enable reader trust survey on enwiki only
    Bug: T209882
    Change-Id: Iea777e2bc3c76ebea0b45945730a2da98763c22b

--- wmf-config/InitialiseSettings-labs.php
+++ wmf-config/InitialiseSettings-labs.php
@@ -397,9 +397,83 @@ function wmfLabsSettings() {
 		],
 
 		'wmgUseQuickSurveys' => [
-			'default' => true,
+			'default' => false,
+			'enwiki' => true
 		],
 
+		$wgQuickSurveysConfig = [
+			'default' => [
+				[
+					"name" => "drink-survey",
+					"type" => "internal",
+					"question" => "anne-survey-question",
+					"answers" => [
+						"anne-survey-answer-one",
+						"anne-survey-answer-two",
+						"anne-survey-answer-three",
+						"anne-survey-answer-four"
+					],
+					"schema" => "QuickSurveysResponses",
+					"enabled" => true,
+					"coverage" => 0,
+					"description" => "anne-survey-description",
+					"platforms" => [
+						"desktop" => [ "stable" ],
+						"mobile" => [ "stable", "beta" ],
+					],
+				],
+				[
+					"name" => "internal example survey",
+					"type" => "internal",
+					"question" => "ext-quicksurveys-example-internal-survey-question",
+					"answers" => [
+						"ext-quicksurveys-example-internal-survey-answer-positive",
+						"ext-quicksurveys-example-internal-survey-answer-neutral",
+						"ext-quicksurveys-example-internal-survey-answer-negative",
+					],
+					"schema" => "QuickSurveysResponses",
+					"enabled" => true,
+					"coverage" => 0,
+					"description" => "ext-quicksurveys-example-internal-survey-description",
+					"platforms" => [
+						"desktop" => [ "stable" ],
+						"mobile" => [ "stable", "beta" ],
+					],
+				],
+				[
+					"name" => "external example survey",
+					"type" => "external",
+					"question" => "ext-quicksurveys-example-external-survey-question",
+					"description" => "ext-quicksurveys-example-external-survey-description",
+					"link" => "ext-quicksurveys-example-external-survey-link",
+					"privacyPolicy" => "ext-quicksurveys-example-external-survey-privacy-policy",
+					"coverage" => 0,
+					"enabled" => true,
+					"platforms" => [
+						"desktop" => [ "stable" ],
+						"mobile" => [ "stable", "beta" ],
+					],
+				],
+			],
+			"enwiki" => [
+				// T209882
+				[
+					"enabled" => true,
+					"type" => "external",
+					"name" => "Reader-trust-survey-en-v1",
+					"description" => "Reader-trust-1-description",
+					"link" => "Reader-trust-1-link",
+					"instanceTokenParameterName" => "token",
+					"question" => "Reader-trust-1-message",
+					"privacyPolicy" => "Reader-trust-1-privacy",
+					"coverage" => 1,  // 1 out of 1
+					"platforms" => [
+						"desktop" => [ "stable" ],
+						"mobile" => [ "stable" ],
+					],
+				],
+			],
+		],
 		'wmgUseSentry' => [
 			'default' => true,
 		],
