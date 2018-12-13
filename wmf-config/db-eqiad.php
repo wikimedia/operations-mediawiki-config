01bfeeddafa708360163fb37d6c272f07061b0de
Manuel Arostegui <marostegui@wikimedia.org>
2018-12-13 07:12:21 +0100

    db-eqiad.php: Depool db1086
    Alter table
    
    Bug: T86338
    Bug: T202167
    Change-Id: I31e7d962e371efc6eea3f544c8e1d8359ac69e7b

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -164,7 +164,7 @@ $wgLBFactoryConf = [
 	's7' => [
 		'db1062' => 0,      # D4 2.8TB 128GB, master
 		'db1079' => 300,    # A2 3.6TB 512GB, api # master for sanitarium db1125
-		'db1086' => 400,    # B3 3.6TB 512GB, api # candidate master
+		# 'db1086' => 400,    # B3 3.6TB 512GB, api # candidate master # alter
 		'db1090:3317' => 1, # C3 3.6TB 512GB, vslow, dump: s2 and s7, old master
 		'db1094' => 500,    # D2 3.6TB 512GB
 		'db1098:3317' => 1, # B5 3.6TB 512GB # rc, log: s6 and s7
@@ -453,7 +453,7 @@ $wgLBFactoryConf = [
 		],
 		'api' => [
 			'db1079' => 3,
-			'db1086' => 1,
+			# 'db1086' => 1,
 		],
 		'watchlist' => [
 			'db1098:3317' => 1,
