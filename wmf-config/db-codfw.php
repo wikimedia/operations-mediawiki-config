194816eb765939b1e4c51fd46dc7841791254b79
Manuel Arostegui <marostegui@wikimedia.org>
2018-12-19 08:09:47 +0100

    db-codfw.php: Depool db2050
    Will reclone db2057 from db2050
    
    Bug: T212275
    Change-Id: I20292ac9e5d62c85fec7073ab2b2bdf5817bce8c

--- wmf-config/db-codfw.php
+++ wmf-config/db-codfw.php
@@ -127,9 +127,9 @@ $wgLBFactoryConf = [
 	/* s3 */ 'DEFAULT' => [
 		'db2043'      => 0,   # C6 2.9TB 160GB, master
 		'db2036'      => 50,  # C6 2.9TB 160GB, old master, rc, log
-		'db2050'      => 50,  # C6 2.9TB 160GB dump (inactive), vslow
+		# 'db2050'      => 50,  # C6 2.9TB 160GB dump (inactive), vslow # will clone db2057
 		# 'db2057'      => 50,  # D6 3.3TB 160GB # rc, log # candidate master # crashed T212275
-		'db2074'      => 500, # D6 3.3TB 512GB # InnoDB compressed # master for sanitarium db2094 # temporary rc
+		'db2074'      => 500, # D6 3.3TB 512GB # InnoDB compressed # master for sanitarium db2094 # temporary rc # temporary vslow
 	],
 	's4' => [
 		'db2051'      => 0,   # B8 2.9TB 160GB, master
@@ -326,10 +326,10 @@ $wgLBFactoryConf = [
 	],
 	/* s3 */ 'DEFAULT' => [
 		'dump' => [
-			'db2050' => 1,
+			'db2074' => 1,
 		],
 		'vslow' => [
-			'db2050' => 1,
+			'db2074' => 1,
 		],
 		'watchlist' => [
 			'db2036' => 1,
