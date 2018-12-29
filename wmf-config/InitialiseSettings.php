9d4957d0c59757e5fbb8549370edba7202fec22e
MarcoAurelio <maurelio@tools.wmflabs.org>
2018-12-29 12:43:02 +0000

    Amend mediawiki AbuseFilter configuration
    Bug: T212667
    Change-Id: Ibec77d059c55c531485bb07b5d415ab9696eacbb

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -13775,10 +13775,10 @@ $wgConf->settings = [
 		'default' => 0.30, // T145765
 	],
 	'metawiki' => [
-		'default' => 0.30 // T173633
+		'default' => 0.30,  // T173633
 	],
-	'mediawikiwiki' => [ // temporary for T212667
-		'default' => 1,
+	'mediawikiwiki' => [ // T212667
+		'default' => 0.30,
 	],
 ],
 'wgAbuseFilterEmergencyDisableCount' => [
@@ -13796,6 +13796,9 @@ $wgConf->settings = [
 	],
 	'metawiki' => [
 		'default' => 25, // T173633
+	],
+	'mediawikiwiki' => [ // T212667
+		'default' => 25,
 	],
 ],
 'wgAbuseFilterEmergencyDisableAge' => [
