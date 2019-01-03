657a2098e3ac754b752967435f7ed0352ba0399f
James D. Forrester <jforrester@wikimedia.org>
2019-01-03 18:08:46 +0000

    Drop the Wikipedia Zero debug log channel
    Bug: T212865
    Change-Id: Ibaeff25fa5ebc29d71da86a6ca0d5ce21664e260

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -5592,7 +5592,6 @@ $wgConf->settings = [
 		'xenon' => 'debug',
 		'XMP' => 'warning', // T89532
 		'xff' => [ 'logstash' => false ],
-		'zero' => [ 'logstash' => false ], // zero-rated debug log
 		'MessageCacheError' => 'debug',
 		'LocalFile' => 'debug',
 		'DBTransaction' => 'debug',
