39ac25f9cf27b83186e3e615529d38bc00b0b6ba
Gergő Tisza <tgr.huwiki@gmail.com>
2019-01-03 00:08:10 -0800

    Remove AICaptcha settings
    WikimediaEvents change is Ib7ce252fbcdfc6a9e79f2896ca9b8d5e4c8b70ef.
    Order should not matter.
    
    Bug: T186244
    Change-Id: I853f9a89c0dafafc82b11bf406699c83647c2e3d

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -21657,13 +21657,6 @@ $wgConf->settings = [
 	'mediawikiwiki' => false,
 	'wikidatawiki' => false,
 ],
-// T183869
-'wgWMEAICaptchaEnabled' => [
-	'default' => false,
-	'private' => false,
-	'fishbowl' => false,
-	'nonglobal' => false,
-],
 
 // T184121
 // Explicitly disabled for non-CentralAuth wikis in CommonSettings.php
