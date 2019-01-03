wikimedia/operations-mediawiki-config

commit c61e7a80b27bb03ab1dd76ddd4a9972b8e716dd6

[cirrus] Allow configuration arrays in production services
Bug: T210381
Change-Id: I2ae162f55e17f45a422f121d9bf674bb657b560d

wmf-config/CirrusSearch-production.php
@@ -19,32 +19,35 @@
 // help things along.
 $wgCirrusSearchMasterTimeout = '2m';
 
+$cirrusConfigUseHhvmPool = function ( $hostConfig, $pool ) {
+	if ( !defined( 'HHVM_VERSION' ) ) {
+		return $hostConfig;
+	}
+	if ( is_array( $hostConfig ) ) {
+		if ( isset( $hostConfig['transport'] ) && $hostConfig['transport'] === 'Https' ) {
+			$hostConfig['transport'] = 'CirrusSearch\\Elastica\\PooledHttps';
+			$hostConfig['config'] = [
+				'pool' => $pool
+			];
+		}
+		return $hostConfig;
+	}
+	return [
+		'transport' => 'CirrusSearch\\Elastica\\PooledHttps',
+		'port' => '9243',
+		'host' => $hostConfig,
+		'config' => [
+			'pool' => $pool,
+		],
+	];
+};
+
 $wgCirrusSearchClusters = [
-	'eqiad' => $wmfAllServices['eqiad']['search'],
-	'codfw' => $wmfAllServices['codfw']['search'],
+	'eqiad' => $cirrusConfigUseHhvmPool( $wmfAllServices['eqiad']['search'], 'cirrus-eqiad' ),
+	'codfw' => $cirrusConfigUseHhvmPool( $wmfAllServices['codfw']['search'], 'cirrus-codfw' ),
 ];
-if ( defined( 'HHVM_VERSION' ) ) {
-	$wgCirrusSearchClusters['eqiad'] = array_map( function ( $host ) {
-		return [
-			'transport' => 'CirrusSearch\\Elastica\\PooledHttps',
-			'port' => '9243',
-			'host' => $host,
-			'config' => [
-				'pool' => 'cirrus-eqiad',
-			],
-		];
-	}, $wgCirrusSearchClusters['eqiad'] );
-	$wgCirrusSearchClusters['codfw'] = array_map( function ( $host ) {
-		return [
-			'transport' => 'CirrusSearch\\Elastica\\PooledHttps',
-			'port' => '9243',
-			'host' => $host,
-			'config' => [
-				'pool' => 'cirrus-codfw',
-			],
-		];
-	}, $wgCirrusSearchClusters['codfw'] );
-}
+
+unset( $cirrusConfigUseHhvmPool );
 
 $wgCirrusSearchConnectionAttempts = 3;
 

wmf-config/CommonSettings.php
@@ -2592,9 +2592,12 @@ if ( $wmgUseTranslate ) {
 				'cutoff' => 0.65,
 				'use_wikimedia_extra' => true,
 				'config' => [
-					'servers' => array_map( function ( $host ) {
+					'servers' => array_map( function ( $hostConfig ) {
+						if ( is_array( $hostConfig ) ) {
+							return $hostConfig;
+						}
 						return [
-							'host' => $host,
+							'host' => $hostConfig,
 							'port' => 9243,
 							'transport' => 'Https',
 						];

commit 6ca9bf7ff548af11881f71fbcbcb5e7c806e680d

[cirrus] re-enable HHVM connection pooling
broken by c61e7a8

Bug: T212768
Change-Id: Ibea243254a31324088ac188cab82e3356ff20378

wmf-config/CirrusSearch-production.php
@@ -19,27 +19,32 @@
 // help things along.
 $wgCirrusSearchMasterTimeout = '2m';
 
-$cirrusConfigUseHhvmPool = function ( $hostConfig, $pool ) {
+$cirrusConfigUseHhvmPool = function ( $hosts, $pool ) {
 	if ( !defined( 'HHVM_VERSION' ) ) {
-		return $hostConfig;
+		return $hosts;
 	}
-	if ( is_array( $hostConfig ) ) {
-		if ( isset( $hostConfig['transport'] ) && $hostConfig['transport'] === 'Https' ) {
-			$hostConfig['transport'] = 'CirrusSearch\\Elastica\\PooledHttps';
-			$hostConfig['config'] = [
-				'pool' => $pool
-			];
+	if ( !is_array( $hosts ) ) {
+		return $hosts;
+	}
+	return array_map( function ( $hostConfig ) use ( $pool ) {
+		if ( is_array( $hostConfig ) ) {
+			if ( isset( $hostConfig['transport'] ) && $hostConfig['transport'] === 'Https' ) {
+				$hostConfig['transport'] = 'CirrusSearch\\Elastica\\PooledHttps';
+				$hostConfig['config'] = [
+					'pool' => $pool
+				];
+			}
+			return $hostConfig;
 		}
-		return $hostConfig;
-	}
-	return [
-		'transport' => 'CirrusSearch\\Elastica\\PooledHttps',
-		'port' => '9243',
-		'host' => $hostConfig,
-		'config' => [
-			'pool' => $pool,
-		],
-	];
+		return [
+			'transport' => 'CirrusSearch\\Elastica\\PooledHttps',
+			'port' => '9243',
+			'host' => $hostConfig,
+			'config' => [
+				'pool' => $pool,
+			],
+		];
+	}, $hosts );
 };
 
 $wgCirrusSearchClusters = [