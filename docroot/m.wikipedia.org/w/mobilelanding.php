2eabbdcd773c5105dfaed08d5faa926ad7dfc66e
James D. Forrester <jforrester@wikimedia.org>
2019-01-03 18:07:59 +0000

    Re-write mobilelanding.php to not break when we drop ZeroBanner
    I'm not sure that this is ever called, but for safety.
    
    Bug: T212865
    Change-Id: I96f489ca6dce8d4b2c43aeea007b56c278a6dc42

--- docroot/m.wikipedia.org/w/mobilelanding.php
+++ docroot/m.wikipedia.org/w/mobilelanding.php
@@ -2,24 +2,20 @@
 require_once __DIR__ . '/../../../multiversion/MWMultiVersion.php';
 require MWMultiVersion::getMediaWiki( 'includes/WebStart.php', 'enwiki' );
 
-$code = '302';
-$state = ZeroBanner\PageRenderingHooks::getState();
-$redirect = $state->getLandingRedirect();
-$redirect = filter_var( $redirect, FILTER_VALIDATE_URL );
-if ( $redirect === false ) {
-	$code = '500';
-}
-$message = HttpStatus::getMessage( $code );
+global $wgServer;
 
-// $lastmod = gmdate( 'D, j M Y H:i:s', wfTimestamp( TS_UNIX, $wgArticle->getTouched() ) ) . ' GMT';
-// header( "Last-modified: $lastmod" );
+$target = '/wiki/Main_Page';
+$urlParts = explode( '.', $wgServer );
+$redirect = '' .
+	(
+		( count( $urlParts ) !== 3 || $urlParts[0] === '//m' )
+			? $wgServer
+			: $urlParts[0] . '.m.' . $urlParts[1] . '.' . $urlParts[2]
+	) . $target;
 
-// note that the following will override any Cache-Control set earlier
-// in extension code called above
+$message = HttpStatus::getMessage( '302' );
 header( 'Cache-Control: public, s-maxage=900, max-age=900' );
 header( "HTTP/1.1 $code $message" );
-if ( $redirect !== false ) {
-	header( 'Location: ' . $redirect );
-}
 header( 'Vary: X-Forwarded-Proto,X-CS,X-Subdomain,Accept-Language' );
 header( 'Content-Type: text/html; charset=utf-8' );
+header( 'Location: ' . $redirect );
