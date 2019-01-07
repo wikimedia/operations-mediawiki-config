a56a29b20930cde50e65075ce3cd4c8c566918d4
zoranzoki21 <zorandori4444@gmail.com>
2019-01-07 10:49:03 +0100

    Update groupOverrides for srwikisource
    Enabled moving categories only for bots, autopatrollers, patrollers,
    rollbackers, administrators and bureaucrats.
    Enabled moving files for patrollers and rollbackers.
    Enabled suppressredirect for patrollers and rollbackers.
    Enabled move-subpages for bots, autopatrollers, patrollers, rollbackers,
    administrators and bureaucrats.
    
    Bug: T213055
    Bug: T213059
    Bug: T213063
    Bug: T213065
    Change-Id: I8db5107edec2c6d1dd64a4c0202b4635afdbd688

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -10094,10 +10094,29 @@ $wgConf->settings = [
 		'autopatrolled' => [ 'autopatrol' => true ],
 		'rollbacker' => [ 'rollback' => true ],
 	],
-	'+srwikisource' => [
-		'patroller' => [ 'patrol' => true ],
-		'autopatrolled' => [ 'autopatrol' => true ],
-		'rollbacker' => [ 'rollback' => true ], // T206935
+	'+srwikisource' => [ // T206935, T213055, T213059, T213063, T213065
+		'user' => [ 'move-categorypages' => false ],
+		'patroller' => [
+			'patrol' => true,
+			'move-categorypages' => true,
+			'movefile' => true,
+			'suppressredirect' => true,
+			'move-categorypages' => true
+		],
+		'autopatrolled' => [
+			'autopatrol' => true,
+			'move-categorypages' => true,
+			'movefile' => true,
+			'move-categorypages' => true
+		],
+		'rollbacker' => [
+			'rollback' => true,
+			'move-categorypages' => true,
+			'movefile' => true,
+			'suppressredirect' => true,
+			'move-categorypages' => true
+		],
+		'bot' => [ 'move-subpages' => true ],
 	],
 	'srwikiquote' => [ // T206936
 		'patroller' => [ 'patrol' => true ],
