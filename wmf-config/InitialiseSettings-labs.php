f0fe7122787b9eba62909bcfa5d1827ed7fca546
Roan Kattouw <roan.kattouw@gmail.com>
2019-01-15 14:46:11 -0800

    Enable help panel in development mode in beta labs
    Change-Id: I102f535bf7957102052e8d2ddb61796077b49d88

--- wmf-config/InitialiseSettings-labs.php
+++ wmf-config/InitialiseSettings-labs.php
@@ -916,6 +916,12 @@ function wmfLabsSettings() {
 				]
 			],
 		],
+		'wgGEHelpPanelSearchEnabled' => [
+			'default' => true,
+		],
+		'wgGEHelpPanelSearchDevMode' => [
+			'default' => true,
+		],
 		'wgWelcomeSurveyPrivacyPolicyUrl' => [
 			'kowiki' => 'https://foundation.wikimedia.org/wiki/%EC%83%88_%EC%82%AC%EC%9A%A9%EC%9E%90_%ED%99%98%EC%98%81_%EC%84%A4%EB%AC%B8_%EA%B0%9C%EC%9D%B8_%EC%A0%95%EB%B3%B4_%EB%B3%B4%ED%98%B8_%EC%A0%95%EC%B1%85',
 		],
