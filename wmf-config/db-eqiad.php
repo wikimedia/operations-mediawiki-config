aba33ac2a65ba47e9290fa9ff11140e29e4fdd94
Jcrespo <jcrespo@wikimedia.org>
2019-01-16 10:57:48 +0100

    mariadb: Depool db1123 for maintenance
    It will be upgraded and restarted in preparation for the s3
    switchover.
    
    Bug: T213858
    Change-Id: If46269e0e09ee51c95cb9926101ca77402d3ccc1

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -132,8 +132,8 @@ $wgLBFactoryConf = [
 	],
 	/* s3 */ 'DEFAULT' => [
 		'db1075' => 0,      # A2 3.6TB 512GB, master
-		'db1077' => 400,    # B1 3.6TB 512GB, rc, log # master for sanitarium db1124
-		'db1123' => 100,    # D8 3.6TB 512GB, vslow, dump
+		'db1077' => 400,    # B1 3.6TB 512GB, rc, log, temp. vslow # master for sanitarium db1124
+		# 'db1123' => 100,    # D8 3.6TB 512GB, vslow, dump # maintenance
 		'db1078' => 500,    # C3 3.6TB 512GB, candidate master
 	],
 	's4' => [
@@ -329,10 +329,10 @@ $wgLBFactoryConf = [
 	],
 	/* s3 */ 'DEFAULT' => [
 		'vslow' => [
-			'db1123' => 1,
+			'db1077' => 1,
 		],
 		'dump' => [
-			'db1123' => 1,
+			'db1077' => 1,
 		],
 		'watchlist' => [
 			'db1077' => 1,
