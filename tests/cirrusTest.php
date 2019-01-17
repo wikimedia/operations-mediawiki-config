ef1d416c6f10c113ec0ef75494d7adc27f89ee59
James D. Forrester <jforrester@wikimedia.org>
2019-01-16 16:18:15 -0800

    dblists: Rename 'wikidatarepo' to 'wikibaserepo' part IV – Delete it
    Bug: T213504
    Change-Id: Ib96aea4e04749025883a76e16ba105dd98a79873

--- tests/cirrusTest.php
+++ tests/cirrusTest.php
@@ -129,7 +129,7 @@ class CirrusTest extends WgConfTestCase {
 		list( $site, $lang ) = $wgConf->siteFromDB( $wgDBname );
 		$wikiTags = [];
 		foreach ( [ 'private', 'fishbowl', 'special', 'closed', 'flow', 'flaggedrevs', 'small', 'medium',
-				'large', 'wikimania', 'wikidata', 'wikibaserepo', 'wikidatarepo', 'wikidataclient', 'visualeditor-nondefault',
+				'large', 'wikimania', 'wikidata', 'wikibaserepo', 'wikidataclient', 'visualeditor-nondefault',
 				'commonsuploads', 'nonbetafeatures', 'group0', 'group1', 'group2', 'wikipedia', 'nonglobal',
 				'wikitech', 'nonecho', 'mobilemainpagelegacy', 'nowikidatadescriptiontaglines',
 				'top6-wikipedia', 'cirrussearch-big-indices'
