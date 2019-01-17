898720dcbae9679c7a50a0adeea72f4fa80f210b
James D. Forrester <jforrester@wikimedia.org>
2019-01-16 16:16:29 -0800

    dblists: Rename 'wikidatarepo' to 'wikibaserepo' part II – Use it
    Bug: T213504
    Change-Id: Ie2decd0774cf12052e00520886ca4fd38a7a2b1d

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -19374,6 +19374,7 @@ $wgConf->settings = [
 
 'wmgUseWikibaseRepo' => [
 	'default' => false,
+	'wikibaserepo' => true,
 	'wikidatarepo' => true,
 ],
 
@@ -19393,6 +19394,20 @@ $wgConf->settings = [
 
 'wmgWikibaseStringLimits' => [
 	'default' => null,
+	'wikibaserepo' => [
+		'multilang' => [
+			'length' => 250,
+		],
+		'VT:monolingualtext' => [
+			'length' => 1500,
+		],
+		'VT:string' => [
+			'length' => 1500,
+		],
+		'PT:url' => [
+			'length' => 1500,
+		],
+	],
 	'wikidatarepo' => [
 		'multilang' => [
 			'length' => 250,
