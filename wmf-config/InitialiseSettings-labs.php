da06d37adadb8e3d3b7f00067d31709f8b617ed8
Roan Kattouw <roan.kattouw@gmail.com>
2019-01-17 12:57:01 -0800

    labs: Remove $wgGEHelpPanelSearchDevMode
    Setting doesn't exist anymore.
    
    Bug: T214083
    Depends-On: I788edf9f3739d763bb195143084a42e2e4964628
    Change-Id: I42a5a5d1674581c0bc4de35f2cd25705f9eb2ccc

--- wmf-config/InitialiseSettings-labs.php
+++ wmf-config/InitialiseSettings-labs.php
@@ -924,9 +924,6 @@ function wmfLabsSettings() {
 		'wgGEHelpPanelSearchEnabled' => [
 			'default' => true,
 		],
-		'wgGEHelpPanelSearchDevMode' => [
-			'default' => true,
-		],
 		'wgGEHelpPanelSearchForeignAPI' => [
 			'default' => 'https://en.wikipedia.org/w/api.php',
 			'cawiki' => 'https://cs.wikipedia.org/w/api.php',
