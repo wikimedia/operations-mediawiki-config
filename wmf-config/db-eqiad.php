cd15e84dc421d58fede9172cc140674cc5e77e86
Manuel Arostegui <marostegui@wikimedia.org>
2019-01-24 11:39:55 +0100

    db-eqiad.php: Weight 0 to db1087
    So the LB doesn't wait for it
    
    Change-Id: I94647c0a151fa386906bb7bf9963a5efb110bdf8

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -174,7 +174,7 @@ $wgLBFactoryConf = [
 	],
 	's8' => [
 		'db1071' => 0,      # D1 2.8TB 160GB, master
-		'db1087' => 1,      # C2 3.6TB 512GB, vslow, dump # master for sanitarium db1124
+		'db1087' => 0,      # C2 3.6TB 512GB, vslow, dump # master for sanitarium db1124
 		'db1092' => 200,    # D2 3.6TB 512GB, api
 		'db1099:3318' => 100, # B2 3.6TB 512GB # rc, log: s1 and s8
 		'db1101:3318' => 100, # C2 3.6TB 512GB # rc, log: s7 and s8
