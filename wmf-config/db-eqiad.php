fc4e38182c5dbeff98b2ac56a3bdf068bcb5a064
Manuel Arostegui <marostegui@wikimedia.org>
2019-01-25 08:25:20 +0100

    db-eqiad.php: Slowly repool db1105:3312
    Also give more traffic to db1105:3311
    
    Change-Id: If46c8327a6791825881c6652d2c592f8cea0de83

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -128,7 +128,7 @@ $wgLBFactoryConf = [
 		'db1090:3312' => 1, # C3 3.6TB 512GB, vslow, dump: s2 and s7
 		'db1122' => 500,    # D6 3.6TB 512GB, api
 		'db1103:3312' => 100, # A3 3.6TB 512GB # rc, log: s2 and s4
-		# 'db1105:3312' => 100, # C3 3.6TB 512GB # rc, log: s1 and s2 # alter
+		'db1105:3312' => 100, # C3 3.6TB 512GB # rc, log: s1 and s2
 	],
 	/* s3 */ 'DEFAULT' => [
 		'db1078' => 0,    # C3 3.6TB 512GB, master
@@ -268,7 +268,7 @@ $wgLBFactoryConf = [
 		],
 		'recentchanges' => [
 			'db1099:3311' => 1,
-			# 'db1105:3311' => 1,
+			'db1105:3311' => 1,
 		],
 		'recentchangeslinked' => [
 			'db1099:3311' => 1,
@@ -308,7 +308,7 @@ $wgLBFactoryConf = [
 		],
 		'watchlist' => [
 			'db1103:3312' => 1,
-			# 'db1105:3312' => 1,
+			'db1105:3312' => 1,
 		],
 		'recentchanges' => [
 			'db1103:3312' => 1,
