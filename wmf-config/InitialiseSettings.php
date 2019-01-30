d0cb93ad898ad3e95e041008c82b92bd26226d78
Ammar <ammarpad@yahoo.com>
2019-01-30 08:55:24 +0100

    Set wgArticleCountMethod='any' for zhwikiversity
    Bug:: T214946
    Change-Id: I6d17e8783edab4f8d63124ff8571de7046769e17

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -14459,6 +14459,7 @@ $wgConf->settings = [
 	'srwikiquote' => 'any', // T172974
 	'wikidatawiki' => 'any', // T144687
 	'zhwikinews' => 'any', // T53604
+	'zhwikiversity' => 'any', // T214946
 ],
 
 'wgUseContributionTracking' => [
