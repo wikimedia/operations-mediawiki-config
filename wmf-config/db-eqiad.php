4689d2196bf50f9928805590447629b1f057a93b
Jaime Crespo <jcrespo@wikimedia.org>
2019-02-07 15:34:02 +0100

    mariadb: Depool db1085 for maintenance
    It will be upgraded and restarted.
    
    Change-Id: Id0e7254b18df54b243c13f086cde7f77db16fb98

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -156,12 +156,12 @@ $wgLBFactoryConf = [
 	],
 	's6' => [
 		'db1061' => 0,      # C3 2.8TB 128GB, master
-		'db1085' => 300,    # B3 3.6TB 512GB, api # master for sanitarium db1125
+		# 'db1085' => 300,    # B3 3.6TB 512GB, api # master for sanitarium db1125
 		'db1088' => 500,    # C2 3.6TB 512GB
 		'db1093' => 300,    # D2 3.6TB 512GB, api # candidate master
 		'db1096:3316' => 1, # A6 3.6TB 512GB # rc, log: s5 and s6
 		'db1098:3316' => 1, # B5 3.6TB 512GB # rc, log: s6 and s7
-		'db1113:3316' => 1, # B8 3.6TB 512GB # vslow, dump: s5 and s6
+		'db1113:3316' => 1, # B8 3.6TB 512GB # vslow, dump: s5 and s6, temp. api
 	],
 	's7' => [
 		'db1062' => 0,      # D4 2.8TB 128GB, master
@@ -422,8 +422,9 @@ $wgLBFactoryConf = [
 			'db1113:3316' => 1,
 		],
 		'api' => [
-			'db1085' => 10,
+			#'db1085' => 10,
 			'db1093' => 1,
+			'db1113:3316' => 2,
 		],
 		'watchlist' => [
 			'db1096:3316' => 1,
