977faf906d3cd4588ea7bb7e7b6b47afa3622142
David Causse <dcausse@wikimedia.org>
2019-02-07 10:59:22 +0100

    [cirrus] Start using local nginx reverse proxy for connections reuse
    Bug: T215491
    Change-Id: I4dab092a82d37e99ec337ae0b504f6013a6a5762

--- wmf-config/LabsServices.php
+++ wmf-config/LabsServices.php
@@ -13,13 +13,6 @@ return [
 		'xenon' => 'deployment-fluorine02.deployment-prep.eqiad.wmflabs',
 		'xhgui' => false, // Not yet available in Beta Cluster, T180761
 		'statsd' => 'labmon1001.eqiad.wmnet',
-		'search' => [
-			// TODO: remove once search-chi is used everywhere
-			// These MUST match the installed SSL certs
-			'deployment-elastic05.deployment-prep.eqiad.wmflabs',
-			'deployment-elastic06.deployment-prep.eqiad.wmflabs',
-			'deployment-elastic07.deployment-prep.eqiad.wmflabs',
-		],
 		'search-chi' => [
 			// These MUST match the installed SSL certs
 			'deployment-elastic05.deployment-prep.eqiad.wmflabs',
