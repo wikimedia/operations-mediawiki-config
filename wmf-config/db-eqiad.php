8fc9c833a3d765797a97186a31990eb66e449f27
Manuel Arostegui <marostegui@wikimedia.org>
2019-02-08 10:33:46 +0100

    db-eqiad.php: Depool db1090:3317
    Add db1098:3317 as a temporary vslow
    
    Bug: T210713
    Change-Id: Iee44ba3368be3f98d189a8a172a45dfed13475f5

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -167,9 +167,9 @@ $wgLBFactoryConf = [
 		'db1062' => 0,      # D4 2.8TB 128GB, master
 		'db1079' => 300,    # A2 3.6TB 512GB, api # master for sanitarium db1125
 		'db1086' => 400,    # B3 3.6TB 512GB, api # candidate master
-		'db1090:3317' => 1, # C3 3.6TB 512GB, vslow, dump: s2 and s7, old master
+		# 'db1090:3317' => 1, # C3 3.6TB 512GB, vslow, dump: s2 and s7, old master # alter
 		'db1094' => 500,    # D2 3.6TB 512GB
-		'db1098:3317' => 150, # B5 3.6TB 512GB # rc, log: s6 and s7
+		'db1098:3317' => 50, # B5 3.6TB 512GB # rc, log: s6 and s7 # temporary vslow
 		'db1101:3317' => 150, # C2 3.6TB 512GB # rc, log: s7 and s8
 	],
 	's8' => [
@@ -448,10 +448,10 @@ $wgLBFactoryConf = [
 	],
 	's7' => [
 		'vslow' => [
-			'db1090:3317' => 1,
+			'db1098:3317' => 1,
 		],
 		'dump' => [
-			'db1090:3317' => 1,
+			'db1098:3317' => 1,
 		],
 		'api' => [
 			'db1079' => 3,
