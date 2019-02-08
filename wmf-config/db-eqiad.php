e7d815db26105cf537135372579782bb971c5ba4
Jcrespo <jcrespo@wikimedia.org>
2019-02-08 10:43:34 +0000

    Revert "mariadb: Depool db1099 from s1 and s8 for maintenance"
    This reverts commit 6f6f57f46074c0df4b0e901438801bac62ef2c03.
    
    Change-Id: Iab265b6e314ff7c4a52ad0b549e6948017f5684e

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -115,7 +115,7 @@ $wgLBFactoryConf = [
 		'db1080' => 200,    # A2 3.6TB 512GB, api
 		'db1083' => 50,     # B1 3.6TB 512GB # candidate master
 		'db1089' => 500,    # C3 3.6TB 512GB
-		# 'db1099:3311' => 50, # B2 3.6TB 512GB # rc, log: s1 and s8
+		'db1099:3311' => 50, # B2 3.6TB 512GB # rc, log: s1 and s8
 		'db1105:3311' => 50, # C3 3.6TB 512GB # rc, log: s1 and s2
 		'db1106' => 50,     # D3 3.6TB 512GB, vslow, dump # master for sanitarium db1124
 		'db1114' => 200,    # D4 3.6TB 512GB, api
@@ -176,7 +176,7 @@ $wgLBFactoryConf = [
 		'db1071' => 0,      # D1 2.8TB 160GB, master
 		'db1087' => 1,      # C2 3.6TB 512GB, vslow, dump # master for sanitarium db1124
 		'db1092' => 200,    # D2 3.6TB 512GB, api
-		# 'db1099:3318' => 100, # B2 3.6TB 512GB # rc, log: s1 and s8
+		'db1099:3318' => 100, # B2 3.6TB 512GB # rc, log: s1 and s8
 		'db1101:3318' => 100, # C2 3.6TB 512GB # rc, log: s7 and s8
 		'db1104' => 400,    # B3 3.6TB 512GB, api # candidate master
 		'db1109' => 500,    # D8 3.6TB 512GB
@@ -263,23 +263,23 @@ $wgLBFactoryConf = [
 'groupLoadsBySection' => [
 	's1' => [
 		'watchlist' => [
-			# 'db1099:3311' => 1,
+			'db1099:3311' => 1,
 			'db1105:3311' => 1,
 		],
 		'recentchanges' => [
-			# 'db1099:3311' => 1,
+			'db1099:3311' => 1,
 			'db1105:3311' => 1,
 		],
 		'recentchangeslinked' => [
-			# 'db1099:3311' => 1,
+			'db1099:3311' => 1,
 			'db1105:3311' => 1,
 		],
 		'contributions' => [
-			# 'db1099:3311' => 1,
+			'db1099:3311' => 1,
 			'db1105:3311' => 1,
 		],
 		'logpager' => [
-			# 'db1099:3311' => 1,
+			'db1099:3311' => 1,
 			'db1105:3311' => 1,
 		],
 		'dump' => [
@@ -490,23 +490,23 @@ $wgLBFactoryConf = [
 			'db1104' => 1,
 		],
 		'watchlist' => [
-			# 'db1099:3318' => 1,
+			'db1099:3318' => 1,
 			'db1101:3318' => 1,
 		],
 		'recentchanges' => [
-			# 'db1099:3318' => 1,
+			'db1099:3318' => 1,
 			'db1101:3318' => 1,
 		],
 		'recentchangeslinked' => [
-			# 'db1099:3318' => 1,
+			'db1099:3318' => 1,
 			'db1101:3318' => 1,
 		],
 		'contributions' => [
-			# 'db1099:3318' => 1,
+			'db1099:3318' => 1,
 			'db1101:3318' => 1,
 		],
 		'logpager' => [
-			# 'db1099:3318' => 1,
+			'db1099:3318' => 1,
 			'db1101:3318' => 1,
 		],
 	],
