aaaf7f5d6e243c41d045e0af4db84b17b6bf181b
Leszek Manicki <leszek.manicki@wikimedia.de>
2019-02-14 12:58:23 +0100

    Added settings to defined Wikibase entity types that have no RDF output
    Bug T213483
    
    Depends-On: Ic7045f06dc444ae0c6d89a16b37a5ebd8f5b092c
    Change-Id: I9ac61c669162792e2a0e641e90d2092f667a4524

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -20297,6 +20297,10 @@ $wgConf->settings = [
 	'wikidatawiki' => true, // T203888
 ],
 
+'wmgWikibaseEntityTypesWithoutRdfOutput' =>
+	'default' => [],
+],
+
 'wmgWikibaseDisabledAccessEntityTypes' => [
 	// Note: When lexeme is removed from here it needs to be added to wmgWikibaseClientRepoNamespaces
 	'default' => [ 'lexeme', 'form', 'sense' ],
