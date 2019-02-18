c12412f5367423f42d8d18e76b3f2a0edb8649f6
Amir Sarabadani <Ladsgroup@gmail.com>
2019-02-18 11:52:11 +0100

    Change Special:ItemDisambiguation from blank special page to disabled page
    After Ib56860bc3 this makes more sense to use
    
    Bug: T216397
    Change-Id: Ic58838f0cb8122f15a2d90da9a834348e5155446

--- wmf-config/Wikibase.php
+++ wmf-config/Wikibase.php
@@ -77,7 +77,7 @@ $wgLockManagers[] = [
 if ( $wmgUseWikibaseRepo ) {
 	if ( $wgDBname === 'wikidatawiki' ) {
 		// Disable Special:ItemDisambiguation on wikidata.org T195756
-		$wgSpecialPages['ItemDisambiguation'] = 'SpecialBlankpage';
+		$wgSpecialPages['ItemDisambiguation'] = 'DisabledSpecialPage';
 	}
 
 	if ( $wgDBname === 'wikidatawiki' || $wgDBname === 'testwikidatawiki' ) {
