ce70b4bddf9600b4eeaf1694d10114d2614b89f7
Manuel Arostegui <marostegui@wikimedia.org>
2019-02-27 10:39:35 +0100

    db-eqiad.php: Fully repool db1082
    After mysql upgrade
    
    Change-Id: I507f95ef30a2b23b80c93fb127dec760ca0bb9c2

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -147,7 +147,7 @@ $wgLBFactoryConf = [
 	],
 	's5' => [
 		'db1070' => 0,      # D1 2.8TB 160GB, master
-		'db1082' => 100,    # A2 3.6TB 512GB, api # master for sanitarium db1124 # low weight
+		'db1082' => 300,    # A2 3.6TB 512GB, api # master for sanitarium db1124
 		'db1096:3315' => 1, # A6 3.6TB 512GB # rc, log: s5 and s6
 		'db1097:3315' => 1, # D1 3.6TB 512GB # rc, log: s4 and s5
 		'db1100' => 100,    # C2 3.6TB 512GB, old master #api # candidate master
