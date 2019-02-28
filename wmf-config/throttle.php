4bd8f12eb050cd7e20a1f4d31135a664feacd89e
Martin Urbanec <martin.urbanec@wikimedia.cz>
2019-02-28 16:23:32 +0100

    Add throttle exception for Amnesty International Editathon
    Bug: T217311
    Change-Id: If402d09b428738c31263b3f2b2e60296be38d154

--- wmf-config/throttle.php
+++ wmf-config/throttle.php
@@ -88,4 +88,11 @@ $wmgThrottlingExceptions[] = [ // T217305
 	'value' => 100
 ];
 
+$wmgThrottlingExceptions[] = [ // T217311
+	'from' => '2019-03-08T10:00 UTC',
+	'to' => '2019-03-08T22:00 UTC',
+	'IP' => '86.83.7.133',
+	'value' => 30 // 25 expected
+];
+
 ## Add throttling definitions above.
