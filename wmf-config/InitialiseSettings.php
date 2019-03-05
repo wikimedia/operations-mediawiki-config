261dc9c9b97e312cfde27ca4dcced83e4cdb257d
Baha <bmansurov@wikimedia.org>
2019-03-05 16:08:48 -0500

    Disable reader trust survey v2
    Bug: T217576
    Change-Id: If451b9bd492242bb00988f0385d3ba6a051fd119

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -22015,22 +22015,6 @@ $wgConf->settings = [
 				'mobile' => [ 'stable' ]
 			],
 		],
-		// T217576
-		[
-			"enabled" => true,
-			"type" => "external",
-			"name" => "reader trust survey en v2",
-			"description" => "Reader-trust-2-description",
-			"link" => "Reader-trust-2-link",
-			"instanceTokenParameterName" => "token",
-			"question" => "Reader-trust-2-message",
-			"privacyPolicy" => "Reader-trust-2-privacy",
-			"coverage" => 0.004,  // 1 out of 250
-			"platforms" => [
-				"desktop" => [ "stable" ],
-				"mobile" => [ "stable" ],
-			],
-		],
 	],
 ],
 
