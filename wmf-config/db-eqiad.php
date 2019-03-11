ccfebd5afbabe194639d8453b21990c68f8a38e2
Manuel Arostegui <marostegui@wikimedia.org>
2019-03-11 15:48:26 +0100

    db-eqiad.php: More weight to db1097
    Change-Id: I5de52d48c92f99e7cfba5ddee1348710f5e7183d

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -370,11 +370,11 @@ $wgLBFactoryConf = [
 			'db1103:3314' => 1,
 		],
 		'recentchangeslinked' => [
-			# 'db1097:3314' => 1,
+			'db1097:3314' => 1,
 			'db1103:3314' => 1,
 		],
 		'contributions' => [
-			# 'db1097:3314' => 1,
+			'db1097:3314' => 1,
 			'db1103:3314' => 1,
 		],
 		'logpager' => [
@@ -403,11 +403,11 @@ $wgLBFactoryConf = [
 		],
 		'recentchangeslinked' => [
 			'db1096:3315' => 1,
-			# 'db1097:3315' => 1,
+			'db1097:3315' => 1,
 		],
 		'contributions' => [
 			'db1096:3315' => 1,
-			# 'db1097:3315' => 1,
+			'db1097:3315' => 1,
 		],
 		'logpager' => [
 			'db1096:3315' => 1,
