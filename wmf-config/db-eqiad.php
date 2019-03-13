311fea150f1cbe64e9394fa6f7388563c0b6f253
Manuel Arostegui <marostegui@wikimedia.org>
2019-03-13 09:22:17 +0100

    db-eqiad.php: Depool db1113:3316
    Schema change
    
    Change-Id: Ia504e5322c870d4953731167356bf3cb1b0d711c

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -160,8 +160,8 @@ $wgLBFactoryConf = [
 		'db1088' => 500,    # C2 3.6TB 512GB
 		'db1093' => 300,    # D2 3.6TB 512GB, api # candidate master
 		'db1096:3316' => 1, # A6 3.6TB 512GB # rc, log: s5 and s6
-		'db1098:3316' => 1, # B5 3.6TB 512GB # rc, log: s6 and s7
-		'db1113:3316' => 1, # B8 3.6TB 512GB # vslow, dump: s5 and s6
+		'db1098:3316' => 1, # B5 3.6TB 512GB # rc, log: s6 and s7 # temporary vslow
+		# 'db1113:3316' => 1, # B8 3.6TB 512GB # vslow, dump: s5 and s6 # schema change
 	],
 	's7' => [
 		'db1062' => 0,      # D4 2.8TB 128GB, master
@@ -416,10 +416,10 @@ $wgLBFactoryConf = [
 	],
 	's6' => [
 		'vslow' => [
-			'db1113:3316' => 1,
+			'db1098:3316' => 1,
 		],
 		'dump' => [
-			'db1113:3316' => 1,
+			'db1098:3316' => 1,
 		],
 		'api' => [
 			'db1085' => 10,
