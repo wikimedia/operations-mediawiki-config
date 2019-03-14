9d0df243c92f37c0ed1a17939e370adb4d6337a0
Manuel Arostegui <marostegui@wikimedia.org>
2019-03-14 06:59:26 +0100

    db-eqiad.php: Depool db1098
    Schema change on db1098:3316
    
    Also upgrade MySQL
    
    Change-Id: Iea636fe32ba8f9bb12ac885d527005fbc4e9438c

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -160,7 +160,7 @@ $wgLBFactoryConf = [
 		'db1088' => 500,    # C2 3.6TB 512GB
 		'db1093' => 300,    # D2 3.6TB 512GB, api # candidate master
 		'db1096:3316' => 1, # A6 3.6TB 512GB # rc, log: s5 and s6
-		'db1098:3316' => 1, # B5 3.6TB 512GB # rc, log: s6 and s7
+		# 'db1098:3316' => 1, # B5 3.6TB 512GB # rc, log: s6 and s7 # schema change and upgrade
 		'db1113:3316' => 1, # B8 3.6TB 512GB # vslow, dump: s5 and s6
 	],
 	's7' => [
@@ -169,7 +169,7 @@ $wgLBFactoryConf = [
 		'db1086' => 200,    # B3 3.6TB 512GB, api # candidate master
 		'db1090:3317' => 1, # C3 3.6TB 512GB, vslow, dump: s2 and s7, old master
 		'db1094' => 500,    # D2 3.6TB 512GB
-		'db1098:3317' => 150, # B5 3.6TB 512GB # rc, log: s6 and s7
+		# 'db1098:3317' => 150, # B5 3.6TB 512GB # rc, log: s6 and s7 # upgrade
 		'db1101:3317' => 150, # C2 3.6TB 512GB # rc, log: s7 and s8
 	],
 	's8' => [
@@ -427,23 +427,23 @@ $wgLBFactoryConf = [
 		],
 		'watchlist' => [
 			'db1096:3316' => 1,
-			'db1098:3316' => 1,
+			# 'db1098:3316' => 1,
 		],
 		'recentchanges' => [
 			'db1096:3316' => 1,
-			'db1098:3316' => 1,
+			# 'db1098:3316' => 1,
 		],
 		'recentchangeslinked' => [
 			'db1096:3316' => 1,
-			'db1098:3316' => 1,
+			# 'db1098:3316' => 1,
 		],
 		'contributions' => [
 			'db1096:3316' => 1,
-			'db1098:3316' => 1,
+			# 'db1098:3316' => 1,
 		],
 		'logpager' => [
 			'db1096:3316' => 1,
-			'db1098:3316' => 1,
+			# 'db1098:3316' => 1,
 		],
 	],
 	's7' => [
@@ -458,23 +458,23 @@ $wgLBFactoryConf = [
 			'db1086' => 1,
 		],
 		'watchlist' => [
-			'db1098:3317' => 1,
+			# 'db1098:3317' => 1,
 			'db1101:3317' => 1,
 		],
 		'recentchanges' => [
-			'db1098:3317' => 1,
+			# 'db1098:3317' => 1,
 			'db1101:3317' => 1,
 		],
 		'recentchangeslinked' => [
-			'db1098:3317' => 1,
+			# 'db1098:3317' => 1,
 			'db1101:3317' => 1,
 		],
 		'contributions' => [
-			'db1098:3317' => 1,
+			# 'db1098:3317' => 1,
 			'db1101:3317' => 1,
 		],
 		'logpager' => [
-			'db1098:3317' => 1,
+			# 'db1098:3317' => 1,
 			'db1101:3317' => 1,
 		],
 	],
