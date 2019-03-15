a75c1387b33397cfb2f51ae3214eb64fe30b7304
Baha <bmansurov@wikimedia.org>
2019-03-15 14:32:29 -0400

    Enable logging for CitationUsage and CitationUsagePageLoad
    Third round.
    
    Sampled at:
    - 100% for CitationUsage
    - 33.3% for CitationUsagePageLoad
    
    Bug: T213969
    Change-Id: I622ec80381dfd669c7ca014d88abe9cade773914

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -22320,6 +22320,14 @@ $wgConf->settings = [
 	'default' => true,
 ],
 
+'wgWMECitationUsagePopulationSize' => [
+	'enwiki' => 1  // 100% — T213969
+],
+
+'wgWMECitationUsagePageLoadPopulationSize' => [
+	'enwiki' => 3  // 33.3% — T213969
+],
+
 'wgWMEReadingDepthEnabled' => [
 	'default' => false,
 	'wikipedia' => true,
