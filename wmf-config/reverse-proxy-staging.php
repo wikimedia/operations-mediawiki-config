612943b804c9106250e37e7537d6dea486e9059e
James D. Forrester <jforrester@wikimedia.org>
2019-03-15 11:19:45 -0700

    Duplicate …Squid variables into …Cdn ahead of MW renaming
    Bug: T104148
    Change-Id: Id34339dff88bc6d1863378ac94b96b2d590b891d

--- wmf-config/reverse-proxy-staging.php
+++ wmf-config/reverse-proxy-staging.php
@@ -1,20 +1,23 @@
 <?php
 # WARNING: This file is publicly viewable on the web. Do not put private data here.
 
-# our text squid in beta labs gets forwarded requests
+# our text CDN in beta labs gets forwarded requests
 # from the ssl terminator, to 127.0.0.1, so adding that
 # address to the NoPurge list so that XFF headers for it
 # will be stripped; purge requests should get to it
-# on the address in the SquidServers list
+# on the address in the CdnServers list
 
 # The beta cluster does not have multicast, hence we route the purge
 # requests directly to the varnish instances.  They have varnishhtcpd listening
 # on UDP port 4827.
 
-$wgSquidServersNoPurge = [ '127.0.0.1',
+// Temporarily setting both …Cdn and …Squid for cut-over.
+$wgCdnServersNoPurge = [ '127.0.0.1',
 	'172.16.4.21',  # deployment-cache-text04
 	'172.16.5.14',  # deployment-cache-upload04
 ];
+$wgSquidServersNoPurge = $wgCdnServersNoPurge;
+
 $wgHTCPRouting = [
 	'|^https?://upload\.beta\.wmflabs\.org|' => [
 		'host' => '172.16.5.14',  # deployment-cache-upload04
