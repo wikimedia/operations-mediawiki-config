76c27d63e997067375e8004ba8dcf20f09793b2f
James D. Forrester <jforrester@wikimedia.org>
2019-03-16 15:15:12 -0700

    [Wikitech] Enable VisualEditor in extra namespaces
    Change-Id: I2fdc4b217db10ead3990a3cd003446a4d7937bb6

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -16124,6 +16124,11 @@ $wgConf->settings = [
 	'+wikidata' => [
 		'Project' => true,
 	],
+	'+wikitech' => [
+		'Obsolete' => true,
+		'OfficeIT' => true,
+		'Tool' => true,
+	],
 
 	// Private wikis
 	'+officewiki' => [
