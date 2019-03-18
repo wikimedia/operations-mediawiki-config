bf77d25305985b7ef9fca38716651ab1f4ba0cd9
Manuel Arostegui <marostegui@wikimedia.org>
2019-03-18 08:17:58 +0100

    db-eqiad.php: Give more traffic to db1101
    After mysql upgrade
    
    Change-Id: I7aef60638efd164a69130c487b8e595bf9a15cba

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -177,7 +177,7 @@ $wgLBFactoryConf = [
 		'db1087' => 1,      # C2 3.6TB 512GB, vslow, dump # master for sanitarium db1124
 		'db1092' => 200,    # D2 3.6TB 512GB, api
 		'db1099:3318' => 100, # B2 3.6TB 512GB # rc, log: s1 and s8
-		# 'db1101:3318' => 100, # C2 3.6TB 512GB # rc, log: s7 and s8 # upgrade
+		'db1101:3318' => 100, # C2 3.6TB 512GB # rc, log: s7 and s8 # upgrade
 		'db1104' => 200,    # B3 3.6TB 512GB, api # candidate master
 		'db1109' => 500,    # D8 3.6TB 512GB
 	],
@@ -459,11 +459,11 @@ $wgLBFactoryConf = [
 		],
 		'watchlist' => [
 			'db1098:3317' => 1,
-			# 'db1101:3317' => 1,
+			'db1101:3317' => 1,
 		],
 		'recentchanges' => [
 			'db1098:3317' => 1,
-			# 'db1101:3317' => 1,
+			'db1101:3317' => 1,
 		],
 		'recentchangeslinked' => [
 			'db1098:3317' => 1,
