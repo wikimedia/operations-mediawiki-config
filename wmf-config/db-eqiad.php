db410fcf1e442871964576d7803e8252daec6407
Manuel Arostegui <marostegui@wikimedia.org>
2019-03-18 09:35:28 +0100

    db-eqiad.php: Depool db1104
    Schema change
    
    Change-Id: I42714e9a78ba03b1ef8a8b1b4966723d5727ff45

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -178,7 +178,7 @@ $wgLBFactoryConf = [
 		'db1092' => 200,    # D2 3.6TB 512GB, api
 		'db1099:3318' => 100, # B2 3.6TB 512GB # rc, log: s1 and s8
 		'db1101:3318' => 100, # C2 3.6TB 512GB # rc, log: s7 and s8 # upgrade
-		'db1104' => 200,    # B3 3.6TB 512GB, api # candidate master
+		# 'db1104' => 200,    # B3 3.6TB 512GB, api # candidate master # schema change
 		'db1109' => 500,    # D8 3.6TB 512GB
 	],
 
@@ -487,7 +487,7 @@ $wgLBFactoryConf = [
 		],
 		'api' => [
 			'db1092' => 3,
-			'db1104' => 1,
+			# 'db1104' => 1,
 		],
 		'watchlist' => [
 			'db1099:3318' => 1,
