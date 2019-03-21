d9aa4675a22d8a0a89300fe6a24cb4d3adc5553b
Manuel Arostegui <marostegui@wikimedia.org>
2019-03-21 09:17:06 +0100

    db-eqiad.php. Depool db1086
    Schema change + upgrade
    
    Change-Id: I3eb639ef769c521f76b68e4e868bfa941e0d7140

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -166,7 +166,7 @@ $wgLBFactoryConf = [
 	's7' => [
 		'db1062' => 0,      # D4 2.8TB 128GB, master
 		'db1079' => 300,    # A2 3.6TB 512GB, api # master for sanitarium db1125
-		'db1086' => 200,    # B3 3.6TB 512GB, api # candidate master
+		# 'db1086' => 200,    # B3 3.6TB 512GB, api # candidate master # upgrade
 		'db1090:3317' => 1, # C3 3.6TB 512GB, vslow, dump: s2 and s7, old master
 		'db1094' => 500,    # D2 3.6TB 512GB
 		'db1098:3317' => 150, # B5 3.6TB 512GB # rc, log: s6 and s7
@@ -455,7 +455,7 @@ $wgLBFactoryConf = [
 		],
 		'api' => [
 			'db1079' => 3,
-			'db1086' => 1,
+			# 'db1086' => 1,
 		],
 		'watchlist' => [
 			'db1098:3317' => 1,
