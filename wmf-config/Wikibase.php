464ba38d1499f1a43e5f39c769a4ec499f5251a1
Amir Sarabadani <Ladsgroup@gmail.com>
2019-03-21 17:11:31 +0100

    Add wikimaniawiki to another special group in Wikibase client
    Bug: T217730
    Change-Id: If52bf9a593ab2255535492d06cb895a6e50662fd

--- wmf-config/Wikibase.php
+++ wmf-config/Wikibase.php
@@ -212,7 +212,7 @@ if ( $wmgUseWikibaseClient ) {
 	$wbSiteGroup = isset( $wmgWikibaseSiteGroup ) ? $wmgWikibaseSiteGroup : null;
 	$wgWBClientSettings['languageLinkSiteGroup'] = $wbSiteGroup;
 
-	if ( in_array( $wgDBname, [ 'commonswiki', 'mediawikiwiki', 'metawiki', 'specieswiki' ] ) ) {
+	if ( in_array( $wgDBname, [ 'commonswiki', 'mediawikiwiki', 'metawiki', 'specieswiki', 'wikimaniawiki' ] ) ) {
 		$wgWBClientSettings['languageLinkSiteGroup'] = 'wikipedia';
 	}
 
