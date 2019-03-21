6de0159cc8799813a87c257f03d080c36d9a3e6c
Stephane Bisson <sbisson@wikimedia.org>
2019-03-21 13:33:15 -0400

    Disable Welcome survey on viwiki
    Bug: T218920
    Change-Id: Ic72ecdc503bc1e4fd82a600e0e0f301121aec5a5

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -22629,7 +22629,6 @@ $wgConf->settings = [
 	'testwiki' => true,
 	'kowiki' => true,
 	'cswiki' => true,
-	'viwiki' => true,
 ],
 
 'wgWelcomeSurveyPrivacyPolicyUrl' => [
@@ -22641,7 +22640,6 @@ $wgConf->settings = [
 'wgWelcomeSurveyExperimentalGroups' => [
 	'kowiki' => [ 'exp2_target_specialpage' => [ 'range' => '0-9' ], 'exp2_target_popup' => [ 'range' => 'x' ] ],
 	'cswiki' => [ 'exp2_target_specialpage' => [ 'range' => '0-9' ], 'exp2_target_popup' => [ 'range' => 'x' ] ],
-	'viwiki' => [ 'exp2_target_specialpage' => [ 'range' => '0-4' ], 'exp2_target_popup' => [ 'range' => 'x' ] ],
 ],
 
 'wgGEHelpPanelEnabled' => [
