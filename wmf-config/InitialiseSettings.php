fbffe939037b7de11b5d62bd3715a8862f644904
David Causse <dcausse@wikimedia.org>
2019-03-22 09:19:52 +0100

    [cirrus] switch all wikis to eqiad (elastic 6.5.4)
    Bug: T218878
    Change-Id: Iaa7223b0072a59933e3144286ae9c0eee6b37cb5

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -20695,7 +20695,6 @@ $wgConf->settings = [
 // bring down a specific cluster.
 'wmgCirrusSearchDefaultCluster' => [
 	'default' => 'local',
-	'cirrussearch-big-indices' => 'codfw',
 ],
 
 'wmgCirrusSearchClusterOverrides' => [
