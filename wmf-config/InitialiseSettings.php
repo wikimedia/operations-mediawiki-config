d4ce87d31f7164e7aa71f08927714808c068cf23
alaasarhan <alaa.sarhan@wikimedia.de>
2019-03-23 20:25:26 +0100

    Add wgScoreLineWidthInches to labs config
    Bug: T218191
    Change-Id: Id69badfe464356c02f52a1cdeddeb908661ed5ec

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -20564,6 +20564,12 @@ $wgConf->settings = [
 	'commonswiki' => true,
 ],
 
+'wgMusicalNotationLineWidthInches' => [
+    'default' => 8,
+    'testwikidatawiki' => 3,
+    'wikidatawiki' => 3,
+],
+
 'wgScoreTrim' => [
 	'default' => true,
 ],
