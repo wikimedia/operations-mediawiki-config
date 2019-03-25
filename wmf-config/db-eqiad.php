fe069113ba34577ea37eaa24c47f6ef13fd2502f
Manuel Arostegui <marostegui@wikimedia.org>
2019-03-25 09:17:22 +0100

    db-eqiad.php: Depool db1105:3311
    Schema change
    
    Change-Id: I60d5f07432bcf7fa4b0d073c9927c7facbfb2f33

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -116,7 +116,7 @@ $wgLBFactoryConf = [
 		'db1083' => 500,    # B1 3.6TB 512GB # candidate master
 		'db1089' => 500,    # C3 3.6TB 512GB # T217397
 		'db1099:3311' => 50, # B2 3.6TB 512GB # rc, log: s1 and s8
-		'db1105:3311' => 50, # C3 3.6TB 512GB # rc, log: s1 and s2
+		# 'db1105:3311' => 50, # C3 3.6TB 512GB # rc, log: s1 and s2 # schema change
 		'db1106' => 50,     # D3 3.6TB 512GB, vslow, dump # master for sanitarium db1124
 		'db1118' => 200,    # B1 3.6TB 512GB, api
 		'db1119' => 200,    # B8 3.6TB 512GB, api
@@ -264,23 +264,23 @@ $wgLBFactoryConf = [
 	's1' => [
 		'watchlist' => [
 			'db1099:3311' => 1,
-			'db1105:3311' => 1,
+			# 'db1105:3311' => 1,
 		],
 		'recentchanges' => [
 			'db1099:3311' => 1,
-			'db1105:3311' => 1,
+			# 'db1105:3311' => 1,
 		],
 		'recentchangeslinked' => [
 			'db1099:3311' => 1,
-			'db1105:3311' => 1,
+			# 'db1105:3311' => 1,
 		],
 		'contributions' => [
 			'db1099:3311' => 1,
-			'db1105:3311' => 1,
+			# 'db1105:3311' => 1,
 		],
 		'logpager' => [
 			'db1099:3311' => 1,
-			'db1105:3311' => 1,
+			# 'db1105:3311' => 1,
 		],
 		'dump' => [
 			'db1106' => 1,
