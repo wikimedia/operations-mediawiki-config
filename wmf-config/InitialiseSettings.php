6268a2faa2ff28edb0945710dbd20f3c03c6f99e
DCausse <dcausse@wikimedia.org>
2019-03-25 19:07:02 +0000

    Revert "Re-enable eventgate-analytics api-request logging for group0 wikis"
    This reverts commit 8c101cc9fe6b3e26ce7a2adb0b38738fa227813b.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Change-Id: I7456a30ed98d9f374b773e2905fb53b12a4249f5

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -5649,7 +5649,6 @@ $wgConf->settings = [
 	],
 	'+group0' => [
 		'authorization' => [ 'udp2log' => 'info', 'logstash' => false ],
-		'api-request' => [ 'udp2log' => false, 'logstash' => false, 'eventbus' => 'debug', 'buffer' => true ],
 	],
 	'+arwiki' => [
 		'authorization' => [ 'udp2log' => 'info', 'logstash' => false ],
