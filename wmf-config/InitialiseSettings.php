37188dee0fa88224c8b9f9071b90bbd796703c11
Erik Bernhardson <ebernhardson@wikimedia.org>
2019-03-29 09:05:49 -0700

    Disable wbcs dispatching query builder on commons (1/3)
    The wikibase full text query builder doesn't support the SDoC use case
    yet. Disable it until it is ready.
    
    First patch deploys the new files but does not reference them.
    
    Bug: T218954
    Change-Id: I5c474c71afde2854cbc8b68f083982f09a5e9c56

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -20462,7 +20462,7 @@ $wgConf->settings = [
 ],
 
 // List of properties to be indexed by elasticsearch
-// see WikibaseSearchSettings.php
+// see WikidataSearchSettings.php
 'wmgWikibaseSearchIndexProperties' => [
 	'default' => [],
 	'wikidatawiki' => [ 'P31', 'P279' ], // instance of, subclass of
@@ -20485,7 +20485,7 @@ $wgConf->settings = [
 ],
 
 // Wikidata search: configure boost based on statements (T148411)
-// see WikibaseSearchSettings.php
+// see WikidataSearchSettings.php
 // NOTE: these properties need to be indexed first (see wmgWikibaseSearchIndexProperties)
 'wmgWikibaseSearchStatementBoosts' => [
 	'default' => [],
