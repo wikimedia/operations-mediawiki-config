dbcd58bf3a603844a9921d82b6ce3a43c4acb8b6
Cormac Parle <cparle@wikimedia.org>
2019-03-29 17:01:51 +0000

    Add 'depicts' statements to search index on testcommons
    Change-Id: Id84cbac928a036b54f47a50817b8f7c15172aac9

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -20410,6 +20410,7 @@ $wgConf->settings = [
 	'default' => [],
 	'wikidatawiki' => [ 'P31', 'P279' ], // instance of, subclass of
 	'testwikidatawiki' => [ 'P7', 'P700' ], // test only: Item and ExternalId
+	'testcommonswiki' => [ 'P180' ], // depicts
 ],
 
 'wmgWikibaseSearchIndexPropertiesExclude' => [
