07cf7cd0178a58219545ae5918ae5316756d78da
Manuel Arostegui <marostegui@wikimedia.org>
2019-04-03 09:35:26 +0200

    db-eqiad,db-codfw.php: Depool s8 sanitarium masters
    The table wikimedia_editor_tasks_entity_description_exists needs to be
    reimported into sanitarium and labs, so depool the sanitarium masters
    
    Bug: T218302
    Change-Id: I783c1b1051eed73356b4efdecf37b18f76459040

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -174,8 +174,8 @@ $wgLBFactoryConf = [
 	],
 	's8' => [
 		'db1071' => 0,      # D1 2.8TB 160GB, master
-		'db1087' => 1,      # C2 3.6TB 512GB, vslow, dump # master for sanitarium db1124
-		'db1092' => 200,    # D2 3.6TB 512GB, api
+		# 'db1087' => 1,      # C2 3.6TB 512GB, vslow, dump # master for sanitarium db1124 # T218302
+		'db1092' => 100,    # D2 3.6TB 512GB, api # temporary vslow
 		'db1099:3318' => 100, # B2 3.6TB 512GB # rc, log: s1 and s8
 		'db1101:3318' => 100, # C2 3.6TB 512GB # rc, log: s7 and s8
 		'db1104' => 200,    # B3 3.6TB 512GB, api # candidate master
@@ -480,13 +480,13 @@ $wgLBFactoryConf = [
 	],
 	's8' => [
 		'vslow' => [
-			'db1087' => 1,
+			'db1092' => 1,
 		],
 		'dump' => [
-			'db1087' => 1,
+			'db1092' => 1,
 		],
 		'api' => [
-			'db1092' => 3,
+			'db1092' => 1,
 			'db1104' => 1,
 		],
 		'watchlist' => [
