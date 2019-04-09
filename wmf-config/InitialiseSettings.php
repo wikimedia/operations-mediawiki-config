a1716e808dca74b7268b269de05398eec5dda13d
C. Scott Ananian <cscott@cscott.net>
2019-04-09 14:13:23 -0400

    Default to Preprocessor_Hash on both PHP7 and HHVM
    There are subtle differences in how resource limits are computed between
    Preprocessor_Hash and Preprocessor_DOM (T216664).  Use the same
    preprocessor on both PHP7 and HHVM to avoid exposing these differences
    to users in production.
    
    Bug: T216664
    Bug: T219901
    Change-Id: Id38c9360e4d02b570996dbf7a660f964f02f1a2c

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -13441,6 +13441,14 @@ $wgConf->settings = [
 ],
 # @} end of wgNamespaceProtection
 
+# Default to Preprocessor_Hash (T219901)
+'wgParserConf' => [
+	'default' => [
+		'class' => Parser::class,
+		'preprocessorClass' => Preprocessor_Hash::class,
+	],
+],
+
 # Enable magic links regardless of core default (T145604)
 'wgEnableMagicLinks' => [
 	'default' => [
