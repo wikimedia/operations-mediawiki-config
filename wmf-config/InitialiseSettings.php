e491e71b3bf3d1172380ce0f4d8820cc439bf8c0
Andrew Otto <acotto@gmail.com>
2019-04-10 13:29:14 -0400

    Enable api-request EventGate logging for group1 wikis
    Bug: T214080
    Change-Id: I0f5e8361a5316ed13b0b88373d5335124da5f7e3

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -5652,6 +5652,8 @@ $wgConf->settings = [
 	],
 	'+group1' => [
 		'authorization' => [ 'udp2log' => 'info', 'logstash' => false ],
+		'api-request' => [ 'udp2log' => false, 'logstash' => false, 'eventbus' => 'debug', 'buffer' => true ],
+
 	],
 	'+group0' => [
 		'authorization' => [ 'udp2log' => 'info', 'logstash' => false ],
