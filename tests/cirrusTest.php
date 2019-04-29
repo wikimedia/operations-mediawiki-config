cc78abf08def8d35c5e4b59d71056f7ffc14e068
Erik Bernhardson <ebernhardson@wikimedia.org>
2019-04-29 15:59:17 -0700

    Add cloudelastic servers to wgCirrusSearchClusters
    Bug: T220625
    Change-Id: I46867cb42a36c380a35b5c8f2f24b9ddb7815f35

--- tests/cirrusTest.php
+++ tests/cirrusTest.php
@@ -13,8 +13,8 @@ class CirrusTest extends WgConfTestCase {
 		// This is transformed from 'local' to 'unittest', but if it was set
 		// to a specific cluster and not 'local' this fails.
 		// $this->assertEquals( 'unittest', $config['wgCirrusSearchDefaultCluster'] );
-		// 2 DCs * 3 ES clusters per DC
-		$this->assertCount( 2 * 3, $config['wgCirrusSearchClusters'] );
+		// (2 DCs + 1 cloudelastic) * 3 ES clusters per
+		$this->assertCount( 3 * 3, $config['wgCirrusSearchClusters'] );
 
 		// testwiki writes to eqiad and codfw
 		$this->assertCount( 2, $config['wgCirrusSearchWriteClusters'] );
@@ -49,8 +49,8 @@ class CirrusTest extends WgConfTestCase {
 		$this->assertArrayNotHasKey( 'wgCirrusSearchServers', $config );
 		$this->assertArrayHasKey( 'wgCirrusSearchClusters', $config );
 		$this->assertArrayHasKey( 'wgCirrusSearchDefaultCluster', $config );
-		// 2 DCs * 3 ES clusters per DC
-		$this->assertCount( 2 * 3, $config['wgCirrusSearchClusters'] );
+		// (2 DCs + 1 cloudelastic) * 3 ES clusters per
+		$this->assertCount( 3 * 3, $config['wgCirrusSearchClusters'] );
 		$this->assertCount( 2, $config['wgCirrusSearchShardCount'] );
 		$this->assertCount( 2, $config['wgCirrusSearchReplicas'] );
 		$this->assertCount( 2, $config['wgCirrusSearchClientSideConnectTimeout'] );
