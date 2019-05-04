6f6ec29fcaba4d5ea287e514817b9efe41e6a1da
Ammar <ammarpad@yahoo.com>
2019-05-04 17:02:48 +0100

    Enable Page Previews as default on hewikivoyage
    Bug: T222017
    Change-Id: I3adecc223a521d3c3dbaab08889c88d3668605fe

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -15007,6 +15007,7 @@ $wgConf->settings = [
 	'sewikimedia' => true, // T68374
 	'wikipedia' => true,
 	'enwikivoyage' => true, // T203981
+	'hewikivoyage' => true // T222017
 ],
 
 'wgPopupsPageBlacklist' => [
@@ -15060,12 +15061,12 @@ $wgConf->settings = [
 // T197719 Enable PagePreviews for all newly created accounts
 'wgPopupsOptInStateForNewAccounts' => [
 	'default' => '1',
-	'enwikivoyage' => '1',
 ],
 
 'wgPopupsOptInDefaultState' => [
 	'default' => '0',
 	'enwikivoyage' => '1',
+	'hewikivoyage' => '1',
 ],
 
 // T160081 (and others): Make sure that Page Previews can detect the many, many
