87572bfcddffa6affda2ee21eb0f9197e728dcf4
Erik Bernhardson <ebernhardson@wikimedia.org>
2019-05-06 09:13:16 -0700

    cloudelastic: Don't write to private wikis on cloudelastic
    Change-Id: I4dc2ac96a6cf7251dc988334586d90325e4fea68

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -20951,6 +20951,7 @@ $wgConf->settings = [
 'wmgCirrusSearchWriteClusters' => [
 	'default' => [ 'eqiad', 'codfw' ],
 	'group0' => [ 'eqiad', 'codfw', 'cloudelastic' ],
+	'private' => [ 'eqiad', 'codfw' ],
 ],
 
 'wmgCirrusSearchReplicaGroup' => [
