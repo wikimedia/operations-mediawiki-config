c0e8b2b3ba5bbad959fe32b0fc054087b23a560c
Nicholas Ray <nray@wikimedia.org>
2019-05-09 12:21:46 -0600

    Enable AdvancedMobileContributions Overflow menu
    This will enable the Overflow Menu
    (I4b50a0e519024a7ab91dae6ab40b23cf14a03861) everywhere that AMC is
    enabled. Where AMC is not enabled, it will be disabled.
    
    Change-Id: I9e798e6539b1cc9e84f5636da99a2b0f640282f3

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -17097,6 +17097,12 @@ $wgConf->settings = [
 	'test2wiki' => true,
 ],
 
+'wgMinervaOverflowInPageActions' => [
+	"base" => false,
+	"beta" => false,
+	"amc" => true
+],
+
 'wgMinervaEnableSiteNotice' => [
 	'default' => false,
 	'arwiki' => true,
