384a77bdd49999021532f016a1990ba65343c2c9
Manuel Arostegui <marostegui@wikimedia.org>
2019-05-09 17:49:26 +0200

    db-eqiad.php: More traffic to db1081
    Also starting to restore original weights
    
    Change-Id: I90e3f4280ccb844dabf5c6657494ad8a94b3d939

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -142,9 +142,9 @@ $wgLBFactoryConf = [
 		'db1081' => 100,    # A2 3.6TB 512GB, api # candidate master
 		'db1084' => 100,    # B1 3.6TB 512GB, api
 		'db1091' => 500,    # D2 3.6TB 512GB
-		'db1097:3314' => 200, # D1 3.6TB 512GB # rc, log: s4 and s5
-		'db1103:3314' => 200, # A3 3.6TB 512GB # rc, log: s2 and s4
-		'db1121' => 100,      # C6 3.6TB 512GB, vslow, dump # master for sanitarium db1125
+		'db1097:3314' => 150, # D1 3.6TB 512GB # rc, log: s4 and s5
+		'db1103:3314' => 150, # A3 3.6TB 512GB # rc, log: s2 and s4
+		'db1121' => 1,      # C6 3.6TB 512GB, vslow, dump # master for sanitarium db1125
 	],
 	's5' => [
 		'db1070' => 0,      # D1 2.8TB 160GB, master
@@ -361,7 +361,7 @@ $wgLBFactoryConf = [
 			'db1121' => 1,
 		],
 		'api' => [
-			'db1081' => 1,
+			'db1081' => 2,
 			'db1084' => 1,
 		],
 		'watchlist' => [
