a334439f6c10fa506594bff75b8357ff027ff263
Michael Große <michael.grosse@wikimedia.de>
2019-05-14 18:47:36 +0200

    Add feature flag config for breaking Wikibase API change
    It was decided that a bugfix for the Wikibase API is a breaking change
    and thus needs a feature flag.
    
    This adds the feature flag to the configuration and activates it for
    beta.
    
    Bug: T223300
    Change-Id: I1105b61c05d8392e6e303e66d3037b6745de89ff
    Depends-On: I7ac01018b43487ed11b1468fff1386682e3db698

--- wmf-config/Wikibase.php
+++ wmf-config/Wikibase.php
@@ -213,6 +213,8 @@ if ( $wmgUseWikibaseRepo ) {
 
 	// Temporary, T138104
 	$wgWBRepoSettings['tmpSerializeEmptyListsAsObjects'] = $wmgWikibaseTmpSerializeEmptyListsAsObjects;
+
+	$wgWBRepoSettings['featureFlagWbeditentitySetEmptyAliases'] = $wmgWikibasefeatureFlagWbeditentitySetEmptyAliases;
 }
 
 if ( $wmgUseWikibaseClient ) {
