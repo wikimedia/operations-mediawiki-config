ba8fa70f0db19eb5cc453f4e7f1e1bd8f31b7e50
Michael Große <michael.grosse@wikimedia.de>
2019-05-21 18:46:32 +0200

    Add a list of IDs to skip in production
    These will be prefilled with a maintenance script.
    
    Change-Id: I8b8725f6e2baab150cec8ca26357e3d83dea6dca

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -18845,6 +18845,12 @@ $wgConf->settings = [
 	'votewiki' => false,
 	'zerowiki' => false,
 ],
+	
+'wgEntitySchemaSkippedIDs' => [
+	'default' => [],
+	'wikidatawiki' => [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 40, 42, 123, 570, 734, 735, 999, 1234, 3300, 11424, 12345 ],
+	'testwikidatawiki' => [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 40, 42, 123, 570, 734, 735, 999, 1234, 3300, 11424, 12345 ],
+],
 
 'wmgUseEntitySchema' => [
 	'default' => false,
