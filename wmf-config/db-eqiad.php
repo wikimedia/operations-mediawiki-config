3ed3bc70e6d4d3701b1e6394ff5e0514777974b0
Manuel Arostegui <marostegui@wikimedia.org>
2019-05-22 07:04:01 +0200

    db-eqiad.php: Depool db1118 from API, pool db1134
    db1118 is one of the hosts that have a weird query plan, pool db1134
    which looks like has a better query plan
    
    Bug: T224017
    Change-Id: I5d951e6d25a475366bacdaeea874e8e93ca66ac4

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -118,9 +118,9 @@ $wgLBFactoryConf = [
 		'db1099:3311' => 50, # B2 3.6TB 512GB # rc, log: s1 and s8
 		'db1105:3311' => 50, # C3 3.6TB 512GB # rc, log: s1 and s2
 		'db1106' => 50,     # D3 3.6TB 512GB, vslow, dump # master for sanitarium db1124
-		'db1118' => 200,    # B1 3.6TB 512GB, api
+		'db1118' => 500,    # B1 3.6TB 512GB
 		'db1119' => 200,    # B8 3.6TB 512GB, api
-		'db1134' => 300,    # C6 4.4TB 512GB
+		'db1134' => 200,    # C6 4.4TB 512GB, api
 	],
 	's2' => [
 		'db1066' => 0,      # A6 2.8TB 160GB # master
@@ -296,8 +296,8 @@ $wgLBFactoryConf = [
 		],
 		'api' => [
 			'db1080' => 1,
-			'db1118' => 1,
 			'db1119' => 1,
+			'db1134' => 1,
 		],
 	],
 	's2' => [
