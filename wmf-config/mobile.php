5ebec21656afab619fd4237506691b248dc2d162
James D. Forrester <jforrester@wikimedia.org>
2019-05-24 11:59:28 -0700

    Even more invariant config moved over to CommonSettings
    Change-Id: Idf294be079a38c38ca8e57dc7e73d66558b115dd

--- wmf-config/mobile.php
+++ wmf-config/mobile.php
@@ -1,79 +1,74 @@
 <?php
 # WARNING: This file is publicly viewable on the web. Do not put private data here.
 
-if ( $wmgMobileFrontend ) {
-	wfLoadExtension( 'MobileFrontend' );
+wfLoadExtension( 'MobileFrontend' );
 
-	// Load skin
-	if ( $wmgMinervaNeue ) {
-		wfLoadSkin( 'MinervaNeue' );
+// Load skin
+wfLoadSkin( 'MinervaNeue' );
+
+$wgMFMobileHeader = 'X-Subdomain';
+$wgMFNoindexPages = false;
+
+$wgMFNearby = !!$wmgEnableGeoData;
+
+if ( $wmgZeroBanner && !$wmgZeroPortal ) {
+	wfLoadExtension( 'ZeroBanner' );
+
+	if ( !isset( $wgJsonConfigs ) ) {
+		$wgJsonConfigs = [];
+	}
+	if ( !isset( $wgJsonConfigs['JsonZeroConfig'] ) ) {
+		$wgJsonConfigs['JsonZeroConfig'] = [];
+	}
+	$wgJsonConfigs['JsonZeroConfig']['isLocal'] = false;
+	$wgJsonConfigs['JsonZeroConfig']['remote'] = [
+		'url' => 'https://zero.wikimedia.org/w/api.php',
+		'username' => $wmgZeroPortalApiUserName,
+		'password' => $wmgZeroPortalApiPassword,
+	];
+
+	$wgGroupPermissions['sysop']['jsonconfig-flush'] = true;
+}
+
+$wgHooks['EnterMobileMode'][] = function () {
+	global $wgCentralAuthCookieDomain, $wgHooks, $wgIncludeLegacyJavaScript;
+
+	// Disable loading of legacy wikibits in the mobile web experience
+	$wgIncludeLegacyJavaScript = false;
+
+	// Hack for T49647
+	if ( $wgCentralAuthCookieDomain == 'commons.wikimedia.org' ) {
+		$wgCentralAuthCookieDomain = 'commons.m.wikimedia.org';
+	} elseif ( $wgCentralAuthCookieDomain == 'meta.wikimedia.org' ) {
+		$wgCentralAuthCookieDomain = 'meta.m.wikimedia.org';
 	}
 
-	$wgMFMobileHeader = 'X-Subdomain';
-	$wgMFNoindexPages = false;
-	if ( !$wmgEnableGeoData ) {
-		$wgMFNearby = false;
-	}
-
-	if ( $wmgZeroBanner && !$wmgZeroPortal ) {
-		wfLoadExtension( 'ZeroBanner' );
-
-		if ( !isset( $wgJsonConfigs ) ) {
-			$wgJsonConfigs = [];
-		}
-		if ( !isset( $wgJsonConfigs['JsonZeroConfig'] ) ) {
-			$wgJsonConfigs['JsonZeroConfig'] = [];
-		}
-		$wgJsonConfigs['JsonZeroConfig']['isLocal'] = false;
-		$wgJsonConfigs['JsonZeroConfig']['remote'] = [
-			'url' => 'https://zero.wikimedia.org/w/api.php',
-			'username' => $wmgZeroPortalApiUserName,
-			'password' => $wmgZeroPortalApiPassword,
-		];
-
-		$wgGroupPermissions['sysop']['jsonconfig-flush'] = true;
-	}
-
-	$wgHooks['EnterMobileMode'][] = function () {
-		global $wgCentralAuthCookieDomain, $wgHooks, $wgIncludeLegacyJavaScript;
-
-		// Disable loading of legacy wikibits in the mobile web experience
-		$wgIncludeLegacyJavaScript = false;
-
-		// Hack for T49647
-		if ( $wgCentralAuthCookieDomain == 'commons.wikimedia.org' ) {
-			$wgCentralAuthCookieDomain = 'commons.m.wikimedia.org';
-		} elseif ( $wgCentralAuthCookieDomain == 'meta.wikimedia.org' ) {
-			$wgCentralAuthCookieDomain = 'meta.m.wikimedia.org';
-		}
-
-		// Better hack for T49647
-		$wgHooks['WebResponseSetCookie'][] = function ( &$name, &$value, &$expire, &$options ) {
-			if ( isset( $options['domain'] ) ) {
-				if ( $options['domain'] == 'commons.wikimedia.org' ) {
-					$options['domain'] = 'commons.m.wikimedia.org';
-				} elseif ( $options['domain'] == 'meta.wikimedia.org' ) {
-					$options['domain'] = 'meta.m.wikimedia.org';
-				}
+	// Better hack for T49647
+	$wgHooks['WebResponseSetCookie'][] = function ( &$name, &$value, &$expire, &$options ) {
+		if ( isset( $options['domain'] ) ) {
+			if ( $options['domain'] == 'commons.wikimedia.org' ) {
+				$options['domain'] = 'commons.m.wikimedia.org';
+			} elseif ( $options['domain'] == 'meta.wikimedia.org' ) {
+				$options['domain'] = 'meta.m.wikimedia.org';
 			}
-		};
-
-		return true;
+		}
 	};
 
-	$wgMFNearbyRange = $wgMaxGeoSearchRadius;
+	return true;
+};
 
-	$wgMFEnableBeta = true;
+$wgMFNearbyRange = $wgMaxGeoSearchRadius;
 
-	// Turn on volunteer recruitment
-	$wgMFEnableJSConsoleRecruitment = true;
+$wgMFEnableBeta = true;
 
-	// Brute-force bandwidth optimization by stripping srcset (T119797)
-	$wgMFStripResponsiveImages = true;
+// Turn on volunteer recruitment
+$wgMFEnableJSConsoleRecruitment = true;
 
-	// Oversample logging for visualeditor
-	$wgMFSchemaEditAttemptStepOversample = 'visualeditor';
-}
+// Brute-force bandwidth optimization by stripping srcset (T119797)
+$wgMFStripResponsiveImages = true;
+
+// Oversample logging for visualeditor
+$wgMFSchemaEditAttemptStepOversample = 'visualeditor';
 
 if ( $wmfRealm === 'labs' ) {
 	require_once __DIR__ . '/mobile-labs.php';
