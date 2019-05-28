5e12e1935643b06c209109ad3726d2d02b27918e
Michael Holloway <mholloway@wikimedia.org>
2019-05-28 16:20:00 -0400

    WikimediaEditorTasks: Update caption edit target counts
    Bug: T224299
    Change-Id: Ib78e93f368ee144ae2563854e48c0c9165e1b85a

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -23001,7 +23001,7 @@ $wgConf->settings = [
 		[
 			'class' => 'MediaWiki\\Extension\\WikimediaEditorTasks\\WikipediaAppCaptionEditCounter',
 			'counter_key' => 'app_caption_edits',
-			'target_counts' => [ 50 ],
+			'target_counts' => [ 3, 15 ],
 			'delay' => 86400,
 		],
 	],
