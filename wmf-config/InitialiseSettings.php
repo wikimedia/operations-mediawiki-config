54eb97ab0536a48e97b7bb698561786b50fb5e22
Michael Holloway <mholloway@wikimedia.org>
2019-05-31 13:23:31 -0400

    WikimediaEditorTasks: Drop caption edit counter unlock delay to 0
    Bug: T218599
    Change-Id: Idad0016f33643023d0ae170a52785eba53fe3721

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -22976,7 +22976,7 @@ $wgConf->settings = [
 			'class' => 'MediaWiki\\Extension\\WikimediaEditorTasks\\WikipediaAppCaptionEditCounter',
 			'counter_key' => 'app_caption_edits',
 			'target_counts' => [ 3, 15 ],
-			'delay' => 86400,
+			'delay' => 0,
 		],
 	],
 ],
