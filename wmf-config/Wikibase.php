60f75592dd21a8531c55a88edef4b58dd54f2cdf
mvolz <marielle.volz@gmail.com>
2019-06-05 12:30:27 +0100

    Enable reftabs on testwikidata
    Enable reference tabs on testwikidata
    only.
    
    Disabled by default.
    
    Depends-on: Iafd8b7221
    Bug: T199197
    Change-Id: Idde139c094c9dd4dc24a3532bfcd42e1effc3ee2

--- wmf-config/Wikibase.php
+++ wmf-config/Wikibase.php
@@ -93,6 +93,11 @@ if ( $wmgUseWikibaseRepo ) {
 		$wgSpecialPages['ItemDisambiguation'] = 'SpecialBlankpage';
 	}
 
+	// Only enable reference tabs on testwikidata - temporary T199197
+	if ( $wgDBname === 'testwikidatawiki' ) {
+		$wgWBRepoSettings['enableRefTabs'] = true;
+	}
+
 	if ( $wgDBname === 'wikidatawiki' || $wgDBname === 'testwikidatawiki' ) {
 		// Don't try to let users answer captchas if they try to add links
 		// on either Item or Property pages. T86453
