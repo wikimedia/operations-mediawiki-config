13d8c9fc8bfb671f66949b4f90675619557988e4
alaasarhan <alaa.sarhan@wikimedia.de>
2019-06-18 17:23:24 +0200

    Introduce config variables for new terms store in mediawiki-config
    Bug: T226086
    Change-Id: Iaad411d59f96ee3ab151533ce5e3fcf7587ec58c

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -23086,7 +23086,13 @@ $wgConf->settings = [
 'wmgWatchRollback' => [
 	'default' => false,
 	'arwiki' => true, // T220186
-]
+],
+'wmgWikibaseTmpPropertyTermsMigrationStage' => [
+	'default' => MIGRATION_OLD,
+],
+'wmgWikibaseTmpItemTermsMigrationStage' => [
+	'default' => [ 'max' => MIGRATION_OLD ],
+],
 ];
 
 ### WMF Labs override #####
