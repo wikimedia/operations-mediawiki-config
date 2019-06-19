05bf3d52004a665cb84d74153988479b92202c23
Manuel Arostegui <marostegui@wikimedia.org>
2019-06-19 11:19:18 +0200

    db-eqiad.php: Slowly repool db1077
    Bug: T225981
    Change-Id: Id50ef3f63ddefc26d5b1b621428889cc01e6f619

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -137,7 +137,7 @@ $wgLBFactoryConf = [
 	/* s3 */ 'DEFAULT' => [
 		'db1075' => 0,      # A2 3.6TB 512GB, master
 		'db1078' => 500,    # C3 3.6TB 512GB, candidate master
-		# 'db1077' => 400,    # B1 3.6TB 512GB, rc, log # master for sanitarium db1124 # T225981
+		'db1077' => 100,    # B1 3.6TB 512GB, rc, log # master for sanitarium db1124 # T225981
 		'db1123' => 300,    # D8 3.6TB 512GB, vslow, dump # temporary rc
 	],
 	's4' => [
