60825e6e4de6a73f2854b7d30b57b46b4e7aa8e1
alaasarhan <alaa.sarhan@wikimedia.de>
2019-06-19 12:04:33 +0200

    Switch property terms migration to WRITE_BOTH on test wikidata
    Bug: T225051
    Change-Id: I039641c16546db65eab3266353d5fff09e8d8ce1

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -23098,6 +23098,7 @@ $wgConf->settings = [
 ],
 'wmgWikibaseTmpPropertyTermsMigrationStage' => [
 	'default' => MIGRATION_OLD,
+	'testwikidatawiki' => MIGRATION_WRITE_BOTH, // T225055
 ],
 'wmgWikibaseTmpItemTermsMigrationStage' => [
 	'default' => MIGRATION_OLD
