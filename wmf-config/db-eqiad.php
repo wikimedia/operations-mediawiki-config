9084f3cad1f5df24979b7628b1a89907122aa4f5
Marostegui <marostegui@wikimedia.org>
2019-06-20 04:58:34 +0000

    Revert "db-eqiad.php: Depool db1077"
    This reverts commit 123746430fb1839d289a1383c426bf5032ed59d9.
    
    Change-Id: Idd8c673a57030ae3fd31c212a7a962d981083aa4

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -137,7 +137,7 @@ $wgLBFactoryConf = [
 	/* s3 */ 'DEFAULT' => [
 		'db1075' => 0,      # A2 3.6TB 512GB, master
 		'db1078' => 500,    # C3 3.6TB 512GB, candidate master
-		# 'db1077' => 400,    # B1 3.6TB 512GB, rc, log # master for sanitarium db1124 # T225981
+		'db1077' => 400,    # B1 3.6TB 512GB, rc, log # master for sanitarium db1124 # T225981
 		'db1123' => 300,    # D8 3.6TB 512GB, vslow, dump
 	],
 	's4' => [
@@ -346,19 +346,19 @@ $wgLBFactoryConf = [
 			'db1123' => 1,
 		],
 		'watchlist' => [
-			'db1123' => 1,
+			'db1077' => 1,
 		],
 		'recentchanges' => [
-			'db1123' => 1,
+			'db1077' => 1,
 		],
 		'recentchangeslinked' => [
-			'db1123' => 1,
+			'db1077' => 1,
 		],
 		'contributions' => [
-			'db1123' => 1,
+			'db1077' => 1,
 		],
 		'logpager' => [
-			'db1123' => 1,
+			'db1077' => 1,
 		],
 	],
 	's4' => [
