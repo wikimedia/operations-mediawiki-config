57cf6724cf7e7d43358734f603466696db21ff4d
Bartosz Dziewoński <matma.rex@gmail.com>
2019-06-20 19:33:16 +0000

    Revert "Centralize enwiki's VisualEditor feedback page"
    This reverts commit 436e781f99b9b62a128c127936d4ffe05e41116c.
    
    Bug: T224851
    Change-Id: Ib9fd2828549ea9345d7ece9803711e6470ca0423

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -16305,6 +16305,7 @@ $wgConf->settings = [
 	'default' => true,
 	'mediawikiwiki' => false,   // This is the consolidation point
 	'dewiki' => false,          // Retaining for local conversations
+	'enwiki' => false,          // Retaining for local conversations
 	'zh_yuewiki' => false,      // Retaining for local conversations
 	'betawikiversity' => false, // No local Flow install, so won't work
 	'dewikivoyage' => false,    // No local Flow install, so won't work
