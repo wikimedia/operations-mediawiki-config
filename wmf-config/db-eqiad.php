14a466c3ff56f4d4812aefb775804e1033e17160
Jcrespo <jcrespo@wikimedia.org>
2019-07-04 12:05:02 +0000

    Revert "mariadb: Depool db1109 for upgrade"
    This reverts commit f0041d78fc7871c5368e079065ad19f254a06979.
    
    Reason for revert: repool
    
    Change-Id: If8f9aa48f5c4eff00449ee91f226f6225cc01a92

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -180,12 +180,12 @@ $wgLBFactoryConf = [
 	],
 	's8' => [
 		'db1071' => 0,      # D1 2.8TB 160GB, master
-		'db1087' => 100,    # C2 3.6TB 512GB, vslow, dump # master for sanitarium db1124
+		'db1087' => 1,      # C2 3.6TB 512GB, vslow, dump # master for sanitarium db1124
 		'db1092' => 200,    # D2 3.6TB 512GB, api
 		'db1099:3318' => 100, # B2 3.6TB 512GB # rc, log: s1 and s8
 		'db1101:3318' => 100, # C2 3.6TB 512GB # rc, log: s7 and s8
 		'db1104' => 200,    # B3 3.6TB 512GB, api # candidate master
-		'db1109' => 300,    # D8 3.6TB 512GB
+		'db1109' => 500,    # D8 3.6TB 512GB
 		'db1126' => 500,    # A1 4.4TB 512GB
 	],
 
