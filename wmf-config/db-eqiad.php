532dfca5e5683c6714bf024450dffb00ae24d254
Jcrespo <jcrespo@wikimedia.org>
2019-07-05 07:27:45 +0000

    Revert "mariadb: Depool db1087 (s8 sanitarium master) for maintenance"
    This reverts commit 53a202dc9e2b89d8e9d0cac7eed4eed5d3d5b825.
    
    Reason for revert: server back in service.
    
    Also increase db1104 api load.
    
    Change-Id: Ife2d05933b612a6a9b5762ccf870cf8fc6cfa1af

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -180,8 +180,8 @@ $wgLBFactoryConf = [
 	],
 	's8' => [
 		'db1071' => 0,      # D1 2.8TB 160GB, master
-		# 'db1087' => 1,      # C2 3.6TB 512GB, vslow, dump # master for sanitarium db1124
-		'db1092' => 100,    # D2 3.6TB 512GB, api
+		'db1087' => 1,      # C2 3.6TB 512GB, vslow, dump # master for sanitarium db1124
+		'db1092' => 200,    # D2 3.6TB 512GB, api
 		'db1099:3318' => 100, # B2 3.6TB 512GB # rc, log: s1 and s8
 		'db1101:3318' => 100, # C2 3.6TB 512GB # rc, log: s7 and s8
 		'db1104' => 200,    # B3 3.6TB 512GB, api # candidate master
@@ -489,14 +489,14 @@ $wgLBFactoryConf = [
 	],
 	's8' => [
 		'vslow' => [
-			'db1092' => 1,
+			'db1087' => 1,
 		],
 		'dump' => [
-			'db1092' => 1,
+			'db1087' => 1,
 		],
 		'api' => [
-			'db1092' => 1,
-			'db1104' => 3,
+			'db1092' => 3,
+			'db1104' => 2,
 		],
 		'watchlist' => [
 			'db1099:3318' => 1,
