883528941ec6a8c195484e819c561c56cde3ebe2
Manuel Arostegui <marostegui@wikimedia.org>
2019-07-19 09:19:34 +0200

    db-eqiad.php: Pool db1109 into API
    db1109 will be serving API once db1104 is promoted to master.
    Start giving it some API traffic
    
    Bug: T227062
    Change-Id: I1ce0a37f69bfaced9b320d062d9d4a24e5fa0ef9

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -185,7 +185,7 @@ $wgLBFactoryConf = [
 		'db1099:3318' => 100, # B2 3.6TB 512GB # rc, log: s1 and s8
 		'db1101:3318' => 100, # C2 3.6TB 512GB # rc, log: s7 and s8
 		'db1104' => 200,    # B3 3.6TB 512GB, api # candidate master
-		'db1109' => 500,    # D8 3.6TB 512GB # future candidate master
+		'db1109' => 400,    # D8 3.6TB 512GB # future candidate master # api
 		'db1126' => 500,    # A1 4.4TB 512GB
 	],
 
@@ -497,6 +497,7 @@ $wgLBFactoryConf = [
 		'api' => [
 			'db1092' => 3,
 			'db1104' => 2,
+			'db1109' => 1,
 		],
 		'watchlist' => [
 			'db1099:3318' => 1,
