be886adf77e058eb79da858b0d43457a4ecae887
viztor <viztor@tools.wmflabs.org>
2019-08-04 15:28:33 +0800

    Add hd variations for zhwikiource project logo
    -o7 optimized
    
    bug: T229715
    Change-Id: Iaec2a2b8e1aff4062927a7b998e110d916913c15

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -1134,7 +1134,7 @@ $wgConf->settings = [
 	'vecwikisource' => '/static/images/project-logos/vecwikisource.png', // T132157
 	'viwikisource' => '/static/images/project-logos/viwikisource.png',
 	'yiwikisource' => '/static/images/project-logos/yiwikisource.png',
-	'zhwikisource' => '/static/images/project-logos/zhwikisource.png',
+	'zhwikisource' => '/static/images/project-logos/zhwikisource.png', //T229715
 	'zh_min_nanwikisource' => '/static/images/project-logos/zh_min_nanwikisource.png',
 
 	// Wikiversity
@@ -1696,6 +1696,7 @@ $wgConf->settings = [
 	'srwikisource' => [ '1.5x' => '/static/images/project-logos/srwikisource-1.5x.png', '2x' => '/static/images/project-logos/srwikisource-2x.png' ], // T172268
 	'viwikisource' => [ '1.5x' => '/static/images/project-logos/viwikisource-1.5x.png', '2x' => '/static/images/project-logos/viwikisource-2x.png' ],
 	'yiwikisource' => [ '1.5x' => '/static/images/project-logos/yiwikisource-1.5x.png', '2x' => '/static/images/project-logos/yiwikisource-2x.png' ], // T193562
+	'zhwikisource' => [ '1.5x' => '/static/images/project-logos/zhwikisource-1.5x.png', '2x' => '/static/images/project-logos/zhwikisource-2x.png' ], //T229715
 
 	# Wikiversity
 	'arwikiversity' => [ '1.5x' => '/static/images/project-logos/arwikiversity-1.5x.png', '2x' => '/static/images/project-logos/arwikiversity-2x.png' ],
