16798ab27ba1b1ca1dd20251759294a4c70c6394
Manuel Arostegui <marostegui@wikimedia.org>
2019-08-14 09:03:55 +0200

    db-codfw,db-eqiad.php: Remove db2063 from config
    db2063 will replace db2044 in m2 codfw
    
    Bug: T230459
    Change-Id: I1670a65e8af4edea0f20a4ebf7ad9ea0727899d6

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -605,7 +605,6 @@ $wgLBFactoryConf = [
 	'db2059' => '10.192.48.11', # do not remove or comment out
 	'db2060' => '10.192.48.12', # do not remove or comment out
 	'db2061' => '10.192.48.13', # do not remove or comment out
-	'db2063' => '10.192.48.15', # do not remove or comment out
 	'db2066' => '10.192.48.18', # do not remove or comment out
 	'db2067' => '10.192.48.19', # do not remove or comment out
 	'db2068' => '10.192.48.20', # do not remove or comment out
