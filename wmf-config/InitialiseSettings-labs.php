wikimedia/operations-mediawiki-config

commit d495d5e249499402f65da369c66677ec214f7f4b

beta: Remove use of SiteConfiguration->siteParamsCallback feature
This was only used by Beta and made it quite confusing to figure
out how these tags get computed. Basically, when MWConfigCacheGenerator
calls wgConfig->getAll(), the SiteConfiguration class looks if its
public 'siteParamsCallback' was overridden, and if so, call that
to get additional params/tags - merging them with the ones passed
to getAll() directly.

There's two ways to avoid this. One, is by forking getMWConfigForCacheing
into a prod and beta version and adding a conditional in CommonSettings.php
where we call it. Or to pass it in and handle explicitly here from
a single, testable, method.

Change-Id: I36a8cdac382a1fc02fe8a88ef7d0995b6f23c339

wmf-config/InitialiseSettings-labs.php
@@ -24,9 +24,11 @@
 #
 
 /**
- * Main entry point to override production settings. Supports key beginning with
- * a dash to completely override a setting.
- * Settings are fetched through wmfLabsSettings() defined below.
+ * Override $wgConf as needed for Beta Cluster.
+ *
+ * Must be called after InitialiseSettings.php has done the $wgConf->settings
+ * assignment for production, but before CommonSettings.php extracs it
+ * using MWConfigCacheGenerator.
  *
  * @return array
  */
@@ -35,26 +37,7 @@ function wmfLabsOverrideSettings() {
 
 	// Override (or add) settings that we need within the labs environment,
 	// but not in production.
-	$betaSettings = wmfGetLabsOverrideSettings();
-
-	$wgConf->siteParamsCallback = function ( $conf, $wiki ) {
-		$wikiTags = [];
-
-		$betaDblistTags = [ 'flow-labs' ];
-		foreach ( $betaDblistTags as $tag ) {
-			$dblist = MWWikiversions::readDbListFile( $tag );
-			if ( in_array( $wiki, $dblist ) ) {
-				$wikiTags[] = $tag;
-			}
-		}
-
-		return [
-			'params' => [],
-			'tags' => $wikiTags
-		];
-	};
-
-	foreach ( $betaSettings as $key => $value ) {
+	foreach ( wmfGetLabsOverrideSettings() as $key => $value ) {
 		if ( substr( $key, 0, 1 ) == '-' ) {
 			// Settings prefixed with - are completely overriden
 			$wgConf->settings[substr( $key, 1 )] = $value;

multiversion/MWConfigCacheGenerator.php
@@ -49,6 +49,10 @@ class MWConfigCacheGenerator {
 		'cirrussearch-big-indices',
 	];
 
+	public static $labsDbLists = [
+		'flow-labs',
+	];
+
 	/**
 	 * Create a MultiVersion config object for a wiki
 	 *
@@ -56,13 +60,18 @@ class MWConfigCacheGenerator {
 	 * @param object $site The wiki's site type family, e.g. 'wikipedia' or 'wikisource'
 	 * @param object $lang The wiki's MediaWiki language code, e.g. 'en' or 'zh-min-nan'
 	 * @param object $wgConf The global MultiVersion wgConf object
+	 * @param string $realm Realm, e.g. 'production' or 'labs'
 	 * @return object The wiki's config object
 	 */
-	public static function getMWConfigForCacheing( $wikiDBname, $site, $lang, $wgConf ) {
+	public static function getMWConfigForCacheing( $wikiDBname, $site, $lang, $wgConf, $realm = 'production' ) {
 		# Collect all the dblist tags associated with this wiki
 		$wikiTags = [];
 
-		foreach ( self::$dbLists as $tag ) {
+		$dbLists = self::$dbLists;
+		if ( $realm === 'labs' ) {
+			$dbLists = array_merge( $dbLists, self::$labsDbLists );
+		}
+		foreach ( $dbLists as $tag ) {
 			$dblist = MWWikiversions::readDbListFile( $tag );
 			if ( in_array( $wikiDBname, $dblist ) ) {
 				$wikiTags[] = $tag;

wmf-config/CommonSettings.php
@@ -212,7 +212,7 @@ if ( !$globals ) {
 	require "$wmfConfigDir/InitialiseSettings.php";
 
 	$globals = Wikimedia\MWConfig\MWConfigCacheGenerator::getMWConfigForCacheing(
-		$wgDBname, $site, $lang, $wgConf
+		$wgDBname, $site, $lang, $wgConf, $wmfRealm
 	);
 
 	$confCacheObject = [ 'mtime' => $confActualMtime, 'globals' => $globals ];

commit 7e4fa1d30521c217af6ae54718edbe8a5834cdf3

beta: Unbreak config vars that used '$variant' placeholder
This isn't a real PHP variable, it was substituted by wgConf
until I broke it in d495d5e249499.

Fix by using a literal instead. We can add this back in in the
future, if needed, to vary all of '.beta.wmflabs.org' like we
do for some service and puppet configs already.

Change-Id: I5e0e2a509ff31835da87949f25643affd089e3a3

wmf-config/InitialiseSettings-labs.php
@@ -77,50 +77,50 @@ function wmfGetLabsOverrideSettings() {
 		],
 
 		'-wgServer' => [
-			'wiktionary'	=> 'https://$lang.wiktionary.$variant.wmflabs.org',
-			'wikipedia'     => 'https://$lang.wikipedia.$variant.wmflabs.org',
-			'wikiversity'	=> 'https://$lang.wikiversity.$variant.wmflabs.org',
-			'wikisource'	=> 'https://$lang.wikisource.$variant.wmflabs.org',
-			'wikiquote'	=> 'https://$lang.wikiquote.$variant.wmflabs.org',
-			'wikinews'	=> 'https://$lang.wikinews.$variant.wmflabs.org',
-			'wikibooks'     => 'https://$lang.wikibooks.$variant.wmflabs.org',
-			'wikivoyage'    => 'https://$lang.wikivoyage.$variant.wmflabs.org',
+			'wiktionary'	=> 'https://$lang.wiktionary.beta.wmflabs.org',
+			'wikipedia'     => 'https://$lang.wikipedia.beta.wmflabs.org',
+			'wikiversity'	=> 'https://$lang.wikiversity.beta.wmflabs.org',
+			'wikisource'	=> 'https://$lang.wikisource.beta.wmflabs.org',
+			'wikiquote'	=> 'https://$lang.wikiquote.beta.wmflabs.org',
+			'wikinews'	=> 'https://$lang.wikinews.beta.wmflabs.org',
+			'wikibooks'     => 'https://$lang.wikibooks.beta.wmflabs.org',
+			'wikivoyage'    => 'https://$lang.wikivoyage.beta.wmflabs.org',
 
-			'commonswiki'   => 'https://commons.wikimedia.$variant.wmflabs.org',
-			'deploymentwiki'      => 'https://deployment.wikimedia.$variant.wmflabs.org',
-			'loginwiki'     => 'https://login.wikimedia.$variant.wmflabs.org',
-			'metawiki'      => 'https://meta.wikimedia.$variant.wmflabs.org',
-			'testwiki'      => 'https://test.wikimedia.$variant.wmflabs.org',
-			'wikidatawiki'  => 'https://wikidata.$variant.wmflabs.org',
-			'en_rtlwiki' => 'https://en-rtl.wikipedia.$variant.wmflabs.org',
+			'commonswiki'   => 'https://commons.wikimedia.beta.wmflabs.org',
+			'deploymentwiki'      => 'https://deployment.wikimedia.beta.wmflabs.org',
+			'loginwiki'     => 'https://login.wikimedia.beta.wmflabs.org',
+			'metawiki'      => 'https://meta.wikimedia.beta.wmflabs.org',
+			'testwiki'      => 'https://test.wikimedia.beta.wmflabs.org',
+			'wikidatawiki'  => 'https://wikidata.beta.wmflabs.org',
+			'en_rtlwiki' => 'https://en-rtl.wikipedia.beta.wmflabs.org',
 		],
 
 		'-wgCanonicalServer' => [
-			'wikipedia'     => 'https://$lang.wikipedia.$variant.wmflabs.org',
-			'wikibooks'     => 'https://$lang.wikibooks.$variant.wmflabs.org',
-			'wikiquote'	=> 'https://$lang.wikiquote.$variant.wmflabs.org',
-			'wikinews'	=> 'https://$lang.wikinews.$variant.wmflabs.org',
-			'wikisource'	=> 'https://$lang.wikisource.$variant.wmflabs.org',
-			'wikiversity'     => 'https://$lang.wikiversity.$variant.wmflabs.org',
-			'wiktionary'     => 'https://$lang.wiktionary.$variant.wmflabs.org',
-			'wikivoyage'    => 'https://$lang.wikivoyage.$variant.wmflabs.org',
+			'wikipedia'     => 'https://$lang.wikipedia.beta.wmflabs.org',
+			'wikibooks'     => 'https://$lang.wikibooks.beta.wmflabs.org',
+			'wikiquote'	=> 'https://$lang.wikiquote.beta.wmflabs.org',
+			'wikinews'	=> 'https://$lang.wikinews.beta.wmflabs.org',
+			'wikisource'	=> 'https://$lang.wikisource.beta.wmflabs.org',
+			'wikiversity'     => 'https://$lang.wikiversity.beta.wmflabs.org',
+			'wiktionary'     => 'https://$lang.wiktionary.beta.wmflabs.org',
+			'wikivoyage'    => 'https://$lang.wikivoyage.beta.wmflabs.org',
 
-			'metawiki'      => 'https://meta.wikimedia.$variant.wmflabs.org',
-			'commonswiki'	=> 'https://commons.wikimedia.$variant.wmflabs.org',
-			'deploymentwiki'      => 'https://deployment.wikimedia.$variant.wmflabs.org',
-			'loginwiki'     => 'https://login.wikimedia.$variant.wmflabs.org',
-			'testwiki'      => 'https://test.wikimedia.$variant.wmflabs.org',
-			'wikidatawiki'  => 'https://wikidata.$variant.wmflabs.org',
-			'en_rtlwiki' => 'https://en-rtl.wikipedia.$variant.wmflabs.org',
+			'metawiki'      => 'https://meta.wikimedia.beta.wmflabs.org',
+			'commonswiki'	=> 'https://commons.wikimedia.beta.wmflabs.org',
+			'deploymentwiki'      => 'https://deployment.wikimedia.beta.wmflabs.org',
+			'loginwiki'     => 'https://login.wikimedia.beta.wmflabs.org',
+			'testwiki'      => 'https://test.wikimedia.beta.wmflabs.org',
+			'wikidatawiki'  => 'https://wikidata.beta.wmflabs.org',
+			'en_rtlwiki' => 'https://en-rtl.wikipedia.beta.wmflabs.org',
 		],
 
 		'-wgUploadPath' => [
-			'default' => 'https://upload.$variant.wmflabs.org/$site/$lang',
+			'default' => 'https://upload.beta.wmflabs.org/$site/$lang',
 			'private' => '/w/img_auth.php',
 		// 'wikimania2005wiki' => 'http://upload..org/wikipedia/wikimania', // back compat
-			'commonswiki' => 'https://upload.$variant.wmflabs.org/wikipedia/commons',
-			'metawiki' => 'https://upload.$variant.wmflabs.org/wikipedia/meta',
-			'testwiki' => 'https://upload.$variant.wmflabs.org/wikipedia/test',
+			'commonswiki' => 'https://upload.beta.wmflabs.org/wikipedia/commons',
+			'metawiki' => 'https://upload.beta.wmflabs.org/wikipedia/meta',
+			'testwiki' => 'https://upload.beta.wmflabs.org/wikipedia/test',
 		],
 
 		'-wgThumbnailBuckets' => [
@@ -132,7 +132,7 @@ function wmfGetLabsOverrideSettings() {
 		],
 
 		'-wmgMathPath' => [
-			'default' => 'https://upload.$variant.wmflabs.org/math',
+			'default' => 'https://upload.beta.wmflabs.org/math',
 		],
 
 		'-wmgDefaultMonologHandler' => [
@@ -270,7 +270,7 @@ function wmfGetLabsOverrideSettings() {
 			]
 		],
 		'wgMFPhotoUploadEndpoint' => [
-			'default' => 'https://commons.wikimedia.$variant.wmflabs.org/w/api.php',
+			'default' => 'https://commons.wikimedia.beta.wmflabs.org/w/api.php',
 		],
 		'wgMFSpecialCaseMainPage' => [
 			// We don't want to support this