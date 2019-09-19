ead70240892e99fd8dfd71596a7f04b2d595f2b2
Timo Tijhof <krinklemail@gmail.com>
2019-09-19 21:29:12 +0100

    Uninstall dev deps from production vendor/
    Effectively undoes b88a57c4ce9c62, and re-applies 17a7f7fc30abf.
    
    Change b88a57c4ce9c62 was merged on 12 Sept. Based on SAL, it was
    deployed on 17 Sept by an unrelated scap for 1.34.0-wmf.23.
    
    These are not needed in production MediaWiki, and have not yet been audited
    for security, and in general impact on it here (wrt repo size).
    
    There is also the potential for version clashes with MW deps.
    This is important because we currently run zero tests in CI on
    MediaWiki with wmf-config installed.
    
    At least one library (composer/semver) appears to clash with
    MediaWiki currently, which means its version was now determined by
    wmf-config and the MediaWiki one shadowed/ignored. We used to have
    a similar issue with wikimedia/cdb in the past.
    
    Including dev deps in prod can certainly be considered, but
    should imho follow a wider discussion about how, why and possibly
    other solutions to the problem(s) at hand.
    
    Change-Id: I9e47d30b528f4233c5aaa58a747b7db5503416ce

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,19 +6,4 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'phpDocumentor\\Reflection\\' => array($vendorDir . '/phpdocumentor/reflection-common/src', $vendorDir . '/phpdocumentor/reflection-docblock/src', $vendorDir . '/phpdocumentor/type-resolver/src'),
-    'Webmozart\\Assert\\' => array($vendorDir . '/webmozart/assert/src'),
-    'Symfony\\Polyfill\\Php73\\' => array($vendorDir . '/symfony/polyfill-php73'),
-    'Symfony\\Polyfill\\Mbstring\\' => array($vendorDir . '/symfony/polyfill-mbstring'),
-    'Symfony\\Polyfill\\Ctype\\' => array($vendorDir . '/symfony/polyfill-ctype'),
-    'Symfony\\Contracts\\Service\\' => array($vendorDir . '/symfony/service-contracts'),
-    'Symfony\\Component\\Console\\' => array($vendorDir . '/symfony/console'),
-    'Psr\\Container\\' => array($vendorDir . '/psr/container/src'),
-    'Prophecy\\' => array($vendorDir . '/phpspec/prophecy/src/Prophecy'),
-    'MediaWiki\\MinusX\\' => array($vendorDir . '/mediawiki/minus-x/src'),
-    'MediaWiki\\' => array($vendorDir . '/mediawiki/mediawiki-codesniffer/MediaWiki'),
-    'Doctrine\\Instantiator\\' => array($vendorDir . '/doctrine/instantiator/src/Doctrine/Instantiator'),
-    'DeepCopy\\' => array($vendorDir . '/myclabs/deep-copy/src/DeepCopy'),
-    'Composer\\Spdx\\' => array($vendorDir . '/composer/spdx-licenses/src'),
-    'Composer\\Semver\\' => array($vendorDir . '/composer/semver/src'),
 );
