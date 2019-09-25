8405bf1c2709744fee521ca1a8bd4893f04afed6
Timo Tijhof <krinklemail@gmail.com>
2019-09-25 03:55:06 +0100

    noc: Make db.php easier to test with a mock fixture in-repo
    Also document how to run it. (I've previously passed this on
    through IM and e-mails, but looks like it never made it into
    a versioned file or wikipage, so hereby documenting).
    
    TMS is the nearest airport to the GPS coordinates 0,-0.
    
    Change-Id: I48a80cd9a9cff4b6cee0c699e4fb406e3cb41b62

--- docroot/noc/db.php
+++ docroot/noc/db.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * This file generates the Database info page at <https://noc.wikimedia.org/db.php>
+ *
+ * In production, noc.wikimedia.org is served from mwwmaint* hosts.
+ *
+ * To test the script locally, run:
+ *
+ *     cd docroot/noc$ php -S localhost:9412
+ *
+ * Then view <http://localhost:9412/db.php>.
+ */
 
 $format = isset( $_GET['format'] ) && $_GET['format'] === 'json' ? 'json' : 'html';
 
@@ -11,20 +22,38 @@ if ( $format === 'json' ) {
 }
 
 // Default to eqiad but allow limited other DCs to be specified with ?dc=foo.
+$dbConfigEtcdPrefix = '/srv/dbconfig';
 $allowedDCs = [
 	'codfw' => 'db-codfw.php',
 	'eqiad' => 'db-eqiad.php',
 ];
-$dbConfigFileName = ( isset( $_GET['dc'] ) && isset( $allowedDCs[$_GET['dc']] ) )
-	? $allowedDCs[$_GET['dc']]
-	: $allowedDCs['eqiad'];
 $dbctlJsonByDC = [
 	'codfw' => 'codfw.json',
 	'eqiad' => 'eqiad.json',
 ];
+$defaultDC = 'eqiad';
+
+if ( !is_dir( $dbConfigEtcdPrefix ) ) {
+	// Local testing and debugging fallback
+	$dbConfigEtcdPrefix = __DIR__ . '/../../tests/data/dbconfig';
+	$allowedDCs = [
+		'tmsx' => 'db-eqiad.php',
+		'tmsy' => 'db-codfw.php',
+	];
+	$dbctlJsonByDC = [
+		'tmsx' => 'tmsx.json',
+		'tmsy' => 'tmsy.json',
+	];
+	$defaultDC = 'tmsx';
+}
+
+$dbConfigFileName = ( isset( $_GET['dc'] ) && isset( $allowedDCs[$_GET['dc']] ) )
+	? $allowedDCs[$_GET['dc']]
+	: $allowedDCs[$defaultDC];
+
 $dbConfigEtcdJsonFilename = ( isset( $_GET['dc'] ) && isset( $dbctlJsonByDC[$_GET['dc']] ) )
 	? $dbctlJsonByDC[$_GET['dc']]
-	: $dbctlJsonByDC['eqiad'];
+	: $dbctlJsonByDC[$defaultDC];
 
 // Mock vars needed by db-*.php (normally set by CommonSettings.php)
 $wgDBname = null;
@@ -38,14 +67,12 @@ require_once __DIR__ . '/../../tests/Defines.php';
 
 // Load the actual db vars
 require_once __DIR__ . "/../../wmf-config/{$dbConfigFileName}";
-// Now load dbctl JSON from disk and merge it in, mimicking what etcd.php's wmfEtcdApplyDBConfig()
-// does in production.
-// On mwmaint hosts, the JSON files are produced by a 'fetch_dbconfig' script & systemd timer defined in puppet.
-// For local testing, you can fetch JSON files from https://noc.wikimedia.org/dbconfig/eqiad.json
-// and then set WMF_NOC_ETCD_JSON_PATH to the directory containing the downloaded eqiad.json.
-$dbConfigEtcdPrefix = getenv( 'WMF_NOC_ETCD_JSON_PATH' ) !== false
-	? getenv( 'WMF_NOC_ETCD_JSON_PATH' )
-	: '/srv/dbconfig';
+
+// Now load the JSON written to Etcd by dbctl, from the local disk and merge it in.
+// This is mimicking what wmfEtcdApplyDBConfig (wmf-config/etcd.php) does in prod.
+//
+// On mwmaint hosts, these JSON files are produced by a 'fetch_dbconfig' script,
+// run via systemd timer, defined in puppet.
 $wmfDbconfigFromEtcd = json_decode( file_get_contents( "$dbConfigEtcdPrefix/$dbConfigEtcdJsonFilename" ), true );
 global $wgLBFactoryConf;
 $wgLBFactoryConf['readOnlyBySection'] = $wmfDbconfigFromEtcd['readOnlyBySection'];
