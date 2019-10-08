fb4940439376be8f41b6587009996480c8b4f9f3
Jon Harald Søby <jhsoby@gmail.com>
2019-10-08 10:25:04 +0200

    Enable more transwiki import sources for hiwikisource
    Bug: T234892
    Change-Id: I8bb29e242c9c55e53cbb3d112a8d6871d86cea7e

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -12396,6 +12396,7 @@ function wmfGetVariantSettings() {
 	'hiwiki' => [ 'wikt', 'commons', 'meta', 'species', 'de', 'en', 'en:b', 'en:q', 'en:n', 'en:s', 'en:v', 'es', 'fr', 'ml', 'ne', 'nl', 'ro', 'ru', 'pl', 'pt', 'simple', 'ta' ], // T43757
 	'hiwikiversity' => [ 'en:w', 'betawikiversity', 'en', 'de:v', 'hi:w', 'hi:b' ], // T168765
 	'hiwikiquote' => [ 'w', 'w:en', 'w:sa', 'q:en', 'q:gu', 'q:sa', 'meta', 'mw' ], // T127593
+	'hiwikisource' => [ 'commons', 'meta', 'oldwikisource', 'en', 'w' ], // T234892
 	'hiwiktionary' => [ 'commons', 'meta', 'w', 'q', 'b', 'species', 'w:en', 'b:en', 'q:en', 's:en', 'v:en', 'voy:en', 'w:es', 'w:fa', 'w:fr', 'w:ml', 'w:ne', 'w:nl', 'w:ro', 'w:ru', 'w:pl', 'w:pt', 'w:simple', 'w:ta', 'w:ur', 'w:mr', 'w:gu', 'w:bh', 'en', 'mg', 'fr', 'es', 'zh', 'ru', 'it', 'pl', 'sv', 'ku', 'el', 'nl', 'de', 'ko', 'it', 'tr', 'ta', 'hu', 'fi', 'kn', 'ja', 'ml', 'my', 'or', 'te', 'fa', 'gu', 'ar', 'th', 'ps', 'simple', 'sw' ], // T132417
 	'hifwiki' => [ 'incubator', 'commons', 'meta', 'species', 'bh', 'de', 'en', 'en:b', 'en:q', 'en:n', 'en:s', 'en:v', 'el', 'es', 'fa', 'fr', 'hi', 'ml', 'ne', 'nl', 'ro', 'ru', 'pa', 'pl', 'pt', 'simple', 'te', 'ta', 'ur' ], // T99826
 	'hrwikibooks' => [ 'w' ], // T176320
