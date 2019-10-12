ec77b1b515940c73a5b839151825a7287ed159df
Timo Tijhof <krinklemail@gmail.com>
2019-10-12 03:18:07 +0100

    Remove unused wmgReduceStartupExpiry logic in CommonSettings.php
    This is always false.
    
    Bug: T235314
    Change-Id: Ibfb38ef4f97ac0696e80fd0e39a9c8ce09e2bc79

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -377,10 +377,6 @@ $wgIncludeLegacyJavaScript = true;
 
 $wgResourceLoaderMaxQueryLength = 5000;
 
-if ( $wmgReduceStartupExpiry ) {
-	$wgResourceLoaderMaxage['unversioned'] = [ 'server' => 30, 'client' => 30 ];
-}
-
 // Cache version key for ResourceLoader client-side module store
 // - Bumped to fix breakage due to old /static/$branchName/ urls still
 // being cached after the switch to /w/static.php (T134368).
