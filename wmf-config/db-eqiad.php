c505c5d9a98d67ac5297a2497785a020d31c5c14
Jaime Crespo <jcrespo@wikimedia.org>
2019-11-06 15:24:42 +0100

    mariadb: Repool es1019 with low weight after maintenance
    Maintenanance done, but pooling it slowly.
    
    Change-Id: I94b975cc094e9d3eae65d831b93a3b5d4128016b

--- wmf-config/db-eqiad.php
+++ wmf-config/db-eqiad.php
@@ -355,9 +355,9 @@ $wgLBFactoryConf = [
 	],
 	# es3
 	'cluster25' => [
-		'10.64.32.65'  => 1, # es1017, C3 11TB 128GB, master
-		'10.64.16.187' => 1, # es1014, B1 11TB 128GB
-		# '10.64.48.116' => 1, # es1019, D8 11TB 128GB # depooled for maintenance
+		'10.64.32.65'  => 10, # es1017, C3 11TB 128GB, master
+		'10.64.16.187' => 10, # es1014, B1 11TB 128GB
+		'10.64.48.116' => 1, # es1019, D8 11TB 128GB # depooled for maintenance
 	],
 	# ExtensionStore shard1
 	'extension1' => [
