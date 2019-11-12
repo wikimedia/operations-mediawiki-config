711c9ce3e66e6756375a2019bc4d2eda10cad023
Brad Jorsch <bjorsch@wikimedia.org>
2019-11-12 14:34:48 -0500

    Set MCR migration stage to NEW on group 0
    Assuming no bugs are found, we'll probably do group 1 later this week
    and group 2 early next.
    
    Bug: T198312
    Change-Id: I7435bccb6d2f1cd5d3ffcd8dc55559a5d01f536a

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -22920,7 +22920,7 @@ function wmfGetVariantSettings() {
 
 'wgMultiContentRevisionSchemaMigrationStage' => [
 	'default' => SCHEMA_COMPAT_WRITE_BOTH | SCHEMA_COMPAT_READ_NEW,
-	'testwiki' => SCHEMA_COMPAT_NEW,
+	'group0' => SCHEMA_COMPAT_NEW,
 ],
 'wmgUseReadingLists' => [
 	'default' => true,
