7b44b095d18f87ebfcd32a6dfcd5c69cafaabad6
James D. Forrester <jforrester@wikimedia.org>
2019-12-05 12:00:41 -0800

    Variant configuration: Pre-calculate config for each wiki on demand
    These files can now be generated, but are not used by scap or hooked
    up to any part of production yet. The next step is to get CI to show
    the diff of a given change to present to users the impact of changes
    they have proposed.
    
    Bug: T223602
    Change-Id: Idb51790690fb8272b3b36d7b507db1e16f9f2c8d

---
+++ multiversion/buildConfigCache.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once 'multiversion/MWConfigCacheGenerator.php';
+require_once 'multiversion/MWWikiversions.php';
+
+// TODO: Better alternative than this?
+require_once __DIR__ . '../../vendor/autoload.php';
+require_once __DIR__ . "../../tests/Defines.php";
+
+$wmfConfigDir = __DIR__ . '/../wmf-config';
+
+global $wmfRealm, $wmfDatacenter;
+
+$wmfRealm = 'production';
+$wmfDatacenter = 'eqiad';
+
+require_once "{$wmfConfigDir}/InitialiseSettings.php";
+$settings['production'] = wmfGetVariantSettings();
+
+require_once "{$wmfConfigDir}/InitialiseSettings-labs.php";
+$settings['labs'] = wmfApplyLabsOverrideSettings( $settings['production'] );
+
+foreach ( [ 'production', 'labs' ] as $realm ) {
+	$config = $settings[$realm];
+
+	$wikiversionsFile = ( $realm === 'labs' ) ? 'wikiversions-labs.json' : 'wikiversions.json';
+
+	$wikiversions = MWWikiversions::readWikiVersionsFile( $wikiversionsFile );
+
+	foreach ( $wikiversions as $wgDBname => $wmgVersionNumber ) {
+
+		$megaConfig[$realm][$wgDBname] = Wikimedia\MWConfig\MWConfigCacheGenerator::getCachableMWConfig(
+			$wgDBname, $config, $realm
+		);
+
+		Wikimedia\MWConfig\MWConfigCacheGenerator::writeToStaticCache(
+			$wmfConfigDir . "/config-cache",
+			"conf-$realm-$wgDBname.json",
+			$megaConfig[$realm][$wgDBname]
+		);
+	}
+
+}
