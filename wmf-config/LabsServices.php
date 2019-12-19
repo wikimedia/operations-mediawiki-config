79d93db2577b77c032cfdc43d37302e7fcb325a7
Eric Evans <eevans@wikimedia.org>
2019-12-18 19:31:57 -0600

    deployment-prep: Create new test instances of Kask
    Moves to two new Kask beta VMs (one for sessions, the other for
    echoseen timestamps), appropriately named and running Stretch.
    
    Bug: T218609
    Change-Id: I4adbd19cd60e38962a4580c2b20167a0dc4cb959

--- wmf-config/LabsServices.php
+++ wmf-config/LabsServices.php
@@ -67,7 +67,7 @@ return [
 		],
 
 		### Kask
-		'sessionstore' => 'https://deployment-sessionstore01.deployment-prep.eqiad.wmflabs:8080',
-		'echostore' => 'https://deployment-sessionstore02.deployment-prep.eqiad.wmflabs:8080',
+		'sessionstore' => 'https://deployment-sessionstore03.deployment-prep.eqiad.wmflabs:8080',
+		'echostore' => 'https://deployment-echostore01.deployment-prep.eqiad.wmflabs:8080',
 	],
 ];
