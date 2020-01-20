a969bc69096486c2a22f2e088fd1070e0eb18687
Cormac Parle <cparle@wikimedia.org>
2020-01-20 10:20:44 +0000

    Remove handler deleted from the MachineVision extension on beta
    DO NOT MERGE until https://gerrit.wikimedia.org/r/559345 has been merged,
    and revert when it is deployed
    
    Bug: T241242
    Change-Id: Ic8d52f5bb8dcfa6e6f368e67ccdc1bf0790c5766

--- wmf-config/InitialiseSettings-labs.php
+++ wmf-config/InitialiseSettings-labs.php
@@ -1649,6 +1649,20 @@ function wmfGetLabsOverrideSettings() {
 		'wgMachineVisionRequestLabelsFromWikidataPublicApi' => [
 			'default' => true,
 		],
+		// @todo remove as soon as https://gerrit.wikimedia.org/r/559345 is deployed
+		'wgMachineVisionHandlers' => [
+			'default' => [
+				'google' => [
+					'class' => 'MediaWiki\\Extension\\MachineVision\\Handler\\GoogleCloudVisionHandler',
+					'services' => [
+						'MachineVisionFetchGoogleCloudVisionAnnotationsJobFactory',
+						'MachineVisionRepository',
+						'MachineVisionRepoGroup',
+						'MachineVisionLabelResolver',
+					],
+				]
+			],
+		],
 		'-wgSpecialSearchFormOptions' => [
 			'wikidatawiki' => [ 'showDescriptions' => true ],
 		],
