5d364a0009ca6fa368544a0a69ca847540f68bd4
Ladsgroup <Ladsgroup@gmail.com>
2020-02-24 15:53:38 +0000

    Revert "Increase the reads for term store for clients for up to Q256K"
    This reverts commit c98766a92c03821d4dc5c5426fa1e29041edfd1a.
    
    Reason for revert: Caused latencies in s8 
    
    Change-Id: I1af3a351ca24e74571ca1e0748500ce61f88e08d

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -24161,7 +24161,7 @@ function wmfGetVariantSettings() {
 ],
 'wmgWikibaseTmpItemTermsMigrationStage' => [
 	'default' => [
-		256000 => MIGRATION_WRITE_NEW, // Reads from new store, still writes to both
+		120000 => MIGRATION_WRITE_NEW, // Reads from new store, still writes to both
 		'max' => MIGRATION_OLD, // Reads from old store, only writes to old store
 	],
 	'testwikidatawiki' => [
