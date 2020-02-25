bb0b4006afda453fcbb470f604155f8a72e26a8b
Amir Sarabadani <Ladsgroup@gmail.com>
2020-02-25 17:54:59 +0100

    Increase the reads for term store for clients for up to Q6Mio
    Bug: T219123
    Change-Id: I37cd8198d9019af4db8aef40a25989fdc49e1bf7

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -24169,7 +24169,7 @@ function wmfGetVariantSettings() {
 ],
 'wmgWikibaseTmpItemTermsMigrationStage' => [
 	'default' => [
-		4000000 => MIGRATION_WRITE_NEW, // Reads from new store, still writes to both
+		6000000 => MIGRATION_WRITE_NEW, // Reads from new store, still writes to both
 		'max' => MIGRATION_OLD, // Reads from old store, only writes to old store
 	],
 	'testwikidatawiki' => [
