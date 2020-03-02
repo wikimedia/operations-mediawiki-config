6169ab749bc800bc1e5e8078434dabebcdeb4f92
addshore <addshorewiki@gmail.com>
2020-03-02 14:19:01 +0000

    Read from the new term store up to Q12 mill everywhere
    With pre warmed caches on db1126 & db1111
    
    Bug: T219123
    Change-Id: I2733f57ac80d0939fdcb96178e7e9aa9dcbb91c4

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -27548,14 +27548,14 @@ function wmfGetVariantSettings() {
 ],
 'wmgWikibaseTmpItemTermsMigrationStage' => [
 	'default' => [
-		10000000 => MIGRATION_WRITE_NEW, // Reads from new store, still writes to both
+		12000000 => MIGRATION_WRITE_NEW, // Reads from new store, still writes to both
 		'max' => MIGRATION_OLD, // Reads from old store, only writes to old store
 	],
 	'testwikidatawiki' => [
 		'max' => MIGRATION_WRITE_NEW, // Reads from new store, still writes to both
 	],
 	'wikidatawiki' => [
-		10000000 => MIGRATION_WRITE_NEW, // Reads from new store, still writes to both
+		12000000 => MIGRATION_WRITE_NEW, // Reads from new store, still writes to both
 		84000000 => MIGRATION_WRITE_BOTH, // Reads from old store, still writes to both
 		'max' => MIGRATION_OLD, // Reads from old store, only writes to old store
 	],
