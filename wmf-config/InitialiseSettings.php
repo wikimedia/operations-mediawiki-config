d80965a5137021703a9c5bd98815ee8efefe4427
Ladsgroup <Ladsgroup@gmail.com>
2020-03-12 19:36:54 +0000

    Revert "Set term store to WRITE_BOTH for all of Wikidata"
    This reverts commit ea0cb89215dbcab69ca97b183c2850f2a120c841.
    
    Reason for revert: Causing issues.
    
    Change-Id: Ifc594d1641fed454d704c44a991669be6b1fce06

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -27749,7 +27749,8 @@ function wmfGetVariantSettings() {
 	],
 	'wikidatawiki' => [
 		30000000 => MIGRATION_WRITE_NEW, // Reads from new store, still writes to both
-		'max' => MIGRATION_WRITE_BOTH, // Reads from old store, still writes to both
+		87500000 => MIGRATION_WRITE_BOTH, // Reads from old store, still writes to both
+		'max' => MIGRATION_OLD, // Reads from old store, only writes to old store
 	],
 ],
 
