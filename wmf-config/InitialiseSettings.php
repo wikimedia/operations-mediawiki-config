2b00e48bdd44abec74c38a1279a1eafed4495332
Amir Sarabadani <Ladsgroup@gmail.com>
2020-03-17 20:11:45 +0100

    Read from the new term store everywhere
    Bug: T219123
    Change-Id: Ic83f17db1ddce9bd4638f525a77ce1e6ec57288f

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -27745,15 +27745,13 @@ function wmfGetVariantSettings() {
 ],
 'wmgWikibaseTmpItemTermsMigrationStage' => [
 	'default' => [
-		80000000 => MIGRATION_WRITE_NEW, // Reads from new store, still writes to both
-		'max' => MIGRATION_OLD, // Reads from old store, only writes to old store
+		'max' => MIGRATION_WRITE_NEW, // Reads from new store, still writes to both
 	],
 	'testwikidatawiki' => [
 		'max' => MIGRATION_WRITE_NEW, // Reads from new store, still writes to both
 	],
 	'wikidatawiki' => [
-		80000000 => MIGRATION_WRITE_NEW, // Reads from new store, still writes to both
-		'max' => MIGRATION_WRITE_BOTH, // Reads from old store, still writes to both
+		'max' => MIGRATION_WRITE_NEW, // Reads from new store, still writes to both
 	],
 ],
 
