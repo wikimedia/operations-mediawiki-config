3a9ee5847460b23c2247625ec7a5be08bfdf82b9
James D. Forrester <jforrester@wikimedia.org>
2020-04-06 10:14:59 -0700

    mobile: Stop reading wmgMobileFrontend and wmgMinervaNeue, always true
    Change-Id: Ie816f46699ccd8c97873333bfd772aa98e585cee

--- wmf-config/mobile.php
+++ wmf-config/mobile.php
@@ -1,57 +1,51 @@
 <?php
 # WARNING: This file is publicly viewable on the web. Do not put private data here.
 
-if ( $wmgMobileFrontend ) {
-	wfLoadExtension( 'MobileFrontend' );
+wfLoadExtension( 'MobileFrontend' );
+wfLoadSkin( 'MinervaNeue' );
 
-	// Load skin
-	if ( $wmgMinervaNeue ) {
-		wfLoadSkin( 'MinervaNeue' );
+$wgMFMobileHeader = 'X-Subdomain';
+if ( !$wmgEnableGeoData ) {
+	$wgMFNearby = false;
+}
+
+$wgHooks['EnterMobileMode'][] = function () {
+	global $wgCentralAuthCookieDomain, $wgHooks, $wgIncludeLegacyJavaScript;
+
+	// Disable loading of legacy wikibits in the mobile web experience
+	$wgIncludeLegacyJavaScript = false;
+
+	// Hack for T49647
+	if ( $wgCentralAuthCookieDomain == 'commons.wikimedia.org' ) {
+		$wgCentralAuthCookieDomain = 'commons.m.wikimedia.org';
+	} elseif ( $wgCentralAuthCookieDomain == 'meta.wikimedia.org' ) {
+		$wgCentralAuthCookieDomain = 'meta.m.wikimedia.org';
 	}
 
-	$wgMFMobileHeader = 'X-Subdomain';
-	if ( !$wmgEnableGeoData ) {
-		$wgMFNearby = false;
-	}
-
-	$wgHooks['EnterMobileMode'][] = function () {
-		global $wgCentralAuthCookieDomain, $wgHooks, $wgIncludeLegacyJavaScript;
-
-		// Disable loading of legacy wikibits in the mobile web experience
-		$wgIncludeLegacyJavaScript = false;
-
-		// Hack for T49647
-		if ( $wgCentralAuthCookieDomain == 'commons.wikimedia.org' ) {
-			$wgCentralAuthCookieDomain = 'commons.m.wikimedia.org';
-		} elseif ( $wgCentralAuthCookieDomain == 'meta.wikimedia.org' ) {
-			$wgCentralAuthCookieDomain = 'meta.m.wikimedia.org';
-		}
-
-		// Better hack for T49647
-		$wgHooks['WebResponseSetCookie'][] = function ( &$name, &$value, &$expire, &$options ) {
-			if ( isset( $options['domain'] ) ) {
-				if ( $options['domain'] == 'commons.wikimedia.org' ) {
-					$options['domain'] = 'commons.m.wikimedia.org';
-				} elseif ( $options['domain'] == 'meta.wikimedia.org' ) {
-					$options['domain'] = 'meta.m.wikimedia.org';
-				}
+	// Better hack for T49647
+	$wgHooks['WebResponseSetCookie'][] = function ( &$name, &$value, &$expire, &$options ) {
+		if ( isset( $options['domain'] ) ) {
+			if ( $options['domain'] == 'commons.wikimedia.org' ) {
+				$options['domain'] = 'commons.m.wikimedia.org';
+			} elseif ( $options['domain'] == 'meta.wikimedia.org' ) {
+				$options['domain'] = 'meta.m.wikimedia.org';
 			}
-		};
-
-		return true;
+		}
 	};
 
-	$wgMFNearbyRange = $wgMaxGeoSearchRadius;
+	return true;
+};
 
-	// Turn on volunteer recruitment
-	$wgMFEnableJSConsoleRecruitment = true;
+$wgMFNearbyRange = $wgMaxGeoSearchRadius;
 
-	// Brute-force bandwidth optimization by stripping srcset (T119797)
-	$wgMFStripResponsiveImages = true;
+// Turn on volunteer recruitment
+$wgMFEnableJSConsoleRecruitment = true;
 
-	if ( $wmgMFDefaultEditor ) {
-		$wgMFDefaultEditor = $wmgMFDefaultEditor;
-	}
+// Brute-force bandwidth optimization by stripping srcset (T119797)
+$wgMFStripResponsiveImages = true;
+
+if ( $wmgMFDefaultEditor ) {
+	$wgMFDefaultEditor = $wmgMFDefaultEditor;
 }
 
 if ( $wmfRealm === 'labs' ) {
