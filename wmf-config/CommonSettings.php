1ff51798d77a4ea7ed3e9fb803d4dcd966dc4703
addshore <adamshorland@gmail.com>
2020-04-07 16:13:13 +0100

    RejectParserCache entries for wb_items_per_site 12/14.5 hours
    Bug: T249565
    Bug: T249595
    Change-Id: I5e2142cfb96df17cedc0cb79b784ad6f6feeffb6

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -3455,7 +3455,7 @@ if ( $wmgUseWikibaseClient ) {
 	$wgHooks['RejectParserCacheValue'][] = function ( $value, WikiPage $wikiPage, $popts ) {
 		$cachedTime = $value->getCacheTime();
 		$incidentStartTime = '20200406230000';
-		$windowRestrictionTime = '20200407090000'; // Window size of 10 hours
+		$windowRestrictionTime = '20200407110000'; // Window size of 12 hours
 		$incidentFullRestoreTime = '20200407133000';
 		if (
 			$wikiPage->getContentModel() === CONTENT_MODEL_WIKITEXT &&
