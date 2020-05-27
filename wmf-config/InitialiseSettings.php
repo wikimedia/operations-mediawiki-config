338cb9068d77f6d60ae0e59c949fca64de2d41b3
Martin Urbanec <martin.urbanec@wikimedia.cz>
2020-05-27 11:23:18 +0000

    wgNamespaceRobotPolicies: Set several namespaces to noindex,nofollow for thwiki
    Bug: T253574
    Change-Id: Iba0400973396a4c2bad0a7bfedddc6bfa134dec1

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -14391,8 +14391,11 @@ function wmfGetVariantSettings() {
 		NS_TEMPLATE_TALK => 'noindex,follow',
 		NS_HELP_TALK => 'noindex,follow',
 		NS_CATEGORY_TALK => 'noindex,follow',
-		100 => 'noindex,follow',
 		101 => 'noindex,follow',
+		109 => 'noindex,nofollow', // T253574
+		118 => 'noindex,nofollow', // T253574
+		119 => 'noindex,nofollow', // T253574
+		829 => 'noindex,nofollow', // T253574
 	],
 	'thwikibooks' => [
 		NS_TALK => 'noindex,follow',
