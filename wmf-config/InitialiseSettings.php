ec074675b78fba165066331079eb80e3caf339a6
Martin Urbanec <martin.urbanec@wikimedia.cz>
2020-06-04 11:49:45 +0000

    wgNamespaceRobotPolicies: thwiki: Add 100 NS to noindex
    Follow-up for 338cb90.
    
    Bug: T253574
    Change-Id: I48299bfbf80c76f7ca605865d6217aaba5dd720e

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -14391,6 +14391,7 @@ function wmfGetVariantSettings() {
 		NS_TEMPLATE_TALK => 'noindex,follow',
 		NS_HELP_TALK => 'noindex,follow',
 		NS_CATEGORY_TALK => 'noindex,follow',
+		100 => 'noindex,follow',
 		101 => 'noindex,follow',
 		109 => 'noindex,nofollow', // T253574
 		118 => 'noindex,nofollow', // T253574
