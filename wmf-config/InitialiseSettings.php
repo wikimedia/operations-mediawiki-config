73a78cb31615d82ad5235650f1d3931894d64b15
Andrew Otto <acotto@gmail.com>
2020-06-18 15:38:56 -0400

    EventLogging - use EventGate on group1 wikis for SearchSatisfaction
    Bug: T249261
    Change-Id: Ie40bf524d808dd00c7031649fe99e82c09cc09ab

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -21273,6 +21273,9 @@ function wmfGetVariantSettings() {
 	'+group0' => [
 		'SearchSatisfaction' => '/analytics/legacy/searchsatisfaction/1.1.0'
 	],
+	'+group1' => [
+		'SearchSatisfaction' => '/analytics/legacy/searchsatisfaction/1.1.0'
+	],
 ],
 
 // Enable Mediawiki client side (browser) Javascript error logging.
