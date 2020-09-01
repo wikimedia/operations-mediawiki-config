29a85069c2684c2034c2444bdcbad05601f485a6
Krinkle <krinklemail@gmail.com>
2020-09-01 14:35:52 +0000

    Revert "Enable MediaWiki client errors on commonswiki and metawiki"
    This reverts commit a7e67ab0390704a3fcae1b2479f002db7c032623.
    
    Reason for revert: Assuming accidentally +2'ed. Today are no deploys,
    and we're in the midst of a switch over procedure. Reverting to ensure
    it does not get pulled or synced by accident.
    
    Change-Id: I0410aa70e08150d0bc51c3386257de9637b409da

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -22059,8 +22059,6 @@ function wmfGetVariantSettings() {
 	'hawwiki' => 'https://intake-logging.wikimedia.org/v1/events?hasty=true',
 	'cawiki' => 'https://intake-logging.wikimedia.org/v1/events?hasty=true',
 	'hewiki' => 'https://intake-logging.wikimedia.org/v1/events?hasty=true',
-	'metawiki' => 'https://intake-logging.wikimedia.org/v1/events?hasty=true',
-	'commonswiki' => 'https://intake-logging.wikimedia.org/v1/events?hasty=true',
 ],
 
 'wmgUseWikimediaEvents' => [
