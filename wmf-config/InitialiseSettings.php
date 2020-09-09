f97a5c792885e6981ca9aafdb6327b1055d27328
Martin Urbanec <martin.urbanec@wikimedia.cz>
2020-09-09 07:39:21 +0000

    Disable DynamicPageList on ruwikinews
    Change-Id: If2ecc09fdb9ac8ddea88afc57fd02b8bb93ed9d0

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -15242,6 +15242,8 @@ function wmfGetVariantSettings() {
 	'wikisource' => true, // T10563 and T14423
 	'wikiversity' => true,
 
+	'ruwikinews' => false, // Emergency disabled per T262240
+
 	'bdwikimedia' => true, // T188109
 	'bswiki' => true, // T10240
 	'dewiktionary' => true,
