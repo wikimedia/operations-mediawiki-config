110a0d9ece08b39656814a858fb29a168cb4195f
Martin Urbanec <martin.urbanec@wikimedia.cz>
2020-09-15 23:09:37 +0000

    Revert "Add constants from FlaggedRevs to defines.php"
    This reverts commit cb1009138e6dd330f7234192512543809ec0e90e.
    
    Reason: This aparently doesn't do its job, yields
    Use of undefined constant APCOND_FR_MAXREVERTEDEDITRATIO
    and many other errors apparently caused by this commit.
    
    Change-Id: Ib0fc24463afff8dd1d7a1513d0788b67e37e33ae

--- src/defines.php
+++ src/defines.php
@@ -160,7 +160,8 @@ if ( !defined( 'NS_MEDIAWIKI' ) ) {
 	/** @} */
 
 	/** @{
-	 * Autopromote conditions
+	 * Autopromote conditions (must be here and not in Autopromote.php, so that
+	 * they're loaded for DefaultSettings.php before AutoLoader.php)
 	 */
 	define( 'APCOND_EDITCOUNT', 1 );
 	define( 'APCOND_AGE', 2 );
@@ -171,21 +172,6 @@ if ( !defined( 'NS_MEDIAWIKI' ) ) {
 	define( 'APCOND_AGE_FROM_EDIT', 7 );
 	define( 'APCOND_BLOCKED', 8 );
 	define( 'APCOND_ISBOT', 9 );
-	/** @} */
-
-	/** @{
-	 * Autopromote conditions defined by FlaggedRevs
-	 */
-	define( 'APCOND_FR_EDITSUMMARYCOUNT', 70821 );
-	define( 'APCOND_FR_NEVERBLOCKED', 70822 );
-	define( 'APCOND_FR_UNIQUEPAGECOUNT', 70823 );
-	define( 'APCOND_FR_CONTENTEDITCOUNT', 70824 );
-	define( 'APCOND_FR_USERPAGEBYTES', 70825 );
-	define( 'APCOND_FR_EDITCOUNT', 70826 );
-	define( 'APCOND_FR_EDITSPACING', 70827 );
-	define( 'APCOND_FR_CHECKEDEDITCOUNT', 70828 );
-	define( 'APCOND_FR_MAXREVERTEDEDITRATIO', 70829 );
-	define( 'APCOND_FR_NEVERDEMOTED', 70830 );
 	/** @} */
 
 	/** @{
