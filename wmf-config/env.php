c06540a82be1969991038d504d661da1e62c0825
Ahmon Dancy <adancy@wikimedia.org>
2020-10-07 11:27:19 -0700

    wmf-config/env.php: Add dcs and servicesFile info
    This commit adds two additional keys to the returned array, to be used
    by change I998177e69a5058e3.
    
    dcs: A list of strings naming the DCs known to the detected realm.
    
    servicesFile: The path to the PHP file containing service definitions
    for the realm.
    
    Change-Id: I245e84e0b8cbdfaafd563a479ad6d16e9c7aeab6

--- wmf-config/env.php
+++ wmf-config/env.php
@@ -1,27 +1,40 @@
 <?php
 # WARNING: This file is publicly viewable on the web. Do not put private data here.
 #
-# env.php statically exports the current realm and datacenter names.
+# This file returns an array with the following keys:
+#
+# - realm:         The name of the realm.
+# - dc:            The name of the local datacenter.
+# - dcs:           A list of all datacenters within the realm.
+# - servicesFile:  The path of the PHP file containing the realm's service definitions.
+#
+# This file is for PRODUCTION, and all other realms.
 #
 # - This file MUST NOT have any side effects when included.
-# - This file MAY may included in scripts on app servers before MediaWiki
-#   is initialised. For example, fatal-error.php and profiler.php.
+# - This file MAY may be included in scripts on app servers before MediaWiki
+#   is initialised. For example, PhpAutoPrepend.php and profiler.php.
 # - This file MUST NOT use any predefined state, only plain PHP.
 #
-# This both for PRODUCTION and for Beta Cluster.
-#
-# For MediaWiki, this is included in ../src/ServiceConfig.php
-#
+# For MediaWiki, this file is included via ../src/ServiceConfig.php.
 
-@$cluster = trim( file_get_contents( '/etc/wikimedia-cluster' ) ?: 'no-cluster-configured!' );
+// In PRODUCTION, /etc/wikimedia-cluster contains the name
+// of the local datacenter (e.g. eqiad, codfw, etc).  For any other
+// realms (e.g., labs), /etc/wikimedia-cluster contains the name of the realm.
+$mockWmgClusterFile = $GLOBALS['mockWmgClusterFile'] ?? '/etc/wikimedia-cluster';
+
+@$cluster = trim( file_get_contents( $mockWmgClusterFile ) ?: 'no-cluster-configured!' );
 if ( $cluster === 'labs' ) {
 	return [
 		'realm' => 'labs',
 		'dc' => 'eqiad',
+		'dcs' => [ 'eqiad' ],
+		'servicesFile' => __DIR__ . '/LabsServices.php',
 	];
 }
 
 return [
 	'realm' => 'production',
 	'dc' => $cluster,
+	'dcs' => [ 'eqiad', 'codfw' ],
+	'servicesFile' => __DIR__ . '/ProductionServices.php',
 ];
