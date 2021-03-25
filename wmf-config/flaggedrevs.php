dcfb7feaace1f397169e5e1bab7efd4e5f605a0f
Martin Urbanec <martin.urbanec@wikimedia.cz>
2021-03-25 19:38:10 +0100

    ruwiki: flaggedrevs: Do not remove autoreview group
    As part of renaming autoeditor to autoreview group, see task.
    
    Bug: T275337
    Change-Id: If65e5b232651e4f6abe5fe92410e5b406ceaa0e7

--- wmf-config/flaggedrevs.php
+++ wmf-config/flaggedrevs.php
@@ -595,7 +595,6 @@ $wgExtensionFunctions[] = function () {
 
 		$wgGroupPermissions['sysop']['stablesettings'] = true; // -aaron 3/20/10
 		$wgGroupPermissions['sysop']['review'] = false; // T275811
-		unset( $wgGroupPermissions['autoreview'] ); // T229596
 	} elseif ( $wgDBname == 'ruwikinews' ) {
 		$wgFlaggedRevsNamespaces = [ NS_MAIN, NS_CATEGORY, NS_TEMPLATE ];
 		$wgFlaggedRevsWhitelist = [ 'Main_Page' ];
