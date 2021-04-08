50042bb279748e31ed9c88c935ac2307bbb045b2
Timo Tijhof <krinklemail@gmail.com>
2021-04-08 04:46:00 +0100

    mc: Remove unused mcrouterAware/cluster/coalesceKeys
    * coalesceKeys was removed in MW with 74ebf0ec.
    * mcrouterAware/cluster were removed in MW with I219ea67416c6d121.
    
    Change-Id: I5869b3c3ba4a068e84ceb9acec792e42c8543381

--- wmf-config/mc.php
+++ wmf-config/mc.php
@@ -47,14 +47,6 @@ if ( $wgDBname === 'labswiki' || $wgDBname === 'labtestwiki' ) {
 		// Specify the route prefix that mcrouter listens for and broadcasts.
 		// The route prefix is configured in Puppet (profile::mediawiki::mcrouter_wancache).
 		'broadcastRoutingPrefix' => '/*/mw-wan/',
-
-		// TODO: Remove these after I219ea67416c6d121 rolls out.
-		'cluster' => 'mw-wan',
-		'mcrouterAware' => true,
-
-		// Reduce connection use by co-locating related keys (T252564)
-		// TODO: Can be removed after 74ebf0ec rolls out (1.36.0-wmf.38)
-		'coalesceKeys' => true
 	];
 }
 
