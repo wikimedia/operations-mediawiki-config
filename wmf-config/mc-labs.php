8107e4a9341ceede1100151b8961e9925a999c92
Timo Tijhof <krinklemail@gmail.com>
2021-04-08 04:45:02 +0100

    [Beta Cluster] mc: Set new 'broadcastRoutingPrefix' option
    This is required ahead of I219ea67416c6d121 rolling out.
    
    Change-Id: Id8fe11556894d2b0f07d9c54efee631c645618a9

--- wmf-config/mc-labs.php
+++ wmf-config/mc-labs.php
@@ -26,9 +26,14 @@ $wgWANObjectCaches['wancache-main-mcrouter'] = [
 	'cacheId' => $wgMainCacheType,
 	// Specify the route prefix that mcrouter listens for and broadcasts.
 	// The route prefix is configured in Puppet (profile::mediawiki::mcrouter_wancache).
+	'broadcastRoutingPrefix' => '/*/mw-wan/',
+
+	// TODO: Remove these after I219ea67416c6d121 rolls out.
 	'cluster' => 'mw-wan',
 	'mcrouterAware' => true,
-	// Reduce connection use by co-locating related keys
+
+	// Reduce connection use by co-locating related keys (T252564)
+	// TODO: Can be removed after 74ebf0ec rolls out (1.36.0-wmf.38)
 	'coalesceKeys' => true
 ];
 
