ef0f68e2a9c1c638911bb06c47ba6e8ef88ee393
Martin Urbanec <martin.urbanec@wikimedia.cz>
2021-04-16 03:28:32 +0200

    testwiki: wgGEMentorshipMigrationStage: Set to WRITE_BOTH/READ_NEW
    Bug: T279853
    Change-Id: Ifaf43f3717ed9ed2c5b33bb6372549a5c7f99d46

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -24833,8 +24833,8 @@ function wmfGetVariantSettings() {
 
 'wgGEMentorshipMigrationStage' => [
 	'default' => SCHEMA_COMPAT_WRITE_OLD | SCHEMA_COMPAT_READ_OLD,
-	'testwiki' => SCHEMA_COMPAT_WRITE_BOTH | SCHEMA_COMPAT_READ_OLD,
 	'cswiki' => SCHEMA_COMPAT_WRITE_BOTH | SCHEMA_COMPAT_READ_OLD,
+	'testwiki' => SCHEMA_COMPAT_WRITE_BOTH | SCHEMA_COMPAT_READ_NEW,
 ],
 
 'wmgGEFeaturesMayBeAvailableToNewcomers' => [
