224ea4a972d8d9db0cc2fc2eb473596d4591fac6
Bartosz Dziewoński <matma.rex@gmail.com>
2021-06-02 19:48:15 +0200

    Enable DiscussionTools' topicsubscription as beta feature on Beta Cluster
    Bug: T284169
    Change-Id: If265489d10116983ee812f14d9a658fc5e881a8a

--- wmf-config/InitialiseSettings-labs.php
+++ wmf-config/InitialiseSettings-labs.php
@@ -1841,6 +1841,10 @@ function wmfGetLabsOverrideSettings() {
 			'default' => 'default',
 		],
 
+		'-wgDiscussionTools_topicsubscription' => [
+			'default' => 'default',
+		],
+
 		'wgWatchlistExpiry' => [
 			'default' => true,
 		],
