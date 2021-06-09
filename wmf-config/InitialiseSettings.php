bc86994503824b74b18d8022cff02f031e206861
Ottomata <aotto@wikimedia.org>
2021-06-09 10:25:16 -0400

    Migrate LandingPageImpression to Event Platform on all wikis
    Bug: T282855
    Change-Id: I87d8ac4f1332c392cf6d5af0903ecf7a2f1b9ffb

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -20746,9 +20746,9 @@ function wmfGetVariantSettings() {
 	'default' => [
 		// Only in progress schema migrations go here, once migration finished, they are removed.
 		'VirtualPageView' => '/analytics/legacy/virtualpageview/1.0.0',
+		'LandingPageImpression' => '/analytics/legacy/landingpageimpression/1.0.0',
 	],
 	'testwiki' => [
-		'LandingPageImpression' => '/analytics/legacy/landingpageimpression/1.0.0',
 		'WMDEBannerEvents' => '/analytics/legacy/wmdebannerevents/1.0.0',
 		'WMDEBannerInteractions' => '/analytics/legacy/wmdebannerinteractions/1.0.0',
 		'WMDEBannerSizeIssue' => '/analytics/legacy/wmdebannersizeissue/1.0.0',
