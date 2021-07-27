90662d0f87c344cf0990462d1812cc26afe77b41
Tyler Cipriani <tcipriani@wikimedia.org>
2021-07-27 23:31:22 +0000

    Revert "Enable WVUI search on Wikimedia Commons"
    This reverts commit 3b3f6a98fec3435d85ab78911218c7e2c7b941c5.
    
    Change-Id: Ibc765d6b335f6a139707231207b45a27865012db

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -7840,6 +7840,7 @@ function wmfGetVariantSettings() {
 ],
 'wgVectorUseWvuiSearch' => [
 	'default' => true,
+	'commonswiki' => false,
 	'wikidatawiki' => false,
 ],
 'wgVectorWvuiSearchOptions' => [
@@ -7851,10 +7852,6 @@ function wmfGetVariantSettings() {
 		'showDescription' => false,
 	],
 	'officewiki' => [
-		'showThumbnail' => false,
-		'showDescription' => false,
-	],
-	'commonswiki' => [
 		'showThumbnail' => false,
 		'showDescription' => false,
 	],
