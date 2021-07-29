26bcaafdcd57b1b7a78f9e0ad000325baaf36a72
essexigyan <eigyan@wikimedia.org>
2021-07-29 14:39:36 -0400

    wmf-config: Restore logging for mediamoderation script to better understand high error rate occurring when running script
    Bug: T287511
    Change-Id: Ib0bd979570114991b5d3d9810dd0ac3ba15d5c9f

--- wmf-config/InitialiseSettings-labs.php
+++ wmf-config/InitialiseSettings-labs.php
@@ -261,6 +261,7 @@ function wmfGetLabsOverrideSettings() {
 				'LoginNotify' => 'debug',
 				'MassMessage' => 'debug', // for 59464 -legoktm 2013/12/15
 				'Math' => 'info',  // mobrovac for T121445
+				'mediamoderation' => 'warning', // for T287511
 				'memcached' => 'error', // -aaron 2012/10/24
 				'message-format' => [ 'logstash' => 'warning' ],
 				'MessageCacheError' => 'debug',
--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -5377,6 +5377,7 @@ function wmfGetVariantSettings() {
 		'LoginNotify' => 'debug',
 		'MassMessage' => 'debug', // for 59464 -legoktm 2013/12/15
 		'Math' => 'info',  // mobrovac for T121445
+		'mediamoderation' => 'warning', // for T287511
 		'memcached' => 'error', // -aaron 2012/10/24
 		'message-format' => [ 'logstash' => 'warning' ],
 		'MessageCacheError' => 'debug',
