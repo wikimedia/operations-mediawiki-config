bd68d63b03925e0c2b7b021b0c6ada3638a3334d
Ahmon Dancy <adancy@wikimedia.org>
2021-07-30 14:05:44 -0700

    Disable $wmgUseTranslationNotifications in train-dev environment
    Change-Id: I0d93d54203bd069b0370d3dadb503f9065b47b51

--- wmf-config/InitialiseSettings-dev.php
+++ wmf-config/InitialiseSettings-dev.php
@@ -56,6 +56,9 @@ function wmfGetOverrideSettings() {
 		'-wmgUseTranslationNotifications' => [
 			'default' => false,
 		],
+		'-wmgUseFundraisingTranslateWorkflow' => [
+			'default' => false,
+		],
 		'-wgGEDatabaseCluster' => [
 			'default' => false,
 		],
