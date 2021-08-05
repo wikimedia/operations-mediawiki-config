e81dacaabb179ac169eed229090a1254c197f843
Amir Sarabadani <ladsgroup@gmail.com>
2021-08-05 20:40:06 +0200

    Increase the ratio for shellbox for constraints to 42% in Wikidata
    It's the answer
    
    Bug: T176312
    Change-Id: I0542b08280e8e9cf85b9214700724fa7c6df9686

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -22543,7 +22543,7 @@ function wmfGetVariantSettings() {
 'wgWBQualityConstraintsFormatCheckerShellboxRatio' => [
 	'default' => 0,
 	'testwikidatawiki' => 1,
-	'wikidatawiki' => 0.21,
+	'wikidatawiki' => 0.42,
 ],
 
 'wgWBQualityConstraintsSparqlEndpoint' => [
