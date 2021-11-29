e3e148904356552c2153588f72ebc2626e99968d
Erik Bernhardson <ebernhardson@wikimedia.org>
2021-11-29 09:11:44 -0800

    Move CirrusSearch traffic back to eqiad
    State within the cluster has been restored; moving traffic
    back to its local datacenter.
    
    Bug: T295705
    Change-Id: I45a908f036dec2d29a6b29c4b7fbdac9d04bbb15

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -23524,11 +23524,11 @@ function wmfGetVariantSettings() {
 // $wgTranslateTranslationDefaultService in CommonSettings.php if you plan to
 // bring down a specific cluster.
 'wgCirrusSearchDefaultCluster' => [
-	'default' => 'codfw',
+	'default' => $GLOBALS['wmfDatacenter'],
 ],
 // Kept for BC with SRE tools that checks siteinfo (see APIQuerySiteInfoGeneralInfo in CommonSettings.php)
 'wmgCirrusSearchDefaultCluster' => [
-	'default' => 'codfw',
+	'default' => 'local',
 ],
 
 'wgCirrusSearchClusterOverrides' => [
