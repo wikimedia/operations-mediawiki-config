d0415c92899fb3b36835d90a93550991638794a7
Erik Bernhardson <ebernhardson@wikimedia.org>
2021-12-06 09:13:12 -0800

    Move cirrus traffic to codfw
    We previously restored the commonswiki_file index from codfw to eqiad,
    but that was insufficient. We need to drop and restore all commonswiki
    indices. To avoid user-visible errors during the restore move read
    traffic to codfw.
    
    Bug: T296897
    Change-Id: I9c0d7c7f8917ad9cc660e7e202ec549fc82dbea2

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -23591,11 +23591,11 @@ function wmfGetVariantSettings() {
 // $wgTranslateTranslationDefaultService in CommonSettings.php if you plan to
 // bring down a specific cluster.
 'wgCirrusSearchDefaultCluster' => [
-	'default' => $GLOBALS['wmfDatacenter'],
+	'default' => 'codfw',
 ],
 // Kept for BC with SRE tools that checks siteinfo (see APIQuerySiteInfoGeneralInfo in CommonSettings.php)
 'wmgCirrusSearchDefaultCluster' => [
-	'default' => 'local',
+	'default' => 'codfw',
 ],
 
 'wgCirrusSearchClusterOverrides' => [
