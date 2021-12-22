34bf91ec2ba1408594bb77745deb6fa7d36ddf8d
Gergő Tisza <tgr.huwiki@gmail.com>
2021-12-22 12:09:44 -0800

    GrowthExperiments: Add campaign pattern for JOSA
    Bug: T298057
    Change-Id: I76be1b22345060f1ea971b2a78a64c425edc8bdd

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -26591,6 +26591,7 @@ function wmfGetVariantSettings() {
 'wgGECampaignPattern' => [
 	'default' => null,
 	'testwiki' => '/^growth-advancement-test-2021$/',
+	'arwiki' => '/^growth-JOSA-jan2022$/',
 	'enwiki' => '/^growth-recurring-english-2021$|^growth-recurring-english-control-2021$/',
 	'eswiki' => '/^growth-advancement-spanish-2021$/',
 	'ptwiki' => '/^growth-advancement-brazil-2021$/'
