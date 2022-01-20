5b7b816440d566421c3e46244d10d6dc21f7939b
Petr Pchelko <ppchelko@wikimedia.org>
2022-01-20 09:43:13 -0800

    Temp settings benchmarking entrypoint enhancements
    1. MW statsd is buffering, so we need
    to flush the buffer in the end.
    2. Print out how long things take in the response
    
    Change-Id: I42edaa8f691a171ee973754c3109e0e81c14ebca

--- w/tmp_settings_bench.php
+++ w/tmp_settings_bench.php
@@ -11,6 +11,7 @@ require_once __DIR__ . '/../multiversion/MWMultiVersion.php';
 require MWMultiVersion::getMediaWiki( 'includes/WebStart.php' );
 
 global $IP;
+$config = MediaWiki\MediaWikiServices::getInstance()->getMainConfig();
 $stats = MediaWiki\MediaWikiServices::getInstance()->getStatsdDataFactory();
 
 // 1. Benchmark loading YAML with SettingsBuilder.php.
@@ -35,30 +36,29 @@ try {
 	$phonySettings
 		->loadFile( 'includes/config-schema.yaml' )
 		->apply();
-	$stats->timing(
-		"tmp_settings_load.${cacheType}_success",
-		1000 * max( 0, microtime( true ) - $start )
-	);
+	$timing = 1000 * max( 0, microtime( true ) - $start );
+	echo "YAML loaded in $timing\n";
+	$stats->timing( "tmp_settings_load.${cacheType}_success", $timing );
 } catch ( Throwable $e ) {
 	MediaWiki\Logger\LoggerFactory::getInstance( 'SettingsBuilder' )
 		->warning(
 			'Failed to load config schema',
 			[ 'exception' => $e, ]
 		);
-	$stats->timing(
-		"tmp_settings_load.${cacheType}_failed",
-		1000 * max( 0, microtime( true ) - $start )
-	);
+	$timing = 1000 * max( 0, microtime( true ) - $start );
+	echo "YAML errored in $timing\n";
+	$stats->timing( "tmp_settings_load.${cacheType}_failed", $timing );
 }
 
 // 2. Benchmark reloading settings from DefaultSettings.php
 ( static function () use ( $IP, $stats ) {
 	$start = microtime( true );
 	require "$IP/includes/DefaultSettings.php";
-	$stats->timing(
-		"tmp_settings_load.old",
-		1000 * max( 0, microtime( true ) - $start )
-	);
+	$timing = 1000 * max( 0, microtime( true ) - $start );
+	echo "PHP loaded in $timing\n";
+	$stats->timing( "tmp_settings_load.old", $timing );
 } )();
+
+MediaWiki::emitBufferedStatsdData( $stats, $config );
 
 echo "^._.^\n";
