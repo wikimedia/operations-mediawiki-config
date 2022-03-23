b1114cb06c9459562f9b50dc6b1cb5f7fecc7fda
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2022-03-23 15:27:56 +0100

    Write "unexpectedUnconnectedPage" page prop everywhere
    Change-Id: I5aab8e6f9df71f207c85a2265f4398ef705303a3

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -28303,8 +28303,7 @@ function wmfGetVariantSettings() {
 ],
 
 'wmgWikibaseClientUnconnectedPageMigrationStage' => [
-	'default' => MIGRATION_OLD,
-	'wikidataclient-test' => MIGRATION_WRITE_BOTH,
+	'default' => MIGRATION_WRITE_BOTH,
 ],
 
 // T123582: enables <link rel="preconnect" href="//upload.wikimedia.org">
