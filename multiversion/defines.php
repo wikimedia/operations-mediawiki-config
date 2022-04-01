436ecf04011827ad4222f04807be76658b0768d3
daniel <dkinzler@wikimedia.org>
2022-04-01 11:29:16 +0200

    Always set MW_USE_CONFIG_SCHEMA.
    The use of MW_USE_CONFIG_SCHEMA will soon be redundant. But before we
    turn on config schema loading in core by default, we want to test it in
    production with an easy way to swicth it off again. That's what this
    patch is. Previously, we were testing this on individual host, by
    checking if /var/run/php/use_config_schema exists.
    
    Bug: T305176
    Change-Id: Id3c39626132c82855352d6f6dc6b6f26adb4bd4d

--- multiversion/defines.php
+++ multiversion/defines.php
@@ -5,7 +5,5 @@
 define( 'MEDIAWIKI_STAGING_DIR', '/srv/mediawiki-staging' );
 define( 'MEDIAWIKI_DEPLOYMENT_DIR', '/srv/mediawiki' );
 
-// Temporary hack, see T304460
-if ( file_exists( '/var/run/php/use-config-schema' ) ) {
-	define( 'MW_USE_CONFIG_SCHEMA', 'yes' );
-}
+// Temporary, will soon be redundant, see T305176
+define( 'MW_USE_CONFIG_SCHEMA', 'yes' );
