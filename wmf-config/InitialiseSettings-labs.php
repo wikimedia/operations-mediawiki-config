40c64e38f81c2ecb726004fc03c22bfcdab7de57
Nicholas Ray <nray@wikimedia.org>
2022-04-12 14:33:53 -0600

    Enable Table of Contents AB test on Beta
    This will be used to QA the work done in T302046.
    
    Bug: T302046
    Change-Id: I2c3b0df82c7f5e18314c4fca00f9ccd5d46664e9

--- wmf-config/InitialiseSettings-labs.php
+++ wmf-config/InitialiseSettings-labs.php
@@ -557,6 +557,21 @@ function wmfGetLabsOverrideSettings() {
 				'default' => true,
 			],
 		],
+		'wgVectorWebABTestEnrollment' => [
+			'name' => 'skin-vector-toc-experiment',
+			'enabled' => true,
+			'buckets' => [
+				'unsampled' => [
+					'samplingRate' => 0
+				],
+				'control' => [
+					'samplingRate' => 0.5
+				],
+				'treatment' => [
+					'samplingRate' => 0.5
+				],
+			]
+		],
 		'wgVectorSkinMigrationMode' => [
 			'default' => true
 		],
