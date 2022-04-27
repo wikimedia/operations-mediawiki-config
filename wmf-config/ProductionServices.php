7f973ec678b61b246d4b0c3bc7a02c34eaf92ea7
Erik Bernhardson <ebernhardson@wikimedia.org>
2022-04-27 13:50:30 -0700

    cirrus: Enable DeprecationLoggedHttps
    Most deprecations have been resolved, enable logging on the cirrus
    side to include stack traces of remaining problematic areas.
    
    Bug: T218994
    Change-Id: I46d45088e00cb8d31ea0c75003020d1ec54f8910

--- wmf-config/ProductionServices.php
+++ wmf-config/ProductionServices.php
@@ -150,21 +150,21 @@ $services = [
 		'search-chi' => [
 			[ // forwarded to https://search.svc.eqiad.wmnet:9243/
 				'host' => 'localhost',
-				'transport' => 'Http',
+				'transport' => CirrusSearch\Elastica\DeprecationLoggedHttp::class,
 				'port' => 6102,
 			]
 		],
 		'search-psi' => [
 			[ // forwarded to https://search.svc.eqiad.wmnet:9643/
 				'host' => 'localhost',
-				'transport' => 'Http',
+				'transport' => CirrusSearch\Elastica\DeprecationLoggedHttp::class,
 				'port' => 6104,
 			]
 		],
 		'search-omega' => [
 			[ // forwarded to https://search.svc.eqiad.wmnet:9443/
 				'host' => 'localhost',
-				'transport' => 'Http',
+				'transport' => CirrusSearch\Elastica\DeprecationLoggedHttp::class,
 				'port' => 6103,
 			]
 		],
