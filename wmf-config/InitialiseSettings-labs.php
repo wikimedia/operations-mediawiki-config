4d192aa553adbf8b545d2335fd1269a940ebef53
Thalia <thalia.e.chan@googlemail.com>
2022-04-29 18:27:58 +0100

    Add QuickSurveys survey for the SimilarEditors feature
    Bug: T307025
    Change-Id: I20f32ef95d37f8fd937af26b8fabc5e6c0487520

--- wmf-config/InitialiseSettings-labs.php
+++ wmf-config/InitialiseSettings-labs.php
@@ -754,6 +754,33 @@ function wmfGetOverrideSettings() {
 						'mobile' => [ 'stable' ]
 					],
 				],
+				'similareditors' => [
+					// T307025
+					'name' => 'similareditors',
+					'type' => 'internal',
+					'layout' => 'single-answer',
+					'answers' => [],
+					// Ensures a text input is added
+					'freeformTextLabel' => 'similareditors-survey-freeform-text-label',
+					'question' => 'similareditors-survey-question',
+					'privacyPolicy' => 'ext-quicksurveys-similareditors-survey-privacy-policy',
+					'enabled' => true,
+					'platforms' => [
+						'desktop' => [ 'stable' ],
+						'mobile' => [ 'stable' ]
+					],
+					'audience' => [
+						// This prevents audience being checked on every page.
+						// However, we must still load quicksurveys manually on
+						// Special:SimilarEditors, because automatic loading only
+						// works for page IDs of pages that exist (not special pages).
+						// 0 matches the page ID check for Special:SimilarEditors.
+						'pageIds' => [ 0 ],
+					],
+					// Ensures the correct survey is added if multiple are enabled
+					'embedElementId' => 'similareditors-survey-embed',
+					'coverage' => 1,
+				],
 				// T294363: QA internal survey custom confirmation and additional info
 				[
 					'name' => 'T294363-1',
