f89f02e306a1fa580fa41ba56de978f4208ea672
MarcoAurelio <maurelio@toolforge.org>
2022-06-30 12:40:59 +0200

    Amend license request contact form per Legal
    Bug: T303359
    Change-Id: I5a665c40a29fd01c6411c8b9dad6f5803478b8ae

--- wmf-config/MetaContactPages.php
+++ wmf-config/MetaContactPages.php
@@ -204,30 +204,8 @@ $wgContactConfig['affcomchapthorg'] = [
 
 /**
  * Configuration for legal contact forms (license stuff)
+ * Modifications: T303359
  */
-$trademark = [
-	'type' => 'multiselect',
-	'options-messages' => [
-		'contactpage-wikimedia-trademark-globe' => 'wikiglobe',
-		'contactpage-wikimedia-trademark-wikiwordmark' => 'wikipediawordmark',
-		'contactpage-wikimedia-trademark-w' => 'stylizedw',
-		'contactpage-wikimedia-trademark-foundation' => 'foundation',
-		'contactpage-wikimedia-trademark-commons' => 'commons',
-		'contactpage-wikimedia-trademark-incubator' => 'incubator',
-		'contactpage-wikimedia-trademark-mediawiki' => 'mediawiki',
-		'contactpage-wikimedia-trademark-wikiquote' => 'wikiquote',
-		'contactpage-wikimedia-trademark-wikibooks' => 'wikibooks',
-		'contactpage-wikimedia-trademark-wikimania' => 'wikimania',
-		'contactpage-wikimedia-trademark-wikimedia' => 'wikimedia',
-		'contactpage-wikimedia-trademark-wikinews' => 'wikinews',
-		'contactpage-wikimedia-trademark-wikisource' => 'wikisource',
-		'contactpage-wikimedia-trademark-wikispecies' => 'wikispecies',
-		'contactpage-wikimedia-trademark-wikiversity' => 'wikiversity',
-		'contactpage-wikimedia-trademark-wiktionary' => 'wiktionary',
-		'contactpage-wikimedia-trademark-wikivoyage' => 'wikivoyage',
-	],
-	'required' => true,
-];
 
 $wgContactConfig['requestlicense'] = [
 	'RecipientUser' => 'Trademarks (WMF)',
@@ -237,63 +215,22 @@ $wgContactConfig['requestlicense'] = [
 	'RequireDetails' => true,
 	'IncludeIP' => false,
 	'AdditionalFields' => [
-		'Username' => [
-			'type' => 'text',
-			'required' => true,
-			'label-message' => 'contactpage-license-request-username',
-		],
-		'Site' => [
-			'type' => 'text',
-			'label-message' => 'contactpage-license-request-relevantsite',
-		],
 		'Group' => [
 			'type' => 'text',
 			'label-message' => 'contactpage-license-request-group',
 		],
-		'Title' => [
-			'type' => 'text',
-			'label-message' => 'contactpage-license-request-title',
-		],
-		'Org' => [
-			'type' => 'text',
-			'label-message' => 'contactpage-license-request-organization',
-		],
-		'OrgType' => [
-			'type' => 'text',
-			'label-message' => 'contactpage-license-request-organization-type',
-		],
-		'ProposedUse' => [
-			'type' => 'selectorother',
-			'label-message' => 'contactpage-license-request-use-proposed',
-			'options-messages' => [
-				'contactpage-license-request-use-online' => 'online',
-				'contactpage-license-request-use-book' => 'book',
-				'contactpage-license-request-use-print' => 'print',
-				'contactpage-license-request-use-tv' => 'tv',
-			],
-			'default' => 'online',
-			'required' => true,
-		],
-
 		'Description' => [
 			'label-message' => 'contactpage-license-request-description',
 			'type' => 'textarea',
 			'rows' => 10,
 			'required' => true,
 		],
-
-		'Trademark' => [
-			'label-message' => 'contactpage-license-request-selectmark',
-		] + $trademark,
-
 		'use-note' => [
 			'type' => 'info',
 			'help-messages' => [ 'contactpage-license-request-use-note' ],
 		],
 	]
 ];
-
-unset( $trademark );
 
 /**
  * Configuration for signup form for [[:meta:Movement_communications_group]]
