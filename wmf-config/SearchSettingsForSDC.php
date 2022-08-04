2508b0225cda2c410077271df0a98fd4f8ec9f26
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2022-08-04 11:36:06 +0200

    Remove unused $wgWBCSEnableDispatchingQueryBuilder
    Removed from WikibaseCirrusSearch in change If45344fe915 (commit
    0cd8339c80), merged three years ago.
    
    Change-Id: If6f38d5bee27419ce5d508a9778a8ebf7daa35b5

--- wmf-config/SearchSettingsForSDC.php
+++ wmf-config/SearchSettingsForSDC.php
@@ -1,6 +1,1 @@
 <?php
-
-// T218954
-// Disable dispatching query builder, which provides specialized
-// entity full text search, until it meets the commons use case.
-$wgWBCSEnableDispatchingQueryBuilder = false;
