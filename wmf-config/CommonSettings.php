a9987aa7423e9c986d4182f6ec0e6942c967cf78
Taavi Väänänen <hi@taavi.wtf>
2022-08-04 16:03:28 +0300

    Remove unused CA P3P config
    Unused after Ia0b7474e8d13eb983fb469688e94561e2ce03236.
    
    Change-Id: Ib0b3fb277107de1d8e4c02424fa6265d7eb8003f

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -1828,10 +1828,6 @@ if ( $wmgUseCentralAuth ) {
 	$wgCentralAuthUseEventLogging = true;
 	$wgCentralAuthPreventUnattached = true;
 
-	// Optimisation: Avoid overhead of computing localised urls
-	// See https://phabricator.wikimedia.org/T189966#5436482
-	$wgCentralAuthCookiesP3P = "CP=\"See $wgCanonicalServer/wiki/Special:CentralAutoLogin/P3P for more info.\"";
-
 	foreach ( $wmgLocalServices['irc'] as $address ) {
 		$wgCentralAuthRC[] = [
 			'formatter' => 'MediaWiki\\Extension\\CentralAuth\\RCFeed\\IRCColourfulCARCFeedFormatter',
