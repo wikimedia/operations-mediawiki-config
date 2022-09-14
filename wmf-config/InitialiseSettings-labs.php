1510e7aa3d37d862b0f3c1d58da7ae325dc2f5b1
WMDE-Fisch <christoph.jauera@wikimedia.de>
2022-09-14 10:26:21 +0200

    [beta] Add WMDE Technical Wishes QuickSurvey
    Bug: T316676
    Change-Id: I1a941048efb844b4a1966a5f6e0862e1a6cf0229

--- wmf-config/InitialiseSettings-labs.php
+++ wmf-config/InitialiseSettings-labs.php
@@ -996,6 +996,28 @@ function wmfGetOverrideSettings() {
 					'shuffleAnswersDisplay' => true,
 				],
 			],
+			'dewiki' => [
+				// T316676
+				[
+					'enabled' => true,
+					'name' => 'T316676',
+					'type' => 'external',
+					'question' => 'wmde-tw-participation-2022-survey-header',
+					'description' => 'wmde-tw-participation-2022-survey-description',
+					'link' => 'wmde-tw-participation-2022-survey-link',
+					'confirmMsg' => 'wmde-tw-participation-2022-survey-confirm',
+					'additionalInfo' => 'wmde-tw-participation-2022-survey-thanks',
+					'privacyPolicy' => 'wmde-tw-participation-2022-survey-privacy-policy',
+					'coverage' => 1.0,
+					'platforms' => [
+						'desktop' => [ 'stable' ],
+						'mobile' => [ 'stable' ],
+					],
+					'audience' => [
+						'anons' => false,
+					],
+				],
+			],
 		],
 
 		'-wgScorePath' => [
