065bd2be57985073a087fe76ec060aa713e1beb4
David Lynch <dlynch@wikimedia.org>
2022-09-28 17:14:52 -0500

    Stop mobile visual enhancements from rolling out to jawiki
    Bug: T318871
    Change-Id: Iefb0a1af0de79a8daaf6ffbe4a93ee7b95e28b0e

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -20420,6 +20420,10 @@ return [
 	'htwiki' => true, // T298221, T302259
 	'mediawikiwiki' => true, // T310960
 ],
+'wgDiscussionToolsEnableMobile_visualenhancements' => [
+	'default' => true,
+	'jawiki' => false, // T318871
+],
 
 'wgDiscussionToolsReplyIndentation' => [
 	'default' => 'invisible',
