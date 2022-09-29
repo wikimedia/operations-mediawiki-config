f84d4ff38f6c58bf9a66d8389d729439804d5093
Brennen Bearnes <bbearnes@wikimedia.org>
2022-09-29 20:55:39 +0000

    Revert "cirrus: Don't configure cloud clusters for private wikis"
    This reverts commit 1ea86adca33913c428fd92fc7a758cd9b682d545
    
    Reason for revert: brennen@deploy1002: syntax error in wmf-config/CirrusSearch-production.php
    
    Change-Id: I0e7656a2e2eb2b3130117ff4033461c1747bf62d

--- wmf-config/CirrusSearch-production.php
+++ wmf-config/CirrusSearch-production.php
@@ -26,14 +26,6 @@ $wgCirrusSearchClusters = [
 	'cloudelastic-omega' => $wmgAllServices['eqiad']['cloudelastic-omega'] + [ 'group' => 'omega', 'replica' => 'cloudelastic' ],
 ];
 
-if ( $wmgPrivateWiki ) {
-	unset(
-		$wgCirrusSearchClusters['cloudelastic-chi'],
-		$wgCirrusSearchClusters['cloudelastic-psi'],
-		$wgCirrusSearchClusters['cloudelastic-omega'],
-	);
-}
-
 // wgCirrusSearchExtraIndexes is set in CirrusSearch-common.php
 if ( isset( $wgCirrusSearchExtraIndexes[NS_FILE] ) ) {
 	$wgCirrusSearchExtraIndexes[NS_FILE] = [ 'chi:commonswiki_file' ];
