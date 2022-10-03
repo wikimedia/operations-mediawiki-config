022690b4739799963c1164e28750dc7653801355
Erik Bernhardson <ebernhardson@wikimedia.org>
2022-10-03 13:07:35 -0700

    cirrus: remove cross-dc poolcounter increases
    With read traffic split between multiple datacenters we no longer
    have a single poolcounter limiting read traffic to a single
    elasticsearch cluster. In instances where we send all traffic to a
    single cluster there is likely to be both local and remote traffic
    going to that cluster.
    
    As such, stop increasing pool counter limits when talking to a remote
    cluster. That cluster will already be under some load. This may result
    in some poolcounter based rejection the next time we do a test
    involving sending all traffic to a single DC, values can be retuned
    as necessary then.
    
    Change-Id: I4e193574b7157a69047c65b8a9981f258df492c8

--- wmf-config/CirrusSearch-common.php
+++ wmf-config/CirrusSearch-common.php
@@ -36,25 +36,6 @@ $wgCirrusSearchCustomPageFields = [
 	'popularity_score' => 'number',
 ];
 
-// PoolCounter needs to be adjusted to account for additional latency when default search
-// is pointed at a remote datacenter. Currently this makes the assumption that it will either
-// be eqiad or codfw which have ~40ms latency between them. Multiples are chosen using
-// (p75 + cross dc latency)/p75
-if ( $wgCirrusSearchDefaultCluster !== $wmgDatacenter ) {
-	// prefix has p75 of ~30ms
-	if ( isset( $wgPoolCounterConf[ 'CirrusSearch-Prefix' ] ) ) {
-		$wgPoolCounterConf['CirrusSearch-Prefix']['workers'] *= 2;
-	}
-	// namespace has a p75 of ~15ms
-	if ( isset( $wgPoolCounterConf['CirrusSearch-NamespaceLookup' ] ) ) {
-		$wgPoolCounterConf['CirrusSearch-NamespaceLookup']['workers'] *= 3;
-	}
-	// completion has p75 of ~30ms
-	if ( isset( $wgPoolCounterConf['CirrusSearch-Completion'] ) ) {
-		$wgPoolCounterConf['CirrusSearch-Completion']['workers'] *= 2;
-	}
-}
-
 if ( $wmgCirrusSearchMLRModel ) {
 	foreach ( $wmgCirrusSearchMLRModel as $name => $mlrModel ) {
 		// LTR Rescore profile
