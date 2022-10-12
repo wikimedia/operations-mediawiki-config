6a620a6736f90849f61a88623ee4aca778a25def
Martin Urbanec <martin.urbanec@wikimedia.cz>
2022-10-12 17:24:38 +0200

    eswiki: Deploy mentorship to only 15% of users
    Mentorship was previously deployed to
    25% of users. There are concerns that
    the current workload is too much for
    eswiki's mentor base. To err on the
    side of caution, deploy the feature
    to less users for now.
    
    Bug: T285235
    Change-Id: I64578eb4a0f3546fa8cd47b9dc63fc2868ca4a82

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -26549,7 +26549,7 @@ return [
 'wgGEMentorshipNewAccountEnablePercentage' => [
 	'default' => 100,
 	'enwiki' => 10, // T290927
-	'eswiki' => 25, // T285235
+	'eswiki' => 15, // T285235
 ],
 
 'wgGEHomepageDefaultVariant' => [
