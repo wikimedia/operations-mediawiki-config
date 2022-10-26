218157950b6d9c560c7cd7e58ab86b911705d8c4
Amir Sarabadani <ladsgroup@gmail.com>
2022-10-26 12:27:44 +0200

    beta: Start doing write both of externallinks
    Bug: T321662
    Change-Id: Ie4761f388ed30fe9cbfb7a0729deffd5b8772029

--- wmf-config/InitialiseSettings-labs.php
+++ wmf-config/InitialiseSettings-labs.php
@@ -1450,6 +1450,10 @@ function wmfGetOverrideSettings() {
 			'default' => SCHEMA_COMPAT_WRITE_NEW | SCHEMA_COMPAT_READ_NEW,
 		],
 
+		'-wgExternalLinksSchemaMigrationStage' => [
+			'default' => SCHEMA_COMPAT_WRITE_BOTH | SCHEMA_COMPAT_READ_OLD,
+		],
+
 		'wgOresUiEnabled' => [
 			'default' => true,
 			'wikidatawiki' => false,
