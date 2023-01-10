7968602e3c84e48ccd9ef3e79ca96f89239ba40e
Bartosz Dziewoński <matma.rex@gmail.com>
2023-01-10 19:18:56 +0100

    Use new DiscussionTools heading markup on group2 wikis except enwiki
    Bug: T314714
    Change-Id: Idb03829a189246db62bc8dcdf331aaad83b954cb

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -19748,10 +19748,8 @@ return [
 ],
 
 'wgDiscussionToolsLegacyHeadingMarkup' => [
-	'default' => true, // T314714
-	'group0' => false, // T314714
-	'group1' => false, // T314714
-	'plwiki' => false, // T314714
+	'default' => false, // T314714
+	'enwiki' => true, // T314714
 ],
 
 'wgDiscussionToolsShowFeedbackLinks' => [
