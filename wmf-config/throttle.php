13305cce5d4d14186341ea6c6d9c8dbf650a0988
Martin Urbanec <martin.urbanec@wikimedia.cz>
2023-01-12 12:14:43 +0100

    throttle: Add new rule for cswiki course
    Also cleans up expired rules.
    
    Bug: T326792
    Change-Id: I3b3d31102df251b9e65c8de008286dda2007809e

--- wmf-config/throttle.php
+++ wmf-config/throttle.php
@@ -32,29 +32,11 @@ $wmgThrottlingExceptions = [];
 ## deploying your change to this file!
 ## https://wikitech.wikimedia.org/wiki/Increasing_account_creation_threshold
 
-// T319212
+// T326792
 $wmgThrottlingExceptions[] = [
-	'from' => '2022-10-06T0:00 +2:00',
-	'to' => '2022-10-06T23:59 +2:00',
-	'IP' => '195.113.145.2',
-	'dbname' => [ 'cswiki' ],
-	'value' => 20,
-];
-
-// T319244
-$wmgThrottlingExceptions[] = [
-	'from' => '2022-10-13T10:00 +1:00',
-	'to' => '2022-10-13T12:00 +1:00',
-	'range' => '185.153.192.0/26',
-	'dbname' => [ 'cswiki', 'commonswiki' ],
-	'value' => 15,
-];
-
-// T324105
-$wmgThrottlingExceptions[] = [
-	'from' => '2022-11-30T0:00 +1:00',
-	'to' => '2022-11-30T23:59 +1:00',
-	'IP' => [ '83.208.68.147', '195.113.244.93' ],
+	'from' => '2023-01-12T0:00 +1:00',
+	'to' => '2023-01-12T23:59 +1:00',
+	'IP' => '195.113.132.25',
 	'dbname' => [ 'cswiki', 'skwiki', 'enwiki', 'commonswiki' ],
 	'value' => 25,
 ];
