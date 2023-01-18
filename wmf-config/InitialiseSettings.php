5e1643b5c8b54a77106a2fff219b045ebf5499fd
essexigyan <eigyan@wikimedia.org>
2023-01-18 14:16:45 -0500

    [config]: Undeploy GDI Safety Survey Wave 4
    Undeploy GDI Safety Survey Wave 4 on EN, ES, FR, FA, PT wikis
    
    Bug: T327296
    Change-Id: I85ed7e7e64ea1a2bd32a9ccd7097471cfb4379ec

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -25465,7 +25465,6 @@ return [
 	'cawiki' => true,
 	'dewiki' => true,
 	'enwikivoyage' => true,
-	'enwiki' => true,
 	'eswiki' => true,
 	'fawiki' => true,
 	'frwiki' => true,
@@ -25475,7 +25474,6 @@ return [
 	'metawiki' => true,
 	'nowiki' => true,
 	'plwiki' => true,
-	'ptwiki' => true,
 	'rowiki' => true,
 	'ruwiki' => true,
 	'swwiki' => true,
@@ -25545,29 +25543,6 @@ return [
 			'privacyPolicy' => 'ext-quicksurveys-performance-internal-survey-privacy-policy',
 			'shuffleAnswersDisplay' => true,
 		],
-		[
-			// T325136
-			'name' => 'internal-gdi-safety-survey-wave4',
-			'type' => 'internal',
-			'layout' => 'single-answer',
-			'question' => 'ext-quicksurveys-internal-gdi-safety-survey-question',
-			'privacyPolicy' => 'ext-quicksurveys-internal-gdi-safety-survey-privacy-policy',
-			'answers' => [
-				'ext-quicksurveys-internal-gdi-safety-survey-answer-positive',
-				'ext-quicksurveys-internal-gdi-safety-survey-answer-negative',
-				'ext-quicksurveys-internal-gdi-safety-survey-answer-neutral',
-			],
-			'audience' => [
-				// T325136
-				'minEdits' => 5
-			],
-			'enabled' => true,
-			'coverage' => 0.1, // T325136
-			'platforms' => [
-				'desktop' => [ 'stable' ],
-				'mobile' => [ 'stable', 'beta' ],
-			],
-		],
 	],
 	'frwiki' => [
 		// T187299
@@ -25587,29 +25562,6 @@ return [
 			],
 			'privacyPolicy' => 'ext-quicksurveys-performance-internal-survey-privacy-policy',
 			'shuffleAnswersDisplay' => true,
-		],
-		[
-			// T325136
-			'name' => 'internal-gdi-safety-survey-wave4',
-			'type' => 'internal',
-			'layout' => 'single-answer',
-			'question' => 'ext-quicksurveys-internal-gdi-safety-survey-question',
-			'privacyPolicy' => 'ext-quicksurveys-internal-gdi-safety-survey-privacy-policy',
-			'answers' => [
-				'ext-quicksurveys-internal-gdi-safety-survey-answer-positive',
-				'ext-quicksurveys-internal-gdi-safety-survey-answer-negative',
-				'ext-quicksurveys-internal-gdi-safety-survey-answer-neutral',
-			],
-			'audience' => [
-				// T325136
-				'minEdits' => 5
-			],
-			'enabled' => true,
-			'coverage' => 0.1, // T325136
-			'platforms' => [
-				'desktop' => [ 'stable' ],
-				'mobile' => [ 'stable', 'beta' ],
-			],
 		],
 	],
 	'ruwiki' => [
@@ -25649,82 +25601,7 @@ return [
 			'link' => 'research-incentive-link',
 			'privacyPolicy' => 'research-incentive-privacy',
 		],
-	],
-	'ptwiki' => [
-		[
-			// T325136
-			'name' => 'internal-gdi-safety-survey-wave4',
-			'type' => 'internal',
-			'layout' => 'single-answer',
-			'question' => 'ext-quicksurveys-internal-gdi-safety-survey-question',
-			'privacyPolicy' => 'ext-quicksurveys-internal-gdi-safety-survey-privacy-policy',
-			'answers' => [
-				'ext-quicksurveys-internal-gdi-safety-survey-answer-positive',
-				'ext-quicksurveys-internal-gdi-safety-survey-answer-negative',
-				'ext-quicksurveys-internal-gdi-safety-survey-answer-neutral',
-			],
-			'audience' => [
-				// T325136
-				'minEdits' => 5
-			],
-			'enabled' => true,
-			'coverage' => 0.2, // T325136
-			'platforms' => [
-				'desktop' => [ 'stable' ],
-				'mobile' => [ 'stable', 'beta' ],
-			],
-		],
-	],
-	'+fawiki' => [
-		[
-			// T325136
-			'name' => 'internal-gdi-safety-survey-wave4',
-			'type' => 'internal',
-			'layout' => 'single-answer',
-			'question' => 'ext-quicksurveys-internal-gdi-safety-survey-question',
-			'privacyPolicy' => 'ext-quicksurveys-internal-gdi-safety-survey-privacy-policy',
-			'answers' => [
-				'ext-quicksurveys-internal-gdi-safety-survey-answer-positive',
-				'ext-quicksurveys-internal-gdi-safety-survey-answer-negative',
-				'ext-quicksurveys-internal-gdi-safety-survey-answer-neutral',
-			],
-			'audience' => [
-				// T325136
-				'minEdits' => 5
-			],
-			'enabled' => true,
-			'coverage' => 0.2, // T325136
-			'platforms' => [
-				'desktop' => [ 'stable' ],
-				'mobile' => [ 'stable', 'beta' ],
-			],
-		],
-	],
-	'+enwiki' => [
-		[
-			// T325136
-			'name' => 'internal-gdi-safety-survey-wave4',
-			'type' => 'internal',
-			'layout' => 'single-answer',
-			'question' => 'ext-quicksurveys-internal-gdi-safety-survey-question',
-			'privacyPolicy' => 'ext-quicksurveys-internal-gdi-safety-survey-privacy-policy',
-			'answers' => [
-				'ext-quicksurveys-internal-gdi-safety-survey-answer-positive',
-				'ext-quicksurveys-internal-gdi-safety-survey-answer-negative',
-				'ext-quicksurveys-internal-gdi-safety-survey-answer-neutral',
-			],
-			'audience' => [
-				// T325136
-				'minEdits' => 5
-			],
-			'enabled' => true,
-			'coverage' => 0.03, // T325136
-			'platforms' => [
-				'desktop' => [ 'stable' ],
-				'mobile' => [ 'stable', 'beta' ],
-			],
-		],
-	],
+	]
 
 	// Note: make sure the wiki also has wmgUseQuickSurveys => true
 ],
