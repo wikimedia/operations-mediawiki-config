d71d5e0bf06c93f55ce9bca72e9829e872178c8f
Dreamy Jazz <dreamyjazzwikipedia@gmail.com>
2023-01-31 14:42:59 +0000

    Disable write old for CheckUserLog reason everywhere
    Bug: T233004
    Change-Id: I6cb2e151fa2e288918b136b96d853f5f1f74a375

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -25276,10 +25276,7 @@ return [
 	'group1' => SCHEMA_COMPAT_NEW,
 ],
 'wgCheckUserLogReasonMigrationStage' => [
-	'default' => SCHEMA_COMPAT_WRITE_BOTH | SCHEMA_COMPAT_READ_NEW,
-	'testwiki' => SCHEMA_COMPAT_NEW,
-	'group0' => SCHEMA_COMPAT_NEW,
-	'group1' => SCHEMA_COMPAT_WRITE_BOTH | SCHEMA_COMPAT_READ_NEW,
+	'default' => SCHEMA_COMPAT_NEW,
 ],
 
 // T324907
