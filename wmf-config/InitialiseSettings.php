bd1a248afe52cefd859b1bdf7da471a3d53776de
Kartik Mistry <kartik.mistry@gmail.com>
2023-02-22 08:56:03 +0530

    Content Translation: Set MT threshold to 45% for Kurdish WP
    Bug: T324941
    Change-Id: Ie039e9567d1bb2065983a5292d92e404cf03fbde

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -14399,6 +14399,7 @@ return [
 	'fywiki' => 90, // T323415
 	'hywiki' => 80, // T313208
 	'idwiki' => 70,
+	'kuwiki' => 45, // T324941
 	'mnwiki' => 89, // T319156
 	'tewiki' => 70,
 	'viwiki' => 90, // T275121
