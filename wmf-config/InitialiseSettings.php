5652faa662681c5fb391517ee9f05fbbf2d49398
Martin Urbanec <martin.urbanec@wikimedia.cz>
2023-03-21 22:59:52 +0100

    [Growth] eswiki: Enable mentorship for 35% newcomers
    This is a part of incremental mentorship rollout that happens
    at es.wikipedia. The target percentage is 50% at this point.
    
    Bug: T332737
    Bug: T285235
    Change-Id: Iff8cbd9ba9cf5ec033ce0092761f9dd1187c90ad

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -12463,7 +12463,7 @@ return [
 'wgGEMentorshipNewAccountEnablePercentage' => [
 	'default' => 100,
 	'enwiki' => 10, // T290927
-	'eswiki' => 15, // T285235
+	'eswiki' => 35, // T285235
 ],
 
 'wgGEHomepageDefaultVariant' => [
