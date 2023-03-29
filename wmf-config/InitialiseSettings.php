ec06dcb3ef8cabe84ab3e31471c0e081b3c28c4e
Jon Robson <jdlrobson@gmail.com>
2023-03-29 14:17:20 -0700

    Disable Vector js/css sharing on pl.wikipedia
    Bug: T332809
    Change-Id: I309490731ddd3bc12f212511a99a42be654cc7c4

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -2224,6 +2224,13 @@ return [
 	'desktop-improvements' => 'vector-2022',
 ],
 
+// T301212
+'wgVectorShareUserScripts' => [
+	'default' => true,
+	// T332809
+	'plwiki' => false,
+],
+
 // Note:
 // This is the list of namespaces the max-width should be EXCLUDED from.
 // It is not a list of namespaces where it should be included.
