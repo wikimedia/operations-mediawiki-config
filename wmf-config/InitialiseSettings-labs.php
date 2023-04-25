6f0fb800c0f88457541bd5be29d292bdb17d61e9
Amir Sarabadani <ladsgroup@gmail.com>
2023-04-25 13:51:57 +0200

    beta: Set externallinks migration stage to READ_NEW
    Bug: T335343
    Change-Id: Iff618bd96b6244e915658070e9833af49dde647c

--- wmf-config/InitialiseSettings-labs.php
+++ wmf-config/InitialiseSettings-labs.php
@@ -1431,7 +1431,7 @@ function wmfGetOverrideSettings() {
 		],
 
 		'-wgExternalLinksSchemaMigrationStage' => [
-			'default' => SCHEMA_COMPAT_WRITE_BOTH | SCHEMA_COMPAT_READ_OLD,
+			'default' => SCHEMA_COMPAT_WRITE_BOTH | SCHEMA_COMPAT_READ_NEW,
 		],
 
 		'-wgCommentTempTableSchemaMigrationStage' => [
