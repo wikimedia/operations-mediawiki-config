169aeac093890bafb1ed7181b32093e3d66f98b7
Gergő Tisza <tgr.huwiki@gmail.com>
2023-05-16 05:13:03 +0100

    [beta] GrowthExperiments: Use ActionApiImageRecommendationApiHandler
    Depends on $wgGEImageRecommendationServiceAccessToken being set
    in labs private repo.
    
    Soft-depends on I535dd7e14ee7c9bfd5becf0072dd7cf501533244
    being in production.
    
    Bug: T335641
    Bug: T337348
    Change-Id: Iec610e7ba9f95ee2669921b00574c838cb58f5ae

--- wmf-config/InitialiseSettings-labs.php
+++ wmf-config/InitialiseSettings-labs.php
@@ -1545,8 +1545,16 @@ function wmfGetOverrideSettings() {
 		],
 		'wgGEImageRecommendationApiHandler' => [
 			// Can be changed to 'production' when T306349 is resolved.
-			'default' => 'mvp'
+			'default' => 'actionapi',
 		],
+		'GEImageRecommendationServiceUrl' => [
+			'default' => 'https://en.wikipedia.org/w/api.php', // not super useful outside beta en but we have to set something
+			'ar' => 'https://ar.wikipedia.org/w/api.php',
+			'bn' => 'https://bn.wikipedia.org/w/api.php',
+			'cs' => 'https://cs.wikipedia.org/w/api.php',
+			'es' => 'https://es.wikipedia.org/w/api.php',
+		],
+		// $wgGEImageRecommendationServiceAccessToken is in private/PrivateSettings.php
 		'-wgGEDatabaseCluster' => [
 			'default' => false,
 		],
