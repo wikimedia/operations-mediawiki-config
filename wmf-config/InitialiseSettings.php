4c027c67d50b064a2484a0ac6aaf95d80b52b483
Ladsgroup <Ladsgroup@gmail.com>
2023-05-30 08:03:00 +0000

    Revert "Switch VisualEditor to not use RESTbase on small and medium wikis"
    This reverts commit e69e7f86438d38023c530f51ae0b0e8bc5efa1a5.
    
    Reason for revert: Do not +2 unless you're deploying it right after merge.
    
    Change-Id: Ibe2f1a55c4793a6d8ad8430eea27345f25ebead3

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -5809,8 +5809,6 @@ return [
 // by calling PHP functions.
 'wgVisualEditorDefaultParsoidClient' => [
 	'default' => 'vrs',
-	'small' => 'direct', // T320529
-	'medium' => 'direct', // T320529
 	'testwiki' => 'direct', // T320531
 	'mediawikiwiki' => 'direct', // T320529
 	'officewiki' => 'direct',
@@ -5820,12 +5818,9 @@ return [
 // Whether VisualEditor should bypass the MediaWiki layer and contact RESTBase directly for speed
 'wmgVisualEditorAccessRestbaseDirectly' => [
 	'default' => true,		// T100026
-	// Needs to be false if wgVisualEditorDefaultParsoidClient is 'direct' for now.
-	'testwiki' => false,
-	'small' => false,
-	'medium' => false,
-	'mediawikiwiki' => false,
-	'hewiki' => false,
+	'testwiki' => false,	// T320531 Needs to be false if wgVisualEditorDefaultParsoidClient is 'direct' for now.
+	'mediawikiwiki' => false,	// T320531 Needs to be false if wgVisualEditorDefaultParsoidClient is 'direct' for now.
+	'hewiki' => false,	// T320529 Needs to be false if wgVisualEditorDefaultParsoidClient is 'direct' for now.
 	'wikitech' => false,	# Wikitech uses local Parsoid operation
 	'closed' => false,		// T315383
 	'private' => false,		# Private wikis aren't yet supported by RESTBase
