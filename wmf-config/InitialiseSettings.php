a5b633f3a363109bdd0275453c6a552f143567fc
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2023-08-02 06:57:40 +0200

    enwiki: temp enable emergencyCaptcha
    Bug: T343294
    Change-Id: I081aa1fdd1d54276e0862f004a462b1d56125330

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -3586,6 +3586,7 @@ return [
 
 'wmgEmergencyCaptcha' => [
 	'default' => false,
+	'enwiki'  => true, // T343294 temporary enabling due to abuse or request of enwiki admins.
 ],
 # @} end of Captcha
 
