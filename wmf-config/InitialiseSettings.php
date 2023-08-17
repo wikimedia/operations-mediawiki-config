6a1d68071a05b8242121c460503bde3fa6afeaea
Daniel Souza <me@posix.dev.br>
2023-08-17 19:16:15 -0300

    Undeploy Research Incentive survey on enwiki
    Change-Id: I089fceaf4c67dc7bb0760a5c57da7272b7937eb8
    Bug: T336092

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -10918,7 +10918,6 @@ return [
 	'default' => false,
 	'cawiki' => true,
 	'dewiki' => true,
-	'enwiki' => true,
 	'enwikivoyage' => true,
 	'eswiki' => true,
 	'fawiki' => true,
@@ -10956,27 +10955,6 @@ return [
 			],
 			'privacyPolicy' => 'ext-quicksurveys-performance-internal-survey-privacy-policy',
 			'shuffleAnswersDisplay' => true,
-		],
-	],
-	'enwiki' => [
-		// T336092
-		[
-			'enabled' => true,
-			'type' => 'external',
-			'name' => 'research-incentive',
-			'question' => 'research-incentive-message',
-			'description' => 'research-incentive-description',
-			'coverage' => 0.002,
-			'platforms' => [
-				'desktop' => [ 'stable' ],
-				'mobile' => [ 'stable' ]
-			],
-			'audience' => [
-				/* Anglophone world: USA, Canada, UK, Ireland, Australia, New Zealand. */
-				'countries' => [ 'US', 'CA', 'GB', 'IE', 'AU', 'NZ' ],
-			],
-			'link' => 'research-incentive-link',
-			'privacyPolicy' => 'research-incentive-privacy',
 		],
 	],
 	'enwikivoyage' => [
