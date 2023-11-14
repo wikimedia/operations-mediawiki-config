137bdda0d97a43187a6b4b35e85c5987d9f01825
Amir Sarabadani <ladsgroup@gmail.com>
2023-11-14 17:47:35 +0100

    beta: Set pagelinks migration to read new
    Bug: T351237
    Change-Id: I055256bc7903a83094ce2552ff250d2a483ceace

--- wmf-config/InitialiseSettings-labs.php
+++ wmf-config/InitialiseSettings-labs.php
@@ -1450,7 +1450,7 @@ function wmfGetOverrideSettings() {
 		],
 
 		'-wgPageLinksSchemaMigrationStage' => [
-			'default' => SCHEMA_COMPAT_WRITE_BOTH | SCHEMA_COMPAT_READ_OLD,
+			'default' => SCHEMA_COMPAT_WRITE_BOTH | SCHEMA_COMPAT_READ_NEW,
 		],
 
 		'-wgAbuseFilterActorTableSchemaMigrationStage' => [
