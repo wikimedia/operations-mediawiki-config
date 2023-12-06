40c357b70b826597b0a6f8bcb3b80ffe70429eea
James D. Forrester <jforrester@wikimedia.org>
2023-12-06 10:15:00 -0500

    Beta Features: Drop Popups, deployed everywhere for ages
    Change-Id: I471108255185bdc40856ebe2267643bc923d2f5b

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -5661,7 +5661,6 @@ return [
 'wgBetaFeaturesAllowList' => [
 	'default' => [
 		'beta-feature-flow-user-talk-page',  // [Growth_] On-going (special permission) – Flow opt-in
-		'popups',                            // [WebTeam] 2019-10-04 – Page Previews
 		'popupsreferencepreviews',           // [WMDE TW] 2019-10-04 – Reference Previews
 		'cx',                                // [LangEng] 2019-11-07 - ContentTranslation
 		'twocolconflict',                    // [WMDE TW] 2019-10-30 – New edit conflict view
