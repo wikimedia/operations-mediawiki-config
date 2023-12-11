2571e71dd706ec5d9ba971fd4072612e05f694b5
Urbanecm <murbanec@wikimedia.org>
2023-12-11 11:31:52 +0000

    Revert "Growth: Enable Welcome survey user research for ar/en/es"
    This reverts commit 1ee9a9231c054336c8a3e62fb657e8078ebb69b6.
    
    Reason for revert: requested deployment period is over
    
    Bug: T351266
    Change-Id: I49f849e570799acf6470bd8b8e962a655fbd6526

--- wmf-config/ext-GrowthExperiments.php
+++ wmf-config/ext-GrowthExperiments.php
@@ -1206,59 +1206,6 @@ return [
 	],
 ],
 
-'wgWelcomeSurveyExperimentalGroups' => [
-	'default' => [
-		'control' => [
-			'percentage' => 100,
-		],
-	],
-	'arwiki' => [
-		'control' => [
-			'percentage' => 0,
-		],
-		'T342353_user_research' => [
-			'percentage' => 100,
-			'questions' => [
-				'reason',
-				'edited',
-				'email',
-				'languages',
-				'user-research',
-			],
-		],
-	],
-	'eswiki' => [
-		'control' => [
-			'percentage' => 0,
-		],
-		'T342353_user_research' => [
-			'percentage' => 100,
-			'questions' => [
-				'reason',
-				'edited',
-				'email',
-				'languages',
-				'user-research',
-			],
-		],
-	],
-	'enwiki' => [
-		'control' => [
-			'percentage' => 0,
-		],
-		'T342353_user_research' => [
-			'percentage' => 100,
-			'questions' => [
-				'reason',
-				'edited',
-				'email',
-				'languages',
-				'user-research',
-			],
-		],
-	],
-],
-
 'wgGECampaignTopics' => [
 	'default' => []
 ],
