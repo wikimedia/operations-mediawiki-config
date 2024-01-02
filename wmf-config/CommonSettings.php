3ddda182d8907a04cd7c25db60407c079905997e
Amir Sarabadani <ladsgroup@gmail.com>
2024-01-02 13:36:13 +0100

    Update virtual domain for url shortener
    Change-Id: Iba2629a45d8ee577524091f67ad22fae642d39ca

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -1385,11 +1385,8 @@ if ( $wmgUseUrlShortener ) {
 	wfLoadExtension( 'UrlShortener' );
 	$wgUrlShortenerTemplate = '/$1';
 	$wgUrlShortenerServer = 'https://w.wiki';
-	// TODO: Remove this after 20.10.2023
-	$wgUrlShortenerDBCluster = 'extension1';
-	$wgUrlShortenerDBName = 'wikishared';
-	// end of TODO
 	$wgVirtualDomainsMapping['urlshortener'] = [ 'cluster' => 'extension1', 'db' => 'wikishared' ];
+	$wgVirtualDomainsMapping['virtual-urlshortener'] = [ 'cluster' => 'extension1', 'db' => 'wikishared' ];
 	$wgUrlShortenerAllowedDomains = [
 		'(.*\.)?wikipedia\.org',
 		'(.*\.)?wiktionary\.org',
