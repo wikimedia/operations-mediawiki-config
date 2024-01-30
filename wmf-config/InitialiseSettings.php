b8e6160ac5cb03a7fc571b0c58fedbe9339a1f7c
superpes <superpes15.itwiki@gmail.com>
2024-01-30 11:57:18 +0100

    [ukwiki] Change autoconfirmed setting
    Set $wgAutoConfirmCount from default to 10 edits on Ukrainian Wikipedia
    
    Bug: T355972
    Change-Id: I2ea8f2ff918923e6d56437f95440c76b275e65b8

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -1892,6 +1892,7 @@ return [
 	'ptwiki' => 10, // T29954
 	'ruwiki' => 15, // T43831
 	'simplewiki' => 10,
+	'ukwiki' => 10, // T355972
 	'viwiki' => 10, // T322105
 	'wikidata' => 50, // T48461
 	'wuuwiki' => 10, // T122476
