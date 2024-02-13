44892c5de05ca44adf1ed600c2e34f6547cd2b4c
Timo Tijhof <krinkle@fastmail.com>
2024-02-13 18:58:04 +0000

    Profiler: Silence "RedisException: Connection timed out"
    We already do most profiling async and catch all exceptions to prevent
    impact on the web response. However, for ease of debugging in the
    future, we do still report it as warning to Logstash.
    
    Since timeouts are a known and acceptable thing to happen, silence
    these to prevent inactionable noise in Logstash.
    
    If timeouts were to become unacceptably common, we will find out via
    the alert we have on the StatsD metric. We do not rely on MediaWiki
    error logging to discover that, and its error logging provides no
    details that we don't already know.
    
    If an unknown/new issue were to happen, we do still log those, as its
    details may be important to investigating such future issues.
    
    Bug: T348756
    Change-Id: Ib57c980034b3da1fc282386f610240cb2cd218ba

--- src/Profiler.php
+++ src/Profiler.php
@@ -332,14 +332,17 @@ class Profiler {
 		} catch ( \Exception $e ) {
 			// Known failure scenarios:
 			//
-			// - "RedisException: read error on connection"
+			// - "RedisException: Connection timed out" (T206092, T348756)
 			//   Each publish() in the above loop writes data to Redis and
 			//   subsequently reads from the socket for Redis' response.
-			//   If any socket read takes longer than $timeout, it throws (T206092).
-			//   As of writing, this is rare (a few times per day at most),
-			//   which is considered an acceptable loss in profile samples.
+			//   When a socket read takes longer than $timeout, php-redis throws.
+			//   We catch these to avoid impacting the web response.
+			//   As of Feb 2024, these is rare (a few per day) which is an acceptable
+			//   loss for the milions of daily samples for flame graphs.
 			$error = 'exception';
-			trigger_error( get_class( $e ) . ': ' . $e->getMessage(), E_USER_WARNING );
+			if ( $e->getMessage() !== 'Connection timed out' ) {
+				trigger_error( get_class( $e ) . ': ' . $e->getMessage(), E_USER_WARNING );
+			}
 		}
 
 		if ( $error ) {
