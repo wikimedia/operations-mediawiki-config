3e0ac3d7e399adbf20d6e36200359634c82c6b08
daniel <dkinzler@wikimedia.org>
2024-04-10 17:52:11 +0200

    LogStash: log HtmlOutputRendererHelper channel
    Bug: T356157
    Change-Id: I97714e296c025fa2accb04ed0e35779610eb615f

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -2151,6 +2151,7 @@ return [
 		'GrowthExperiments' => 'info',
 		'headers-sent' => 'debug',
 		'HttpError' => 'error', // Only log http errors with a 500+ code T85795
+		'HtmlOutputRendererHelper' => 'debug', // T356157
 		'IpReputation' => 'debug',
 		'JobExecutor' => [ 'logstash' => 'warning' ],
 		'Kartographer' => 'debug', // Temporary monitoring for T304813.
