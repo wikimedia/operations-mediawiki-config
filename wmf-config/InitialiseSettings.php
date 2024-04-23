0566462cb189bc3b70f1aa5852e1596de3d0d4c1
Jon Robson <jdlrobson@gmail.com>
2024-04-23 11:43:09 -0700

    Enable night mode styles on Vector 2022 skin
    Bug: T362726
    Change-Id: I47a3875c3e3da1666962a3da2aee56f21087b9f7

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -10869,8 +10869,7 @@ return [
 
 'wgWikimediaStylesSkins'  => [
 	// [[phab:T361589]]
-	'default' => [ 'minerva' ],
-	'enwiki' => [ 'minerva', 'vector-2022' ],
+	'default' => [ 'minerva', 'vector-2022' ],
 ],
 
 // Which licensing/copyright message overrides to use in the WikimediaMessages extension
