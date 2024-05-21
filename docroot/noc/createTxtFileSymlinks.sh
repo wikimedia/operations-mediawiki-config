2c8f30c2a5c435eccfdc04baa74245bacb4bb461
Reedy <reedy@wikimedia.org>
2024-05-21 19:14:24 +0000

    Delete docroot/noc/createTxtFileSymlinks.sh
    Bug: T365514
    Change-Id: I393f6562fa3218925935b140536c837091064f67

--- docroot/noc/createTxtFileSymlinks.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# TODO: remove this file once we're sure we're not
-# executing it in some CI/script.
-echo "Nothing to do here. Please add files to docroot/noc/conf/filelist.php instead.";
