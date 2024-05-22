33f9e9d3e2af1661b3f438e6ab4721b737250d58
Moh'd Khier Abualruz <mkh.ruzz@live.com>
2024-05-22 21:17:59 +0200

    deploy(Popups): Make use of conditional user defaults
    Bug: T364347
    Change-Id: I015b8c62146308d2bb05f6470fb20cc35bd74500

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -2599,6 +2599,14 @@ if ( $wmgUseMultimediaViewer ) {
 
 if ( $wmgUsePopups ) {
 	wfLoadExtension( 'Popups' );
+	$wgConditionalUserOptions[ 'popups' ] = [
+		[ 1, [ CUDCOND_AFTER, '20170816000000' ] ],
+		[ $wgPopupsOptInDefaultState, [ CUDCOND_NAMED ] ],
+	];
+	$wgConditionalUserOptions[ 'popups-reference-previews' ] = [
+		[ 1, [ CUDCOND_AFTER, '20170816000000' ] ],
+		[ $wgPopupsOptInDefaultState, [ CUDCOND_NAMED ] ],
+	];
 }
 
 if ( $wmgUseLinter ) {
