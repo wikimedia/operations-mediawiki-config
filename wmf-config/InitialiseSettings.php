32697a960a814b84fb2d23eefabf545bfd2e52b7
Alexander Vorwerk <zabe@avorwerk.net>
2024-05-27 11:05:58 +0200

    Stop writing to af_user(_text)/afh_user(_text) in group1 wikis
    Bug: T337920
    Change-Id: Ic1aaa785bb93e84c17d6f0e771e04bea011e0d1c

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -11560,6 +11560,7 @@ return [
 	'test2wiki' => SCHEMA_COMPAT_NEW,
 	'testwikidatawiki' => SCHEMA_COMPAT_NEW,
 	'group0' => SCHEMA_COMPAT_NEW,
+	'group1' => SCHEMA_COMPAT_NEW,
 ],
 
 'wgXmlDumpSchemaVersion' => [
