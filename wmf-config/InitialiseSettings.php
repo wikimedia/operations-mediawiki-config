dfec7f31f0d937be214f18abcfcb496b4a96b59a
Anzx <anoop@mail-me.com>
2024-05-29 08:14:22 +0000

    commonswiki: Enable numeric wgCategoryCollation
    Bug: T362494
    Change-Id: I5aa7c74b38a8a613e61253c3ca8a96be79498a11

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -6520,6 +6520,7 @@ return [
 'wgCategoryCollation' => [
 	'default' => 'uppercase',
 	// Multilingual wikis
+	'commonswiki' => 'uca-default-u-kn', // T362494
 	'mediawikiwiki' => 'uca-default',
 	// Single-language wikis
 	'abwiki' => 'uppercase-ab', // T183430
