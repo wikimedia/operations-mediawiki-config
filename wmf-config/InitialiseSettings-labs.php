4121d0266a5b03611aeb5252c6270e5854c7a01e
Bartosz Dziewoński <dziewonski@fastmail.fm>
2024-06-18 16:12:52 +0000

    Revert "Show experimental login popup links on the beta cluster"
    This reverts commit 1c7f919714d2037f090334b72d88985d3b3ca229.
    
    Bug: T367891
    Change-Id: Ia8f01beaf0c00389ca234ac8d5107c42185cda49

--- wmf-config/InitialiseSettings-labs.php
+++ wmf-config/InitialiseSettings-labs.php
@@ -2287,17 +2287,5 @@ function wmfGetOverrideSettings() {
 			'default' => false, // NOTE: Do not enable without OK from Moderator Tools team.
 			'enwiki' => true,
 		],
-
-		// T366486
-		// Do not enable this in production
-		'wgExperimentalLoginPopup' => [
-			'default' => true,
-		],
-		// T366486 (needed for wgExperimentalLoginPopup iframe mode)
-		// Do not enable this in production
-		'wgEditPageFrameOptions' => [
-			'default' => 'SAMEORIGIN',
-		],
-
 	];
 } # wmfGetOverrideSettings()
