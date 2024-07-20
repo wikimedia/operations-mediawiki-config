ce7cf66502724367b3fba538754328d9ca3f0e14
Ferran Tufan <southparkfan223@hotmail.com>
2024-07-20 16:01:59 +0200

    LabsServices: replace parsoid endpoint
    Bug: T361386
    Change-Id: I6a058dda0030b1b278c1ce0114f89d108c76a043

--- wmf-config/LabsServices.php
+++ wmf-config/LabsServices.php
@@ -43,7 +43,7 @@ return [
 		// No parser cache DBs in beta yet
 		'parsercache-dbs' => [],
 		'urldownloader' => 'http://deployment-urldownloader03.deployment-prep.eqiad1.wikimedia.cloud:8080',
-		'parsoid' => 'http://deployment-parsoid12.deployment-prep.eqiad1.wikimedia.cloud/w/rest.php',
+		'parsoid' => 'http://parsoid.svc.deployment-prep.eqiad1.wikimedia.cloud/w/rest.php',
 		'mathoid' => 'http://deployment-docker-mathoid02.deployment-prep.eqiad1.wikimedia.cloud:10044',
 		'eventgate-analytics' => 'http://deployment-eventgate-4.deployment-prep.eqiad1.wikimedia.cloud:8192',
 		'eventgate-analytics-external' => 'http://deployment-eventgate-4.deployment-prep.eqiad1.wikimedia.cloud:8492',
