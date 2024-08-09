a28bf37e2ed12843dc9a20567597b426ed7c744e
Alexander Vorwerk <zabe@avorwerk.net>
2024-08-09 16:34:06 +0200

    beta: Introduce encrypted PBKDF2
    Change-Id: Ibd1d56c08ee249c827a2e39558fb3920598ffe04

--- wmf-config/CommonSettings-labs.php
+++ wmf-config/CommonSettings-labs.php
@@ -552,5 +552,16 @@ if ( $wmgRealm == 'labs' ) {
 			],
 		];
 	}
+
+	$wgPasswordConfig['EP'] = [
+		'class' => EncryptedPassword::class,
+		'underlying' => 'pbkdf2',
+		'secrets' => [ $wmgPasswordSecretKey ],
+		'cipher' => 'aes-256-cbc',
+	];
+	$wgPasswordConfig['BEP'] = [
+		'class' => LayeredParameterizedPassword::class,
+		'types' => [ 'B', 'EP' ],
+	];
 }
 // end safeguard
