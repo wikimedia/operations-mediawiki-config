390f70f4ee5d9ce7e271fa0f920363030c71b66f
Erik Bernhardson <ebernhardson@wikimedia.org>
2024-08-15 09:41:56 -0700

    cirrus: Stop general writes to private wikis
    The replacement streaming updater is now performing writes to
    private wikis. Disable the writes coming from cirrus as they
    are no longer necessary.
    
    Bug: T341332
    Change-Id: Ia7fa7dd4870779b0019bbaa3451c16b809825985

--- wmf-config/ext-CirrusSearch.php
+++ wmf-config/ext-CirrusSearch.php
@@ -32,7 +32,12 @@ return [
 	],
 	// wikitech is not using kafka (T192361) should be re-evaluated when T237773 is adressed
 	'labswiki' => [ 'eqiad', 'codfw', 'cloudelastic' ],
-	'private' => [ 'eqiad', 'codfw' ],
+	// private wikis don't write to cloudelastic
+	'private' => [
+		'default' => [],
+		'archive' => [ 'eqiad', 'codfw' ],
+		'weighted_tags' => [ 'eqiad', 'codfw' ],
+	],
 ],
 
 'wgCirrusSearchReplicaGroup' => [
