93241ca59a7cd3969c5e071603a93435c07f5f96
Amir Sarabadani <ladsgroup@gmail.com>
2024-08-28 23:42:53 +0200

    Remove the "powered by mediawiki" override
    This was needed during the transition period where mw's footer icon
    would have looked weird but now that the new footer is deployed
    everywhere, it's fine to remove the override.
    
    Also remove the old png files, we kept them because of client side
    caching or cdn caches but now enough time passed that we don't need them
    anymore, may them rest in peace.
    
    Change-Id: I90f3476e08771d1902f2a7864ebfa827be73981e

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -1729,12 +1729,6 @@ $wgFooterIcons['copyright']['copyright'] =
 	'href="https://wikimediafoundation.org/" target="https://wikimediafoundation.org/"><img src="' .
 	$wmgWikimediaIcon['svg'] . '" width="84" height="29" alt="Wikimedia Foundation"></a>';
 
-$wgFooterIcons['poweredby'] = [ 'mediawiki' =>
-	'<a class="cdx-button cdx-button--fake-button cdx-button--size-large cdx-button--fake-button--enabled" ' .
-	'style="padding-left: 8px; padding-right: 8px;" ' .
-	'href="https://www.mediawiki.org" target="https://www.mediawiki.org"><img src="' .
-	$wmgPoweredByMediaWikiIcon['svg'] . '" width="84" height="29" alt="Powered by MediaWiki"></a>' ];
-
 # :SEARCH:
 
 # All wikis are special and get Cirrus :)
