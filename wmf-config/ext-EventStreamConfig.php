412b064eddb2069bab462a62ec2db72330068942
Dmitry Brant <me@dmitrybrant.com>
2024-11-22 13:34:21 -0500

    New stream config for Android Rabbit Holes feature.
    Bug: T380107
    Change-Id: Ie12ee1136bed962b709d84d09a9d291f821b6a1d

--- wmf-config/ext-EventStreamConfig.php
+++ wmf-config/ext-EventStreamConfig.php
@@ -679,6 +679,10 @@ return [
 			'schema_title' => 'analytics/mobile_apps/app_interaction',
 			'destination_event_service' => 'eventgate-analytics-external',
 		],
+		'app_rabbit_holes' => [
+			'schema_title' => 'analytics/mobile_apps/app_interaction',
+			'destination_event_service' => 'eventgate-analytics-external',
+		],
 		'ios.edit_history_compare' => [
 			'schema_title' => 'analytics/mobile_apps/ios_edit_history_compare',
 			'destination_event_service' => 'eventgate-analytics-external',
