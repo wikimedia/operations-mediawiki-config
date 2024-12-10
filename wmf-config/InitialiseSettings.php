c00d312b328b5d9001adf1261ca691460f386488
anwon <iramosu@protonmail.com>
2024-12-10 16:36:37 +0300

    [enwikinews] & [hewikinews] & [plwikinews]: Upgrade license to CC BY 4.0
    InitialiseSettings.php: Upgrade the license of enwikinews,
    hewikinews, and plwikinews from Creative Commons Attribution
    2.5 (CC BY 2.5) to Creative Commons Attribution 4.0 (CC BY 4.0)
    per communityconsensus
    
    Bug: T381421
    Change-Id: Ic71d6fbadab2f489dd37f9c47afc2ce2354bf144

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -11125,15 +11125,21 @@ return [
 	// For unrecognized language codes, creativecommons.org falls back to English
 	'default' => 'https://creativecommons.org/licenses/by-sa/4.0/deed.$lang',
 	'arwikinews' => 'https://creativecommons.org/licenses/by-sa/4.0/deed.ar',
+	'enwikinews' => 'https://creativecommons.org/licenses/by/4.0/',
+	'hewikinews' => 'https://creativecommons.org/licenses/by/4.0/',
+	'huwikinews' => 'https://creativecommons.org/licenses/by/3.0/',
+	'plwikinews' => 'https://creativecommons.org/licenses/by/4.0/',
 	// Don't try to link to weird things like /deed.meta or /deed.commons
 	'special' => 'https://creativecommons.org/licenses/by-sa/4.0/',
-	'huwikinews' => 'https://creativecommons.org/licenses/by/3.0/',
 	'wikinews' => 'https://creativecommons.org/licenses/by/2.5/',
 ],
 'wgRightsText' => [
 	'default' => 'Creative Commons Attribution-Share Alike 4.0',
 	'arwikinews' => 'Creative Commons Attribution-Share Alike 4.0',
+	'enwikinews' => 'Creative Commons Attribution 4.0',
+	'hewikinews' => 'Creative Commons Attribution 4.0',
 	'huwikinews' => 'Creative Commons Attribution 3.0',
+	'plwikinews' => 'Creative Commons Attribution 4.0',
 	'wikinews' => 'Creative Commons Attribution 2.5',
 	// Wikidata footer, T112606
 	'wikidata' => 'All structured data from the main and property namespace is available under ' .
