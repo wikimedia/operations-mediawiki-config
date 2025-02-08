3e12ad2cb3f070aecf25664ca1656b23ff0f9861
Ori Livneh <ori.livneh@gmail.com>
2025-02-08 11:03:44 -0500

    Remove temporary '-php8' and '-k8s' suffixes from ArcLamp pipeline
    This change removes the conditional logic that appended -k8s or -php8 to
    the excimer Redis channel (T288165). With the migrations to Kubernetes
    and PHP8 both complete, there is no longer a need to distinguish these
    samples. A follow-up change to operations/puppet will decommission the
    k8s- and php8-specific ArcLamp consumers.
    
    Bug: T391516
    Change-Id: I138a591224f2899236bf1349a758d99bf9e5e343

--- src/Profiler.php
+++ src/Profiler.php
@@ -226,22 +226,7 @@ class Profiler {
 		$cpuProf->setMaxDepth( 250 );
 		$realProf->setMaxDepth( 250 );
 
-		// The excimer-k8s definitions are temporary, to assist with migration
-		// (T288165).  We unfortunately have to duplicate the logic for
-		// $wmgUsingKubernetes from CommonSettings.php, since this file is loaded
-		// before that one.
-		// grep: excimer-k8s, excimer-wall, excimer-k8s-wall
 		$redisChannel = 'excimer';
-		if ( ClusterConfig::getInstance()->isK8s() ) {
-			$redisChannel .= '-k8s';
-		}
-	   // Temporarily route PHP 8 profiles to a separate set of sinks
-	   // (excimer-k8s-php8, excimer-k8s-php8-wall) during the migration to
-	   // PHP 8, to facilitate comparison and diffing with PHP 7 profiles
-	   // (T385199).
-		if ( PHP_MAJOR_VERSION === 8 ) {
-			$redisChannel .= '-php8';
-		}
 
 		// The period is 60s, so there's no point waiting for more samples to arrive
 		// before the end of the request, they probably won't.
