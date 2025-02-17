00d1d78501a903e51481f4ca7f4f47fcd619e94b
Michael Große <mgrosse@wikimedia.org>
2025-02-17 21:25:51 +0100

    beta(Growth): enable Surfacing Add Link also on beta-cswiki
    beta-enwiki also has the Add Link ramp-up enabled, like production and
    that makes the Surfacing feature harder to test. So enable that also on
    one of our other usual beta-testing wikis.
    
    Change-Id: I6ad3000792b793d9f9c653b9fba66536831c59c9

--- wmf-config/InitialiseSettings-labs.php
+++ wmf-config/InitialiseSettings-labs.php
@@ -1421,6 +1421,7 @@ function wmfGetOverrideSettings() {
 			'viwiki' => null,
 		],
 		'wgGESurfacingStructuredTasksEnabled' => [
+			'cswiki' => true,
 			'enwiki' => true,
 		],
 		'wgGESurfacingStructuredTasksReadModeUpdateEnabled' => [
