a88c7716b1bebe627db78ea146b242b5c83a974b
Zoë <zoe@wikimedia.org>
2025-03-11 14:36:49 +0000

    Remove Flow as the default talk system
    I believe DiscussionTools is the default in the absence of Flow.
    
    Bug: T383569
    Change-Id: I1718e65ad92395c662bcf757e2f073f0f4cc3bce

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -8006,57 +8006,6 @@ return [
 
 'wmgFlowNamespaces' => [
 	'default' => [],
-	'officewiki' => [
-		NS_TALK,
-		NS_USER_TALK,
-		NS_PROJECT_TALK,
-		NS_FILE_TALK,
-		NS_MEDIAWIKI_TALK,
-		NS_TEMPLATE_TALK,
-		NS_HELP_TALK,
-		NS_CATEGORY_TALK,
-		91, // Thread_talk from LQT
-		93, // Summary_talk from LQT
-		101, // Report_talk
-		111, // Outreach_talk
-		113, // Archive_talk
-		829, // Module_talk from Scribunto
-		2301, // Gadget_talk from Gadgets
-		2303, // Gadget_definition_talk from Gadgets
-		5501, // T176199 Newsletter talk from Newsletter extension
-	],
-	'kabwiki' => [
-		NS_USER_TALK, // T140588
-	],
-	'sewikimedia' => [
-		NS_TALK,
-		NS_USER_TALK,
-		NS_PROJECT_TALK,
-		NS_FILE_TALK,
-		NS_MEDIAWIKI_TALK,
-		NS_TEMPLATE_TALK,
-		NS_HELP_TALK,
-		NS_CATEGORY_TALK,
-		91, // Thread_talk from LQT
-		93, // Summary_talk from LQT
-		101, // Projektdiskussion
-		829, // Module_talk from Scribunto
-		2301, // Gadget_talk from Gadgets
-		2303, // Gadget_definition_talk from Gadgets
-	],
-	'cawikiquote' => [
-		NS_TALK,
-		NS_USER_TALK,
-		NS_PROJECT_TALK,
-		NS_FILE_TALK,
-		NS_MEDIAWIKI_TALK,
-		NS_TEMPLATE_TALK,
-		NS_HELP_TALK,
-		NS_CATEGORY_TALK,
-		829, // Module_talk from Scribunto
-		2301, // Gadget_talk from Gadgets
-		2303, // Gadget_definition_talk from Gadgets
-	],
 ],
 
 // Enable editing other users posts by autoconfirmed
