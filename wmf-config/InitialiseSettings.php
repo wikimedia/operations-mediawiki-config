d4e1c561e4216e512d58530326629f8f4821102b
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2025-03-13 13:53:44 +0100

    Reapply "Make WikibaseQualityConstraints use split-graph query service"
    This reverts commit 8154982d5e027fd7a2e0417aae4416a2e9bf5339.
    
    Bug: T374021
    Change-Id: Id272053c93db594c16e195bd829a33821f8ccc94
    Depends-On: I72365edf417d35a72326b77b68f98a021860948e

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -9828,7 +9828,11 @@ return [
 ],
 
 'wgWBQualityConstraintsSparqlEndpoint' => [
-	'default' => 'http://localhost:6009/sparql',
+	'default' => 'http://localhost:6041/sparql',
+],
+
+'wgWBQualityConstraintsAdditionalSparqlEndpoints' => [
+	'default' => [ 'http://localhost:6042/sparql' ],
 ],
 
 'wgWBQualityConstraintsSparqlMaxMillis' => [
