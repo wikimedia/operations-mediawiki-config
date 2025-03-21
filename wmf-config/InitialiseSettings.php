13c6c0ee07f3d8225540752ac73417dcab29cce3
Santiago Faci <sfaci@wikimedia.org>
2025-03-21 14:59:22 +0100

    [Experiment Platform] Disable test experiment
    This patch disable ExtLabTest1 experiment and the related module everywhere
    
    Bug: T383801
    Change-Id: I22b6e7817c7d410ccacfb89878294fba8afb66d7

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -10694,13 +10694,6 @@ return [
 	'wikitech' => true,
 ],
 
-// Enables a test module for verifying experiment enrollment.
-// This config var is temporary - it will be removed in T383801
-'wgWMEExLabTest1Enabled' => [
-	'default' => false,
-	'testwiki' => true,
-],
-
 // Disable redirects for search inputs to exact matches by default
 'wmgDoNotRedirectOnSearchMatch' => [
 	'default' => false,
