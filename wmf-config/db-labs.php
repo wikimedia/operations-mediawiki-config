c604b9795f7a495f5e896b08ded70d036757618c
Amir Sarabadani <ladsgroup@gmail.com>
2025-03-24 11:53:16 +0100

    beta: Fix mainstash config
    Bug: T387654
    Change-Id: I98587430aed69578bcb16937ca7ab732ab3a7d5c

--- wmf-config/db-labs.php
+++ wmf-config/db-labs.php
@@ -62,13 +62,6 @@ if ( $wmgRealm == 'labs' ) {
 				// deployment-db14.deployment-prep.eqiad1.wikimedia.cloud
 				'172.16.5.170:3306' => 3,
 			],
-
-			'extension2' => [
-				// deployment-db11.deployment-prep.eqiad1.wikimedia.cloud, master
-				'172.16.5.150:3306' => 1,
-				// deployment-db14.deployment-prep.eqiad1.wikimedia.cloud
-				'172.16.5.170:3306' => 3,
-			]
 		],
 
 		'templateOverridesByCluster' => [
@@ -79,6 +72,13 @@ if ( $wmgRealm == 'labs' ) {
 
 	$wgDefaultExternalStore = [
 		'DB://cluster1',
+	];
+
+	$wmgMainStashServers = [
+		// deployment-db11.deployment-prep.eqiad1.wikimedia.cloud
+		'ms1' => '172.16.5.150',
+		// deployment-db14.deployment-prep.eqiad1.wikimedia.cloud
+		'ms2' => '172.16.5.170',
 	];
 
 // end safe guard
