b6c46ca72e4fabd949e9166e3d3cd22fc15de70b
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2025-04-07 15:23:46 +0200

    Remove unused EntitySchema config
    The code now uses the equivalent of the `true` setting unconditionally.
    
    Bug: T371196
    Change-Id: I5ae9f52b8074e121b7226096acc186f95e560c8b
    Depends-On: I43324b66b7381abb061ef0d73f046b24b5530327

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -7848,11 +7848,6 @@ return [
 	'testwikidatawiki' => [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 40, 42, 123, 570, 734, 735, 999, 1234, 3300, 11424, 12345 ],
 ],
 
-'wgEntitySchemaTmpFixRdfUri' => [
-	'wikidatawiki' => true,
-	'testwikidatawiki' => true,
-],
-
 'wmgUseFlow' => [
 	'default' => false,
 	'flow' => true,
