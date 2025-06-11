6fc702312c043c279d855cc83d4f4217a81fb2e6
Bking <bking@wikimedia.org>
2025-06-11 15:37:31 +0000

    Revert "search: Return traffic to all DCs"
    This reverts commit 6e9b9f3dfc982d65edf4d105c63ea00169c40500.
    
    Reason for revert: accidentally hit button
    
    Change-Id: I7f2176f9aa1f33c890acd2a0d07f7c5284354be1

--- wmf-config/ext-CirrusSearch.php
+++ wmf-config/ext-CirrusSearch.php
@@ -12,11 +12,11 @@ return [
 // $wgTranslateTranslationDefaultService in CommonSettings.php if you plan to
 // bring down a specific cluster.
 'wgCirrusSearchDefaultCluster' => [
-	'default' => 'local',
+	'default' => 'codfw',
 ],
 // Kept for BC with SRE tools that checks siteinfo (see APIQuerySiteInfoGeneralInfo in CommonSettings.php)
 'wmgCirrusSearchDefaultCluster' => [
-	'default' => $GLOBALS['wmgDatacenter'],
+	'default' => 'codfw',
 ],
 
 'wgCirrusSearchClusterOverrides' => [
