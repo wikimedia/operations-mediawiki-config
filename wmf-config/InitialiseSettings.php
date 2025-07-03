d5eb9b86c828b560dcb57b25e3b1e5673979c20f
Alexander Vorwerk <zabe@avorwerk.net>
2025-07-03 22:49:57 +0200

    Set categorylinks to read new on small wikis
    Bug: T397912
    Change-Id: Ibd4e212670ee199232d055a2c6c9c30edf5e135a

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -11771,6 +11771,7 @@ return [
 'wgCategoryLinksSchemaMigrationStage' => [
 	'default' => SCHEMA_COMPAT_WRITE_BOTH | SCHEMA_COMPAT_READ_OLD,
 	'group0' => SCHEMA_COMPAT_WRITE_BOTH | SCHEMA_COMPAT_READ_NEW,
+	'small' => SCHEMA_COMPAT_WRITE_BOTH | SCHEMA_COMPAT_READ_NEW,
 ],
 
 'wgXmlDumpSchemaVersion' => [
