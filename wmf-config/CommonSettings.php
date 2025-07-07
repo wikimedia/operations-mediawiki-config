e043b95ed72fcebbeaa561c976a4484befd9a5e4
Ladsgroup <Ladsgroup@gmail.com>
2025-07-07 12:12:15 +0000

    Revert "Increase max db connection count before circuit breaking"
    This reverts commit e872ee87ab3f7f30f2f11607cf3bb3a65df15126.
    
    Reason for revert: Root cause has been fixed
    
    Bug: T398692
    Change-Id: I0e2dd99981f004fde0efadd4f8caedce124f1168

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -401,7 +401,7 @@ if ( $wmgRealm === 'production' ) {
 		$wgLBFactoryConf['loadMonitor']['class'] = '\Wikimedia\Rdbms\LoadMonitorNull';
 	}
 	// T360930
-	$wgLBFactoryConf['loadMonitor']['maxConnCount'] = 450;
+	$wgLBFactoryConf['loadMonitor']['maxConnCount'] = 350;
 
 	// Dumps use their own set of databases
 	if ( ClusterConfig::getInstance()->isDumps() ) {
