63cbb6ebffa5269a85823ae4da61436a30828ff0
Zabe <zabe@avorwerk.net>
2025-07-15 20:21:15 +0000

    Revert^2 "initialiseSettings: set wgSecurePollUseMediaWikiNamespace = true for enwiki"
    This reverts commit 9f65696477549f0c8c3d17733d46da5a259165ee.
    
    Reason for revert: no longer needed
    
    Change-Id: I83669f50498174e4c989c2d643b4b4b066ab9b97

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -10781,6 +10781,11 @@ return [
 	'officewiki' => true,
 ],
 
+'wgSecurePollUseMediaWikiNamespace' => [
+	'default' => false,
+	'enwiki' => true, // T398080
+],
+
 // Enable SecurePoll logging
 'wgSecurePollUseLogging' => [
 	'default' => false,
