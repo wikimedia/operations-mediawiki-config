310508e1f945fada87d1fd40f90b5c5ae7ef1e1e
Daniel Souza <me@posix.dev.br>
2025-07-20 19:37:49 -0300

    Deploy Readers Use Cases Survey v2
    Bug: T399736
    Change-Id: I53d12ca5b44690b3e11082ec3dc658ae5908fae1

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -11415,7 +11415,7 @@ return [
 			'name' => 'reader-foundational-oe-survey',
 			'type' => 'external',
 			'enabled' => true,
-			'coverage' => 0,
+			'coverage' => 0.0025,
 			'privacyPolicy' => 'reader-foundational-oe-survey-privacy-policy',
 			'platforms' => [
 				'desktop' => [ 'stable' ],
