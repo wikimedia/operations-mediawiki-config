a4ced8a179b8b492ed750d3ee79216c1fdf0d321
MusikAnimal <musikanimal@gmail.com>
2025-11-25 23:20:50 +0000

    Revert "[metawiki] enable voting on entities with the 'Under review' status"
    This reverts commit 65194cd61520c5d3ea6aacc50b356ba4ff7acfc9.
    
    Reason for revert: did not work!
    
    Change-Id: I6d958e4d83e980810b172b76a70a41ee4aa8b12c

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -4918,7 +4918,6 @@ if ( $wmgUseCommunityRequests ) {
 			'db' => false,
 		];
 	}
-	$wgCommunityRequestsStatuses['under-review']['voting'] = true; // T409613
 }
 
 // This is a temporary hack for hooking up Parsoid/PHP with MediaWiki
