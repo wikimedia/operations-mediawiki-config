63e08e262aef9a8e9b412f684cc215126e6c18b6
Alexander Vorwerk <zabe@avorwerk.net>
2025-12-10 00:05:47 +0100

    BETA: Start reading from file table
    Bug: T412164
    Change-Id: Ia3a9c545b5077948c2b9a6a88b7c5b70c28dd812

--- wmf-config/InitialiseSettings-labs.php
+++ wmf-config/InitialiseSettings-labs.php
@@ -1281,7 +1281,7 @@ function wmfGetOverrideSettings() {
 		],
 
 		'-wgFileSchemaMigrationStage' => [
-			'default' => SCHEMA_COMPAT_WRITE_BOTH | SCHEMA_COMPAT_READ_OLD,
+			'default' => SCHEMA_COMPAT_WRITE_BOTH | SCHEMA_COMPAT_READ_NEW,
 		],
 
 		'-wgImageLinksSchemaMigrationStage' => [
