aea8a105751716927cb9845fbcb42061938a79c3
Alexander Vorwerk <zabe@avorwerk.net>
2026-01-13 22:12:35 +0100

    Start writing to il_target_id on group0 wikis
    Bug: T413526
    Change-Id: I0b9013294e6af4fafd58461858f75270bfc3a4b4

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -11952,7 +11952,7 @@ return [
 // T299953
 'wgImageLinksSchemaMigrationStage' => [
 	'default' => SCHEMA_COMPAT_OLD,
-	'testwiki' => SCHEMA_COMPAT_WRITE_BOTH | SCHEMA_COMPAT_READ_OLD,
+	'group0' => SCHEMA_COMPAT_WRITE_BOTH | SCHEMA_COMPAT_READ_OLD,
 ],
 
 'wgXmlDumpSchemaVersion' => [
