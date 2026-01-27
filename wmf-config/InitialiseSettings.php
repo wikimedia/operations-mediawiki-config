177b31204283f8aa4d02f8c818e92a6d1e86bb93
Dreamy Jazz <wpgbrown@wikimedia.org>
2026-01-27 16:19:20 +0000

    CheckUser: Read new for user agent table migration everywhere
    Bug: T361199
    Change-Id: I3d937efc3f81b33ab50ea7cb1d7a80dba863f522

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -7303,9 +7303,7 @@ return [
 
 // T361139
 'wgCheckUserUserAgentTableMigrationStage' => [
-	'group0' => SCHEMA_COMPAT_READ_NEW | SCHEMA_COMPAT_WRITE_BOTH,
-	'group1' => SCHEMA_COMPAT_READ_NEW | SCHEMA_COMPAT_WRITE_BOTH,
-	'default' => SCHEMA_COMPAT_OLD | SCHEMA_COMPAT_WRITE_BOTH,
+	'default' => SCHEMA_COMPAT_READ_NEW | SCHEMA_COMPAT_WRITE_BOTH,
 ],
 
 'wmgUseUserInfoCard' => [
