6df41f1662e4c14681e205ee648f547daa7fb3f7
Dreamy Jazz <wpgbrown@wikimedia.org>
2026-02-09 12:33:55 +0000

    Stop writing old for CheckUser user agent table migration everywhere
    Bug: T361206
    Change-Id: Ife4a91239a12ae5afed5e429f1b312b8f0e46d22

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -7320,9 +7320,7 @@ return [
 
 // T361139
 'wgCheckUserUserAgentTableMigrationStage' => [
-	'default' => SCHEMA_COMPAT_READ_NEW | SCHEMA_COMPAT_WRITE_BOTH,
-	'group0' => SCHEMA_COMPAT_NEW,
-	'group1' => SCHEMA_COMPAT_NEW,
+	'default' => SCHEMA_COMPAT_NEW,
 ],
 
 'wmgUseUserInfoCard' => [
