fedbd087f7971620fc68a2de61a56294d18b69a3
gkyziridis <gkyziridis@wikimedia.org>
2026-04-01 14:00:25 +0200

    EventStreamConfig: Add rr-multilingual prediction_change stream
    This is the revertrisk-multilingual prediction of an article revesion using the rr-multilingual model.
    Configuration of the event stream: mediawiki.page_revert_risk_multilingual_prediction_change.v1
    
    Bug: T415892
    Change-Id: I5d70864c06474a967db1f8cad85ceab7dbb6753a

--- wmf-config/ext-EventStreamConfig.php
+++ wmf-config/ext-EventStreamConfig.php
@@ -2423,6 +2423,10 @@ return [
 			'schema_title' => 'mediawiki/page/prediction_classification_change',
 			'destination_event_service' => 'eventgate-main',
 		],
+		'mediawiki.page_revert_risk_multilingual_prediction_change.v1' => [
+			'schema_title' => 'mediawiki/page/prediction_classification_change',
+			'destination_event_service' => 'eventgate-main',
+		],
 		'mediawiki.revision-tags-change' => [
 			'schema_title' => 'mediawiki/revision/tags-change',
 			'destination_event_service' => 'eventgate-main',
