2af238a72ee71a7914faf50aecae8b71f2f56493
C. Scott Ananian <cscott@cscott.net>
2026-04-07 18:20:52 -0400

    Turn on Parsoid Read Views for eswiki
    eswiki is responsible for ~5% of total page views, which is around the
    limit for what we're comfortable deploying in one go.
    
    Bug: T422524
    Change-Id: I30adf5081b509fd902659a189d1ef797b930c373

--- dblists-index.php
+++ dblists-index.php
@@ -259,7 +259,7 @@ return [
 'eowikisource' => [ 'small', 'wikidataclient', 'visualeditor-nondefault', 'commonsuploads', 'group1', 'nowikidatadescriptiontaglines', 'sul' ],
 'eowikivoyage' => [ 'small', 'wikidataclient', 'commonsuploads', 'group1', 'parsoidrendered', 'sul' ],
 'eowiktionary' => [ 'medium', 'wikidataclient', 'visualeditor-nondefault', 'commonsuploads', 'group1', 'nowikidatadescriptiontaglines', 'wikifunctionsclient', 'parsoidrendered', 'sul' ],
-'eswiki' => [ 'wikipedia', 'footer-legal-contact-link', 'wikidataclient', 'cirrussearch-big-indices', 'growthexperiments', 'ores', 'sul', 'wikilove', 'automoderator-revertrisk-multilingual' ],
+'eswiki' => [ 'wikipedia', 'footer-legal-contact-link', 'wikidataclient', 'cirrussearch-big-indices', 'growthexperiments', 'parsoidrendered', 'ores', 'sul', 'wikilove', 'automoderator-revertrisk-multilingual' ],
 'eswikibooks' => [ 'medium', 'wikidataclient', 'visualeditor-nondefault', 'group1', 'nowikidatadescriptiontaglines', 'cirrussearch-big-indices', 'ores', 'sul' ],
 'eswikinews' => [ 'flaggedrevs', 'medium', 'wikidataclient', 'visualeditor-nondefault', 'group1', 'nowikidatadescriptiontaglines', 'sul' ],
 'eswikiquote' => [ 'medium', 'wikidataclient', 'group1', 'ores', 'sul' ],
