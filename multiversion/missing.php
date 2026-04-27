3976312f1f136aa757dbcd68046114529035055b
Jon Harald Søby <jhsoby@gmail.com>
2026-04-27 13:30:06 +0200

    missing.php: Fix Wikiversity logo and improve dark mode
    Fix the logo that was broken by thumbnail size changes, and
    improve dark mode support by adding a dark background for the
    footer, and aligning colors with current Codex colors.
    
    Bug: T424511
    Bug: T424512
    Change-Id: I33b80b9f5e12bd55060f42dd78a6331201f9f4a5

--- multiversion/missing.php
+++ multiversion/missing.php
@@ -250,7 +250,7 @@ function wmfHandleMissingWiki() {
 		// Wikiversity.
 		$escLanguage = htmlspecialchars( $language );
 		wmfShowErrorPage( [
-			'logo' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Wikiversity-logo.svg/300px-Wikiversity-logo.svg.png',
+			'logo' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Wikiversity-logo.svg/330px-Wikiversity-logo.svg.png',
 			'favicon' => 'https://beta.wikiversity.org/favicon.ico',
 			'title' => 'This wiki does not exist',
 			'heading' => 'Welcome to Wikiversity',
@@ -319,7 +319,7 @@ function wmfShowErrorPage( array $info ) {
 * { margin: 0; padding: 0; }
 body { background: #fff; color: #202122; font: 0.938em/1.6 sans-serif; }
 .content { margin: 7% auto 0; padding: 2em 1em 1em; max-width: 640px; }
-.footer { clear: both; margin-top: 14%; border-top: 1px solid #e5e5e5; background: #f9f9f9; padding: 2em 0; font-size: 0.8em; text-align: center; }
+.footer { clear: both; margin-top: 14%; border-top: 1px solid #dadde3; background: #f8f9fa; padding: 2em 0; font-size: 0.8em; text-align: center; }
 img { float: left; margin: 0 2em 5em 0; }
 a img { border: 0; }
 h1 { margin-top: 1em; font-size: 1.2em; }
@@ -328,9 +328,9 @@ a { color: #36c; text-decoration: none; }
 a:hover { text-decoration: underline; }
 em { color: #72777d; font-style: normal; }
 @media (prefers-color-scheme: dark) {
-  body { background: transparent; color: #dfdedd; }
-  a { color: #9e9eff; }
-  em { color: #8d8882; }
+  body { background: #101418; color: #eaecf0; }
+  .footer { border-color: #404244; background: #202122; }
+  a { color: #88a3e8; }
   #logo { filter: invert(1) hue-rotate(180deg); }
 }
 </style>
