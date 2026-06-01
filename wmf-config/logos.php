dc9dc2c6ce69d328f8d4d516f33810e45494ba5a
Chlod Alejandro <chlod@chlod.net>
2026-06-02 01:25:59 +0800

    nlwiki: change to Wikipedia 25 logo
    This changes the wiki logo, wordmark, and tagline of the English
    Wikipedia to a version with Wikipedia 25 branding. More info on
    Wikipedia 25 branding can be found at https://w.wiki/FQcB.
    
    Logo variant design by User:TheWanderingTraders with input from
    the Wikimedia Foundation. Wordmark designed by the Wikimedia
    Foundation. Taglines translated by Ciell. Permalink to
    consensus: https://w.wiki/QVhv
    
    Bug: T424519
    Change-Id: I2db0eb2dcc4dde5bd5998bf53b70f91333cec168

--- wmf-config/logos.php
+++ wmf-config/logos.php
@@ -268,7 +268,7 @@ return [
 	'newwiki' => '/static/images/project-logos/newwiki.png', // T42285
 	'ngwiki' => '/static/images/project-logos/ngwiki.png',
 	'niawiki' => '/static/images/project-logos/niawiki.png',
-	'nlwiki' => '/static/images/project-logos/nlwiki.png',
+	'nlwiki' => '/static/images/project-logos/nlwiki-25.png', // T424519
 	'nnwiki' => '/static/images/project-logos/nnwiki.png',
 	'novwiki' => '/static/images/project-logos/novwiki.png', // T46974
 	'nowiki' => '/static/images/project-logos/nowiki.png',
@@ -1169,7 +1169,7 @@ return [
 	'newiki' => '/static/images/project-logos/newiki-2x.png', // T139240
 	'newwiki' => '/static/images/project-logos/newwiki-2x.png', // T150618
 	'niawiki' => '/static/images/project-logos/niawiki-2x.png',
-	'nlwiki' => '/static/images/project-logos/nlwiki-2x.png',
+	'nlwiki' => '/static/images/project-logos/nlwiki-25-2x.png',
 	'nnwiki' => '/static/images/project-logos/nnwiki-2x.png', // T150618
 	'novwiki' => '/static/images/project-logos/novwiki-2x.png', // T150618
 	'nowiki' => '/static/images/project-logos/nowiki-2x.png', // T150618
@@ -2433,6 +2433,11 @@ return [
 		'src' => '/static/images/mobile/copyright/wikipedia-wordmark-new.svg',
 		'width' => 120,
 		'height' => 28,
+	],
+	'nlwiki' => [ // T424519
+		'src' => '/static/images/mobile/copyright/wikipedia-wordmark-nl-25.svg',
+		'width' => 140,
+		'height' => 22,
 	],
 	'novwiki' => [
 		'src' => '/static/images/mobile/copyright/wikipedia-wordmark-nov.svg',
@@ -6289,10 +6294,10 @@ return [
 		'width' => 110,
 		'height' => 14,
 	],
-	'nlwiki' => [ // T258552
-		'src' => '/static/images/mobile/copyright/wikipedia-tagline-nl.svg',
-		'width' => 120,
-		'height' => 13,
+	'nlwiki' => [ // T258552, T424519
+		'src' => '/static/images/mobile/copyright/wikipedia-tagline-nl-25.svg',
+		'width' => 139,
+		'height' => 12,
 	],
 	'nnwiki' => [
 		'src' => '/static/images/mobile/copyright/wikipedia-tagline-nn.svg',
@@ -7862,6 +7867,7 @@ return [
 	'enwiki' => '/static/images/icons/enwiki-25.svg', // T414271
 	'itwiki' => '/static/images/icons/itwiki-temp25.svg',
 	'kkwiki' => '/static/images/icons/kkwiki-temp25.svg', // T414267
+	'nlwiki' => '/static/images/icons/nlwiki-25.svg',
 
 	// Wiktionary
 	'enwiktionary' => '/static/images/icons/enwiktionary.svg',
