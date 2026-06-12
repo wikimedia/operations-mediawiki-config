ee3d1b814a5c342eae33f6c35ab49ccc1bbfcc6e
Ahmon Dancy <adancy@wikimedia.org>
2026-06-12 14:22:00 -0700

    beta: Add deployment-db15 to db-labs config at weight 0
    Add deployment-db15 (Trixie / MariaDB 10.11) to the beta sectionLoads,
    hostsByName, and externalLoads at weight 0 for testing.
    
    Bug: T428930
    Change-Id: I6fee89e0c6f9980403dbb58626b8ac1a460e8def

--- wmf-config/db-labs.php
+++ wmf-config/db-labs.php
@@ -20,6 +20,7 @@ if ( $wmgRealm == 'labs' ) {
 			'DEFAULT' => [
 				'deployment-db11' => 0,
 				'deployment-db14' => 400,
+				'deployment-db15' => 0,
 			],
 		],
 
@@ -39,6 +40,8 @@ if ( $wmgRealm == 'labs' ) {
 			'deployment-db11' => '172.16.5.150:3306',
 			// deployment-db14.deployment-prep.eqiad1.wikimedia.cloud
 			'deployment-db14' => '172.16.5.170:3306',
+			// deployment-db15.deployment-prep.eqiad1.wikimedia.cloud
+			'deployment-db15' => '172.16.20.82:3306',
 		],
 
 		'externalLoads' => [
@@ -47,6 +50,8 @@ if ( $wmgRealm == 'labs' ) {
 				'172.16.5.150:3306' => 1,
 				// deployment-db14.deployment-prep.eqiad1.wikimedia.cloud
 				'172.16.5.170:3306' => 3,
+				// deployment-db15.deployment-prep.eqiad1.wikimedia.cloud
+				'172.16.20.82:3306' => 0,
 			],
 
 			'flow_cluster1' => [
@@ -54,6 +59,8 @@ if ( $wmgRealm == 'labs' ) {
 				'172.16.5.150:3306' => 1,
 				// deployment-db14.deployment-prep.eqiad1.wikimedia.cloud
 				'172.16.5.170:3306' => 3,
+				// deployment-db15.deployment-prep.eqiad1.wikimedia.cloud
+				'172.16.20.82:3306' => 0,
 			],
 
 			'extension1' => [
@@ -61,6 +68,8 @@ if ( $wmgRealm == 'labs' ) {
 				'172.16.5.150:3306' => 1,
 				// deployment-db14.deployment-prep.eqiad1.wikimedia.cloud
 				'172.16.5.170:3306' => 3,
+				// deployment-db15.deployment-prep.eqiad1.wikimedia.cloud
+				'172.16.20.82:3306' => 0,
 			],
 		],
 
