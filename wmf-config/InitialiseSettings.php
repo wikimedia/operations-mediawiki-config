f2c5163f956289be15d13d650edefd96f19a3d81
Caro Medelius <cmedelius@wikimedia.org>
2026-06-15 11:39:12 -0700

    Enable "exit the editor" survey on 11 wikis for phase 2
    Bug: T426132
    Change-Id: I6ee0a9259cfa29bdf5c8b2718035e894bb482ff0

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -8497,6 +8497,22 @@ return [
 	'enwiki' => 'source',
 ],
 
+'wgMFEnableAbandonSurvey' => [
+	'default' => false,
+	// T422931, T426132
+	'frwiki' => true,
+	'dewiki' => true,
+	'jawiki' => true,
+	'eswiki' => true,
+	'ruwiki' => true,
+	'zhwiki' => true,
+	'plwiki' => true,
+	'hewiki' => true,
+	'arwiki' => true,
+	'kowiki' => true,
+	'viwiki' => true,
+],
+
 'wgExtraGenderNamespaces' => [
 	'default' => [],
 	'cswiki' => [
