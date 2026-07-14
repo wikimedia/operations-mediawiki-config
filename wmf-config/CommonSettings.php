eaff2632f22e66a43086f43863cb3c924e540c26
Timo Tijhof <krinkle@fastmail.com>
2026-07-14 16:25:57 +0100

    Set $wgMathInternalRestbaseURL explicitly
    == Status quo in theory ==
    
    From wmf-config:
    
    ```
    'wmgUseRestbaseVRS' => [
    	'default' => true,
    	…
    ],
    
    …
    if ( $wmgUseRestbaseVRS ) {
    	$wgVirtualRestConfig['modules']['restbase'] = [
    		'url' => $wmgLocalServices['restbase'],
    		'domain' => $wgCanonicalServer,
    		…
    ```
    
    From Math/extension.json:
    
    ```
    MathUseInternalRestbasePath: { default: true }
    ```
    
    From Math/Hooks.php:
    
    ```
    $internalRestbaseURL = $config->get( 'MathInternalRestbaseURL' );
    $useInternalRestbasePath = $config->get( 'MathUseInternalRestbasePath' );
    $virtualRestConfig = $config->get( 'VirtualRestConfig' );
    
    if ( !$internalRestbaseURL ) {
    	if ( isset( $virtualRestConfig['modules']['restbase'] ) ) {
    		$restBaseUrl = $virtualRestConfig['modules']['restbase']['url'];
    		$restBaseUrl = rtrim( $restBaseUrl, '/' );
    		$restBaseDomain = $virtualRestConfig['modules']['restbase']['domain'] ?? 'localhost';
    		$restBaseDomain = preg_replace( '/^((https?:)?\/\/)?([^\/:]+?)(:\d+)?\/?$/', '$3', $restBaseDomain );
    		$internalRestbaseURL = "$restBaseUrl/$restBaseDomain/";
    	} else {
    		…
    	}
    	$settings->overrideConfigValue( 'MathInternalRestbaseURL', $internalRestbaseURL );
    }
    ```
    
    == Status quo emperically ==
    
    ```
    krinkle at deploy1003.eqiad.wmnet in ~$ mwscript eval.php enwiki
    > return $wgMathInternalRestbaseURL;
    http://localhost:6011/en.wikipedia.org/
    ```
    
    Where http://localhost:6011 matches wmf-config/ProductionServices.php#restbase
    
    == Simplified ==
    
    This patch:
    
    ```
    $wgMathInternalRestbaseURL = $wmgLocalServices['restbase'] . "/$wgServerName/";
    ```
    
    Once in place, the complicated fallback code involving the deprecated
    $wgVirtualRestConfig variable can be removed from the Math extension.
    
    Bug: T349582
    Change-Id: I9c197a985c1f2dd77250dce3d243b3dde464efe7

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -3226,11 +3226,14 @@ if ( $wmgUseMath ) {
 	// HACK: $wgServerName is not available yet at this point, it's set by Setup.php
 	// so use a hook
 	$wgExtensionFunctions[] = static function () {
-		global $wgServerName, $wgMathFullRestbaseURL, $wmgRealm;
+		global $wgServerName, $wgMathFullRestbaseURL, $wmgRealm,
+			$wgMathInternalRestbaseURL;
 
 		$wgMathFullRestbaseURL = $wmgRealm === 'production'
 			? 'https://wikimedia.org/api/rest_'  // T136205
 			: "//$wgServerName/api/rest_";
+
+		$wgMathInternalRestbaseURL = $wmgLocalServices['restbase'] . "/$wgServerName/";
 	};
 }
 
