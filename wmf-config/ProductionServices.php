c3d753935c4f86915b9b158386824f2b939d8d3e
Effie Mouzeli <effie@wikimedia.org>
2026-07-16 17:13:10 +0300

    ProductionServices: repool poolcounter2006 after reboot
    Put last rebooted poolcounter host back to prod.
    
    Depends-On: If5f2ed6f4a17802b85bd84377ae5e2e3a007e784
    Bug: T431705
    Change-Id: I7c7c17986fe95063dfc51acc067f2eed1693b8a2

--- wmf-config/ProductionServices.php
+++ wmf-config/ProductionServices.php
@@ -248,6 +248,7 @@ $services = [
 
 		'poolcounter' => [
 			'10.192.0.9',    # poolcounter2005.codfw.wmnet
+			'10.192.16.6',   # poolcounter2006.codfw.wmnet
 		],
 
 		// LOOKING FOR parsercache-dbs? It no longer lives in the production
