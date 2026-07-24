0fefc42ac79d45878755921a9e2186945658d05a
Ahmon Dancy <adancy@wikimedia.org>
2026-07-23 19:48:46 -0700

    wmf-config/db-dev.php: Configure $wgLBFactoryConf
    Configure `$wgLBFactoryConf` to be somewhat more production-like in
    train-dev.  This allows us to enable more extensions.
    
    Change-Id: I41b109ea73b643fbad565652af02a7318f600f93

--- wmf-config/db-dev.php
+++ wmf-config/db-dev.php
@@ -5,3 +5,58 @@
 $wmgParserCacheDBs = [];
 
 $wmgMainStashServers = [];
+
+$wgDBTableOptions = "ENGINE=InnoDB, DEFAULT CHARSET=binary";
+
+$wgLBFactoryConf = [
+
+	# Requires 'sectionsByDB', 'sectionLoads', 'serverTemplate'
+
+	'class' => 'LBFactoryMulti',
+
+	# Everyone to DEFAULT
+	'sectionsByDB' => [],
+
+	'sectionLoads' => [
+		'DEFAULT' => [
+			'db' => 0,
+		],
+	],
+
+	'serverTemplate' => [
+		'dbname'	  => $wgDBname,
+		'user'		  => $wgDBuser,
+		'password'	  => $wgDBpassword,
+		'type'		  => 'mysql',
+		'flags'		  => DBO_DEFAULT | ( $wgDebugDumpSql ? DBO_DEBUG : 0 ),
+		// 5 minutes
+		'max lag'	  => 300,
+	],
+
+	'hostsByName' => [
+		'db' => 'db:3306',
+	],
+
+	'externalLoads' => [
+		'cluster1' => [
+			'db:3306' => 1,
+		],
+
+		'flow_cluster1' => [
+			'db:3306' => 1,
+		],
+
+		'extension1' => [
+			'db:3306' => 1,
+		],
+	],
+
+	'templateOverridesByCluster' => [
+		'cluster1' => [ 'blobs table' => 'blobs1' ],
+		'flow_cluster1' => [ 'blobs table' => 'blobs_flow1' ],
+	],
+];
+
+$wgDefaultExternalStore = [
+	'DB://cluster1',
+];
