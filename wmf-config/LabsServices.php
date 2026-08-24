f9e022a77b87944fce9e0dcd3a1c16eae20309aa
Taavi Väänänen <hi@taavi.wtf>
2026-08-24 23:26:36 +0300

    Drop irc host from the Beta Cluster
    Bug: T396088
    Change-Id: Ibdef2245a29f4b859654454e352fc7517074e50f

--- wmf-config/LabsServices.php
+++ wmf-config/LabsServices.php
@@ -70,7 +70,7 @@ return [
 
 		'wikifunctions-orchestrator' => 'deployment-docker-wikifunctions01.deployment-prep.eqiad1.wikimedia.cloud:6254',
 
-		'irc' => [ 'irc.svc.deployment-prep.eqiad1.wikimedia.cloud' ],
+		'irc' => [],
 		'redis_default_lock' => [
 			'rdb1' => 'deployment-rdb01.deployment-prep.eqiad1.wikimedia.cloud',
 		],
