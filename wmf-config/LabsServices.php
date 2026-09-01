0a9a37b2325009f3ed3e51f6ac82de49b6f948ae
Ferran Tufan <southparkfan223@hotmail.com>
2026-09-02 00:11:57 +0200

    LabsServices: adjust Arc Lamp and Excimer URIs to use service RRs
    Bug: T436464
    Change-Id: I26d5173abd241bc06548c110db1fffea7f98a006

--- wmf-config/LabsServices.php
+++ wmf-config/LabsServices.php
@@ -10,10 +10,10 @@
 return [
 	'eqiad' => [
 		'udp2log' => 'deployment-mwlog02.deployment-prep.eqiad1.wikimedia.cloud:8420',
-		'xenon' => 'deployment-mwlog02.deployment-prep.eqiad1.wikimedia.cloud',
+		'xenon' => 'arclamp.svc.deployment-prep.eqiad1.wikimedia.cloud',
 		'xhgui-pdo' => 'mysql:host=deployment-db15.deployment-prep.eqiad1.wikimedia.cloud;dbname=xhgui',
 		'excimer-ui-url' => 'https://performance.wikimedia.beta.wmcloud.org/excimer/',
-		'excimer-ui-server' => 'https://deployment-webperf21.deployment-prep.eqiad1.wikimedia.cloud/excimer/',
+		'excimer-ui-server' => 'https://webperf.svc.deployment-prep.eqiad1.wikimedia.cloud/excimer/',
 		'statsd' => 'prometheus-labmon.eqiad.wmnet',
 		'search-chi' => [
 			// These MUST match the installed SSL certs
