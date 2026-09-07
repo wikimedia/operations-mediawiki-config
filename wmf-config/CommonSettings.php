21f0850e625bae4a379500a5c12ea78fbb4b7eda
Alexander Vorwerk <zabe@avorwerk.net>
2026-09-07 21:41:38 +0200

    Move production reads for commons link tables to x4 for 10% of requests
    Bug: T437108
    Change-Id: Ie2c4a2a609557395bb56264d33bacc8981a09baf

--- wmf-config/CommonSettings.php
+++ wmf-config/CommonSettings.php
@@ -2848,8 +2848,8 @@ if ( $wmgUseLiquidThreads || $wmgLiquidThreadsFrozen ) {
 
 }
 
-// 1% of requests use x4 for querying commons' virtual-links
-if ( mt_rand( 1, 100 ) === 1 ) {
+// 10% of requests use x4 for querying commons' virtual-links
+if ( mt_rand( 1, 10 ) === 1 ) {
 	$wgRemoteVirtualDomainsMapping['commonswiki'] = [
 		'virtual-links' => [ 'cluster' => 'extension4', 'db' => 'commonswiki' ],
 	];
