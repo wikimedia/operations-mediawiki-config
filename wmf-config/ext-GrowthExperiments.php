b7fc93698961d5a8e3206802443c90f4548b6810
Jon Robson <jdlrobson@gmail.com>
2026-09-11 15:27:27 -0700

    Enable Reading Recommendations experiment on test wiki
    Bug: T437665
    Change-Id: Ifd3a7486b0497a90ba0282931d99b0e99b00d1d5

--- wmf-config/ext-GrowthExperiments.php
+++ wmf-config/ext-GrowthExperiments.php
@@ -884,4 +884,13 @@ return [
 	'testwiki' => '2026-09-09T13:00:00',
 ],
 
+'wgGEHomepageReadingRecommendationsEnabled' => [
+	'default' => false,
+	// T437665
+	'testwiki' => true,
+	'test2wiki' => true,
+],
+'wgGEHomepageReadingRecommendationsFeaturedCategory' => [
+	'default' => 'Category:Featured articles',
+],
 ];
