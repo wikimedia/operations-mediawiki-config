c78d2dcef6fa9216ba3246f0a3f485ac7291be1f
Robertsky <contact@robertsky.com>
2026-09-17 19:52:45 +0000

    Revert "Update wikimania wordmark for 2026"
    This reverts commit 792ed38bb4329240ce86418988e354b7bbe5d7f7.
    
    Reason for revert: change back to generic wordmark post conference.
    
    Bug: T437625 
    Change-Id: I914f211616e07ded649ca9a893d05b9bec80e0a0

--- wmf-config/logos.php
+++ wmf-config/logos.php
@@ -5205,10 +5205,10 @@ return [
 		'width' => 145,
 		'height' => 17,
 	],
-	'wikimaniawiki' => [ // T286405, T413331
+	'wikimaniawiki' => [ // T286405
 		'src' => '/static/images/mobile/copyright/wikimaniawiki-wordmark.svg',
-		'width' => 127,
-		'height' => 50,
+		'width' => 144,
+		'height' => 17,
 	],
 	'wikitech' => [ // T334666
 		'src' => '/static/images/mobile/copyright/wikitech-wordmark.svg',
