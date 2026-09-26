c0def662aa09053b5f0a20e2b41423666ea2a72c
Amir Sarabadani <Ladsgroup@gmail.com>
2026-09-26 05:17:08 +0200

    Disable Score exec
    This breaks execution of bash commands from score
    
    Bug: T439297
    Bug: T438443
    Change-Id: I3623dcd1de0c7b80f7cb6b3c29f3c751bebb58e0

--- wmf-config/InitialiseSettings.php
+++ wmf-config/InitialiseSettings.php
@@ -3978,7 +3978,8 @@ return [
 # Set to false in case of emergency to disable Score with a nice
 # fallback message instead of breaking pages with error messages
 'wmgUseScoreShellbox' => [
-	'default' => true,
+	// Emergency
+	'default' => false,
 	# Do not enable on higher-security wikis
 	'private' => false,
 	'lockeddown' => false,
